e89a15b4fc87920114a08d75d6161e7363f8f138
Mark Dittmer <markdittmer@google.com>
2016-01-26 14:57:04 -0500

    Testing tools update: add some more branches to example test; make npm checkCoverage run on any input -- relies on hook to pass a good code coverage summary to work properly

--- src/Alpha.js
+++ src/Alpha.js
@@ -2,8 +2,13 @@ var GLOBAL = global || this;
 function Alpha(a) {
   if (a) {
     this.truthy = true;
+    if (a.b) {
+      this.extraTruthy = true;
+    } else {
+      this.extraTruthy = false;
+    }
   } else {
-    this.truthy = false;
+    this.truthy = this.extraTruthy = false;
   }
 }
 GLOBAL.Alpha = Alpha;
--- test/js/Alpha.js
+++ test/js/Alpha.js
@@ -5,9 +5,16 @@ describe('Alpha', function() {
   it('Truthy truth', function() {
     var alpha = new Alpha(true);
     expect(alpha.truthy).toBe(true);
+    expect(alpha.extraTruthy).toBe(false);
+  });
+  it('Extra truthy truth', function() {
+    var alpha = new Alpha({ b: true });
+    expect(alpha.truthy).toBe(true);
+    expect(alpha.extraTruthy).toBe(true);
   });
   it('Falsey false', function() {
     var alpha = new Alpha(false);
     expect(alpha.truthy).toBe(false);
+    expect(alpha.extraTruthy).toBe(false);
   });
 });
