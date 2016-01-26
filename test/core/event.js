bcf7395bd1dc2f1333443dab28fe94c94b830db8
Jackson Stone <jacksonic@google.com>
2016-01-26 15:53:10 -0500

    Refactored test with beforeEach and afterEach

--- test/core/event.js
+++ test/core/event.js
@@ -3,76 +3,72 @@ var _events = require('../../src/core/event.js');
 var EventPublisher = _events.EventPublisher;
 var EventService = _events.EventService;
 
-describe('EventPublisher', function() {
+describe('EventPublisher.hasListeners()', function() {
+  var ep;
 
-  it('reports hasListeners correctly for no listeners, ever', function() {
-    var ep = Object.create(EventPublisher);
+  beforeEach(function() {
+    ep = Object.create(EventPublisher);
+  });
+  afterEach(function() {
+    ep = null;
+  });
+
+  it('reports correctly for no listeners, ever', function() {
     expect(ep.subs_).toBeNull();
     expect(ep.hasListeners()).toBe(false);
   });
 
-  it('reports hasListeners correctly for no listeners after removing them', function() {
-    var ep = Object.create(EventPublisher);
+  it('reports correctly for no listeners after removing them', function() {
     ep.subs_ = {}; // listeners might have been there, but removed.
     expect(ep.hasListeners()).toBe(false);
   });
 
-  it('reports hasListeners correctly for one listener', function() {
-    var ep = Object.create(EventPublisher);
+  it('reports correctly for one listener', function() {
     ep.subs_ = { null: ['myFakeListener'] };
     expect(ep.hasListeners()).toBe(true);
   });
 
-  it('reports hasListeners correctly for an empty listener list', function() {
-    var ep = Object.create(EventPublisher);
+  it('reports correctly for an empty listener list', function() {
     ep.subs_ = { null: [] };
     expect(ep.hasListeners()).toBe(false);
   });
 
-  it('reports hasListeners correctly for a specific listener', function() {
-    var ep = Object.create(EventPublisher);
+  it('reports correctly for a specific listener', function() {
     ep.subs_ = { 'cake': { null: ['myFakeListener'] } };
     expect(ep.hasListeners(['cake'])).toBe(true);
   });
 
-  it('reports hasListeners correctly and ignores a specific listener', function() {
-    var ep = Object.create(EventPublisher);
+  it('reports correctly and ignores a specific listener', function() {
     ep.subs_ = { 'cake': { null: ['myFakeListener'] } };
     expect(ep.hasListeners(['lie'])).toBe(false);
   });
 
-  it('reports hasListeners correctly for a multi-level topic with a listener', function() {
-    var ep = Object.create(EventPublisher);
+  it('reports correctly for a multi-level topic with a listener', function() {
     ep.subs_ = { 'the' : { 'cake': { 'is' : { null: ['myFakeListener'] } } } };
     expect(ep.hasListeners(['the','cake','is'])).toBe(true);
   });
 
-  it('reports hasListeners correctly for a multi-level topic with no listener', function() {
-    var ep = Object.create(EventPublisher);
+  it('reports correctly for a multi-level topic with no listener', function() {
     ep.subs_ = { 'the' : { 'cake': { 'is' : { null: ['myFakeListener'] } } } };
     expect(ep.hasListeners(['the','cake'])).toBe(false);
   });
 
-  it('reports hasListeners correctly for a multi-level topic that is complete but empty listener list', function() {
-    var ep = Object.create(EventPublisher);
+  it('reports correctly for a multi-level topic that is complete but empty listener list', function() {
     ep.subs_ = { 'the' : { 'cake': { 'is' : { null: [] } } } };
     expect(ep.hasListeners(['the','cake', 'is'])).toBe(false);
   });
 
-  it('reports hasListeners correctly for a multi-level topic with a wildcard', function() {
-    var ep = Object.create(EventPublisher);
+  it('reports correctly for a multi-level topic with a wildcard', function() {
     ep.subs_ = { 'the' : { 'cake': { 'is' : { null: ['myFakeListener'] } } } };
     expect(ep.hasListeners(['the', EventService.WILDCARD])).toBe(true);
   });
 
-  it('reports hasListeners correctly for a root level wildcard', function() {
-    var ep = Object.create(EventPublisher);
+  it('reports correctly for a root level wildcard', function() {
     ep.subs_ = { 'the' : { 'cake': { 'is' : { null: ['myFakeListener'] } } } };
     expect(ep.hasListeners([EventService.WILDCARD])).toBe(true);
   });
 
-  it('reports hasListeners correctly for a given topic but no listeners', function() {
-    var ep = Object.create(EventPublisher);
+  it('reports correctly for a given topic but no listeners', function() {
     expect(ep.hasListeners([EventService.WILDCARD])).toBe(false);
   });
 
