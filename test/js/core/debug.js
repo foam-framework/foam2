c4da42582f954b19f3afbad7c53c872f4b101fc8
Jackson Stone <jacksonic@google.com>
2016-02-04 14:13:02 -0500

    Debug test added, Pub/sub test fixup

---
+++ test/js/core/debug.js
@@ -0,0 +1,24 @@
+var corePromise = GLOBAL.loadCoreTo('core/debug.js');
+var beforeEachTest = function(callback) {
+  return beforeEach(function(done) {
+    corePromise.then(function() {
+      callback();
+      done();
+    });
+  });
+};
+
+
+describe('coverage for debugging helpers', function() {
+  beforeEachTest(function() {
+  });
+  afterEach(function() {
+  });
+
+  it('covers describe()', function() {
+    var p = /*X.*/Property.create({});
+    p.describe();
+    p.cls_.describe();
+  });
+
+});
