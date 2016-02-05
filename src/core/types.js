ac48c7803a00fe7b1c07fec994a1cbca025a165a
Jackson Stone <jacksonic@google.com>
2016-02-05 16:40:34 -0500

    Type checking primitives and bare returns tested

--- src/core/types.js
+++ src/core/types.js
@@ -31,27 +31,32 @@ CLASS({
     { name: 'typeName' },
     { name: 'type' },
     { name: 'optional', defaultValue: false },
+    { name: 'index', defaultValue: -1 }
   ],
   
   methods: [
     /** If any type checks are failed, a TypeError is thrown.
         @param i optional argument index for error reporting. */
-    function check(arg, i) {
-      i = i ? ''+i+', ' : '';
+    function validate(arg) {
+      i = ( this.index >= 0 ) ? ' '+this.index+', ' : ', ';
       // optional check
-      if ( ( ! arg ) && typeof arg === 'undefined' && ! this.optional ) {
-        throw new TypeError(this.PREFIX+' '+i + this.name+', is not optional, but was undefined in a function call.');
+      if ( ( ! arg ) && typeof arg === 'undefined' ) {
+        if ( ! this.optional ) {
+          throw new TypeError(this.PREFIX + i + this.name+', is not optional, but was undefined in a function call.');
+        } else {
+          return; // value is undefined, but ok with that
+        }
       }
       // type this for non-modelled types (no model, but a type name specified)
       if ( ! this.type ) {
         if ( this.typeName && typeof arg !== this.typeName ) {
-          throw new TypeError(this.PREFIX+' '+i + this.name+', expected type '+this.typeName+' but passed '+(typeof arg)+'.');
+          throw new TypeError(this.PREFIX + i + this.name+', expected type '+this.typeName+' but passed '+(typeof arg)+'.');
         } // else no this: no type, no typeName
       } else {
         // have a modelled type
-        if ( ! this.type.isInstance(arg) ) {        
+        if ( ! arg.cls_ || ! this.type.isInstance(arg) ) {   // TODO: .cls_ check in isInstance() instead?     
           var gotType = (arg.cls_) ? arg.cls_.name : typeof arg;  
-          throw new TypeError(this.PREFIX+' '+i + this.name+', expected type '+this.typeName+' but passed '+gotType+'.');
+          throw new TypeError(this.PREFIX + i + this.name+', expected type '+this.typeName+' but passed '+gotType+'.');
         }
       }
     }
@@ -73,8 +78,8 @@ CLASS({
   * @return An array of Argument objects.
   */
 foam.types.getFunctionArgs = function getFunctionArgs(fn) {
-  
-  var args = fn.toString().match(/^function[ _$\w]*\((.*)\)/)[1];
+  // strip newlines and find the function(...) declaration
+  var args = fn.toString().replace(/(\r\n|\n|\r)/gm,"").match(/^function(\s+[_$\w]+|\s*)\((.*)\)/)[2];
   if ( args )
     args = args.split(',').map(function(name) { return name.trim(); });
   else
@@ -82,16 +87,18 @@ foam.types.getFunctionArgs = function getFunctionArgs(fn) {
   
   var ret = [];
   // check each arg for types
+  var index = 0;
   args.forEach(function(arg) {
     // Optional commented type(incl. dots for packages), argument name, optional commented return type
     // ws [/* ws package.type? ws */] ws argname ws [/* ws retType ws */]
-    var typeMatch = arg.match(/^\s*(\/\*\s*([\w\.]+)(\?)?\s*\*\/)?\s*(\w+)\s*(\/\*\s*([\w\.]+)\s*\*\/)?\s*/);
+    var typeMatch = arg.match(/^\s*(\/\*\s*([\w._$]+)(\?)?\s*\*\/)?\s*([\w_$]+)\s*(\/\*\s*([\w._$]+)\s*\*\/)?\s*/);
     if ( typeMatch ) {
       ret.push(/*X.*/Argument.create({
         name: typeMatch[4],
         typeName: typeMatch[2],
         type: global[typeMatch[2]],
-        optional: typeMatch[3] == '?'
+        optional: typeMatch[3] == '?',
+        index: index++,
       }));
       // TODO: this is only valid on the last arg
       if ( typeMatch[6] ) {
@@ -101,7 +108,18 @@ foam.types.getFunctionArgs = function getFunctionArgs(fn) {
         });
       }
     } else {
-      throw "foam.types.getFunctionArgs argument parsing error: " + typeMatch.toString();
+      // check for bare return type with no args
+      typeMatch = arg.match(/^\s*\/\*\s*([\w._$]+)\s*\*\/\s*/);
+      if ( typeMatch) {
+        if ( typeMatch[1] ) {
+          ret.returnType = /*X.*/ReturnValue.create({
+            typeName: typeMatch[1],
+            type: global[typeMatch[1]]
+          });
+        }
+      } else {
+        throw "foam.types.getFunctionArgs argument parsing error: " + args.toString();
+      }
     } 
   });
 
@@ -109,10 +127,14 @@ foam.types.getFunctionArgs = function getFunctionArgs(fn) {
 }
 
 /** Decorates the given function with a runtime type checker.
+  * Types should be denoted before each argument:
+  * <code>function(\/\*TypeA\*\/ argA, \/\*string\*\/ argB) { ... }</code>
+  * 
   * @fn The function to decorate. The toString() of the function must be
   *     accurate.
-  * @return A new function that will throw errors if arguments or the return
-  *         doesn't match the declared types, and run the original function.
+  * @return A new function that will throw errors if arguments
+  *         doesn't match the declared types, run the original function,
+  *         then type check the returned value.
   */
 foam.types.typeCheck = function typeCheck(fn) {
   // parse out the arguments and their types
@@ -120,14 +142,15 @@ foam.types.typeCheck = function typeCheck(fn) {
   var ret = function() {
     // check each incoming argument
     for (var i = 0; i < arguments.length; ++i) {
-      args[i].check(arguments[i]);
+      if ( ! args[i] ) break; // don't validate extra args
+      args[i].validate(arguments[i]);
     }
     // If nothing threw an exception, we are free to run the function
     var retVal = fn.apply(this, arguments);
   
     // check the return value
     if ( args.returnValue ) {
-      args.returnValue.check(retVal);
+      args.returnValue.validate(retVal);
     }
     
     return retVal;
--- test/js/core/types.js
+++ test/js/core/types.js
@@ -9,18 +9,28 @@ var beforeEachTest = function(callback) {
   });
 };
 
+function makeTestFn() {
+  CLASS({  name: 'TypeA' });
+  CLASS({  name: 'TypeB' });
+  CLASS({  name: 'TypeBB', extends: 'TypeB' });
+  CLASS({  name: 'package.TypeC' });
+  CLASS({  name: 'RetType' });
+  return function test(/* TypeA */ paramA, /*TypeB?*/ paramB , /* package.TypeC*/ paramC, noType /* RetType */ ) {
+    return true;
+  }  
+}
+function makePrimitiveTestFn() {
+return function(/* string */ str, /*boolean*/ bool , 
+  /* function*/ func, /*object*/obj, /* number */num ) {
+    return true;
+  }  
+}
 
 describe('foam.types.getFunctionArgs', function() {
   var fn;
 
   beforeEachTest(function() {
-    fn = function test(/* TypeA */ paramA, /*TypeB?*/ paramB , /* package.TypeC*/ paramC, noType /* RetType */ ) {
-      return true;
-    }
-    CLASS({  name: 'TypeA' });
-    CLASS({  name: 'TypeB' });
-    CLASS({  name: 'package.TypeC' });
-    CLASS({  name: 'RetType' });
+    fn = makeTestFn();
   });
   afterEach(function() {
     fn = null;
@@ -48,12 +58,75 @@ describe('foam.types.getFunctionArgs', function() {
     expect(params.returnType.typeName).toEqual('RetType');
     
   });
+  it('accepts a return with no args', function() {
+    var params = foam.types.getFunctionArgs(function(/*RetType*/){});
+  
+    expect(params.returnType.typeName).toEqual('RetType');    
+  });
+});
 
+describe('Argument.validate', function() {
+  var fn;
 
-  // it('returns the type of the function return', function() {
-  //   var params = foam.types.getFunctionArgs(fn);
-  //
-  //
-  // });
+  beforeEachTest(function() {
+    fn = makeTestFn();
+  });
+  afterEach(function() {
+    fn = null;
+  });
+
+  it('allows optional args to be omitted', function() {
+    var params = foam.types.getFunctionArgs(fn);
+
+    expect(function() { params[1].validate(undefined); }).not.toThrow();
+    expect(function() { params[2].validate(undefined); }).toThrow();
+  });
+  it('checks modelled types', function() {
+    var params = foam.types.getFunctionArgs(fn);
+
+    expect(function() { params[0].validate(TypeA.create()); }).not.toThrow();
+    expect(function() { params[1].validate(TypeB.create()); }).not.toThrow();
+    expect(function() { params[1].validate(TypeBB.create()); }).not.toThrow(); //subclass should be ok
+    expect(function() { params[2].validate(global['package.TypeC'].create()); }).not.toThrow();
+
+    expect(function() { params[3].validate(TypeA.create()); }).not.toThrow(); // arg 3 not typed
+    expect(function() { params[3].validate(99); }).not.toThrow();
+
+    expect(function() { params.returnType.validate(RetType.create()); }).not.toThrow();
+  });
+  it('rejects wrong modelled types', function() {
+    var params = foam.types.getFunctionArgs(fn);
+
+    expect(function() { params[0].validate(TypeB.create()); }).toThrow();
+    expect(function() { params[1].validate(TypeA.create()); }).toThrow();
+    expect(function() { params[2].validate(RetType.create()); }).toThrow();
+
+    expect(function() { params.returnType.validate(global['package.TypeC'].create()); }).toThrow();
+  });
+  it('checks primitive types', function() {
+    var params = foam.types.getFunctionArgs(makePrimitiveTestFn());
+
+    // /* string */ str, /*boolean*/ bool , /* function*/ func, /*object*/obj, /* number */num 
+    expect(function() { params[0].validate('hello'); }).not.toThrow();
+    expect(function() { params[1].validate(true); }).not.toThrow();
+    expect(function() { params[2].validate(function() {}); }).not.toThrow();
+    expect(function() { params[3].validate({}); }).not.toThrow();
+    expect(function() { params[4].validate(86); }).not.toThrow();
+  });
+  it('rejects wrong primitive types', function() {
+    var params = foam.types.getFunctionArgs(makePrimitiveTestFn());
+
+    // /* string */ str, /*boolean*/ bool , /* function*/ func, /*object*/obj, /* number */num 
+    expect(function() { params[0].validate(78); }).toThrow();
+    expect(function() { params[1].validate('nice'); }).toThrow();
+    expect(function() { params[2].validate({}); }).toThrow();
+    expect(function() { params[3].validate(function() {}); }).toThrow();
+    expect(function() { params[4].validate(false); }).toThrow();
+  });
 
 });
+
+
+
+
+
