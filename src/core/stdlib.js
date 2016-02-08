07540e5fb3287b4b3898ee6e6ffea4b44da50372
Kevin Greer <kgr@chromium.org>
2016-02-08 12:08:59 -0500

    Restructured stdlib to use new foam.LIB() feature.

--- src/core/stdlib.js
+++ src/core/stdlib.js
@@ -18,74 +18,159 @@
 // Setup nodejs-like 'global' on web
 var global = global || this;
 
+
 // Top-Level of foam package
-foam = { array: {}, fn: {}, string: {}, util: {} };
-
-
-foam.fn.memoize1 = function memoize1(f) {
-  // Faster version of memoize() when only dealing with one argument.
-  var cache = {};
-  var g = function(arg) {
-    console.assert(arguments.length == 1, "Memoize1'ed functions must take exactly one argument.");
-    var key = arg ? arg.toString() : '';
-    if ( ! cache.hasOwnProperty(key) ) cache[key] = f.call(this, arg);
-    return cache[key];
-  };
-  foam.fn.setName(g, 'memoize1(' + f.name + ')');
-  return g;
+foam = {
+  Object:   Object.prototype,
+  Function: Function.prototype,
+  Array:    Array.prototype
 };
 
 
-foam.fn.setName = function setName(f, name) {
-  // Set a function's name for improved debugging and profiling
-  //Object.defineProperty(f, 'name', {value: name, configurable: true}); //TODO: re-enable if supported
+foam.LIB = function LIB(model) {
+  var proto;
+
+  function defineProperty(proto, key, map) {
+    console.log('defineProperty: ', key);
+
+    if ( ! map.value || proto === Object.prototype || proto === Array.prototype )
+      Object.defineProperty.apply(this, arguments);
+    else
+      proto[key] = map.value;
+  }
+
+  proto = foam[model.name] || ( foam[model.name] = {} );
+
+  if ( model.properties ) for ( var i = 0 ; i < model.properties.length ; i++ ) {
+    var p = model.properties[i];
+    defineProperty(
+      proto,
+      p.name,
+      { get: p.getter, enumerable: false });
+  }
+
+  for ( key in model.constants )
+    defineProperty(
+      proto,
+      key,
+      { value: model.constants[key], writable: true, enumerable: false });
+
+  if ( model.methods ) for ( var i = 0 ; i < model.methods.length ; i++ ) {
+    var m = model.methods[i];
+    defineProperty(
+      proto,
+      m.name,
+      { value: m.code || m, writable: true, enumerable: false });
+  }
 };
 
-/** convenience method to append 'arguments' onto a real array */
-foam.fn.appendArguments = function appendArguments(a, args, start) {
-  for ( var i = start ; i < args.length ; i++ ) a.push(args[i]);
-  return a;
-},
 
-/** convenience method to turn 'arguments' into a real array */
-foam.fn.argsToArray = function argsToArray(args) {
-  return foam.fn.appendArguments([], args, 0);
-},
+foam.LIB({
+  name: 'Object',
 
-
-foam.string.constantize = foam.fn.memoize1(function(str) {
-  // switchFromCamelCaseToConstantFormat to SWITCH_FROM_CAMEL_CASE_TO_CONSTANT_FORMAT
-  return str.replace(/[a-z][^0-9a-z_]/g, function(a) {
-    return a.substring(0,1) + '_' + a.substring(1,2);
-  }).toUpperCase();
+  properties: [
+    {
+      name: '$UID',
+      getter: (function() {
+        var id = 1;
+        return function() {
+          if ( Object.hasOwnProperty.call(this, '$UID__') ) return this.$UID__;
+          this.$UID__ = id;
+          id++;
+          return this.$UID__;
+        };
+      })()
+    }
+  ]
 });
 
 
-foam.string.pad = function(str, size) {
-  // Right pads to size if size > 0, Left pads to -size if size < 0
-  return size < 0 ?
-    (new Array(-size).join(' ') + str).slice(size)       :
-    (str + new Array(size).join(' ')).substring(0, size) ;
-};
+foam.LIB({
+  name: 'array',
+
+  methods: [
+    function argsToArray(args) {
+      /** convenience method to turn 'arguments' into a real array */
+      return foam.fn.appendArguments([], args, 0);
+    }
+  ]
+});
 
 
-foam.events = {
+foam.LIB({
+  name: 'events',
 
-  oneTime: function oneTime(listener) {
-    /** Create a "one-time" listener which unsubscribes itself after its first invocation. **/
-    return function(subscription) {
-      listener.apply(this, foam.fn.argsToArray(arguments));
-      subscription.destroy();
-    };
-  },
+  methods: [
+    function oneTime(listener) {
+      /** Create a "one-time" listener which unsubscribes itself after its first invocation. **/
+      return function(subscription) {
+        listener.apply(this, foam.array.argsToArray(arguments));
+        subscription.destroy();
+      };
+    },
 
-  consoleLog: function consoleLog(listener) {
-    /** Log all listener invocations to console. **/
-    return function() {
-      var args = foam.fn.argsToArray(arguments);
-      console.log(args);
+    function consoleLog(listener) {
+      /** Log all listener invocations to console. **/
+      return function() {
+        var args = foam.array.argsToArray(arguments);
+        console.log(args);
+        listener.apply(this, args);
+      };
+    }
+  ]
+});
 
-      listener.apply(this, args);
-    };
-  }
-};
+
+foam.LIB({
+  name: 'fn',
+
+  methods: [
+    function memoize1(f) {
+      // Faster version of memoize() when only dealing with one argument.
+      var cache = {};
+      var g = function(arg) {
+        console.assert(arguments.length == 1, "Memoize1'ed functions must take exactly one argument.");
+        var key = arg ? arg.toString() : '';
+        if ( ! cache.hasOwnProperty(key) ) cache[key] = f.call(this, arg);
+        return cache[key];
+      };
+      foam.fn.setName(g, 'memoize1(' + f.name + ')');
+      return g;
+    },
+
+    function setName(f, name) {
+      // Set a function's name for improved debugging and profiling
+      //Object.defineProperty(f, 'name', {value: name, configurable: true}); //TODO: re-enable if supported
+    },
+
+    function appendArguments(a, args, start) {
+      /** convenience method to append 'arguments' onto a real array */
+      for ( var i = start ; i < args.length ; i++ ) a.push(args[i]);
+      return a;
+    }
+  ]
+});
+
+
+foam.LIB({
+  name: 'string',
+
+  methods: [
+    {
+      name: 'constantize',
+      code: foam.fn.memoize1(function(str) {
+        // switchFromCamelCaseToConstantFormat to SWITCH_FROM_CAMEL_CASE_TO_CONSTANT_FORMAT
+        return str.replace(/[a-z][^0-9a-z_]/g, function(a) {
+          return a.substring(0,1) + '_' + a.substring(1,2);
+        }).toUpperCase();
+      })
+    },
+
+    function pad(str, size) {
+      // Right pads to size if size > 0, Left pads to -size if size < 0
+      return size < 0 ?
+        (new Array(-size).join(' ') + str).slice(size)       :
+        (str + new Array(size).join(' ')).substring(0, size) ;
+    }
+  ]
+});
