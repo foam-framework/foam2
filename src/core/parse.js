bdb9ec07ace027525cc6ba31f6713fd2b41e79f4
Adam Van Ymeren <adamvy@google.com>
2016-02-18 09:44:26 -0500

    Implement parsers

---
+++ src/core/parse.js
@@ -0,0 +1,508 @@
+foam.CLASS({
+  name: 'SimpleProperty',
+  extends: 'Property',
+  methods: [
+    function installInProto(proto) {}
+  ]
+});
+
+foam.CLASS({
+  package: 'foam.parsers',
+  name: 'StringPS',
+  properties: [
+    {
+      name: 'str',
+      type: 'Simple'
+    },
+    {
+      name: 'pos',
+      type: 'Simple'
+    },
+    {
+      name: 'head',
+      getter: function() {
+        return this.str[0][this.pos];
+      }
+    },
+    {
+      name: 'tail',
+      getter: function() {
+        if ( ! this.instance_.tail ) this.instance_.tail = foam.parsers.StringPS.create(this.str, this.pos + 1);
+        return this.instance_.tail;
+      },
+      setter: function(value) {
+        this.instance_.tail = value;
+      }
+    },
+    {
+      name: 'value',
+      setter: function(value) { this.instance_.value = value; },
+      getter: function() {
+        return this.instance_.value !== undefined ?
+          this.instance_.value :
+          this.str[0].charAt(this.pos - 1);
+      }
+    }
+  ],
+  methods: [
+    function initArgs(str, pos, tail, value) {
+      if ( arguments.length > 0 ) this.str = str;
+      else this.str = [''];
+      if ( arguments.length > 1 ) this.pos = pos;
+      else this.pos = 0
+      if ( arguments.length > 2 ) this.tail = tail;
+      if ( arguments.length > 3 ) this.value = value;
+    },
+    function setValue(value) {
+      // Force undefined values to null so that hasOwnProperty checks are faster.
+      if ( value === undefined ) value = null;
+      return foam.parsers.StringPS.create(
+        this.str,
+        this.pos,
+        this.tail,
+        value
+      );
+    },
+    function setString(s) {
+      this.str[0] = s;
+    }
+  ]
+});
+
+foam.CLASS({
+  package: 'foam.parsers',
+  name: 'ParserArrayProperty',
+  extends: 'ArrayProperty',
+  properties: [
+    {
+      name: 'adaptArrayElement',
+      defaultValue: function(a) {
+        return typeof(a) === 'string' ?
+          foam.parsers.Literal.create({ s: a }) :
+          a;
+      }
+    }
+  ]
+})
+
+foam.CLASS({
+  package: 'foam.parsers',
+  name: 'ParserProperty',
+  extends: 'Property',
+  properties: [
+    {
+      name: 'adapt',
+      defaultValue: function(_, v) { return typeof v == 'string' ? foam.parsers.Literal.create({ s: v }) : v }
+    }
+  ]
+});
+
+foam.CLASS({
+  package: 'foam.parsers',
+  name: 'ParserDecorator',
+  properties: [
+    {
+      name: 'p',
+      type: 'foam.parsers.Parser',
+      final: true
+    }
+  ]
+});
+
+foam.CLASS({
+  package: 'foam.parsers',
+  name: 'Literal',
+  properties: [
+    {
+      name: 's',
+      final: true
+    },
+    {
+      name: 'value',
+      final: true
+    }
+  ],
+  methods: [
+    function parse(ps, obj) {
+      var str = this.s;
+      for ( var i = 0 ; i < str.length ; i++, ps = ps.tail ) {
+        if ( str.charAt(i) !== ps.head ) return undefined;
+      }
+      return ps.setValue(this.value !== undefined ? this.value : str);
+    }
+  ]
+});
+
+foam.CLASS({
+  package: 'foam.parsers',
+  name: 'Alternate',
+  properties: [
+    {
+      name: 'args',
+      final: true,
+      type: 'foam.parsers.ParserArray'
+    }
+  ],
+  methods: [
+    function parse(ps, obj) {
+      // TODO: Should we remove the obj argument in favour of
+      // passing the obj along via context or something?
+      var args = this.args;
+      for ( var i = 0, p ; p = args[i] ; i++ ) {
+        var ret = p.parse(ps, obj);
+        if ( ret ) return ret;
+      }
+      return undefined;
+    }
+  ]
+});
+
+foam.CLASS({
+  package: 'foam.parsers',
+  name: 'Sequence',
+  properties: [
+    {
+      name: 'args',
+      final: true,
+      type: 'foam.parsers.ParserArray'
+    }
+  ],
+  methods: [
+    function parse(ps, obj) {
+      var ret = [];
+      var args = this.args;
+      for ( var i = 0, p ; p = args[i] ; i++ ) {
+        if ( ! ( ps = p.parse(ps, obj) ) ) return undefined;
+        ret.push(ps.value);
+      }
+      return ps.setValue(ret);
+    }
+  ]
+});
+
+foam.CLASS({
+  package: 'foam.parsers',
+  name: 'Sequence1',
+  properties: [
+    {
+      name: 'args',
+      final: true,
+      type: 'foam.parsers.ParserArray'
+    },
+    {
+      name: 'n',
+      final: true
+    }
+  ],
+  methods: [
+    function parse(ps, obj) {
+      var ret;
+      var args = this.args;
+      var n = this.n
+      for ( var i = 0, p ; p = args[i] ; i++ ) {
+        if ( ! ( ps = p.parse(ps, obj) ) ) return undefined;
+        if ( i == n ) ret = ps.value;
+      }
+      return ps.setValue(ret);
+    }
+  ]
+});
+
+foam.CLASS({
+  package: 'foam.parsers',
+  name: 'Optional',
+  extends: 'foam.parsers.ParserDecorator',
+  methods: [
+    function parse(ps, obj) {
+      return this.p.parse(ps, obj) || ps.setValue(null);
+    }
+  ]
+});
+
+// TODO: This should be a singleton
+foam.CLASS({
+  package: 'foam.parsers',
+  name: 'AnyChar',
+  methods: [
+    function parse(ps, obj) {
+      return ps.head ? ps.tail : undefined;
+    }
+  ]
+});
+
+foam.CLASS({
+  package: 'foam.parsers',
+  name: 'NotChars',
+  properties: [
+    {
+      name: 'string',
+      final: true
+    }
+  ],
+  methods: [
+    function parse(ps) {
+      return this.string.indexOf(ps.head) == -1 ?
+        ps.tail : undefined;
+    }
+  ]
+});
+
+foam.CLASS({
+  package: 'foam.parsers',
+  name: 'Repeat',
+  extends: 'foam.parsers.ParserDecorator',
+  methods: [
+    function parse(ps, obj) {
+      var ret = [];
+      var p = this.p;
+      while ( ps ) {
+        var last = ps;
+        ps = p.parse(ps, obj);
+        if ( ps ) ret.push(ps.value);
+      }
+      return last.setValue(ret);
+    }
+  ]
+});
+
+foam.CLASS({
+  package: 'foam.parsers',
+  name: 'Repeat0',
+  extends: 'foam.parsers.ParserDecorator',
+  methods: [
+    function parse(ps, obj) {
+      var res;
+      var p = this.p;
+      while ( res = p.parse(ps, obj) ) ps = res;
+      return ps.setValue('');
+    }
+  ]
+});
+
+foam.CLASS({
+  package: 'foam.parsers',
+  name: 'Not',
+  extends: 'foam.parsers.ParserDecorator',
+  properties: [
+    {
+      name: 'else',
+      final: true,
+      type: 'foam.parsers.Parser'
+    }
+  ],
+  methods: [
+    function parse(ps, obj) {
+      return this.p.parse(ps, obj) ?
+        undefined :
+        (this.else ? this.else.parse(ps, obj) : ps);
+    }
+  ]
+});
+
+foam.CLASS({
+  package: 'foam.parsers',
+  name: 'ParserAxiom',
+  extends: 'Method',
+  properties: [
+    {
+      name: 'parser'
+    }
+  ],
+  methods: [
+    function installInProto(proto) {
+      var parser = this.parser;
+      proto[this.name] = function(ps) {
+        return parser.parse(ps, this);
+      };
+    }
+  ]
+});
+
+foam.CLASS({
+  package: 'foam.parsers',
+  name: 'Symbol',
+  properties: [
+    {
+      name: 'name',
+      final: true
+    }
+  ],
+  methods: [
+    function parse(ps, obj) {
+      var p = obj[this.name];
+      if ( ! p ) {
+        console.error("No symbol found for", this.name);
+        return undefined;
+      }
+      return p.call(obj, ps);
+    }
+  ]
+});
+
+foam.CLASS({
+  package: 'foam.parsers',
+  name: 'ParsersAxiomProperty',
+  extends: 'AxiomArrayProperty',
+  methods: [
+    function seq() {
+      return foam.lookup('foam.parsers.Sequence').create({
+        args: foam.array.argsToArray(arguments)
+      });
+    },
+    function repeat0(p) {
+      return foam.lookup('foam.parsers.Repeat0').create({
+        p: p
+      });
+    },
+    function simpleAlt() {
+      return foam.lookup('foam.parsers.Alternate').create({
+        args: foam.array.argsToArray(arguments)
+      });
+    },
+    function alt() {
+      return foam.lookup('foam.parsers.Alternate').create({
+        args: foam.array.argsToArray(arguments)
+      });
+    },
+    function sym(name) {
+      return foam.lookup('foam.parsers.Symbol').create({
+        name: name
+      });
+    },
+    function seq1(n) {
+      return foam.lookup('foam.parsers.Sequence1').create({
+        n: n,
+        args: foam.array.argsToArray(arguments).slice(1)
+      });
+    },
+    function repeat(p) {
+      return foam.lookup('foam.parsers.Repeat').create({
+        p: p
+      });
+    },
+    function notChars(s) {
+      return foam.lookup('foam.parsers.NotChars').create({
+        string: s
+      });
+    },
+    function not(p, opt_else) {
+      return foam.lookup('foam.parsers.Not').create({
+        p: p,
+        else: opt_else
+      });
+    },
+    function optional(p) {
+      return foam.lookup('foam.parsers.Optional').create({
+        p: p,
+      });
+    },
+    function literal(s, value) {
+      return foam.lookup('foam.parsers.Literal').create({
+        s: s,
+        value: value
+      });
+    }
+  ],
+  properties: [
+    {
+      name: 'anyChar',
+      factory: function() {
+        return foam.lookup('foam.parsers.AnyChar').create();
+      }
+    },
+    {
+      name: 'adapt',
+      defaultValue: function(_, o, prop) {
+        if ( Array.isArray(o) ) return o;
+
+        if ( typeof o === "function" ) {
+          var args = o.toString().match(/\((.*?)\)/);
+          if ( ! args ) {
+            throw "Could not parse arguments from parser factory function";
+          }
+
+          args = args[1].split(",").map(function(a) { return a.trim(); });
+          for ( var i = 0 ; i < args.length; i++ ) {
+            if ( prop[args[i]] ) args[i] = prop[args[i]];
+            else {
+              var cls = foam.lookup(args[i]) || foam.lookup('foam.parsers.' + args[i]);
+              if ( ! cls ) {
+                throw "Could not find class for " + args[i];
+              }
+
+              args[i] = (function(cls) {
+                return function(args) {
+                  return cls.create(args);
+                };
+              })();
+            }
+          }
+
+          o = o.apply(null, args);
+        }
+
+        var a = [];
+        for ( var key in o ) {
+          a.push(foam.lookup('foam.parsers.ParserAxiom').create({
+            name: key,
+            parser: o[key]
+          }));
+        }
+        return a;
+      }
+    }
+  ]
+});
+
+foam.CLASS({
+  package: 'foam.parsers',
+  name: 'ParserAxioms',
+  refines: 'foam.core.Model',
+  properties: [
+    {
+      name: 'grammar',
+      type: 'foam.parsers.ParsersAxiom'
+    }
+  ]
+});
+
+foam.CLASS({
+  package: 'foam.parsers',
+  name: 'ParserAction',
+  extends: 'foam.core.Method',
+  methods: [
+    function installInProto(proto) {
+      var f = this.code;
+      var name = this.name;
+      var parser = proto[this.name];
+
+      if ( ! parser ) {
+        throw "No existing parser found for " + this.name;
+      }
+
+      proto[this.name] = function(ps, grammar) {
+        ps = parser.call(this, ps, grammar);
+        return ps ? ps.setValue(f.call(this, ps.value)) : null;
+      };
+    }
+  ]
+});
+
+foam.CLASS({
+  package: 'foam.parsers',
+  name: 'ParseAction',
+  refines: 'foam.core.Model',
+  properties: [
+    {
+      name: 'grammarActions',
+      type: 'AxiomArray',
+      adaptArrayElement: function(o) {
+        if ( foam.parsers.ParserAction.isInstance(o) ) return o;
+        return foam.parsers.ParserAction.create({
+          name: o.name,
+          code: typeof o === 'function' ? o : o.code
+        });
+      }
+    }
+  ]
+});
---
+++ src/core/templates.js
@@ -0,0 +1,356 @@
+foam.CLASS({
+  package: 'foam.templates',
+  name: 'TemplateOutput',
+  properties: [
+    {
+      name: 'buf',
+      factory: function() { return []; }
+    }
+  ],
+  methods: [
+    function f() {
+      for ( var i = 0 ; i < arguments.length ; i++ ) {
+        var o = arguments[i];
+
+        if ( typeof o === 'string' ) {
+          this.buf.push(o);
+        } else {
+          this.buf.push(o.toString());
+        }
+      }
+    },
+    function toString() {
+      return this.buf.length == 0 ? '' :
+        this.buf.length == 1 ? this.buf[0] :
+        this.buf.join('');
+    }
+  ]
+});
+
+foam.CLASS({
+  package: 'foam.templates',
+  name: 'TemplateUtil',
+
+  requires: [
+    'foam.parsers.StringPS'
+  ],
+
+  grammar: function(repeat0, simpleAlt, sym, seq1, seq, repeat, notChars, anyChar, not, optional, literal) {
+    return {
+      markup: repeat0(simpleAlt(
+        sym('comment'),
+        sym('create child'),
+        sym('simple value'),
+        sym('live value tag'),
+        sym('raw values tag'),
+        sym('values tag'),
+        sym('code tag'),
+        sym('ignored newline'),
+        sym('newline'),
+        sym('single quote'),
+        sym('text')
+      )),
+
+      'comment': seq1(1, '<!--', repeat0(not('-->', anyChar)), '-->'),
+
+
+      'create child': seq(
+        '$$',
+        repeat(notChars(' $\r\n<{,.'))),
+
+      'simple value': seq('%%', repeat(notChars(' ()-"\r\n><:;,')), optional('()')),
+
+      'live value tag': seq('<%#', repeat(not('%>', anyChar)), '%>'),
+
+      'raw values tag': simpleAlt(
+        seq('<%=', repeat(not('%>', anyChar)), '%>'),
+        seq('{{{', repeat(not('}}}', anyChar)), '}}}')
+      ),
+
+      'values tag': seq('{{', repeat(not('}}', anyChar)), '}}'),
+
+      'code tag': seq('<%', repeat(not('%>', anyChar)), '%>'),
+      'ignored newline': simpleAlt(
+        literal('\\\r\\\n'),
+        literal('\\\n')
+      ),
+      newline: simpleAlt(
+        literal('\r\n'),
+        literal('\n')
+      ),
+      'single quote': literal("'"),
+      text: anyChar
+    };
+  },
+
+  properties: [
+    {
+      name: 'out',
+      factory: function() {
+        return [];
+      }
+    },
+    {
+      name: 'simple',
+      defaultValue: true
+    },
+    {
+      name: 'ps',
+      factory: function() {
+        return this.StringPS.create();
+      }
+    }
+  ],
+
+  methods: [
+    function push() {
+      this.simple = false;
+      this.pushSimple.apply(this, arguments);
+    },
+    function pushSimple() {
+      this.out.push.apply(this.out, arguments);
+    },
+    function compileTemplate(t, name) {
+      this.ps.setString(t);
+      var result = this.markup(this.ps);
+      if ( ! result ) {
+        throw "Error parsing template " + name;
+      }
+      result = result.value;
+
+      var code = "var self = this; var outputter = opt_out || foam.templates.TemplateOutput.create(); var out = outputter.f.bind(outputter); out('" + result[1] + "'); return opt_out ? outputter : outputter.toString();";
+
+      var args = ['opt_out'];
+      var f = eval(
+        '(function() { ' +
+          'return function(' + args.join(',') + '){' + code + '};})()');
+
+      return f;
+    }
+  ],
+
+  grammarActions: [
+    function markup (v) {
+      var wasSimple = this.simple;
+      var ret = wasSimple ? null : this.out.join('');
+      this.out = [];
+      this.simple = true;
+      return [wasSimple, ret];
+    },
+    {
+      name: 'create child',
+      code: function(v) {
+        var name = v[1].join('');
+        this.push(
+          "', self.createTemplateView('", name, "'",
+          v[2] ? ', ' + v[2] : '',
+          "),\n'");
+      }
+    },
+    {
+      name: 'simple value',
+      code: function(v) {
+        this.push("',\n self.",
+                  v[1].join(''),
+                  v[2],
+                  ",\n'");
+      },
+    },
+    {
+      name: 'raw values tag',
+      code: function (v) {
+        this.push("',\n",
+                  v[1].join(''),
+                  ",\n'");
+      },
+    },
+    {
+      name: 'values tag',
+      code:     function (v) {
+        this.push("',\nescapeHTML(",
+                  v[1].join(''),
+                  "),\n'");
+      },
+    },
+    {
+      name: 'live value tag',
+      code: function (v) {
+        this.push("',\nself.dynamicTag('span', function() { return ",
+                  v[1].join(''),
+                  "; }.bind(this)),\n'");
+      },
+    },
+    {
+      name: 'code tag',
+      code: function (v) {
+        this.push("');\n",
+                  v[1].join(''),
+                  ";out('");
+      },
+    },
+    {
+      name: 'single quote',
+      code: function () {
+        this.pushSimple("\\'");
+      },
+    },
+    function newline() {
+      this.pushSimple('\\n');
+    },
+    function text(v) {
+      this.pushSimple(v);
+    }
+  ]
+});
+
+
+foam.CLASS({
+  name: 'TemplateBenchmark',
+  constants: {
+    JAVA_SOURCE: '// Generated by FOAM, do not modify.\n' +
+'// Version <%= this.version %><%\n' +
+'  var className       = this.javaClassName;\n' +
+"'  var parentClassName = 'AbstractFObject';\n" +
+"  var parentModel = '';\n" +
+"  if (this.extends) {\n" +
+"    parentClassName = this.extends;\n" +
+"    parentModel = this.extends + '.MODEL(), ';\n" +
+"  }\n" +
+"  if ( GLOBAL[parentClassName] && GLOBAL[parentClassName].abstract )\n" +
+"    parentClassName = 'Abstract' + parentClassName;\n" +
+"%><% if ( this.package ) { %>\n" +
+"package <%= this.package %>;\n" +
+"<% } %>\n" +
+"import foam.core.*;\n" +
+"import foam.dao.*;\n" +
+"import java.util.Arrays;\n" +
+"import java.util.List;\n" +
+"\n" +
+"public<%= this.abstract ? ' abstract' : '' %> class <%= className %>\n" +
+"    extends <%= parentClassName %> {\n" +
+"<% for ( var key in this.properties ) {\n" +
+"  var prop = this.properties[key];\n" +
+"  if ( prop.labels && prop.labels.indexOf('compiletime') != -1  )\n" +
+"    continue;\n" +
+"  javaSource.propertySource.call(this, out, prop);\n" +
+"}\n" +
+"if (this.relationships && this.relationships.length) {\n" +
+"  for ( var i = 0; i < this.relationships.length; i++) {\n" +
+"    var rel = this.relationships[i];\n" +
+"    javaSource.relationshipSource.call(this, out, rel);\n" +
+"  }\n" +
+"}\n" +
+"\n" +
+"var allProps = this.getRuntimeProperties();\n" +
+"allProps = allProps.filter(function(m) {\n" +
+"  if ( m.labels &&\n" +
+"        ( m.labels.indexOf('java') == -1 ||\n" +
+"          m.labels.indexOf(\"compiletime\") != -1 ) ) {\n" +
+"    return false;\n" +
+"  }\n" +
+"  return true;\n" +
+"});\n" +
+"\n" +
+" %>\n" +
+"final static Model model__ = new AbstractModel(<%= parentModel %>new Property[] {<% for (var i = 0; i < allProps.length; i++) { var prop = allProps[i]; %> <%= constantize(prop.name) %>,<% } %>} , new Relationship[] {<% if (this.relationships && this.relationships.length) { for (var i = 0; i < this.relationships.length; i++) { %> <%= constantize(this.relationships[i].name) %>, <% } } %> }) {\n" +
+"    public String getName() { return \"<%= this.id %>\"; }\n" +
+"    public String getShortName() { return \"<%= this.name %>\"; }\n" +
+"    public String getLabel() { return \"<%= this.label %>\"; }\n" +
+"    public Property getID() { return <%= this.ids.length ? constantize(this.ids[0]) : 'null' %>; }\n" +
+"    public FObject newInstance() { return new <%= className %>(); }\n" +
+"  };\n" +
+"\n" +
+"  public Model model() {\n" +
+"    return model__;\n" +
+"  }\n" +
+"  public static Model MODEL() {\n" +
+"    return model__;\n" +
+"  }\n" +
+"\n" +
+"  public int hashCode() {\n" +
+"    int hash = 1;\n" +
+"<% for (var i = 0; i < allProps.length; i++) { var prop = allProps[i]; %>\n" +
+"    hash = hash * 31 + hash(<%= prop.name %>_);<% } %>\n" +
+"\n" +
+"    return hash;\n" +
+"  }\n" +
+"\n" +
+"  public int compareTo(Object obj) {\n" +
+"    if ( obj == this ) return 0;\n" +
+"    if ( obj == null ) return 1;\n" +
+"\n" +
+"    <%= this.name %> other = (<%= this.name %>) obj;\n" +
+"\n" +
+"    int cmp;\n" +
+"<% for (var i = 0; i < allProps.length; i++) { var prop = allProps[i]; %>\n" +
+"    if ( ( cmp = compare(get<%= prop.name.capitalize() %>(), other.get<%= prop.name.capitalize() %>()) ) != 0 ) return cmp;<% } %>\n" +
+"\n" +
+"    return 0;\n" +
+"  }\n" +
+"\n" +
+"  public StringBuilder append(StringBuilder b) {\n" +
+"    return b<% for (var i = 0; i < allProps.length; i++) { var prop = allProps[i]; %>\n" +
+"        .append(\"<%= prop.name %>=\").append(get<%= prop.name.capitalize() %>())<%= i < allProps.length - 1 ? '.append(\", \")' : '' %><% } %>;\n" +
+"  }\n" +
+"\n" +
+"  public <%= className %> fclone() {\n" +
+"    <%= this.name %> c = new <%= this.name %>();\n" +
+"<% for (var i = 0; i < allProps.length; i++) { var prop = allProps[i]; %>\n" +
+"    c.set<%= prop.name.capitalize() %>(get<%= prop.name.capitalize() %>());<% } %>\n" +
+"    return c;\n" +
+"  }\n" +
+"<%\n" +
+"  function feature(f) {\n" +
+"    f.javaSource$f && f.javaSource$f(out, self);\n" +
+"  }\n" +
+"\n" +
+"  this.methods.forEach(feature);\n" +
+"  this.listeners.forEach(feature);\n" +
+"%>\n" +
+      "}\n"
+  },
+  grammar: function(alt, repeat, seq, literal) {
+    return {
+      start: alt(
+        literal("hello world"),
+        literal("help me"))
+    }
+  },
+  methods: [
+    function benchmark() {
+
+      var ps = foam.parsers.StringPS.create();
+      var util = foam.templates.TemplateUtil.create();
+
+      console.time('template-util');
+      for ( var i = 0 ; i < 500; i++ ) {
+        ps.setString(this.JAVA_SOURCE);
+        util.markup(ps);
+      }
+      console.timeEnd('template-util');
+    }
+  ]
+});
+
+
+//Templatebenchmark.create().benchmark();
+
+
+foam.CLASS({
+  package: 'foam.templates',
+  name: 'TemplateAxiom',
+  properties: [
+    {
+      name: 'template',
+      type: 'String'
+    }
+  ],
+  methods: [
+    function installInProto(proto) {
+      foam.templates.TemplateUtil
+    }
+  ]
+});
+
+var f = foam.templates.TemplateUtil.create().compileTemplate('Hello <%= this.name %>');
+console.log(f.call({ name: 'Adam' }));
