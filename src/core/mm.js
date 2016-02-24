cf3d62e0de10a78a0e56ab1ec5d6e01ef52d3854
Kevin Greer <kgr@chromium.org>
2016-02-24 15:50:46 -0500

    Basic JSON support.

--- src/core/mm.js
+++ src/core/mm.js
@@ -1933,6 +1933,85 @@ if ( foam.isServer ) {
 foam.X = foam.core.Window.create({window: global}, foam).Y;
 
 
+// JSON Support
+//
+// TODO:
+//   - don't output default values (optionally)
+//   - don't output default classes
+//   - quote keys when required
+//   - escape values
+//   - don't output transient properties
+//   - pretty printing
+//   - property filtering
+//   - allow for custom Property JSON support
+//   - compact output
+//   -
+
+foam.CLASS({
+  refines: 'foam.core.FObject',
+  methods: [
+    function toJSON() {
+      return foam.json.stringify(this);
+    },
+    function outputJSON(out, opt_options) {
+      out('{class:"', this.cls_.id, '"');
+      var ps = this.cls_.getAxiomsByClass(foam.core.Property);
+      for ( var i = 0 ; i < ps.length ; i++ ) {
+        var p = ps[i];
+        out(',', p.name, ':');
+        foam.json.output(out, this[p.name]);
+      }
+      out('}');
+    }
+  ]
+});
+
+
+foam.LIB({
+  name: 'json',
+  methods: [
+    function createOut() {
+      var buf = '';
+      function out() {
+        for ( var i = 0 ; i < arguments.length ; i++ )
+          buf += arguments[i];
+      }
+      out.toString = function() { return buf; };
+      return out;
+    },
+    function output(out, o) {
+      if ( typeof o === 'string' ) {
+        out('"', o, '"');
+      } else if ( typeof o === 'number' ) {
+        out(o);
+      } else if ( o.outputJSON ) {
+        o.outputJSON(out);
+      } else if ( Array.isArray(o) ) {
+        out('[');
+        for ( var i = 0 ; i < o.length ; i++ ) {
+          this.output(out, o[i]);
+          if ( i < o.length -1 ) out(',');
+        }
+        out(']');
+      }
+    },
+    function parse(json) {
+      if ( json.class ) {
+        var cls = foam.lookup(json.class);
+        foam.X.assert(cls, 'Unknown class "', json.class, '" in foam.json.parse.');
+        return cls.create(json);
+      }
+      return json;
+    },
+    function stringify(o, opt_options) {
+      var out = this.createOut();
+      o.outputJSON(out, opt_options);
+      return out.toString();
+    }
+  ]
+});
+
+
 /**  TODO:
   - context $ binding
   - support class: instead of type:
