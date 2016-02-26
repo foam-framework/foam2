a0375c7742ccc2ad4c29b18e9abac0a22ecf7c7c
Adam Van Ymeren <adamvy@google.com>
2016-02-26 15:07:53 -0500

    Handle undefined when outputting json.

--- src/core/mm.js
+++ src/core/mm.js
@@ -2033,7 +2033,9 @@ foam.LIB({
       return out;
     },
     function output(out, o) {
-      if ( typeof o === 'string' ) {
+      if ( typeof o === 'undefined' ) {
+        out('undefined');
+      } else if ( typeof o === 'string' ) {
         out('"', o, '"');
       } else if ( typeof o === 'number' ) {
         out(o);
