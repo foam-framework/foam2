0bcd4b8d696b31ac58f029397f0a84c341762de4
Kevin Greer <kgr@chromium.org>
2016-03-04 13:09:33 -0500

    Initial check-in of foam.js which loads all foam files.

--- src/core/node.js
+++ src/core/node.js
@@ -1,13 +1,1 @@
-require("./stdlib.js");
-require("./Boot.js");
-require("./Dynamic.js");
-require("./Window.js");
-require("./JSON.js");
-
-require("./objects.js");
-require("./patterns.js");
-require("./debug.js");
-require("./types.js");
-require("./parse.js");
-require("./templates.js");
-require("./Action.js");
+require("./foam.js");
