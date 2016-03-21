424203cee883b50ab0a04bae56469ba86b2aaac3
Kevin Greer <kgr@chromium.org>
2016-03-21 16:26:37 -0400

    DAO test copyright notice and formatting.

--- test/js/lib/dao.js
+++ test/js/lib/dao.js
@@ -1,8 +1,25 @@
+/**
+ * @license
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-if (typeof localStorage === "undefined" || localStorage === null) {
+if ( typeof localStorage === "undefined" || localStorage === null ) {
   var LocalStorage = require('node-localstorage').LocalStorage;
   localStorage = new LocalStorage('./tmp');
 }
+
 
 describe('LocalStorageDAO', function() {
   var a;
@@ -11,13 +28,13 @@ describe('LocalStorageDAO', function() {
 
   beforeEach(function() {
     foam.CLASS({
-      name: 'CompA',
       package: 'test',
+      name: 'CompA',
       properties: [ 'id', 'a', 'b' ]
     });
     foam.CLASS({
-      name: 'CompB',
       package: 'test',
+      name: 'CompB',
       properties: [ 'id', 'b' ]
     });
     a = test.CompA.create({id: 4, a:1, b:2});
