0ea6ce1a54bfd7f8ea0863c02347bfa21ca60153
Jackson Stone <jacksonic@google.com>
2016-04-04 10:18:35 -0400

    Removed physics from node builds

--- src/core/foam.js
+++ src/core/foam.js
@@ -78,10 +78,10 @@
     [ "../lib/graphics", ! isServer ],
     "../lib/dao",
     "../lib/mlang",
-    "../lib/Physical",
-    "../lib/Collider",
-    "../lib/PhysicsEngine",
-    "../lib/PhysicalCircle",
+    ["../lib/Physical", ! isServer ],
+    ["../lib/Collider", ! isServer ],
+    ["../lib/PhysicsEngine", ! isServer ],
+    ["../lib/PhysicalCircle", ! isServer ],
     [ "../lib/node/json_dao", isServer ]
   ].
       filter(function (f) { return ! Array.isArray(f) || f[1]; }).
