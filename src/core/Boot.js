6082cdea6360e559e50e47cc1ddd366862ba7ac4
Adam Van Ymeren <adamvy@google.com>
2016-04-11 10:02:30 -0400

    Allow methods to call this.SUPER() even if the parent class doesn't have an implementation for that method.

--- src/core/Boot.js
+++ src/core/Boot.js
@@ -850,16 +850,18 @@ foam.CLASS({
     function override_(proto, method) {
       var super_ = proto[this.name];
 
-      // Not overriding, so just return original method
-      if ( ! super_ ) return method;
+      if ( ! method ) return;
+
+      // Not using SUPER, so just return original method
+      if ( method.toString().indexOf('SUPER') == -1 ) return method;
+
+      // Not overriding, stub out super_
+      if ( ! super_ ) super_ = function() {};
 
       console.assert(
           typeof super_ === 'function',
           'Attempt to override non-method: ',
           this.name);
-
-      // Not using SUPER, so just return original method
-      if ( method.toString().indexOf('SUPER') == -1 ) return method;
 
       function SUPER() { return super_.apply(this, arguments); }
 
