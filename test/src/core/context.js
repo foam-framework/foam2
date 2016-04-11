abeadf18eee315bc6b2fb9cc4836309ab01f7373
Adam Van Ymeren <adamvy@google.com>
2016-04-11 11:33:21 -0400

    Flush out context unit tests.

--- test/src/core/context.js
+++ test/src/core/context.js
@@ -1,8 +1,72 @@
+/**
+ * @license
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 describe('ConteXt object', function() {
-  it('exists', function() {
+  it('register and lookup', function() {
     expect(foam).toBeTruthy();
-    foam.register({id: 'com.acme.Foo'});
-    foam.lookup('com.acme.Foo');
+
+    var someClass = {
+      id: 'com.acme.Foo'
+    };
+
+    var coreClass = {
+      package: 'foam.core',
+      name: 'Bar',
+      id: 'foam.core.Bar'
+    };
+
+    foam.register(someClass);
+    foam.register(coreClass);
+
+    expect(foam.lookup('com.acme.Foo')).toBe(someClass);
+    expect(foam.lookup('Bar')).toBe(coreClass);
+  });
+
+  it('lookup library', function() {
+    foam.LIB({
+      name: 'foam.core.testlib',
+      constants: {
+        VALUE: 'FOO'
+      }
+    });
+
+    expect(foam.lookup('testlib')).toBe(foam.core.testlib);
+    expect(foam.lookup('foam.core.testlib')).toBe(foam.core.testlib);
+    expect(foam.lookup('foam.core.testlib.VALUE')).toEqual('FOO');
+  });
+
+  it('subContext', function() {
+    var named = foam.subContext(null, "HELLO");
+    var unNamed = named.subContext();
+
+    for ( var key in named ) {
+      expect(key).not.toEqual("NAME");
+    }
+
+    expect(named.NAME).toEqual("HELLO");
+    expect(unNamed.NAME).toEqual("HELLO");
+
+    var foo = {
+      id: 'some.packaged.Foo'
+    };
+
+    foam.register(foo);
+
+    expect(named.lookup('some.packaged.Foo')).toBe(foo);
   });
 
   it('subcontexts', function() {
@@ -27,10 +91,8 @@ describe('ConteXt object', function() {
 
   it('describes', function() {
     foam.subContext().describe();
-
     foam.subContext({ hello: 'thing', wee: foam.core.Property.create() }, 'namey').describe();
   });
-
 });
 
 describe('Context Import/Export', function() {
