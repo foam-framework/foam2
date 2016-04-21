622b1dee625ab19195eea7bc4313dfc741006344
Adam Van Ymeren <adamvy@google.com>
2016-04-21 13:56:33 -0400

    Proxy DAO events via ProxyDAO and PromiseDAO.

--- src/lib/dao.js
+++ src/lib/dao.js
@@ -472,7 +472,23 @@ foam.CLASS({
       class: 'Proxy',
       of: 'foam.dao.DAO',
       name: 'delegate',
-      delegates: [ 'where', 'orderBy', 'skip', 'limit' ]
+      delegates: [ 'where', 'orderBy', 'skip', 'limit' ],
+      postSet: function(old, nu) {
+        if ( old ) old.on.unsub(this.onEvent);
+        if ( nu ) nu.on.sub(this.onEvent);
+
+        if ( old && nu ) this.on.reset.pub();
+      }
+    }
+  ],
+  listeners: [
+    {
+      name: 'onEvent',
+      code: function(s, a, b, c, d, e, f, g) {
+        // TODO: There should be a standard method for doing this
+        // that will keep up with the maximum amount of supported pub arguments.
+        this.pub(a, b, c, d, e, f, g);
+      }
     }
   ]
 });
@@ -721,7 +737,15 @@ foam.CLASS({
         }
       ]
     },
-    'delegate',
+    {
+      name: 'delegate',
+      postSet: function(old, nu) {
+        // TODO: This should probably be inherited from ProxyDAO propertly.
+        if ( old ) old.on.unsub(this.onEvent);
+        if ( nu ) nu.on.sub(this.onEvent);
+        if ( old && nu ) this.on.reset.pub();
+      }
+    },
     {
       name: 'promise',
       final: true,
@@ -763,6 +787,16 @@ foam.CLASS({
           });
         }
       ]
+    }
+  ],
+  listeners: [
+    {
+      name: 'onEvent',
+      code: function(s, a, b, c, d, e, f, g) {
+        // TODO: There should be a standard method for doing this
+        // that will keep up with the maximum amount of supported pub arguments.
+        this.pub(a, b, c, d, e, f, g);
+      }
     }
   ]
 });
