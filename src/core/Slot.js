a78c3fd74ef4281a25a067d8d065f5e975c405ad
Kevin Greer <kgr@chromium.org>
2016-05-03 15:52:14 -0400

    Moved ExpressionSlot to orphanage.

--- src/core/Slot.js
+++ src/core/Slot.js
@@ -24,7 +24,6 @@
 
   <ul>Types of Slots include:
     <li>PropertySlot
-    <li>ExpressionSlot
     <li>ConstantSlot
 </ul>
 */
@@ -108,53 +107,6 @@ foam.CLASS({
     function clear() {
       this.obj.clearProperty(this.prop.name);
     }
-  ]
-});
-
-
-/** Tracks dependencies for a dynamic function and invalidates is they change. */
-foam.CLASS({
-  package: 'foam.core',
-  name: 'ExpressionSlot',
-  implements: [ 'foam.core.Slot' ],
-
-  properties: [
-    'args',
-    'fn',
-    {
-      name: 'value',
-      factory: function() {
-        return this.fn.apply(this, this.args.map(function(a) {
-          return a.get();
-        }));
-      }
-    }
-  ],
-
-  methods: [
-    function init() {
-      for ( var i = 0 ; i < this.args.length ; i++ ) {
-        this.onDestroy(this.args[i].sub(this.invalidate));
-      }
-    },
-
-    function get() {
-      return this.value;
-    },
-
-    function set() { /* nop */ },
-
-    function sub(l) {
-      return this.SUPER('propertyChange', 'value', l);
-    },
-
-    function unsub(l) {
-      this.SUPER('propertyChange', 'value', l);
-    }
-  ],
-
-  listeners: [
-    function invalidate() { this.clearProperty('value'); }
   ]
 });
 
