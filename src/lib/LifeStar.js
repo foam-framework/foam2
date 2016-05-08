21e22d19ec8309acfa2651e3186e9c04eb23d3ea
Kevin Greer <kgr@chromium.org>
2016-05-08 18:51:42 -0400

    Improved LifeStar demo.

--- src/lib/LifeStar.js
+++ src/lib/LifeStar.js
@@ -27,16 +27,9 @@ foam.CLASS({
         },
         function doTransform(x) {
           var t = this.transform;
-          var s = 1 - this.z/5000;
-          t.translate(-200,-200);
+          var s = 1 - this.z/300;
           t.scale(s, s);
-          t.translate(200,200);
           x.transform(t.a, t.d, t.b, t.e, t.c, t.f);
-        },
-        function polar3D(r, t, g) {
-          this.x = Math.sin(t) * Math.cos(g) * r;
-          this.y = Math.cos(t) * r;
-          this.z = Math.sqrt(200*200 - this.x*this.x - this.y*this.y);
         },
         function paintSelf(x) {
           this.SUPER(x);
@@ -53,7 +46,7 @@ foam.CLASS({
   ],
 
   properties: [
-    [ 'n',      128 ],
+    [ 'n',      217 ],
     [ 'x',      500 ],
     [ 'y',      350 ],
     [ 'width',  1000 ],
@@ -66,15 +59,11 @@ foam.CLASS({
     function initCView() {
       this.SUPER();
       for ( var i = 0 ; i < this.n ; i++ ) this.addPoint(i);
-      for ( var i = 0 ; i < this.n ; i++ ) {
-        var p = this.addPoint(i);
-        p.z *= -1;
-      }
       this.tick();
     },
     function addPoint(i) {
       var p = this.Point.create({
-        r: i*200/this.n,
+        r: Math.sin(Math.PI * i/this.n)*200,
         t: i*Math.PI*20/this.n,
         g: 0, //i*Math.PI/5/this.n,
         radius: 3,
@@ -82,14 +71,19 @@ foam.CLASS({
         arcWidth: 0
       });
 
-      p.polar3D(p.r, p.t, p.g);
+      p.x = Math.sin(p.t) * Math.cos(p.g) * p.r;
+      p.y = Math.cos(p.t) * p.r;
+      p.z = Math.sqrt(200*200 - p.x*p.x - p.y*p.y);
+
+      if ( i > this.n/2 ) p.z *= -1;
 
       this.time$.sub(function() {
         p.rotateY(0.01);
+        p.rotateX(0.005);
         var on = Math.abs((this.time % this.n - i + this.n)%this.n) < 20
         p.glowRadius = on ? 8 : 0;
         var s = on ? 100 : 70;
-        var l = on ?  70 : 30;
+        var l = on ?  70 : 40;
         p.color = this.hsl(i*365/this.n, s, l);
       }.bind(this));
       this.addChildren(p);
