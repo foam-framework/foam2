14489f4162f0d592601e6055f696eb75c2f28cf3
Kevin Greer <kgr@chromium.org>
2016-05-12 17:19:05 -0400

    Formatting.

--- src/lib/QueryParser.js
+++ src/lib/QueryParser.js
@@ -14,9 +14,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 foam.CLASS({
   package: 'foam.parse',
   name: 'QueryParser',
+
   axioms: [
     foam.pattern.Multiton.create({ property: 'of' })
   ],
