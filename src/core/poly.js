09b4094993bb94460944faae238f727728e5197b
Kevin Greer <kgr@chromium.org>
2016-05-13 15:32:21 -0400

    Formatting.

--- src/core/poly.js
+++ src/core/poly.js
@@ -27,24 +27,22 @@ if ( ! Array.from ) {
 }
 
 // TODO: Polyfill this? Enum is using array.find, IE and old node have no support.
-if (!Array.prototype.find) {
+if ( ! Array.prototype.find ) {
   Array.prototype.find = function(predicate) {
-    if (this === null) {
+    if ( this === null ) {
       throw new TypeError('Array.prototype.find called on null or undefined');
     }
-    if (typeof predicate !== 'function') {
+    if ( typeof predicate !== 'function' ) {
       throw new TypeError('predicate must be a function');
     }
-    var list = Object(this);
-    var length = list.length >>> 0;
+    var list    = Object(this);
+    var length  = list.length >>> 0;
     var thisArg = arguments[1];
     var value;
 
-    for (var i = 0; i < length; i++) {
+    for ( var i = 0 ; i < length ; i++ ) {
       value = list[i];
-      if (predicate.call(thisArg, value, i, list)) {
-        return value;
-      }
+      if ( predicate.call(thisArg, value, i, list) ) return value;
     }
     return undefined;
   };
