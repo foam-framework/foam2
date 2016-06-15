14ff6e0477cf0dab1954ed4844eb83964e570b7a
Adam Van Ymeren <adamvy@google.com>
2016-06-15 13:59:50 -0400

    Add boxmail app that demonstrates usage of Boxs for Sockets and WebSockets.

---
+++ src/apps/boxmail/boxmail.js
@@ -0,0 +1,18 @@
+foam.CLASS({
+  package: 'boxmail',
+  name: 'Message',
+  extends: 'foam.box.Message',
+  properties: [
+    {
+      name: 'id'
+    },
+    {
+      class: 'String',
+      name: 'subject'
+    },
+    {
+      class: 'String',
+      name: 'body'
+    }
+  ]
+});
---
+++ src/apps/boxmail/getmail.js
@@ -0,0 +1,33 @@
+require('./bootFOAMNode.js');
+
+
+var env = foam.box.Context.create();
+
+var dst = foam.box.SocketBox.create({
+  address: 'localhost:7000',
+}, env);
+
+var msg = boxmail.Message.create({
+  subject: 'random mail',
+  body: 'hello world'
+});
+
+var dao = foam.dao.ClientDAO.create({
+  delegate: foam.box.SubBox.create({
+    name: 'INBOX',
+    delegate: dst
+  }, env)
+}, env);
+
+dao.select().then(function(m) {
+  console.log("Read", m.a.length, 'mails');
+  for ( var i = 0 ; i < m.a.length ; i++ ) {
+    var mail = m.a[i];
+    console.log('');
+    console.log('');
+    console.log('MSGID:', mail.id);
+    console.log('Subject:', mail.subject);
+    console.log(mail.body);
+  }
+  process.exit(0);
+});
