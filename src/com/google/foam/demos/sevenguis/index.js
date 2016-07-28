3a5023f2d8ee29a3155875ebda6b4414e079f5cd
Kevin Greer <kgr@chromium.org>
2016-07-28 09:56:58 -0400

    7GUIS work.

--- src/com/google/foam/demos/sevenguis/index.js
+++ src/com/google/foam/demos/sevenguis/index.js
@@ -2,12 +2,19 @@
 
 var els = document.getElementsByTagName('foam');
 
-for ( var i = 3 ; i < 4 ; i++ ) {
+for ( var i = 0 ; i < els.length ; i++ ) {
+  if ( i !== 0 && i !== 3 ) continue;
   var el = els[i];
   var modelName = els[i].getAttribute('model');
   var cls = foam.lookup(modelName, true);
+  console.log('***', modelName, !!cls);
   if ( cls ) {
     var view = cls.create(null, foam.__context__);
+    if ( view.toE ) {
+      view = view.toE(foam.__context__);
+    } else if ( ! foam.u2.Element.isInstance(view) )  {
+      view = foam.u2.DetailView.create({data: view, showActions: true});
+    }
     console.log('Installing view: ', modelName, view);
     el.outerHTML = view.outerHTML;
     view.load();
