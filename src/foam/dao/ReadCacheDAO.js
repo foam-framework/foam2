8ca62c9090aa8901b51a84e10890d0ac7b5c0623
Jackson Stone <jacksonic@google.com>
2016-08-02 11:46:23 -0400

    ReadCacheDAO basic port added

---
+++ src/foam/dao/ReadCacheDAO.js
@@ -0,0 +1,65 @@
+/**
+ * @license
+ * Copyright 2014 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+// TODO: Don't use yet! Port awaiting manual testing
+
+/**
+  ReadCacheDAO will do all queries from its fast delegate (cache). Writes
+  are sent through to the remote, which will eventually update the cache.
+  The cache maintains full copy of the remote, but the remote is the source
+  of truth.
+*/
+foam.CLASS({
+  package: 'foam.dao',
+  name: 'ReadCacheDAO',
+
+  extends: 'foam.dao.ProxyDAO',
+
+  properties: [
+    {
+      name: 'remote',
+      postSet: function() { this.tryPreLoad(); }
+    },
+    {
+      name: 'delegate',
+      postSet: function() { this.tryPreLoad(); }
+    },
+    {
+      name: 'model',
+      expression: function(delegate, remote) {
+        return delegate.model || remote.model;
+      }
+    }
+  ],
+
+  methods: [
+    function tryPreLoad() {
+      // pre-load contents of remote into cache
+      // TODO: replace this with the same kind of linkage as LazyCacheDAO
+      // ends up with (make listener expression dependent on two props)
+      this.delegate && this.remote && this.remote.pipe(this.delegate);
+    },
+    function put(obj) {
+      return this.remote.put(obj);
+    },
+    function remove(obj) {
+      return this.remote.remove(obj);
+    },
+    function removeAll(skip, limit, order, predicate) {
+      return this.remote.removeAll(skip, limit, order, predicate);
+    },
+  ]
+});
