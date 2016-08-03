d2e5b95c6c2c6cc66cff424ad40e17f808a55f43
Jackson Stone <jacksonic@google.com>
2016-08-03 12:46:05 -0400

    ReadCacheDAO updated to use PreloadDAO, PreloadDAO added to handle preloading promise.

--- src/files.js
+++ src/files.js
@@ -79,6 +79,7 @@ FOAM_FILES([
   { name: "foam/dao/RelationshipDAO" },
   { name: "foam/dao/LazyCacheDAO" },
   { name: "foam/dao/ReadCacheDAO" },
+  { name: "foam/dao/PreloadDAO" },
   { name: "foam/dao/SequenceNumberDAO" },
   { name: "foam/dao/EasyDAO" },
   { name: "foam/dao/IDBDAO", flags: ['web'] },
