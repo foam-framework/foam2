4a13ccd2317513a6313a8d8881f39b29ce142c26
Kevin Greer <kgr@chromium.org>
2016-08-05 17:21:11 -0400

    Start of port of Sweeper to FOAM2.

---
+++ src/com/google/foam/sweeper/Board.js
@@ -0,0 +1,78 @@
+/**
+ * @license
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.CLASS({
+  package: 'com.google.sweeper',
+  name: 'Board',
+  extends: 'foam.u2.Element',
+
+  requires: [ 'com.google.sweeper.Cell' ],
+
+  exports: [ 'as board' ],
+
+  properties: [
+    [ 'width', 14 ],
+    [ 'height', 14 ],
+    {
+      name: 'cells',
+      factory: function() {
+        var cells = [];
+        for ( var row = 0 ; row < this.height ; row++ ) {
+          cells[row] = [];
+          for ( var col = 0 ; col < this.width ; col++ ) {
+            var cell = cells[row][col] = this.Cell.create({x: col, y: row});
+            cell.covered$.addListener(this.cellUncovered);
+          }
+        }
+        return cells;
+      }
+    }
+  ],
+
+  methods: [
+    function forEachNeighbour(cell, f) {
+      for ( var row = Math.max(0, cell.y-1) ; row < Math.min(this.height, cell.y+2) ; row++ )
+        for ( var col = Math.max(0, cell.x-1) ; col < Math.min(this.width, cell.x+2) ; col++ )
+          f(this.cells[row][col]);
+    },
+
+    function getMineCount(cell) {
+      var count = 0;
+      this.forEachNeighbour(cell, function(c) { if ( c.mined ) count++; });
+      return count;
+    }
+  ],
+
+  templates: [
+    function CSS() {/*
+      ^ {
+        border: 1px solid gray;
+        display: inline-block;
+      }
+    */},
+    function initE() {/*#U2
+      <div class="^">
+        <div class="^row" repeat="row in 0..this.height-1">
+          (( for ( var col = 0 ; col < this.width ; col++ ) { ))
+            {{this.cells[row][col]}}
+          (( } ))
+        </div>
+      </div>
+    */}
+  ],
+
+  listeners: [
+    function cellUncovered(cell) {
+      if ( cell.mineCount ) return;
+      this.X.setTimeout(this.forEachNeighbour.bind(this, cell, function(c) { if ( ! c.mined ) c.covered = false; }), 32);
+    }
+  ]
+});
---
+++ src/com/google/foam/sweeper/Cell.js
@@ -0,0 +1,95 @@
+/**
+ * @license
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.CLASS({
+  package: 'com.google.sweeper',
+  name: 'Cell',
+  extends: 'foam.u2.Element',
+
+  imports: [ 'board' ],
+
+  constants: {
+    COLOURS: [ '', 'green', 'blue', 'orange', 'red', 'red', 'red', 'red' ],
+  },
+
+  properties: [
+    'x',
+    'y',
+    {
+      type: 'Int',
+      name: 'mineCount',
+      lazyFactory: function() { return this.board.getMineCount(this); }
+    },
+    {
+      type: 'Boolean',
+      name: 'covered',
+      defaultValue: true
+    },
+    {
+      type: 'Boolean',
+      name: 'marked'
+    },
+    {
+      type: 'Boolean',
+      name: 'mined',
+      factory: function() { return Math.random() < 0.18; }
+    }
+  ],
+
+  methods: [
+    function stateClass() {
+      return this.dynamic(
+        function(covered, marked) { return marked ? 'marked' : covered ? 'covered' : ''; },
+        this.covered$, this.marked$);
+    }
+  ],
+
+  templates: [
+    function CSS() {/*
+      body { -webkit-user-select: none; }
+      ^ {
+        border: 1px solid gray;
+        display: table-cell;
+        font-size: 18px;
+        font-weight: bold;
+        height: 26px;
+        text-align: center;
+        vertical-align: middle;
+        width: 26px;
+      }
+      ^.covered {
+        background: #ccc;
+        box-shadow: -2px -2px 10px rgba(0,0,0,.25) inset, 2px 2px 10px white inset;
+      }
+      ^.marked ^flag {
+        display: block;
+        color: #BD1616;
+      }
+      ^.covered font { visibility: hidden; }
+      ^.marked font { display: none; }
+      ^flag { display: none; }
+      ^.marked { background-color: #ccc; }
+    */},
+    function initE() {/*#U2
+      <span class="^" class={{this.stateClass()}} onclick="sweep" oncontextmenu="mark">
+        <span class="^flag">&#x2691;</span>
+        <font if={{this.mined}}>&#x2699;</font>
+        <font if={{!this.mined && this.mineCount}} color={{this.COLOURS[this.mineCount]}}>{{this.mineCount}}</font>
+      </span>
+    */}
+  ],
+
+  listeners: [
+    function mark(e) { this.marked = ! this.marked; e.preventDefault(); },
+
+    function sweep(e) { this.covered = false; }
+  ]
+});
