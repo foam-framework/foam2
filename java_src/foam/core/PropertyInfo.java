59ce8cf49aab2bc15a5431370fba2c36dcb0afa3
Adam Van Ymeren <adamvy@google.com>
2016-08-18 12:28:26 -0400

    Java: Add toJSON and get/setClassInfo() to PropertyInfo.

--- java_src/foam/core/PropertyInfo.java
+++ java_src/foam/core/PropertyInfo.java
@@ -3,8 +3,12 @@ package foam.core;
 import foam.lib.parse.Parser;
 
 public interface PropertyInfo extends foam.mlang.Expr {
+  public PropertyInfo setClassInfo(ClassInfo p);
+  public ClassInfo getClassInfo();
+
   public String getName();
   public Object get(Object obj);
   public void set(Object obj, Object value);
   public Parser jsonParser();
+  public void toJSON(foam.lib.json.Outputter outputter, StringBuilder out, Object value);
 }
