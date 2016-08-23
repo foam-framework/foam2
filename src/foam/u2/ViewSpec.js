dd475b5079fcd72f1835694e1eb2c52c62fdc5ff
Kevin Greer <kgr@chromium.org>
2016-08-23 15:59:59 -0400

    Initial check-in of ViewSpec.

---
+++ src/foam/u2/ViewSpec.js
@@ -0,0 +1,37 @@
+/**
+ * @license
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+foam.CLASS({
+  package: 'foam.u2',
+  name: 'ViewSpec',
+  extends: 'foam.core.Property',
+
+  documentation: 'Set a ViewFactory to be a string containing a class name, ' +
+      'a Class object, or a factory function(args, context). ' +
+      'Useful for rowViews and similar.',
+
+  properties: [
+    [ 'adapt', function(_, spec, prop) {
+      return foam.String.is(spec) ? { class: spec } : spec ;
+    } ]
+    /*
+    [ 'toJSON', function(value) {
+      Output as string if 'class' is only defined value.
+    } ]
+    */
+  ]
+});
