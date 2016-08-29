12f4b1d782aaa95a9d1f526d2633fa2ec36fbbd9
Adam Van Ymeren <adamvy@google.com>
2016-08-29 16:32:21 -0400

    HTTPBox: Only decorate the replyBox if present.

--- src/lib/box.js
+++ src/lib/box.js
@@ -1268,20 +1268,26 @@ foam.CLASS({
         var replyBox = msg.replyBox;
         var errorBox = msg.errorBox;
 
-        msg.replyBox = this.HTTPReplyBox.create();
+        if ( replyBox ) {
+          msg.replyBox = this.HTTPReplyBox.create();
+        }
         // TODO: set error box as well?
 
         var req = this.HTTPRequest.create({
           url: this.url,
           method: this.method,
           payload: foam.json.Network.stringify(msg)
-        }).send().then(function(resp) {
-          return resp.payload;
-        }).then(function(p) {
-          replyBox.send(foam.json.parse(foam.json.parseString(p, null, this)));
-        }, function(e) {
-          errorBox.send(e);
-        });
+        }).send();
+
+        if ( replyBox ) {
+          req.then(function(resp) {
+            return resp.payload;
+          }).then(function(p) {
+            replyBox.send(foam.json.parse(foam.json.parseString(p, null, this)));
+          }, function(e) {
+            errorBox.send(e);
+          });
+        }
       }
     }
   ]
