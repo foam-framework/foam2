f3ca2f5cdc97110639d5b3ca05bb218dceed8e96
Kevin Greer <kgr@chromium.org>
2016-09-02 14:15:57 -0400

    Added SampleSplitContainer demo.

--- src/com/google/foam/demos/u2/Border.js
+++ src/com/google/foam/demos/u2/Border.js
@@ -7,7 +7,7 @@ foam.CLASS({
       code: function() {/*
         ^ { background: gray; padding: 10px; display: inline-block; }
         ^title { padding: 6px; align-content: center; background: aliceblue; }
-        ^content { padding: 6px; width: 300px; height: 300px; background: white; }
+        ^content { padding: 6px; width: 300px; height: 200px; background: white; }
       */}
     })
   ],
@@ -31,3 +31,44 @@ foam.CLASS({
 var sb = SampleBorder.create({title: 'Title'});
 sb.write();
 sb.add('content');
+
+
+var E = foam.__context__.E.bind(foam.__context__);
+E('br').write();
+E('br').write();
+
+foam.CLASS({
+  name: 'SampleSplitContainer',
+  extends: 'foam.u2.Element',
+
+  axioms: [
+    foam.u2.CSS.create({
+      code: function() {/*
+        ^ { background: gray; padding: 10px; display: inline-flex; }
+        ^content { margin: 4px; padding: 6px; width: 300px; height: 200px; background: white; }
+      */}
+    })
+  ],
+
+  properties: [
+    'leftPanel', 'rightPanel'
+  ],
+
+  methods: [
+    function initE() {
+      this.
+          cssClass(this.myCls()).
+          start('div', null, this.leftPanel$).
+            cssClass(this.myCls('content')).
+          end().
+          start('div', null, this.rightPanel$).
+            cssClass(this.myCls('content')).
+          end();
+    }
+  ]
+});
+
+var split = SampleSplitContainer.create();
+split.write();
+split.leftPanel.add('leftContent');
+split.rightPanel.add('rightContent');
