fd4e63389d2078e1b0982d5c554e521988870b74
Kevin Greer <kgr@chromium.org>
2016-09-09 16:20:22 -0400

    Improved CSS for Heroes CitationView.

--- src/com/google/foam/demos/heroes/CitationView.js
+++ src/com/google/foam/demos/heroes/CitationView.js
@@ -25,15 +25,51 @@ foam.CLASS({
     'editHero'
   ],
 
+  axioms: [
+    foam.u2.CSS.create({
+      code: function() {/*
+        ^ {
+          padding-right: 8px;
+          margin: 8px;
+          display: flex;
+          background: #DDD;
+          width: 200px;
+          border-radius: 5px;
+        }
+        ^:hover {
+          background: #EEE;
+        }
+        ^id {
+          padding: 8px;
+          border-radius: 4px 0 0 4px;
+          color: white;
+          background: #607D8B;
+        }
+        ^name {
+          margin: 8px 0 0 10px;
+          width: 100%;
+        }
+        ^ button {
+          box-shadow: none;
+          cursor: pointer;
+          border: none;
+          border-radius: 4px;
+          padding: 6px 8px;
+          margin: 4px;
+          background: gray;
+          color: white;
+        }
+      */}
+    })
+  ],
+
   methods: [
     function initE() {
       this.
-        start('span').
-          on('click', this.onClick).
-          add(this.data.id, ' ').
-          entity('nbsp').
-          add(' ', this.data.name).
-        end().
+        cssClass(this.myCls()).
+        on('click', this.onClick).
+        start('div').cssClass(this.myCls('id')).add(this.data.id).end().
+        start('div').cssClass(this.myCls('name')).add(this.data.name).end().
         add(this.REMOVE_HERO);
     }
   ],
