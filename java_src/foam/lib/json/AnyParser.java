f1d54b0ec5a403049e472f06b29423585f91df23
Adam Van Ymeren <adamvy@google.com>
2016-09-19 12:26:04 -0400

    Add Null to the types parsable by AnyParser.

--- java_src/foam/lib/json/AnyParser.java
+++ java_src/foam/lib/json/AnyParser.java
@@ -5,6 +5,7 @@ import foam.lib.parse.*;
 public class AnyParser extends ProxyParser {
   public AnyParser() {
     super(new Alt(
+                  new NullParser(),
                   new StringParser(),
                   new IntParser(),
                   new BooleanParser(),
