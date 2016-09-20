0c54f9e3844bf9706117465812eaa9fddae1eaab
Kevin Greer <kgr@chromium.org>
2016-09-20 09:14:52 -0400

    Initial check-in of Grid demo.

---
+++ src/com/google/foam/demos/grid/Grid.js
@@ -0,0 +1,118 @@
+/**
+ * @license
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+foam.CLASS({
+  package: 'com.google.foam.demos.grid',
+  name: 'Resource',
+
+  tableProperties: [ 'description', 'url' ],
+
+  properties: [
+    { name: 'id', hidden: true },
+    { name: 'description' },
+    { name: 'url', label: 'URL' }
+  ]
+});
+
+
+foam.CLASS({
+  package: 'com.google.foam.demos.grid',
+  name: 'Controller',
+  extends: 'foam.u2.Controller',
+
+  requires: [
+    'foam.u2.DetailView',
+    'foam.u2.TableView',
+    'foam.dao.EasyDAO',
+    'com.google.foam.demos.grid.Resource'
+  ],
+
+  axioms: [
+    foam.u2.CSS.create({
+      code: function() {/*
+      */}
+    })
+  ],
+
+  properties: [
+    {
+      name: 'dao',
+      view: { class: 'foam.u2.TableView', of: com.google.foam.demos.grid.Resource },
+      factory: function() {
+        return foam.dao.EasyDAO.create({
+          of: foam.demos.sevenguis.Person,
+          daoType: 'MDAO',
+          seqNo: true
+        });
+      }
+    },
+    {
+      name: 'person',
+      view: 'foam.u2.DetailView',
+      factory: function() { return this.Resource.create(); }
+    }
+  ],
+
+  methods: [
+    function initE() {
+      this.
+        cssClass(this.myCls()).
+        start('h1').add('Add Resources').end().
+        add(this.PERSON, this.ADD_RESOURCE).
+        start('h1').add('List of Resources').end().
+        add(this.DAO, this.SHOW);
+    }
+  ],
+
+  actions: [
+    {
+      name: 'addResource',
+      label: 'Add',
+      code: function() {
+        var p = this.person;
+        this.dao.put(p.clone());
+        p.id = p.description = p.url = undefined;
+      }
+    },
+    {
+      name: 'show',
+      code: function() {
+        console.log('show');
+        var a = [];
+        this.dao.select({put: function(r) { a.push(r); }}).then(function() {
+          window.alert(foam.json.Outputer.create({
+            pretty: false,
+            outputClassNames: false
+          }).stringify(a));
+        });
+      }
+    },
+    {
+      name: 'deleteItem',
+      label: 'Delete',
+      code: function() {
+      }
+    }
+  ]
+});
+
+// Didn't specify view for dao
+// Didn't specify 'of' for TableView
+// Invalid names in tableProperties:
+// added invalid action name, no error
+// select should default to ArraySink (does it?)
+// clone on DAO.put
