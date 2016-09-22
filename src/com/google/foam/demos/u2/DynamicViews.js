6ea28ea444e879beb20428e6dee30b09b052616a
Kevin Greer <kgr@chromium.org>
2016-09-22 16:41:22 -0400

    Update DynamicViews demo.

--- src/com/google/foam/demos/u2/DynamicViews.js
+++ src/com/google/foam/demos/u2/DynamicViews.js
@@ -5,14 +5,40 @@ foam.CLASS({
     {
       class: 'Int',
       label: 'How wide would you like your text field?',
-      name: 'width'
+      name: 'width',
+      value: 30
     },
     {
       class: 'String',
-      name: 'string',
+      name: 'stringValue',
       view: function(_, X) {
         return X.data.slot(function(width) {
-          return foam.u2.TextField.create({displayWidth: width, data$: this.string$});
+          return foam.u2.TextField.create({displayWidth: width, data$: this.stringValue$});
+        });
+      }
+    },
+    {
+      name: 'typeOfDays',
+      view: {
+        class: 'foam.u2.view.ChoiceView',
+        value: 'All',
+        choices: [
+          'All',
+          'Week Days',
+          'Weekends'
+        ]
+      }
+    },
+    {
+      name: 'daysOfWeek',
+      view: function(_, X) {
+        return X.data.slot(function(typeOfDays) {
+          var map = {
+            All: [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ],
+            'Week Days': [ 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday' ],
+            Weekends: [ 'Sunday', 'Saturday' ],
+          };
+          return foam.u2.view.ChoiceView.create({choices: map[typeOfDays], data$: this.daysOfWeek$});
         });
       }
     }
@@ -20,5 +46,5 @@ foam.CLASS({
 });
 
 
-var t = Test.create();
+var t = Test.create({stringValue: 'This is a test!'});
 foam.u2.DetailView.create({data: t}).write();
