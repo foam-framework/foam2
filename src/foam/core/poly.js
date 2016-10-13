31890526910c1271d30e5c0379a46415fc12a292
Braden Shepherdson <braden.shepherdson@gmail.com>
2016-10-13 15:01:10 -0400

    Document which targets require each polyfill

--- src/foam/core/poly.js
+++ src/foam/core/poly.js
@@ -17,13 +17,14 @@
 
 // Polyfill
 
+// Required on IE 11, Android Browser before 5.1.
 if ( ! Math.trunc ) {
   Math.trunc = function trunc(v) {
     return v > 0 ? Math.floor(v) : Math.ceil(v);
   };
 }
 
-
+// Required on IE 11, Android Browser (at least to 5.1).
 if ( ! Array.from ) {
   /** Turn array-like objects into real arrays. **/
   Array.from = function(a) {
@@ -33,7 +34,7 @@ if ( ! Array.from ) {
   }
 }
 
-
+// Required on IE 11, Android Browser (at least to 5.1).
 if ( ! Array.prototype.find ) {
   Array.prototype.find = function(predicate) {
     if ( this === null ) {
@@ -55,7 +56,7 @@ if ( ! Array.prototype.find ) {
   };
 }
 
-
+// Required on IE 11, Android Browser (at least to 5.1).
 if ( ! String.prototype.endsWith ) {
   // Official polyfill
   String.prototype.endsWith = function(searchString, position) {
