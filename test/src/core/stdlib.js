bc7783c2d01b31fab016bfad613b4a32a7103128
Jackson Stone <jacksonic@google.com>
2016-10-19 10:48:37 -0400

    stdlib tests formatting

--- test/src/core/stdlib.js
+++ test/src/core/stdlib.js
@@ -208,44 +208,6 @@ describe('Array clone (deep copy)', function() {
   });
 });
 
-describe('foam.Function.formalArgs', function() {
-
-  it('handles an empty arg list', function() {
-    var fn = function( ) {
-      return (true);
-    }
-    var args = foam.Function.formalArgs(fn);
-    expect(args).toEqual([]);
-  });
-
-  it('grabs simple argument names', function() {
-    var fn = function(str, bool ,
-       func, obj, num,  arr ) {
-      return (true);
-    }
-    var args = foam.Function.formalArgs(fn);
-    expect(args).toEqual([ 'str', 'bool', 'func', 'obj', 'num', 'arr' ]);
-  });
-
-  it('grabs typed argument names', function() {
-    var fn = function(/* string */ str, /*boolean*/ bool ,
-      /* function*/ func, /*object*/obj, /* number */num, /* array*/ arr ) {
-      return (true);
-    }
-    var args = foam.Function.formalArgs(fn);
-    expect(args).toEqual([ 'str', 'bool', 'func', 'obj', 'num', 'arr' ]);
-  });
-
-  it('grabs commented argument names', function() {
-    var fn = function(/* any // the argument value to validate. */ arg, more,
-        /* // a comment here */ name, another /* return // comment */) {
-      return (true);
-    }
-    var args = foam.Function.formalArgs(fn);
-    expect(args).toEqual([ 'arg', 'more', 'name', 'another' ]);
-  });
-
-});
 
 
 describe('Date', function() {
@@ -416,7 +378,7 @@ describe('foam.Function', function() {
     expect(foam.Function.clone(fn)).toBe(fn);
   });
   it('equals', function() {
-    var fn1 = function(a) { return a + 1; }; 
+    var fn1 = function(a) { return a + 1; };
     var fn2 = function(a) { return a + 1; };
     var fnNoMatch = function(b) { return b + 1; };
     expect(foam.Function.equals(fn1, fn2)).toBe(true);
@@ -424,7 +386,7 @@ describe('foam.Function', function() {
     expect(foam.Function.equals(fn1, undefined)).toBe(false);
   });
   it('compare', function() {
-    var fn1 = function(a) { return a + 1; }; 
+    var fn1 = function(a) { return a + 1; };
     var fn2 = function(a) { return a + 1; };
     var fnNoMatch = function(b) { return b + 1; };
     expect(foam.Function.compare(fn1, fn2)).toBe(0);
@@ -438,7 +400,7 @@ describe('foam.Function', function() {
     expect(foam.Function.hashCode(fn1)).not.toEqual(
       foam.Function.hashCode(fn2));
   });
-  
+
   it('memoize1 accepts a null argument', function() {
     var f = foam.Function.memoize1(function(arg) { return arg; });
     var r = f(null);
@@ -446,7 +408,7 @@ describe('foam.Function', function() {
   });
 
   it('setName', function() {
-    var f = foam.Function.setName(function(a) { return a + 1; }, 
+    var f = foam.Function.setName(function(a) { return a + 1; },
       'myTestFunction');
     expect(f.name).toBe('myTestFunction');
   });
@@ -454,7 +416,7 @@ describe('foam.Function', function() {
   it('appendArguments', function() {
     var array1 = ['a'];
     var array2 = ['b'];
-    (function(a, b, c, d) { 
+    (function(a, b, c, d) {
       foam.Function.appendArguments(array1, arguments);
       foam.Function.appendArguments(array2, arguments, 2);
     })(1, 2, 3, 4);
@@ -476,7 +438,7 @@ describe('foam.Function', function() {
   it('argsStr', function() {
     // normal case
     var str = foam.Function.argsStr(
-      function(a, /*string?*/b, c /*array*/) { 
+      function(a, /*string?*/b, c /*array*/) {
         return [ a, b, c ];
       }
     );
@@ -492,7 +454,7 @@ describe('foam.Function', function() {
 
     // empty args
     var str3 = foam.Function.argsStr(
-      function() { 
+      function() {
         return 1;
       }
     );
@@ -507,50 +469,59 @@ describe('foam.Function', function() {
       );
     }).toThrow();
   });
-  
-  it('formalArgs', function() {
-    // normal case
-    var args = foam.Function.formalArgs(
-      function(a, /*string?*/b, c /*array*/) { 
-        return [1];
-      }
-    );   
-    expect(args[0]).toBe('a');
-    expect(args[1]).toBe('b');
-    expect(args[2]).toBe('c');
-    
-    // empty args
-    var args2 = foam.Function.formalArgs(
-      function( ) { 
-        return [1];
-      }
-    );   
-    expect(args2.length).toEqual(0);
 
-    // invalid function string
-    expect(function() {
-      foam.Function.formalArgs(
-      "  fun ction invalid(a, c\r \p\n  { " +
-      "         return [ a, b, c ]; " +
-      "      } "
-      );
-    }).toThrow();
+
+  describe('formalArgs', function() {
+
+    it('handles an empty arg list', function() {
+      var fn = function( ) {
+        return (true);
+      }
+      var args = foam.Function.formalArgs(fn);
+      expect(args).toEqual([]);
+    });
+
+    it('grabs simple argument names', function() {
+      var fn = function(str, bool ,
+         func, obj, num,  arr ) {
+        return (true);
+      }
+      var args = foam.Function.formalArgs(fn);
+      expect(args).toEqual([ 'str', 'bool', 'func', 'obj', 'num', 'arr' ]);
+    });
+
+    it('grabs typed argument names', function() {
+      var fn = function(/* string */ str, /*boolean*/ bool ,
+        /* function*/ func, /*object*/obj, /* number */num, /* array*/ arr ) {
+        return (true);
+      }
+      var args = foam.Function.formalArgs(fn);
+      expect(args).toEqual([ 'str', 'bool', 'func', 'obj', 'num', 'arr' ]);
+    });
+
+    it('grabs commented argument names', function() {
+      var fn = function(/* any // the argument value to validate. */ arg, more,
+          /* // a comment here */ name, another /* return // comment */) {
+        return (true);
+      }
+      var args = foam.Function.formalArgs(fn);
+      expect(args).toEqual([ 'arg', 'more', 'name', 'another' ]);
+    });
 
   });
 
-  
   it('withArgs', function() {
     // normal case
-    var fn = function(a, /*string?*/b, c /*array*/) { 
+    var fn = function(a, /*string?*/b, c /*array*/) {
       return [ a + b + c ];
     };
     var src = { a: 'A', b: 'B', c: 'C' };
-    
+
     expect(foam.Function.withArgs(fn, src)).toEqual(['ABC']);
-    
+
     // missing args
     var src2 = { a: 'A', c: 'C' };
-    
+
     expect(foam.Function.withArgs(fn, src2)).toEqual(['AundefinedC']);
 
     // opt_self
@@ -559,7 +530,7 @@ describe('foam.Function', function() {
     };
     var self = { value: 77 };
     expect(foam.Function.withArgs(fn2, src2, self)).toEqual('77A');
-    
+
     // function args are bound
     var selfFns = {
       a: function() { return this.valA; },
@@ -576,150 +547,150 @@ describe('foam.Function', function() {
       .toEqual(105);
 
   });
+});
 
-  describe('foam.Number', function() {
-    it('isInstance', function() {
-      expect(foam.Number.isInstance(2)).toBe(true);
-      expect(foam.Number.isInstance('2')).toBe(false);
-      expect(foam.Number.isInstance(Math.Infinity)).toBe(false);
-      expect(foam.Number.isInstance(null)).toBe(false);
-    });
-    it('clone', function() {
-      expect(foam.Number.clone(9)).toBe(9);
-    });
-    it('equals', function() {
-      expect(foam.Number.equals(44, 44)).toBe(true);
-      expect(foam.Number.equals(Math.NaN, Math.NaN)).toBe(true);
-      expect(foam.Number.equals(56, '56')).toBe(false);
-      expect(foam.Number.equals(0, false)).toBe(false);
-    });
-    it('compare', function() {
-      expect(foam.Number.compare(3, 4)).toBe(-1);
-      expect(foam.Number.compare(4, 3)).toBe(1);
-      expect(foam.Number.compare(24, 24)).toBe(0);
-      
-      expect(foam.Number.compare(3, null)).toBe(1);
-      expect(foam.Number.compare(3, undefined)).toBe(1);
-      
-    });
-    it('hashCode', function() {
-      expect(foam.Number.hashCode(5)).toBe(5);
-      // caps at 32-bits
-      expect(foam.Number.hashCode(999999999999)).toBe(-727379969);
-    });
+describe('foam.Number', function() {
+  it('isInstance', function() {
+    expect(foam.Number.isInstance(2)).toBe(true);
+    expect(foam.Number.isInstance('2')).toBe(false);
+    expect(foam.Number.isInstance(Math.Infinity)).toBe(false);
+    expect(foam.Number.isInstance(null)).toBe(false);
   });
+  it('clone', function() {
+    expect(foam.Number.clone(9)).toBe(9);
+  });
+  it('equals', function() {
+    expect(foam.Number.equals(44, 44)).toBe(true);
+    expect(foam.Number.equals(Math.NaN, Math.NaN)).toBe(true);
+    expect(foam.Number.equals(56, '56')).toBe(false);
+    expect(foam.Number.equals(0, false)).toBe(false);
+  });
+  it('compare', function() {
+    expect(foam.Number.compare(3, 4)).toBe(-1);
+    expect(foam.Number.compare(4, 3)).toBe(1);
+    expect(foam.Number.compare(24, 24)).toBe(0);
 
-  describe('foam.String', function() {
-    it('isInstance', function() {
-      expect(foam.String.isInstance('hello')).toBe(true);
-      expect(foam.String.isInstance('2')).toBe(true);
-      expect(foam.String.isInstance('')).toBe(true);
-      expect(foam.String.isInstance(2)).toBe(false);
-      expect(foam.String.isInstance(null)).toBe(false);
-    });
-    it('clone', function() {
-      expect(foam.String.clone('clone me')).toBe('clone me');
-    });
-    it('equals', function() {
-      expect(foam.String.equals('a string', 'a string')).toBe(true);
-      expect(foam.String.equals('a string', 'not the same')).toBe(false);
-    });
-    it('compare', function() {
-      expect(foam.String.compare('string a', 'string c')).toBeLessThan(0);     
-      expect(foam.String.compare('string b', 'string a')).toBeGreaterThan(0);     
-      expect(foam.String.compare('string d', 'string d')).toBe(0);     
-
-      expect(foam.String.compare('string d', null)).toBe(1);     
-      expect(foam.String.compare('string d', undefined)).toBe(1);     
-    });
-    it('hashCode', function() {
-      expect(foam.String.hashCode('a short string'))
-        .not.toBe(foam.String.hashCode('a shorp string'))
-      expect(foam.String.hashCode(
-        'here is a very very long string' +
-        'that could, in theory, be harder to' +
-        'uniquely hash given that only one character' +
-        'has changed in the slightest.'
-      )).not.toBe(foam.String.hashCode(
-        'here is a very very long string' +
-        'that could, in theory, be harder to' +
-        'uniquely hash giwen that only one character' +
-        'has changed in the slightest.'
-      ))
-    });
-
-    it('constantize', function() {
-      expect(foam.String.constantize("camelCaseName"))
-        .toBe("CAMEL_CASE_NAME");
-      expect(foam.String.constantize("under_Score"))
-        .toBe("UNDER_SCORE");
-      expect(foam.String.constantize("ALREADY_CONST_NAME"))
-        .toBe("ALREADY_CONST_NAME");
-      expect(function() { 
-        foam.String.constantize(null);
-      }).toThrow();
-    });
-
-    it('capitalize', function() {
-      expect(foam.String.capitalize('lower Case String'))
-        .toBe('Lower Case String');
-      expect(foam.String.capitalize('99$$'))
-        .toBe('99$$');
-      expect(function() { 
-        foam.String.capitalize(null);
-      }).toThrow();
-        
-    });
-
-    it('labelize', function() {
-      expect(foam.String.labelize("camelCaseName"))
-        .toBe("Camel Case Name");
-      expect(foam.String.labelize("CONSTANT_NAME"))
-        .toBe("CONSTANT_NAME");
-      expect(foam.String.labelize(''))
-        .toBe('');
-      expect(function() { 
-        foam.String.labelize(null);
-      }).toThrow();
-    });
-    
-    it('toUpperCase', function() {
-      expect(foam.String.toUpperCase('lower Case String'))
-        .toBe('LOWER CASE STRING');
-      expect(foam.String.toUpperCase('99$$'))
-        .toBe('99$$');
-      expect(function() { 
-        foam.String.toUpperCase(null);
-      }).toThrow();
-        
-    });
-
-    it('cssClassize', function() {
-      expect(foam.String.cssClassize('some.class.Name'))
-        .toBe('some-class-Name');
-      expect(foam.String.cssClassize('99.$$'))
-        .toBe('99-$$');
-      expect(function() { 
-        foam.String.cssClassize(null);
-      }).toThrow();
-        
-    });
-
-
-    it('pad', function() {
-      expect(foam.String.pad("wee", -6)).toEqual("   wee");
-      expect(foam.String.pad("wee", 6)).toEqual("wee   ");
-    });
-
-    it('should convert "FooBar" to "fooBarDAO"', function() {
-      expect(foam.String.daoize('Foo')).toBe('fooDAO');         
-      expect(foam.String.daoize('package.FooBar')).toBe('package.FooBarDAO');
-    });
-
-    
-
+    expect(foam.Number.compare(3, null)).toBe(1);
+    expect(foam.Number.compare(3, undefined)).toBe(1);
 
   });
+  it('hashCode', function() {
+    expect(foam.Number.hashCode(5)).toBe(5);
+    // caps at 32-bits
+    expect(foam.Number.hashCode(999999999999)).toBe(-727379969);
+  });
+});
+
+describe('foam.String', function() {
+  it('isInstance', function() {
+    expect(foam.String.isInstance('hello')).toBe(true);
+    expect(foam.String.isInstance('2')).toBe(true);
+    expect(foam.String.isInstance('')).toBe(true);
+    expect(foam.String.isInstance(2)).toBe(false);
+    expect(foam.String.isInstance(null)).toBe(false);
+  });
+  it('clone', function() {
+    expect(foam.String.clone('clone me')).toBe('clone me');
+  });
+  it('equals', function() {
+    expect(foam.String.equals('a string', 'a string')).toBe(true);
+    expect(foam.String.equals('a string', 'not the same')).toBe(false);
+  });
+  it('compare', function() {
+    expect(foam.String.compare('string a', 'string c')).toBeLessThan(0);
+    expect(foam.String.compare('string b', 'string a')).toBeGreaterThan(0);
+    expect(foam.String.compare('string d', 'string d')).toBe(0);
+
+    expect(foam.String.compare('string d', null)).toBe(1);
+    expect(foam.String.compare('string d', undefined)).toBe(1);
+  });
+  it('hashCode', function() {
+    expect(foam.String.hashCode('a short string'))
+      .not.toBe(foam.String.hashCode('a shorp string'))
+    expect(foam.String.hashCode(
+      'here is a very very long string' +
+      'that could, in theory, be harder to' +
+      'uniquely hash given that only one character' +
+      'has changed in the slightest.'
+    )).not.toBe(foam.String.hashCode(
+      'here is a very very long string' +
+      'that could, in theory, be harder to' +
+      'uniquely hash giwen that only one character' +
+      'has changed in the slightest.'
+    ))
+  });
+
+  it('constantize', function() {
+    expect(foam.String.constantize("camelCaseName"))
+      .toBe("CAMEL_CASE_NAME");
+    expect(foam.String.constantize("under_Score"))
+      .toBe("UNDER_SCORE");
+    expect(foam.String.constantize("ALREADY_CONST_NAME"))
+      .toBe("ALREADY_CONST_NAME");
+    expect(function() {
+      foam.String.constantize(null);
+    }).toThrow();
+  });
+
+  it('capitalize', function() {
+    expect(foam.String.capitalize('lower Case String'))
+      .toBe('Lower Case String');
+    expect(foam.String.capitalize('99$$'))
+      .toBe('99$$');
+    expect(function() {
+      foam.String.capitalize(null);
+    }).toThrow();
+
+  });
+
+  it('labelize', function() {
+    expect(foam.String.labelize("camelCaseName"))
+      .toBe("Camel Case Name");
+    expect(foam.String.labelize("CONSTANT_NAME"))
+      .toBe("CONSTANT_NAME");
+    expect(foam.String.labelize(''))
+      .toBe('');
+    expect(function() {
+      foam.String.labelize(null);
+    }).toThrow();
+  });
+
+  it('toUpperCase', function() {
+    expect(foam.String.toUpperCase('lower Case String'))
+      .toBe('LOWER CASE STRING');
+    expect(foam.String.toUpperCase('99$$'))
+      .toBe('99$$');
+    expect(function() {
+      foam.String.toUpperCase(null);
+    }).toThrow();
+
+  });
+
+  it('cssClassize', function() {
+    expect(foam.String.cssClassize('some.class.Name'))
+      .toBe('some-class-Name');
+    expect(foam.String.cssClassize('99.$$'))
+      .toBe('99-$$');
+    expect(function() {
+      foam.String.cssClassize(null);
+    }).toThrow();
+
+  });
+
+
+  it('pad', function() {
+    expect(foam.String.pad("wee", -6)).toEqual("   wee");
+    expect(foam.String.pad("wee", 6)).toEqual("wee   ");
+  });
+
+  it('should convert "FooBar" to "fooBarDAO"', function() {
+    expect(foam.String.daoize('Foo')).toBe('fooDAO');
+    expect(foam.String.daoize('package.FooBar')).toBe('package.FooBarDAO');
+  });
+
+
 
 
 });
+
+
