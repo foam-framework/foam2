4585bf09f3179bca13953a4541d2ebdc913dcdb4
Kevin Greer <kgr@chromium.org>
2016-10-22 10:17:36 -0400

    Added Mushroom to LivePaint.

---
+++ src/com/google/livepaint/Mushroom.js
@@ -0,0 +1,65 @@
+/**
+ * @license
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+foam.CLASS({
+  package: 'com.google.livepaint',
+  name: 'Mushroom',
+  extends: 'foam.graphics.Circle',
+
+  implements: [ 'foam.physics.Physical' ],
+
+  requires: [ 'foam.graphics.Box' ],
+
+  properties: [
+    [ 'mass', 1000 ],
+    [ 'border', null ],
+    [ 'color', 'red' ],
+    [ 'start',  Math.PI ],
+    [ 'radius', 20 ],
+    [ 'width', 20 ],
+    [ 'height', 35 ],
+    'stem'
+  ],
+
+  methods: [
+    function init() {
+      this.SUPER();
+
+      this.gravity = 0;
+
+      this.add(this.stem = this.Box.create({
+        x: -7.5,
+        y: -0.5,
+        width: 15,
+        height: 20,
+        color: 'gray'
+      }));
+    }
+  ],
+
+  actions: [
+    function explode() {
+      this.stem.color = 'red';
+      // Movement.animate(200, function() {
+      //   this.scaleX = this.scaleY = 30;
+      //   this.alpha = 0;
+      //   this.a = Math.PI * 1.5;
+      //   this.stem.alpha = 0;
+      // }.bind(this)/*, function() { this.table.removeChild(o2); }.bind(this)*/)();
+    }
+  ]
+});
