f228020f31916ba252fcb05e985a28e4077ae066
Adam Van Ymeren <adamvy@google.com>
2016-10-24 15:58:50 -0400

    Update X.create to be generic to reduce the amount of casting required.

--- java_src/foam/core/SimpleFacetManager.java
+++ java_src/foam/core/SimpleFacetManager.java
@@ -5,14 +5,14 @@ public class SimpleFacetManager implements FacetManager {
     return create(type, x);
   }
 
-  public Object create(Class type, X x) {
+  public <T> create(Class<T> type, X x) {
     try {
       // Automatically load FooImpl if Foo is abstract.
       if ( java.lang.reflect.Modifier.isAbstract(type.getModifiers()) ) {
         type = Class.forName(type.getName() + "Impl");
       }
 
-      Object obj = type.newInstance();
+      T obj = type.newInstance();
 
       if ( obj instanceof ContextAware ) {
         ((ContextAware)obj).setX(x);
