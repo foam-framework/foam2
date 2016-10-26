6943cc538300efd8aec84773411ae799da921909
Adam Van Ymeren <adamvy@google.com>
2016-10-26 14:28:53 -0400

    Add resources to generate WAR.

--- java_src/gen.sh
+++ java_src/gen.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
 node ../tools/genjava.js .
+
+node ../tools/build.js web
+
+mkdir -p WEB-INF/resources
+
+cp ../src/files.js WEB-INF/resources
+cp ../src/foam.js WEB-INF/resources
+cp -r ../src/foam WEB-INF/resources
+cp -r ../src/com WEB-INF/resources
