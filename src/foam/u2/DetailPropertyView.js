680a5ab462679e4f1f495f4c59fcd3679cd9902c
Kevin Greer <kgr@chromium.org>
2016-10-27 08:57:09 -0400

    Formatting.

--- src/foam/u2/DetailPropertyView.js
+++ src/foam/u2/DetailPropertyView.js
@@ -50,12 +50,12 @@ foam.CLASS({
     function initE() {
       var prop = this.prop;
 
-      this.cssClass('foam-u2-PropertyView-')
-          .start('td').cssClass('foam-u2-PropertyView-label').add(prop.label).end()
-          .start('td').cssClass('foam-u2-PropertyView-view').add(
+      this.cssClass('foam-u2-PropertyView-').
+          start('td').cssClass('foam-u2-PropertyView-label').add(prop.label).end().
+          start('td').cssClass('foam-u2-PropertyView-view').add(
               prop,
-              prop.units && this.E('span').cssClass('foam-u2-PropertyView-units').add(' ', prop.units))
-          .end();
+              prop.units && this.E('span').cssClass('foam-u2-PropertyView-units').add(' ', prop.units)).
+          end();
     }
   ]
 });
