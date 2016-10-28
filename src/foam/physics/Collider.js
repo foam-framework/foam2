6df96f26595dd0f982cad22c51c45a5026d1ccc1
Kevin Greer <kgr@chromium.org>
2016-10-28 15:39:22 -0400

    Temporarily disable Collider partitioning until it supports mixed shapes.

--- src/foam/physics/Collider.js
+++ src/foam/physics/Collider.js
@@ -45,9 +45,9 @@ foam.CLASS({
   methods: [
     function updateChild(c) {
       if ( this.bounceOnWalls && this.bounds ) {
-        if ( c.x < this.bounds.x ) c.vx = Math.abs(c.vx);
-        if ( c.y < this.bounds.y ) c.vy = Math.abs(c.vy);
-        if ( c.x > this.bounds.width ) c.vx = -Math.abs(c.vx);
+        if ( c.x < this.bounds.x      ) c.vx = Math.abs(c.vx);
+        if ( c.y < this.bounds.y      ) c.vy = Math.abs(c.vy);
+        if ( c.x > this.bounds.width  ) c.vx = -Math.abs(c.vx);
         if ( c.y > this.bounds.height ) c.vy = -Math.abs(c.vy);
       }
     },
@@ -61,7 +61,10 @@ foam.CLASS({
 
     function detectCollisions() {
       /* implicit k-d-tree divide-and-conquer algorithm */
-      this.detectCollisions_(0, this.children.length-1, 'x', false, '');
+      //      this.detectCollisions_(0, this.children.length-1, 'x', false, '');
+
+      // TODO: put back above line when properly supports mixing circles and squares
+      this.detectCollisions__(0, this.children.length-1, 'x', false, '');
     },
 
     function detectCollisions__(start, end) {
