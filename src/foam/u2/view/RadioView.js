02332b1336e565c8a59ff0c91e9abc4461e20c14
Kevin Greer <kgr@chromium.org>
2016-11-04 16:44:27 -0400

    Initial check-in of RadioView.

---
+++ src/foam/u2/view/RadioView.js
@@ -0,0 +1,60 @@
+/**
+ * @license
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+foam.CLASS({
+  package: 'foam.u2.view',
+  name: 'RadioView',
+  extends: 'foam.u2.view.ChoiceView',
+
+  properties: [
+  ],
+
+  methods: [
+    function initE() {
+      // If no item is selected, and data has not been provided, select the 0th
+      // entry.
+      if ( ! this.data && ! this.index ) {
+        this.index = 0;
+      }
+
+      if ( this.dao ) this.onDAOUpdate();
+      this.choices$.sub(this.onChoicesUpdate);
+      this.onChoicesUpdate();
+    }
+  ],
+
+  listeners: [
+    function onChoicesUpdate() {
+      var self = this;
+
+      this.removeAllChildren();
+      this.add(this.choices.map(function(c) {
+        return this.E('input').
+            attrs({
+              type: 'radio',
+              name: this.id,
+              value: c[0],
+              checked: self.slot(function (data) { return data === c[0]; })
+            }).
+            on('change', function(evt) {
+              self.data = evt.srcElement.value;
+            }).
+            add(c[1]);
+      }.bind(this)));
+    },
+  ]
+});
