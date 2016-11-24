be3a2f27564326d6f501b7e40193d77994db90a3
Kevin Greer <kgr@chromium.org>
2016-11-24 17:16:17 -0500

    Remove missing context warning.

--- src/foam/core/EndBoot.js
+++ src/foam/core/EndBoot.js
@@ -149,10 +149,6 @@ foam.CLASS({
       Replaces simpler version defined in original FObject definition.
     */
     function initArgs(args, ctx) {
-      if ( ! ctx &&
-           ! (this.cls_.package && this.cls_.package.startsWith('foam')) ) {
-        this.warn('Missing Context when creating:', this.cls_.id);
-      }
       if ( ctx  ) this.__context__ = ctx;
       if ( args ) this.copyFrom(args, true);
     },
