39fdac18e9da491f1eeaba095b40268ad766b5cd
Jackson Stone <jacksonic@google.com>
2016-11-26 15:42:16 -0500

    String.daoize() Dots allowed in DAO names, Relationship demo fixed

--- test/src/lib/dao.js
+++ test/src/lib/dao.js
@@ -1734,7 +1734,7 @@ describe('String.daoize', function() {
     expect(foam.String.daoize('MyModel')).toEqual('myModelDAO');
   });
   it('handles a model name with package', function() {
-    expect(foam.String.daoize('test.package.PkgModel')).toEqual('test_package_PkgModelDAO');
+    expect(foam.String.daoize('test.package.PkgModel')).toEqual('test.package.PkgModelDAO');
   });
 
 });
