7efbfef8a1106426200fe84f68aa51af97ed0b1f
Adam Van Ymeren <adamvy@google.com>
2016-12-05 14:51:31 -0500

    Java: Add support for deafult class to JSON parsing.

--- java_src/foam/lib/json/FObjectParser.java
+++ java_src/foam/lib/json/FObjectParser.java
@@ -4,42 +4,58 @@ import foam.lib.parse.*;
 import foam.core.*;
 
 public class FObjectParser extends ProxyParser {
-  public FObjectParser() {
-    super(new Seq1(7,
+  public FObjectParser(final Class defaultClass) {
+    super(new Seq1(3,
                    new Whitespace(),
                    new Literal("{"),
                    new Whitespace(),
-                   new KeyParser("class"),
-                   new Whitespace(),
-                   new Literal(":"),
-                   new Whitespace(),
                    new Parser() {
-        private Parser delegate = new StringParser();
-        public PStream parse(PStream ps, ParserContext x) {
-          ps = delegate.parse(ps, x);
-          if ( ps == null ) return null;
+                     private Parser delegate = new Seq1(4,
+                                                        new KeyParser("class"),
+                                                        new Whitespace(),
+                                                        new Literal(":"),
+                                                        new Whitespace(),
+                                                        new StringParser(),
+                                                        new Optional(new Literal(",")));
+                     public PStream parse(PStream ps, ParserContext x) {
+                       PStream ps1 = delegate.parse(ps, x);
 
-          Class c;
+                       Class c;
 
-          try {
-            c = Class.forName(ps.value().toString());
-          } catch(ClassNotFoundException e) {
-            throw new RuntimeException(e);
-          }
+                       try {
 
-          ParserContext subx = x.sub();
-          Object obj = ((X)x.get("X")).create(c);
-          subx.set("obj", obj);
+                         c = ps1 != null ? Class.forName(ps1.value().toString()) :
+                           x.get("defaultClass") != null ? (Class)x.get("defaultClass") :
+                           defaultClass;
 
-          ps = ModelParserFactory.getInstance(c).parse(ps, subx);
+                         if ( c == null ) {
+                           throw new RuntimeException("No class specified in JSON and no defaultClass available.");
+                         }
+                       } catch(ClassNotFoundException e) {
+                         throw new RuntimeException(e);
+                       }
 
-          if ( ps != null ) {
-            return ps.setValue(subx.get("obj"));
-          }
-          return null;
-        }
-      },
+                       if ( ps1 != null ) {
+                         ps = ps1;
+                       }
+
+                       ParserContext subx = x.sub();
+                       Object obj = ((X)x.get("X")).create(c);
+                       subx.set("obj", obj);
+
+                       ps = ModelParserFactory.getInstance(c).parse(ps, subx);
+
+                       if ( ps != null ) {
+                         return ps.setValue(subx.get("obj"));
+                       }
+                       return null;
+                     }
+                   },
                    new Whitespace(),
                    new Literal("}")));
+  }
+
+  public FObjectParser() {
+    this(null);
   }
 }
