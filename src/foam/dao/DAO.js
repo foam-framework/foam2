619b5fca65e74613e53e2fade8d84273435522d1
Kevin Greer <kgr@chromium.org>
2016-12-13 15:27:56 -0500

    Uncommented documentation.

--- src/foam/dao/DAO.js
+++ src/foam/dao/DAO.js
@@ -19,7 +19,7 @@ foam.INTERFACE({
   package: 'foam.dao',
   name: 'DAO',
 
-  // documentation: 'DAO Interface',
+  documentation: 'DAO Interface',
 
   methods: [
     {
@@ -162,4 +162,3 @@ foam.INTERFACE({
     }
   ]
 });
-
