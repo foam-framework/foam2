6ddfe6087dbfaf7583cce13512e736ebcf83097e
Jackson Stone <jacksonic@google.com>
2016-12-13 10:45:33 -0500

    Split dao.js into DAO.js, AbstractDAO.js, and leftovers in daoUtils.js

--- src/foam/dao/AbstractDAO.js
+++ src/foam/dao/AbstractDAO.js
@@ -227,3 +227,225 @@ foam.CLASS({
     function reset() {}
   ]
 });
+
+
+foam.CLASS({
+  package: 'foam.core',
+  name: 'Exception',
+  properties: [
+    'message'
+  ]
+});
+
+foam.CLASS({
+  package: 'foam.dao',
+  name: 'InternalException',
+  extends: 'Exception'
+});
+
+
+foam.CLASS({
+  package: 'foam.dao',
+  name: 'ExternalException',
+  extends: 'Exception'
+})
+
+
+foam.CLASS({
+  package: 'foam.dao',
+  name: 'ObjectNotFoundException',
+  extends: 'foam.dao.ExternalException',
+
+  properties: [
+    'id',
+    {
+      name: 'message',
+      expression: function(id) { return "No record found for id: " + id; }
+    }
+  ]
+});
+
+
+foam.CLASS({
+  package: 'foam.dao',
+  name: 'FilteredDAO',
+  extends: 'foam.dao.AbstractDAO',
+
+  requires: [
+    'foam.mlang.predicate.And'
+  ],
+
+  properties: [
+    {
+      name: 'predicate',
+      required: true
+    },
+    {
+      name: 'of',
+      expression: function(delegate) {
+        return delegate.of;
+      }
+    },
+    {
+      class: 'Proxy',
+      of: 'foam.dao.DAO',
+      name: 'delegate',
+      topics: [],
+      forwards: [ 'put', 'remove', 'find', 'select', 'removeAll' ],
+      postSet: function(old, nu) {
+        // Only fire a 'reset' when the delegate is actually changing, not being
+        // set for the first time.
+        if ( old ) {
+          this.on.reset.pub();
+        }
+
+        // TODO: replace this with a manually installed ProxySub,
+        //   or implement interceptors in Proxy
+        if ( this.delegateSub_ ) {
+          this.delegateSub_.destroy();
+          this.delegateSub_ = nu.on.sub(this.onEvent);
+        }
+      }
+    },
+    'delegateSub_',
+  ],
+
+  listeners: [
+    /** If the predicate returns false for the object added or updated, change
+      to an on.remove event. If the listener had previously been told about
+      the object, it should now remove it since it no longer matches. */
+    function onEvent(s, on, putRemoveReset, obj) {
+      if ( putRemoveReset === 'put' ) {
+        if ( this.predicate.f(obj) ) {
+          this.pub(on, 'put', obj);
+        } else {
+          this.pub(on, 'remove', obj);
+        }
+      } else {
+        this.pub(on, putRemoveReset, obj);
+      }
+    },
+  ],
+
+  methods: [
+    function sub(arg1) {
+      if ( arg1 === 'on' && ! this.delegateSub_ ) {
+        var self = this;
+        this.delegateSub_ = this.delegate.on.sub(this.onEvent);
+        this.onDestroy(function() {
+          self.delegateSub_ && self.delegateSub_.destroy();
+          self.delegateSub_ = null;
+        });
+      }
+      return this.SUPER.apply(this, arguments);
+    },
+
+    function select(sink, skip, limit, order, predicate) {
+      return this.delegate.select(
+        sink, skip, limit, order,
+        predicate ?
+          this.And.create({ args: [this.predicate, predicate] }) :
+          this.predicate);
+    },
+
+    function removeAll(skip, limit, order, predicate) {
+      return this.delegate.removeAll(
+        skip, limit, order,
+        predicate ?
+          this.And.create({ args: [this.predicate, predicate] }) :
+          this.predicate);
+    },
+
+    function listen(sink, predicate) {
+      return this.delegate.listen(
+        sink,
+        predicate ?
+          this.And.create({ args: [this.predicate, predicate] }) :
+          this.predicate);
+    }
+  ]
+});
+
+
+foam.CLASS({
+  package: 'foam.dao',
+  name: 'OrderedDAO',
+  extends: 'foam.dao.ProxyDAO',
+
+  properties: [
+    {
+      name: 'comparator'
+    }
+  ],
+
+  methods: [
+    function select(sink, skip, limit, order, predicate) {
+      return this.delegate.select(sink, skip, limit, order ? order : this.comparator, predicate);
+    },
+    function removeAll(skip, limit, order, predicate) {
+      return this.delegate.removeAll(skip, limit, order ? order : this.comparator, predicate);
+    },
+    function listen(sink, predicate) {
+      return this.delegate.listen(sink, predicate);
+    }
+  ]
+});
+
+
+foam.CLASS({
+  package: 'foam.dao',
+  name: 'SkipDAO',
+  extends: 'foam.dao.ProxyDAO',
+
+  properties: [
+    {
+      name: 'skip_'
+    }
+  ],
+
+  methods: [
+    function select(sink, skip, limit, order, predicate) {
+      return this.delegate.select(sink, this.skip_, limit, order, predicate);
+    },
+    function removeAll(skip, limit, order, predicate) {
+      return this.delegate.removeAll(this.skip_, limit, order, predicate);
+    },
+    function listen(sink, predicate) {
+      return this.delegate.listen(sink, predicate);
+    },
+  ]
+});
+
+
+foam.CLASS({
+  package: 'foam.dao',
+  name: 'LimitedDAO',
+  extends: 'foam.dao.ProxyDAO',
+
+  properties: [
+    {
+      name: 'limit_'
+    }
+  ],
+
+  methods: [
+    function select(sink, skip, limit, order, predicate) {
+      return this.delegate.select(
+        sink, skip,
+        limit !== undefined ? Math.min(this.limit_, limit) : this.limit_,
+        order, predicate);
+    },
+
+    function removeAll(skip, limit, order, predicate) {
+      return this.delegate.removeAll(
+        skip,
+        limit !== undefined ? Math.min(this.limit_, limit) : this.limit_,
+        order, predicate);
+    },
+
+    function listen(sink, predicate) {
+      return this.delegate.listen(sink, predicate);
+    }
+  ]
+});
+
---
+++ src/foam/dao/DAO.js
@@ -0,0 +1,165 @@
+/**
+ * @license
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+foam.INTERFACE({
+  package: 'foam.dao',
+  name: 'DAO',
+
+  // documentation: 'DAO Interface',
+
+  methods: [
+    {
+      name: 'put',
+      returns: 'Promise',
+      javaReturns: 'foam.core.FObject',
+      args: [
+        {
+          name: 'obj',
+          javaType: 'foam.core.FObject'
+        }
+      ]
+    },
+    {
+      name: 'remove',
+      returns: 'Promise',
+      javaReturns: 'foam.core.FObject',
+      args: [
+        {
+          name: 'obj',
+          javaType: 'foam.core.FObject'
+        }
+      ]
+    },
+    {
+      name: 'find',
+      returns: 'Promise',
+      javaReturns: 'foam.core.FObject',
+      args: [
+        {
+          name: 'id',
+          javaType: 'Object'
+        }
+      ]
+    },
+    {
+      name: 'select',
+      returns: 'Promise',
+      javaReturns: 'foam.dao.Sink',
+      args: [
+        {
+          name: 'sink',
+          javaType: 'foam.dao.Sink'
+        },
+        {
+          name: 'skip',
+          javaType: 'Integer'
+        },
+        {
+          name: 'limit',
+          javaType: 'Integer'
+        },
+        {
+          name: 'order',
+          javaType: 'foam.mlang.order.Comparator'
+        },
+        {
+          name: 'predicate',
+          javaType: 'foam.mlang.predicate.Predicate'
+        }
+      ]
+    },
+    {
+      name: 'removeAll',
+      returns: '',
+      javaReturns: 'void',
+      args: [
+        {
+          name: 'skip',
+          javaType: 'Integer'
+        },
+        {
+          name: 'limit',
+          javaType: 'Integer'
+        },
+        {
+          name: 'order',
+          javaType: 'foam.mlang.order.Comparator'
+        },
+        {
+          name: 'predicate',
+          javaType: 'foam.mlang.predicate.Predicate'
+        }
+      ]
+    },
+    {
+      name: 'pipe', // TODO: return a promise? don't put pipe and listen here?
+      returns: '',
+      javaReturns: 'void',
+      args: [
+        {
+          name: 'sink',
+          javaType: 'foam.dao.Sink'
+        }
+      ]
+    },
+    {
+      name: 'where',
+      returns: 'foam.dao.DAO',
+      javaReturns: 'foam.dao.DAO',
+      args: [
+        {
+          name: 'predicate',
+          javaType: 'foam.mlang.predicate.Predicate'
+        }
+      ]
+    },
+    {
+      name: 'orderBy',
+      returns: 'foam.dao.DAO',
+      javaReturns: 'foam.dao.DAO',
+      args: [
+        {
+          name: 'comparator',
+          javaType: 'foam.mlang.order.Comparator'
+        }
+      ]
+    },
+    {
+      name: 'skip',
+      returns: 'foam.dao.DAO',
+      javaReturns: 'foam.dao.DAO',
+      args: [
+        {
+          name: 'count',
+          javaType: 'int'
+        }
+      ]
+    },
+    {
+      name: 'limit',
+      returns: 'foam.dao.DAO',
+      javaReturns: 'foam.dao.DAO',
+      args: [
+        {
+          name: 'count',
+          javaType: 'int'
+        }
+      ]
+    }
+  ]
+});
+
