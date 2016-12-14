3eccbf8a5a9f8ffc7e20e96e3295538a01069c76
Jackson Stone <jacksonic@google.com>
2016-12-14 14:04:04 -0500

    TypeChecking on LIBs active. Memoize0/1 retain the original function's toString() in order to support type checking on memo'd functions.

--- src/foam/core/debug.js
+++ src/foam/core/debug.js
@@ -351,9 +351,34 @@ foam.LIB({
       *         then type check the returned value.
       */
     function typeCheck(fn) {
+      // Multiple definitions of LIBs may trigger this multiple times
+      // on the same function
+      if ( fn.isTypeChecker__ ) return fn;
+      
       // parse out the arguments and their types
       var args = foam.Function.args(fn);
-      var ret = function() {
+      
+      // check if no checkable arguments
+      var checkable = false;
+      function isArgUncheckable(a) {
+        return ( ( a.typeName === '' || a.typeName === 'any' || foam.Undefined.isInstance(a.typeName) ) && 
+          ( a.optional || a.repeated ) );
+      }
+      for ( var i = 0 ; i < args.length ; i++ ) {
+        if ( ! isArgUncheckable(args[i]) ) {
+          checkable = true;
+          break;
+        }
+      }
+      if ( ! checkable && args.returnType ) {
+        checkable = ! isArgUncheckable(args.returnType);
+      }
+      if ( ! checkable ) {
+        // nothing to check, don't decorate
+        return fn;
+      }
+      
+      var typeChecker = function() {
         // check each declared argument, arguments[i] can be undefined for
         // missing optional args, extra arguments are ok
         for ( var i = 0 ; i < args.length ; i++ )
@@ -368,44 +393,78 @@ foam.LIB({
         }
 
         // If nothing threw an exception, we are free to run the function
-        var retVal = fn.apply(this, arguments);
+        var typeCheckerVal = fn.apply(this, arguments);
 
-        // check the return value
-        if ( args.returnType ) args.returnType.validate(retVal);
+        // check the typeCheckerurn value
+        if ( args.returnType ) args.returnType.validate(typeCheckerVal);
 
-        return retVal;
+        return typeCheckerVal;
       }
 
       // keep the old value of toString (hide the decorator)
-      ret.toString = function() { return fn.toString(); }
+      typeChecker.toString = function() { return fn.toString(); }
+      typeChecker.isTypeChecker__ = true;
 
-      return ret;
+      return typeChecker;
     }
   ]
 });
 
-// // -- Uncomment to enable type checking on all methods --
-// // Access Argument now to avoid circular reference because of lazy model building.
-// foam.core.Argument;
-//
-// /* Methods gain type checking. */
-// foam.CLASS({
-//   refines: 'foam.core.Method',
-//
-//   properties: [
-//     {
-//       name: 'code',
-//       adapt: function(old, nu) {
-//         if ( nu )
-//           return foam.Function.typeCheck(nu);
-//
-//         return nu;
-//       }
-//     }
-//
-//   ]
-// });
+// -- Uncomment to enable type checking on all methods --
+// Access Argument now to avoid circular reference because of lazy model building.
+foam.core.Argument;
 
+/* Methods gain type checking. */
+foam.CLASS({
+  refines: 'foam.core.Method',
+
+  properties: [
+    {
+      name: 'code',
+      adapt: function(old, nu) {
+        if ( nu )
+          return foam.Function.typeCheck(nu);
+
+        return nu;
+      }
+    }
+
+  ]
+});
+// Upgrade a LIBs
+var upgradeLib = function upgradeLib(lib) {
+  for ( var key in lib ) {
+    var func = lib[key];
+    if ( foam.Function.isInstance(func) ) {
+      lib[key] = foam.Function.typeCheck(func);
+    }
+  }
+};
+
+// Upgrade each existing LIB
+for ( var name in foam.__LIBS__ ) {
+  upgradeLib(foam.__LIBS__[name]);
+}
+foam.__LIBS__ = null;
+
+// Decorate foam.LIB to typeCheck new libs
+var oldLIB = foam.LIB;
+foam.LIB = function typeCheckedLIB(model) {
+  // Create the lib normally
+  oldLIB(model);
+
+  // Find the created LIB
+  var root = global;
+  var path = model.name.split('.');
+  var i;
+  for ( i = 0 ; i < path.length ; i++ ) {
+    root = root[path[i]];
+  }
+  if ( ! root ) {
+    throw 'debug.js: type checking for LIB ' + model.name + ', LIB not created.';
+  }
+  upgradeLib(root);
+}
 
 
 
