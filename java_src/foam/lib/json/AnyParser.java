29c8aa668ddb702b8ade7a4fcde2db35696882fe
Joel Hughes <joel.l.hughes@gmail.com>
2016-12-21 01:48:00 -0500

    Add Java support for property class type Float.

--- java_src/foam/lib/json/AnyParser.java
+++ java_src/foam/lib/json/AnyParser.java
@@ -7,6 +7,7 @@ public class AnyParser extends ProxyParser {
     super(new Alt(
                   new NullParser(),
                   new StringParser(),
+                  new FloatParser(),
                   new IntParser(),
                   new BooleanParser(),
                   new FObjectParser()));
