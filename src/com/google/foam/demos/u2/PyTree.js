e518166f9dae74de88b0f7934b2eaf159f81c8d4
Kevin Greer <kgr@chromium.org>
2017-01-02 09:41:01 -0500

    Start of PyTree U2 demo.

---
+++ src/com/google/foam/demos/u2/PyTree.js
@@ -0,0 +1,44 @@
+foam.CLASS({
+  name: 'PyTree',
+  extends: 'foam.u2.Element',
+
+  properties: [
+    { class: 'Float', name: 'w', value: 50 },
+    { class: 'Float', name: 'x' },
+    { class: 'Float', name: 'y' },
+    { class: 'Float', name: 'rotate', value: 10 },
+    { name: 'heightFactor', value: 0.5 },
+    { name: 'lvl', value: 1 },
+    { name: 'maxlvl', value: 3 }
+  ],
+
+  methods: [
+    function initE() {
+      this.SUPER();
+
+      console.log('****', this.lvl);
+      var x = this.x, y = this.y, rotate = this.rotate;
+      this.setNodeName('g').
+          attrs({transform: 'translate(' + this.x + ' ' + this.y + ') rotate(' + this.rotate + ')'}).
+          start('rect').
+            attrs({width: this.w, height: this.w, x: 0, y: 0}).
+            style({fill: `hsl({this.lvl/this.maxlvl*360},70,70)`}).
+          end();
+
+      if ( this.lvl < this.maxlvl ) {
+        this.add(this.cls_.create({
+          x: 0,
+          y: -50,
+          w: this.w * this.heightFactor,
+          lvl: this.lvl+1,
+          maxlvl: this.maxlvl,
+          heightFactor: this.heightFactor,
+        }));
+      }
+    }
+  ]
+});
+
+
+var tree = PyTree.create();
+foam.__context__.E('svg').add(tree).write();
