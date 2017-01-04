89e5cad1416c43df15d9e492f2917b897bce515a
Kevin Greer <kgr@chromium.org>
2017-01-04 11:53:16 -0500

    PyTree demo.

--- src/com/google/foam/demos/u2/PyTree3.js
+++ src/com/google/foam/demos/u2/PyTree3.js
@@ -15,6 +15,7 @@ foam.CLASS({
       this.setNodeName('g').
         attrs({transform: this.transform}).
         start('rect').
+          setID('').
           attrs({width: 1, height: 1}).
           style({fill: this.fillColor(this.lvl)}).
         end();
@@ -28,7 +29,7 @@ foam.CLASS({
     {
       name: 'fillColor',
       code: foam.Function.memoize1(function(lvl) {
-        return 'hsl(' + lvl/this.maxLvl*180 + ',70%,70%)';
+        return 'hsl(' + Math.floor(lvl/this.maxLvl*180) + ',70%,70%)';
       })
     }
   ]
@@ -58,7 +59,8 @@ foam.CLASS({
       this.SUPER();
 
       this.setNodeName('svg').
-        attrs({width: 1600, height: 800}).
+        style({border: '1px solid lightgray'}).
+        attrs({width: 1200, height: 600}).
         on('mousemove', this.onMouseMove).
         add(this.slot(function(heightFactor, lean) {
           var a = Math.atan2(heightFactor, .5-lean);
@@ -67,21 +69,21 @@ foam.CLASS({
           this.lScale = Math.sqrt(heightFactor**2 + (.5-lean)**2);
           this.rScale = Math.sqrt(heightFactor**2 + (.5+lean)**2);
 
-          this.lTransform = 'scale(' + this.lScale + ') translate(0 -1) rotate(' + this.radToDeg(-a) + ' 0 1)';
-          this.rTransform = 'translate(1 0) scale(' + this.rScale + ') translate(-1 -1) rotate(' + this.radToDeg(b) + ' 1 1)';
+          this.lTransform = 'scale(' + this.lScale.toFixed(2) + ') translate(0 -1) rotate(' + this.radToDeg(-a) + ' 0 1)';
+          this.rTransform = 'translate(1 0) scale(' + this.rScale.toFixed(2) + ') translate(-1 -1) rotate(' + this.radToDeg(b) + ' 1 1)';
 
-          return this.PyBranch.create({lvl: 1, w: 80, transform: 'translate(760 500) scale(80)'});
+          return this.PyBranch.create({lvl: 1, w: 80, transform: 'translate(560 510) scale(80)'});
         }));
     },
 
-    function radToDeg(r) { return 180*r/Math.PI; }
+    function radToDeg(r) { return Math.floor(180*r/Math.PI); }
   ],
 
   listeners: [
     {
       name: 'onMouseMove',
       code: function(e) {
-        this.heightFactor = e.clientY / this.getAttribute('height') * 0.8;
+        this.heightFactor = (1 - e.clientY / this.getAttribute('height')) * 0.8;
         this.lean         = e.clientX / this.getAttribute('width') - 0.5;
       }
     }
