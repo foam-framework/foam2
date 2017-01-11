c79870992d25b81244e5d837c59a0f95e92d9660
Kevin Greer <kgr@chromium.org>
2017-01-11 16:48:42 -0500

    Make some CView methods fluent.

--- src/foam/graphics/CView.js
+++ src/foam/graphics/CView.js
@@ -140,21 +140,25 @@ foam.CLASS({
     function translate(dx, dy) {
       if ( ! dx && ! dy ) return;
       this.mul(1, 0, dx, 0, 1, dy, 0, 0, 1);
+      return this;
     },
 
     function skew(x, y) {
       if ( ! x && ! y ) return;
       this.mul(1, x, 0, y, 1, 0, 0, 0, 1);
+      return this;
     },
 
     function scale(x, y) {
       if ( x === 1 && y === 1 ) return;
       this.mul(x, 0, 0, 0, y, 0, 0, 0, 1);
+      return this;
     },
 
     function rotate(a) {
       if ( ! a ) return;
       this.mul(Math.cos(a), Math.sin(a), 0, -Math.sin(a), Math.cos(a), 0, 0, 0, 1);
+      return this;
     }
   ]
 });
