a0901ce2716c7bb76dd98af14c7535a46a8c0157
Kevin Greer <kgr@chromium.org>
2017-01-15 08:54:02 -0500

    Working PyTreeCView.

--- src/com/google/foam/demos/u2/PyTreeCView.js
+++ src/com/google/foam/demos/u2/PyTreeCView.js
@@ -1,23 +1,30 @@
 foam.CLASS({
   name: 'PyBranch',
-  extends: 'foam.graphics.CView',
+  extends: 'foam.graphics.Box',
 
-  requires: [ 'PyBranch', 'foam.graphics.Box' ],
+  requires: [ 'PyBranch' ],
 
-  imports: [ 'lScale', 'rScale', 'lTransform', 'rTransform', 'maxLvl' ],
+  imports: [ 'lTransform', 'rTransform', 'maxLvl' ],
 
-  properties: [ 'w', 'transform', 'lvl' ],
+  properties: [
+    [ 'width', 1 ],
+    [ 'height', 1 ],
+    [ 'borderWidth', 0 ],
+    { name: 'color', factory: function() { return this.fillColor(this.lvl); } },
+    'lvl'
+  ],
 
   methods: [
     function init() {
-      this.add(this.Box.create({width: 1, height: 1, color: this.fillColor(this.lvl)}));
+      this.SUPER();
 
       if ( this.lvl < this.maxLvl ) {
-        var lW = this.w * this.lScale, rW = this.w * this.rScale;
-        this.add(this.PyBranch.create({id: null, w: lW, lvl: this.lvl+1, transform: this.lTransform}));
-        this.add(this.PyBranch.create({id: null, w: rW, lvl: this.lvl+1, transform: this.rTransform}));
+        this.add(
+          this.PyBranch.create({lvl: this.lvl+1, transform$: this.lTransform$}),
+          this.PyBranch.create({lvl: this.lvl+1, transform$: this.rTransform$}));
       }
     },
+
     {
       name: 'fillColor',
       code: foam.Function.memoize1(function(lvl) {
@@ -29,61 +36,55 @@ foam.CLASS({
 
 
 foam.CLASS({
-  name: 'PyTree',
+  name: 'PyTreeCView',
   extends: 'foam.graphics.CView',
 
   requires: [ 'PyBranch', 'foam.graphics.Transform' ],
 
-  exports: [ 'lScale', 'rScale', 'lTransform', 'rTransform', 'maxLvl' ],
+  exports: [ 'lTransform', 'rTransform', 'maxLvl' ],
 
   properties: [
-    { name: 'heightFactor', value: 0.55 },
-    { name: 'lean',         value: 0 },
-    { name: 'maxLvl',       value: 11 },
-    'lScale',
-    'rScale',
+    [ 'width', 1600 ],
+    [ 'height', 600 ],
+    [ 'heightFactor', 0.55 ],
+    [ 'lean', 0 ],
+    [ 'maxLvl', 11 ],
     'lTransform',
     'rTransform'
   ],
 
   methods: [
-    function init() {
-      this.el().on('mousemove', this.onMouseMove);
-
-      this.setNodeName('svg').
-        style({border: '1px solid lightgray'}).
-        attrs({width: 1200, height: 600}).
-        add(this.slot(function(heightFactor, lean) {
-          var a = Math.atan2(heightFactor, .5+lean);
-          var b = Math.atan2(heightFactor, .5-lean);
-
-          this.lScale = Math.sqrt(heightFactor**2 + (.5+lean)**2);
-          this.rScale = Math.sqrt(heightFactor**2 + (.5-lean)**2);
-
-          this.lTransform = this.Transform.create().
-              scale(this.lScale, this.lScale).
-              translate(0, -1).
-              rotateAround(this.radToDeg(-a), 0, 1);
-
-          this.rTransform = this.Transform.create().
-              translate(1, 0).
-              scale(this.rScale, this.rScale).
-              translate(-1, -1).
-              rotateAround(this.radToDeg(b), 1, 1);
-
-          return this.PyBranch.create({lvl: 1, w: 80, transform: 'translate(560 510) scale(80)'});
-        }));
+    function initCView() {
+      this.SUPER();
+      this.orient();
+      this.canvas.on('mousemove', this.onMouseMove);
+      this.add(this.PyBranch.create({x: 560, y: 510, scaleX: 80, scaleY: 80, lvl: 1}));
     },
 
-    function radToDeg(r) { return 180*r/Math.PI; }
+    function orient() {
+      var heightFactor = this.heightFactor, lean = this.lean;
+      var a      = Math.atan2(heightFactor,    .5+lean);
+      var b      = Math.atan2(heightFactor,    .5-lean);
+      var lScale = this.distance(heightFactor, .5+lean);
+      var rScale = this.distance(heightFactor, .5-lean);
+
+      this.lTransform = this.Transform.create().
+        scale(lScale).rotate(a).translate(0, -1);
+
+      this.rTransform = this.Transform.create().
+        translate(1, 0).scale(rScale).rotate(-b).translate(-1, -1);
+    },
+
+    function distance(x, y) { return Math.sqrt(x*x + y*y); }
   ],
 
   listeners: [
     function onMouseMove(e) {
-      this.heightFactor = (1 - e.offsetY / this.getAttribute('height')) * 0.8;
-      this.lean         = e.offsetX / this.getAttribute('width') - 0.5;
+      this.heightFactor = (1 - e.clientY / this.height) * 0.8;
+      this.lean         = e.clientX / this.width - 0.5;
+      this.orient();
     }
-]
+  ]
 });
 
 
