6e8f4cfd01b152310482680e47c8d2734be6dffe
Kevin Greer <kgr@chromium.org>
2017-01-16 12:21:55 -0500

    Update PyTreeCView.

--- src/com/google/foam/demos/u2/PyTreeCView.js
+++ src/com/google/foam/demos/u2/PyTreeCView.js
@@ -1,3 +1,22 @@
+/**
+ * @license
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Dancing Pythagoras Tree
+// Ported from: https://swizec.com/blog/animating-svg-nodes-react-preact-inferno-vue/swizec/7311
 foam.CLASS({
   name: 'PyBranch',
   extends: 'foam.graphics.Box',
@@ -44,11 +63,11 @@ foam.CLASS({
   exports: [ 'lTransform', 'rTransform', 'maxLvl' ],
 
   properties: [
-    [ 'width', 1600 ],
-    [ 'height', 600 ],
     [ 'heightFactor', 0.55 ],
     [ 'lean', 0 ],
     [ 'maxLvl', 11 ],
+    [ 'width', 1600 ],
+    [ 'height', 600 ],
     'lTransform',
     'rTransform'
   ],
@@ -56,12 +75,16 @@ foam.CLASS({
   methods: [
     function initCView() {
       this.SUPER();
-      this.orient();
       this.canvas.on('mousemove', this.onMouseMove);
       this.add(this.PyBranch.create({x: 560, y: 510, scaleX: 80, scaleY: 80, lvl: 1}));
+      this.redraw();
     },
 
-    function orient() {
+    function distance(x, y) { return Math.sqrt(x*x + y*y); }
+  ],
+
+  listeners: [
+    function redraw() {
       var heightFactor = this.heightFactor, lean = this.lean;
       var a      = Math.atan2(heightFactor,    .5+lean);
       var b      = Math.atan2(heightFactor,    .5-lean);
@@ -75,17 +98,10 @@ foam.CLASS({
         translate(1, 0).scale(rScale).rotate(-b).translate(-1, -1);
     },
 
-    function distance(x, y) { return Math.sqrt(x*x + y*y); }
-  ],
-
-  listeners: [
     function onMouseMove(e) {
       this.heightFactor = (1 - e.clientY / this.height) * 0.8;
       this.lean         = e.clientX / this.width - 0.5;
-      this.orient();
+      this.redraw();
     }
   ]
 });
-
-
-PyTreeCView.create().write();
