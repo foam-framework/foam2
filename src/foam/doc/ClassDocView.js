e5ee422ad740c49adf8d45d03b7e71e2b41e5789
Kevin Greer <kgr@chromium.org>
2017-01-28 08:03:50 -0500

    Updates to ClassDocView.

--- src/foam/doc/ClassDocView.js
+++ src/foam/doc/ClassDocView.js
@@ -1,12 +1,58 @@
 foam.CLASS({
   package: 'foam.doc',
+  name: 'AxiomInfo',
+
+  properties: [
+    {
+      name: 'type'
+    },
+    {
+      name: 'cls',
+      label: 'Source Class'
+    },
+    {
+      name: 'name'
+    }
+  ]
+});
+
+
+foam.CLASS({
+  package: 'foam.doc',
   name: 'ClassDocView',
   extends: 'foam.u2.View',
+
+  requires: [
+    'foam.doc.AxiomInfo',
+    'foam.u2.TableView',
+    'foam.dao.ArrayDAO'
+  ],
 
   methods: [
     function initE() {
       this.SUPER();
-      this.add('CLASS: ', this.data.name);
+
+      var data = this.data;
+
+      this.add('CLASS: ', data.name).br();
+      this.add('extends: ', data.model_.extends).br();
+      this.add(data.documentation);
+
+      var axs = [];
+      for ( var key in data.axiomMap_ ) {
+        var a = data.axiomMap_[key];
+        var ai = foam.doc.AxiomInfo.create({
+          type: a.cls_ ? a.cls_.name : 'anonymous',
+          cls: a.sourceCls_.name,
+          name: a.name
+        });
+        axs.push(ai);
+      }
+
+      this.add(this.TableView.create({
+        of: this.AxiomInfo,
+        data: this.ArrayDAO.create({array: axs})
+      }));
     }
   ]
 });
