32d404071784728cea32786f28e192a46156807b
Kevin Greer <kgr@chromium.org>
2017-02-04 12:10:45 -0500

    Fix DocBrowser window parameters.

--- src/foam/doc/DocBrowser.js
+++ src/foam/doc/DocBrowser.js
@@ -312,7 +312,7 @@ foam.CLASS({
   methods: [
     function init() {
       // TODO: There should be some helper support to make this easier
-      var w = this.window.open(null, null, {width: 700, heigh: 1000});
+      var w = this.window.open('', '', 'width=700, heigh=1000');
       var window = foam.core.Window.create({window: w});
       var browser = this.DocBrowser.create({path: this.initialClass}, window.__subContext__);
       w.document.body.insertAdjacentHTML('beforeend', browser.outerHTML);
