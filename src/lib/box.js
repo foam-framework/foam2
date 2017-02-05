932b5158ce898ea5375322b8cfc02726a7b9dd5b
Joel Hughes <joel.l.hughes@gmail.com>
2017-02-05 13:32:18 -0500

    Add Java support for Float and Long

--- src/lib/box.js
+++ src/lib/box.js
@@ -821,7 +821,7 @@ foam.CLASS({
       }
 
       throw this.InvalidMessageException.create({
-        messageType: message.cls_.id
+        messageType: message.cls_ && message.cls_.id
       });
     }
   ]
