7987b0c2a4107c55228dbeed70107a3b471ed819
Mike Carcasole <mikecarcasole@gmail.com>
2017-02-09 18:53:03 -0500

    Add a bunch of refinements and models for swift support.

--- src/foam/swift/SwiftClass.js
+++ src/foam/swift/SwiftClass.js
@@ -1,8 +1,28 @@
+/**
+ * @license
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 foam.CLASS({
   package: 'foam.swift',
   name: 'SwiftClass',
 
   requires: [
+    'foam.swift.Argument',
+    'foam.swift.Field',
+    'foam.swift.Method',
     'foam.swift.Outputter'
   ],
 
@@ -17,6 +37,12 @@ foam.CLASS({
       name: 'fields',
       factory: function() { return []; }
     },
+    {
+      class: 'FObjectArray',
+      of: 'foam.swift.Method',
+      name: 'methods',
+      factory: function() { return []; }
+    },
   ],
 
   methods: [
@@ -25,7 +51,8 @@ foam.CLASS({
       o.out('class ', this.name, ' {\n');
       o.increaseIndent();
 
-      this.fields.forEach(function(f) { o.out(f, '\n'); });
+      this.fields.forEach(function(f) { o.out('\n', f, '\n'); });
+      this.methods.forEach(function(f) { o.out('\n', f, '\n'); });
 
       o.decreaseIndent();
       o.out('}');
