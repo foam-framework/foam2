147351a76b8c699f4743cec8fefcf419d25342e7
Mike Carcasole <mikecarcasole@gmail.com>
2017-02-09 18:54:04 -0500

    Add the swift stuff to files.js and the nodejs classloaders. Also update swift gen demo script to flip the swift flag.

--- demos/swift/gen_swift.js
+++ demos/swift/gen_swift.js
@@ -1,14 +1,15 @@
 #!/usr/bin/env node
 
+global.FOAM_FLAGS = {
+  'node': true,
+  'swift': true,
+  'debug': true,
+};
+
 var dir = __dirname;
 var root = dir + '/../..';
 
 require(root + '/src/foam.js');
-require(root + '/src/foam/classloader/OrDAO.js');
-require(root + '/src/foam/classloader/NodeModelFileDAO.js');
-require(root + '/src/foam/classloader/NodeJsModelExecutor.js');
-require(root + '/src/foam/swift/Refines.js');
-require(root + '/src/foam/swift/Field.js');
 
 var executor = foam.classloader.NodeJsModelExecutor.create({
   classpaths: [
