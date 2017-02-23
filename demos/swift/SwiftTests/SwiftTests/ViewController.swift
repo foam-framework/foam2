a31f0c8f689f3c0ebcc5c63d17f76665f107e997
Mike Carcasole <mikecarcasole@gmail.com>
2017-02-23 15:16:05 -0500

    Use the FOAMUILabel in the swift demo.

--- demos/swift/SwiftTests/SwiftTests/ViewController.swift
+++ demos/swift/SwiftTests/SwiftTests/ViewController.swift
@@ -9,17 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  let o = Context.GLOBAL.create(type: Test.self) as! Test
+  let firstNameLabel = FOAMUILabel()
+  func swapFirstAndLast() {
+    o.swapFirstAndLast()
+  }
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
+    firstNameLabel.data$ = o.firstName$
+
+    let b = UIButton()
+    b.backgroundColor = UIColor.green
+    b.setTitle("swapFirstAndLast", for: .normal)
+    b.addTarget(self, action: #selector(swapFirstAndLast), for: .touchUpInside)
+
+    let views: [String:UIView] = [
+      "firstName": firstNameLabel.view,
+      "button": b,
+    ]
+
+    for (_, v) in views {
+      v.translatesAutoresizingMaskIntoConstraints = false
+      view.addSubview(v)
+    }
+
+    view.addConstraints(NSLayoutConstraint.constraints(
+      withVisualFormat: "V:|-[firstName]-[button]-|",
+      options: NSLayoutFormatOptions.init(rawValue: 0),
+      metrics: nil,
+      views: views))
+    view.addConstraints(NSLayoutConstraint.constraints(
+      withVisualFormat: "H:|-[firstName]-|",
+      options: NSLayoutFormatOptions.init(rawValue: 0),
+      metrics: nil,
+      views: views))
+    view.addConstraints(NSLayoutConstraint.constraints(
+      withVisualFormat: "H:|-[button]-|",
+      options: NSLayoutFormatOptions.init(rawValue: 0),
+      metrics: nil,
+      views: views))
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
 }
-
