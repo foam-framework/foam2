f54aceea25170ca17dc321023ddddc2c38c5565c
Kevin Greer <kgr@chromium.org>
2017-03-08 16:59:17 -0500

    Improved MCW Button.

--- src/foam/u2/mcw/Button.js
+++ src/foam/u2/mcw/Button.js
@@ -3,6 +3,23 @@ foam.CLASS({
   name: 'Button',
   extends: 'foam.u2.ActionView',
 
+  properties: [
+    {
+      name: 'ripple_',
+      factory: function() { return new mdc.ripple.MDCRipple(this.el()); }
+    },
+    {
+      class: 'Boolean',
+      name: 'raised',
+      value: true
+    },
+    {
+      class: 'Boolean',
+      name: 'primary',
+      value: false
+    }
+  ],
+
   methods: [
     function initE() {
       this.
@@ -10,8 +27,15 @@ foam.CLASS({
         SUPER();
     },
 
+    function load() {
+      this.SUPER();
+      this.ripple_.foundation_.init();
+    },
+
     function initCls() {
-      this.cssClass('mdc-button mdc-button--raised mdc-button--primary mdc-ripple-surface');
+      this.cssClass('mdc-button');
+      if ( this.raised ) this.cssClass('mdc-button--raised');
+      if ( this.primary ) this.cssClass('mdc-button--primary');
     }
   ]
 });
