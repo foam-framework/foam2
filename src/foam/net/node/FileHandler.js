4ee7832427a0903bd6bdef329abf8988060963ab
Mark Dittmer <mark.s.dittmer@gmail.com>
2017-03-09 16:31:14 -0500

    Basic Node Server support with file, dir tree, and DAO handlers

---
+++ src/foam/net/node/FileHandler.js
@@ -0,0 +1,87 @@
+/**
+ * @license
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+foam.CLASS({
+  package: 'foam.net.node',
+  name: 'FileHandler',
+  extends: 'foam.net.node.Handler',
+
+  properties: [
+    {
+      class: 'String',
+      name: 'urlPath',
+      documentation: 'Path part of URL to map to file.',
+      required: true
+    },
+    {
+      class: 'String',
+      name: 'filePath',
+      documentation: 'File location.',
+      required: true
+    },
+    {
+      name: 'mimeTypes',
+      factory: function() {
+        return {
+          '.js': 'text/javascript',
+          '.css': 'text/css',
+          '.html': 'text/html',
+          __default: 'application/octet-stream'
+        };
+      }
+    },
+    {
+      name: 'fs',
+      factory: function() { return require('fs'); }
+    },
+    {
+      name: 'path',
+      factory: function() { return require('path'); }
+    }
+  ],
+  methods: [
+    function handle(req, res) {
+      if ( req.url !== this.urlPath ) return false;
+
+      // Ensure that file exists.
+      if ( ! this.fs.existsSync(this.filePath) ) {
+        this.send404(req, res);
+        this.warn('File not found: ' + this.filePath);
+        return true;
+      }
+
+      // Ensure that file is not a directory.
+      var stats = this.fs.statSync(this.filePath);
+      if ( stats.isDirectory() ) {
+        this.send404(req, res);
+        this.warn('Attempt to read directory: ' + this.filePath);
+        return true;
+      }
+
+      // Lookup mime type and set header accordingly.
+      var ext = this.path.extname(this.filePath);
+      var mimetype = this.mimeTypes[ext] || this.mimeTypes.__default;
+      res.statusCode = 200;
+      res.setHeader('Content-type', mimetype);
+
+      this.fs.createReadStream(this.filePath).pipe(res);
+      this.info('200 OK ' + this.urlPath + ' => ' + this.filePath);
+
+      return true;
+    }
+  ]
+});
---
+++ src/foam/net/node/Server.js
@@ -0,0 +1,97 @@
+/**
+ * @license
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+foam.CLASS({
+  package: 'foam.net.node',
+  name: 'Server',
+
+  requires: [
+    'foam.dao.ArrayDAO',
+    'foam.node.handlers.RestDAOHandler',
+    'foam.node.handlers.StaticFileHandler'
+  ],
+
+  properties: [
+    {
+      name: 'server',
+      documentation: 'The Node JS HTTP Server object.'
+    },
+    {
+      class: 'FObjectArray',
+      of: 'foam.net.node.Handler',
+      name: 'handlers'
+    },
+    {
+      type: 'Int',
+      name: 'port',
+      value: 8000
+    },
+    {
+      name: 'http',
+      factory: function() { return require('http'); }
+    }
+  ],
+  methods: [
+    function execute() {
+      this.server = this.http.createServer(this.onRequest);
+      this.server.listen(this.port);
+    },
+    function addHandler(handler) {
+      // TODO(adamvy): Not wild about this design, consider a better model for handling
+      // oauth.
+      this.handlers.push(handler);
+    },
+    function exportDAO(dao, prefix) {
+      this.addHandler(this.RestDAOHandler.create({
+        dao: dao,
+        prefix: prefix
+      }));
+
+      this.log('Export DAO to ' + prefix);
+    },
+    function exportFile(urlPath, filePath) {
+      this.handlers.push(this.FileHandler.create({
+        urlPath: urlPath,
+        filePath: filePath
+      }));
+
+      this.log('Export File ' + filePath + ' to ' + urlPath);
+    },
+    function exportDirectory(urlPath, dir) {
+      this.handlers.push(
+        this.StaticFileHandler.create({
+          dir: dir,
+          urlPath: urlPath
+        }));
+
+      this.log('Export directory ' + dir + ' to ' + urlPath);
+    }
+  ],
+
+  listeners: [
+    function onRequest(req, res) {
+      for ( var i = 0 ; i < this.handlers.length ; i++ ) {
+        if ( this.handlers[i].handle(req, res) ) break;
+      }
+      if ( i === this.handlers.length ) {
+        res.statusCode = 404;
+        res.write('File not found: ' + req.url, 'utf8');
+        res.end();
+      }
+    }
+  ]
+});
