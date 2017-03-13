cf4ffd5104f6a16c1001ed200e576d1e9c6e6a56
Adam Van Ymeren <adam@vany.ca>
2017-03-13 01:31:23 -0400

    Start InlineDAOCOntrollerView vs DAOControllerView as alternative DAOCotnroller views, where the inline controller does not have reciprical search.
    Expand foam.u2.view.RecipricalSearch view.  Not usable yet, but its coming.

--- src/files.js
+++ src/files.js
@@ -184,6 +184,7 @@ FOAM_FILES([
   { name: "foam/comics/DAOCreateControllerView", flags: ['web'] },
   { name: "foam/comics/DAOController" },
   { name: "foam/comics/DAOControllerView", flags: ['web'] },
+  { name: "foam/comics/InlineDAOControllerView", flags: ['web'] },
   { name: "foam/comics/DAOUpdateController" },
   { name: "foam/comics/DAOUpdateControllerView", flags: ['web'] },
   { name: "foam/u2/view/RecipricalSearch", flags: ['web'] },
