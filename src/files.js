b1e8feda49d770c25cc4753d82bf962ae9b9360a
Adam Van Ymeren <adam@vany.ca>
2017-04-15 01:06:57 -0400

    Add ImageView

--- src/files.js
+++ src/files.js
@@ -168,6 +168,7 @@ FOAM_FILES([
   { name: "foam/u2/view/ScrollTableView", flags: ['web'] },
   { name: "foam/u2/view/BlobView", flags: ['web'] },
   { name: "foam/u2/view/StringArrayView", flags: ['web'] },
+  { name: "foam/u2/view/ImageView", flags: ['web'] },
   { name: "foam/u2/ClassView", flags: ['web'] },
   { name: "foam/u2/view/ReferenceView", flags: ['web'] },
   { name: "foam/u2/tag/Card", flags: ['web'] },
