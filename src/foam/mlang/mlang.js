c9d5e6baa3ea8b3f3ed24fac9332c358068e3d70
drish <d.erich@hotmail.com>
2017-04-18 19:46:48 -0400

    make code more dry

--- src/foam/mlang/mlang.js
+++ src/foam/mlang/mlang.js
@@ -1613,9 +1613,7 @@ foam.CLASS({
 
   methods: [
     function put(obj) {
-      if ( ! this.hasOwnProperty('value') ) {
-        this.value = this.arg1.f(obj);
-      } else if ( foam.util.compare(this.value, this.arg1.f(obj) ) > 0 ) {
+      if ( ! this.hasOwnProperty('value') || foam.util.compare(this.value, this.arg1.f(obj) ) > 0) {
         this.value = this.arg1.f(obj);
       }
     }
