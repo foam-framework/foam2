301189d786b9d20eb332545589dceaacf082bd52
Adam Van Ymeren <adam@vany.ca>
2017-04-21 11:33:46 -0400

    Cleanup InlineDAOControllerView.  Make it extend DAOControllerView to save some code.

--- src/foam/comics/InlineDAOControllerView.js
+++ src/foam/comics/InlineDAOControllerView.js
@@ -18,42 +18,8 @@
 foam.CLASS({
   package: 'foam.comics',
   name: 'InlineDAOControllerView',
-  extends: 'foam.u2.Element',
-
-  imports: [
-    'stack'
-  ],
-
-  exports: [
-    'editRecord'
-  ],
-
-  requires: [
-    'foam.comics.DAOController'
-  ],
-
-  properties: [
-    'data',
-    'of',
-    {
-      name: 'controller',
-      factory: function() {
-        var controller = this.DAOController.create();
-        this.onDetach(controller.of$.follow(this.of$));
-        this.onDetach(controller.data$.follow(this.data$));
-        return controller;
-      }
-    }
-  ],
-
+  extends: 'foam.comics.DAOControllerView',
   methods: [
-    function editRecord(obj) {
-      this.stack.push({
-        class: 'foam.comics.DAOUpdateControllerView',
-        of: this.of,
-        data: obj.id
-      });
-    },
     function initE() {
       this.startContext({ data: this.controller }).
         add(this.DAOController.FILTERED_DAO,
