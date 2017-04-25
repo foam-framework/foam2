f2106035f02868f73b88e9b615f83df39ccf40af
Kevin Greer <kgrgreer@gmail.com>
2017-04-24 21:40:02 -0400

    Updates to ListCreateController.

--- src/foam/u2/ListCreateController.js
+++ src/foam/u2/ListCreateController.js
@@ -5,61 +5,6 @@ foam.CLASS({
   name: 'ListCreateController',
   extends: 'foam.u2.stack.StackView',
 
-  classes: [
-    {
-      name: 'ListController',
-      extends: 'foam.u2.Element',
-
-      imports: [ 'controller', 'dao', 'createLabel' ],
-
-      methods: [
-        function initE() {
-          this
-            .tag(this.controller.summaryView, {data: this.dao})
-            .tag(this.CREATE, this.createLabel && {label: this.createLabel});
-        }
-      ],
-
-      actions: [
-        function create(X) {
-          X.controller.data.push(X.controller.CreateController.create());
-        }
-      ]
-    },
-
-    {
-      name: 'CreateController',
-      extends: 'foam.u2.Element',
-
-      imports: [ 'controller', 'dao', 'factory' ],
-      exports: [ 'as data' ],
-
-      properties: [
-          {
-            name: 'obj',
-            factory: function() { return this.factory(); }
-          }
-      ],
-
-      methods: [
-        function initE() {
-          this.tag(this.controller.detailView, {data: this.obj}).add(this.CANCEL, this.SAVE);
-        }
-      ],
-
-      actions: [
-        function cancel(X) {
-          this.controller.back();
-        },
-
-        function save(X) {
-          this.dao.put(this.obj);
-          this.controller.back();
-        }
-      ]
-    }
-  ],
-
   requires: [
     'foam.u2.TableView',
     'foam.u2.DetailView'
@@ -104,6 +49,99 @@ foam.CLASS({
       code: function() {
         this.data.push(this.detailView);
       }
+    }
+  ],
+
+  classes: [
+    {
+      name: 'ListController',
+      extends: 'foam.u2.Element',
+
+      imports: [ 'controller', 'dao', 'createLabel' ],
+
+      properties: [
+        {
+          name: 'selection'
+        }
+      ],
+
+      methods: [
+        function initE() {
+          this
+            .tag(this.controller.summaryView, {data: this.dao, selection$: this.selection$})
+            .tag(this.CREATE, this.createLabel && {label: this.createLabel});
+
+          var self = this;
+          this.selection$.sub(function() {
+            self.controller.data.push(self.controller.ViewController.create({obj: self.selection}));
+          });
+        }
+      ],
+
+      actions: [
+        function create(X) {
+          X.controller.data.push(X.controller.CreateController.create());
+        }
+      ]
+    },
+
+    {
+      name: 'CreateController',
+      extends: 'foam.u2.Element',
+
+      imports: [ 'controller', 'dao', 'factory' ],
+      exports: [ 'as data' ],
+
+      properties: [
+          {
+            name: 'obj',
+            factory: function() { return this.factory(); }
+          }
+      ],
+
+      methods: [
+        function initE() {
+          this.tag(this.controller.detailView, {data: this.obj}).add(this.CANCEL, this.SAVE);
+        }
+      ],
+
+      actions: [
+        function cancel(X) {
+          this.controller.back();
+        },
+
+        function save(X) {
+          this.dao.put(this.obj);
+          this.controller.back();
+        }
+      ]
+    },
+
+    {
+      name: 'ViewController',
+      extends: 'foam.u2.Element',
+
+      imports: [ 'controller' ],
+      exports: [ 'as data' ],
+
+      properties: [
+          {
+            name: 'obj',
+            factory: function() { return this.factory(); }
+          }
+      ],
+
+      methods: [
+        function initE() {
+          this.tag(this.controller.detailView, {data: this.obj, controllerMode: foam.u2.ControllerMode.VIEW}).add(this.BACK);
+        }
+      ],
+
+      actions: [
+        function back(X) {
+          this.controller.back();
+        }
+      ]
     }
   ]
 });
