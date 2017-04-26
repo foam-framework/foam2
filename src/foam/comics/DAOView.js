cf2b1779f5d181eec77fc84b430e2819011aeb31
Mike Carcasole <mikecarcasole@gmail.com>
2017-04-26 16:20:30 -0400

    Forgot DAOView

---
+++ src/foam/comics/DAOView.js
@@ -0,0 +1,43 @@
+/**
+ * @license
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ * Copyright 2017 The FOAM Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+foam.CLASS({
+  package: 'foam.comics',
+  name: 'DAOView',
+  extends: 'foam.u2.View',
+
+  requires: [
+    'foam.comics.DAOController',
+    'foam.comics.InlineDAOControllerView',
+  ],
+
+  properties: [
+    {
+      name: 'controller',
+      expression: function(data) {
+        return this.DAOController.create({data: data}, data.__subContext__);
+      },
+    },
+  ],
+
+  methods: [
+    function initE() {
+      this.start(this.InlineDAOControllerView, {data$: this.controller$}).end();
+    }
+  ]
+});
