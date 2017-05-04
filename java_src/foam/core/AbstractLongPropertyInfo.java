fe4fe06c3f7f4420418f3a51ea4572b8909450f5
Kirk Eaton <kirk@nanopay.net>
2017-05-04 09:27:26 -0400

    Modified AbstractPropertyInfo implementations for Byte, Double, Long, Short to provide the full package name for the class when comparing

--- java_src/foam/core/AbstractLongPropertyInfo.java
+++ java_src/foam/core/AbstractLongPropertyInfo.java
@@ -2,6 +2,6 @@ package foam.core;
 
 public abstract class AbstractLongPropertyInfo extends AbstractPropertyInfo {
   public int compareValues(long o1, long o2) {
-    return Long.compare(o1, o2);
+    return java.lang.Long.compare(o1, o2);
   }
 }
--- java_src/foam/core/AbstractShortPropertyInfo.java
+++ java_src/foam/core/AbstractShortPropertyInfo.java
@@ -2,6 +2,6 @@ package foam.core;
 
 public abstract class AbstractShortPropertyInfo extends AbstractPropertyInfo {
   public int compareValues(short o1, short o2) {
-    return Short.compare(o1, o2);
+    return java.lang.Short.compare(o1, o2);
   }
 }
