959508ae0d5a8ee9cbe0dcf66eeb74e0a5c4d067
Kevin Greer <kgrgreer@gmail.com>
2017-05-05 15:05:05 -0400

    Fixed typo.

--- java_src/foam/core/ContextAwareSupport.java
+++ java_src/foam/core/ContextAwareSupport.java
@@ -3,7 +3,7 @@ package foam.core;
 public class ContextAwareSupport
   implements ContextAware
 {
-  private X x_;
+  protected X x_;
 
   public X    getX() { return x_; }
   public void setX(X x) { x_ = x; }
