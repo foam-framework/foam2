bc39c3ffe9a13206d6f114877cf1a6cee4f5c243
William Dias <wrddias@gmail.com>
2017-05-05 12:02:33 -0400

    Create Boot and NSpec models

---
+++ src/foam/nanos/boot/Boot.java
@@ -0,0 +1,41 @@
+package foam.nanos.boot;
+
+import foam.core.*;
+
+public class Boot {
+
+  protected DAO serviceDAO_;
+  protected X   root_ = EmptyContext.instance();
+
+  public Boot() {
+    serviceDAO_ = new ArrayDAO();
+    loadTestData();
+    serviceDAO_.setOf(foam.nanos.boot.NSpec);
+
+    serviceDAO.select(new AbstractSink() {
+      public void put(Detachable sub, FObject obj) {
+        NSpec sp = (FObject) obj;
+        System.out.println("NSpec: " + sp.name);
+
+        NanoService ns = sp.createService();
+        ns.setContext(root_);
+        ns.start();
+        root_ = root_.put(sp.name, ns);
+      }
+    });
+  }
+
+  protected void loadTestData() {
+    NSpec s = new NSpec();
+    s.setName('http');
+    s.setServiceClass("foam.nano.http.HttpServer");
+    serviceDAO_.put(s);
+  }
+
+  public void main (String[] args) throws Exception {
+    new Boot();
+
+    Thread.currentThread.setDaemon(true);
+  }
+
+}
