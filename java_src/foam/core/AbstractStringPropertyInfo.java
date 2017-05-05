1cc49314b0ec534f07e79b24b967c25545241d27
Kirk Eaton <eatonk@mail.uoguelph.ca>
2017-05-04 20:44:36 -0400

    Added abstract getWidth method to AbstractStringPropertyInfo.java

--- java_src/foam/core/AbstractStringPropertyInfo.java
+++ java_src/foam/core/AbstractStringPropertyInfo.java
@@ -4,4 +4,6 @@ public abstract class AbstractStringPropertyInfo extends AbstractPropertyInfo {
   public int compareValues(String o1, String o2) {
     return o1.compareTo(o2);
   }
+
+  public abstract int getWidth();
 }
