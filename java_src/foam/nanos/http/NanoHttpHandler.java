0088b52a676947eca3b78b38494cdc3faea91c07
William Dias <wrddias@gmail.com>
2017-05-09 17:27:45 -0400

    Add missing license headers.

--- java_src/foam/nanos/http/NanoHttpHandler.java
+++ java_src/foam/nanos/http/NanoHttpHandler.java
@@ -1,3 +1,9 @@
+/**
+ * @license
+ * Copyright 2017 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 package foam.nanos.http;
 
 import java.io.IOException;
--- src/foam/nanos/boot/NSpec.js
+++ src/foam/nanos/boot/NSpec.js
@@ -1,3 +1,9 @@
+/**
+ * @license
+ * Copyright 2017 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 foam.CLASS({
   package: 'foam.nanos.boot',
   name: 'NSpec',
