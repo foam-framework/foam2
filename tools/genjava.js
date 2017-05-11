4720e86654f5ba1447e7b19523c31a7e77549e5a
MarcRoopchand <marc.roopchand@gmail.com>
2017-05-11 10:05:23 -0400

    Indentation finally fixed properly

--- tools/genjava.js
+++ tools/genjava.js
@@ -21,8 +21,8 @@ global.FOAM_FLAGS = { 'java': true, 'debug': true };
 require('../src/foam.js');
 
 if ( process.argv.length != 4 ) {
- console.log("USAGE: genjava.js input-path output-path");
- process.exit(1);
+  console.log("USAGE: genjava.js input-path output-path");
+  process.exit(1);
 }
 
 var indir = process.argv[2];
@@ -38,88 +38,88 @@ var outdir = process.argv[3];
 outdir = require('path').resolve(require('path').normalize(outdir));
 
 function ensurePath(p) {
- var i = 1 ;
- var parts = p.split(require('path').sep);
- var path = '/' + parts[0];
+  var i = 1 ;
+  var parts = p.split(require('path').sep);
+  var path = '/' + parts[0];
 
- while ( i < parts.length ) {
-   try {
-     var stat = require('fs').statSync(path);
-     if ( ! stat.isDirectory() ) throw path + 'is not a directory';
-   } catch(e) {
-     require('fs').mkdirSync(path);
-   }
+  while ( i < parts.length ) {
+    try {
+      var stat = require('fs').statSync(path);
+      if ( ! stat.isDirectory() ) throw path + 'is not a directory';
+    } catch(e) {
+      require('fs').mkdirSync(path);
+    }
 
-   path += require('path').sep + parts[i++];
- }
+    path += require('path').sep + parts[i++];
+  }
 }
 
 function loadClass(c) {
- if ( ! foam.lookup(c, true) ) require('../src/' + c.replace(/\./g, '/') + '.js');
- return foam.lookup(c);
+  if ( ! foam.lookup(c, true) ) require('../src/' + c.replace(/\./g, '/') + '.js');
+  return foam.lookup(c);
 }
 
 function generateClass(cls) {
- if ( typeof cls === 'string' )
-   cls = foam.lookup(cls);
+  if ( typeof cls === 'string' )
+    cls = foam.lookup(cls);
 
- var outfile = outdir + require('path').sep +
-   cls.id.replace(/\./g, require('path').sep) + '.java';
+  var outfile = outdir + require('path').sep +
+  cls.id.replace(/\./g, require('path').sep) + '.java';
 
- ensurePath(outfile);
+  ensurePath(outfile);
 
- require('fs').writeFileSync(outfile, cls.buildJavaClass().toJavaSource());
+  require('fs').writeFileSync(outfile, cls.buildJavaClass().toJavaSource());
 }
 
 function generateAbstractClass(cls) {
- cls = foam.lookup(cls);
+  cls = foam.lookup(cls);
 
- var outfile = outdir + require('path').sep +
-   cls.id.replace(/\./g, require('path').sep) + '.java';
+  var outfile = outdir + require('path').sep +
+  cls.id.replace(/\./g, require('path').sep) + '.java';
 
- ensurePath(outfile);
+  ensurePath(outfile);
 
- var javaclass = cls.buildJavaClass();
- javaclass.abstract = true;
+  var javaclass = cls.buildJavaClass();
+  javaclass.abstract = true;
 
- require('fs').writeFileSync(outfile, javaclass.toJavaSource());
+  require('fs').writeFileSync(outfile, javaclass.toJavaSource());
 }
 
 function generateSkeleton(cls) {
- cls = foam.lookup(cls);
+  cls = foam.lookup(cls);
 
- var outfile = outdir + require('path').sep +
-   cls.package.replace(/\./g, require('path').sep) +
-   require('path').sep + cls.name + 'Skeleton.java';
+  var outfile = outdir + require('path').sep +
+    cls.package.replace(/\./g, require('path').sep) +
+    require('path').sep + cls.name + 'Skeleton.java';
 
- ensurePath(outfile);
+  ensurePath(outfile);
 
- require('fs').writeFileSync(
-   outfile,
-   foam.java.Skeleton.create({ of: cls }).buildJavaClass().toJavaSource());
+  require('fs').writeFileSync(
+    outfile,
+    foam.java.Skeleton.create({ of: cls }).buildJavaClass().toJavaSource());
 }
 
 function generateProxy(intf) {
- intf = foam.lookup(intf);
+  intf = foam.lookup(intf);
 
- var existing = foam.lookup(intf.package + '.Proxy' + intf.name, true);
+  var existing = foam.lookup(intf.package + '.Proxy' + intf.name, true);
 
- if ( existing ) {
-   generateClass(existing.id);
-   return;
- }
+  if ( existing ) {
+    generateClass(existing.id);
+    return;
+  }
 
- var proxy = foam.core.Model.create({
-   package: intf.package,
-   name: 'Proxy' + intf.name,
-   implements: [intf.id],
-   properties: [
-     {
-       class: 'Proxy',
-       of: intf.id,
-       name: 'delegate'
-     }
-   ]
+  var proxy = foam.core.Model.create({
+    package: intf.package,
+    name: 'Proxy' + intf.name,
+    implements: [intf.id],
+    properties: [
+      {
+        class: 'Proxy',
+        of: intf.id,
+        name: 'delegate'
+      }
+    ]
  });
 
  generateClass(proxy.buildClass());
