4b1b348e58ac64e06de76f0477a7204a78555c95
MarcRoopchand <marc.roopchand@gmail.com>
2017-05-15 09:49:38 -0400

    Added classes file for nanos server

---
+++ tools/nanosClasses.js
@@ -0,0 +1,24 @@
+var classes = [
+	'foam.nanos.boot.NSpec',
+	'foam.nanos.auth.AuthService',
+	'foam.nanos.auth.User'
+];
+
+var abstractClasses = [
+
+];
+
+var skeletons = [
+
+];
+
+var proxies = [
+
+];
+
+module.exports = {
+    classes: classes,
+    abstractClasses: abstractClasses,
+    skeletons: skeletons,
+    proxies: proxies
+}
