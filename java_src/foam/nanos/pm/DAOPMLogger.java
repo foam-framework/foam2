1e88416657f42275eb330fc3fb62a60f13daa872
Nick Prat <nick@nanopay.net>
2017-05-17 15:25:02 -0400

    Applied patches to make mapdao work

--- java_src/foam/nanos/pm/DAOPMLogger.java
+++ java_src/foam/nanos/pm/DAOPMLogger.java
@@ -1,31 +1,47 @@
 package foam.nanos.pm;
 
 import foam.core.ContextAwareSupport;
-import foam.core.FObject;
-import foam.dao.PMInfoDAO;
-
-import java.util.Map;
+import foam.dao.MapDAO;
+import foam.nanos.NanoService;
 
 /**
  * Created by nick on 17/05/17.
  */
-public class DAOPMLogger extends ContextAwareSupport implements PerformanceLogger {
+public class DAOPMLogger extends ContextAwareSupport implements PerformanceLogger, NanoService {
 
   @Override
-  public void log(PerformanceMonitor pm) {
-    PMInfo pmi = new PMInfo();
-    pmi.setClsname(pm.getClassType().getName())
+  public void log(PM pm) {
+    MapDAO dao = (MapDAO)getX().get("pminfodao");
+    //System.out.println(dao);
+    PMInfo pmi = new PMInfo()
+            .setClsname(pm.getClassType().getName())
             .setPmname(pm.getName())
             .setMintime(pm.getTime())
             .setMaxtime(pm.getTime())
             .setTotaltime(pm.getTime())
             .setNumoccurrences(1);
-    ((PMInfoDAO)(getX().get("pminfodao"))).put(pmi);
+    if(pmi == null) System.out.println("null pmi");
+    if(dao.find(pmi) == null) {
+      System.out.println("Inserting new");
+      dao.put(pmi);
+      if(pmi == null) System.out.println("now it's nul");
+    } else {
+      PMInfo dpmi = (PMInfo) dao.find(pmi);
+      System.out.println("Updating");
+      if(pm.getTime() < dpmi.getMintime())
+        dpmi.setMintime(pm.getTime());
+      if(pm.getTime() > dpmi.getMaxtime())
+        dpmi.setMaxtime(pm.getTime());
+      dpmi.setNumoccurrences(dpmi.getNumoccurrences() + 1);
+      dpmi.setTotaltime(dpmi.getTotaltime() + pm.getTime());
+    }
   }
 
   @Override
   public void start() {
-    PMInfoDAO dao = new PMInfoDAO();
+    System.out.println("pdl");
+    MapDAO dao = new MapDAO();
+    dao.setOf(PMInfo.getOwnClassInfo());
     dao.setX(getX());
     getX().put("pminfodao", dao);
   }
