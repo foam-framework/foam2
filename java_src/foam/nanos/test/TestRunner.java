629ac28874dbc0a580dc0111bb1902246a7e5856
Waseem Chamaa <waseemchamaa@C02RH4STFVH5.local>
2017-05-17 16:36:03 -0400

    Java TestRunner made

--- java_src/foam/nanos/test/TestRunner.java
+++ java_src/foam/nanos/test/TestRunner.java
@@ -9,41 +9,66 @@ import foam.core.*;
 import foam.dao.*;
 import foam.nanos.*;
 import bsh.*;
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
 public class TestRunner extends ContextAwareSupport implements NanoService {
 
   public void start() {
     final MapDAO tests = (MapDAO) getX().get("TestDAO");
-    final Interpreter shell = new Interpreter();
+    
     tests.select(new AbstractSink() {
       public void put(FObject o, Detachable sub) {
-        Test test = (Test) o;
+        Test                  test = (Test) o;
+        ByteArrayOutputStream bas  = new ByteArrayOutputStream();
+        PrintStream           ps   = new PrintStream(bas);
+        // Creates a new Interpreter at each call
+        final Interpreter    shell = new Interpreter();
 
-        // phase 2
-        // define assert()
-       try{
-        shell.eval(test.getCode());
-       } catch (EvalError e) {
-           e.printStackTrace();
-       }
-       
+        try {  
+          shell.set("currentTest", test);
+          test.setPassed(0);
+          test.setFailed(0);
+          test.setOutput("");
+
+          // Sets the shell output to a Stream that will later be redirected to the output parameter  
+          shell.setOut(ps);
+
+          // creates the testing method 
+          shell.eval("test(boolean exp, String message) { if ( exp ) { currentTest.setPassed(currentTest.getPassed()+1); } else currentTest.setFailed(currentTest.getFailed()+1); print((exp ? \"SUCCESS: \" : \"FAILURE:\")+message);}");
+        
+          shell.eval(test.getCode());
+        } catch (EvalError e) {
+          e.printStackTrace();
+        }
+  
+        test.setLastRun(new Date());
+
+        // sets stream to output property
+        ps.flush();
+        test.setOutput(bas.toString());
+
         // increment lastRun, success, failures
-       // tests.put(test); 
+        tests.put(test);
       }
     });
   }
 
- public static void main(String[] args){
+  public static void main(String[] args){
     MapDAO tests = new MapDAO();
+    X      x     = EmptyX.instance().put("TestDAO", tests);
+
+    tests.setX(x);
     tests.setOf(Test.getOwnClassInfo());
+
     Test test1 = new Test();
     test1.setId("Test 1");
-    test1.setCode("System.out.println(\"I'm working\");");
-
+    test1.setCode("test(2==2 ,\"WORKS\");test(1==2 ,\"BROKEN\");print(\"All Done.\");");
     tests.put(test1);
-    X x = EmptyX.instance().put("TestDAO", tests);
+
     TestRunner runner = new TestRunner();
     runner.setX(x);
     runner.start();
   }
-
 }
