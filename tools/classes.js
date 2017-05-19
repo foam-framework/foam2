8eb075bcbd436a62b38b730a295ec2b76ee5665a
MarcRoopchand <marc.roopchand@gmail.com>
2017-05-19 10:59:26 -0400

    Fixed spacing on classes file

--- tools/classes.js
+++ tools/classes.js
@@ -53,7 +53,7 @@ var classes = [
   'foam.nanos.auth.LastModifiedByAware',
   'foam.nanos.auth.Permission',
   'foam.nanos.auth.User',
-	'foam.nanos.auth.AuthService',
+  'foam.nanos.auth.AuthService',
   'foam.nanos.pm.PMInfo',
   'foam.nanos.script.Script',
   'foam.nanos.test.Test'
