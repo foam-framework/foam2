43f4e09787d82b55c47eaa3feed2a26682d73c54
Nick Prat <nick@nanopay.net>
2017-05-23 10:58:31 -0400

    Added PMDAO decorator

--- java_src/foam/dao/PMDAO.java
+++ java_src/foam/dao/PMDAO.java
@@ -2,6 +2,7 @@ package foam.dao;
 
 import foam.core.FObject;
 import foam.nanos.pm.PM;
+import foam.nanos.pm.PMInfo;
 
 /**
  * Created by nick on 19/05/17.
@@ -10,11 +11,12 @@ public class PMDAO extends ProxyDAO {
 
   @Override
   public FObject put(FObject obj) {
-    PM pm = new PM(getOf().getClass(), "test");
-
-    getDelegate().put(obj);
-
-    pm.end(getX());
+    if(obj instanceof PMInfo) {
+      PMInfo pmi = (PMInfo)obj;
+      PM pm = new PM(PMDAO.class, pmi.getClsname() + ":" + pmi.getPmname());
+      getDelegate().put(pmi);
+      pm.end(getX());
+    }
     return obj;
   }
 
