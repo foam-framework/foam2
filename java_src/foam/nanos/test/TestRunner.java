9f9daac980875cff8897e093f0ad9b4edff953b8
Waseem Chamaa <waseemchamaa@C02RH4STFVH5.local>
2017-05-25 13:34:40 -0400

    seperated function

--- java_src/foam/nanos/test/TestRunner.java
+++ java_src/foam/nanos/test/TestRunner.java
@@ -9,12 +9,14 @@ package foam.nanos.test;
 import bsh.EvalError;
 import bsh.Interpreter;
 import foam.core.*;
+import foam.core.PropertyInfo;
 import foam.dao.AbstractSink;
 import foam.dao.MapDAO;
 import foam.nanos.NanoService;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.Date;
+import foam.mlang.MLang.*;
 
 public class TestRunner
   extends    ContextAwareSupport
@@ -23,39 +25,58 @@ public class TestRunner
 
   public void start() {
     final MapDAO tests = (MapDAO) getX().get("TestDAO");
+    /* TODO   
+     * If the test's schedule flag has changed to true then run that test
+     
+    tests.listen(
+      function listener (newValue, oldValue){
+        if(newValue !== oldValue){
+      runTest(newValue);
+    );
+    
+    */
+    runAllTests();
+  }
 
-    tests.select(new AbstractSink() {
+  public void runTest(Test test) {
+    final MapDAO tests = (MapDAO) getX().get("TestDAO");
+    ByteArrayOutputStream bas   = new ByteArrayOutputStream();
+    PrintStream           ps    = new PrintStream(bas);
+    final Interpreter     shell = new Interpreter(); // Creates a new Interpreter at each call
+
+    try {
+      shell.set("currentTest", test);
+      test.setPassed(0);
+      test.setFailed(0);
+      test.setOutput("");
+
+      // Sets the shell output to a Stream that will later be redirected to the output parameter
+      shell.setOut(ps);
+
+      // creates the testing method
+      shell.eval("test(boolean exp, String message) { if ( exp ) { currentTest.setPassed(currentTest.getPassed()+1); } else currentTest.setFailed(currentTest.getFailed()+1); print((exp ? \"SUCCESS: \" : \"FAILURE: \")+message);}");
+
+      shell.eval(test.getCode());
+    } catch (EvalError e) {
+      e.printStackTrace();
+    }
+
+    test.setLastRun(new Date());
+
+    // sets stream to output property
+    ps.flush();
+    test.setOutput(bas.toString());
+    // increment lastRun, success, failures
+    tests.put(test);
+
+  }
+
+  public void runAllTests() {
+    final MapDAO tests = (MapDAO) getX().get("TestDAO");
+    tests.where(EQ(Test.SCHEDULED, true)).select(new AbstractSink() {
       public void put(FObject o, Detachable sub) {
-        Test                  test  = (Test) o;
-        ByteArrayOutputStream bas   = new ByteArrayOutputStream();
-        PrintStream           ps    = new PrintStream(bas);
-        final Interpreter     shell = new Interpreter(); // Creates a new Interpreter at each call
-
-        try {
-          shell.set("currentTest", test);
-          test.setPassed(0);
-          test.setFailed(0);
-          test.setOutput("");
-
-          // Sets the shell output to a Stream that will later be redirected to the output parameter
-          shell.setOut(ps);
-
-          // creates the testing method
-          shell.eval("test(boolean exp, String message) { if ( exp ) { currentTest.setPassed(currentTest.getPassed()+1); } else currentTest.setFailed(currentTest.getFailed()+1); print((exp ? \"SUCCESS: \" : \"FAILURE:\")+message);}");
-
-          shell.eval(test.getCode());
-        } catch (EvalError e) {
-          e.printStackTrace();
-        }
-
-        test.setLastRun(new Date());
-
-        // sets stream to output property
-        ps.flush();
-        test.setOutput(bas.toString());
-
-        // increment lastRun, success, failures
-        tests.put(test);
+        Test     test  = (Test) o;
+        runTest(test);
       }
     });
   }
