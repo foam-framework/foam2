fb7e3e14422c2c10a7917da37a9d8857a063c680
Adam Van Ymeren <adam@vany.ca>
2017-05-25 15:35:29 -0400

    Fix duplicate parsing in firebase and jsondao.

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -98,7 +98,7 @@ foam.CLASS({
         payload = JSON.parse(payload);
 
         //        if ( obj.id ) {
-          var o2 = foam.json.parse(foam.json.parseString(payload.data));
+        var o2 = foam.json.parseString(payload.data, this);
           if ( this.timestampProperty ) {
             this.timestampProperty.set(o2, payload.lastUpdate);
           }
@@ -145,8 +145,7 @@ foam.CLASS({
         try {
           data = JSON.parse(data);
 
-          var obj = foam.json.parse(
-            foam.json.parseString(data.data));
+          var obj = foam.json.parseString(data.data, this);
 
           if ( this.timestampProperty ) {
             this.timestampProperty.set(obj, data.lastUpdate);
@@ -245,8 +244,7 @@ foam.CLASS({
         for ( var key in data ) {
           if ( detached ) break;
 
-          var obj = foam.json.parse(
-            foam.json.parseString(data[key].data));
+          var obj = foam.json.parseString(data[key].data, this);
           if ( this.timestampProperty ) {
             this.timestampProperty.set(obj, data[key].lastUpdate);
           }
@@ -279,7 +277,7 @@ foam.CLASS({
         }
 
         for ( var key in data.data ) {
-          var obj = foam.json.parse(foam.json.parseString(data.data[key].data));
+          var obj = foam.json.parseString(data.data[key].data, this);
           if ( this.timestampProperty ) {
             this.timestampProperty.set(obj, data.data[key].lastUpdate);
           }
@@ -293,7 +291,7 @@ foam.CLASS({
           this.on.remove.pub(obj);
           return;
         }
-        var obj = foam.json.parse(foam.json.parseString(data.data.data));
+        var obj = foam.json.parseString(data.data.data, this);
         if ( this.timestampProperty ) {
           this.timestampProperty.set(obj, data.data.lastUpdate);
         }
@@ -311,7 +309,7 @@ foam.CLASS({
           this.on.put.pub(obj);
         }.bind(this));
 
-        // var obj = foam.json.parse(foam.json.parseString(data.data));
+        // var obj = foam.json.parseString(data.data, this);
         // this.on.put.pub(obj);
       } else if ( path.indexOf('/lastUpdate') === path.length - 11 ) {
         // Timestamp of an existing row updated, do anything?
