79be76f2425f1ca46bfe877014fa449e84c09037
drish <d.erich@hotmail.com>
2017-05-26 11:35:47 -0400

    java_src: make FileJournal not to fail if X is null

--- java_src/foam/lib/json/JournalParser.java
+++ java_src/foam/lib/json/JournalParser.java
@@ -1,14 +1,18 @@
 package foam.lib.json;
 
+import foam.core.ContextAwareSupport;
 import foam.core.FObject;
+import foam.core.ProxyX;
+import foam.core.X;
 
 /**
  * Class responsible for parsing journal files.
  * journal files follows this structure.
+ *
  * put(foam.json.parse({"class":"foam.nanos.boot.NSpec","name":"Gibs1" }))
  * remove(foam.json.parse({"class":"foam.nanos.boot.NSpec","name":"Gibs" }))
  */
-public class JournalParser {
+public class JournalParser extends ContextAwareSupport {
 
     public FObject parseObject(String journalLine) {
 
@@ -16,9 +20,12 @@ public class JournalParser {
         int objectIndex = journalLine.indexOf("{");
         String object = journalLine.substring(objectIndex, journalLine.lastIndexOf("}") + 1);
 
+        // do not fail if context is null
+        X ctx = (getX() == null) ? new ProxyX() : getX();
         JSONParser jsonParser = new JSONParser();
-        FObject obj = jsonParser.parseString(object);
-        return obj;
+        jsonParser.setX(ctx);
+
+        return jsonParser.parseString(object);
     }
 
     /**
