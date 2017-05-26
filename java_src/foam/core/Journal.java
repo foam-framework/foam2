c7eeae5e74805dd7536748b37f317e84ad613374
drish <d.erich@hotmail.com>
2017-05-26 11:35:47 -0400

    java_src: make FileJournal not to fail if X is null

--- java_src/foam/core/Journal.java
+++ java_src/foam/core/Journal.java
@@ -5,6 +5,7 @@ import foam.dao.Sink;
 
 import java.io.IOException;
 
+// TODO(drish): use Index interface.
 public interface Journal extends Sink {
     public void replay(DAO dao) throws IOException;
 }
