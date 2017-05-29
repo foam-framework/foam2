5006efa0c17fa15e43fa0a5ce5952d54956657b8
Nick Prat <nick@nanopay.net>
2017-05-29 11:29:45 -0400

    Implemented PMDAO

--- java_src/foam/dao/PMDAO.java
+++ java_src/foam/dao/PMDAO.java
@@ -1,6 +1,8 @@
 package foam.dao;
 
 import foam.core.FObject;
+import foam.mlang.order.Comparator;
+import foam.mlang.predicate.Predicate;
 import foam.nanos.pm.PM;
 
 /**
@@ -10,14 +12,60 @@ public class PMDAO extends ProxyDAO {
 
   @Override
   public FObject put(FObject obj) {
-    PM pm = new PM(PMDAO.class, obj.getClassInfo().getId());
+    PM pm = new PM(PMDAO.class, obj.getClassInfo().getId() + ":put");
     try {
-      getDelegate().put(obj);
+      super.put(obj);
     } catch(Exception e) {
       e.printStackTrace();
     } finally {
       pm.log(getX());
     }
     return obj;
+  }
+
+  @Override
+  public FObject find(Object id) {
+    PM pm;
+    if (id instanceof FObject) {
+      FObject obj = (FObject)id;
+      pm = new PM(PMDAO.class, obj.getClassInfo().getId() + ":find");
+    } else {
+      pm = new PM(PMDAO.class, id.getClass().getName() + ":find");
+    }
+    FObject fobj = null;
+    try {
+      fobj = super.find(id);
+    } catch(Exception e) {
+      e.printStackTrace();
+    } finally {
+      pm.log(getX());
+    }
+    return fobj;
+  }
+
+  @Override
+  public FObject remove(FObject obj) {
+    PM pm = new PM(PMDAO.class, obj.getClassInfo().getId() + ":remove");
+    try {
+      super.remove(obj);
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      pm.log(getX());
+    }
+    return obj;
+  }
+
+  @Override
+  public void removeAll(Integer skip, Integer limit, Comparator order, Predicate predicate) {
+    // Is this the right key to use?
+    PM pm = new PM(PMDAO.class, getOwnClassInfo().getId() + ":removeAll");
+    try {
+      super.removeAll(skip, limit, order, predicate);
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      pm.log(getX());
+    }
   }
 }
