b590309ed37d38d7b37ce72371002604ded0ae29
MarcRoopchand <marc.roopchand@gmail.com>
2017-06-01 13:13:26 -0400

    Fixed code style to comform to FOAM2 Style Guides

--- java_src/foam/nanos/auth/CachedUserAndGroupAuthService.java
+++ java_src/foam/nanos/auth/CachedUserAndGroupAuthService.java
@@ -1,7 +1,8 @@
 package foam.nanos.auth;
 
 import foam.core.X;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by marcroopchand on 2017-05-30.
@@ -12,7 +13,7 @@ public class CachedUserAndGroupAuthService extends UserAndGroupAuthService {
    *{
    *  permission1: {
    *    user1: true,
-   *    user2: falsex
+   *    user2: false
    *    ...
    *  },
    *  permission2: {
@@ -25,78 +26,61 @@ public class CachedUserAndGroupAuthService extends UserAndGroupAuthService {
    */
 
   //TODO: Limit the size of this map to 10000
-  protected ConcurrentHashMap<String, ConcurrentHashMap<String, Boolean>> permissionMap;
+  protected Map<String, Map<String, Boolean>> permissionMap;
 
   @Override
   public void start() {
     super.start();
-    permissionMap = new ConcurrentHashMap<>();
+    permissionMap = new HashMap<>();
   }
 
   @Override
   public Boolean check(foam.core.X x, java.security.Permission permission) {
-    if (x == null || permission == null) {
-      return false;
-    }
+    if ( x == null || permission == null ) return false;
 
     User user = (User) x.get("user");
-    if (user == null) {
-      return false;
-    }
+    if ( user == null ) return false;
 
     Group group = (Group) user.getGroup();
-    if (group == null) {
-      return false;
-    }
+    if ( group == null ) return false;
 
     /**
      * Check if data is already cached, if it is return this data
      * If not, save it to the maps
      * */
-    if (permissionMap.containsKey(permission.getName())) {
-      ConcurrentHashMap<String, Boolean> userMap = permissionMap.get(permission.getName());
-
-      if (userMap.containsKey(user.getId())) {
-        return userMap.get(user.getId());
-      }
+    if ( permissionMap.containsKey(permission.getName()) ) {
+      Map<String, Boolean> userMap = permissionMap.get(permission.getName());
+      if ( userMap.containsKey(user.getId()) ) return userMap.get(user.getId());
 
       Boolean permissionCheck = group.implies(permission.getName());
       userMap.put(user.getId(), permissionCheck);
       permissionMap.put(permission.getName(), userMap);
       return permissionCheck;
     }
-    else {
-      ConcurrentHashMap<String, Boolean> userMap = new ConcurrentHashMap<>();
-      Boolean permissionCheck = group.implies(permission.getName());
-      userMap.put(user.getId(), permissionCheck);
 
-      permissionMap.put(permission.getName(), userMap);
+    Map<String, Boolean> userMap = new HashMap<>();
+    boolean permissionCheck = group.implies(permission.getName());
+    userMap.put(user.getId(), permissionCheck);
 
-      return permissionCheck;
-    }
+    permissionMap.put(permission.getName(), userMap);
+
+    return permissionCheck;
   }
 
   /**
    * On logout, walk through entire map and remove all instances of the user
    * */
   @Override
-  public X logout(X x) {
-    if (x == null) {
-      return null;
-    }
+  public void logout(X x) {
+    if ( x == null ) return;
 
     User user = (User) x.get("user");
-    if (user == null) {
-      return null;
+    if ( user == null ) return;
+
+    for ( String key: permissionMap.keySet() ) {
+      permissionMap.get(key).remove(user.getId());
     }
 
-    for (String key: permissionMap.keySet()) {
-      ConcurrentHashMap<String, Boolean> userMap = permissionMap.get(key);
-      if (userMap.containsKey(user.getId())) {
-        userMap.remove(user.getId());
-      }
-    }
-
-    return this.getX().put("user", null);
+    this.getX().put("user", null);
   }
 }
