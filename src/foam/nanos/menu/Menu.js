7d25a626f2c8cdb94999d3ea52f78f24a8f70046
Kevin Greer <kgrgreer@gmail.com>
2017-06-05 15:17:29 -0400

    Added ListMenu handler.

--- src/foam/nanos/menu/Menu.js
+++ src/foam/nanos/menu/Menu.js
@@ -58,6 +58,36 @@ foam.CLASS({
 
 foam.CLASS({
   package: 'foam.nanos.menu',
+  name: 'ListMenu',
+  extends: 'foam.nanos.menu.AbstractMenu',
+
+  requires: [ 'foam.u2.Element' ],
+
+  methods: [
+    function createView(X, menu) {
+      var e = this.Element.create(undefined, X);
+
+      X.stack.push(e);
+
+      menu.children.select({
+        put: function(menu) {
+          if ( menu.handler ) {
+            e.tag(menu.handler.createView(X, menu));
+          } else {
+            e.add('Coming Soon!');
+          }
+        },
+        eof: function() {}
+      });
+
+      return e;
+    }
+  ]
+});
+
+
+foam.CLASS({
+  package: 'foam.nanos.menu',
   name: 'TabsMenu',
   extends: 'foam.nanos.menu.AbstractMenu',
 
