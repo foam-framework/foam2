2228dfd31afb50f344c92378a94132de9d2c6225
Nick Prat <nick@nanopay.net>
2017-06-06 14:36:15 -0400

    Implemented PMDAO service for access to pm infos

--- java_src/foam/nanos/boot/Boot.java
+++ java_src/foam/nanos/boot/Boot.java
@@ -8,14 +8,17 @@ package foam.nanos.boot;
 
 import foam.core.*;
 import foam.dao.*;
+import foam.nanos.PMTest;
 import foam.nanos.auth.*;
 import foam.nanos.pm.*;
 import foam.mlang.*;
+import foam.nanos.pm.PMDAO;
 
 public class Boot {
   protected DAO serviceDAO_;
   protected DAO userDAO_;
   protected DAO groupDAO_;
+  protected MapDAO pmDAO_;
   protected X   root_ = new ProxyX();
 
   public Boot() {
@@ -36,7 +39,13 @@ public class Boot {
     ((MapDAO) groupDAO_).setX(root_);
     root_.put("groupDAO", groupDAO_);
 
-    // loadServices();
+//    pmDAO_ = new MapDAO();
+//    pmDAO_.setOf(PMInfo.getOwnClassInfo());
+//    pmDAO_.setX(root_);
+//    root_.put(DAOPMLogger.DAOName, pmDAO_);
+
+    loadServices();
+    loadTestData();
 
     ((AbstractDAO) serviceDAO_).select(new AbstractSink() {
       public void put(FObject obj, Detachable sub) {
@@ -59,6 +68,18 @@ public class Boot {
     });
   }
 
+  protected void loadServices() {
+    NSpec dpl = new NSpec();
+    dpl.setName(DAOPMLogger.ServiceName);
+    dpl.setServiceClass(DAOPMLogger.class.getName());
+    serviceDAO_.put(dpl);
+
+    NSpec pmd = new NSpec();
+    pmd.setName(PMDAO.ServiceName);
+    pmd.setServiceClass(PMDAO.class.getName());
+    serviceDAO_.put(pmd);
+  }
+
   protected void loadTestData() {
     NSpec s = new NSpec();
     s.setName("http");
@@ -66,16 +87,17 @@ public class Boot {
     s.setLazy(false);
     serviceDAO_.put(s);
 
-    NSpec dpl = new NSpec();
-    dpl.setName(DAOPMLogger.ServiceName);
-    dpl.setServiceClass(DAOPMLogger.class.getName());
-    serviceDAO_.put(dpl);
+    NSpec pmt = new NSpec();
+    pmt.setName("tester");
+    pmt.setServiceClass(PMTest.class.getName());
+    pmt.setLazy(false);
+    serviceDAO_.put(pmt);
 
-    NSpec authTest = new NSpec();
-    authTest.setName("authTest");
-    authTest.setServiceClass("foam.nanos.auth.UserAndGroupAuthServiceTest");
-    authTest.setLazy(false);
-    serviceDAO_.put(authTest);
+//    NSpec authTest = new NSpec();
+//    authTest.setName("authTest");
+//    authTest.setServiceClass("foam.nanos.auth.UserAndGroupAuthServiceTest");
+//    authTest.setLazy(false);
+//    serviceDAO_.put(authTest);
   }
 
   public static void main (String[] args) throws Exception {
