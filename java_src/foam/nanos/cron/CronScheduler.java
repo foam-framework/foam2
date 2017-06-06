d1568dbe07d87a60d6057e6be9fc1067ecebaac1
Kirk Eaton <kirk@nanopay.net>
2017-06-06 16:33:51 -0400

    Removed day of month property

--- java_src/foam/nanos/cron/CronScheduler.java
+++ java_src/foam/nanos/cron/CronScheduler.java
@@ -6,10 +6,18 @@
 
 package foam.nanos.cron;
 
+import bsh.EvalError;
+import bsh.Interpreter;
 import foam.core.ContextAwareSupport;
-import foam.dao.MapDAO;
+import foam.core.Detachable;
+import foam.core.FObject;
+import foam.dao.*;
+import foam.mlang.MLang;
+import foam.mlang.sink.Min;
 import foam.nanos.NanoService;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Date;
 
 public class CronScheduler
@@ -17,8 +25,68 @@ public class CronScheduler
     implements NanoService
 {
   protected MapDAO cronDAO_;
-  
+
+  /**
+   * Gets the minimum scheduled cron job
+   *
+   * @return Date of the minimum scheduled cron job
+   */
+  private Date getMinScheduledTime() {
+    Min min = (Min) MLang.MIN(Cron.SCHEDULED_TIME);
+    cronDAO_.select(min);
+    return ((Cron) min.getValue()).getScheduledTime();
+  }
+
+  /**
+   * Runs a Cron job script
+   *
+   * @param cron the cron job to run
+   * @return the updated cron job
+   */
+  private void runCronJob(Cron cron) {
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    PrintStream ps = new PrintStream(baos);
+    Interpreter shell = new Interpreter();
+
+    try {
+      shell.set("currentCron", cron);
+      cron.setOutput("");
+      shell.setOut(ps);
+      shell.eval(cron.getCode());
+    } catch (EvalError e) {
+      e.printStackTrace();
+    }
+
+    cron.setLastRun(new Date());
+    ps.flush();
+    cron.setOutput(baos.toString());
+  }
+
+  private final Thread cronJobs = new Thread() {
+    @Override
+    public void run() {
+      try {
+        while (true) {
+          Date dtnow = new Date();
+          ((AbstractDAO) cronDAO_.where(MLang.LTE(Cron.SCHEDULED_TIME, dtnow))).select(new AbstractSink() {
+            @Override
+            public void put(FObject obj, Detachable sub) {
+              runCronJob((Cron) obj);
+              cronDAO_.put(obj);
+            }
+          });
+
+          Date minScheduledTime = getMinScheduledTime();
+          Thread.sleep(minScheduledTime.getTime() - dtnow.getTime());
+        }
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+  };
+
   public void start() {
     cronDAO_ = (MapDAO) getX().get("MapDAO");
+    cronJobs.start();
   }
 }
--- src/foam/nanos/cron/Cron.js
+++ src/foam/nanos/cron/Cron.js
@@ -30,13 +30,6 @@ foam.CLASS({
     },
     {
       class: 'Int',
-      name: 'dayOfMonth',
-      value: -1,
-      documentation: `Day of month to execute script.
-          Ranges from 1 - 31. -1 for wildcard`
-    },
-    {
-      class: 'Int',
       name: 'month',
       value: -1,
       documentation: `Month to execute script.
@@ -55,14 +48,40 @@ foam.CLASS({
       documentation: `Scheduled time to run Cron script.`,
       javaFactory: function() {
 /*Calendar now = Calendar.getInstance();
+now.setLenient(false);
 now.set(Calendar.MILLISECOND, 0);
 now.set(Calendar.SECOND, 0);
 if ( getMinute() >= 0 && getMinute() <= 59 ) now.set(Calendar.MINUTE, getMinute());
 if ( getHour() >= 0 && getHour() <= 23 ) now.set(Calendar.HOUR, getHour());
-if ( getDayOfMonth() >= 1 && getDayOfMonth() <= 31 ) now.set(Calendar.DAY_OF_MONTH, getDayOfMonth());
 if ( getMonth() >= 1 && getMonth() <= 12 ) now.set(Calendar.MONTH, getMonth() - 1);
 if ( getDayOfWeek() >= 0 && getDayOfWeek() <= 6 ) now.set(Calendar.DAY_OF_WEEK, getDayOfWeek() + 1);
 return now.getTime();*/
+      }
+    }
+  ],
+
+  methods: [
+    {
+      name: 'getNextScheduledTime',
+      javaReturns: 'Date',
+      javaCode: function() {
+/*Calendar now = Calendar.getInstance();
+Calendar scheduled = Calendar.getInstance();
+
+scheduled.setLenient(false);
+scheduled.setTime(getScheduledTime());
+
+if ( getMinute() < 0)
+  scheduled.set(Calendar.MINUTE, (getMinute() + 1) % 60);
+if ( getHour() < 0)
+  scheduled.set(Calendar.HOUR, (getHour() + 1) % 24);
+if ( getMonth() < 0)
+  scheduled.set(Calendar.MONTH, getMonth() % 12);
+if (getDayOfWeek() < 0)
+  scheduled.set(Calendar.DAY_OF_WEEK, ((getDayOfWeek() + 1) % 7) + 1);
+if (now.compareTo(scheduled) > 0)
+  scheduled.set(Calendar.YEAR, scheduled.get(Calendar.YEAR) + 1); // update year
+return scheduled.getTime();*/
       }
     }
   ]
