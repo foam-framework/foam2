2fba19eb51324529d036c5e68e087293c067496d
Kirk Eaton <kirk@nanopay.net>
2017-06-06 14:34:44 -0400

    Added cron properties

--- src/foam/nanos/cron/Cron.js
+++ src/foam/nanos/cron/Cron.js
@@ -48,33 +48,21 @@ foam.CLASS({
       value: -1,
       documentation: `Day of week to execute script.
           Ranges from 0 - 6, where 0 is Sunday. -1 for wildcard`
-    },
+
     {
       class: 'DateTime',
       name: 'scheduledTime',
       documentation: `Scheduled time to run Cron script.`,
       javaFactory: function() {
 /*Calendar now = Calendar.getInstance();
+now.set(Calendar.MILLISECOND, 0);
+now.set(Calendar.SECOND, 0);
 if ( getMinute() >= 0 && getMinute() <= 59 ) now.set(Calendar.MINUTE, getMinute());
 if ( getHour() >= 0 && getHour() <= 23 ) now.set(Calendar.HOUR, getHour());
 if ( getDayOfMonth() >= 1 && getDayOfMonth() <= 31 ) now.set(Calendar.DAY_OF_MONTH, getDayOfMonth());
 if ( getMonth() >= 1 && getMonth() <= 12 ) now.set(Calendar.MONTH, getMonth() - 1);
 if ( getDayOfWeek() >= 0 && getDayOfWeek() <= 6 ) now.set(Calendar.DAY_OF_WEEK, getDayOfWeek() + 1);
 return now.getTime();*/
-      }
-    }
-  ],
-
-  methods: [
-    {
-      name: 'getNextScheduledTime',
-      javaReturns: 'java.util.Date',
-      javaCode: function() {
-/*Calendar now = Calendar.getInstance();
-if ( scheduledTimeIsSet_ && now.getTime().compareTo(scheduledTime_) < 0 ) {
-  return scheduledTime_;
-}
-return ScheduledTimeFactory_();*/
       }
     }
   ]
