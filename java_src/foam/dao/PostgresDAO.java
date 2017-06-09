1930e130f85c145a3f307258809723bd3d0bccaf
drish <d.erich@hotmail.com>
2017-06-09 10:12:47 -0400

    java_src: set obj id with generated key from pg

--- java_src/foam/dao/PostgresDAO.java
+++ java_src/foam/dao/PostgresDAO.java
@@ -1,14 +1,9 @@
 package foam.dao;
 
-import com.sun.istack.internal.NotNull;
 import foam.core.FObject;
 import foam.core.PropertyInfo;
 
-import java.lang.reflect.Field;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class PostgresDAO extends ProxyDAO {
 
@@ -25,6 +20,36 @@ public class PostgresDAO extends ProxyDAO {
         PgConnectionPool.setup(host, port, dbName, username, password);
     }
 
+//    @Override
+//    public FObject find(Object o) {
+//        Field f = ((FObject) o).getClassInfo().get
+//
+//        Object value = ((PropertyInfo) f.get(o)).get(o);
+//
+//        try {
+//            SQLData data = new SQLData(((FObject) o));
+//            Connection c = PgConnectionPool.getConnection();
+//
+//            //TODO(drish)
+//            String sql = "select * from " + data.getTableName();
+//            sql += " where id=?";
+//
+//            System.out.println(data.getTableName());
+//            System.out.println(data.getValues());
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
+
+    public FObject setId(FObject obj, Object id) {
+
+        PropertyInfo prop = ((PropertyInfo) obj.getClassInfo().getAxiomByName("id"));
+        prop.set(obj, id);
+        return obj;
+    }
+
     @Override
     public FObject put(FObject obj) {
 
@@ -32,20 +57,37 @@ public class PostgresDAO extends ProxyDAO {
             SQLData data = new SQLData(obj);
             Connection c = PgConnectionPool.getConnection();
 
-            String sql = "insert into " + data.getTableName();
-            sql += data.getFormatedColumnNames();
-            sql += "values " + data.getFormatedPlaceholders();
+            StringBuilder sql = new StringBuilder("insert into " + data.getTableName());
+            sql.append(data.getFormatedColumnNames());
+            sql.append("values " + data.getFormatedPlaceholders());
 
-            PreparedStatement smt = c.prepareStatement(sql);
+            PreparedStatement smt = c.prepareStatement(sql.toString(), Statement.RETURN_GENERATED_KEYS);
 
+            // set values in order for prepared statement
             int i = 1;
             for (Object key: data.getValues().keySet()) {
                 smt.setObject(i, data.getValues().get(key));
                 i++;
             }
 
-            ResultSet rs = smt.executeQuery();
-            System.out.println(rs);
+            System.out.println(smt.toString());
+            int inserted = smt.executeUpdate();
+            if (inserted == 0) {
+                throw new SQLException("Error while inserting.");
+            }
+
+            // get auto-generated keys
+            try (ResultSet generatedKeys = smt.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    long pgKey = generatedKeys.getLong(1);
+                    setId(obj, pgKey);
+                    System.out.println(pgKey);
+                }
+                else {
+                    throw new SQLException("Error while inserting: no ID returned");
+                }
+            }
+
             smt.close();
             c.close();
         } catch (Exception e) {
