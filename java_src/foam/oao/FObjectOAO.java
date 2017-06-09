ff37c4baaba85cb598c8d9136674233e4ee4309c
Kirk Eaton <kirk@nanopay.net>
2017-06-09 10:09:20 -0400

    Added FObjectOAO for Java

---
+++ java_src/foam/oao/FObjectOAO.java
@@ -0,0 +1,39 @@
+/**
+ * @license
+ * Copyright 2017 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package foam.oao;
+
+import foam.core.FObject;
+import foam.core.PropertyInfo;
+import foam.core.X;
+
+import java.util.Map;
+
+public class FObjectOAO
+    implements OAO
+{
+  protected FObject obj;
+
+  @Override
+  public FObject get(X x) {
+    return obj;
+  }
+
+  @Override
+  public void setProperty(X x, String name, Object value) {
+    PropertyInfo prop = (PropertyInfo) obj.getClassInfo().getAxiomByName(name);
+    if ( prop != null ) prop.set(obj, value);
+  }
+
+  @Override
+  public void setProperties(X x, Map values) {
+    for (Object o : values.keySet()) {
+      String key = (String) o;
+      PropertyInfo prop = (PropertyInfo) obj.getClassInfo().getAxiomByName(key);
+      if ( prop != null ) prop.set(obj, values.get(key));
+    }
+  }
+}
