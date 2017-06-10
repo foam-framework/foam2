f45db8a8565d8fc685c2daa266753df3b4da1bdb
Mike Carcasole <mikecarcasole@gmail.com>
2017-06-10 17:08:26 -0400

    Only show a warning on the last NodeModelFileDAO that's checked.

--- src/foam/classloader/NodeModelFileDAO.js
+++ src/foam/classloader/NodeModelFileDAO.js
@@ -23,7 +23,10 @@ foam.CLASS({
   properties: [
     {
       name: 'classpath'
-    }
+    },
+    {
+      name: 'surpressWarning',
+    },
   ],
 
   methods: [
@@ -44,7 +47,7 @@ foam.CLASS({
       try {
         require(path);
       } catch(e) {
-        console.warn('Unable to load at ' + path + '. Error: ' + e.stack);
+        !self.surpressWarning && console.warn('Unable to load at ' + path + '. Error: ' + e.stack);
         return Promise.resolve(null);
       } finally {
         foam.CLASS = foamCLASS;
