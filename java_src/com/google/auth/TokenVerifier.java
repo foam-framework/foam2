fd78e5c6617971b6781f1dfd98c0b74bfb59305e
blakegreen <blake@nanopay.net>
2017-06-13 16:57:56 -0400

    Took out auto generated files

--- java_src/com/google/auth/TokenVerifier.java
+++
@@ -1,7 +0,0 @@
-// WARNING: GENERATED CODE, DO NOT MODIFY BY HAND!
-package com.google.auth;
-
-public interface TokenVerifier {
-    public String verify(String token) throws java.security.GeneralSecurityException;
-
-}
--- java_src/foam/core/Serializable.java
+++
@@ -1,5 +0,0 @@
-// WARNING: GENERATED CODE, DO NOT MODIFY BY HAND!
-package foam.core;
-
-public interface Serializable {
-}
