fd4d4bc9239b64bb2fd0aa8981c23559a0d7a16f
drish <d.erich@hotmail.com>
2017-06-12 22:40:55 -0400

    update when id is present

--- java_src/foam/dao/PostgresDAO.java
+++ java_src/foam/dao/PostgresDAO.java
@@ -51,49 +51,65 @@ public class PostgresDAO extends ProxyDAO {
     public FObject put(FObject obj) {
 
         try {
-            SQLData data = new SQLData(obj);
             Connection c = PgConnectionPool.getConnection();
 
-            StringBuilder sql = new StringBuilder("insert into " + data.getTableName());
-            sql.append(data.getFormatedColumnNames());
-            sql.append("values " + data.getFormatedPlaceholders());
+            // updating existing one
+            // when FObject isIdSet is null it returns 0 , is it ok ?
+            if ((Long) obj.getProperty("id") != 0) {
+                SQLData data = new SQLData(obj);
+                PreparedStatement smt = c.prepareStatement(data.createUpdateStatement());
 
-            PreparedStatement smt = c.prepareStatement(sql.toString(), Statement.RETURN_GENERATED_KEYS);
+                // put data into statement preserving order.
+                int i = 1;
+                for (Object value: data.getValues()) {
+                    smt.setObject(i, value);
+                    i++;
+                }
 
-            int i = 1;
-            for (Object key: data.getValues().keySet()) {
-                smt.setObject(i, data.getValues().get(key));
-                i++;
-            }
+                int inserted = smt.executeUpdate();
+                if (inserted == 0) {
+                    throw new SQLException("Error while inserting.");
+                }
 
-            System.out.println(smt.toString());
-            int inserted = smt.executeUpdate();
-            if (inserted == 0) {
-                throw new SQLException("Error while inserting.");
-            }
+                smt.close();
 
-            // get auto-generated keys
-            try (ResultSet generatedKeys = smt.getGeneratedKeys()) {
+            // inserting new one
+            } else {
+                SQLData data = new SQLData(obj);
+                PreparedStatement smt = c.prepareStatement(data.createInsertStatement(),
+                        Statement.RETURN_GENERATED_KEYS);
+
+                // put data into statement preserving order.
+                int i = 1;
+                for (Object key: data.getValues()) {
+                    smt.setObject(i, key);
+                    i++;
+                }
+
+                int inserted = smt.executeUpdate();
+                if (inserted == 0) {
+                    throw new SQLException("Error while inserting.");
+                }
+
+                // get auto-generated postgres keys
+                ResultSet generatedKeys = smt.getGeneratedKeys();
                 if (generatedKeys.next()) {
                     long pgKey = generatedKeys.getLong(1);
-//                    setId(obj, pgKey);
-                    System.out.println(pgKey);
-                }
-                else {
+                    obj.setProperty("id", pgKey);
+                } else {
                     throw new SQLException("Error while inserting: no ID returned");
                 }
+
+                smt.close();
             }
 
-            ResultSet rs = smt.executeQuery();
-            System.out.println(rs);
-            smt.close();
-            c.close();
+        c.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
 
         if (getDelegate() != null) return getDelegate().put(obj);
-        return null;
+        return obj;
     }
 
     /**
