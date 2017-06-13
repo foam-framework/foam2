4226a0dfd59a950ef5cab0ba715be11550aca17a
blakegreen <blake@nanopay.net>
2017-06-13 16:57:56 -0400

    Took out auto generated files

--- java_src/foam/box/Box.java
+++
@@ -1,7 +0,0 @@
-// WARNING: GENERATED CODE, DO NOT MODIFY BY HAND!
-package foam.box;
-
-public interface Box {
-    public void send(foam.box.Message message);
-
-}
--- java_src/foam/nanos/auth/EnabledAware.java
+++
@@ -1,5 +0,0 @@
-// WARNING: GENERATED CODE, DO NOT MODIFY BY HAND!
-package foam.nanos.auth;
-
-public interface EnabledAware {
-}
