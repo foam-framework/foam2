dd1107a34a689d52ea63052106dd657fe4ed24e3
MarcRoopchand <marc.roopchand@gmail.com>
2017-06-13 14:07:52 -0400

    Used multi-line backquote instead of concatentaion new lines

--- src/foam/nanos/auth/Group.js
+++ src/foam/nanos/auth/Group.js
@@ -56,13 +56,13 @@ foam.CLASS({
         }
       ],
       javaCode:
-        'if ( getPermissions() == null ) return false; \n' +
-        'for ( int i = 0; i < permissions_.length; i++ ) { \n' +
-        '  if ( new javax.security.auth.AuthPermission(permissions_[i].getId()).implies(permission) ) { \n' +
-        '    return true; \n' +
-        '  } \n' +
-        '} \n' +
-        'return false;'
+        `if ( getPermissions() == null ) return false;
+        for ( int i = 0 ; i < permissions_.length ; i++ ) {
+          if ( new javax.security.auth.AuthPermission(permissions_[i].getId()).implies(permission) ) {
+            return true;
+          }
+        }
+        return false;`
     }
   ]
 });
