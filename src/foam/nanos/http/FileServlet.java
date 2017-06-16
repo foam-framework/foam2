3f46c120e422593079166027d89af20ee7b3673e
Nick Prat <nick@nanopay.net>
2017-06-16 14:04:11 -0400

    Fixed relative pathing

--- src/foam/nanos/http/FileServlet.java
+++ src/foam/nanos/http/FileServlet.java
@@ -68,9 +68,9 @@ public class FileServlet
                 "<ul style=\"list-style-type:disc\">");
 
         File[] files = srcFile.listFiles();
-        if(files != null && files.length > 0) {
-          for(File file : files) {
-            pw.write("<li>" + "<a href=\"./" + filePath + (filePath.isEmpty() ? "" : "/") + file.getName() + "\"?>"
+        if ( files != null && files.length > 0 ) {
+          for ( File file : files ) {
+            pw.write("<li>" + "<a href=\"/static/" + filePath + (filePath.isEmpty() ? "" : "/") + file.getName() + "\"?>"
                 + file.getName()
                 + "</a></li>");
           }
