ebf78266c23495a49fc5c35e7fc3574a7e9a15a7
Pavneet Kapoor <pavikapoor@gmail.com>
2017-06-16 15:51:33 -0400

    Removed dateFormat and added Date conversion

--- src/foam/core/AbstractDatePropertyInfo.java
+++ src/foam/core/AbstractDatePropertyInfo.java
@@ -14,11 +14,8 @@ public abstract class AbstractDatePropertyInfo
     return o1.compareTo(o2);
   }
 
-  public abstract String getDateFormat();
-
   public void setFromString(Object obj, String value) {
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern(getDateFormat()).withZone(ZoneOffset.UTC);
-    LocalDateTime date = LocalDateTime.parse(value, formatter);
-    this.set(obj, Date.from(date.atZone(ZoneId.of("UTC")).toInstant()));
+    Date date = new Date(value);
+    this.set(obj, date);
   }
 }
