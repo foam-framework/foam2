b57c76a0385ee658f99e8f6da4f9620549b9c2fe
Kirk Eaton <kirk@nanopay.net>
2017-06-20 11:26:44 -0400

    Using JournaledDAO to ExportService

--- src/foam/nanos/export/ExportService.java
+++ src/foam/nanos/export/ExportService.java
@@ -12,25 +12,8 @@ public class ExportService
 {
 
   public void start() {
-    // TODO switch to journaled DAO
-    DAO dao = new MapDAO();
+    DAO dao = new JournaledDAO(new MapDAO(), "driverRegistry.txt");
     dao.setOf(DriverRegistry.class);
     setDelegate(dao);
-
-    DriverRegistry r;
-    r = new DriverRegistry();
-    r.setId("JSON");
-    r.setDriverName("foam.nanos.export.JSONDriver");
-    dao.put(r);
-
-    r = new DriverRegistry();
-    r.setId("XML");
-    r.setDriverName("foam.nanos.export.XMLDriver");
-    dao.put(r);
-
-    r = new DriverRegistry();
-    r.setId("CSV");
-    r.setDriverName("foam.nanos.export.CSVDriver");
-    dao.put(r);
   }
 }
