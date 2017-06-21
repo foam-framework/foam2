ed8f600fb884d112393d1e0d4f3e0bcd62e81b1b
Kevin Greer <kgrgreer@gmail.com>
2017-06-21 09:51:02 -0400

    Added copyright headers.

--- src/foam/core/AbstractDoublePropertyInfo.java
+++ src/foam/core/AbstractDoublePropertyInfo.java
@@ -1,7 +1,15 @@
+/**
+ * @license
+ * Copyright 2017 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 package foam.core;
 
-public abstract class AbstractDoublePropertyInfo extends AbstractPropertyInfo {
+public abstract class AbstractDoublePropertyInfo
+  extends AbstractPropertyInfo
+{
   public int compareValues(double d1, double d2) {
-    return java.lang.Double.compare(d1, d2);
+    return Double.compare(d1, d2);
   }
 }
--- src/foam/core/AbstractFObjectPropertyInfo.java
+++ src/foam/core/AbstractFObjectPropertyInfo.java
@@ -1,7 +1,14 @@
+/**
+ * @license
+ * Copyright 2017 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 package foam.core;
 
 public abstract class AbstractFObjectPropertyInfo
-  extends AbstractObjectPropertyInfo {
+  extends AbstractObjectPropertyInfo
+{
   //  public int compareValues(FObject o1, FObject o2) {
   //    return o1.compareTo(o2);
   //  }
