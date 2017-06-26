d187330842910e8e1c9aeb32c7a4d347d4b15f76
Mark Dittmer <mark.s.dittmer@gmail.com>
2017-06-26 13:18:44 -0400

    Boxes: Bug fix: MessagePortBox use of postMessage()

--- src/lib/box.js
+++ src/lib/box.js
@@ -1584,7 +1584,7 @@ foam.CLASS({
 	var channel = new MessageChannel();
 	this.messagePortService.addPort(channel.port1);
 
-	this.target.postMessage('', '*', [channel.port2]);
+	this.target.postMessage(channel.port2, [ channel.port2 ]);
 
         channel.port1.postMessage(foam.json.Network.stringify(this.Message.create({
           object: this.RegisterSelfMessage.create({ name: this.me.name })
