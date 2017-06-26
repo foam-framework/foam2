d26a81d9b57d21f84f2b7610c3556a437bb31620
Mark Dittmer <mark.s.dittmer@gmail.com>
2017-06-26 13:23:29 -0400

    Boxes: StreamingClientDAO: Uses BoxDAOListener on select and listen to stream results

--- src/lib/box.js
+++ src/lib/box.js
@@ -615,7 +615,8 @@ foam.CLASS({
     },
     {
       class: 'FObjectProperty',
-      name: 'obj'
+      name: 'obj',
+      value: null
     }
   ]
 });
@@ -649,6 +650,13 @@ foam.CLASS({
       this.box.send(this.Message.create({
         object: this.DAOEvent.create({
           name: 'remove', obj: obj
+        })
+      }));
+    },
+    function eof() {
+      this.box.send(this.Message.create({
+        object: this.DAOEvent.create({
+          name: 'eof'
         })
       }));
     },
@@ -824,6 +832,114 @@ foam.CLASS({
     function removeAll_(x, skip, limit, order, predicate) {
       this.SUPER(x, skip, limit, order, predicate);
       this.on.reset.pub();
+    }
+  ]
+});
+
+foam.CLASS({
+  package: 'foam.dao',
+  name: 'StreamingClientDAO',
+  extends: 'foam.dao.BaseClientDAO',
+
+  requires: [
+    'foam.dao.ArraySink',
+    'foam.dao.BoxDAOListener'
+  ],
+  imports: [ 'registry' ],
+
+  classes: [
+    {
+      name: 'StreamingReplyBox',
+
+      properties: [
+        {
+          name: 'id',
+          factory: function() { return foam.next$UID(); }
+        },
+        {
+          class: 'FObjectProperty',
+          of: 'foam.dao.Sink',
+          name: 'sink'
+        },
+        // TODO(markdittmer): Signal remote of detached and unregister
+        // reply box.
+        {
+          name: 'sinkSub_',
+          factory: function() {
+            var sub = foam.core.FObject.create();
+            sub.onDetach(function() { this.detached_ = true; }.bind(this));
+          }
+        },
+        {
+          class: 'Boolean',
+          name: 'detached_'
+        },
+        {
+          name: 'promise',
+          factory: function() {
+            var self = this;
+            return new Promise(function(resolve, reject) {
+              self.resolve_ = resolve;
+              self.reject_ = reject;
+            });
+          }
+        },
+        'resolve_',
+        'reject_'
+      ],
+
+      methods: [
+        function send(msg) {
+          // TODO(markdittmer): Error check message type.
+
+          if ( this.detached_ ) return;
+          switch ( msg.object.name ) {
+            case 'put':
+              this.sink.put(msg.object.obj, this.sinkSub_);
+              break;
+            case 'remove':
+              this.sink.remove(msg.object.obj, this.sinkSub_);
+              break;
+            case 'eof':
+              this.sink.eof();
+              this.resolve_(this.sink);
+              break;
+            case 'reset':
+              this.sink.reset();
+              break;
+          }
+        }
+      ]
+    }
+  ],
+
+  methods: [
+    function select_(x, sink, skip, limit, order, predicate) {
+      var replyBox = this.StreamingReplyBox.create({
+        sink: sink || this.ArraySink.create()
+      });
+      var promise = replyBox.promise;
+
+      replyBox = this.registry.register(replyBox.id, null, replyBox);
+
+      // TODO(markdittmer): Shouldn't there be an annotation for an errorBox
+      // somewhere here?
+      this.SUPER(
+          null, this.BoxDAOListener.create({ box: replyBox }),
+          skip, limit, order, predicate)
+              .catch(function(error) { replyBox.reject_(error); });
+      return promise;
+    },
+    function listen_(x, sink, predicate) {
+      var replyBox = this.StreamingReplyBox.create({
+        sink: sink || this.ArraySink.create()
+      });
+      replyBox = this.registry.register(replyBox.id, null, replyBox);
+
+      // TODO(markdittmer): Shouldn't there be an annotation for an errorBox
+      // somewhere here?
+      this.SUPER(null, this.BoxDAOListener.create({ box: replyBox }),
+                 predicate);
     }
   ]
 });
