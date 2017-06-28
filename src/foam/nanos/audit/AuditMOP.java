8a3aa38ee52550b450796367b0d99cf1bd217757
Adam Kenneweg <akenne@users.noreply.github.com>
2017-06-28 14:30:57 -0400

    Composite Logger

--- src/foam/nanos/audit/AuditMOP.java
+++ src/foam/nanos/audit/AuditMOP.java
@@ -10,7 +10,7 @@ import foam.core.FObject;
 import foam.core.X;
 import foam.mop.ProxyMOP;
 import foam.nanos.auth.User;
-import foam.nanos.logger.NanoLogger;
+import foam.nanos.logger.BaseLogger;
 
 import java.util.*;
 
@@ -37,7 +37,7 @@ public class AuditMOP
   @Override
   public FObject setProperty(X x, String name, Object value) {
     User       user     = (User) x.get("user");
-    NanoLogger logger   = (NanoLogger) x.get("logger");
+    BaseLogger logger   = (BaseLogger) x.get("logger");
     FObject    obj      = getDelegate().get(x);
     Object     objectId = obj.getProperty("id");
     Map        values   = new HashMap();
@@ -51,7 +51,7 @@ public class AuditMOP
   @Override
   public FObject setProperties(X x, Map values) {
     User       user     = (User) x.get("user");
-    NanoLogger logger   = (NanoLogger) x.get("logger");
+    BaseLogger logger   = (BaseLogger) x.get("logger");
     FObject    obj      = getDelegate().get(x);
     Object     objectId = obj.getProperty("id");
 
--- src/foam/nanos/logger/NanoLogger.java
+++ src/foam/nanos/logger/NanoLogger.java
@@ -6,112 +6,58 @@
 
 package foam.nanos.logger;
 
+import foam.nanos.*;
 import foam.core.*;
-import foam.nanos.NanoService;
+import java.util.ArrayList; 
+import java.util.List; 
 import java.util.logging.*;
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
 import java.io.IOException;
 
 public class NanoLogger
   extends    ContextAwareSupport
   implements NanoService
 {
-  protected Logger logger;
-
-  private static final ThreadLocal<SimpleDateFormat> sdf = new ThreadLocal<SimpleDateFormat>() {
-    @Override
-    protected SimpleDateFormat initialValue() {
-      return new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss");
-    }
-  };
-
-  private ThreadLocal<StringBuilder> sb = new ThreadLocal<StringBuilder>() {
-    @Override
-    protected StringBuilder initialValue() {
-      return new StringBuilder();
-    }
-
-    @Override
-    public StringBuilder get() {
-      StringBuilder b = super.get();
-      b.setLength(0);
-      return b;
-    }
-  };
+  private List<ILogger> childLoggers = new ArrayList<ILogger>();
 
   public void start() {
-    logger = Logger.getAnonymousLogger();
-    logger.setUseParentHandlers(false);
-    logger.setLevel(Level.ALL);
-
-    try {
-      Handler handler = new FileHandler("nano.log");
-      handler.setFormatter(new CustomFormatter());
-      logger.addHandler(handler);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
   }
 
-  private class CustomFormatter extends Formatter {
-    long   prevTime;
-    String prevTimestamp;
-
-    @Override
-    public String format(LogRecord record) {
-      int           lev = record.getLevel().intValue();
-      String        msg = record.getMessage();
-      StringBuilder str = sb.get();
-
-      if ( prevTime / 1000 != System.currentTimeMillis() / 1000 ) {
-        prevTime = System.currentTimeMillis();
-        prevTimestamp = sdf.get().format(new Timestamp(prevTime));
-      }
-
-      str.append(prevTimestamp);
-      str.append(',');
-
-      // debug special case, fine level == 500
-      if (lev == 500) {
-        str.append("DEBUG");
-      } else {
-        str.append(record.getLevel());
-      }
-
-      str.append(msg);
-      str.append('\n');
-      return str.toString();
-    }
+  public void add(ILogger logger) {
+    logger.start();
+    childLoggers.add(logger);
   }
 
-  public String combine(Object[] args) {
-    StringBuilder str = sb.get();
-    for (Object n : args) {
-      str.append(',');
-      str.append(n.toString());
-    }
-    return str.toString();
+  public void remove(ILogger logger) {
+    childLoggers.remove(logger);
   }
 
   public void log(Object... args) {
-    logger.info(combine(args));
+    for (ILogger logger : childLoggers) {
+      logger.info(args);
+    };
   }
 
   public void info(Object... args) {
-    logger.info(combine(args));
+    for (ILogger logger : childLoggers) {
+      logger.info(args);
+    };
   }
 
   public void warning(Object... args) {
-    logger.warning(combine(args));
+    for (ILogger logger : childLoggers) {
+      logger.warning(args);
+    };
   }
 
   public void error(Object... args) {
-    logger.severe(combine(args));
+    for (ILogger logger : childLoggers) {
+      logger.error(args);
+    };
   }
 
-  // can't normally do .debug() with custom formatter: use fine instead
   public void debug(Object...  args) {
-    logger.fine(combine(args));
+    for (ILogger logger : childLoggers) {
+      logger.debug(args);
+    };
   }
 }
