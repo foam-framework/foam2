7dad24f602c9393f0b183f59fb4dd1cc544d4b96
Tanay Parikh <me@tanayparikh.com>
2017-06-29 12:39:42 -0400

    Style guide spacing

--- src/foam/lib/csv/CSV.js
+++ src/foam/lib/csv/CSV.js
@@ -187,7 +187,7 @@ foam.CLASS({
     },
 
     function splitIntoValues(csvString) {
-      if (csvString == undefined || csvString.length == 0) return [];
+      if ( ( csvString == undefined ) || ( csvString.length == 0) ) return [];
 
       var parser = foam.lookup('foam.lib.csv.CSVParser').create();
       return parser.parseString(csvString).map(field => field.value == undefined ? '' : field.value);
