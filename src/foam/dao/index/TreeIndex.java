14ccb267364b9fd1d5ed6d8820aad49e55291e3c
marcogbarcellos <marcogbarcellos@gmail.com>
2017-06-29 11:58:05 -0400

    code standards for protected vars

--- src/foam/dao/index/TreeIndex.java
+++ src/foam/dao/index/TreeIndex.java
@@ -20,30 +20,30 @@ import foam.mlang.sink.Count;
 import java.util.Arrays;
 
 public class TreeIndex implements Index {
-  protected Index tail;
-  protected PropertyInfo prop;
-  protected long selectCount;
+  protected Index tail_;
+  protected PropertyInfo prop_;
+  protected long selectCount_;
 
   public TreeIndex(PropertyInfo prop) {
     this(prop, ValueIndex.instance());
   }
 
   public TreeIndex(PropertyInfo prop, Index tail) {
-    this.prop = prop;
-    this.selectCount = 0;
-    this.tail = tail;
+    prop_ = prop;
+    selectCount_ = 0;
+    tail_ = tail;
   }
 
   public Object bulkLoad(FObject[] a) {
     Arrays.sort(a);
-    return TreeNode.getNullNode().bulkLoad(this.tail, this.prop, 0, a.length-1, a);
+    return TreeNode.getNullNode().bulkLoad(tail_, prop_, 0, a.length-1, a);
   }
 
   protected Binary isExprMatch(Predicate predicate, Class model) {
-    if ( predicate != null && this.prop != null && model != null ) {
+    if ( predicate != null && prop_ != null && model != null ) {
       if ( predicate instanceof Binary &&
            model.equals(predicate.getClass()) &&
-           ((Binary) predicate).getArg1().equals(this.prop) ) {
+           ((Binary) predicate).getArg1().equals(prop_) ) {
 
         Binary b = new Binary() {};
         b.setArg2(((Binary) predicate).getArg2());
@@ -55,7 +55,7 @@ public class TreeIndex implements Index {
 
   public Object get(Object state, FObject obj) {
     if ( state != null ) {
-      return ((TreeNode) state).get(((TreeNode) state), obj, this.prop);
+      return ((TreeNode) state).get(((TreeNode) state), obj, prop_);
     }
     return null;
   }
@@ -65,11 +65,11 @@ public class TreeIndex implements Index {
       state = TreeNode.getNullNode();
     }
     return ((TreeNode) state).putKeyValue((TreeNode)state,
-      this.prop, this.prop.f(value), value, this.tail);
+      prop_, prop_.f(value), value, tail_);
   }
 
   public Object remove(Object state, FObject value) {
-    return ((TreeNode) state).removeKeyValue((TreeNode) state, this.prop, this.prop.f(value), value, this.tail);
+    return ((TreeNode) state).removeKeyValue((TreeNode) state, prop_, prop_.f(value), value, tail_);
   }
 
   public Object removeAll() {
@@ -106,16 +106,16 @@ public class TreeIndex implements Index {
     TreeNode subTree = ((TreeNode) state);
 
     Binary expr = isExprMatch(predicate, Gt.class);
-    if ( expr != null ) subTree = subTree.gt(subTree, expr.getArg2().f(expr), prop);
+    if ( expr != null ) subTree = subTree.gt(subTree, expr.getArg2().f(expr), prop_);
 
     expr = isExprMatch(predicate, Gte.class);
-    if ( expr != null ) subTree = subTree.gte(subTree, expr.getArg2().f(expr), prop);
+    if ( expr != null ) subTree = subTree.gte(subTree, expr.getArg2().f(expr), prop_);
 
     expr = isExprMatch(predicate, Lt.class);
-    if ( expr != null ) subTree = subTree.lt(subTree, expr.getArg2().f(expr), prop);
+    if ( expr != null ) subTree = subTree.lt(subTree, expr.getArg2().f(expr), prop_);
 
     expr = isExprMatch(predicate, Lte.class);
-    if ( expr != null ) subTree = subTree.lte(subTree, expr.getArg2().f(expr), prop);
+    if ( expr != null ) subTree = subTree.lte(subTree, expr.getArg2().f(expr), prop_);
 
     long cost = subTree.size;
 
