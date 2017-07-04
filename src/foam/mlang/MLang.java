6026ebcc03e3587dae97d4b5cb6a435b4100a316
Kirk Eaton <kirk@nanopay.net>
2017-07-04 09:12:58 -0400

    Made use of all args constructors in MLang.java

--- src/foam/mlang/MLang.java
+++ src/foam/mlang/MLang.java
@@ -19,69 +19,42 @@ public class MLang
 {
 
   public static Expr prepare(Object o) {
-    if ( o instanceof Expr )
-      return (Expr) o;
-    Constant constant = new Constant();
-    constant.setValue(o);
-    return constant;
+    return o instanceof Expr ? (Expr) o : new Constant(o);
   }
 
   public static Predicate LT(Object o1, Object o2) {
-    Lt lt = new Lt();
-    lt.setArg1(MLang.prepare(o1));
-    lt.setArg2(MLang.prepare(o2));
-    return lt;
+    return new Lt(MLang.prepare(o1), MLang.prepare(o2));
   }
 
   public static Predicate LTE(Object o1, Object o2) {
-    Lte lte = new Lte();
-    lte.setArg1(MLang.prepare(o1));
-    lte.setArg2(MLang.prepare(o2));
-    return lte;
+    return new Lte(MLang.prepare(o1), MLang.prepare(o2));
   }
 
   public static Predicate EQ(Object o1, Object o2) {
-    Eq eq = new Eq();
-    eq.setArg1(MLang.prepare(o1));
-    eq.setArg2(MLang.prepare(o2));
-    return eq;
+    return new Eq(MLang.prepare(o1), MLang.prepare(o2));
   }
 
   public static Predicate GTE(Object o1, Object o2) {
-    Gte gte = new Gte();
-    gte.setArg1(MLang.prepare(o1));
-    gte.setArg2(MLang.prepare(o2));
-    return gte;
+    return new Gte(MLang.prepare(o1), MLang.prepare(o2));
   }
 
   public static Predicate GT(Object o1, Object o2) {
-    Gt gt = new Gt();
-    gt.setArg1(MLang.prepare(o1));
-    gt.setArg2(MLang.prepare(o2));
-    return gt;
+    return new Gt(MLang.prepare(o1), MLang.prepare(o2));
   }
 
   public static Predicate AND(Predicate... args) {
-    And and = new And();
-    and.setArgs(args);
-    return and;
+    return new And(args);
   }
 
   public static Predicate OR(Predicate... args) {
-    Or or = new Or();
-    or.setArgs(args);
-    return or;
+    return new Or(args);
   }
 
   public static Sink MAX(Object o1) {
-    Max max = new Max();
-    max.setArg1(MLang.prepare(o1));
-    return max;
+    return new Max(0, MLang.prepare(o1));
   }
 
   public static Sink MIN(Object o1) {
-    Min min = new Min();
-    min.setArg1(MLang.prepare(o1));
-    return min;
+    return new Min(0, MLang.prepare(o1));
   }
 }
