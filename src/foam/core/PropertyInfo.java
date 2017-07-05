34fc15eedb84f1c58994bf7f825bd3c79c2805f5
Pavneet Kapoor <pavikapoor@gmail.com>
2017-07-05 16:59:18 -0400

    Added PropertyInfo modifications

--- src/foam/core/PropertyInfo.java
+++ src/foam/core/PropertyInfo.java
@@ -9,10 +9,12 @@ package foam.core;
 import foam.lib.parse.Parser;
 import java.util.Comparator;
 import java.util.Map;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLStreamWriter;
 
 // ???: Why is this interface mutable?
 public interface PropertyInfo
-  extends foam.mlang.Expr, Comparator
+        extends foam.mlang.Expr, Comparator
 {
   public PropertyInfo setClassInfo(ClassInfo p);
   public ClassInfo getClassInfo();
@@ -25,4 +27,7 @@ public interface PropertyInfo
   public Parser jsonParser();
   public void toJSON(foam.lib.json.Outputter outputter, StringBuilder out, Object value);
   public void diff(FObject o1, FObject o2, Map diff, PropertyInfo prop);
+  public void setFromString(Object obj, String value);
+  public Object fromXML(X x, XMLStreamReader reader);
+  public void toXML(FObject obj, Document doc, Element objElement);
 }
