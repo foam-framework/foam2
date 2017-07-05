fb703e197e98511451bb9206abc55c86930e2969
Pavneet Kapoor <pavikapoor@gmail.com>
2017-07-05 16:54:02 -0400

    Modified AbstractProperty Infos and XMLSupport added

---
+++ src/foam/core/AbstractArrayPropertyInfo.java
@@ -0,0 +1,88 @@
+/**
+ * @license
+ * Copyright 2017 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package foam.core;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLStreamWriter;
+
+public abstract class AbstractArrayPropertyInfo
+  extends AbstractPropertyInfo
+{
+
+  @Override
+  public void setFromString(Object obj, String value) {
+    // TODO
+  }
+
+  // NESTED ARRAY
+  @Override
+  public Object fromXML(X x, XMLStreamReader reader) {
+    List objList = new ArrayList();
+    String startTag = reader.getLocalName();
+    try {
+      int eventType;
+      while ( reader.hasNext() ) {
+        eventType = reader.next();
+        switch ( eventType ) {
+          case XMLStreamConstants.START_ELEMENT:
+            // Nested object in array
+            if ( reader.getLocalName().equals("object") ) {
+              FObject clsInstance = XMLSupport.createObj(x, reader);
+              if ( clsInstance != null ) {
+                objList.add(clsInstance);
+              }
+            } else if ( reader.getLocalName().equals("value") ) {
+              // Move to characters TODO
+              // Type of array required for casting
+              String type = this.of();
+              reader.next();
+              String value = reader.getText();
+              objList.add(value);
+            }
+            break;
+          case XMLStreamConstants.END_DOCUMENT:
+            if ( reader.getLocalName() == startTag ) { return objList; }
+            break;
+        }
+      }
+    } catch (XMLStreamException ex) {
+    }
+    return objList;
+  }
+
+  @Override
+  public void toXML (FObject obj, Document doc, Element objElement) {
+    // Empty Array Properties
+    if ( this.f(obj) == null ) return;
+
+    Element prop = doc.createElement(this.getName());
+    objElement.appendChild(prop);
+
+    // FObject Array check
+    if ( this.f(obj) instanceof FObject[] ) {
+      FObject[] nestedArray = (FObject[]) this.f(obj);
+      for ( int j = 0; j < nestedArray.length; j++ ) {
+        XMLSupport.toXML(nestedArray[j], doc, prop);
+      }
+      return;
+    } else {
+      Object[] nestObj = (Object[]) this.f(obj);
+      for ( int j = 0; j < nestObj.length; j++ ) {
+        Element nestedProp = doc.createElement("value");
+        nestedProp.appendChild(doc.createTextNode(nestObj[j].toString()));
+        prop.appendChild(nestedProp);
+      }
+    }
+  }
+}
--- src/foam/core/AbstractFObjectPropertyInfo.java
+++ src/foam/core/AbstractFObjectPropertyInfo.java
@@ -6,10 +6,43 @@
 
 package foam.core;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLStreamWriter;
+
 public abstract class AbstractFObjectPropertyInfo
   extends AbstractObjectPropertyInfo
 {
   //  public int compareValues(FObject o1, FObject o2) {
   //    return o1.compareTo(o2);
   //  }
+
+  @Override
+  public Object fromXML(X x, XMLStreamReader reader) {
+    FObject obj = null;
+    try {
+      String objClass = reader.getAttributeValue(null, "class");
+      Class cls = Class.forName(objClass);
+      obj = (FObject) x.create(cls);
+      XMLSupport.copyFromXML(x, obj, reader);
+    } catch (ClassNotFoundException | XMLStreamException ex) {
+
+    }
+    return obj;
+  }
+
+  @Override
+  public void toXML(FObject obj, Document doc, Element objElement) {
+    Object nestObj = this.f(obj);
+    String objName = nestObj.getClass().getName();
+    Element objTag = doc.createElement(objName.substring(objName.lastIndexOf(".")).replace(".",""));
+    objElement.appendChild(objTag);
+    if ( nestObj.getClass().isEnum() ) {
+      XMLSupport.enumXML( (Enum) nestObj, doc, objTag);
+    } else {
+      XMLSupport.toXML((FObject) nestObj, doc, objTag);
+    }
+  }
 }
