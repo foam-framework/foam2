63f93e174821ad1407da3df1e961ed691e7bea39
Tanay Parikh <me@tanayparikh.com>
2017-07-06 10:25:08 -0400

    Added headerParser CFG

--- src/foam/lib/csv/CSVParser.js
+++ src/foam/lib/csv/CSVParser.js
@@ -18,7 +18,11 @@ foam.CLASS({
       name: 'delimiter'
     },
     {
-      name: 'parser',
+      class: 'String',
+      name: 'nestedObjectSeperator'
+    },
+    {
+      name: 'stringParser',
       factory: function() {
         var X = this.X;
         var self = this;
@@ -56,13 +60,58 @@ foam.CLASS({
           escapedQuote: function() { return '"'; }
         });
       }
+    },
+    {
+      name: 'headerParser',
+      factory: function() {
+        var X = this.X;
+        var self = this;
+        
+        return this.ImperativeGrammar.create({ 
+          symbols: function(alt, anyChar, literal, literalIC, not, notChars, optional,
+          plus, range, repeat, repeat0, seq, seq1, str, sym) {
+            return {
+
+              START: seq1(1, sym('ws'), repeat(sym('field'), literal(self.nestedObjectSeperator)), sym('ws')),
+
+              field: alt(sym('text'), ''),
+
+              text: repeat(alt(sym('escapedSeperator'), not(self.nestedObjectSeperator, anyChar())), '', 1),
+
+              escapedSeperator: literal(self.nestedObjectSeperator + self.nestedObjectSeperator),
+
+              white: alt(' ', '\t', '\r', '\n'),
+
+              // 0 or more whitespace characters.
+              ws: repeat0(sym('white'))
+            }
+          }
+        }).addActions({
+          text: function(a) {
+            return { node: 'text', value: self.recoverHeaderTitle(a.join('')) };
+          },
+
+          escapedQuote: function() { return '"'; }
+        });
+      }
     }
   ],
 
   methods: [
     function parseString(str, delimiter) {
       this.delimiter = delimiter;
-      return this.parser.parseString(str);
+      return this.stringParser.parseString(str);
+    },
+    
+    function parseHeader(str, nestedObjectSeperator) {
+      this.nestedObjectSeperator = nestedObjectSeperator;
+      return this.headerParser.parseString(str);
+    },
+
+    function recoverHeaderTitle(t) {
+      // Recovers header title by replacing the nested object seperator x 2, by itself
+      return t.replace(new RegExp(this.nestedObjectSeperator + this.nestedObjectSeperator, 'g'), 
+                this.nestedObjectSeperator);
     }
   ]
 });
