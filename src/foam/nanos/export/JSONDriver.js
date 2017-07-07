22e33d5b0d2a5e5a2f8245b5e6fd5711350886f8
Kirk Eaton <kirk@nanopay.net>
2017-07-07 10:01:21 -0400

    Wrote CSVDriver

--- src/foam/nanos/export/JSONDriver.js
+++ src/foam/nanos/export/JSONDriver.js
@@ -4,25 +4,27 @@
  * http://www.apache.org/licenses/LICENSE-2.0
  */
 
- foam.CLASS({
-   package: 'foam.nanos.export',
-   name: 'JSONDriver',
-   implements: [ 'foam.nanos.export.ExportDriver' ],
+foam.CLASS({
+  package: 'foam.nanos.export',
+  name: 'JSONDriver',
+  implements: [ 'foam.nanos.export.ExportDriver' ],
 
-   properties: [
-     {
-       class: 'FObjectProperty',
-       of: 'foam.json.Outputer',
-       name: 'outputer',
-       factory: function() { return foam.json.PrettyStrict; }
-     }
-   ],
+  documentation: 'Class for exporting data from a DAO to JSON',
 
-   methods: [
-     function exportDAO(X, dao) {
-       return dao.select().then(function (sink) {
-         return outputer.stringify(sink.array);
-       });
-     }
-   ]
- });
+  properties: [
+    {
+      class: 'FObjectProperty',
+      of: 'foam.json.Outputer',
+      name: 'outputer',
+      factory: function() { return foam.json.PrettyStrict; }
+    }
+  ],
+
+  methods: [
+    function exportDAO(X, dao) {
+      return dao.select().then(function (sink) {
+        return outputer.stringify(sink.array);
+      });
+    }
+  ]
+});
