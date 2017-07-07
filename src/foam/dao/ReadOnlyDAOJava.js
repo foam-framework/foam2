40bdd116d412a2394e01092a4c910e649adee273
Tanay Parikh <me@tanayparikh.com>
2017-07-07 09:25:48 -0400

    Removed method structure info for ReadOnlyDAO (reflected in DAOJava.js)

--- src/foam/dao/ReadOnlyDAOJava.js
+++ src/foam/dao/ReadOnlyDAOJava.js
@@ -6,59 +6,14 @@ foam.CLASS({
   methods: [
     {
       name: 'put_',
-      javaReturns: 'foam.core.FObject',
-      args: [
-        {
-          name: 'x',
-          javaType: 'foam.core.X'
-        },
-        {
-          name: 'obj',
-          javaType: 'foam.core.FObject'
-        }
-      ],
       javaCode: `throw new UnsupportedOperationException("Cannot put into ReadOnlyDAO");`
     },
     {
       name: 'remove_',
-      javaReturns: 'foam.core.FObject',
-      args: [
-        {
-          name: 'x',
-          javaType: 'foam.core.X'
-        },
-        {
-          name: 'obj',
-          javaType: 'foam.core.FObject'
-        }
-      ],
       javaCode: `throw new UnsupportedOperationException("Cannot remove from ReadOnlyDAO");`
     },
     {
       name: 'removeAll_',
-      javaReturns: 'void',
-      args: [
-        {
-          name: 'x',
-          javaType: 'foam.core.X'
-        },
-        {
-          name: 'skip',
-          javaType: 'long'
-        },
-        {
-          name: 'limit',
-          javaType: 'long'
-        },
-        {
-          name: 'order',
-          javaType: 'foam.mlang.order.Comparator'
-        },
-        {
-          name: 'predicate',
-          javaType: 'foam.mlang.predicate.Predicate'
-        }
-      ],
       javaCode: `throw new UnsupportedOperationException("Cannot removeAll from ReadOnlyDAO");`
     }
   ]
