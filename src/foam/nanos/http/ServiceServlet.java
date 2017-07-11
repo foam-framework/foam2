e4db3bb9c22e62e023fb991a6ccc10cd1040b1ba
Kevin Greer <kgrgreer@gmail.com>
2017-07-11 14:07:37 -0400

    Formatting.

--- src/foam/nanos/http/ServiceServlet.java
+++ src/foam/nanos/http/ServiceServlet.java
@@ -63,53 +63,53 @@ public class ServiceServlet
 
   public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
     try {
-    CharBuffer buffer_        = CharBuffer.allocate(65535);
-    Reader     reader         = req.getReader();
-    int        count          = reader.read(buffer_);
-    X          requestContext = getX().put("httpRequest", req).put("httpResponse", resp);
+      CharBuffer buffer_        = CharBuffer.allocate(65535);
+      Reader     reader         = req.getReader();
+      int        count          = reader.read(buffer_);
+      X          requestContext = getX().put("httpRequest", req).put("httpResponse", resp);
 
-    System.out.println("Service Request");
-    resp.setHeader("Access-Control-Allow-Origin", "*");
-    buffer_.rewind();
+      System.out.println("Service Request");
+      resp.setHeader("Access-Control-Allow-Origin", "*");
+      buffer_.rewind();
 
-System.out.println("Request: " + buffer_.toString()); buffer_.rewind();
+      System.out.println("Request: " + buffer_.toString()); buffer_.rewind();
 
-    FObject result = requestContext.create(JSONParser.class).parseString(buffer_.toString());
+      FObject result = requestContext.create(JSONParser.class).parseString(buffer_.toString());
 
-    if ( result == null ) {
-      resp.setStatus(resp.SC_BAD_REQUEST);
-      PrintWriter out = resp.getWriter();
-      System.err.println("Failed to parse request");
-      out.print("Failed to parse request");
-      out.flush();
-      return;
+      if ( result == null ) {
+        resp.setStatus(resp.SC_BAD_REQUEST);
+        PrintWriter out = resp.getWriter();
+        System.err.println("Failed to parse request");
+        out.print("Failed to parse request");
+        out.flush();
+        return;
+      }
+
+      if ( ! ( result instanceof foam.box.Message ) ) {
+        resp.setStatus(resp.SC_BAD_REQUEST);
+        PrintWriter out = resp.getWriter();
+        System.err.println("Expected instance of foam.box.Message");
+        out.print("Expected instance of foam.box.Message");
+        out.flush();
+        return;
+      }
+
+      foam.box.Message msg = (foam.box.Message) result;
+
+      skeleton_.send(msg);
+
+      System.err.println("Response: " + msg.getObject().toString());
+
+      if ( ! ( msg.getAttributes().get("replyBox") instanceof foam.box.HTTPReplyBox ) ) {
+        // resp.complete(); //flushBuffer();
+        System.err.println("No ReplyBox");
+      }
+      resp.setStatus(resp.SC_OK);
+      resp.flushBuffer();
+    } catch (Throwable t) {
+      System.err.println("Error: " + t);
+      t.printStackTrace();
     }
-
-    if ( ! ( result instanceof foam.box.Message ) ) {
-      resp.setStatus(resp.SC_BAD_REQUEST);
-      PrintWriter out = resp.getWriter();
-      System.err.println("Expected instance of foam.box.Message");
-      out.print("Expected instance of foam.box.Message");
-      out.flush();
-      return;
-    }
-
-    foam.box.Message msg = (foam.box.Message) result;
-
-    skeleton_.send(msg);
-
-    System.err.println("Response: " + msg.getObject().toString());
-
-    if ( ! ( msg.getAttributes().get("replyBox") instanceof foam.box.HTTPReplyBox ) ) {
-      // resp.complete(); //flushBuffer();
-      System.err.println("No ReplyBox");
-    }
-    resp.setStatus(resp.SC_OK);
-    resp.flushBuffer();
-  } catch (Throwable t) {
-    System.err.println("Error: " + t);
-    t.printStackTrace();
-  }
   }
 
   public void doOptions(HttpServletRequest req, HttpServletResponse resp)
