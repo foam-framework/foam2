1661000d212bc910b937ff3580830ed1963d4cbe
drish <d.erich@hotmail.com>
2017-07-11 16:35:46 -0400

    add second constructor to remove sink class

--- src/foam/dao/RemoveSink.java
+++ src/foam/dao/RemoveSink.java
@@ -2,6 +2,8 @@ package foam.dao;
 
 import foam.core.Detachable;
 import foam.core.FObject;
+import foam.mlang.order.Comparator;
+import foam.mlang.predicate.Predicate;
 
 public class RemoveSink {
 
@@ -12,5 +14,14 @@ public class RemoveSink {
         dao.remove(obj);
       }
     }, 0, AbstractDAO.MAX_SAFE_INTEGER,null, null);
+  }
+
+  public RemoveSink(final DAO dao, long skip, long limit, Comparator order, Predicate predicate) {
+    dao.select_(((AbstractDAO) dao).getX(), new AbstractSink() {
+      @Override
+      public void put(FObject obj, Detachable sub) {
+        dao.remove(obj);
+      }
+    }, skip, limit, order, predicate);
   }
 }
