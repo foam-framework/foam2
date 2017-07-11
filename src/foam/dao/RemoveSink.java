7ddf6dced52160cf5c00aee66185dc14ae51c80f
drish <d.erich@hotmail.com>
2017-07-11 16:38:19 -0400

    use remove_ instead

--- src/foam/dao/RemoveSink.java
+++ src/foam/dao/RemoveSink.java
@@ -2,25 +2,28 @@ package foam.dao;
 
 import foam.core.Detachable;
 import foam.core.FObject;
+import foam.core.X;
 import foam.mlang.order.Comparator;
 import foam.mlang.predicate.Predicate;
 
 public class RemoveSink {
 
   public RemoveSink(final DAO dao) {
-    dao.select_(((AbstractDAO) dao).getX(), new AbstractSink() {
+    final X x = ((AbstractDAO) dao).getX();
+    dao.select_(x, new AbstractSink() {
       @Override
       public void put(FObject obj, Detachable sub) {
-        dao.remove(obj);
+        dao.remove_(x, obj);
       }
     }, 0, AbstractDAO.MAX_SAFE_INTEGER,null, null);
   }
 
   public RemoveSink(final DAO dao, long skip, long limit, Comparator order, Predicate predicate) {
-    dao.select_(((AbstractDAO) dao).getX(), new AbstractSink() {
+    final X x = ((AbstractDAO) dao).getX();
+    dao.select_(x, new AbstractSink() {
       @Override
       public void put(FObject obj, Detachable sub) {
-        dao.remove(obj);
+        dao.remove_(x, obj);
       }
     }, skip, limit, order, predicate);
   }
