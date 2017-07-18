5d846198df1ed5eb4f640e5bbea83624087dcb8d
Tanay Parikh <me@tanayparikh.com>
2017-07-18 10:06:01 -0400

    Updated MapParser grammer to account for empty maps

--- src/foam/lib/json/MapParser.java
+++ src/foam/lib/json/MapParser.java
@@ -11,20 +11,23 @@ import foam.lib.parse.*;
 public class MapParser extends ProxyParser {
   public MapParser() {
     super(
-          new Seq1(2,
+          new Seq1(1,
                    new Whitespace(),
-                   new Literal("{"),
-                   new Repeat(
-                              new Seq2(1, 5,
-                                       new Whitespace(),
-                                       new AnyKeyParser(),
-                                       new Whitespace(),
-                                       new Literal(":"),
-                                       new Whitespace(),
-                                       new AnyParser()),
-                              new Seq0(new Whitespace(), new Literal(","))),
-                   new Whitespace(),
-                   new Literal("}"))
+                   new Alt(
+                      new Seq1(1, new Literal("{"), new Whitespace(), new Literal("}")),
+                      new Seq1(1, new Literal("{"),
+                                  new Repeat(
+                                      new Seq2(1, 5,
+                                              new Whitespace(),
+                                              new AnyKeyParser(),
+                                              new Whitespace(),
+                                              new Literal(":"),
+                                              new Whitespace(),
+                                              new AnyParser()),
+                                      new Seq0(new Whitespace(), new Literal(","))),
+                                  new Literal("}"))
+                   ),
+                   new Whitespace())
           );
   }
 
@@ -32,6 +35,12 @@ public class MapParser extends ProxyParser {
     ps = super.parse(ps, x);
 
     if ( ps != null ) {
+      // Checks if ps value is "{" char (as whitespace doesn't set ps value)
+      if ( ( ps.value() instanceof String ) && ( ( (String) ps.value() ).equals("{") ) ) {
+        // Sets value to empty hashmap
+        return ps.setValue(new java.util.HashMap());
+      }
+
       Object[] values = (Object[])ps.value();
       java.util.Map map = new java.util.HashMap(values.length);
 
