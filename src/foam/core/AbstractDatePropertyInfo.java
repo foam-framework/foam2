0475009ce2ed25a243ff9233b7699ab782285dcb
Pavneet Kapoor <pavikapoor@gmail.com>
2017-07-27 14:55:47 -0400

    PR changes made

--- src/foam/core/AbstractDatePropertyInfo.java
+++ src/foam/core/AbstractDatePropertyInfo.java
@@ -13,8 +13,8 @@ public abstract class AbstractDatePropertyInfo
   extends AbstractPropertyInfo
 {
 
-  public int compareValues(java.util.Date o1, java.util.Date o2) {
-    return o1.compareTo(o2);
+  public int compareValues(java.lang.Object o1, java.lang.Object o2) {
+    return ((Date)o1).compareTo(((Date)o2));
   }
 
   public void setFromString(Object obj, String value) {
