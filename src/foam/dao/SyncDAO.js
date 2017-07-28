eb75be9f025c939b21f46139b9c3bc3eb52c4354
Mark Dittmer <mark.s.dittmer@gmail.com>
2017-07-28 14:20:22 -0400

    SyncDAO overhaul
    Many of these changes are aesthetic (updating documentation strings;
    following latest conventions for public/private method names, etc.). Largest
    functional changes are synchronization:
    
    - `sync()` passes do not overlap; they wait for the previous pass to complete
    - `delegate.put()/remove()` + `syncRecordDAO.put()` are transactional
    
    These changes appear to fix issues identified in standalone SyncDAO stress test:
    `test/node/syncDAOStress.js`

--- src/foam/dao/SyncDAO.js
+++ src/foam/dao/SyncDAO.js
@@ -16,248 +16,353 @@
  */
 
 foam.CLASS({
-  package: 'foam.dao.sync',
-  name: 'SyncRecord',
-
-  documentation: 'Used by foam.dao.SyncDAO to track object updates and deletions.',
-
-  properties: [
-    'id',
-    {
-      class: 'Int',
-      name: 'syncNo',
-      value: -1
-    },
-    {
-      class: 'Boolean',
-      name: 'deleted',
-      value: false
-    }
-  ]
-});
-
-
-/**
-  SyncDAO synchronizes data between multiple client's offline caches and a server.
-  When syncronizing, each client tracks the last-seen version of each object,
-  or if the object was deleted. The most recent version is retained.
-  <p>
-  Make sure to set the syncProperty to use to track each object's version.
-  It will automatically be incremented as changes are put() into the SyncDAO.
-*/
-foam.CLASS({
   package: 'foam.dao',
   name: 'SyncDAO',
   extends: 'foam.dao.ProxyDAO',
 
-  requires: [
-    'foam.dao.sync.SyncRecord'
-  ],
+  documentation: `SyncDAO synchronizes data between multiple clients' offline
+      caches and a server. When syncronizing, each client tracks the last-seen
+      version of each object, or if the object was deleted. The most recent
+      version is retained.
 
-  implements: [
-    'foam.mlang.Expressions'
-  ],
+      Make sure to set the syncProperty to use to track each object's version.
+      It will automatically be incremented as changes are put() into the
+      SyncDAO.
 
-  imports: [
-    'setInterval'
-  ],
+      Remote DAOs that interact with SyncDAO clients should be decorated with
+      foam.dao.VersionNoDAO, or similar, to provision new version numbers for
+      records being written to the server.`,
+
+  requires: [ 'foam.dao.sync.SyncRecord' ],
+
+  implements: [ 'foam.mlang.Expressions' ],
+
+  imports: [ 'setInterval' ],
 
   properties: [
     {
-      /**
-        The shared server DAO to synchronize through.
-      */
       class: 'foam.dao.DAOProperty',
       name: 'remoteDAO',
+      documentation: 'The shared server DAO to synchronize to.',
       transient: true,
       required: true
     },
     {
-      /** The local cache to sync with the server DAO. */
       name: 'delegate',
+      documentation: 'The local cache to sync with the server DAO.'
     },
     {
-      /**
-        The DAO in which to store SyncRecords for each object. Each client
-        tracks their own sync state in a separate syncRecordDAO.
-      */
+      class: 'foam.dao.DAOProperty',
       name: 'syncRecordDAO',
+      documentation: `The DAO in which to store SyncRecords for each object.
+          Each client tracks their own sync state in a separate syncRecordDAO.`,
       transient: true,
       required: true
     },
     {
-      /**
-        The property to use to store the object version. This value on each
-        object will be incremented each time it is put() into the SyncDAO.
-      */
       name: 'syncProperty',
+      documentation: `The property to use to store the object version. This
+          value on each object will be incremented each time it is put() into
+          the SyncDAO.`,
       required: true,
       transient: true
     },
     {
-      /**
-        The class of object this DAO will store.
-      */
       name: 'of',
+      documentation: 'The class of object this DAO will store.',
       required: true,
       transient: true
     },
     {
-      /**
-        If using a remote DAO without push capabilities, such as an HTTP
-        server, polling will periodically attempt to synchronize.
-      */
       class: 'Boolean',
       name: 'polling',
-      value: false
+      documentation: `If using a remote DAO without push capabilities, such as
+          an HTTP server, polling will periodically attempt to synchronize.`
     },
     {
       /**
-        If using polling, pollingFrequency will determine the number of
-        milliseconds to wait between synchronization attempts.
+
       */
       class: 'Int',
       name: 'pollingFrequency',
+      documentation: `If using polling, pollingFrequency will determine the
+        number of milliseconds to wait between synchronization attempts.`,
       value: 1000
+    },
+    {
+      name: 'synced',
+      documentation: `A promise that resolves after any in-flight
+          synchronization pass completes.`,
+      factory: function() { return Promise.resolve(); }
+    },
+    {
+      name: 'syncRecordWriteSync_',
+      factory: function() { return Promise.resolve(); }
     }
   ],
 
   methods: [
-    /** @private */
     function init() {
       this.SUPER();
 
-      this.remoteDAO$proxy.sub('on', this.onRemoteUpdate);
+      // Only listen to DAOs that support push (i.e., do not require polling).
+      if ( ! this.polling )
+        this.remoteDAO$proxy.sub('on', this.onRemoteUpdate);
 
-      this.delegate.on.sub(this.onLocalUpdate);
-
-      if ( this.polling ) {
-        this.setInterval(function() {
-          this.sync();
-        }.bind(this), this.pollingFrequency);
-      }
-    },
-
-    /**
-      Updates the object's last seen info.
-    */
-    function put_(x, obj) {
-      return this.delegate.put_(x, obj).then(function(o) {
-        this.syncRecordDAO.put_(x,
-          this.SyncRecord.create({
-            id: o.id,
+      // Push initial data from delegate.
+      var self = this;
+      self.synced = self.delegate.select().then(function(sink) {
+        var array = sink.array;
+        for ( var i = 0; i < array.length; i++ ) {
+          self.syncRecordDAO.put(self.SyncRecord.create({
+            id: array[i].id,
             syncNo: -1
           }));
-        return o;
+        }
+      }).
+          // Like sync(), but on initial sync, syncFromRemote_() regardless of
+          // whether "polling" is set.
+          then(self.syncToRemote_.bind(self)).
+          then(self.syncFromRemote_.bind(self));
+
+      // Setup polling after initial sync.
+      if ( ! self.polling ) return;
+      self.synced.then(function() {
+        self.setInterval(function() {
+          self.sync();
+        }, self.pollingFrequency);
+      });
+    },
+    function sync() {
+      // Sync after any sync(s) in progress complete.
+      return this.synced = this.synced.then(function() {
+        console.log('sync');
+        var ret = this.syncToRemote_();
+
+        // Only syncFromRemote_ when polling (otherwise, updates will get pushed
+        // anyway).
+        if ( this.polling ) ret = ret.then(this.syncFromRemote_.bind(this));
+
+        return ret;
       }.bind(this));
     },
 
-    /**
-      Marks the object as deleted.
-    */
+    //
+    // DAO overrides.
+    //
+
+    function put_(x, obj) {
+      var self = this;
+      var ret;
+      return self.withSyncRecordTx_(function() {
+        return self.delegate.put_(x, obj).then(function(o) {
+          ret = o;
+          // Updates the object's last seen info.
+          return self.syncRecordDAO.put_(x, self.SyncRecord.create({
+            id: o.id,
+            syncNo: -1
+          }));
+        });
+      }).then(self.onLocalUpdate).then(function() { return ret; });
+    },
     function remove_(x, obj) {
-      return this.delegate.remove_(x, obj).then(function(o) {
-        this.syncRecordDAO.put_(x,
-          this.SyncRecord.create({
+      var self = this;
+      var ret;
+      return self.withSyncRecordTx_(function() {
+        return self.delegate.remove_(x, obj).then(function(o) {
+          ret = o;
+          // Marks the object as deleted.
+          self.syncRecordDAO.put_(x, self.SyncRecord.create({
             id: obj.id,
             deleted: true,
             syncNo: -1
           }));
-      }.bind(this));
-    },
-
-    /**
-      Marks all the removed objects' sync records as deleted.
-    */
-    function removeAll_(x, skip, limit, order, predicate) {
-      this.delegate.select_(x, null, skip, limit, order, predicate).then(function(a) {
-        a = a.array;
-        for ( var i = 0 ; i < a.length ; i++ ) {
-          this.remove_(x, a[i]);
-        }
-      }.bind(this));
-    },
-
-    /** @private */
-    function processFromServer(obj) {
-      this.delegate.put(obj).then(function(obj) {
-        this.syncRecordDAO.put(
-          this.SyncRecord.create({
-            id: obj.id,
-            syncNo: obj[this.syncProperty.name]
-          }));
-      }.bind(this));
-    },
-
-    /** @private */
-    function syncFromServer() {
-      var E = this;
-
-      this.syncRecordDAO.select(E.MAX(this.SyncRecord.SYNC_NO)).then(function(m) {
-        this.remoteDAO
-          .where(E.GT(this.syncProperty, m.value || 0))
-          .select().then(function(a) {
-            a = a.array;
-            for ( var i = 0 ; i < a.length ; i++ ) {
-              this.processFromServer(a[i]);
-            }
-          }.bind(this));
-      }.bind(this));
-    },
-
-    /** @private */
-    function syncToServer() {
-      var E = this;
-      var self = this;
-
-      this.syncRecordDAO
-        .where(E.EQ(this.SyncRecord.SYNC_NO, -1))
-        .select().then(function(records) {
-          records = records.array;
-
-          for ( var i = 0 ; i < records.length ; i++ ) {
-            var record = records[i]
-            var id = record.id;
-            var deleted = record.deleted;
-
-            if ( deleted ) {
-              var obj = self.of.create(undefined, self);
-              obj.id = id;
-              self.remoteDAO.remove(obj);
-            } else {
-              // TODO: Stop sending updates if the first one fails.
-              self.delegate.find(id).then(function(obj) {
-                if ( obj ) return self.remoteDAO.put(obj).then(function(obj) {
-                  self.processFromServer(obj);
-                });
-                return null;
-              });
-            }
-          }
         });
+      }).then(self.onLocalUpdate).then(function() { return ret; });
+    },
+    function removeAll_(x, skip, limit, order, predicate) {
+      // Marks all the removed objects' sync records as deleted via remove_().
+      return this.delegate.select_(x, null, skip, limit, order, predicate).
+          then(function(a) {
+            a = a.array;
+            var promises = [];
+
+            for ( var i = 0 ; i < a.length ; i++ ) {
+              promises.push(this.remove_(x, a[i]));
+            }
+
+            return Promise.all(promises);
+          }.bind(this));
     },
 
-    /** @private */
-    function sync() {
-      this.syncToServer();
-      this.syncFromServer();
+    //
+    // Private synchronization details.
+    //
+
+    {
+      name: 'putFromRemote_',
+      documentation: 'Process a put() to cache from remote.',
+      code: function(obj) {
+        // console.log('putFromRemote_', foam.json.stringify(obj));
+        var self = this;
+        var ret;
+        return self.withSyncRecordTx_(function() {
+          return self.delegate.put(obj).then(function(o) {
+            ret = o;
+            // console.log('putFromRemote_: store syncRecord for', foam.json.stringify(o));
+            return self.syncRecordDAO.put(self.SyncRecord.create({
+              id: o.id,
+              syncNo: o[self.syncProperty.name]
+            }));
+          });
+        }).then(function() { return ret; });
+      }
+    },
+    {
+      name: 'removeFromRemote_',
+      documentation: 'Process a remove() on cache from remote.',
+      code: function(obj) {
+        // console.log('removeFromRemote_', foam.json.stringify(obj));
+        var self = this;
+        var ret;
+        return self.withSyncRecordTx_(function() {
+          return self.delegate.remove(obj).then(function(o) {
+            ret = o;
+            // console.log('removeFromRemote_: store syncRecord for', foam.json.stringify(obj));
+            return self.syncRecordDAO.put(self.SyncRecord.create({
+              id: obj.id,
+              syncNo: obj[self.syncProperty.name],
+              deleted: true
+            }));
+          });
+        }).then(function() { return ret; });
+      }
+    },
+    {
+      name: 'resetFromRemote_',
+      documentation: 'Process a reset signal on cache from remote.',
+      code: function(obj) {
+        // console.log('resetFromRemote_', foam.json.stringify(obj));
+        // Clear sync records and data not associated with unsynced data, then
+        // sync.
+        var self = this;
+        var ret;
+        return self.withSyncRecordTx_(function() {
+          return self.syncRecordDAO(self.GT(self.SyncRecord.SYNC_NO, -1))
+              .removeAll().
+              then(self.syncRecordDAO.select.bind(self.syncRecordDAO)).
+              then(function(sink) {
+                var idsToKeep = sink.array.map(function(syncRecord) {
+                  return syncRecord.id;
+                });
+                // console.log('resetFromRemote_: Keeping', idsToKeep,
+                //             'not yet pushed');
+                return self.delegate.where(
+                    self.NOT(self.IN(self.of.ID, idsToKeep))).
+                    removeAll();
+              });
+        }).then(self.sync.bind(self));
+      }
+    },
+    {
+      name: 'syncFromRemote_',
+      documentation: 'Pull data from remote and sync it to cache.',
+      code: function() {
+        // console.log('syncFromRemote_');
+        var self = this;
+        var SYNC_NO = self.SyncRecord.SYNC_NO;
+
+        return self.syncRecordDAO.
+            // Like MAX(), but faster on DAOs that can optimize order+limit.
+            orderBy(self.DESC(SYNC_NO)).limit(1).
+            select().then(function(sink) {
+              var value = sink.array[0] && SYNC_NO.f(sink.array[0]);
+              // console.log('syncFromRemote_: version >', value || 0);
+              return self.remoteDAO.
+                  where(self.GT(self.syncProperty, value || 0)).
+                  select().then(function(sink) {
+                    var array = sink.array;
+                    var promises = [];
+
+                    for ( var i = 0 ; i < array.length ; i++ ) {
+                      promises.push(self.putFromRemote_(array[i]));
+                    }
+
+                    return Promise.all(promises);
+                  });
+            });
+      }
+    },
+    {
+      name: 'syncToRemote_',
+      documentation: 'Push data from cach to remote.',
+      code: function() {
+        // console.log('syncToRemote_');
+        var self = this;
+
+        return this.syncRecordDAO
+          .where(self.EQ(this.SyncRecord.SYNC_NO, -1))
+          .select().then(function(records) {
+            records = records.array;
+            var promises = [];
+
+            for ( var i = 0 ; i < records.length ; i++ ) {
+              var record = records[i];
+              var id = record.id;
+              var deleted = record.deleted;
+
+              if ( deleted ) {
+                var obj = self.of.create(undefined, self);
+                obj.id = id;
+                promises.push(self.remoteDAO.remove(obj));
+              } else {
+                // TODO: Stop sending updates if the first one fails.
+                promises.push(self.delegate.find(id).then(function(obj) {
+                  if ( ! obj ) return null;
+                  var ret = self.remoteDAO.put(obj);
+
+                  // When not polling, server result will processed when
+                  // onRemoteUpdate listener is fired.
+                  if ( self.polling ) {
+                    ret = ret.then(function(obj) {
+                      self.putFromRemote_(obj);
+                    });
+                  }
+
+                  return ret;
+                }));
+              }
+            }
+
+            return Promise.all(promises);
+          });
+      }
+    },
+    {
+      name: 'withSyncRecordTx_',
+      documentation: `Run a computation that writes to syncRecordDAO
+          transactionally with respect to other syncRecordDAO writes.`,
+      code: function(f) {
+        return this.syncRecordWriteSync_ = this.syncRecordWriteSync_.then(f);
+      }
     }
   ],
 
   listeners: [
-    /** @private */
-    function onRemoteUpdate(s, on, event, obj) {
-      if ( event == 'put' ) {
-        this.processFromServer(obj);
-      } else if ( event === 'remove' ) {
-        this.delegate.remove(obj);
-      } else if ( event === 'reset' ) {
-        this.delegate.removeAll();
+    {
+      name: 'onRemoteUpdate',
+      documentation: 'Respond to push event from remote.',
+      code: function(s, on, event, obj) {
+        if ( event == 'put' ) {
+          this.putFromRemote_(obj);
+        } else if ( event === 'remove' ) {
+          this.removeFromRemote_(obj);
+        } else if ( event === 'reset' ) {
+          this.resetFromRemote_();
+        }
       }
     },
-
     {
-      /** @private */
       name: 'onLocalUpdate',
       isMerged: true,
       mergeDelay: 100,
