4db23f331a92258790ae810edec1584730597b15
Kevin Greer <kgrgreer@gmail.com>
2017-07-31 08:51:34 -0400

    Add runFailedTests action to TestBorder.

--- src/foam/nanos/test/TestBorder.js
+++ src/foam/nanos/test/TestBorder.js
@@ -7,51 +7,62 @@
  foam.CLASS({
    package: 'foam.nanos.test',
    name: 'TestBorder',
-   extends: 'foam.u2.Controller',
+   extends: 'foam.u2.view.ScrollTableView',
+
+   implements: [ 'foam.mlang.Expressions' ],
+
+   requires: [ 'foam.nanos.test.Test' ],
 
    properties: [
      'status',
      { class: 'Int', name: 'passed' },
-     { class: 'Int', name: 'failed' },
-     { class: 'foam.dao.DAOProperty', name: 'data' }
+     { class: 'Int', name: 'failed' }
    ],
 
    methods: [
      function initE() {
-       this.SUPER();
-
        this.
-         add(this.RUN_ALL).
+         startContext({data: this}).add(this.RUN_ALL, this.RUN_FAILED_TESTS).endContext().
          start('span').style({'padding-left': '12px'}).add('Passed: ', this.passed$).end().
          start('span').style({'padding-left': '12px'}).add('Failed: ', this.failed$).end().
-         start('span').style({'padding-left': '12px'}).add('Status: ', this.status$).end().
-         tag({class: 'foam.u2.view.TableView', data: this.data});
-     }
+         start('span').style({'padding-left': '12px'}).add('Status: ', this.status$).end();
+
+      this.SUPER();
+    },
+
+    function runTests(dao) {
+      var self  = this;
+      var count = 0;
+      var startTime = Date.now();
+
+      this.status = 'Testing...';
+      this.passed = this.failed = 0;
+
+      dao.select({
+        put: function(t) {
+          count++;
+          self.status = 'Testing: ' + t.id;
+          t.run();
+          self.passed += t.passed;
+          self.failed += t.failed;
+          console.log(t.stringify());
+        },
+        eof: function() {
+          var duration = (Date.now() - startTime) / 1000;
+          self.status = count + ' tests run in ' + duration.toFixed(2) + ' seconds';
+        }
+      });
+    }
    ],
+
+
 
    actions: [
      function runAll() {
-       var self  = this;
-       var count = 0;
-       var startTime = Date.now();
-
-       this.status = 'Testing...';
-       this.passed = this.failed = 0;
-
-       this.data.select({
-         put: function(t) {
-           count++;
-           self.status = 'Testing: ' + t.id;
-           t.run();
-           self.passed += t.passed;
-           self.failed += t.failed;
-           console.log(t.stringify());
-         },
-         eof: function() {
-           var duration = Math.round((Date.now() - startTime) / 1000);
-           self.status = count + ' tests run in ' + duration + ' seconds';
-         }
-       });
-     }
+       this.runTests(this.data);
+     },
+     function runFailedTests() {
+       this.runTests(this.data.where(this.GT(this.Test.FAILED, 0)));
+     },
    ]
 });
