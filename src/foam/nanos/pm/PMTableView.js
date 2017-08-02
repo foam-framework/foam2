33b385cc04e37369cfb4a45e0c343459fc705e28
Kevin Greer <kgrgreer@gmail.com>
2017-08-02 22:10:48 +0200

    Set custom PMTableView for PMInfo menu.

---
+++ src/foam/nanos/pm/PMTableView.js
@@ -0,0 +1,49 @@
+/**
+ * @license
+ * Copyright 2017 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.CLASS({
+  package: 'foam.nanos.pm',
+  name: 'PMTableView',
+  extends: 'foam.u2.view.TableView',
+
+  documentation: 'TableView for displaying PMInfos.',
+
+  implements: [ 'foam.mlang.Expressions' ],
+
+  requires: [ 'foam.nanos.pm.PMInfo' ],
+
+  exports: [ 'maxTotalTime' ],
+
+  properties: [
+    {
+      class: 'Long',
+      name: 'maxTotalTime',
+      value: 10000
+    }
+  ],
+
+  methods: [
+    function initE() {
+      this.SUPER();
+
+      this.updateMax();
+      this.data$.sub(this.updateMax);
+    }
+  ],
+
+  listeners: [
+    {
+      name: 'updateMax',
+      isFramed: true,
+      code: function() {
+        var self = this;
+        this.data.select(this.MAX(this.PMInfo.TOTALTIME)).then(function(max) {
+          self.maxTotalTime = max.value;
+        });
+      }
+    }
+  ]
+ });
