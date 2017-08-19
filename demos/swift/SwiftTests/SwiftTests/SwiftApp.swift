9c6e1e5c771530dde4bba81dbb1188b5db337470
Mike Carcasole <mikecarcasole@gmail.com>
2017-08-18 21:09:04 -0400

    Cleaning up the swift demo app a little.

--- demos/swift/SwiftTests/SwiftTests/SwiftApp.swift
+++ demos/swift/SwiftTests/SwiftTests/SwiftApp.swift
@@ -4,33 +4,22 @@ class SwiftApp {
   lazy var data: Tabata = {
     return Context.GLOBAL.create(type: Tabata.self) as! Tabata
   }()
-  lazy var sound: TabataSoundView = {
-    return Context.GLOBAL.create(type: TabataSoundView.self, args: ["data": self.data]) as! TabataSoundView
-  }()
-  lazy var detailView: DetailView = {
+
+  lazy var navVc: UINavigationController = {
+
     let dv = Context.GLOBAL.create(type: DetailView.self, args: ["data": self.data]) as! DetailView
     dv.initAllViews()
-    return dv
-  }()
 
-  // TODO this shouldn't be necessary but we do this to hold onto a reference.
-  private lazy var slot: Slot = {
-    return self.data.timer$.dot("isStarted")
-  }()
-
-  lazy var vc: ScrollingViewController = {
     let vc = ScrollingViewController([
-      "view$": self.detailView.view$,
+      "view$": dv.view$,
       "title": self.data.ownClassInfo().label
-      ])
-    return vc
-  }()
-  lazy var navVc: UINavigationController = {
-    return UINavigationController(rootViewController: self.vc.vc)
+    ])
+
+    return UINavigationController(rootViewController: vc.vc)
   }()
   func startListeners() {
-    _ = self.sound
-    _ = slot.swiftSub { (_, _) in
+    _ = Context.GLOBAL.create(type: TabataSoundView.self, args: ["data": self.data])
+    _ = data.timer$.dot("isStarted").swiftSub { (_, _) in
       UIApplication.shared.isIdleTimerDisabled = self.data.timer.isStarted
     }
   }
