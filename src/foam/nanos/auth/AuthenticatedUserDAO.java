5ed2d0ae4005a05122501a0ab564621a8a86652b
MarcRoopchand <marc.roopchand@gmail.com>
2017-08-24 10:37:08 -0400

    Fixed PR comments

--- src/foam/nanos/auth/AuthenticatedUserDAO.java
+++ src/foam/nanos/auth/AuthenticatedUserDAO.java
@@ -19,42 +19,39 @@ public class AuthenticatedUserDAO
 
   @Override
   public FObject put_(X x, FObject fObject) {
-    if ( fObject instanceof User ) {
-      User user = (User) fObject;
+    User user = (User) fObject;
 
-      if ( super.find(user.getId()) != null ) {
-        System.out.println("A user has already been registered with this account");
-        return null;
-      }
-
-      AuthService service = (AuthService) x.get("auth");
-      if ( service == null ) {
-        System.out.println("Auth Service not started");
-        return null;
-      }
-
-      try {
-        service.validateUser(user);
-        Count count = (Count) this.limit(1).where(MLang.EQ(User.EMAIL, user.getEmail())).select(new Count());
-
-        if ( count.getValue() > 0 ) {
-          System.out.println("An account is already registered with this email address");
-          return null;
-        }
-
-        String salt = UserAndGroupAuthService.generateRandomSalt();
-        user.setPassword(UserAndGroupAuthService.hashPassword(user.getPassword(), salt) + ":" + salt);
-        return getDelegate().put_(x, user);
-      }
-      catch (RuntimeException e) {
-        e.printStackTrace();
-        return null;
-      }
-      catch (NoSuchAlgorithmException e) {
-        e.printStackTrace();
-        return null;
-      }
+    if ( super.find_(x, user.getId()) != null ) {
+      System.out.println("A user has already been registered with this account");
+      return null;
     }
-    return null;
+
+    AuthService service = (AuthService) x.get("auth");
+    if ( service == null ) {
+      System.out.println("Auth Service not started");
+      return null;
+    }
+
+    try {
+      service.validateUser(user);
+      Count count = (Count) this.limit(1).where(MLang.EQ(User.EMAIL, user.getEmail())).select(new Count());
+
+      if ( count.getValue() > 0 ) {
+        System.out.println("An account is already registered with this email address");
+        return null;
+      }
+
+      String salt = UserAndGroupAuthService.generateRandomSalt();
+      user.setPassword(UserAndGroupAuthService.hashPassword(user.getPassword(), salt) + ":" + salt);
+      return getDelegate().put_(x, user);
+    }
+    catch (RuntimeException e) {
+      e.printStackTrace();
+      return null;
+    }
+    catch (NoSuchAlgorithmException e) {
+      e.printStackTrace();
+      return null;
+    }
   }
 }
