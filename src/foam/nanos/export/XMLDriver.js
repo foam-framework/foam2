aafd37504ca4a620e60b9d86c8d49ba729fe3f51
Kirk Eaton <kirk@nanopay.net>
2017-08-31 14:09:02 -0400

    Added export function which exports a single object instead of all data in a dao

--- src/foam/nanos/export/XMLDriver.js
+++ src/foam/nanos/export/XMLDriver.js
@@ -4,27 +4,30 @@
  * http://www.apache.org/licenses/LICENSE-2.0
  */
 
- foam.CLASS({
-   package: 'foam.nanos.export',
-   name: 'XMLDriver',
-   implements: [ 'foam.nanos.export.ExportDriver' ],
+foam.CLASS({
+  package: 'foam.nanos.export',
+  name: 'XMLDriver',
+  implements: [ 'foam.nanos.export.ExportDriver' ],
 
-   documentation: 'Class for exporting data from a DAO to XML',
-   
-   properties: [
-     {
-       class: 'FObjectProperty',
-       of: 'foam.xml.Outputter',
-       name: 'outputter',
-       factory: function() { return foam.xml.Compact; }
-     }
-   ],
-   
-   methods: [
-     function exportDAO(X, dao) {
-       return dao.select().then(function (sink) {
-         return outputter.stringify(sink.array);
-       });
-     }
-   ]
- });
+  documentation: 'Class for exporting data from a DAO to XML',
+
+  properties: [
+    {
+      class: 'FObjectProperty',
+      of: 'foam.xml.Outputter',
+      name: 'outputter',
+      factory: function() { return foam.xml.Compact; }
+    }
+  ],
+
+  methods: [
+    function export(X, obj) {
+      return outputter.stringify(obj);
+    },
+    function exportDAO(X, dao) {
+      return dao.select().then(function (sink) {
+        return outputter.stringify(sink.array);
+      });
+    }
+  ]
+});
