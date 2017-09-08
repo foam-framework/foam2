488170c2c844eb747b724e9fb504d84a8f6e3398
Kirk Eaton <kirk@nanopay.net>
2017-09-08 16:41:02 -0400

    Added business information

--- src/foam/nanos/auth/User.js
+++ src/foam/nanos/auth/User.js
@@ -4,131 +4,147 @@
  * http://www.apache.org/licenses/LICENSE-2.0
  */
 
- foam.CLASS({
-   package: 'foam.nanos.auth',
-   name: 'User',
+foam.CLASS({
+  package: 'foam.nanos.auth',
+  name: 'User',
 
-   implements: [
-     'foam.nanos.auth.EnabledAware',
-     'foam.nanos.auth.LastModifiedAware',
-     'foam.nanos.auth.LastModifiedByAware'
-   ],
+  implements: [
+    'foam.nanos.auth.EnabledAware',
+    'foam.nanos.auth.LastModifiedAware',
+    'foam.nanos.auth.LastModifiedByAware'
+  ],
 
-   documentation: '',
+  documentation: '',
 
-   tableColumns: [
-     'id', 'enabled', 'firstName', 'lastName', 'organization', 'lastModified'
-   ],
+  tableColumns: [
+    'id', 'enabled', 'firstName', 'lastName', 'organization', 'lastModified'
+  ],
 
-   properties: [
-     {
-       class: 'Long',
-       name: 'id',
-       displayWidth: 30,
-       width: 100
-     },
-     {
-       class: 'String',
-       // class: 'SPID',
-       label: 'Service Provider',
-       name: 'spid',
-       documentation: "User's service provider."
-     },
-     {
-       class: 'DateTime',
-       name: 'lastLogin'
-     },
-     {
-       class: 'String',
-       name: 'firstName'
-     },
-     {
-       class: 'String',
-       name: 'middleName'
-     },
-     {
-       class: 'String',
-       name: 'lastName'
-     },
-     {
-       class: 'String',
-       name: 'organization'
-     },
-     {
-       class: 'String',
-       name: 'department'
-     },
-     {
-       class: 'String',
-       // class: 'Email',
-       name: 'email'
-     },
-     {
-       class: 'String',
-       // class: 'Phone',
-       name: 'phone'
-     },
-     {
-       class: 'String',
-       // class: 'Phone',
-       name: 'mobile'
-     },
-     {
-       class: 'String',
-       name: 'type'
-     },
-     {
-       class: 'DateTime',
-       name: 'birthday'
-     },
-     {
-       class: 'String',
-       name: 'profilePicture'
-     },
-     {
+  properties: [
+    {
+      class: 'Long',
+      name: 'id',
+      displayWidth: 30,
+      width: 100
+    },
+    {
+      class: 'String',
+      // class: 'SPID',
+      label: 'Service Provider',
+      name: 'spid',
+      documentation: "User's service provider."
+    },
+    {
+      class: 'DateTime',
+      name: 'lastLogin'
+    },
+    {
+      class: 'String',
+      name: 'firstName'
+    },
+    {
+      class: 'String',
+      name: 'middleName'
+    },
+    {
+      class: 'String',
+      name: 'lastName'
+    },
+    {
+      class: 'String',
+      name: 'organization'
+    },
+    {
+      class: 'String',
+      name: 'department'
+    },
+    {
+      class: 'String',
+      // class: 'Email',
+      name: 'email'
+    },
+    {
+      class: 'String',
+      // class: 'Phone',
+      name: 'phone'
+    },
+    {
+      class: 'String',
+      // class: 'Phone',
+      name: 'mobile'
+    },
+    {
+      class: 'String',
+      name: 'type'
+    },
+    {
+      class: 'DateTime',
+      name: 'birthday'
+    },
+    {
+      class: 'String',
+      name: 'profilePicture'
+    },
+    {
       class: 'FObjectProperty',
       of: 'foam.nanos.auth.Address',
       name: 'address'
-     },
-     {
+    },
+    {
       class: 'FObjectArray',
       of: 'foam.core.FObject',
       name: 'accounts'
-     },
-     {
-       class: 'Reference',
-       name: 'language',
-       of: 'foam.nanos.auth.Language',
-       value: 'en'
-     },
-     {
-       class: 'String',
-       name: 'timeZone'
-       // TODO: create custom view or DAO
-     },
-     {
-       class: 'Password',
-       name: 'password',
-       displayWidth: 30,
-       width: 100
-     },
-     {
-       class: 'Password',
-       name: 'previousPassword',
-       displayWidth: 30,
-       width: 100
-     },
-     {
-       class: 'DateTime',
-       name: 'passwordLastModified'
-     },
-     // TODO: startDate, endDate,
-     // TODO: do we want to replace 'note' with a simple ticket system?
-     {
-       class: 'String',
-       name: 'note',
-       displayWidth: 70,
-       displayHeight: 10
-     }
-   ]
- });
+    },
+    {
+      class: 'Reference',
+      name: 'language',
+      of: 'foam.nanos.auth.Language',
+      value: 'en'
+    },
+    {
+      class: 'String',
+      name: 'timeZone'
+      // TODO: create custom view or DAO
+    },
+    {
+      class: 'Password',
+      name: 'password',
+      displayWidth: 30,
+      width: 100
+    },
+    {
+      class: 'Password',
+      name: 'previousPassword',
+      displayWidth: 30,
+      width: 100
+    },
+    {
+      class: 'DateTime',
+      name: 'passwordLastModified'
+    },
+    // TODO: startDate, endDate,
+    // TODO: do we want to replace 'note' with a simple ticket system?
+    {
+      class: 'String',
+      name: 'note',
+      displayWidth: 70,
+      displayHeight: 10
+    },
+    // TODO: remove after demo
+    {
+      class: 'String',
+      name: 'businessName',
+      documentation: 'Name of the business'
+    },
+    {
+      class: 'String',
+      name: 'businessIdentificationNumber',
+      name: 'Business Identification Number (BIN)'
+    },
+    {
+      class: 'String',
+      name: 'bankIdentificationCode',
+      documentation: 'Bank Identification Code (BIC)'
+    }
+  ]
+});
