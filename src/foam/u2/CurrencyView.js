7042d8d9cb8ec346935991f4cc837ff1a72f3844
blakegreen <blake@nanopay.net>
2017-09-11 15:15:19 -0400

    Setup CurrencyView.js

---
+++ src/foam/u2/CurrencyView.js
@@ -0,0 +1,80 @@
+/**
+ * @license
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+foam.CLASS({
+  package: 'foam.u2',
+  name: 'CurrencyView',
+  extends: 'foam.u2.FloatView',
+
+  documentation: 'View for formatting cents into dollars.',
+
+  axioms: [
+    foam.u2.CSS.create({
+      code: '^:read-only { border: none; background: rgba(0,0,0,0); }'
+    })
+  ],
+
+  properties: [
+    [ 'type', 'number' ],
+    { class: 'Float', name: 'data' },
+    'precision',
+    'min',
+    'max'
+  ],
+
+  methods: [
+    function initE() {
+      this.SUPER();
+      this.addClass(this.myClass());
+      if ( this.min != undefined ) this.setAttribute('min', this.min);
+      if ( this.max != undefined ) this.setAttribute('max', this.max);
+    },
+
+    function link() {
+      this.attrSlot(null, this.onKey ? 'input' : null).relateFrom(
+          this.data$,
+          this.textToData.bind(this),
+          this.dataToText.bind(this));
+    },
+
+    function fromProperty(p) {
+      this.SUPER(p);
+
+      this.precision = p.precision;
+      this.min       = p.min;
+      this.max       = p.max;
+    },
+
+    function formatNumber(val) {
+      if ( ! val ) return '0';
+      val = val.toFixed(this.precision);
+      var i = val.length - 1;
+      for ( ; i > 0 && val.charAt(i) === '0' ; i-- ) {}
+      return val.substring(0, val.charAt(i) === '.' ? i : i + 1);
+    },
+
+    function dataToText(val) {
+      return this.precision !== undefined ?
+        this.formatNumber(val / 100) :
+        '' + val ;
+    },
+
+    function textToData(text) {
+      return parseFloat(text) * 100 || 0;
+    }
+  ]
+});
