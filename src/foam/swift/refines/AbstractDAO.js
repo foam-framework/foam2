848dadc87c5386950101f3bd94c18da2ee0d1e9f
Mike Carcasole <mikecarcasole@gmail.com>
2017-09-23 08:50:53 -0400

    Forgot to add a file from prev commit.

---
+++ src/foam/swift/refines/AbstractDAO.js
@@ -0,0 +1,11 @@
+foam.CLASS({
+  refines: 'foam.dao.AbstractDAO',
+  methods: [
+    // Here to finish implementing the interface.
+    { name: 'select_' },
+    { name: 'put_' },
+    { name: 'remove_' },
+    { name: 'find_' },
+    { name: 'removeAll_' },
+  ],
+})
