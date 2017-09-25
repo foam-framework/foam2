f06e2c6fa9ccddb14f3f9a694ff7101eb0240f9f
Kirk Eaton <kirk@nanopay.net>
2017-09-25 11:50:25 -0400

    Removed SQLType

--- src/files.js
+++ src/files.js
@@ -169,7 +169,6 @@ FOAM_FILES([
   { name: "foam/dao/LastModifiedAwareDAO" },
   { name: "foam/dao/ValidationDAODecorator" },
   { name: "foam/dao/SQL", flags: ['sql'] },
-  { name: "foam/dao/SQLType", flags: ['sql'] },
   { name: "foam/dao/NoDisjunctionDAO" },
   { name: "foam/dao/NoNeqDAO" },
   { name: "foam/parse/QueryParser" },
