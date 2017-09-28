e951c14d75342cf8ba12868cc2342ad1c3d42ae9
Kirk Eaton <kirk@nanopay.net>
2017-09-28 11:27:19 -0400

    Modified JDAO to chop first two characters and last character off before passing it along to JournalParser. Modified ErrorReportingPStream to add a space between valid characters

--- src/foam/lib/parse/ErrorReportingPStream.java
+++ src/foam/lib/parse/ErrorReportingPStream.java
@@ -77,6 +77,6 @@ public class ErrorReportingPStream
         "Valid characters include: " +
         validCharacters.stream()
             .map(e -> "'" + e.toString() + "'")
-            .collect(Collectors.joining(","));
+            .collect(Collectors.joining(", "));
   }
 }
