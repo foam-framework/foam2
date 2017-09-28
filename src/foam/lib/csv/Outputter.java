1726d92624549c075e70865964812aed204f224d
Kirk Eaton <kirk@nanopay.net>
2017-09-28 16:41:08 -0400

    Added CSV output functions for different types'

--- src/foam/lib/csv/Outputter.java
+++ src/foam/lib/csv/Outputter.java
@@ -6,9 +6,51 @@
 
 package foam.lib.csv;
 
+import foam.core.FObject;
+
+import java.util.Date;
+
 public class Outputter {
 
-  public void output(StringBuilder out, Object value) {
+  public String stringify(FObject obj) {
+    StringBuilder sb = new StringBuilder();
+    outputFObject(sb, obj);
+    return sb.toString();
+  }
 
+  public String escape(String s) {
+    return s.replace("\n","\\n").replace("\"", "\\\"");
+  }
+
+  protected void outputString(StringBuilder out, String s) {
+    out.append(escape(s));
+  }
+
+  protected void outputNumber(StringBuilder out, Number value) {
+    out.append(value.toString());
+  }
+
+  protected void outputBoolean(StringBuilder out, Boolean value) {
+    out.append(value ? "true" : "false");
+  }
+
+  protected void outputDate(StringBuilder out, Date value) {
+
+  }
+
+  protected void outputFObject(StringBuilder out, FObject o) {
+
+  }
+
+  public void output( StringBuilder out, Object value ) {
+    if ( value instanceof String ) {
+      outputString(out, (String) value);
+    } else if ( value instanceof Number ) {
+      outputNumber(out, (Number) value);
+    } else if ( value instanceof Boolean ) {
+      outputBoolean(out, (Boolean) value);
+    } else if ( value instanceof Date ) {
+      outputDate(out, (Date) value);
+    }
   }
 }
