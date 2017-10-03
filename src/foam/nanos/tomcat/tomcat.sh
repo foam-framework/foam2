4785d4a80d11db60c84255ad70d4694b32593315
MarcRoopchand <marc.roopchand@gmail.com>
2017-10-03 14:04:10 -0400

    Working tomcat server on AWS

--- src/foam/nanos/tomcat/tomcat.sh
+++ src/foam/nanos/tomcat/tomcat.sh
@@ -1,5 +1,5 @@
 /$CATALINA_HOME/bin/shutdown.sh
-rm -rf ../../../../build/
+#rm -rf ../../../../build/
 cd ../../../
 ./gen.sh
 cd ../build/
@@ -7,5 +7,14 @@ mvn clean package
 cd target
 mv foam-1.0-SNAPSHOT.war ROOT.war
 cp ROOT.war $CATALINA_HOME/webapps/
+
+
+
+#cd ../../src/
+#cp foam.js $CATALINA_HOME/bin/foam2/
+#cp foam/nanos/nanos.js $CATALINA_HOME/bin/foam2/
+#cp foam/u2/ListCreateController.js $CATALINA_HOME/bin/foam2/
+#cp -r ../../NANOPAY/retail/ $CATALINA_HOME/bin/foam2/
+
 cd $CATALINA_HOME/bin/
 ./startup.sh
