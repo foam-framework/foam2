7786ebc45501c92448a374c39a21474fcea93ec7
Kevin Greer <kgrgreer@gmail.com>
2017-10-05 17:13:20 -0400

    Remove fake id from PMInfo.

--- src/foam/nanos/pm/PMInfo.js
+++ src/foam/nanos/pm/PMInfo.js
@@ -11,15 +11,11 @@ foam.CLASS({
 
   documentation: 'Performance Measurement database entry.',
 
-//  ids: [ 'clsname', 'pmname' ],
+  ids: [ 'clsname', 'pmname' ],
 
   searchColumns: [ ],
 
   properties: [
-    {
-      class: 'Long',
-      name: 'id'
-    },
     {
       class: 'String',
       name: 'clsname',
