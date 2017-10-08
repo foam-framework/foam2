3a0aa54d2e9e6ff3e888df1507fb71eb0da97b86
Mike Carcasole <mikecarcasole@gmail.com>
2017-10-08 11:41:38 -0400

    Create a genswift tool and use that in the demo.

--- demos/swift/gen_swift.js
+++ demos/swift/gen_swift.js
@@ -1,58 +1,21 @@
 #!/usr/bin/env node
 
-global.FOAM_FLAGS = {
-  'node': true,
-  'swift': true,
-  'debug': true,
-};
-
-var execSync = require('child_process').execSync
-
 var dir = __dirname;
 var root = dir + '/../..';
-var genDir = dir + '/gen';
 
-require(root + '/src/foam.js');
-execSync('rm -rf ' + genDir);
+var classpaths = [
+  dir + '/../../src',
+  dir + '/js',
+  dir + '/../../src/com/google/foam/demos/tabata',
+].join(',')
 
-var executor = foam.classloader.NodeJsModelExecutor.create({
-  classpaths: [
-    dir + '/../../src',
-    dir + '/js',
-    dir + '/../../src/com/google/foam/demos/tabata',
-  ],
-  modelId: 'foam.swift.GenSwift',
-  modelArgs: {
-    models: [
-      // UI
-      'foam.swift.ui.DetailView',
-      'foam.swift.ui.FOAMUILabel',
-      'foam.swift.ui.FOAMUITextField',
-      'foam.swift.ui.FOAMUITextFieldInt',
-      'foam.swift.ui.PropertyView',
-      'foam.swift.ui.ScrollingViewController',
-      'foam.swift.parse.json.output.HTTPBoxOutputter',
-      'foam.u2.Visibility',
+var command = [
+  'node',
+  root + '/tools/genswift.js',
+  dir + '/classes.js',
+  dir + '/gen',
+  classpaths,
+].join(' ');
 
-      'Tabata',
-      'TestExtended',
-      'TabataSoundView',
-      'foam.swift.dao.ArrayDAO',
-      'foam.swift.dao.ArraySink',
-      'foam.swift.parse.StringPStream',
-
-      'foam.box.Context',
-      'foam.box.Box',
-      'foam.box.HTTPBox',
-      'foam.box.BoxService',
-      'foam.box.BoxRegistry',
-      'foam.box.ReplyBox',
-      'foam.box.ClientBoxRegistry',
-      'foam.swift.box.RPCReturnBox',
-      'foam.box.RPCMessage',
-      'foam.box.Message',
-    ],
-    outdir: genDir,
-  },
-});
-executor.execute();
+console.log(command);
+require('child_process').execSync(command, {stdio:[0,1,2]});
