3ea84698ebf02d6eda67d12e77b7a9db8103503c
Adam Van Ymeren <adam@vany.ca>
2017-10-11 18:57:45 -0400

    Add support for parsing enums of the form { class: "EnumClass", ordinal: 12 }
    Skip unknown properties when parsing fobjects, rather than failing.

---
+++ src/foam/lib/json/KeyValueParser0.java
@@ -0,0 +1,21 @@
+/**
+  * @license
+  * Copyright 2017 The FOAM Authors. All Rights Reserved.
+  * http://www.apache.org/licenses/LICENSE-2.0
+  */
+
+package foam.lib.json;
+
+import foam.lib.parse.*;
+
+public class KeyValueParser0
+  extends ProxyParser
+{
+  public KeyValueParser0() {
+    super(new Seq0(new Whitespace(),
+                   new AnyKeyParser(),
+                   new Whitespace(),
+                   new Literal(":"),
+                   new AnyParser()));
+  }
+}
