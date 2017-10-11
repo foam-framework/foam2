8cf294f93774fb3621a8390d8542ede839ea3580
Adam Van Ymeren <adam@vany.ca>
2017-10-11 15:46:27 -0400

    Move WebSocket and WebSocketService into their own files.

---
+++ src/foam/net/web/WebSocketService.js
@@ -0,0 +1,70 @@
+/**
+ * @license
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ * Copyright 2017 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.CLASS({
+  package: 'foam.net.web',
+  name: 'WebSocketService',
+
+  requires: [
+    'foam.box.Message',
+    'foam.box.RegisterSelfMessage',
+    'foam.json.Parser',
+    'foam.net.web.WebSocket',
+    'foam.box.RawWebSocketBox'
+  ],
+  imports: [ 'creationContext' ],
+
+  properties: [
+    {
+      name: 'delegate'
+    },
+    {
+      class: 'FObjectProperty',
+      of: 'foam.json.Parser',
+      name: 'parser',
+      factory: function() {
+        return this.Parser.create({
+          strict: true,
+          creationContext: this.creationContext
+        });
+      }
+    }
+  ],
+
+  methods: [
+    function addSocket(socket) {
+      var X = this.creationContext.createSubContext({
+        returnBox: this.RawWebSocketBox.create({ socket: socket })
+      });
+
+      var sub1 = socket.message.sub(function onMessage(s, _, msgStr) {
+        var msg = this.parser.parseString(msgStr, X);
+
+        if ( ! this.Message.isInstance(msg) ) {
+          console.warn('Got non-message:', msg, msgStr);
+        }
+
+/*        if ( this.RegisterSelfMessage.isInstance(msg.object) ) {
+          var named = foam.box.NamedBox.create({
+            name: msg.object.name
+          });
+
+          named.delegate = foam.box.RawWebSocketBox.create({
+            socket: socket
+          });
+        } else {*/
+          this.delegate.send(msg);
+//        }
+      }.bind(this));
+
+      socket.disconnected.sub(function(s) {
+        s.detach();
+        sub1.detach();
+      });
+    }
+  ]
+});
