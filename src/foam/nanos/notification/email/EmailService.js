1843311ec4cbc2101babe1669d27133acaea8ff5
Kirk Eaton <kirk@nanopay.net>
2017-10-13 11:23:43 -0400

    Added sendEmailFromTemplate function to EmailService interface. Added createMimeMessage function to SMTPEmailService

--- src/foam/nanos/notification/email/EmailService.js
+++ src/foam/nanos/notification/email/EmailService.js
@@ -1,3 +1,9 @@
+/**
+ * @license
+ * Copyright 2017 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 foam.INTERFACE({
   package: 'foam.nanos.notification.email',
   name: 'EmailService',
@@ -15,6 +21,28 @@ foam.INTERFACE({
         {
           name: 'emailMessage',
           javaType: 'foam.nanos.notification.email.EmailMessage'
+        }
+      ]
+    },
+    {
+      name: 'sendEmailFromTemplate',
+      javaReturns: 'void',
+      returns: 'Promise',
+      javaThrows: [
+        'javax.mail.MessagingException'
+      ],
+      args: [
+        {
+          name: 'emailMessage',
+          javaType: 'foam.nanos.notification.email.EmailMessage'
+        },
+        {
+          name: 'template',
+          javaType: 'String'
+        },
+        {
+          name: 'args',
+          javaType: 'Object[]'
         }
       ]
     }
--- src/foam/nanos/notification/email/SMTPEmailService.js
+++ src/foam/nanos/notification/email/SMTPEmailService.js
@@ -1,3 +1,9 @@
+/**
+ * @license
+ * Copyright 2017 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 foam.CLASS({
   package: 'foam.nanos.notification.email',
   name: 'SMTPEmailService',
@@ -45,10 +51,16 @@ foam.CLASS({
 
   methods: [
     {
-      name: 'sendEmail',
-      javaReturns: 'void',
+      name: 'createMimeMessage',
+      javaReturns: 'MimeMessage',
       javaThrows: [
         'javax.mail.MessagingException'
+      ],
+      args: [
+        {
+          name: 'emailMessage',
+          javaType: 'foam.nanos.notification.email.EmailMessage'
+        }
       ],
       javaCode:
 `MimeMessage message = new MimeMessage(getSession());
@@ -56,40 +68,40 @@ foam.CLASS({
 // don't send email if no sender
 String from = emailMessage.getFrom();
 if ( from == null || from.isEmpty() )
-  return;
+  return null;
 message.setFrom(new InternetAddress(from));
 
 // don't send email if no subject
 String subject = emailMessage.getSubject();
 if ( subject == null || subject.isEmpty() )
-  return;
+  return null;
 message.setSubject(subject);
 
 // don't send email if no body
 String body = emailMessage.getBody();
 if ( body == null || body.isEmpty() )
-  return;
+  return null;
 message.setContent(body, "text/html; charset=utf-8");
 
 // don't send email if no recipient
 String[] to = emailMessage.getTo();
 if ( to == null || to.length <= 0 )
-  return;
+  return null;
 
 if ( to.length == 1 ) {
   message.setRecipient(Message.RecipientType.TO, new InternetAddress(to[0], false));
 } else {
   message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(
-    Arrays.stream(to).collect(Collectors.joining(",")), false));
+      Arrays.stream(to).collect(Collectors.joining(",")), false));
 }
 
-// send email even if no CC
-String[] cc = emailMessage.getCc();
-if ( cc != null && cc.length == 1 ) {
-  message.setRecipient(Message.RecipientType.CC, new InternetAddress(cc[0], false));
+ // send email even if no CC
+ String[] cc = emailMessage.getCc();
+ if ( cc != null && cc.length == 1 ) {
+   message.setRecipient(Message.RecipientType.CC, new InternetAddress(cc[0], false));
 } else if ( cc != null && cc.length > 1 ) {
   message.setRecipients(Message.RecipientType.CC, InternetAddress.parse(
-    Arrays.stream(cc).collect(Collectors.joining(",")), false));
+      Arrays.stream(cc).collect(Collectors.joining(",")), false));
 }
 
 // send email even if no BCC
@@ -98,17 +110,38 @@ if ( bcc != null && bcc.length == 1 ) {
   message.setRecipient(Message.RecipientType.BCC, new InternetAddress(bcc[0], false));
 } else if ( bcc != null && bcc.length > 1 ) {
   message.setRecipients(Message.RecipientType.BCC, InternetAddress.parse(
-    Arrays.stream(bcc).collect(Collectors.joining(",")), false));
+      Arrays.stream(bcc).collect(Collectors.joining(",")), false));
 }
 
 // set date
 message.setSentDate(new Date());
+return message;`
+    },
+    {
+      name: 'sendEmail',
+      javaReturns: 'void',
+      javaThrows: [
+        'javax.mail.MessagingException'
+      ],
+      javaCode:
+`MimeMessage message = createMimeMessage(emailMessage);
+if ( message == null )
+  return;
 
 // send message
 Transport transport = getSession().getTransport("smtp");
 transport.connect(getHost(), getUsername(), getPassword());
 transport.sendMessage(message, message.getAllRecipients());
 transport.close();`
+    },
+    {
+      name: 'sendEmailFromTemplate',
+      javaReturns: 'void',
+      javaThrows: [
+        'javax.mail.MessagingException'
+      ],
+      javaCode:
+`return;`,
     },
     {
       name: 'start',
