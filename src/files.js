8c0a55ece7eaf264e7d51460721d9815e86170c2
Patrick <zahnno@gmail.com>
2017-10-25 14:43:44 -0400

    FObjectArrayView

--- src/files.js
+++ src/files.js
@@ -303,6 +303,7 @@ FOAM_FILES([
   { name: "foam/u2/history/HistoryItemView", flags: ['web'] },
   { name: "foam/u2/history/HistoryView", flags: ['web'] },
   { name: "foam/u2/view/FObjectView", flags: ['web'] },
+  { name: "foam/u2/view/FObjectArrayView", flags: ['web'] },  
   { name: "foam/u2/view/ChoiceView", flags: ['web'] },
   { name: "foam/u2/view/RadioView", flags: ['web'] },
   { name: "foam/u2/view/TextField", flags: ['web'] },
