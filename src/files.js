cc907474f63f7f2cd057c4eaa34bd7aeb85d42f8
Adam Van Ymeren <adam@vany.ca>
2017-10-25 15:43:33 -0400

    Add ImageBlobView.

--- src/files.js
+++ src/files.js
@@ -317,6 +317,7 @@ FOAM_FILES([
   { name: "foam/u2/view/ScrollTableView", flags: ['web'] },
   { name: "foam/u2/view/ScrollDAOView", flags: ['web'] },
   { name: "foam/u2/view/BlobView", flags: ['web'] },
+  { name: "foam/u2/view/ImageBlobView", flags: ['web'] },
   { name: "foam/u2/view/StringArrayView", flags: ['web'] },
   { name: "foam/u2/view/ImageView", flags: ['web'] },
   { name: "foam/u2/EnumView", flags: ['web'] },
