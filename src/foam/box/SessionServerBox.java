b3dea19689e772cd19100eb0fab0519897ca571f
Kevin Greer <kgrgreer@gmail.com>
2017-10-26 14:11:11 -0400

    Session support.

--- src/foam/box/SessionServerBox.java
+++ src/foam/box/SessionServerBox.java
@@ -7,19 +7,49 @@
 package foam.box;
 
 import foam.core.*;
+import foam.dao.*;
+import foam.nanos.session.Session;
+import java.util.Date;
+import javax.servlet.http.HttpServletRequest;
+
 
 public class SessionServerBox
   extends ProxyBox
 {
 
-  // protected Map map_ = new LRULinkedHash
   public SessionServerBox(X x, Box delegate) {
     super(x, delegate);
+    System.err.println("*** creating session server box");
   }
 
   public void send(Message msg) {
     String sessionID = (String) msg.getAttributes().get("sessionId");
-    System.err.println("**** SESSIONID: " + sessionID);
+
+    try {
+
+      if ( sessionID != null ) {
+        System.err.println("**** SESSIONID: " + sessionID);
+
+        DAO     dao     = (DAO) getX().get("sessionDAO");
+        Session session = (Session) dao.find(sessionID);
+
+        if ( session == null ) {
+          session = new Session();
+          session.setId(sessionID);
+
+          HttpServletRequest req = (HttpServletRequest) getX().get(HttpServletRequest.class);
+          session.setRemoteHost(req.getRemoteHost());
+        }
+
+        session.setLastUsed(new Date());
+        session.setUses(session.getUses()+1);
+
+        dao.put(session);
+      }
+    } catch (Throwable t) {
+      t.printStackTrace();
+    }
+
     getDelegate().send(msg);
   }
 }
