8d289291d9af3c512a53922f17dbe2d7fad72eac
Kirk Eaton <kirk@nanopay.net>
2017-10-27 10:24:28 -0400

    Modelled BlobStore

--- src/foam/blob/BlobJava.js
+++ src/foam/blob/BlobJava.js
@@ -210,3 +210,151 @@ return getParent().read(buffer, offset + getOffset());`
     }
   ]
 });
+
+foam.CLASS({
+  refines: 'foam.blob.BlobStore',
+
+  javaImports: [
+    'foam.core.X',
+    'org.apache.geronimo.mail.util.Hex',
+    'java.io.File',
+    'java.io.FileOutputStream',
+    'java.nio.ByteBuffer',
+    'java.security.MessageDigest'
+  ],
+
+  constants: [
+    {
+      name: 'BUFFER_SIZE',
+      value: 8192,
+      type: 'int'
+    }
+  ],
+
+  methods: [
+    {
+      name: 'put_',
+      javaCode:
+`if ( blob instanceof IdentifiedBlob ) {
+  return blob;
+}
+
+this.setup();
+
+try {
+  MessageDigest hash = MessageDigest.getInstance("SHA-256");
+  Buffer buffer = new Buffer(BUFFER_SIZE, ByteBuffer.allocate(BUFFER_SIZE));
+
+  long chunk = 0;
+  long size = blob.getSize();
+  long remaining = size;
+  long chunks = (long) Math.ceil((double) size / (double) BUFFER_SIZE);
+
+  File tmp = allocateTmp(1);
+  while (chunk < chunks) {
+    buffer = blob.read(buffer, chunkOffset(chunk));
+    byte[] buf = buffer.getData().array();
+    hash.update(buf);
+    FileOutputStream os = new FileOutputStream(tmp);
+    os.write(buf, 0, buf.length);
+    os.close();
+    buffer.getData().clear();
+    chunk++;
+  }
+
+  String digest = new String(Hex.encode(hash.digest()));
+  File dest = new File(sha256_ + File.separator + digest);
+  tmp.renameTo(dest);
+
+  IdentifiedBlob result = new IdentifiedBlob();
+  result.setId(digest);
+  result.setX(getX());
+  return result;
+} catch (Throwable t) {
+  t.printStackTrace();
+  return null;
+}`
+    },
+    {
+      name: 'find_',
+      javaCode:
+`this.setup();
+if ( ((String) id).indexOf(File.separatorChar) != -1 ) {
+  throw new RuntimeException("Invalid file name");
+}
+
+File file = new File(sha256_ + File.separator + id);
+if ( ! file.exists() ) {
+  throw new RuntimeException("File does not exist");
+}
+
+if ( ! file.canRead() ) {
+  throw new RuntimeException("Cannot read file");
+}
+
+return new FdBlob(file);`
+    },
+    {
+      name: 'urlFor_',
+      javaCode: 'throw new UnsupportedOperationException("Unsupported operation: urlFor_");'
+    },
+    {
+      name: 'setup',
+      javaReturns: 'void',
+      javaCode:
+`if ( this.getIsSet() )
+  return;
+ensureDir(getRoot());
+ensureDir(getTmp());
+ensureDir(getSha256());
+setIsSet(true);`
+    },
+    {
+      name: 'ensureDir',
+      javaReturns: 'void',
+      args: [
+        {
+          name: 'path',
+          javaType: 'String'
+        }
+      ],
+      javaCode:
+`File parsed = new File(path);
+if ( parsed.exists() && parsed.isDirectory() ) {
+  return;
+}
+
+if ( ! parsed.mkdirs() ) {
+  throw new RuntimeException("Failed to create: " + path);
+}`
+    },
+    {
+      name: 'allocateTmp',
+      javaReturns: 'File',
+      args: [
+        {
+          name: 'name',
+          javaType: 'long'
+        }
+      ],
+      javaCode:
+`String path = this.tmp_ + File.separator + (name++);
+File file = new File(path);
+if ( file.exists() ) {
+  return allocateTmp(name);
+}
+return file;`
+    },
+    {
+      name: 'chunkOffset',
+      javaReturns: 'long',
+      args: [
+        {
+          name: 'i',
+          javaType: 'long'
+        }
+      ],
+      javaCode: 'return i * BUFFER_SIZE;'
+    }
+  ]
+});
