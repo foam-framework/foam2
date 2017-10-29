48c83ba31ed59d9fa6eb0873edf336109ec125e9
Mike Carcasole <mikecarcasole@gmail.com>
2017-10-28 23:12:34 -0400

    Update the demo app to use the DAOViewController.

--- demos/swift/SwiftTests/SwiftTests/AppDelegate.swift
+++ demos/swift/SwiftTests/SwiftTests/AppDelegate.swift
@@ -5,14 +5,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
 
-  let app = SwiftApp()
+  let app = Context.GLOBAL.create(TestApp.self)!
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
     window = UIWindow(frame: UIScreen.main.bounds)
     window?.backgroundColor = .white
-    window?.rootViewController = app.navVc
+    window?.rootViewController = app.stack
     window?.makeKeyAndVisible()
 
     return true
   }
+
 }
