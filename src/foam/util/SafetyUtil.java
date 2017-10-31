cfc811c867e39e38ce4e5fd53672100c62d4ba62
Waseem <waseem@nanopay.net>
2017-10-31 11:09:36 -0400

    cleaned up code

--- src/foam/util/SafetyUtil.java
+++ src/foam/util/SafetyUtil.java
@@ -5,31 +5,19 @@ import java.math.BigDecimal;
 
 public class SafetyUtil{
 
-  public static int compare(Object o1 , Object o2)
-  {
-    try {
-      if (o1 == null && o2 == null){    
-        return 0;
-      }
-      if (o1 instanceof Number && o2 instanceof Number) {
-        if (o1.toString() != "NaN" || o2.toString() != "NaN") {
-          int i = new BigDecimal(o1.toString()).compareTo(new BigDecimal(o2.toString()));
-          return i;
-        }
-      }
-      if (o1 instanceof String && o2 instanceof String) {
-        int i = ((String) o1).compareTo((String) o2);
-        return i;
-      }
-      return ((Comparable) o1).compareTo(o2);
-    }catch (Exception e){
-      e.printStackTrace();
-      return -99;
+  public static int compare(Object o1 , Object o2) {
+  
+    if (o1 == null && o2 == null) {
+      return 0;
     }
-  }
-  public static boolean equals(Object o1 , Object o2)
-  {
-    if (compare(o1,o2)!= 0 ) return false;
-    return true;
+    if (o1 instanceof Number && o2 instanceof Number) {
+      if (o1.toString() != "NaN" || o2.toString() != "NaN") {
+        return new BigDecimal(o1.toString()).compareTo(new BigDecimal(o2.toString()));
+      }
+    }
+    if (o1 instanceof String && o2 instanceof String) {
+      return ((String) o1).compareTo((String) o2);
+    }
+    return ((Comparable) o1).compareTo(o2);
   }
 }
