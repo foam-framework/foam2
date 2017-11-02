7f14fd85187a949470bcd2c40b65863dd4dc023b
Kevin Greer <kgrgreer@gmail.com>
2017-11-02 13:51:08 -0400

    Formatting.

--- src/foam/util/SafetyUtil.java
+++ src/foam/util/SafetyUtil.java
@@ -1,25 +1,26 @@
+/**
+ * @license
+ * Copyright 2017 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 package foam.util;
 
-
-import java.math.BigDecimal;
-
-public class SafetyUtil{
-
+public class SafetyUtil {
   public static int compare(Object o1 , Object o2) {
-    
     if ( o1 == null && o2 == null ) return 0;
     if ( o2 == null ) return 1;
     if ( o1 == null ) return -1;
 
-    if ( o1 instanceof Number && o2 instanceof Number ) {      
+    if ( o1 instanceof Number && o2 instanceof Number ) {
       double d1 = ( (Number) o1 ).doubleValue();
       double d2 = ( (Number) o2 ).doubleValue();
-     
-      if ( d1 == d2 ) return 0;
-      if ( d1 > d2 ) return 1;
-      if ( d1 < d2 ) return -1;
-     
+
+      if ( d1 == d2 ) return  0;
+      if ( d1  > d2 ) return  1;
+      if ( d1  < d2 ) return -1;
     }
-    return ( (Comparable) o1 ).compareTo(o2);
+
+    return ((Comparable) o1).compareTo(o2);
   }
 }
