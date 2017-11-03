8512e0b8ac27dba18ad49eb564fa54f5b31c916f
Kirk Eaton <kirk@nanopay.net>
2017-11-03 13:04:34 -0400

    Combined name and method into prefix variable. Added context to AuthenticatedSink constructor

--- src/foam/dao/AuthenticatedSink.java
+++ src/foam/dao/AuthenticatedSink.java
@@ -8,6 +8,7 @@ package foam.dao;
 
 import foam.core.Detachable;
 import foam.core.FObject;
+import foam.core.X;
 import foam.nanos.auth.AuthService;
 
 import javax.security.auth.AuthPermission;
@@ -16,19 +17,19 @@ import java.security.Permission;
 public class AuthenticatedSink
     extends ProxySink
 {
-  protected String name_;
+  protected String prefix_;
   protected String method_;
 
-  public AuthenticatedSink(String name, String method, Sink delegate) {
-    this.name_ = name;
-    this.method_ = method;
+  public AuthenticatedSink(X x, String name, String method, Sink delegate) {
+    setX(x);
     setDelegate(delegate);
+    this.prefix_ = name + "." + method + ".";
   }
 
   @Override
   public void put(FObject obj, Detachable sub) {
     AuthService authService = (AuthService) getX().get("auth");
-    Permission permission = new AuthPermission(name_ + "." + method_ + "." + obj.getProperty("id"));
+    Permission permission = new AuthPermission(prefix_ + obj.getProperty("id"));
     if ( authService.check(getX(), permission) ) {
       super.put(obj, sub);
     }
@@ -37,7 +38,7 @@ public class AuthenticatedSink
   @Override
   public void remove(FObject obj, Detachable sub) {
     AuthService authService = (AuthService) getX().get("auth");
-    Permission permission = new AuthPermission(name_ + "." + method_ + "." + obj.getProperty("id"));
+    Permission permission = new AuthPermission(prefix_ + obj.getProperty("id"));
     if ( authService.check(getX(), permission) ) {
       super.remove(obj, sub);
     }
