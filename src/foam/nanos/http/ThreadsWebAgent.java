54053725dcafe39b5da2ca6a716bba6873c9a702
Kevin Greer <kgrgreer@gmail.com>
2017-11-08 15:26:27 -0500

    Formatting.

--- src/foam/nanos/http/ThreadsWebAgent.java
+++ src/foam/nanos/http/ThreadsWebAgent.java
@@ -7,11 +7,11 @@
 package foam.nanos.http;
 
 import foam.core.*;
-import java.lang.Thread;
+import java.io.PrintWriter;
 import java.lang.StackTraceElement;
 import java.lang.String;
+import java.lang.Thread;
 import java.util.Set;
-import java.io.PrintWriter;
 import javax.servlet.http.HttpServletRequest;
 
 public class ThreadsWebAgent
@@ -20,41 +20,40 @@ public class ThreadsWebAgent
   public ThreadsWebAgent() {}
 
   public void execute(X x) {
-    final PrintWriter out  = (PrintWriter) x.get(PrintWriter.class);
-    final HttpServletRequest req = (HttpServletRequest) x.get(HttpServletRequest.class);
+    PrintWriter        out         = (PrintWriter)        x.get(PrintWriter.class);
+    HttpServletRequest req         = (HttpServletRequest) x.get(HttpServletRequest.class);
+    Set<Thread>        threadSet   = Thread.getAllStackTraces().keySet();
+    Thread[]           threadArray = threadSet.toArray(new Thread[threadSet.size()]);
 
-    Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
-
-    Thread[] threadArray = threadSet.toArray(new Thread[threadSet.size()]);
     out.println("<HTML>");
     out.println("<HEAD><TITLE>Threads</TITLE></HEAD>\n");
     out.println("<BODY>");
-    out.println("<pre>");
     out.println("<H1>Threads</H1>\n");
+    out.println("<pre>");
 
     for ( Thread thread : threadArray ){
       out.println("<a href=\"threads?id="+ thread.getId() + "\">" + thread.toString() + "</a>");
     }
 
     String param = req.getParameter("id");
-    if( param != null && !param.isEmpty() ){
-      out.println("<H1>Stack Trace</H1>\n");
-      
-      for ( Thread thread : threadArray ){
+    if ( param != null && ! param.isEmpty() ) {
+      out.println("<br><br><H2>Stack Trace</H2>\n");
+
+      for ( Thread thread : threadArray ) {
         Long id = new Long(thread.getId());
 
-        if( param.equals(id.toString()) ){
-          out.println("<H4>Thread: " + thread.getName() + "</H4>\n");
+        if ( param.equals(id.toString()) ) {
+          out.println("<b>Thread: " + thread.getName() + "</b>\n");
           StackTraceElement[] elements = thread.getStackTrace();
 
-          for( StackTraceElement element : elements ) {
+          for ( StackTraceElement element : elements ) {
             out.println(element.toString());
           }
           break;
         }
       }
     }
-    out.println("</pre>");
-    out.println("</BODY></HTML>");
+
+    out.println("</pre></BODY></HTML>");
   }
 }
