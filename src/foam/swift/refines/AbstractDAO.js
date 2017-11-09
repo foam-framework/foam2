eeb5e5c4d89499cfaa341421015eb5821a8286dc
Mike Carcasole <mikecarcasole@gmail.com>
2017-11-08 21:18:13 -0500

    Add Promised support to swift.

--- src/foam/swift/refines/AbstractDAO.js
+++ src/foam/swift/refines/AbstractDAO.js
@@ -5,18 +5,6 @@
  */
 
 foam.CLASS({
-  refines: 'foam.dao.AbstractDAO',
-  methods: [
-    // Here to finish implementing the interface.
-    { name: 'select_' },
-    { name: 'put_' },
-    { name: 'remove_' },
-    { name: 'find_' },
-    { name: 'removeAll_' },
-  ],
-})
-
-foam.CLASS({
   refines: 'foam.dao.DAOProperty',
   properties: [
     {
