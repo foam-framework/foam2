ac3f2aadfb33b13b142aa65f0f27dd00aa250599
xuerongCode <xuerong.work@gmail.com>
2017-11-13 15:24:07 -0500

    modify to tree context

--- src/foam/core/EmptyX.java
+++ src/foam/core/EmptyX.java
@@ -20,7 +20,7 @@ package foam.core;
 // TODO: make this a functional tree rather than a linked list. (for performance)
 
 abstract class AbstractX
-  implements X
+  implements X, Cloneable
 {
   public Object get(Object key) {
     return get(this, key);
@@ -41,11 +41,23 @@ abstract class AbstractX
   }
 
   public X put(Object key, Object value) {
-    return new XI(this, key, value);
+    if ( getKey().toString().compareTo(key.toString()) == 0 ) {
+      return new XI(getLeftParent(), getRightParent(), getKey(), value);
+    } else if ( getKey().toString().compareTo(key.toString()) < 0 ) {
+      return clone(getLeftParent().put(key, value), getRightParent());
+    } else {
+      return clone(getLeftParent(), getRightParent().put(key, value));
+    }
   }
 
   public X putFactory(Object key, XFactory factory) {
-    return new FactoryXI(this, key, factory);
+    if ( getKey().toString().compareTo(key.toString()) == 0 ) {
+      return new FactoryXI(getLeftParent(), getRightParent(), getKey(), factory);
+    } else if ( getKey().toString().compareTo(key.toString()) < 0 ) {
+      return clone(getLeftParent().putFactory(key, factory), getRightParent());
+    } else {
+      return clone(getLeftParent(), getRightParent().putFactory(key, factory));
+    }
   }
 
   public Object getInstanceOf(Object value, Class type) {
@@ -55,6 +67,30 @@ abstract class AbstractX
   public <T> T create(Class<T> type) {
     return ((FacetManager)get("facetManager")).create(type, this);
   }
+
+  private X clone(X left, X right) {
+    AbstractX result = this.clone();
+    result.setLeftParent(left);
+    result.setRightParent(right);
+    return result;
+  }
+
+  @Override 
+  protected AbstractX clone() {
+    AbstractX x = null;
+    try {
+      x = (AbstractX) super.clone();
+    } catch ( CloneNotSupportedException e ) {
+      e.printStackTrace();
+    }
+    return x;
+  }
+
+  abstract protected Object getKey();
+  abstract protected X getLeftParent();
+  abstract protected X getRightParent();
+  abstract protected void setLeftParent(X leftParent);
+  abstract protected void setRightParent(X rightParent);
 }
 
 
@@ -62,20 +98,36 @@ abstract class AbstractX
 class XI
   extends AbstractX
 {
-  final X      parent_;
-  final Object key_;
-  final Object value_;
+  X             leftParent_;
+  X             rightParent_;
+  final Object  key_;
+  final Object  value_;
 
-  XI(X parent, Object key, Object value) {
-    parent_ = parent;
-    key_    = key;
-    value_  = value;
+  XI(X leftParent, X rightParent, Object key, Object value) {
+    leftParent_   = leftParent;
+    rightParent_  = rightParent;
+    key_          = key;
+    value_        = value;
   }
 
-  X parent() { return parent_; }
+  protected Object getKey() { return key_; }
+  protected X getLeftParent() { return leftParent_; }
+  protected X getRightParent() { return rightParent_; }
+  protected void setLeftParent(X leftParent) { leftParent_ = leftParent; }
+  protected void setRightParent(X rightParent) { rightParent_ = rightParent; }
 
   public Object get(X x, Object key) {
-    return key.equals(key_) ? value_ : parent().get(key);
+    System.out.println("key: " + key.toString());
+    System.out.println("key_: " + key_.toString());
+    if ( key_.toString().compareTo(key.toString()) == 0 ) {
+      System.out.println("success: " + key.toString());
+      System.out.println("");
+      return value_;
+    } else if ( key_.toString().compareTo(key.toString()) < 0 ) {
+      return getLeftParent().get(key);
+    } else {
+      return getRightParent().get(key);
+    }
   }
 }
 
@@ -84,24 +136,36 @@ class XI
 class FactoryXI
   extends AbstractX
 {
-  final X        parent_;
-  final Object   key_;
-  final XFactory factory_;
+  X               leftParent_;
+  X               rightParent_;
+  final Object    key_;
+  final XFactory  factory_;
 
-  FactoryXI(X parent, Object key, XFactory factory) {
-    parent_  = parent;
-    key_     = key;
-    factory_ = factory;
+  FactoryXI(X leftParent, X rightParent, Object key, XFactory factory) {
+    leftParent_   = leftParent;
+    rightParent_  = rightParent;
+    key_          = key;
+    factory_      = factory;
   }
 
-  X parent() {
-    return parent_;
-  }
+  protected Object getKey() { return key_; }
+  protected X getLeftParent() { return leftParent_; }
+  protected X getRightParent() { return rightParent_; }
+  protected void setLeftParent(X leftParent) { leftParent_ = leftParent; }
+  protected void setRightParent(X rightParent) { rightParent_ = rightParent; }
 
   public Object get(X x, Object key) {
-    return key.equals(key_) ?
-      factory_.create(x)    :
-      parent().get(x, key) ;
+    System.out.println("keyFactory: " + key.toString());
+    System.out.println("keyFactory_: " + key_.toString());
+    if ( key_.toString().compareTo(key.toString()) == 0 ) {
+      System.out.println("successFactory: " + key.toString());
+      System.out.println("");
+      return factory_.create(x);
+    } else if ( key_.toString().compareTo(key.toString()) < 0 ) {
+      return getLeftParent().get(x, key);
+    } else {
+      return getRightParent().get(x, key);
+    }
   }
 }
 
@@ -116,5 +180,19 @@ public class EmptyX
 
   public static X instance() { return x_; }
 
-  public Object get(X x, Object key) { return null; }
+  public Object get(X x, Object key) { System.out.println("emtpy: " + key.toString());return null; }
+
+  public X put(Object key, Object value) {
+    return new XI(this, this, key, value);
+  }
+
+  public X putFactory(Object key, XFactory factory) {
+    return new FactoryXI(this, this, key, factory);
+  }
+
+  protected Object getKey() { throw new UnsupportedOperationException("Unsupported operation: getKey"); }
+  protected X getLeftParent() { throw new UnsupportedOperationException("Unsupported operation: getLeftParent"); }
+  protected X getRightParent() { throw new UnsupportedOperationException("Unsupported operation: getRightParent"); }
+  protected void setLeftParent(X left) { throw new UnsupportedOperationException("Unsupported operation: setLeftParent"); }
+  protected void setRightParent(X right) { throw new UnsupportedOperationException("Unsupported operation: setRightParent"); }
 }
