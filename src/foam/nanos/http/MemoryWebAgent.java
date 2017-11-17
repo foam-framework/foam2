5b6fd6f8350c7564078dafa3985b0d5373d3268b
Kevin Greer <kgrgreer@gmail.com>
2017-11-17 16:57:02 -0500

    Formatting.

--- src/foam/nanos/http/MemoryWebAgent.java
+++ src/foam/nanos/http/MemoryWebAgent.java
@@ -7,33 +7,39 @@
 package foam.nanos.http;
 
 import foam.core.*;
-
 import java.io.PrintWriter;
-import javax.servlet.http.HttpServletRequest;
 import java.lang.management.MemoryUsage;
 import java.lang.Runtime;
+import javax.servlet.http.HttpServletRequest;
 
+/** Display memory usage statistics. **/
 public class MemoryWebAgent
-    implements WebAgent
+  implements WebAgent
 {
+  final static long MB = 1024 * 1024;
+
   public MemoryWebAgent() {}
 
   public static long bytesToMegabytes(long bytes) {
-      long mb = 1024 * 1024;
-      return bytes / mb;
+    return bytes / MB;
   }
 
   public void execute(X x) {
-    final PrintWriter out   = (PrintWriter) x.get(PrintWriter.class);
-    Runtime runtime = Runtime.getRuntime();
+    PrintWriter out     = (PrintWriter) x.get(PrintWriter.class);
+    Runtime     runtime = Runtime.getRuntime();
+
     out.println("<HTML>");
     out.println("<HEAD><TITLE>Memory Usage (JVM)</TITLE></HEAD>\n");
     out.println("<BODY>");
     out.println("<H1>Memory Usage (JVM)</H1>\n");
     out.println("<pre>");
+
+    // Takes two calls for force a GC.
     runtime.gc();
+    runtime.gc();
+
     long memory = runtime.totalMemory() - runtime.freeMemory();
-    
+
     out.println("Max memory in bytes: " + runtime.totalMemory());
     out.println("Max memory in megabytes: " + bytesToMegabytes(runtime.totalMemory()));
     out.println("</br>");
