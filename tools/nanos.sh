ac575014de60e9b0cd5ff41e5b2363bc34ac9050
Patrick <zahnno@gmail.com>
2017-11-22 15:19:00 -0500

    added services script

--- tools/nanos.sh
+++ tools/nanos.sh
@@ -1,6 +1,6 @@
 # run from parent directory of foam2
 cd foam2/build/
 mvn dependency:build-classpath -Dmdep.outputFile=cp.txt;
-cp services ../..
+
 cd ../..
 java -cp `cat foam2/build/cp.txt`:foam2/build/target/foam-1.0-SNAPSHOT.jar -Dfoam.main=main foam.nanos.boot.Boot
