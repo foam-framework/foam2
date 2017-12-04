e0343f9acc75305c0080628b525dc97df908e678
Mike Carcasole <mikecarcasole@gmail.com>
2017-12-04 11:36:16 -0500

    (java) Create a 'Factory' interface and use that interface for the Multiton axiom. Make the SimpleFacetManager look for this axiom when creating objects.

---
+++ src/foam/core/Factory.java
@@ -0,0 +1,11 @@
+/**
+ * @license
+ * Copyright 2017 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package foam.core;
+
+public interface Factory<T> {
+  public T getInstance(java.util.Map<String, Object> args, X x);
+}
