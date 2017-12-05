6b92910f3d4e6e2c93a024555a051bb595591170
MarcRoopchand <marc.roopchand@gmail.com>
2017-12-05 11:45:36 -0500

    Added check to see if tomcat is running

--- src/foam/nanos/http/NanoHttpServer.java
+++ src/foam/nanos/http/NanoHttpServer.java
@@ -21,8 +21,18 @@ public class NanoHttpServer
   protected int        port_ = 8080;
 
   public void start() {
-    System.out.println("Starting HTTP Server on port " + port_);
+    try {
+      Class cls = Class.forName("org.apache.tomcat.SimpleInstanceManager");
+      System.out.println("Tomcat is running!");
+      return;
+    } catch(ClassNotFoundException ex) {
+      System.out.println(ex.toString());
+      startNanos();
+    }
+  }
 
+  public void startNanos() {
+    System.out.println("Starting HTTP Server on port " + port_);
     try {
       server_ = HttpServer.create(new InetSocketAddress(port_), 0);
       server_.createContext("/", new NanoHttpHandler(getX()));
