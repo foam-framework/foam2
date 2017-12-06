695fa1718056ab44b57ef45b0f31fe6ccffeaf66
xuerongCode <xuerong.work@gmail.com>
2017-12-06 10:09:25 -0500

    create CSVParser.java

---
+++ src/foam/lib/csv/CSVParser.java
@@ -0,0 +1,13 @@
+import java.io.InputStream;
+
+/**
+ * @license
+ * Copyright 2017 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+public class CSVParser {
+  inputCSV(InputStream is, Sink sink, ClassInfo classInfo) {
+    
+  }
+}
