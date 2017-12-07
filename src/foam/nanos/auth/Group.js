d57240d076bfa86bf9fc22352ea6f85389bc93b5
Kevin Greer <kgrgreer@gmail.com>
2017-12-07 10:21:26 -0500

    Update Group tableColumns.

--- src/foam/nanos/auth/Group.js
+++ src/foam/nanos/auth/Group.js
@@ -14,7 +14,7 @@ foam.CLASS({
 
   documentation: 'A Group of Users.',
 
-  tableColumns: [ 'id', 'description', 'defaultMenu' ],
+  tableColumns: [ 'id', 'description', 'defaultMenu', 'parent' ],
 
   searchColumns: [ ],
 
