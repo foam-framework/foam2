9f89baba6d3fde1d06313de11c87504b5e377947
xuerongCode <xuerong.work@gmail.com>
2017-12-11 14:33:51 -0500

    todo

---
+++ src/foam/flinks/FlinksWebAgent.java
@@ -0,0 +1,19 @@
+/**
+ * @license
+ * Copyright 2017 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package foam.Flinks;
+
+import foam.nanos.http.WebAgent;
+
+public class FlinksWebAgent
+  implements WebAgent
+{
+  public FlinksWebAgent() {}
+
+  public void execute(X x) {
+    
+  }
+}
