d581732cb97f5c1c16095c44c04aaa69efd2f5de
xuerongCode <xuerong.work@gmail.com>
2017-12-11 15:32:18 -0500

    todo

--- src/foam/flinks/FlinksWebAgent.java
+++ src/foam/flinks/FlinksWebAgent.java
@@ -7,6 +7,7 @@
 package foam.Flinks;
 
 import foam.nanos.http.WebAgent;
+import foam.core.X;
 
 public class FlinksWebAgent
   implements WebAgent
@@ -14,6 +15,7 @@ public class FlinksWebAgent
   public FlinksWebAgent() {}
 
   public void execute(X x) {
-    
+    HttpServletRequest req = (HttpServletRequest) x.get(HttpServletRequest.class);
+
   }
 }
