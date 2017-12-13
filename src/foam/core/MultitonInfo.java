f31c8690d3b28b770377be148a102d96f73272d5
Mike Carcasole <mikecarcasole@gmail.com>
2017-12-13 07:12:27 -0800

    Make multiton creation thread safe and fix some code style.

--- src/foam/core/MultitonInfo.java
+++ src/foam/core/MultitonInfo.java
@@ -26,14 +26,14 @@ public class MultitonInfo<T>
     return name;
   }
 
-  public T getInstance(Map<String, Object> args, X x) {
+  public synchronized T getInstance(Map<String, Object> args, X x) {
     Object key = args.get(p.getName());
     if ( ! instanceMap.containsKey(key) ) {
       try {
         Class<T> type = (Class<T>)p.getClassInfo().getObjClass();
         T obj = type.newInstance();
         ((ContextAware)obj).setX(x);
-        for (Map.Entry<String, Object> entry : args.entrySet()) {
+        for ( Map.Entry<String, Object> entry : args.entrySet() ) {
           ((FObject)obj).setProperty(entry.getKey(), entry.getValue());
         }
         instanceMap.put(key, obj);
