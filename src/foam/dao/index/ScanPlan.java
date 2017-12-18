c1f05fe48f7ec29d3e179bdd0a66954efabead11
Amy Meng <amymeng5277@gmail.com>
2017-12-18 12:18:33 -0500

    1.Fixed space in TreeNode and ScanPlan 2. Change some part of costCalculate() function in Scanplan 3.Change name from selectAllHelper to select_

--- src/foam/dao/index/ScanPlan.java
+++ src/foam/dao/index/ScanPlan.java
@@ -36,24 +36,25 @@ public class ScanPlan implements FindPlan, SelectPlan {
   public long calculateCost(PropertyInfo propertyInfo){
     long cost;
     if ( state_ == null ) {
-      cost = 0;
+      return 0;
     } else {
       cost = ((TreeNode) state_).size;
     }
     boolean sortRequired = false;
     boolean reverseSort = false;
     if ( order_ != null ) {
-      if ( order_.getClass().toString().equals(propertyInfo.toString()) ) order_ = null;
-      else if ( order_ instanceof Desc ) {
+      if ( order_.toString().equals(propertyInfo.toString()) ) {
+        order_ = null;
+      } else if ( order_ instanceof Desc ) {
         reverseSort = true;
       } else {
         sortRequired = true;
-        if ( cost != 0 ) cost *= Math.log(cost) / Math.log(2);
+        cost *= Math.log(cost) / Math.log(2);
       }
     }
     if ( ! sortRequired ) {
-      if ( skip_ != 0 ) cost -= skip_;
-      if ( limit != 0 ) cost = Math.min(cost, limit);
+      if ( skip_ != 0 ) cost = Math.max(cost - skip_, 0);
+      if ( limit_ != 0 ) cost = Math.min(cost, limit_);
     }
     return cost;
   }
@@ -67,6 +68,6 @@ public class ScanPlan implements FindPlan, SelectPlan {
   public void select(Object state, Sink sink, long skip, long limit, Comparator order, Predicate predicate) {
     if(state == null)
       return;
-    ((TreeNode)state).select((TreeNode)state_, sink, skip_, limit_, order_, predicate_,tail_);
+    ((TreeNode)state).select((TreeNode)state_, sink, skip_, limit_, order_, predicate_, tail_);
   }
 }
