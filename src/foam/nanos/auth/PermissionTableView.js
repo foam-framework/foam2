f2250cde6c971568b7ee43a7b6ef21a882ebd26c
Minsun <minsun@nanopay.net>
2017-12-20 16:02:03 -0500

    Added PermissionTableView.js

--- src/foam/nanos/auth/PermissionTableView.js
+++ src/foam/nanos/auth/PermissionTableView.js
@@ -9,11 +9,36 @@ foam.CLASS({
     'auth'
   ],
 
+  css: `
+    ^ .net-nanopay-ui-ActionView{
+      width: 95.5%;
+      height: 40px;
+      background: #59aadd;
+      margin-bottom: 15px;
+    }
+  `,
+
+  actions: [
+    {
+      name: 'save',
+      label: 'Save',
+      code: function(X) {
+        X.dao.put(this);
+        X.stack.push({class: 'foam.nanos.auth.permissionDAO'});
+      }
+    }
+  ],
+
   methods: [
     function initE() {
       this.SUPER();
       var self = this;
 
+      this
+        .addClass(this.myClass())
+        .start().addClass('button-row')
+          .start(this.SAVE).addClass('float-right').end()
+        .end()
       this.start('table')
         .start('tr')
           .tag('td')
@@ -23,17 +48,27 @@ foam.CLASS({
         .end()
         .select(this.permissionDAO, function(p) {
           this.start('tr')
-            .start('th').add('permission: ', p.id).end()
+            .start('th').add(p.id).end()
             .select(self.groupDAO, function(g) {
-              this.start('td').tag({class: 'foam.u2.CheckBox', data: self.checkPermissionForGroup(p.id, g.id)}).end();
+              this.start('td').tag({class: 'foam.u2.CheckBox', data: self.checkPermissionForGroup(p.id, g)}).end();
             })
-          .end();
+          .end()
         })
+      .end()
       .end();
     },
 
     function checkPermissionForGroup(permission, group) {
-      return Math.random() < 0.5;
+      for ( i = 0 ; i < group.permissions.length ; i++ ) {
+        if ( permission == group.permissions[i].id ) {
+          return true;
+        }
+      }
+    },
+
+    function SavePermissionForGroup() {
+
     }
+
   ]
-})
+});
