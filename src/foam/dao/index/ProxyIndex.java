058c92156eb44fdb3cdb865fd5dc79c8cbab303c
Kirk Eaton <kirk@nanopay.net>
2017-12-21 16:13:16 -0500

    Added ProxyIndex

---
+++ src/foam/dao/index/ProxyIndex.java
@@ -0,0 +1,64 @@
+package foam.dao.index;
+
+import foam.core.FObject;
+import foam.dao.Sink;
+import foam.mlang.order.Comparator;
+import foam.mlang.predicate.Predicate;
+
+public class ProxyIndex
+    implements Index
+{
+  protected Index delegate_;
+  private boolean delegateIsSet_ = false;
+
+  public ProxyIndex(Index index) {
+    setDelegate(index);
+  }
+
+  public Index getDelegate() {
+    if ( ! delegateIsSet_ ) {
+      return null;
+    }
+    return delegate_;
+  }
+
+  public void setDelegate(foam.dao.index.Index val) {
+    delegate_ = val;
+    delegateIsSet_ = true;
+  }
+
+  @Override
+  public void onAdd(Sink sink) {
+    getDelegate().onAdd(sink);
+  }
+
+  @Override
+  public Object put(Object state, FObject value) {
+    return getDelegate().put(state, value);
+  }
+
+  @Override
+  public Object remove(Object state, FObject value) {
+    return getDelegate().remove(state, value);
+  }
+
+  @Override
+  public Object removeAll() {
+    return getDelegate().removeAll();
+  }
+
+  @Override
+  public FindPlan planFind(Object state, Object key) {
+    return getDelegate().planFind(state, key);
+  }
+
+  @Override
+  public SelectPlan planSelect(Object state, Sink sink, long skip, long limit, Comparator order, Predicate predicate) {
+    return getDelegate().planSelect(state, sink, skip, limit, order, predicate);
+  }
+
+  @Override
+  public long size(Object state) {
+    return getDelegate().size(state);
+  }
+}
