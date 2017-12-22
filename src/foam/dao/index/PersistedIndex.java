edff0cb7c4d5bad90519c0860f2e3217b548d216
Kirk Eaton <kirk@nanopay.net>
2017-12-22 11:15:41 -0500

    Added wrap and unwrap functions to PersistedIndex

--- src/foam/dao/index/PersistedIndex.java
+++ src/foam/dao/index/PersistedIndex.java
@@ -6,29 +6,63 @@
 
 package foam.dao.index;
 
-import java.io.File;
-import java.io.IOException;
+import org.apache.commons.io.IOUtils;
+
+import java.io.*;
 
 public class PersistedIndex
     extends ProxyIndex
 {
-  protected File file_;
+  protected final File file_;
+  protected FileInputStream fis_;
+  protected FileOutputStream fos_;
 
   public PersistedIndex(String filename, Index index) throws IOException {
     this.file_ = new File(filename).getAbsoluteFile();
     if ( ! file_.exists() ) {
-      this.file_.createNewFile();
+      if ( ! this.file_.createNewFile() ) {
+        throw new IOException("Unable to create file: " + filename);
+      }
     }
+    this.fis_ = new FileInputStream(this.file_);
+    this.fos_ = new FileOutputStream(this.file_);
     setDelegate(index);
   }
 
   @Override
   public Object wrap(Object state) {
-    return super.wrap(state);
+    ByteArrayOutputStream bos = null;
+    ObjectOutputStream oos = null;
+
+    synchronized ( file_ ) {
+      try {
+        bos = new ByteArrayOutputStream();
+        oos = new ObjectOutputStream(bos);
+        oos.writeObject(state);
+        oos.flush();
+
+        bos.writeTo(fos_);
+        return fos_.getChannel().position();
+      } catch (Throwable t) {
+        throw new RuntimeException(t);
+      } finally {
+        IOUtils.closeQuietly(bos);
+        IOUtils.closeQuietly(oos);
+      }
+    }
   }
 
   @Override
   public Object unwrap(Object state) {
-    return super.unwrap(state);
+    synchronized ( file_ ) {
+      try {
+        long position = (long) state;
+        fis_.getChannel().position(position);
+        ObjectInputStream iis = new ObjectInputStream(fis_);
+        return iis.readObject();
+      } catch (Throwable t) {
+        throw new RuntimeException(t);
+      }
+    }
   }
 }
