42da61a0027df0c20a2886aeca3c53e520f5a2c8
Mike Carcasole <mikecarcasole@gmail.com>
2018-01-09 14:51:50 -0500

    Create a tool for relocating and formatting models. It's in the demo directory with an execute.js script that shows how to use it.

---
+++ demos/filerelocator/execute.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+var dir = __dirname;
+var root = dir + '/../..';
+
+require(root + '/src/foam.js');
+
+var executor = foam.classloader.NodeJsModelExecutor.create({
+  classpaths: [
+    root + '/src',
+    dir + '/src',
+  ],
+  modelId: 'foam.tools.FileRelocator',
+  modelArgs: {
+    outputPath: dir + '/OUTPUT/',
+    filePath: root + '/src/foam/u2/Element.js',
+  },
+});
+executor.execute()
