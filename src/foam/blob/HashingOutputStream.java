7c436f767586259f9f0bc9a3f1bfa2509f4008e3
Kirk Eaton <kirk@nanopay.net>
2018-01-10 08:53:14 -0500

    Added comments to HashingOutputStream. Added license header to InputStreamBlob

--- src/foam/blob/HashingOutputStream.java
+++ src/foam/blob/HashingOutputStream.java
@@ -1,3 +1,9 @@
+/**
+ * @license
+ * Copyright 2017 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 package foam.blob;
 
 import java.io.IOException;
@@ -5,25 +11,51 @@ import java.io.OutputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+/**
+ * HashingOutputStream
+ *
+ * OutputStream decorator that, given a digest, hashes incoming data before writing it
+ * to the underlying OutputStream
+ */
 public class HashingOutputStream
     extends OutputStream
 {
   protected MessageDigest digest_;
   protected OutputStream os_;
 
+  /**
+   * HashingOutputStream constructor using SHA-256 as the default digest
+   * @param os OutputStream delegate
+   * @throws NoSuchAlgorithmException
+   */
   public HashingOutputStream(OutputStream os) throws NoSuchAlgorithmException {
     this("SHA-256", os);
   }
 
+  /**
+   * HashingOutputStream constructor allowing for user specified digest string
+   * @param digest digest string
+   * @param os OutputStream delegate
+   * @throws NoSuchAlgorithmException
+   */
   public HashingOutputStream(String digest, OutputStream os) throws NoSuchAlgorithmException {
     this(MessageDigest.getInstance(digest), os);
   }
 
+  /**
+   * HashingOutputStream constructor allowing for user specified digest object
+   * @param digest digest object
+   * @param os OutputStream delegate
+   */
   public HashingOutputStream(MessageDigest digest, OutputStream os) {
     os_ = os;
     digest_ = digest;
   }
 
+  /**
+   * Returns the current value of the digest
+   * @return digest as a byte array
+   */
   public byte[] digest() {
     return digest_.digest();
   }
