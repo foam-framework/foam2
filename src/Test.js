d72d3a6af3ceb330d235dd782a17987784cb58ee
Mike Carcasole <mikecarcasole@gmail.com>
2018-01-12 12:11:22 -0500

    Add a DAO property to the Test I've been using to test the scrolling changes.

--- src/Test.js
+++ src/Test.js
@@ -2,6 +2,7 @@ foam.CLASS({
   name: 'Test',
   requires: [
     'TestPropView',
+    'foam.dao.EasyDAO',
   ],
   properties: [
     {
@@ -10,6 +11,36 @@ foam.CLASS({
       view: {
         class: 'TestPropView',
       },
+    },
+    {
+      class: 'foam.dao.DAOProperty',
+      name: 'dao',
+      factory: function() {
+        var data = [];
+        for ( var i = 0; i < 1000; i++ ) {
+          data.push(this.DAOClass.create({id: i}));
+        }
+        return this.EasyDAO.create({
+          of: this.DAOClass,
+          daoType: 'MDAO',
+          testData: data,
+        });
+      },
+    },
+  ],
+  classes: [
+    {
+      name: 'DAOClass',
+      properties: [
+        'id',
+        {
+          class: 'String',
+          name: 'name',
+          expression: function(id) {
+            return 'Person ' + id;
+          },
+        },
+      ],
     },
   ],
 });
