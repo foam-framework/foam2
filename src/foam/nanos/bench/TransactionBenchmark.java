561d5f008fbdee38e88361f76d7606c1ecacf2bf
Kirk Eaton <kirk@nanopay.net>
2018-01-16 15:20:43 -0500

    Modified TransactionBenchmark setup and execute functions

--- src/foam/nanos/bench/TransactionBenchmark.java
+++ src/foam/nanos/bench/TransactionBenchmark.java
@@ -8,63 +8,76 @@ package foam.nanos.bench;
 
 import foam.core.X;
 import foam.dao.DAO;
+import foam.dao.ListSink;
+import foam.dao.Sink;
 import foam.nanos.auth.User;
-import foam.util.Password;
 import net.nanopay.model.Account;
 import net.nanopay.tx.model.Transaction;
+
+import java.util.List;
 
 public class TransactionBenchmark
   implements Benchmark
 {
-  protected User payee_;
-  protected User payer_;
+  List users = null;
+  List accounts = null;
 
   protected DAO userDAO_;
   protected DAO accountDAO_;
   protected DAO transactionDAO_;
-
+  protected DAO transactionLimitDAO_;
 
   @Override
   public void setup(X x) {
-    userDAO_ = (DAO) x.get("userDAO");
+    userDAO_ = (DAO) x.get("localUserDAO");
     accountDAO_ = (DAO) x.get("localAccountDAO");
-    transactionDAO_ = (DAO) x.get("transactionDAO");
+    transactionDAO_ = (DAO) x.get("localTransactionDAO");
+    transactionLimitDAO_ = (DAO) x.get("transactionLimitDAO");
 
-    // create payer agent
-    User payer = new User();
-    payer.setFirstName("Test");
-    payer.setLastName("Payer");
-    payer.setGroup("tester");
-    payer.setEmail("test1@nanopay.net");
-    payer.setPassword("Mintchip123");
-    payer_ = (User) userDAO_.put(payer);
+    transactionDAO_.removeAll();
+    transactionLimitDAO_.removeAll();
 
-    // set payer balance to $1000.00
-    Account payerAccount = (Account) accountDAO_.find(payer_.getId());
-    payerAccount.setBalance(1000 * 100);
-    accountDAO_.put(payerAccount);
+    Sink sink = new ListSink();
+    sink = userDAO_.select(sink);
+    users = ((ListSink) sink).getData();
 
-    // create payee agent
-    User payee = new User();
-    payee.setFirstName("Test");
-    payee.setLastName("Payee");
-    payee.setGroup("tester");
-    payee.setEmail("test2@nanopay.net");
-    payee.setPassword("Mintchip123");
-    payee_ = (User) userDAO_.put(payee);
+    sink = new ListSink();
+    sink = accountDAO_.select(sink);
+    accounts = ((ListSink) sink).getData();
 
-    // set payee balance to $1000.00
-    Account payeeAccount = (Account) accountDAO_.find(payee_.getId());
-    payeeAccount.setBalance(1000 * 100);
-    accountDAO_.put(payeeAccount);
+    for ( int i = 0 ; i < accounts.size() ; i++ ) {
+      Account account = (Account) accounts.get(i);
+      account.setBalance(1000000);
+      accountDAO_.put(account);
+    }
+
+    for ( int i = 0 ; i < users.size() ; i++ ) {
+      User user = (User) users.get(i);
+      Account account = (Account) accountDAO_.find(user.getId());
+      if ( account == null ) {
+        account = new Account();
+        account.setId(user.getId());
+        account.setBalance(1000000);
+        accountDAO_.put(account);
+      }
+    }
   }
 
   @Override
   public void execute(X x) {
-    Transaction transaction = new Transaction();
-    transaction.setPayerId(payer_.getId());
-    transaction.setPayeeId(payee_.getId());
-    transaction.setAmount(1);
-    transactionDAO_.put(transaction);
+    int fi = (int) (Math.random() * users.size());
+    int ti = (int) (Math.random() * users.size());
+    int amount = (int) ((Math.random() + 0.1) * 10000);
+
+    long payeeId = ((User) users.get(ti)).getId();
+    long payerId = ((User) users.get(fi)).getId();
+
+    if ( payeeId != payerId ) {
+      Transaction transaction = new Transaction();
+      transaction.setPayeeId(payeeId);
+      transaction.setPayerId(payerId);
+      transaction.setAmount(amount);
+      transactionDAO_.put(transaction);
+    }
   }
 }
