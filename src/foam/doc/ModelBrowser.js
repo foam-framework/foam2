6102445b1cf6495c85f3de006d81015d5f5fd94b
Patrick <zahnno@gmail.com>
2018-02-06 16:24:02 -0500

    model browser foam used models

--- src/foam/doc/ModelBrowser.js
+++ src/foam/doc/ModelBrowser.js
@@ -1,12 +1,19 @@
 foam.CLASS({
   package: 'foam.doc',
-  name: 'ModelView',
+  name: 'ModelBrowser',
   extends: 'foam.u2.Element',
   documentation: 'Show UML & properties for passed in models',
 
   requires: [
+    'foam.doc.DocBorder',
+    'foam.doc.ClassList',
     'foam.doc.ClassDocView',
-    'foam.doc.UMLDiagram'
+    'foam.doc.UMLDiagram',
+    'foam.nanos.boot.NSpec'
+  ],
+
+  imports: [
+    'nSpecDAO'
   ],
 
   exports: [
@@ -16,7 +23,10 @@ foam.CLASS({
 
   properties: [
     {
-      name: 'models'
+      name: 'models',
+      factory: function(){
+        return Object.values(foam.USED);
+      }
     },
     {
       name: 'showOnlyProperties',
@@ -44,9 +54,10 @@ foam.CLASS({
       width: 700px;
     }
     ^ .foam-u2-view-TableView-foam-doc-AxiomInfo{
-      width: 600px;
+      width: 700px;
       float: left;
       margin-top: 20px;
+      margin-bottom: 30px;
     }
     ^ .net-nanopay-ui-ActionView-printPage{
       margin-top: 20px;
@@ -63,13 +74,15 @@ foam.CLASS({
         .start().add(this.PRINT_PAGE).end()
         .add(this.slot(function(models) {
           return self.E().forEach(models, function(a) {
-            model = self.lookup(a, true);
+            model = self.lookup(a.id, true);
+            this.start()
             if( !model ) {
-              return self.start().add("Could not find class ", model).end()
+              return;
             }
             this.start().style({ 'font-size' : '20px', 'margin-top' : '20px'}).add("Model " + a).end()
-            this.tag(self.UMLDiagram.create({ data: model }));
-            this.tag(self.ClassDocView.create({data: model }));
+            this.tag(self.UMLDiagram.create({ data: model }))
+            this.tag(self.ClassDocView.create({data: model }))
+            .end()
           })
         }))
       .end()
