c74bcca4387e9911e16d29b602a45a65649330d8
Mike Carcasole <mikecarcasole@gmail.com>
2018-02-07 17:26:34 -0500

    Create a NodeModelFileDAO.

---
+++ src/foam/apploader/NodeModelFileDAO.js
@@ -0,0 +1,29 @@
+foam.CLASS({
+  package: 'foam.apploader',
+  name: 'NodeModelFileDAO',
+  extends: 'foam.apploader.AbstractModelFileDAO',
+  properties: [
+    {
+      name: 'root'
+    },
+  ],
+  methods: [
+    function getFile(id) {
+      var self = this;
+      return new Promise(function(ret, err) {
+        var sep = require('path').sep;
+        var path = self.root + sep + id.replace(/\./g, sep) + '.js';
+        try {
+          var fs = require('fs');
+          var js = fs.readFileSync(path, 'utf8');
+          ret(js);
+        } catch(e) {
+          err(null);
+        }
+      });
+    },
+    function onError() {
+      return null;
+    },
+  ]
+});
