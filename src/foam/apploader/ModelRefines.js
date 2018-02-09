6b6313a10abfddb6c45b9afcaa53c063bb18e354
Mike Carcasole <mikecarcasole@gmail.com>
2018-02-09 17:50:39 -0500

    Remove the opt_flags arg from getClassDeps. Strip the model of requires and implements before serializing it. Remove AxiomArray refinement because it's no longer necessary at the moment.

--- src/foam/apploader/ModelRefines.js
+++ src/foam/apploader/ModelRefines.js
@@ -5,15 +5,13 @@ foam.CLASS({
   methods: [
     {
       name: 'getClassDeps',
-      code: function(opt_flags) {
-        var filter = foam.util.flagFilter(opt_flags);
-
+      code: function() {
         var deps = this.requires ?
-            this.requires.filter(filter).map(function(r) { return r.path }) :
+            this.requires.map(function(r) { return r.path }) :
             [];
 
         deps = deps.concat(this.implements ?
-                           this.implements.filter(filter).map(function(i) { return i.path }) :
+                           this.implements.map(function(i) { return i.path }) :
                            []);
 
         if ( this.extends ) deps.push(this.extends);
