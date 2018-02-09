8405336084e5741ad87765260c0692c80b8ce20c
Kirk Eaton <kirk@nanopay.net>
2018-02-09 11:15:07 -0500

    Fixed issue with HTTPSink

--- src/foam/dao/HTTPSink.java
+++ src/foam/dao/HTTPSink.java
@@ -10,61 +10,62 @@ import foam.core.Detachable;
 import foam.core.FObject;
 import foam.lib.Outputter;
 import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpException;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class HTTPSink
     extends AbstractSink
-    implements Closeable
 {
-  protected Writer writer_ = null;
-  protected HttpURLConnection conn_;
-  protected boolean connected_ = false;
+  protected String url_;
   protected Outputter outputter_;
 
   public HTTPSink(String url, Outputter outputter) throws IOException {
-    conn_ = (HttpURLConnection) new URL(url).openConnection();
-    conn_.setConnectTimeout(5 * 1000);
-    conn_.setReadTimeout(5 * 1000);
-    conn_.setRequestMethod("PUT");
+    url_ = url;
     outputter_ = outputter;
   }
 
   @Override
   public void put(FObject obj, Detachable sub) {
+    HttpURLConnection conn = null;
+    OutputStream os = null;
+    BufferedWriter writer = null;
+
     try {
-      // connect if not connected
-      if ( ! connected_) {
-        conn_.connect();
-        connected_ = true;
+      conn = (HttpURLConnection) new URL(url_).openConnection();
+      conn.setRequestMethod("POST");
+      conn.setDoInput(true);
+      conn.setDoOutput(true);
+      if ( outputter_ instanceof foam.lib.json.Outputter ) {
+        conn.addRequestProperty("Accept", "application/json");
+        conn.addRequestProperty("Content-Type", "application/json");
       }
+      conn.connect();
 
-      // create new writer if null
-      if ( writer_ == null ) {
-        writer_ = new BufferedWriter(
-            new OutputStreamWriter(conn_.getOutputStream()));
+      os = conn.getOutputStream();
+      writer = new BufferedWriter(new OutputStreamWriter(os, StandardCharsets.UTF_8));
+      writer.write(outputter_.stringify(obj));
+      writer.flush();
+      writer.close();
+      os.close();
+
+      // check response code
+      int code = conn.getResponseCode();
+      if ( code != HttpServletResponse.SC_OK ) {
+        throw new HttpException();
       }
-
-      writer_.write(outputter_.stringify(obj));
-      super.put(obj, sub);
     } catch (Throwable t) {
       throw new RuntimeException(t);
+    } finally {
+      IOUtils.closeQuietly(writer);
+      IOUtils.closeQuietly(os);
+      if ( conn != null ) {
+        conn.disconnect();
+      }
     }
-  }
-
-  @Override
-  public void eof() {
-    this.close();
-    super.eof();
-  }
-
-  @Override
-  public void close() {
-    IOUtils.closeQuietly(writer_);
-    writer_ = null;
-    conn_.disconnect();
-    connected_ = false;
   }
 }
