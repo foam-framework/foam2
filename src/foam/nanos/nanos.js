ce4b6f0cc7a0036fff5042894fd5302047768bb9
Adam Van Ymeren <adam@vany.ca>
2018-02-09 21:47:15 -0500

    Add models for many to many relationship demo and test.

--- src/foam/nanos/nanos.js
+++ src/foam/nanos/nanos.js
@@ -91,5 +91,10 @@ FOAM_FILES([
   { name: 'foam/nanos/auth/twofactor/authy/AuthyService' },
   { name: 'foam/nanos/demo/DemoObject' },
   { name: 'foam/nanos/demo/Demo' },
-  { name: 'foam/nanos/dig/DUG' }
+  { name: 'foam/nanos/dig/DUG' },
+  { name: 'foam/nanos/demo/relationship/Course' },
+  { name: 'foam/nanos/demo/relationship/Professor' },
+  { name: 'foam/nanos/demo/relationship/Student' },
+  { name: 'foam/nanos/demo/relationship/Controller' },
+  { name: 'foam/nanos/fs/File' }
 ]);
