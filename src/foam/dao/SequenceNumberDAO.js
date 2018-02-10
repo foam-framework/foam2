fa1d6e43482c94f7db84b04d5d410e388cdb29d3
Kevin Greer <kgrgreer@gmail.com>
2018-02-10 15:01:20 -0500

    Update copyright header.

--- src/foam/dao/SequenceNumberDAO.js
+++ src/foam/dao/SequenceNumberDAO.js
@@ -1,18 +1,7 @@
 /**
  * @license
- * Copyright 2014 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2014 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
  */
 
 foam.CLASS({
@@ -24,11 +13,11 @@ foam.CLASS({
     'foam.mlang.Expressions'
   ],
 
-  documentation: 'DAO Decorator which sets a specified property\'s value with an auto-increment sequence number on DAO.put() if the value is set to the default value.',
-
   requires: [
     'foam.dao.InternalException'
   ],
+
+  documentation: 'DAO Decorator which sets a specified property\'s value with an auto-increment sequence number on DAO.put() if the value is set to the default value.',
 
   properties: [
     {
