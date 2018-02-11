3006f6bdae19a209fb97dbec56f593ca741ef844
mcarcaso <mikecarcasole@gmail.com>
2018-02-11 13:36:03 -0500

    Make the foam.Function.args tolerant of comments that are just /**/ or /*``*/.

--- src/foam/core/Argument.js
+++ src/foam/core/Argument.js
@@ -69,7 +69,7 @@ foam.LIB({
       // optional commented return type
       // ws [/* ws package.type? ws */] ws argname ws [/* ws retType ws */]
       var argIdx = 0;
-      var argMatcher = /(\s*\/\*\s*(\.\.\.)?([\w._$\[\]]+)(\=)?\s*(\/\/\s*(.*?))?\s*\*\/)?\s*(\.\.\.)?([\w_$]+)\s*(\/\*\s*([\w._$\[\]]+)(\?)?\s*\*\/)?\s*\,+/g;
+      var argMatcher = /(\s*\/\*\s*(\.\.\.)?([\w._$\[\]]+)(\=)?\s*(\/\/\s*(.*?))?\s*\*\/)?\s*(\.\.\.)?([\w_$]+)\s*(\/\*\s*([\w._$\[\]`]*)(\?)?\s*\*\/)?\s*\,+/g;
       var typeMatch;
 
       while ( typeMatch = argMatcher.exec(args) ) {
