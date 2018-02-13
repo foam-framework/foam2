e2cd8cd9d582df8a3dc8fd696fd30a6e2754b52c
Adam Van Ymeren <adam@vany.ca>
2018-02-13 11:42:25 -0500

    Break some nanos classes into their own files so that the classloader can find them.

---
+++ src/foam/nanos/fs/FileDAODecorator.js
@@ -0,0 +1,45 @@
+/**
+ * @license
+ * Copyright 2018 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.CLASS({
+  package: 'foam.nanos.fs',
+  name: 'FileDAODecorator',
+  extends: 'foam.dao.AbstractDAODecorator',
+
+  imports: [
+    'fileDAO'
+  ],
+
+  properties: [
+    {
+      class: 'Class',
+      name: 'of'
+    }
+  ],
+
+  methods: [
+    function write(X, dao, obj, existing) {
+      var self = this;
+      var i = 0;
+      var props = obj.cls_.getAxiomsByClass(foam.core.File);
+
+      return Promise.resolve().then(function a() {
+        var prop = props[i++];
+
+        if ( ! prop ) return obj;
+
+        var file = prop.f(obj);
+
+        if ( ! file ) return obj;
+
+        return self.fileDAO.put(file).then(function (b) {
+          prop.set(obj, b);
+          return a();
+        });
+      });
+    }
+  ]
+});
