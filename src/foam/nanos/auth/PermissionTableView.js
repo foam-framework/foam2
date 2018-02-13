ad10b94a9935fa694baea94a9ae415e80a3973f3
Minsun <minsun@nanopay.net>
2018-02-13 10:03:44 -0500

    removed unnecessary codes

--- src/foam/nanos/auth/PermissionTableView.js
+++ src/foam/nanos/auth/PermissionTableView.js
@@ -21,55 +21,13 @@ foam.CLASS({
   ],
 
   css: `
-    ^ table {
-          border-collapse: collapse;
-          margin: auto;
-          width: 962px;
-        }
-        ^ thead > tr > th {
-          font-family: 'Roboto';
-          font-size: 14px;
-          background-color: rgba(110, 174, 195, 0.2);
-          color: #093649;
-          line-height: 1.14;
-          letter-spacing: 0.3px;
-          border-spacing: 0;
-          text-align: left;
-          padding-left: 15px;
-          height: 40px;
-        }
-        ^ tbody > tr > th > td {
-          font-size: 12px;
-          letter-spacing: 0.2px;
-          text-align: left;
-          color: #093649;
-          padding-left: 15px;
-          height: 60px;
-        }
-        ^ .foam-u2-view-TableView th {
-          font-family: 'Roboto';
-          padding-left: 15px;
-          font-size: 14px;
-          line-height: 1;
-          letter-spacing: 0.4px;
-          color: #093649;
-        }
-        ^ .foam-u2-view-TableView td {
-          font-family: Roboto;
-          font-size: 12px;
-          line-height: 1.33;
-          letter-spacing: 0.2px;
-          padding-left: 15px;
-          font-size: 12px;
-          color: #093649;
-        }
-        ^ tbody > tr {
-          height: 60px;
-          background: white;
-        }
-        ^ .changeColour:nth-child(odd) {
-          background: #f6f9f9;
-        }
+    ^ .net-nanopay-ui-ActionView{
+      width: 95.5%;
+      height: 40px;
+      background: #59aadd;
+      margin-bottom: 15px;
+      width: 200;
+  }
   `,
 
   methods: [
