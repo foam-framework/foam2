9cb05ca18265687279f84a4f7f208bed8a051e5a
Minsun <minsun@nanopay.net>
2018-02-16 10:59:01 -0500

    added colour of the view

--- src/foam/nanos/auth/PermissionTableView.js
+++ src/foam/nanos/auth/PermissionTableView.js
@@ -7,11 +7,7 @@
 foam.CLASS({
   package: 'foam.nanos.auth',
   name: 'PermissionTableView',
-<<<<<<< HEAD
   extends: 'foam.u2.View',
-=======
-  extends: 'foam.u2.Element',
->>>>>>> d17a6d1b6072ea63eed6d51ff43402f5c31dee2b
 
   imports: [
     'auth',
@@ -25,18 +21,9 @@ foam.CLASS({
   ],
 
   css: `
-<<<<<<< HEAD
     ^ table > tbody:nth-child(odd) {
       background: #f6f9f9;
-=======
-    ^ .net-nanopay-ui-ActionView{
-      width: 95.5%;
-      height: 40px;
-      background: #59aadd;
-      margin-bottom: 15px;
-      width: 200;
->>>>>>> d17a6d1b6072ea63eed6d51ff43402f5c31dee2b
-    }
+    } 
   `,
 
   methods: [
@@ -44,7 +31,6 @@ foam.CLASS({
       this.SUPER();
       var self = this;
 
-<<<<<<< HEAD
       this.addClass(this.myClass()).start('table')
         .start('thead').start('tr').style({'background': '#D4E3EB'})
           .tag('td').style({'text-align': 'left', 'width': '480', 'height': '35'})
@@ -65,28 +51,6 @@ foam.CLASS({
             })
         })
         .end()
-=======
-      this.start('table')
-        .start('tr')
-          .tag('td').style({'text-align': 'left', 'width': '480'})
-          .select(this.groupDAO.orderBy(this.Group.ID), function(g) {
-            this.start('td').start().style({'text-align': 'center', 'width': '100'}).add(g.id).end().end();
-          })
-        .end()
-        .select(this.permissionDAO.orderBy(this.Permission.ID), function(p) {
-          this.start('tr')
-            .start('td').style({'text-align': 'left', 'width': '480'}).add(p.id).end()
-            .select(self.groupDAO.orderBy(self.Group.ID), function(g) {
-              var cb = foam.u2.md.CheckBox.create({data: self.checkPermissionForGroup(p.id, g)});
-              cb.data$.sub(function() { self.updateGroup(p, g, cb.data); });
-              this.start('td').style({'text-align': 'center', 'width': '100'}).tag(cb).call(function() {
-                if ( g.implies(p.id)  ) { cb.style({'border-color': '#40C75B'}) };
-              })
-              .end();
-            })
-            .end()
-        })
->>>>>>> d17a6d1b6072ea63eed6d51ff43402f5c31dee2b
       .end();
     },
 
