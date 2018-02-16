6d062baf1cc10c7b007a2848f44ac137da4c9c5e
pakkiraju <prad.akkiraju@gmail.com>
2018-02-16 14:14:36 -0500

    Fixing bug in regards to nanopay ticket #877

--- src/foam/u2/view/ScrollTableView.js
+++ src/foam/u2/view/ScrollTableView.js
@@ -65,7 +65,7 @@
               value$: this.skip$,
               extent$: this.limit$,
               height: 40*18+41, // TODO use window height.
-              width: 22,
+              width: 15,
               size$: this.daoCount$,
             })).
           end().
