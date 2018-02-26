9058b4f1281e00b6ad63b63f3233d21028c36195
Patrick <zahnno@gmail.com>
2018-02-26 16:36:12 -0500

    model browser printable

--- src/foam/doc/ModelBrowser.js
+++ src/foam/doc/ModelBrowser.js
@@ -7,7 +7,7 @@ foam.CLASS({
   requires: [
     'foam.doc.DocBorder',
     'foam.doc.ClassList',
-    'foam.doc.ClassDocView',
+    'foam.doc.SimpleClassView',
     'foam.doc.UMLDiagram',
     'foam.nanos.boot.NSpec'
   ],
@@ -24,9 +24,7 @@ foam.CLASS({
   properties: [
     {
       name: 'models',
-      factory: function(){
-        return Object.values(foam.USED);
-      }
+      value: []
     },
     {
       name: 'showOnlyProperties',
@@ -53,7 +51,7 @@ foam.CLASS({
     ^ .foam-doc-UMLDiagram canvas{
       width: 700px;
     }
-    ^ .foam-u2-view-TableView-foam-doc-AxiomInfo{
+    ^ .foam-u2-view-TableView-foam-doc-PropertyInfo{
       width: 700px;
       float: left;
       margin-top: 20px;
@@ -62,6 +60,14 @@ foam.CLASS({
     ^ .net-nanopay-ui-ActionView-printPage{
       margin-top: 20px;
     }
+    @media print{
+      .foam-u2-view-TableView-th-editColumns{
+        display: none;
+      }
+      ^ .net-nanopay-ui-ActionView-printPage{
+        display: none;
+      }
+    }
   `,
 
   methods: [
@@ -69,19 +75,33 @@ foam.CLASS({
       this.SUPER();
       var self = this;
 
+      this.nSpecDAO.select().then(function(d){
+        d.array.forEach(function(n){
+          var cls = JSON.parse(n.client);
+          if ( cls.of ){
+            self.models.push(cls.of)
+            self.models = Array.from(self.models)
+          }
+          if ( cls.class ) {
+            self.models.push(cls.class)
+            self.models = Array.from(self.models)
+          }
+        });
+      });
+
       this.start().addClass(this.myClass())
         .start('h2').add("Model Browser").end()
         .start().add(this.PRINT_PAGE).end()
         .add(this.slot(function(models) {
           return self.E().forEach(models, function(a) {
-            model = self.lookup(a.id, true);
+            model = foam.lookup(a, true);
             this.start()
             if( !model ) {
               return;
             }
             this.start().style({ 'font-size' : '20px', 'margin-top' : '20px'}).add("Model " + a).end()
             this.tag(self.UMLDiagram.create({ data: model }))
-            this.tag(self.ClassDocView.create({data: model }))
+            this.tag(self.SimpleClassView.create({data: model }))
             .end()
           })
         }))
@@ -97,6 +117,124 @@ foam.CLASS({
         window.print();
       }
     }
-  ]
+  ],
 
+});
+
+foam.CLASS({
+  package: 'foam.doc',
+  name: 'SimpleClassView',
+  extends: 'foam.u2.View',
+
+  requires: [
+    'foam.dao.ArrayDAO',
+    'foam.doc.AxiomInfo',
+    'foam.doc.PropertyInfo',
+    'foam.doc.ClassLink',
+    'foam.doc.Link',
+    'foam.u2.view.TableView'
+  ],
+
+  imports: [
+    'selectedAxiom',
+    'showInherited',
+    'showOnlyProperties'
+  ],
+
+  methods: [
+    function initE() {
+      this.SUPER();
+
+      var data = this.data;
+
+      this.
+          start('b').add(data.id).end().br().
+          // start('span').style({float:'right','font-size':'smaller'}).add(data.count_, ' created').end().br().
+          add('Extends: ');
+
+      var cls = data;
+      for ( var i = 0 ; cls ; i++ ) {
+        cls = this.lookup(cls.model_.extends, true);
+        if ( i ) this.add(' : ');
+        this.start(this.ClassLink, {data: cls}).end();
+        if ( cls === foam.core.FObject ) break;
+      }
+      this.br();
+      this.start(foam.u2.HTMLElement).style({ 'margin-top' : '10px'}).add('Documentation: ', data.model_.documentation).end();
+
+      this.add( this.slot(function (showInherited, showOnlyProperties) {
+        // TODO: hide 'Source Class' column if showInherited is false
+        var axs = [];
+        for ( var key in data.axiomMap_ ) {
+          if ( showInherited || Object.hasOwnProperty.call(data.axiomMap_, key) ) {
+            var a  = data.axiomMap_[key];
+	            if ( ( ! showOnlyProperties ) || foam.core.Property.isInstance(a) ) {
+
+                var ai = foam.doc.PropertyInfo.create({
+                  axiom: a,
+                  type: a.cls_,
+                  required: a.required,
+                  documentation: a.documentation,
+                  name: a.name
+                });
+              axs.push(ai);
+	          }
+          }
+        }
+
+        return this.TableView.create({
+          of: this.PropertyInfo,
+          data: this.ArrayDAO.create({array: axs})
+        });
+      }));
+    }
+  ]
+});
+
+foam.CLASS({
+  package: 'foam.doc',
+  name: 'PropertyInfo',
+  documentation: "Table view model to display model in printable model browser's",
+  ids: ['name'],
+
+  tableColumns: [
+    'name', 'required', 'type', 'documentation'
+  ],
+
+  properties: [
+    {
+      name: 'name',
+      tableCellFormatter: function(value, obj, axiom) {
+        this.add(value);
+      }
+    },
+    {
+      name: 'axiom',
+      hidden: true
+    },
+    {
+      name: 'required',
+      label: 'Required',
+      tableCellFormatter: function(value, obj, axiom) {
+        this.add(value);
+      }
+    },
+    {
+      name: 'type',
+      tableCellFormatter: function(value, obj, axiom) {
+        if ( value ) {
+          this.add(value.name);
+          return;
+        }
+        this.add('anonymous');
+      }
+    },
+    {
+      name: 'documentation',
+      label: 'Documentation',
+      tableCellFormatter: function(value, obj, axiom) {
+        this.add(value);
+      }
+    }
+  ]
 });
