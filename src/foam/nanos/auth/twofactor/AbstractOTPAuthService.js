f4a431e4a21d397b72937cbf1dd51feb4dead432
Kirk Eaton <kirk@nanopay.net>
2018-02-27 13:21:39 -0500

    Added algorithm parameter

--- src/foam/nanos/auth/twofactor/AbstractOTPAuthService.js
+++ src/foam/nanos/auth/twofactor/AbstractOTPAuthService.js
@@ -9,6 +9,19 @@ foam.CLASS({
     'foam.nanos.auth.twofactor.OTPAuthService'
   ],
 
+  javaImports: [
+    'javax.crypto.Mac',
+    'javax.crypto.spec.SecretKeySpec'
+  ],
+
+  properties: [
+    {
+      class: 'String',
+      name: 'algorithm',
+      value: 'SHA256'
+    }
+  ],
+
   methods: [
     {
       name: 'generateSecret',
@@ -23,6 +36,51 @@ foam.CLASS({
 `final byte[] bytes = new byte[size];
 java.util.concurrent.ThreadLocalRandom.current().nextBytes(bytes);
 return bytes;`
+    },
+    {
+      name: 'calculateCode',
+      javaReturns: 'long',
+      javaThrows: [
+        'java.security.InvalidKeyException',
+        'java.security.NoSuchAlgorithmException'
+      ],
+      args: [
+        {
+          name: 'key',
+          javaType: 'byte[]'
+        },
+        {
+          name: 'interval',
+          javaType: 'long'
+        }
+      ],
+      javaCode:
+`byte[] data = new byte[8];
+long value = interval;
+for ( int i = 8 ; i-- > 0; value >>>= 8 ) {
+  data[i] = (byte) value;
+}
+
+SecretKeySpec signKey = new SecretKeySpec(key, "Hmac" + getAlgorithm());
+Mac mac = Mac.getInstance("Hmac" + getAlgorithm());
+mac.init(signKey);
+byte[] hash = mac.doFinal(data);
+
+int offset = hash[20 - 1] & 0xF;
+
+// We're using a long because Java hasn't got unsigned int.
+long truncatedHash = 0;
+for ( int i = 0 ; i < 4 ; ++i ) {
+  truncatedHash <<= 8;
+  // We are dealing with signed bytes:
+  // we just keep the first byte.
+  truncatedHash |= (hash[offset + i] & 0xFF);
+}
+
+truncatedHash &= 0x7FFFFFFF;
+truncatedHash %= 1000000;
+
+return (int) truncatedHash;`
     }
   ]
 });
