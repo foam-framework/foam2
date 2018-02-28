033733b835339907300750b9de5beabb8a1ce932
Mark Dittmer <markdittmer@google.com>
2018-02-28 10:25:09 -0500

    ScrollDAOView bug fix: Perform remove-from-selection based on id property, not object identity

--- src/foam/u2/view/ScrollDAOView.js
+++ src/foam/u2/view/ScrollDAOView.js
@@ -350,14 +350,19 @@ foam.CLASS({
               }, this.selectionEnabled$, this.data$, this.selection$));
           this.on('click', function(evt) {
             if ( ! self.data ) return;
-            if ( self.selection.some(function(d) {
-                   return d.id === self.data.id;
-                 }) ) {
-              foam.Array.remove(self.selection, self.data);
-            } else {
-              self.selection.push(self.data);
+
+            var oldNum = self.selection.length;
+            var newSelection = self.selection.filter(function(d) {
+              return ! foam.util.equals(d.id, self.data.id);
+            });
+
+            // If length didn't change, add to selection. Otherwise, filter()
+            // already committed removal action.
+            if ( newSelection.length === oldNum ) {
+              newSelection.push(self.data);
             }
-            self.selection = Array.from(self.selection);
+
+            self.selection = newSelection;
           });
           this.columns$ && this.columns$.sub(this.render);
         }
