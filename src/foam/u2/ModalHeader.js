d751f54b3a6dbfbe4fc99f26b7275833da28df98
Patrick <zahnno@gmail.com>
2018-03-02 16:22:01 -0500

    testmodal rmved, modal header tweaked

--- src/foam/u2/ModalHeader.js
+++ src/foam/u2/ModalHeader.js
@@ -1,17 +1,12 @@
-
 foam.CLASS({
   package: 'foam.u2',
   name: 'ModalHeader',
   extends: 'foam.u2.View',
 
-  documentation: 'Modal Container close/title',
+  documentation: 'Modal Container close/title use in modal class to append title & close buttons.',
 
   imports: [
     'stack',
-    'closeDialog'
-  ],
-
-  exports: [
     'closeDialog'
   ],
 
@@ -20,70 +15,73 @@ foam.CLASS({
   ],
 
   css: `
-      ^{
-        width: 448px;
-        margin: auto;
-      }
-      ^ .container{
-        height: 40.8px;
-        background-color: #093649;
-        margin-bottom: 20px;
-      }
-      ^ .title{
-        height: 40px;
-        font-family: Roboto;
-        font-size: 14px;
-        line-height: 2.86;
-        text-align: left;
-        color: #ffffff;
-        margin-left: 19px;
-        display: inline-block;
-      }
-      ^ .close{
-        width: 24px;
-        height: 24px;
-        margin-top: 5px;
-        cursor: pointer;
-        position: relative;
-        top: 4px;
-        right: 20px;
-        float: right;
-      }
-      ^ .foam-u2-ActionView-closeModal{
-        position: relative;
-        right: -330px;
-        width: 50px;
-        height: 40px;
-        opacity: 0.01;
-      }
+    ^{
+      width: 448px;
+      margin: auto;
+    }
+    ^ .container{
+      height: 40.8px;
+      background-color: #093649;
+      margin-bottom: 20px;
+    }
+    ^ .title{
+      height: 40px;
+      font-family: Roboto;
+      font-size: 14px;
+      line-height: 2.86;
+      text-align: left;
+      color: #ffffff;
+      margin-left: 19px;
+      display: inline-block;
+    }
+    ^ .close{
+      width: 24px;
+      height: 24px;
+      margin-top: 5px;
+      cursor: pointer;
+      position: relative;
+      top: 4px;
+      right: 20px;
+      float: right;
+    }
+    ^ .foam-u2-ActionView-closeModal{
+      position: relative;
+      right: 0px;
+      width: 50px;
+      height: 40px;
+      background: transparent;
+      margin-top: 0;
+      top: 0;
+      right: 0;
+      border: none;
+    }
   `,
-  
+
   methods: [
     function initE(){
     this.SUPER();
     var self = this;
-    
+
     this
     .addClass(this.myClass())
       .start()
         .start()
           .start().addClass('container')
             .start().addClass('title').add(this.title).end()
-            .start({class:'foam.u2.tag.Image', data: 'ic-cancelwhite.svg'}).addClass('close')
-              .add(this.CLOSE_MODAL)
-            // .end()
+            .start(this.CLOSE_MODAL).addClass('close').end()
           .end()
         .end()
       .end()
-    } 
+    }
   ],
-    
+
   actions: [
     {
       name: 'closeModal',
+      icon: 'ic-cancelwhite.svg',
       code: function(X){
         X.closeDialog()
       }
     }
-  ] 
+  ]
 });
