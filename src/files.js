12a61030ae9ae9634263c034b71b0f53ef5b7434
Emre Ozel <emreozel@npadmins-MacBook-Pro-2.local>
2018-03-09 15:47:15 -0500

    deleted extra lines and comma

--- src/files.js
+++ src/files.js
@@ -500,7 +500,5 @@ FOAM_FILES([
   { name: "foam/crypto/hash/Hashable" },
   { name: "foam/crypto/sign/Signer" },
   { name: "foam/crypto/sign/Signable" },
-  { name: "foam/crypto/sign/SignedFObject" },
-
-
+  { name: "foam/crypto/sign/SignedFObject" }
 ]);
