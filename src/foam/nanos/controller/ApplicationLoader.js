265b45f131425549544465e17d54c474d83ed176
Mike Carcasole <mikecarcasole@gmail.com>
2018-03-13 18:46:35 -0400

    Make ApplicationLoader take an optional controller to load.

--- src/foam/nanos/controller/ApplicationLoader.js
+++ src/foam/nanos/controller/ApplicationLoader.js
@@ -8,17 +8,24 @@ foam.CLASS({
     'foam.mlang.Expressions',
   ],
   requires: [
-    'foam.nanos.controller.ApplicationController',
     'foam.box.HTTPBox',
     'foam.box.RetryBox',
     'foam.dao.RequestResponseClientDAO',
     'foam.nanos.boot.NSpec',
     'foam.dao.EasyDAO',
+    'foam.nanos.controller.ApplicationConfig',
+  ],
+  imports: [
+    'classloader',
   ],
   properties: [
     {
       name: 'view',
       value: 'Loading...',
+    },
+    {
+      name: 'controller',
+      value: 'foam.nanos.controller.ApplicationController',
     },
     {
       name: 'nSpecDAO',
@@ -41,77 +48,79 @@ foam.CLASS({
       name: 'promise',
       factory: function() {
         var self = this;
-        return new Promise(function(resolve) {
+        return self.classloader.load(self.controller).then(function(cls) {
+          return new Promise(function(resolve) {
 
-          var client = {
-            refines: 'foam.nanos.controller.ApplicationController',
-            requires: [
-              'foam.dao.EasyDAO',
-            ],
-            exports: [],
-            properties: [],
-            methods: [
-              function createDAO(config) {
-                config.daoType = 'MDAO'; // 'IDB';
-                config.cache   = true;
+            var client = {
+              refines: cls.model_.id,
+              requires: [
+                'foam.dao.EasyDAO',
+              ],
+              exports: [],
+              properties: [],
+              methods: [
+                function createDAO(config) {
+                  config.daoType = 'MDAO'; // 'IDB';
+                  config.cache   = true;
 
-                return this.EasyDAO.create(config);
-              }
-            ]
-          };
+                  return this.EasyDAO.create(config);
+                }
+              ]
+            };
 
-          var references = [];
+            var references = [];
 
-          // Force hard reload when app version updates
-          self.nSpecDAO.find("appConfig").then(function(spec) {
-            var appConfig = spec.service;
-            var version   = appConfig.version;
+            // Force hard reload when app version updates
+            self.nSpecDAO.find("appConfig").then(function(spec) {
+              var appConfig = spec.service;
+              var version   = appConfig.version;
 
-            if ( "CLIENT_VERSION" in localStorage ) {
-              var oldVersion = localStorage.CLIENT_VERSION;
-              if ( version != oldVersion ) {
+              if ( "CLIENT_VERSION" in localStorage ) {
+                var oldVersion = localStorage.CLIENT_VERSION;
+                if ( version != oldVersion ) {
+                  localStorage.CLIENT_VERSION = version;
+                  location.reload(true);
+                }
+              } else {
                 localStorage.CLIENT_VERSION = version;
-                location.reload(true);
               }
-            } else {
-              localStorage.CLIENT_VERSION = version;
-            }
-          });
+            });
 
-          self.nSpecDAO.where(self.EQ(self.NSpec.SERVE, true)).select({
-            put: function(spec) {
-              if ( spec.client ) {
-                client.exports.push(spec.name);
+            self.nSpecDAO.where(self.EQ(self.NSpec.SERVE, true)).select({
+              put: function(spec) {
+                if ( spec.client ) {
+                  client.exports.push(spec.name);
 
-                var json = JSON.parse(spec.client);
+                  var json = JSON.parse(spec.client);
 
-                references = references.concat(foam.json.references(self.__context__, json));
+                  references = references.concat(foam.json.references(self.__context__, json));
 
-                client.properties.push({
-                  name: spec.name,
-                  factory: function() {
-                    if ( ! json.serviceName ) json.serviceName = 'service/' + spec.name;
-                    if ( ! json.class       ) json.class       = 'foam.dao.EasyDAO'
-                    if ( ! json.daoType     ) json.daoType     = 'CLIENT';
-                    return foam.json.parse(json, null, this);
-                    //return foam.json.parseString(spec.client, this.__context__);
-                  }
+                  client.properties.push({
+                    name: spec.name,
+                    factory: function() {
+                      if ( ! json.serviceName ) json.serviceName = 'service/' + spec.name;
+                      if ( ! json.class       ) json.class       = 'foam.dao.EasyDAO'
+                      if ( ! json.daoType     ) json.daoType     = 'CLIENT';
+                      return foam.json.parse(json, null, this);
+                      //return foam.json.parseString(spec.client, this.__context__);
+                    }
+                  });
+                }
+              },
+              eof: function() {
+                Promise.all(references).then(function() {
+                  foam.core.Model.create(client).buildClass();
+
+                  var args = {};
+                  self.ApplicationConfig.getAxiomsByClass(foam.core.Property).forEach(function(p) {
+                    args[p.name] = self[p.name]
+                  })
+                  resolve(cls.create(args));
                 });
               }
-            },
-            eof: function() {
-              Promise.all(references).then(function() {
-                foam.core.Model.create(client).buildClass();
-
-                var args = {};
-                foam.nanos.controller.ApplicationConfig.getAxiomsByClass(foam.core.Property).forEach(function(p) {
-                  args[p.name] = self[p.name]
-                })
-                resolve(self.ApplicationController.create(args));
-              });
-            }
-          });
-        })
+            });
+          })
+        });
       },
     },
   ],
