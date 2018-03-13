631a6f89ca765830f1ad346dffe5de4048986dc5
Mike Carcasole <mikecarcasole@gmail.com>
2018-03-12 21:20:27 -0400

    Remove genmodel.

--- src/foam/apploader/ClassLoader.js
+++ src/foam/apploader/ClassLoader.js
@@ -135,16 +135,7 @@ have multiple classloaders running alongside eachother`
 
           return this.pending[id] = this.modelDAO.find(id).then(function(m) {
             if ( ! m ) return Promise.reject(new Error('Class Not Found: ' + id));
-
-            var cls = this.buildClass_(m, path);
-
-            if ( foam.core.GenModel.isInstance(m) ) {
-              return m.build(this.__context__).then(function(m) {
-                return this.buildClass_(m, path);
-              }.bind(this));
-            }
-
-            return cls;
+            return this.buildClass_(m, path);
           }.bind(this), function() {
             throw new Error("Failed to load class " + id);
           });
