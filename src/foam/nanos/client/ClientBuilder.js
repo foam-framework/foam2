26275d0e04cf1dd2bd731c76d61be72fb27d1a46
Mike Carcasole <mikecarcasole@gmail.com>
2018-03-14 11:26:24 -0400

    Bring the ClientBuilder back and remove the AppLoader. Initialize the AppController a little more lazily so we can construct the Client and use its subcontext when creating things.

---
+++ src/foam/nanos/client/ClientBuilder.js
@@ -0,0 +1,107 @@
+/**
+ * @license
+ * Copyright 2017 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.CLASS({
+  package: 'foam.nanos.client',
+  name: 'ClientBuilder',
+
+  implements: [
+    'foam.mlang.Expressions'
+  ],
+
+  requires: [
+    'foam.box.HTTPBox',
+    'foam.box.RetryBox',
+    'foam.dao.EasyDAO',
+    'foam.dao.RequestResponseClientDAO',
+    'foam.nanos.boot.NSpec',
+  ],
+
+  properties: [
+    {
+      name: 'nSpecDAO',
+      factory: function() {
+        // The client completely fails if nSpecDAO fails to load, so infinitely retry
+        // requests to nSpecDAO.
+        return this.RequestResponseClientDAO.create({
+          of: this.NSpec,
+          delegate: this.RetryBox.create({
+            maxAttempts: -1,
+            delegate: this.HTTPBox.create({
+              method: 'POST',
+              url: 'service/nSpecDAO'
+            })
+          })
+        });
+      }
+    },
+    {
+      name: 'promise',
+      factory: function() {
+        var self = this;
+        return new Promise(function(resolve) {
+          var client = {
+            package: 'foam.nanos.client',
+            name: 'Client',
+            exports: [],
+            properties: [],
+          };
+
+          var references = [];
+
+          // Force hard reload when app version updates
+          self.nSpecDAO.find("appConfig").then(function(spec) {
+            var appConfig = spec.service;
+            var version   = appConfig.version;
+
+            if ( "CLIENT_VERSION" in localStorage ) {
+              var oldVersion = localStorage.CLIENT_VERSION;
+              if ( version != oldVersion ) {
+                localStorage.CLIENT_VERSION = version;
+                location.reload(true);
+              }
+            } else {
+              localStorage.CLIENT_VERSION = version;
+            }
+          });
+
+          self.nSpecDAO.where(self.EQ(self.NSpec.SERVE, true)).select({
+            put: function(spec) {
+              if ( spec.client ) {
+                client.exports.push(spec.name);
+
+                var json = JSON.parse(spec.client);
+
+                references = references.concat(foam.json.references(self.__context__, json));
+
+                client.properties.push({
+                  name: spec.name,
+                  factory: function() {
+                    if ( ! json.serviceName ) json.serviceName = 'service/' + spec.name;
+                    if ( ! json.class       ) json.class       = 'foam.dao.EasyDAO'
+                    if ( ! json.daoType     ) json.daoType     = 'CLIENT';
+                    return foam.json.parse(json, null, this);
+                  }
+                });
+              }
+            },
+            eof: function() {
+              Promise.all(references).then(function() {
+                resolve(foam.core.Model.create(client));
+              });
+            }
+          });
+        })
+      },
+    },
+  ],
+
+  methods: [
+    function createModel() {
+      return this.promise;
+    }
+  ]
+});
