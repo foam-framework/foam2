262a74930bef60dd015b89f86e9b5ea448333f72
Patrick <zahnno@gmail.com>
2018-03-14 16:22:58 -0400

    dig web agent

--- src/foam/nanos/dig/DigWebAgent.java
+++ src/foam/nanos/dig/DigWebAgent.java
@@ -119,18 +119,24 @@ public class DigWebAgent
             dao = dao.inX(x);
             dataObj = parameters.getData();
             if ( dataObj != null ) {
+              foam.lib.json.Outputter outputterJson = new foam.lib.json.Outputter(OutputterMode.NETWORK);
+              outputterJson.setOutputDefaultValues(true);
+              outputterJson.setOutputClassNames(false);
               if ( dataObj instanceof Object[] ) {
                 Object[] objs = (Object[]) dataObj;
                 for ( int i = 0; i < objs.length; i++ ) {
                   FObject obj = (FObject) objs[i];
                   dao.put(obj);
                 }
+                outputterJson.output(objs);
+                out.println(outputterJson.toString());
               } else {
                 FObject obj = (FObject) dataObj;
                 dao.put(obj);
+                outputterJson.output(obj);
+                out.println(outputterJson.toString());
               }
               resp.setStatus(HttpServletResponse.SC_OK);
-              out.println("Success");
               return;
             }
           }
