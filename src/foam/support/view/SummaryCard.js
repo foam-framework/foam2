800ab29da02d192ded66f488c4271bd3341a88d7
Patrick <zahnno@gmail.com>
2018-03-16 11:35:41 -0400

    Summary Card update

--- src/foam/support/view/SummaryCard.js
+++ src/foam/support/view/SummaryCard.js
@@ -1,77 +1,59 @@
+
 foam.CLASS({
-	package:'foam.support.view',
-  name:'SummaryCard',
-	extends: 'foam.u2.View',
-	
-  css:`
-		^  
-		{
-			width: 228px;
-			height: 92px;
-			display:inline-block;
+  package: 'foam.support.view',
+  name: 'SummaryCard',
+  extends: 'foam.u2.View',
+
+  documentation: 'Cards for summary views',
+
+	css: `
+		^{
+			display: inline-block;
+			width: 20%;
+			background: white;
+			height: 100px;
+			vertical-align: top;
+			margin-left: 6px;
+			border-radius: 3px;
+			overflow: hidden;
 		}
-		^ .firstdiv 
-		{       
-			width: 223px;
-			height: 92px;
-			border-radius: 2px;
-			margin-left:0px;
-			margin-bottom:140px;
+		^ .label{
+			position: relative;
+			top: 35;
+			left: 10;
+			font-size: 12px;
 		}
-		^ .label
-		{
-			width: 15px;
-			height: 30px;
-			font-family: Roboto;
+		^ .count{
 			font-size: 30px;
 			font-weight: 300;
-			font-style: normal;
-			font-stretch: normal;
 			line-height: 1;
-			letter-spacing: 0.5px;
+			position: relative;
+			top: 20;
+			left: 20;
+		}
+		^ .amount{
+			line-height: 0.86;
 			text-align: left;
 			color: #093649;
-			padding-top:15px;
-			margin-left:5px;
+			opacity: 0.6;
+			float: right;
+			margin-right: 15px;
 		}
-		^ .statuslabel 
-		{
-			width: 37px;
-			height: 20px;
-			font-family: Roboto;
-			font-size: 12px;
-			font-weight: normal;
-			font-style: normal;
-			font-stretch: normal;
-			line-height: 1.67;
-			letter-spacing: 0.2px;
-			text-align: left;
-			color: #ffffff;
-			padding-left:10px;
-		}
-		^ .ticketCount 
-		{
-			width: 57px;
-			height: 20px;
-			border-radius: 100px;
-			background-color: #a4b3b8;
-			margin-left:5px;
-			margin-top:7px; 	         
-		}
-  `,
-	
-	methods: [
+	`,
+
+  properties: [
+    'count',
+    'status'
+ 	],
+
+  methods: [
     function initE(){
-			this
-			.addClass(this.myClass())
-			.start()
-				.start().addClass('firstdiv')
-					.start().add('1001').addClass('label').end()
-					.start().addClass('ticketCount')
-						.start().add('Status').addClass('statuslabel').end()
-					.end() 
-				.end()
-			.end()
-    }  
+      var self = this;
+      this
+        .addClass(this.myClass())
+          .start().addClass('count').add(this.count$).end()
+          .start().addClass(this.status + ' label special-status-tag').add(this.status).end()
+        .end()
+    },
   ]
 });
