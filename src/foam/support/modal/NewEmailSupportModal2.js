9d7f5babbd61b6a889e31a4b3b0a61c40e61b383
eozel83 <emreozel@gmail.com>
2018-03-20 11:13:25 -0400

    change delete btn

--- src/foam/support/modal/NewEmailSupportModal2.js
+++ src/foam/support/modal/NewEmailSupportModal2.js
@@ -19,7 +19,7 @@ foam.CLASS({
 
   css:`
     ^ {
-      height: 235px;
+      height: 230px;
     }
     ^ .label1 {
       width: 338px;
@@ -33,9 +33,9 @@ foam.CLASS({
       letter-spacing: 0.2px;
       text-align: center;
       color: #093649;
-      margin-left: 47px;
-      margin-bottom: 75px;
-      margin-top: 10px;
+      padding-left: 40px;
+      margin-bottom: 100px;
+      
     }
     ^ .Mask {
       width: 448px;
@@ -79,15 +79,17 @@ foam.CLASS({
       margin-top: 40px; 
     }
     ^ .div2 {
-      padding: 20px;
-      margin-top: 10px;
+      padding: 0 20px;
     }
     `,
 
     properties: [
       {
         class: 'String',
-        name: 'email'
+        name: 'email',
+        factory: function(){
+          return 'Disable'
+        }
       },
       {
         class: 'Long',
@@ -95,10 +97,7 @@ foam.CLASS({
       },
       {
         class: 'String',
-        name: 'status',
-        factory: function(){
-          return 'Disable'
-      }
+        name: 'status', 
     }
     ],
     
@@ -131,14 +130,23 @@ foam.CLASS({
       {
         name: 'deleteButton',
         label: 'Delete',
-        code: function() {
-          this.supportEmailDAO.put(this.email).then(function() {
-          }.bind(this));
-        }
-      },
+        //code: function() {
+          //this.supportEmailDAO.put(this.email).then(function() {
+          //}.bind(this));
+       // }
+      //},
        // code: function(){
          // console.log(this.email)
         //}
+        code: function(X){
+          var email = this.SupportEmail.create({
+            email: this.email,
+            //userId: this.user.id
+          })
+
+          this.supportEmailDAO.put(email);
+        }
+      },
       {
         name: 'closeModal',
         label: 'Close',
