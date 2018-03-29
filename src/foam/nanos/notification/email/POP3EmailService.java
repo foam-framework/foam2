0f76d7667c4b3e35a6d532fef92559438cd46abd
Patrick <zahnno@gmail.com>
2018-03-29 15:10:23 -0400

    rmved import

--- src/foam/nanos/notification/email/POP3EmailService.java
+++ src/foam/nanos/notification/email/POP3EmailService.java
@@ -1,25 +1,5 @@
 package foam.nanos.notification.email;
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Date;
-import java.util.Properties;
-
-import javax.mail.Address;
-import javax.mail.Folder;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Multipart;
-import javax.mail.NoSuchProviderException;
-import javax.mail.Part;
-import javax.mail.Session;
-import javax.mail.Store;
 import foam.core.ContextAwareSupport;
 import foam.nanos.NanoService;
 import foam.nanos.notification.email.POP3Email;
