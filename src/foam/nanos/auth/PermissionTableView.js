a2bb6232890a567008edd1456d415fdcc660726f
Minsun <minsun@nanopay.net>
2018-04-02 13:26:42 -0400

    added filtering and searching features on PermissionTableView

--- src/foam/nanos/auth/PermissionTableView.js
+++ src/foam/nanos/auth/PermissionTableView.js
@@ -7,7 +7,7 @@
 foam.CLASS({
   package: 'foam.nanos.auth',
   name: 'PermissionTableView',
-  extends: 'foam.u2.View',
+  extends: 'foam.u2.Controller',
 
   imports: [
     'auth',
@@ -17,7 +17,8 @@ foam.CLASS({
 
   requires: [
     'foam.nanos.auth.Group',
-    'foam.nanos.auth.Permission'
+    'foam.nanos.auth.Permission',
+    'foam.u2.search.SearchManager'
   ],
 
   css: `
@@ -26,32 +27,88 @@ foam.CLASS({
     }
   `,
 
+  properties: [
+    {
+      class: 'String',
+      name: 'query',
+      view: {
+        class: 'foam.u2.TextField',
+        type: 'search',
+        placeholder: 'Search',
+        onKey: true
+      }
+    },
+    {
+      name: 'selected'
+    },
+    {
+      name: 'columns_'
+    },
+    {
+      name: 'rows_'
+    },
+    {
+      name: 'table'
+    }
+  ],
+
   methods: [
     function initE() {
       this.SUPER();
       var self = this;
+      this.selected = []
+      this.columns_ = []
 
-      this.addClass(this.myClass()).start('table')
-        .start('thead').start('tr').style({'background': '#D4E3EB'})
-          .tag('td').style({'text-align': 'left', 'width': '480', 'height': '35'})
-            .select(this.groupDAO.orderBy(this.Group.ID), function(g) {
-              this.start('td').start().style({'text-align': 'center', 'width': '100'}).add(g.id).end();
-            })
-        .end().end()
+      this.query$.sub(function() {
+        self.filterPermission(self, self.query);
+      });
 
-        .select(this.permissionDAO.orderBy(this.Permission.ID), function(p) {
-          this.start('tr')
-            .start('td').style({'text-align': 'left', 'width': '480', 'padding-left': '8px'}).add(p.id).end()
-            .select(self.groupDAO.orderBy(self.Group.ID), function(g) {
-                var cb = foam.u2.md.CheckBox.create({data: self.checkPermissionForGroup(p.id, g)});
-                cb.data$.sub(function() { self.updateGroup(p, g, cb.data); });
-                this.start('td').style({'text-align': 'center', 'width': '100'}).tag(cb).call(function() {
-                  if ( g.implies(p.id) ) { cb.style({'border-color': '#40C75B'}) };
-                }).end()
-            })
-        })
+      this.start('table')
+        .start('tbody')
+          .start('tr')
+            .start('td').style({'display': 'block', 'padding': '30'})
+              .start('h2').add('Permission').end()
+              .add('Search: ').start(this.QUERY).end()
+              .tag('br').tag('br').tag('br')
+              .add('Groups: ').tag('br')
+
+              .start().select(self.groupDAO.orderBy(self.Group.ID), function(g) {
+                var cbGroup = foam.u2.md.CheckBox.create({label: g.id, data: true});
+                this.tag(cbGroup).tag('br')
+
+                self.columns_.push(g.id);
+                self.selected.push(cbGroup.data$);
+                cbGroup.data$.sub(function() { self.filterGroup(g, self); });
+              }).end()
+
+              .start('td').style({'padding-top': '50'}).call(function() { self.td = this; })
+                .addClass(this.myClass())
+                  .start('table').call(function() { self.table = this; })
+                    .start('thead')
+                      .start('tr').style({'background': '#D4E3EB'})
+                        .tag('td').style({'text-align': 'left', 'width': '480', 'height': '35'})
+                        .select(this.groupDAO.orderBy(this.Group.ID), function(g) {
+                          this.start('td').call(function() { self.groupHead = this; }).addClass(g.id).start().style({'text-align': 'center', 'width': '100'}).add(g.id).end().end();
+                        }).end()
+                      .end()
+                      .select(this.permissionDAO.orderBy(this.Permission.ID), function(p) {
+                        this.start('tr')
+                          .start('td').style({'text-align': 'left', 'width': '480', 'padding-left': '8px'}).add(p.id).end()
+                          .select(self.groupDAO.orderBy(self.Group.ID), function(g) {
+                              var cb = foam.u2.md.CheckBox.create({data: self.checkPermissionForGroup(p.id, g)});
+                              cb.data$.sub(function() { self.updateGroup(p, g, cb.data); });
+                              this.start('td').call(function() { self.groupBody = this; }).style({'text-align': 'center', 'width': '100'}).tag(cb).call(function() {
+                                if ( g.implies(p.id) ) { cb.style({'border-color': '#40C75B'}) };
+                              }).end()
+                          })
+                        .end()
+                      })
+                    .end()
+                .end()
+            .end()
         .end()
-      .end();
+      .end()
+    .end();
     },
 
     function checkPermissionForGroup(permissionId, group) {
@@ -76,6 +133,64 @@ foam.CLASS({
         group.permissions = permissions;
         dao.put(group);
       });
+    },
+
+    function showTable(self, columns_, rows_) {
+      this.table.remove();
+
+      this.td.addClass(this.myClass())
+      .start('table').call(function() { self.table = this; })
+        .start('thead')
+          .start('tr').style({'background': '#D4E3EB'})
+            .tag('td').style({'text-align': 'left', 'width': '480', 'height': '35'})
+            .forEach(columns_, function(g) {
+                this.start('td').start().style({'text-align': 'center', 'width': '140'}).add(g).end();
+            })
+            .end()
+           .end()
+
+        .select(this.permissionDAO.orderBy(this.Permission.ID), function(p) {
+          if ( rows_ == null || p.id.indexOf(rows_) != -1 ) {
+            this.start('tr')
+              .start('td').style({'text-align': 'left', 'width': '480', 'padding-left': '8px'}).add(p.id).end()
+
+              .select(self.groupDAO.orderBy(self.Group.ID), function(g) {
+                    for ( var j = 0 ; j < columns_.length ; j++ ) {
+                      if ( ( columns_[j] == g.id ) ) {
+                        var cb = foam.u2.md.CheckBox.create({data: self.checkPermissionForGroup(p.id, g)});
+                        cb.data$.sub(function() { self.updateGroup(p, g, cb.data); });
+                            this.start('td').style({'text-align': 'center', 'width': '120'}).tag(cb).call(function() {
+                              if ( g.implies(p.id) ) { cb.style({'border-color': '#40C75B'}) };
+                            }).end()
+                      }
+                   }
+              })
+          .end()
+        }
+        })
+      .end();
+     }
+  ],
+
+  listeners: [
+    function filterGroup(group, self) {
+      this.columns_ = []
+
+      for ( var i = 0 ; i < this.selected.length ; i++ ) {
+        var cbGroupData = this.selected[i].obj.data;
+        var curProp = this.selected[i].obj.label;
+
+        if ( cbGroupData ) {
+          this.columns_.push(curProp);
+        }
+      }
+
+      this.showTable(self, this.columns_, this.rows_);
+    },
+
+    function filterPermission(self, rows) {
+      this.rows_ = rows;
+      this.showTable(self, this.columns_, rows);
     }
   ]
 });
