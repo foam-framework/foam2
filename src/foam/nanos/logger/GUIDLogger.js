75776821c31fdff817b6f067dbf1d8735c76e95f
xuerongCode <xuerong.work@gmail.com>
2018-04-03 18:07:25 -0400

    resolve wrong reference bug cause by this

---
+++ src/foam/nanos/logger/GUIDLogger.js
@@ -0,0 +1,111 @@
+foam.CLASS({
+  package: 'foam.nanos.logger',
+  name: 'GUIDLogger',
+
+  imports: [
+    'logDAO',
+    'user'
+  ],
+
+  propertes: [
+    {
+      name: 'self',
+      factory: function() {
+        return this;
+      }
+    }
+  ],
+
+  requires: [
+    'foam.nanos.logger.Log'
+  ],
+
+  methods: [
+    function log() {
+      if (arguments.length > 1) {
+        var ret = ''
+        for (var i = 0 ; i < arguments.length ; i++ ) {
+          ret = ret + this.normalizeDetail(arguments[i]);
+        }
+        this.addToDAO('log', 'Please see details', ret);
+      } else if ( arguments.length == 1 ) {
+        var des = this.normalizeDescription(arguments[0]) || 'Please see details';
+        this.addToDAO('log', des, this.normalizeDetail(arguments[0]));
+      }
+    },
+    function warning(e) {
+      if (arguments.length > 1) {
+        var ret = ''
+        for (var i = 0 ; i < arguments.length ; i++ ) {
+          ret = ret + this.normalizeDetail(arguments[i]);
+        }
+        this.addToDAO('warning', 'Please see details', ret);
+      } else if ( arguments.length == 1 ) {
+        var des = this.normalizeDescription(arguments[0]) || 'Please see details';
+        this.addToDAO('warning', des, this.normalizeDetail(arguments[0]));
+      }
+    },
+    function info(e) {
+      if (arguments.length > 1) {
+        var ret = ''
+        for (var i = 0 ; i < arguments.length ; i++ ) {
+          ret = ret + this.normalizeDetail(arguments[i]);
+        }
+        this.addToDAO('info', 'Please see details', ret);
+      } else if ( arguments.length == 1 ) {
+        var des = this.normalizeDescription(arguments[0]) || 'Please see details';
+        this.addToDAO('info', des, this.normalizeDetail(arguments[0]));
+      }
+    },
+    function error(e) {
+      if (arguments.length > 1) {
+        var ret = ''
+        for (var i = 0 ; i < arguments.length ; i++ ) {
+          ret = ret + this.normalizeDetail(arguments[i]);
+        }
+        this.addToDAO('error', 'Please see details', ret);
+      } else if ( arguments.length == 1 ) {
+        var des = this.normalizeDescription(arguments[0]) || 'Please see details';
+        this.addToDAO('error', des, this.normalizeDetail(arguments[0]));
+      }
+    },
+    function debug(e) {
+      if (arguments.length > 1) {
+        var ret = ''
+        for (var i = 0 ; i < arguments.length ; i++ ) {
+          ret = ret + this.normalizeDetail(arguments[i]);
+        }
+        this.addToDAO('debug', 'Please see details', ret);
+      } else if ( arguments.length == 1 ) {
+        var des = this.normalizeDescription(arguments[0]) || 'Please see details';
+        this.addToDAO('debug', des, this.normalizeDetail(arguments[0]));
+      }
+    },
+    function normalizeDetail(e) {
+      if ( foam.core.Exception.isInstance(e) ) {
+        return '' + e + '\n' + '[name]: ' + e.name + '\n' + '[message]: ' + e.message + '\n';
+      } else if ( e instanceof Error ) {
+        return '' + e + '\n' + '[name]: ' + e.name + '\n' + '[message]: ' + e.message + '\n';
+      } else {
+        return '' + e + '\n';
+      }
+    },
+    function normalizeDescription(e) {
+      return (!! e.name) ? '' + e.name : undefined; 
+    },
+    function addToDAO(type, description, detail) {
+      var l = this.generateLogModel(type, description, detail);
+      this.logDAO.put(l);
+    },
+    function generateLogModel(type, description, detail) {
+      return this.Log.create({
+        time: new Date(),
+        from: 'Web',
+        belong: '' + this.user.id,
+        type: type,
+        description: description,
+        detail: detail
+      })
+    }
+  ]
+})
