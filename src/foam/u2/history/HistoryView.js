5668c67a2aa5e151294f2906b8b81812c45ac7f4
Patrick <zahnno@gmail.com>
2018-04-19 15:29:20 -0400

    added minor changes to history view

--- src/foam/u2/history/HistoryView.js
+++ src/foam/u2/history/HistoryView.js
@@ -57,6 +57,7 @@ foam.CLASS({
   properties: [
     'data',
     'historyItemView',
+    'records',
     { class: 'String', name: 'title', value: 'History' }
   ],
 
@@ -70,11 +71,105 @@ foam.CLASS({
         .call(function outputRecords() {
           // Gets records from DAO
           view.data.select().then(function(records) {
+            view.records = records;
             // Reverses records array for chronological output
-            view.forEach(records.array.reverse(), function(record) {
+            view.forEach(records.array.reverse(), function(record, index) {
               view.start('div')
                 .addClass('timelineRecord')
-                .start('div').addClass('timeline').end()
+                .callIf(index != view.records.array.length - 1, function(){
+                  this.start('div').addClass('timeline').end()
+                })
+                .call(function() {
+                  view.historyItemView.outputRecord(this, record)
+                })
+              .end();
+            })
+          })
+        });
+    }
+  ]
+});
+/**
+ * @license
+ * Copyright 2017 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.CLASS({
+  package: 'foam.u2.history',
+  name: 'HistoryView',
+  extends: 'foam.u2.View',
+  requires: [ 'foam.u2.history.HistoryItemView' ],
+
+  documentation: 'View displaying history',
+
+  css: `
+    ^ {
+      height: 370px;
+      background: white;
+      position: relative;
+      vertical-align: top;
+      border-radius: 2px;
+      overflow: auto;
+      font-size: 12px;
+      padding-left: 20px;
+      padding-right: 20px;
+      z-index: 0;
+    }
+    ^ h2 {
+      height: 20px;
+      opacity: 0.6;
+      font-family: Roboto;
+      font-size: 20px;
+      font-weight: 300;
+      font-style: normal;
+      font-stretch: normal;
+      line-height: 1;
+      letter-spacing: 0.3px;
+      text-align: left;
+      color: #093649;
+    }
+    ^ .timelineRecord {
+      position: relative;
+    }
+    ^ .timeline {
+      width: 2px;
+      height: 100%;
+      background: rgba(164, 179, 184, 0.3);
+      position: absolute;
+      left: 23px;
+      top: 5px;
+      z-index: -1;
+      margin-bottom: 20px;
+      content: '';
+    }
+  `,
+
+  properties: [
+    'data',
+    'historyItemView',
+    'records',
+    { class: 'String', name: 'title', value: 'History' }
+  ],
+
+  methods: [
+    function initE() {
+      var view = this;
+
+      this
+        .addClass(this.myClass())
+        .start('h2').add(this.title).end()
+        .call(function outputRecords() {
+          // Gets records from DAO
+          view.data.select().then(function(records) {
+            view.records = records;
+            // Reverses records array for chronological output
+            view.forEach(records.array.reverse(), function(record, index) {
+              view.start('div')
+                .addClass('timelineRecord')
+                .callIf(index != view.records.array.length - 1, function(){
+                  this.start('div').addClass('timeline').end()
+                })
                 .call(function() {
                   view.historyItemView.outputRecord(this, record)
                 })
