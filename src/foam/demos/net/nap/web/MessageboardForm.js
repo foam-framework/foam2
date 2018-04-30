69bbb38743c82f468d4cfd119edf0c2d8530a12e
Minsun <minsun@nanopay.net>
2018-04-30 11:51:35 -0400

    temp commit

--- src/foam/demos/net/nap/web/MessageboardForm.js
+++ src/foam/demos/net/nap/web/MessageboardForm.js
@@ -7,7 +7,7 @@
 foam.CLASS({
   package: 'foam.demos.net.nap.web',
   name: 'MessageboardForm',
-  extends: 'foam.u2.Controller',
+  extends: 'foam.u2.View',
 
   requires: [
     'foam.blob.BlobBlob',
@@ -17,23 +17,22 @@ foam.CLASS({
   ],
 
   imports: [
+    'user',
     'messageboard',
     'messageboardDAO',
     'stack',
     'blobService',
-    'onInvoiceFileRemoved',
-    'user',
+    'onInvoiceFileRemoved'
   ],
 
   exports: [
     'as data',
-    'onInvoiceFileRemoved'
   ],
 
   documentation: 'New Messageboard Form',
 
   tableColumns: [
-    'id', 'title', 'createDate', 'creator'
+    'id', 'title', 'createdDate', 'creator'
   ],
 
   properties: [
@@ -74,8 +73,6 @@ foam.CLASS({
       name: 'dragActive',
       value: false
     },
-    'fileNumber',
-    [ 'removeHidden', false ],
     [ 'uploadHidden', false ]
   ],
 
@@ -131,32 +128,6 @@ foam.CLASS({
     ^ .attachment-btn {
       margin: 10px 0;
     }
-    ^ .attachment-filename {
-      max-width: 342px;
-      overflow: hidden;
-      text-overflow: ellipsis;
-      white-space: nowrap;
-      float: left;
-    }
-    ^ .attachment-filename a {
-      height: 16px;
-      font-size: 12px;
-      line-height: 1.66;
-      letter-spacing: 0.2px;
-      text-align: left;
-      color: #59a5d5;
-      padding-left: 12px;
-    }
-    ^ .net-nanopay-invoice-ui-InvoiceFileView {
-      min-width: 175px;
-      max-width: 275px;
-      height: 40px;
-      background-color: #ffffff;  //#EEF0F5;
-      padding-left: 10px;
-      padding-right: 10px;
-      padding-top: 5px;
-    }
-
   `,
 
   methods: [
@@ -169,6 +140,7 @@ foam.CLASS({
           .start().addClass('left-actions')
             .start(this.BACK_ACTION).end()
             .start(this.SAVE_ACTION).end()
+            //.start().add('Save').on('click', this.save).end()
           .end()
 
           .start('table')
@@ -186,7 +158,7 @@ foam.CLASS({
             .end()
             .start('tr')
               .start('td').add('Creator').end()
-              .start('td').add(this.user.id).end()
+              .start('td').add(this.CREATOR).end()
             .end()
             .start('tr')
               .start('td').add('Content').end()
@@ -196,46 +168,37 @@ foam.CLASS({
               .start('td').add('Attachments').end()
               .start('td')
 
-              .start('div').addClass('box-for-drag-drop')
-                 .add(this.slot(function (data) {
-                   var e = this.E();
-                   for ( var i = 0 ; i < data.length ; i++ ) {
-                     e.tag({
-                       class: 'net.nanopay.invoice.ui.InvoiceFileView',
-                       data: data[i],
-                       fileNumber: i + 1,
-                     });
-                   }
-                   return e;
-                  }, this.data$))
-
-
-
-
-
-
-                .start('input').addClass('attachment-input')
-                  .attrs({
-                    type: 'file',
-                    accept: 'image/jpg,image/gif,image/jpeg,image/bmp,image/png,application/msword,application/pdf'
-                  })
-                  .on('change', this.onChange)
-                .end()
-
-                .start().addClass('attachment-btn white-blue-button btn')
+              // .start('div').addClass('box-for-drag-drop')
+              //    .add(this.slot(function (data) {
+              //      var e = this.E();
+              //      for ( var i = 0 ; i < data.length ; i++ ) {
+              //        // e.tag({
+              //        //   class: 'net.nanopay.invoice.ui.InvoiceFileView',
+              //        //   data: data[i],
+              //        //   fileNumber: i + 1,
+              //        // });
+              //      }
+              //      return e;
+              //    }, this.data$))
+              //
+              //
+              //
+              //
+              //
+              //   .start('input').addClass('attachment-input')
+              //     .attrs({
+              //       type: 'file',
+              //       accept: 'image/jpg,image/gif,image/jpeg,image/bmp,image/png,application/msword,application/pdf'
+              //     })
+              //     .on('change', this.onChange)
+              //   .end()
+              .start().addClass('attachment-btn')
                   .add('Choose File')
                   .on('click', this.onAddAttachmentClicked)
                 .end()
-
             .end()
           .end()
         .end();
-    },
-    function onInvoiceFileRemoved (fileNumber) {
-      alert(fileNumber);
-      this.document.querySelector('.attachment-input').value = null;
-      this.data.splice(fileNumber - 1, 1);
-      this.data = Array.from(this.data);
     }
   ],
 
@@ -245,30 +208,31 @@ foam.CLASS({
       label: 'Save',
       code: function(X) {
         var self = this;
-        //alert(foam.demos.net.nap.web.Messageboard);
 
         // if (!this.data.amount || this.data.amount < 0){
         //   this.add(foam.u2.dialog.NotificationMessage.create({ message: 'Please Enter Amount.', type: 'error' }));
         //   return;
         // }
 
-        var message = self.Messageboard.create({
+        var message = this.Messageboard.create({
           id : self.id,
           title: self.title,
           content: self.content,
-          creator : this.user.id,
-          createdDate : self.createdDate,
-          data : self.data
+          creator : self.creator,
+          createdDate : self.createdDate
         });
-        X.messageboardDAO.put(message).then(function() {
-          X.stack.push({ class: 'foam.demos.net.nap.web.MessageboardList' });
+
+        self.messageboardDAO.put(message).then(function() {
+          self.stack.push({ class: 'foam.demos.net.nap.web.MessageboardList' }, this);
         })
       }
     },
+
     {
       name: 'backAction',
       label: 'Back',
       code: function(X){
+        var self = this;
         X.stack.push({ class: 'foam.demos.net.nap.web.MessageboardList' });
       }
     }
@@ -289,21 +253,6 @@ foam.CLASS({
       var files = e.target.files;
       this.addFiles(files)
     },
-
-    // function addFile (file) {
-    //   if ( file.size > ( 2 * 1024 * 1024 ) ) {
-    //     this.add(this.NotificationMessage.create({ message: this.ErrorMessage, type: 'error' }));
-    //     return;
-    //   }
-    //   this.data = this.File.create({
-    //     filename: file.name,
-    //     filesize: file.size,
-    //     mimeType: file.type,
-    //     data: this.BlobBlob.create({
-    //       blob: file
-    //     })
-    //   });
-    // }
 
     function addFiles(files){
       var errors = false;
@@ -333,6 +282,28 @@ foam.CLASS({
       }
       this.data = Array.from(this.data);
     }
+    // ,
+    //
+    // function save() {
+    //   var self = this;
+    //
+    //   // if (!this.data.amount || this.data.amount < 0){
+    //   //   this.add(foam.u2.dialog.NotificationMessage.create({ message: 'Please Enter Amount.', type: 'error' }));
+    //   //   return;
+    //   // }
+    //
+    //   var message = this.Messageboard.create({
+    //     id : self.id,
+    //     title: self.title,
+    //     content: self.content,
+    //     creator : self.creator,
+    //     createdDate : self.createdDate
+    //   });
+    //
+    //   self.messageboardDAO.put(message).then(function(result) {
+    //     self.stack.push({ class: 'foam.demos.net.nap.web.MessageboardList' }, this);
+    //   })
+    // }
   ]
 
 });
