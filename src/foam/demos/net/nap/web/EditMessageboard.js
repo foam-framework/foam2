cb0f705002e6b579afbc2a021a4a1ef1f64dd99f
Minsun <minsun@nanopay.net>
2018-04-30 11:51:27 -0400

    temp commit

--- src/foam/demos/net/nap/web/EditMessageboard.js
+++ src/foam/demos/net/nap/web/EditMessageboard.js
@@ -18,12 +18,16 @@
   requires: [
     'foam.demos.net.nap.web.model.Messageboard',
     'foam.nanos.fs.File',
+    'foam.blob.BlobBlob',
+    'foam.u2.dialog.NotificationMessage'
   ],
 
   imports: [
+    'user',
     'stack',
     'messageboard',
-    'messageboardDAO'
+    'messageboardDAO',
+    'blobService'
   ],
 
   exports: [
@@ -55,249 +59,359 @@
       float: right;
       border-radius: 2px;
       background-color: %SECONDARYCOLOR%;
-      color: white;
-      margin-top: 10px;
-    }
-    ^ .net-nanopay-ui-ActionView-saveAction:hover {
-      background: %SECONDARYCOLOR%;
-      opacity: 0.9;
+      margin-bottom: 10px;
+      width: 140px;
+      height: 40px;
+      background-color: #59a5d5;
+      font-family: Roboto;
+      font-size: 14px;
+      font-weight: normal;
+      font-style: normal;
+      font-stretch: normal;
+      line-height: 2.86;
+      letter-spacing: 0.2px;
+      text-align: center;
+      color: #ffffff;
     }
     ^ .net-nanopay-ui-ActionView-backAction {
       border-radius: 2px;
       background-color: rgba(164, 179, 184, 0.1);
       box-shadow: 0 0 1px 0 rgba(9, 54, 73, 0.8);
-      margin-top: 10px;
     }
     ^ .net-nanopay-ui-ActionView-backAction:hover {
       background: lightgray;
     }
-
-
-
-
-
-
-
-    ^ .settingsBar {
-      width: 100%;
-      height: 40px;
-      line-height: 40px;
-      background-color: #FFFFFF;
-      margin-bottom: 20px;
+    ^ .attachment-btn {
+      margin-left: 5px;
     }
-    ^ .settingsBarContainer {
-      width: 992px;
-      margin: auto;
+    ^ .attachment-input {
+      width: 0.1px;
+      height: 0.1px;
+      opacity: 0;
+      overflow: hidden;
+      position: absolute;
+      z-index: -1;
     }
-    ^ .foam-u2-ActionView {
-      opacity: 0.6;
-      font-family: Roboto;
-      font-size: 14px;
-      font-weight: bold;
-      letter-spacing: 0.3px;
-      color: #093649;
-      padding: 0;
-      padding-left: 30px;
-      display: inline-block;
-      cursor: pointer;
-      margin: 0;
-      border: none;
-      background: transparent;
-      outline: none;
-      line-height: 40px;
+    ^ .box-for-drag-drop {
+      margin: 20px;
+      border: dashed 4px #edf0f5;
+      height: 100px;
+      width: 560px;
+      overflow: scroll;
     }
-    ^ .foam-u2-ActionView-personalProfile {
-      padding-left: 0;
+    ^ .boxless-for-drag-drop {
+      border: solid 4px white;
+      width: 90%;
+      height: 110px;
+      padding: 10px 10px;
+      position: relative;
     }
-    ^ .foam-u2-ActionView:hover {
-      background: white;
-      opacity: 1;
+    ^ .tableView {
+      ackground: #fafafa;
+      border: 1px solid grey;
     }
-    ^ .editBusinessContainer {
-      width: 992px;
-      margin: auto;
-    }
-    ^ h2{
-      opacity: 0.6;
-      font-family: Roboto;
-      font-size: 20px;
-      font-weight: 300;
-      line-height: 1;
-      letter-spacing: 0.3px;
+    ^ .foam-u2-PropertyView-label {
+      color: #444;
+      display: block;
+      float: left;
+      font-size: 13px;
+      padding: 4px 8px 4px 8px;
       text-align: left;
-      color: #093649;
-      margin-bottom: 50px;
+      vertical-align: top;
+      white-space: nowrap;
     }
-    ^registration-container{
-      background: white;
-      padding: 4px 25px;
-      margin-bottom: 20px;
+    ^ .foam-u2-PropertyView {
+      padding: 2px 8px 2px 6px;
     }
-    ^ h3{
-      font-size: 14px;
-      font-weight: bold;
-      letter-spacing: 0.2px;
+    ^ .net-nanopay-ui-ActionView-delete {
+      float: right;
+      margin-left: 10px;
     }
-    ^ img{
+    ^ .net-nanopay-ui-ActionView-delete {
+      background: #d55;
+      color: white;
+    }
+    ^ .uploadButtonContainer {
+      height: 80px;
       display: inline-block;
+      vertical-align: text-bottom;
+      margin-left: 40px;
     }
-    ^upload-button{
+    ^ .removeButtonContainer {
       display: inline-block;
-      width: 135px;
-      height: 40px;
-      border-radius: 2px;
-      background: white;
-      border: solid 1px #59a5d5;
-      color: #59a5d5;
-      position: relative;
-      top: -35;
-      right: -40;
-      font-size: 14px;
-      cursor: pointer;
-      outline: 0;
-    }
-    ^ p{
-      font-size: 10px;
-      color: #093649;
-      font-weight: 300;
-      display: inline-block;
-      position: relative;
-      right: 100;
-    }
-    ^ input{
-      width: 100%;
-      height: 40px;
-      margin-top: 7px;
-      padding: 10px;
-    }
-    ^ label{
-      font-weight: 300;
-      font-size: 14px;
-      color: #093649;
-    }
-    .input-container{
-      width: 46%;
-      display: inline-block;
-      margin-bottom: 20px;
-      margin-right: 15px;
-    }
-    .input-container-quarter{
-      width: 13%;
-      display: inline-block;
-      margin-bottom: 20px;
-      margin-right: 15px;
-    }
-    .input-container-third{
-      width: 30%;
-      display: inline-block;
-      margin-bottom: 20px;
-      margin-right: 15px;
-    }
-    .dropdown{
-      width: 200px;
-      height: 200px;
-      background: black;
-    }
-    ^ .foam-u2-tag-Select{
-      height: 40px;
-      width: 100%;
-      background: white;
-      border: 1px solid lightgrey;
+      vertical-align: text-bottom;
+      margin-left: 20px;
+      vertical-align: top;
       margin-top: 5px;
     }
-    ^ .foam-u2-ActionView-saveBusiness{
-      width: 100%;
-      height: 40px;
-      background: #59aadd;
-      margin-bottom: 15px;
-      outline: 0;
-      border: 0;
-      font-size: 14px;
-      color: white;
-      cursor: pointer;
-      opacity: 1;
+    ^ .attachment-number {
+      float: left;
+      width: 21px;
+      height: 20px;
+      font-size: 12px;
+      line-height: 1.67;
+      letter-spacing: 0.2px;
+      text-align: left;
+      color: #093649;
+      margin-right: 10px;
     }
-    ^ .foam-u2-ActionView-saveBusiness:hover {
-      background-color: #59aadd;
-    }
-    ^ .net-nanopay-ui-ActionView-closeButton {
-      width: 24px;
-      height: 35px;
-      margin: 0;
-      cursor: pointer;
-      display: inline-block;
-      float: right;
-      outline: 0;
-      border: none;
-      background: transparent;
-      box-shadow: none;
-      padding-top: 15px;
-      margin-right: 15px;
-    }
-    ^ .net-nanopay-ui-ActionView-closeButton:hover {
-      background: transparent;
-      background-color: transparent;
-    }
-    ^ .input-companytype-width select {
-      width: 100% !important;
+    ^ .attachment-filename {
+      max-width: 342px;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      float: left;
     }
 
-    ^ .input-businesssector-width select {
-      width: 100% !important;
-    }
-    ^ .input-businesssector-width select {
-      width: 100% !important;
-    }
-    ^ .business-image-container {
-      padding-bottom: 20px;
-    }
   `,
+
+  properties: [
+    {
+      class: 'foam.nanos.fs.FileArray',
+      name: 'messageboardFile',
+      value: this.exportData
+    },
+    'exportData',
+    {
+      class: 'Boolean',
+      name: 'dragActive',
+      value: false
+    },
+    [ 'uploadHidden', false ]
+  ],
+
+  messages: [
+    { name: 'UploadDesc', message: 'Or drag and drop an image here' },
+    { name: 'UploadRestrict', message: '* jpg, jpeg, or png only, 2MB maximum, 100*100 72dpi recommanded' },
+    { name: 'FileError', message: 'File required' },
+    { name: 'FileTypeError', message: 'Wrong file format' },
+    { name: 'ErrorMessage', message: 'Please upload an image less than 2MB' }
+  ],
 
   methods: [
     function initE() {
       this.SUPER();
       var self = this;
-      //this.data = this.messageboard;
+      var userId = this.user.id;
+      var userName = this.user.firstName;
+      var messageboardObj = this.data;
+      this.messageboardFile = Array.from(messageboardObj.messageboardFile);
 
       this
         .addClass(this.myClass())
         .start().addClass('actions')
           .start().addClass('left-actions')
             .start(this.BACK_ACTION).end()
-            .start(this.SAVE_ACTION).end()
+            .start(this.DELETE).addClass('net-nanopay-ui-ActionView-delete').end()
+            //.start(this.SAVE_ACTION).addClass('net-nanopay-ui-ActionView-saveAction').end()
+            .start().add('SAVE').addClass('net-nanopay-ui-ActionView-saveAction').on('click', this.onSave).end()
           .end()
             .start('table').addClass('tableView')
               .start('tr')
-                .start('td').add('Id').end()
-                .start('td').add(this.data.ID).end()
+                .start('td').addClass('foam-u2-PropertyView-label').add('Id').end()
+                .start('td').addClass('foam-u2-PropertyView').add(this.data.ID).end()
               .end()
               .start('tr')
-                .start('td').add('Title').end()
-                .start('td').add(this.data.TITLE).end()
+                .start('td').addClass('foam-u2-PropertyView-label').add('Title').end()
+                .start('td').addClass('foam-u2-PropertyView').add(this.data.TITLE).end()
               .end()
               .start('tr')
-                .start('td').add('Date').end()
-                .start('td').add(this.data.CREATED_DATE).end()
+                .start('td').addClass('foam-u2-PropertyView-label').add('Date').end()
+                .start('td').addClass('foam-u2-PropertyView').add(this.data.CREATED_DATE).end()
               .end()
               .start('tr')
-                .start('td').add('Creator').end()
-                .start('td').add(this.data.CREATOR).end()
+                .start('td').addClass('foam-u2-PropertyView-label').add('Creator').end()
+                .start('td').addClass('foam-u2-PropertyView').add(userName).end()
               .end()
               .start('tr')
-                .start('td').add('Content').end()
-                .start('td').add(this.data.CONTENT).end()
+                .start('td').addClass('foam-u2-PropertyView-label').add('Content').end()
+                .start('td').addClass('foam-u2-PropertyView').add(this.data.CONTENT).end()
               .end()
-              //.start().add(this.UPLOAD_BUTTON, { showLabel:true }).end()
+              .start('tr')
+                .start('td').addClass('foam-u2-PropertyView-label').add('Attachments').end()
+                .start('td')
+
+                .start().addClass('attachment-btn white-blue-button btn')
+                  .add('Choose File')
+                  .on('click', this.onAddAttachmentClicked)
+                .end()
+                //.start('div').addClass('box-for-drag-drop')
+                .start('div').addClass(this.dragActive$.map(function (drag) {
+                  return drag ? 'box-for-drag-drop':'boxless-for-drag-drop';
+                }))
+                  .add(this.slot(function (data) {
+                    var e = this.E();
+                    for ( var i = 0 ; i < data.length ; i++ ) {
+                      var fileData = messageboardObj.messageboardFile;
+                      //e.tag({
+                        e.start().addClass('attachment-number')
+                          .add(this.formatFileNumber(i+1))
+                        .end()
+                        .start().addClass('attachment-filename')
+                          .start('a')
+                            .attrs({
+                              href: this.messageboardFile$.map(function (fileData) {
+                                if ( data ) {
+                                    var blob = data.data;
+                                    var sessionId = localStorage['defaultSession'];
+                                    if ( self.BlobBlob.isInstance(blob) ) {
+                                      return URL.createObjectURL(blob.blob);
+                                    } else {
+                                      var url = '/service/httpFileService/' + data.id;
+                                      // attach session id if available
+                                      if ( sessionId ) {
+                                        url += '?sessionId=' + sessionId;
+                                      }
+                                      return url;
+                                    }
+                                }
+                                // else {
+                                //   alert('2');
+                                //   start().add(messageboardObj.messageboardFile[i].filename).end()
+                                // }
+                             }),
+                             target: '_blank'
+                           })  //attrs
+                           .add(this.slot(function (filename) {
+                             var len = filename.length;
+                             return ( len > 35 ) ? (filename.substr(0, 20) +
+                               '...' + filename.substr(len - 10, len)) : filename;
+                           }, this.messageboardFile[i].filename$))   //this.messageboardFile[i].filename$ messageboardObj
+                        .end()
+                     .end()
+                    //});
+                    }
+                    return e;
+                  }, this.messageboardFile$))
+                  .on('dragstart', this.onDragStart)
+                  .on('dragenter', this.onDragEnter)
+                  .on('dragover', this.onDragOver)
+                  .on('drop', this.onDrop)
+                  .start().addClass('uploadButtonContainer').hide(this.uploadHidden)
+                    .start('input').addClass('attachment-input')
+                      .attrs({
+                        type: 'file',
+                        accept: 'image/jpg,image/gif,image/jpeg,image/bmp,image/png,application/msword,application/pdf'
+                      })
+                      .on('change', this.onChange)
+                    .end()
+                  .end()
+              .end()
+              .end()
+
             .end()
             .end();
+    },
+
+    function formatFileNumber(i) {
+      return ('000' + i).slice(-3);
+    },
+
+    function formatFileSize() {
+      return Math.ceil(this.data.filesize / 1024) + 'K';
     }
   ],
 
   actions: [
+    // {
+    //   name: 'saveAction',
+    //   label: 'Save',
+    //   code: function(X) {
+    //     var self = this;
+    //
+    //     // if (!this.data.amount || this.data.amount < 0){
+    //     //   this.add(foam.u2.dialog.NotificationMessage.create({ message: 'Please Enter Amount.', type: 'error' }));
+    //     //   return;
+    //     // }
+    //
+    //     var message = foam.demos.net.nap.web.model.Messageboard.create({
+    //       id : this.data.id,
+    //       title: this.data.title,
+    //       content: this.data.content,
+    //       creator : this.data.creator,
+    //       createdDate : this.data.createdDate,
+    //       data : this.messageboardFile
+    //     });
+    //
+    //     X.messageboardDAO.put(message).then(function() {
+    //       X.stack.push({ class: 'foam.demos.net.nap.web.MessageboardList' });
+    //     });
+    //   }
+    // },
+
     {
-      name: 'saveAction',
-      label: 'Save',
-      code: function(X) {
+      name: 'backAction',
+      label: 'Back',
+      code: function(X){
+        X.stack.back();
+      }
+    },
+    {
+      name: 'delete',
+      label: 'Delete',
+      confirmationRequired: true,
+      code: function(X){
+        var self = this;
+
+        X.messageboardDAO.remove(this).then(function() {
+          X.stack.back();
+        });
+      }
+    }
+  ],
+
+  listeners: [
+    function onAddAttachmentClicked (e) {
+      this.document.querySelector('.attachment-input').click();
+    },
+
+    function onRemoveClicked (e) {
+      this.dragActive = false;
+      this.data = null;
+    },
+
+    function onChange (e) {
+      this.dragActive = false;
+      var files = e.target.files;
+      this.addFiles(files)
+    },
+
+    function onDragOver(e) {
+      this.dragActive = true;
+      e.preventDefault();
+    },
+
+    function addFiles(files){
+      var errors = false;
+      for ( var i = 0 ; i < files.length ; i++ ) {
+        // skip files that exceed limit
+        if ( files[i].size > ( 10 * 1024 * 1024 ) ) {
+          if ( ! errors ) errors = true;
+          this.add(this.NotificationMessage.create({ message: this.ErrorMessage, type: 'error' }));
+          continue;
+        }
+        var isIncluded = false
+        for ( var j = 0 ; j < this.data.length ; j++ ) {
+          if( this.data[j].filename.localeCompare(files[i].name) === 0 ) {
+            isIncluded = true;
+            break
+          }
+        }
+        if ( isIncluded ) continue ;
+        this.messageboardFile.push(this.File.create({
+          filename: files[i].name,
+          filesize: files[i].size,
+          mimeType: files[i].type,
+          data: this.BlobBlob.create({
+            blob: files[i]
+          })
+        }))
+      }
+      this.messageboardFile = Array.from(this.messageboardFile);
+      this.exportData = this.data;
+    },
+
+    function onSave(X) {
         var self = this;
 
         // if (!this.data.amount || this.data.amount < 0){
@@ -305,30 +419,19 @@
         //   return;
         // }
 
-        // var message = self.Messageboard.create({
-        //   id : this.data.id,
-        //   title: this.data.title,
-        //   content: this.data.content,
-        //   creator : this.data.creator,
-        //   createdDate : this.data.createdDate
-        // });
+        var message = self.Messageboard.create({
+          id : this.data.id,
+          title: this.data.title,
+          content: this.data.content,
+          creator : this.data.creator,
+          createdDate : this.data.createdDate,
+          messageboardFile : this.messageboardFile
+        });
 
-        X.messageboardDAO.put(this).then(function() {
-          //this.message = message;
-          X.stack.push({ class: 'foam.demos.net.nap.web.MessageboardList' });
+        this.messageboardDAO.put(message).then(function() {
+          self.stack.push({ class: 'foam.demos.net.nap.web.MessageboardList' });
         });
       }
-    },
-    {
-      name: 'backAction',
-      label: 'Back',
-      code: function(X){
-        X.stack.back();
-        //alert("1 : " + this.stack);
-        //this.stack.push({ class: 'foam.demos.net.nap.web.MessageboardList' });
-        //this.stack.back();
-      }
-    }
   ]
 
 });
