23ab88838e2f8a58c4d1d533fe57b77e2d31581a
deejes <j.devansh29@gmail.com>
2018-05-07 17:08:03 -0400

    add first working RL with enums for LogLevels

--- src/foam/nanos/logger/RepeatLogger.js
+++ src/foam/nanos/logger/RepeatLogger.js
@@ -9,6 +9,9 @@ foam.CLASS({
   package: 'foam.nanos.logger',
   name: 'DiscardLogger',
   extends: 'foam.nanos.logger.ProxyLogger',
+  requires: [
+    'foam.nanos.logger.LogLevel',
+  ],
   properties: [
     {
       class: 'Object',
@@ -19,6 +22,12 @@ foam.CLASS({
       class: 'Int',
       name: 'repeatCount',
       value: 0
+    },
+    {
+      class: 'Enum',
+      of: 'foam.nanos.logger.LogLevel',
+      name: 'lastLogLevel',
+      factory: function() { return foam.nanos.logger.LogLevel.INFO; }
     }
   ],
 
@@ -33,22 +42,54 @@ foam.CLASS({
       ],
       javaReturns: 'void',
       javaCode: `
+      if ( getLastLogLevel() != LogLevel.INFO){
+          if (getRepeatCount() > 1){
+            switch ( getLastLogLevel() ) {
+              case DEBUG: {
+                getDelegate().debug("previous count repeated " + getRepeatCount() +" times");
+                break;
+              }
+              case INFO:  {
+                getDelegate().info("previous count repeated " + getRepeatCount() +" times");
+                break;
+              }
+              case WARNING:  {
+                getDelegate().warning("previous count repeated " + getRepeatCount() +" times");
+                break;      
+              }        
+              case ERROR:  {
+                getDelegate().error("previous count repeated " + getRepeatCount() +" times");
+                break;
+              }
+            }      
+          setRepeatCount(1);
+          setLastUniqueObject(args);
+          setLastLogLevel(LogLevel.INFO);
+          getDelegate().info(args);          
+          return;
+        };
+        }
+
+
         if ( getLastUniqueObject() == null){
           setLastUniqueObject(args);
           setRepeatCount(1);  
           getDelegate().info(args);
-          System.err.println(getRepeatCount());
+
         }
+
+
         else {
           Object[] o = (Object[]) getLastUniqueObject();
           if ( ! (args.length == o.length ) ){
+            // Different Message
             if (getRepeatCount() > 1){
               getDelegate().info("previous count repeated " + getRepeatCount() +" times");             
             }
             setLastUniqueObject(args);
             setRepeatCount(1);        
             getDelegate().info(args);
-            System.err.println(getLastUniqueObject());
+            // System.err.println(getLastUniqueObject());
           }
           else{
             for ( int i = 0; i < args.length; i ++) {
@@ -65,8 +106,87 @@ foam.CLASS({
             setRepeatCount(getRepeatCount()+1);
           }
         }
-      `
+      
+       `
+    },
+    {
+      name: 'error',
+      args:[
+        {
+          name: 'args',
+          javaType: 'Object...',
+        }
+      ],
+      javaReturns: 'void',
+      javaCode: `
+      if ( getLastLogLevel() != LogLevel.ERROR){
+          if (getRepeatCount() > 1){
+            switch ( getLastLogLevel() ) {
+              case DEBUG: {
+                getDelegate().debug("previous count repeated " + getRepeatCount() +" times");
+                break;
+              }
+              case INFO:  {
+                getDelegate().info("previous count repeated " + getRepeatCount() +" times");
+                break;
+              }
+              case WARNING:  {
+                getDelegate().warning("previous count repeated " + getRepeatCount() +" times");
+                break;     
+              }         
+              case ERROR:  {
+                getDelegate().error("previous count repeated " + getRepeatCount() +" times");
+                break;
+              }
+            }      
+          setRepeatCount(1);
+          setLastUniqueObject(args);
+          setLastLogLevel(LogLevel.ERROR);
+          getDelegate().error(args);
+          return;          
+          };
+        }
+
+
+        if ( getLastUniqueObject() == null){
+          setLastUniqueObject(args);
+          setRepeatCount(1);  
+          getDelegate().error(args);
+
+        }
+
+
+        else {
+          Object[] o = (Object[]) getLastUniqueObject();
+          if ( ! (args.length == o.length ) ){
+            // Different Message
+            if (getRepeatCount() > 1){
+              getDelegate().error("previous count repeated " + getRepeatCount() +" times");             
+            }
+            setLastUniqueObject(args);
+            setRepeatCount(1);        
+            getDelegate().error(args);
+            // System.err.println(getLastUniqueObject());
+          }
+          else{
+            for ( int i = 0; i < args.length; i ++) {
+              if  ( ! ( args[i].equals(o[i]) && !(args[i] instanceof Exception) )  ) {
+                if (getRepeatCount() > 1){
+                  getDelegate().error("previous count repeated " + getRepeatCount() +" times");             
+                }
+              setLastUniqueObject(args);
+              setRepeatCount(1);
+              getDelegate().error(args);
+              return;        
+              } 
+            }
+            setRepeatCount(getRepeatCount()+1);
+          }
+        }
+      
+       `
     }
+
 
   ]
 });
