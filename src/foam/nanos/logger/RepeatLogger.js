4f1542269326bb1f59034fdea0e091046d1354a2
deejes <j.devansh29@gmail.com>
2018-05-09 12:38:02 -0400

    move a break; in setLastLogLevel, prettify code in general

--- src/foam/nanos/logger/RepeatLogger.js
+++ src/foam/nanos/logger/RepeatLogger.js
@@ -12,6 +12,7 @@ foam.CLASS({
   requires: [
     'foam.nanos.logger.LogLevel'
   ],
+
   properties: [
     {
       class: 'Object',
@@ -28,186 +29,185 @@ foam.CLASS({
       of: 'foam.nanos.logger.LogLevel',
       name: 'lastLogLevel',
       factory: function() { return foam.nanos.logger.LogLevel.INFO; },
-      javaFactory: `return foam.nanos.logger.LogLevel.INFO;`
+      // javaFactory: `return foam.nanos.logger.LogLevel.INFO;`
     }
   ],
+
   methods: [
     {
-        name: 'newLogHandler',
-        args: [ 
-          {
+      name: 'newLogHandler',
+      args: [ 
+        {
           name: 'logLevelName',
           javaType: 'Enum'
-          },
-          {
-            name: 'setLastLogLevel',
-            javaType: 'Boolean'
-          },
-          {
-            name: 'args',
-            javaType: 'Object...'
-          },
-        ],
-        javaReturns: 'void',
-        javaCode: `
-        switch ( LogLevel.values()[logLevelName.ordinal()] ) {
-          case DEBUG: {
-              setRepeatCount(1);
-              setLastUniqueObject(args);
-              getDelegate().debug(args);  
-              if (setLastLogLevel){
-                setLastLogLevel(LogLevel.DEBUG);
-              break;
-          }
-              }
-              case INFO:  {
-                setRepeatCount(1);
-                setLastUniqueObject(args);
-                getDelegate().info(args);  
-                if (setLastLogLevel){
-                  setLastLogLevel(LogLevel.INFO);
-                }
-                break;
-              }
-              case WARNING:  {
-                setRepeatCount(1);
-                setLastUniqueObject(args);
-                getDelegate().warning(args);  
-                if (setLastLogLevel){
-                  setLastLogLevel(LogLevel.WARNING);
-                }
-                break;
-                
-                }     
-                case ERROR:  {
-                  setRepeatCount(1);
-                  setLastUniqueObject(args);
-                  getDelegate().error(args);  
-                  if (setLastLogLevel){
-                    setLastLogLevel(LogLevel.ERROR);
-                  }
-                break;
-                }
-              }
-    `  
+        },
+        {
+          name: 'setLastLogLevel',
+          javaType: 'Boolean'
+        },
+        {
+          name: 'args',
+          javaType: 'Object...'
+        },
+      ],
+      javaReturns: 'void',
+      javaCode: `
+  switch ( LogLevel.values()[logLevelName.ordinal()] ) {
+    case DEBUG: {
+      setRepeatCount(1);
+      setLastUniqueObject(args);
+      getDelegate().debug(args);  
+      if (setLastLogLevel){
+        setLastLogLevel(LogLevel.DEBUG);
+      }
+      break;
+    }
+    case INFO: {
+      setRepeatCount(1);
+      setLastUniqueObject(args);
+      getDelegate().info(args);  
+      if (setLastLogLevel){
+        setLastLogLevel(LogLevel.INFO);
+      }
+      break;
+    }
+    case WARNING: {
+      setRepeatCount(1);
+      setLastUniqueObject(args);
+      getDelegate().warning(args);  
+      if (setLastLogLevel){
+        setLastLogLevel(LogLevel.WARNING);
+      }
+      break;
+      
+    }     
+    case ERROR: {
+      setRepeatCount(1);
+      setLastUniqueObject(args);
+      getDelegate().error(args);  
+      if (setLastLogLevel){
+        setLastLogLevel(LogLevel.ERROR);
+      }
+      break;
+    }
+  }
+`  
     },
     {
       name: 'logLastLogRepeats',
       javaReturns: 'void',
       javaCode: 
       `
-      switch ( getLastLogLevel() ) {
-        case DEBUG: {
-          getDelegate().debug("The last log was repeated " + getRepeatCount() + " times" );
-          break;
-        }
-        case INFO:  {
-          getDelegate().info("The last log was repeated "+ getRepeatCount()  + " times" );
-          break;
-        }
-        case WARNING:  {
-          getDelegate().warning("The last log was repeated "+ getRepeatCount()  + " times" );
-          break;      
-        }        
-        case ERROR:  {
-          getDelegate().error("The last log was repeated " + getRepeatCount() + " times" );
-          break;
-        }
-      }     
-      `
+  switch ( getLastLogLevel() ) {
+    case DEBUG: {
+      getDelegate().debug("The last log was repeated " + getRepeatCount() + " times" );
+      break;
+    }
+    case INFO: {
+      getDelegate().info("The last log was repeated "+ getRepeatCount()  + " times" );
+      break;
+    }
+    case WARNING: {
+      getDelegate().warning("The last log was repeated "+ getRepeatCount()  + " times" );
+      break;      
+    }        
+    case ERROR: {
+      getDelegate().error("The last log was repeated " + getRepeatCount() + " times" );
+      break;
+    }
+  }     
+`
     },
     {
       name: 'repeatLogConstructor',
       args: [ 
         {
-        name: 'logLevelName',
-        javaType: 'Enum'
+          name: 'logLevelName',
+          javaType: 'Enum'
         },
         {
           name: 'args',
-          javaType: 'Object...',
+          javaType: 'Object...'
         }
       ],
       synchronized: true,
       javaReturns: 'void',
       javaCode: `
-        if ( getLastLogLevel().ordinal() != logLevelName.ordinal() ){
-              if (getRepeatCount() > 1){
-                logLastLogRepeats();   
-              }
-              newLogHandler(logLevelName,true,args);
-              return;
-            };
-            
-        if ( getLastUniqueObject() == null){
-          newLogHandler(logLevelName,false,args);
+  if ( getLastLogLevel().ordinal() != logLevelName.ordinal() ) {
+    if ( getRepeatCount() > 1 ) {
+      logLastLogRepeats();   
+    }
+    newLogHandler(logLevelName, true, args);
+    return;
+  };
+  
+  if ( getLastUniqueObject() == null ) {
+    newLogHandler(logLevelName,false,args);
+  } else {
+    Object[] o = (Object[]) getLastUniqueObject();
+    if ( ! (args.length == o.length ) ) {
+      // Different Message
+      if ( getRepeatCount() > 1 ) {
+        logLastLogRepeats();   
+      }
+      newLogHandler(logLevelName,false,args);
+    } else {
+      for ( int i = 0 ; i < args.length ; i++ ) {
+        if ( ! ( args[i].equals(o[i]) && !(args[i] instanceof Exception) ) ) {
+          // simplify above logic
+          if ( getRepeatCount() > 1 ) {
+            logLastLogRepeats();   
           }
-        else {
-          Object[] o = (Object[]) getLastUniqueObject();
-          if ( ! (args.length == o.length ) ){
-            // Different Message
-            if (getRepeatCount() > 1){
-              logLastLogRepeats();   
-            }
-            newLogHandler(logLevelName,false,args);
-          }
-             
-          else{
-            for ( int i = 0; i < args.length; i ++) {
-              if  ( ! ( args[i].equals(o[i]) && !(args[i] instanceof Exception) )  ) {
-                if (getRepeatCount() > 1){
-                  logLastLogRepeats();   
-                }
-                newLogHandler(logLevelName,false,args);
-                return;        
-              } 
-            }
-            setRepeatCount(getRepeatCount()+1);
-          }
-        }
-      `
+          newLogHandler(logLevelName, false, args);
+          return;        
+        } 
+      }
+      setRepeatCount( getRepeatCount() + 1 );
+    }
+  }
+`
     },
     {
       name: 'debug',
       args:[
         {
           name: 'args',
-          javaType: 'Object...',
+          javaType: 'Object...'
         }
       ],
       javaReturns: 'void',
       javaCode: `
-      Enum logLevelName = LogLevel.DEBUG;
-      repeatLogConstructor(logLevelName,args);
-      `   
+  Enum logLevelName = LogLevel.DEBUG;
+  repeatLogConstructor(logLevelName, args);
+  `   
     },
     {
       name: 'info',
       args:[
         {
           name: 'args',
-          javaType: 'Object...',
+          javaType: 'Object...'
         }
       ],
       javaReturns: 'void',
       javaCode: `
-      Enum logLevelName = LogLevel.INFO;
-      repeatLogConstructor(logLevelName,args);
-      `
+  Enum logLevelName = LogLevel.INFO;
+  repeatLogConstructor(logLevelName,args);
+`
     },
     {
       name: 'warning',
       args:[
         {
           name: 'args',
-          javaType: 'Object...',
+          javaType: 'Object...'
         }
       ],
       javaReturns: 'void',
       javaCode: `
-      Enum logLevelName = LogLevel.WARNING;
-      repeatLogConstructor(logLevelName,args);
-      `
+  Enum logLevelName = LogLevel.WARNING;
+  repeatLogConstructor(logLevelName,args);
+`
     },
     {
       name: 'error',
@@ -219,9 +219,9 @@ foam.CLASS({
       ],
       javaReturns: 'void',
       javaCode: `
-      Enum logLevelName = LogLevel.ERROR;
-      repeatLogConstructor(logLevelName,args);
-      `
+  Enum logLevelName = LogLevel.ERROR;
+  repeatLogConstructor(logLevelName,args);
+`
     }
   ]
 });
