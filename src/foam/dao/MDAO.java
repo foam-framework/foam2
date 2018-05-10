aa31889e3d42e9bd5c656f22ba4aff252f5480a5
Kevin Greer <kgrgreer@gmail.com>
2018-05-10 15:43:29 -0400

    Make Java MDAO thread-safe.

--- src/foam/dao/MDAO.java
+++ src/foam/dao/MDAO.java
@@ -16,6 +16,8 @@ import foam.mlang.predicate.Or;
 import foam.mlang.predicate.Predicate;
 import foam.mlang.sink.GroupBy;
 import java.util.ArrayList;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.List;
 
 /**
@@ -44,8 +46,9 @@ import java.util.List;
 public class MDAO
   extends AbstractDAO
 {
-  protected AltIndex index_;
-  protected Object   state_;
+  protected AltIndex      index_;
+  protected Object        state_;
+  protected ReadWriteLock lock_ = new ReentrantReadWriteLock();
 
   public MDAO(ClassInfo of) {
     setOf(of);
@@ -66,39 +69,49 @@ public class MDAO
   }
 
   public FObject put_(X x, FObject obj) {
-    FObject oldValue = find(obj);
+    synchronized ( lock_.writeLock() ) {
+      FObject oldValue = find(obj);
 
-    if ( oldValue != null ) {
-      state_ = index_.remove(state_, oldValue);
+      if ( oldValue != null ) {
+        state_ = index_.remove(state_, oldValue);
+      }
+
+      obj = obj.fclone();
+      obj.freeze();
+
+      state_ = index_.put(state_, obj);
+
+      return obj;
     }
-
-    obj = obj.fclone();
-    obj.freeze();
-
-    state_ = index_.put(state_, obj);
-
-    return obj;
   }
 
   public FObject remove_(X x, FObject obj) {
     if ( obj == null ) return null;
 
-    FObject found = find(obj);
+    synchronized ( lock_.writeLock() ) {
+      FObject found = find(obj);
 
-    if ( found != null ) {
-      state_ = index_.remove(state_, found);
+      if ( found != null ) {
+        state_ = index_.remove(state_, found);
+      }
+
+      return found;
     }
-
-    return found;
   }
 
   public FObject find_(X x, Object o) {
+    Object state;
+
+    synchronized ( lock_.readLock() ) {
+      state = state_;
+    }
+
     if ( o == null ) return null;
 
     return AbstractFObject.maybeClone(
         getOf().isInstance(o)
-            ? (FObject)index_.planFind(state_,getPrimaryKey().get(o)).find(state_,getPrimaryKey().get(o))
-            : (FObject)index_.planFind(state_, o).find(state_,o)
+            ? (FObject)index_.planFind(state, getPrimaryKey().get(o)).find(state, getPrimaryKey().get(o))
+            : (FObject)index_.planFind(state, o).find(state,o)
     );
   }
 
@@ -109,7 +122,13 @@ public class MDAO
     // use partialEval to wipe out such useless predicate such as: And(EQ()) ==> EQ(), And(And(EQ()),GT()) ==> And(EQ(),GT())
     if ( predicate != null ) simplePredicate = predicate.partialEval();
 
-    //Whe did or logic by seperate request from MDAO. We return different plan for each parameter of OR logic.
+    Object state;
+
+    synchronized ( lock_.readLock() ) {
+      state = state_;
+    }
+
+    // We handle OR logic by seperate request from MDAO. We return different plan for each parameter of OR logic.
     if ( simplePredicate instanceof Or ) {
       Sink dependSink = new ArraySink();
       // When we have groupBy, order, skip, limit such requirement, we can't do it separately so I replace a array sink to temporarily holde the whole data
@@ -118,21 +137,23 @@ public class MDAO
       List<Plan> planList = new ArrayList<>();
       for ( int i = 0; i < length; i++ ) {
         Predicate arg = ( (Or) simplePredicate ).getArgs()[i];
-        planList.add(index_.planSelect(state_, dependSink, 0, AbstractDAO.MAX_SAFE_INTEGER, null, arg));
+        planList.add(index_.planSelect(state, dependSink, 0, AbstractDAO.MAX_SAFE_INTEGER, null, arg));
       }
       plan = new OrPlan(simplePredicate, planList);
     } else {
-      plan = index_.planSelect(state_, sink, skip, limit, order, simplePredicate);
+      plan = index_.planSelect(state, sink, skip, limit, order, simplePredicate);
     }
 
     // TODO: if plan cost is >= size, log a warning
-    
-    plan.select(state_, sink, skip, limit, order, simplePredicate);
+
+    plan.select(state, sink, skip, limit, order, simplePredicate);
 
     return sink;
   }
 
   public void removeAll_(X x, long skip, long limit, Comparator order, Predicate predicate) {
-    state_ = null;
+    synchronized ( lock_.writeLock() ) {
+      state_ = null;
+    }
   }
 }
