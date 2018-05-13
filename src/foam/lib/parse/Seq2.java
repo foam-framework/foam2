33fd3d5a1538641cb5e40d51a8bade2eeef72e79
Kevin Greer <kgrgreer@gmail.com>
2018-05-13 12:33:29 -0400

    Removed TODO.

--- src/foam/lib/parse/Seq2.java
+++ src/foam/lib/parse/Seq2.java
@@ -17,9 +17,6 @@
 
 package foam.lib.parse;
 
-// TODO: this doesn't make any sense, just use Seq instead.
-// Seq1 is faster than Seq because it doesn't build an array, but this one
-// does so there's no reason for it to exist.
 public class Seq2
   implements Parser
 {
