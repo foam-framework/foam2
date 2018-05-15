53d48e4c6aa7b89fdbb257482a312c344a5f080a
Henry Han <henryhan@MacBook-Pro-2.local>
2018-05-15 15:08:30 -0400

    Fix the conflict in Ticket.js:

--- src/foam/support/model/Ticket.js
+++ src/foam/support/model/Ticket.js
@@ -14,78 +14,6 @@ foam.CLASS({
     'java.util.Date'
   ],
 
-<<<<<<< HEAD
-    tableColumns: [
-      'id', 'requestorEmail', 'subject', 'createdAt', 'status'
-    ],
-    exports: [
-      'subject'
-    ],
-    properties: [
-      {
-        class: 'Long',
-        name: 'id',
-        visibility: foam.u2.Visibility.RO,
-        label:'Ticket ID'
-      }, 
-      {
-        class: 'String',
-        name: 'requestorEmail'
-      },
-      {
-        class: 'String',
-        name: 'requestorName'
-      },
-      {
-        class: 'String',
-        name: 'subject',
-        label:'Subject',
-      },
-      {
-        class: 'String',
-        name: 'publicMessage'
-      },
-      {
-        class: 'Date',
-        name: 'createdAt',
-        visibility: foam.u2.Visibility.RO,
-        label: 'Time',
-        factory: function(){
-           return new Date();
-        },
-        javaFactory: 'return new Date();',
-        tableCellFormatter: function(state, obj, rel){
-          if(!state) return;
-          var locale = "en-us";
-          var month = state.toLocaleString(locale, {month: "short"});
-          var date=state.getDate();
-          var year=state.getFullYear();
-          this.start().add(month+" "+date+", "+year).end();
-        }
-      },
-      {
-        class: 'String',
-        name: 'internalNote'
-      },
-      {
-        class: 'String',
-        name: 'status',
-        label:'Status',
-        factory: function(){
-          return 'New'
-        },
-        tableCellFormatter: function(state, obj, rel) {
-           this.start()
-              .start().add(state).addClass('generic-status '+ state).end()
-           .end()
-        }
-      },
-      {
-        class: 'Long',
-        name: 'emailId'
-      }
-    ]
-=======
   tableColumns: [
     'id', 'requestorEmail', 'subject', 'createdAt', 'status'
   ],
@@ -159,7 +87,6 @@ foam.CLASS({
       name: 'userId'
     }
   ]
->>>>>>> dba75e58a364f5f89a57f13a6ec1bb5a21a61141
 });
 
 foam.RELATIONSHIP({
