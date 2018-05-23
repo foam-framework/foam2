d772298ae52075abeb6f92dff1adcf1d164e5ab8
Kevin Greer <kgrgreer@gmail.com>
2018-05-23 14:51:18 -0400

    Fix wrapCSS() which was broken by changing lets to vars.

--- src/foam/nanos/controller/ApplicationController.js
+++ src/foam/nanos/controller/ApplicationController.js
@@ -250,10 +250,10 @@ foam.CLASS({
           });
         }
 
-        var eid = foam.u2.Element.NEXT_ID();
+        let eid = foam.u2.Element.NEXT_ID();
 
         for ( var i = 0 ; i < this.MACROS.length ; i++ ) {
-          var m     = this.MACROS[i];
+          let m     = this.MACROS[i];
           var text2 = this.expandShortFormMacro(this.expandLongFormMacro(text, m), m);
 
             // If the macro was found, then listen for changes to the property
