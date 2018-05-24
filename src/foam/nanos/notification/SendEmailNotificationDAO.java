b16cb94053d9cd8fe8f3a5f924ac9dd5e53cb217
penzital <penzital888@gmail.com>
2018-05-24 11:54:26 -0400

    review applied

--- src/foam/nanos/notification/SendEmailNotificationDAO.java
+++ src/foam/nanos/notification/SendEmailNotificationDAO.java
@@ -1,34 +1,32 @@
 package foam.nanos.notification;
 import foam.core.Detachable;
-import foam.dao.AbstractSink;
-import foam.nanos.notification.email.EmailMessage;
 import foam.core.FObject;
 import foam.core.X;
+import foam.dao.AbstractSink;
 import foam.dao.DAO;
 import foam.dao.ProxyDAO;
 import foam.nanos.app.AppConfig;
 import foam.nanos.auth.User;
-import foam.nanos.logger.Logger;
+import foam.nanos.notification.email.EmailMessage;
 import foam.nanos.notification.email.EmailMessage;
 import foam.nanos.notification.email.EmailService;
-
 import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
-
 import static foam.mlang.MLang.AND;
 import static foam.mlang.MLang.EQ;
 
-public class EmailEnabledDAO extends ProxyDAO {
+// If notification's emailEnabled is true, the decorator creates an email based on provided or default emailTemplate, sets the reciever based on notification userId/groupId/broadcasted 
+public class SendEmailNotificationDAO extends ProxyDAO {
 
-  public EmailEnabledDAO(DAO delegate) {
+  public SendEmailNotificationDAO(DAO delegate) {
     setDelegate(delegate);
   }
 
-  public EmailEnabledDAO(X x, DAO delegate) {
+  public SendEmailNotificationDAO(X x, DAO delegate) {
     setX(x);
     setDelegate(delegate);
   }
@@ -38,11 +36,8 @@ public class EmailEnabledDAO extends ProxyDAO {
     AppConfig config     = (AppConfig) x.get("appConfig");
     EmailService email      = (EmailService) x.get("email");
     ArrayList<String> users = new ArrayList<>();
-
     Notification notif = (Notification) obj;
     User user = (User) userDAO.find(notif.getUserId());
-
-
     Notification oldNotif = (Notification) getDelegate().find(obj);
 
     if ( oldNotif != null )
@@ -51,17 +46,15 @@ public class EmailEnabledDAO extends ProxyDAO {
     if ( ! notif.getEmailIsEnabled() )
       return super.put_(x, obj);
 
-
-    if ( notif.getEmailName() == "notification" ) {
+    if ( "notification".equals(notif.getEmailName()) ) {
       notif.getEmailArgs().put("type", notif.getNotificationType());
       notif.getEmailArgs().put("link", config.getUrl());
     }
 
     if ( notif.getUserId() != null ) {
-      if ( Arrays.asList(user.getDisabledNotifsEmail()).contains(notif.getNotificationType()) )
+      if ( Arrays.asList(user.getDisabledTopicsEmail()).contains(notif.getNotificationType()) )
         return super.put_(x,obj);
       users.add(user.getEmail());
-
     }
 
     if ( notif.getGroupId() != null ) {
@@ -69,7 +62,7 @@ public class EmailEnabledDAO extends ProxyDAO {
         @Override
         public void put(Object o, Detachable d) {
           User curUser = (User) o;
-          if ( ! Arrays.asList(curUser.getDisabledNotifsEmail()).contains(notif.getNotificationType()) && curUser.getDisabledNotifsEmail() != null )
+          if ( ! Arrays.asList(curUser.getDisabledTopicsEmail()).contains(notif.getNotificationType()) && curUser.getDisabledTopicsEmail() != null )
             users.add(curUser.getEmail());
         }
       });
@@ -80,7 +73,7 @@ public class EmailEnabledDAO extends ProxyDAO {
         @Override
         public void put(Object o, Detachable d) {
           User curUser = (User) o;
-          if ( ! Arrays.asList(curUser.getDisabledNotifsEmail()).contains(notif.getNotificationType()) && curUser.getDisabledNotifsEmail() != null )
+          if ( ! Arrays.asList(curUser.getDisabledTopicsEmail()).contains(notif.getNotificationType()) && curUser.getDisabledTopicsEmail() != null )
             users.add(curUser.getEmail());
         }
       });
@@ -92,9 +85,8 @@ public class EmailEnabledDAO extends ProxyDAO {
     try {
       email.sendEmailFromTemplate(user, message, notif.getEmailName(), notif.getEmailArgs());
     } catch(Throwable t) {
-      ((Logger) x.get(Logger.class)).error("Error sending notification email.", t);
+      System.out.println("Error sending notification email. Error: " + t);
     }
-
 
     return super.put_(x,notif);
 
