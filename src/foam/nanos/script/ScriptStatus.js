0637b5291dba4387ab38c23ff988577af73bb736
Shrey Mahendru <shreymahendru@gmail.com>
2018-05-25 15:16:16 -0400

    formatting...

--- src/foam/nanos/script/ScriptStatus.js
+++ src/foam/nanos/script/ScriptStatus.js
@@ -1,22 +1,22 @@
 foam.ENUM({
-    package: 'foam.nanos.script', 
-    name: 'ScriptStatus',
-    values: [
-        {
-            name:'SCHEDULED', 
-            label: 'Scheduled'
-        }, 
-        {
-            name:'UNSCHEDULED', 
-            label: 'Unscheduled'
-        },
-        {
-            name:'RUNNING',
-            label: 'Running'
-        }, 
-        {
-            name: 'ERROR', 
-            label: 'Error'
-        }
-    ]
+	package: 'foam.nanos.script', 
+	name: 'ScriptStatus',
+	values: [
+		{
+			name:'SCHEDULED', 
+				label: 'Scheduled'
+		}, 
+		{
+			name:'UNSCHEDULED', 
+			label: 'Unscheduled'
+		},
+		{
+			name:'RUNNING',
+			label: 'Running'
+		}, 
+		{
+			name: 'ERROR', 
+			label: 'Error'
+		}
+	]
 });
