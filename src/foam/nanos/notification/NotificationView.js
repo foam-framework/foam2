358cd2b3d3558ff17a33ea32b51f59fb390b5db5
penzital <penzital888@gmail.com>
2018-05-28 10:51:17 -0400

    expand

--- src/foam/nanos/notification/NotificationView.js
+++ src/foam/nanos/notification/NotificationView.js
@@ -3,11 +3,39 @@ foam.CLASS({
   name: 'NotificationView',
   extends: 'foam.u2.View',
   axioms: [ foam.pattern.Faceted.create() ],
+
+  css: `
+   .body {
+    word-wrap: break-word;
+    padding-bottom: 0;
+    padding-top: 20;
+    line-height: 1.4;
+    padding-left: 20px;
+    width: 414px;
+    -webkit-line-clamp: 4;
+    -webkit-box-orient: vertical;
+    display: -webkit-box;
+    text-overflow: ellipsis;
+    margin-right: 10;
+    overflow: hidden;
+    color: #093649;
+  }`,
+
   properties: [ 'of' ],
   methods: [
     function initE() {
       this
-        .addClass(this.myClass());
+        .addClass(this.myClass())
+        .start('div').on('click', this.onClick).addClass('body').add(this.data.body).end();
+    }
+  ],
+  listeners: [
+    function onClick () {
+      if ( this.getElementById(this.childNodes[0].id).style.display == "block" ) {
+        this.getElementById(this.childNodes[0].id).style.display = "-webkit-box";
+      }
+      else
+        this.getElementById(this.childNodes[0].id).style.display = "block";
     }
   ]
 })
