68ff8fd7579a9c3a1ee5c018c5c6477a0f019bfb
shreymahendru <shreymahendru@gmail.com>
2018-05-30 13:47:03 -0400

    default script status set to Unscheduled

--- src/foam/nanos/script/Script.js
+++ src/foam/nanos/script/Script.js
@@ -30,7 +30,8 @@ foam.CLASS({
     'java.io.PrintStream',
     'java.util.Date',
     'java.util.List',
-    'static foam.mlang.MLang.*'
+    'static foam.mlang.MLang.*',
+    'foam.nanos.script.ScriptStatus'
   ],
 
   tableColumns: [
@@ -85,7 +86,8 @@ foam.CLASS({
       visibility: foam.u2.Visibility.RO,
       factory: function() {
         return this.ScriptStatus.UNSCHEDULED;
-      }
+      },
+      javaValue: 'ScriptStatus.UNSCHEDULED'
     },
     {
       class: 'String',
