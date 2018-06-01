d2b873f9a1ce5d0f4ba0a75aa61c898803c66848
penzital <penzital888@gmail.com>
2018-05-31 21:10:18 -0400

    formatting

--- src/foam/nanos/cron/RemoveExpiredNotificationCronjob.java
+++ src/foam/nanos/cron/RemoveExpiredNotificationCronjob.java
@@ -7,22 +7,12 @@ import foam.dao.AbstractSink;
 import foam.dao.DAO;
 import foam.mlang.MLang;
 import foam.nanos.notification.Notification;
-
 import java.util.Date;
-
-import static foam.nanos.http.Command.select;
-
 
 public class RemoveExpiredNotificationCronjob implements ContextAgent {
   @Override
   public void execute(X x){
     DAO notificationDAO = (DAO) x.get("notificationDAO");
-    notificationDAO.where(MLang.LTE(Notification.EXPIRY_DATE, new Date())).select(new AbstractSink() {
-      @Override
-      public void put(Object o, Detachable d) {
-        Notification notification = (Notification) o;
-        notificationDAO.remove(notification);
-      }
-    });
+    notificationDAO.where(MLang.LTE(Notification.EXPIRY_DATE, new Date())).removeAll();
   }
 }
