b10d6a1ae3e093251ef0a0e69827bc77ac961ead
Kevin Greer <kgrgreer@gmail.com>
2018-06-03 09:27:41 -0400

    Updated Fern demo.

--- src/com/google/foam/demos/fern/Fern.js
+++ src/com/google/foam/demos/fern/Fern.js
@@ -17,25 +17,27 @@ foam.CLASS({
       imports: [ 'timer' ],
       properties: [
         { class: 'Int', name: 'age' },
+        [ 'depth', 1 ],
         [ 'alpha', 0.8 ],
-        [ 'radius', 2 ],
+        [ 'radius', 1.2 ],
         [ 'color', 'green' ],
         [ 'border', null ]
       ],
       methods: [
         function initCView() {
-          this.timer.time$.sub(this.onTick);
-        },
+          if ( this.depth < 10 ) this.timer.time$.sub(this.onTick);
+        }
       ],
       listeners: [
         function onTick() {
           this.age++;
-          if ( this.age === 97 ) {
-            this.add(this.cls_.create({x: 0, y: -2, scaleX: 0.8, scaleY: 0.8, rotation: -Math.PI/20}, this.__context__));
+          this.rotation -= 0.0001;
+          if ( this.age === 60 ) {
+            this.add(this.cls_.create({depth: this.depth+1, x: 0, y: -2, scaleX: 0.8, scaleY: 0.8, xxxrotation: -Math.PI/20}, this.__context__));
           }
-          if ( this.age === 293 ) {
-            this.add(this.cls_.create({x: -2, y: 0, scaleX: 0.6, scaleY: 0.6, rotation:  Math.PI/2}, this.__context__));
-            this.add(this.cls_.create({x: +2, y: 0, scaleX: 0.6, scaleY: 0.6, rotation: -Math.PI/2}, this.__context__));
+          if ( this.age === 100 ) {
+            this.add(this.cls_.create({depth: this.depth+1.5, x: -1.5, y: 0, scaleX: 0.5, scaleY: 0.5, rotation:  Math.PI/2}, this.__context__));
+            this.add(this.cls_.create({depth: this.depth+1.5, x: +1.5, y: 0, scaleX: 0.5, scaleY: 0.5, rotation: -Math.PI/2}, this.__context__));
           }
         }
       ]
@@ -43,10 +45,9 @@ foam.CLASS({
   ],
 
   properties: [
-    [ 'width', 800 ],
-    [ 'height', 600 ],
-    [ 'color', 'black' ],
-    [ 'autorepaint' , true ],
+    [ 'width', 1000 ],
+    [ 'height', 800 ],
+    [ 'color', 'white' ],
     { class: 'Int', name: 'time' },
     'root'
   ],
