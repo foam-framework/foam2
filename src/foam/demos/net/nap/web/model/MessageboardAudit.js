895db62daccf39b62b5532c5aca0785c6a2b3abd
Minsun <minsun@nanopay.net>
2018-06-11 13:24:34 -0400

    messageboard is done

---
+++ src/foam/demos/net/nap/web/model/MessageboardAudit.js
@@ -0,0 +1,40 @@
+/**
+ * @license
+ * Copyright 2018 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.CLASS({
+  package: 'foam.demos.net.nap.web.model',
+  name: 'MessageboardAudit',
+
+  imports: [
+    'messageboardAuditDAO',
+    'user'
+  ],
+
+  properties: [
+    {
+      class: 'Long',
+      name: 'id'
+    },
+    {
+      class: 'Reference',
+      of: 'foam.nanos.auth.User',
+      name: 'viewer',
+      view: { class: 'foam.u2.view.ReferenceView', placeholder: 'select user' }
+    },
+    {
+      class: 'Long',
+      name: 'userId'
+    },
+    {
+      class: 'Long',
+      name: 'messageboardId'
+    },
+    {
+      class: 'Long',
+      name: 'totalCount'
+    }
+  ]
+});
