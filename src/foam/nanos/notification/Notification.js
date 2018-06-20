7110bea6b96a367c39a75078ba7d5867445c0898
deejes <devansh@nanopay.net>
2018-06-20 12:08:56 -0400

    add slack fields to notification model

--- src/foam/nanos/notification/Notification.js
+++ src/foam/nanos/notification/Notification.js
@@ -80,6 +80,18 @@ foam.CLASS({
     {
       class: 'Boolean',
       name: 'emailIsEnabled'
+    },
+    {
+      class: 'Boolean',
+      name: 'sendSlackMessage'
+    },
+    {
+      class: 'String',
+      name: 'slackWebhook'
+    },
+    {
+      class: 'String',
+      name: 'slackMessage'
     }
   ]
 });
