6eb70de9201e985bdd57896c14d369f41c185d35
Mike Carcasole <mikecarcasole@gmail.com>
2018-06-29 14:14:19 -0400

    Use the new journaling for countryDAO in foam2 nanos.

--- tools/classes.js
+++ tools/classes.js
@@ -244,7 +244,9 @@ var classes = [
   'foam.lib.query.TestModel',
   'foam.lib.query.FooEnum',
   'foam.demos.net.nap.web.model.RegulatoryNotice',
-  'foam.demos.net.nap.web.model.RegulatoryNoticeAudit'
+  'foam.demos.net.nap.web.model.RegulatoryNoticeAudit',
+
+  'foam.dao.journal.EasyJournalEntryDAO',
 ];
 
 var abstractClasses = [
