fc4fea6dc55831283d6f5581ac249f1f65393e57
Kirk Eaton <kirk@nanopay.net>
2018-07-10 11:12:53 -0400

    Modified ProxyJournalEntry to delegate getter functions for properties

--- src/foam/dao/journal/ProxyJournalEntry.js
+++ src/foam/dao/journal/ProxyJournalEntry.js
@@ -8,13 +8,34 @@ foam.CLASS({
   package: 'foam.dao.journal',
   name: 'ProxyJournalEntry',
   extends: 'foam.dao.journal.JournalEntry',
+
   properties: [
     {
       class: 'FObjectProperty',
       of: 'foam.dao.journal.JournalEntry',
       name: 'delegate',
     },
+    {
+      class: 'Long',
+      name: 'id',
+      javaGetter: `return getDelegate().getId();`,
+      javaSetter: `getDelegate().setId(val);`
+    },
+    {
+      class: 'Enum',
+      of: 'foam.dao.journal.JournalEntryCommand',
+      name: 'cmd',
+      javaGetter: `return getDelegate().getCmd();`,
+      javaSetter: `getDelegate().setCmd(val);`
+    },
+    {
+      class: 'FObjectProperty',
+      name: 'data',
+      javaGetter: `return getDelegate().getData();`,
+      javaSetter: `getDelegate().setCmd(val);`
+    }
   ],
+
   methods: [
     {
       name: 'replay',
