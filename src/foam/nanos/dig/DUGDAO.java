40e64d55f6b28010f5a9e03ace4e79e34131da6a
Kevin Greer <kgrgreer@gmail.com>
2018-07-12 07:40:49 -0400

    Add error logging to DUGDAO.

--- src/foam/nanos/dig/DUGDAO.java
+++ src/foam/nanos/dig/DUGDAO.java
@@ -14,6 +14,7 @@ import foam.dao.DAO;
 import foam.dao.JDAO;
 import foam.dao.ProxyDAO;
 import foam.nanos.auth.User;
+import foam.nanos.logger.Logger;
 import foam.util.Auth;
 
 /** Execute DUGs on startup or when first put(). Set DUG.owner to creator. **/
@@ -26,10 +27,11 @@ public class DUGDAO
 
   public DUGDAO(X x, DAO delegate) {
     super(x, delegate);
+
     delegate.select(new AbstractSink() {
       @Override
       public void put(Object obj, Detachable sub) {
-        ((DUG) obj).execute(Auth.sudo(x, ((DUG) obj).getOwner()));
+        executeDUG(x, (DUG) obj);
       }
     });
   }
@@ -37,8 +39,20 @@ public class DUGDAO
   @Override
   public FObject put_(X x, FObject obj) {
     User user = (User) x.get("user");
-    ((DUG) obj).setOwner(user.getId());
-    ((DUG) obj).execute(x);
+    DUG  dug  = (DUG) obj;
+
+    dug.setOwner(user.getId());
+    executeDUG(x, dug);
+
     return super.put_(x, obj);
+  }
+
+  protected void executeDUG(X x, DUG dug) {
+    try {
+      dug.execute(Auth.sudo(x, dug.getOwner()));
+    } catch (Throwable t) {
+      Logger logger = (Logger) x.get("logger");
+      logger.error(DUGDAO.class, "Error executing DUG", dug.getId(), t);
+    }
   }
 }
