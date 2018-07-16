28f11807cc89f96e81368d29a6e4c247f3b0eb4b
Kirk Eaton <kirk@nanopay.net>
2018-07-16 13:15:41 -0400

    Fixed year in header

--- src/foam/blob/AbstractIdentifiedBlobPropertyInfo.java
+++ src/foam/blob/AbstractIdentifiedBlobPropertyInfo.java
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright 2017 The FOAM Authors. All Rights Reserved.
+ * Copyright 2018 The FOAM Authors. All Rights Reserved.
  * http://www.apache.org/licenses/LICENSE-2.0
  */
 
