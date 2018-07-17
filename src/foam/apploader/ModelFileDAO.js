438a48a20533effe0b4f134e51f461ed66e57b2a
Mike Carcasole <mikecarcasole@gmail.com>
2018-07-17 12:32:55 -0400

    Make ModelFileDAO's foam.SCRIPT handling closer resemble the foam.INTERFACE, foam.ENUM, etc. calls.

--- src/foam/apploader/ModelFileDAO.js
+++ src/foam/apploader/ModelFileDAO.js
@@ -61,6 +61,11 @@ foam.CLASS({
             context.foam.CLASS(json);
           };
 
+          context.foam.SCRIPT = function(json) {
+            json.class = json.class || 'foam.core.Script';
+            context.foam.CLASS(json);
+          };
+
           context.foam.RELATIONSHIP = function(r) {
             var s = r.sourceModel;
             var si = s.lastIndexOf('.');
@@ -81,12 +86,6 @@ foam.CLASS({
               });
             }
           };
-
-          context.foam.SCRIPT = function(m) {
-            m.class = 'foam.core.Script';
-            json = m;
-          };
-
 
           if ( foam.String.isInstance(text) ) {
             with ( context ) { eval(text); }
