f9fe8ef4468d9d72be490789fb4a4525c82669e3
Troy Wolters <troy@nanopay.net>
2018-07-23 08:51:48 -0400

    Pass InvalidX error as argument
    It was hard-coded previously. This makes it more reusable elsewhere.

--- src/foam/core/InvalidX.java
+++ src/foam/core/InvalidX.java
@@ -6,41 +6,29 @@
 
 package foam.core;
 
-import java.security.AccessControlException;
-
 public class InvalidX
   extends AbstractX
 {
-  private static X x_ = new InvalidX();
+  private RuntimeException exception_;
 
-  private InvalidX() {}
-
-  public static X instance() {
-    return x_;
+  public InvalidX(RuntimeException exception) {
+    exception_ = exception;
   }
 
   public Object get(X x, Object key) {
-    throw getException();
+    throw exception_;
   }
 
   public X put(Object key, Object value) {
-    throw getException();
+    throw exception_;
   }
 
   public X putFactory(Object key, XFactory factory) {
-    throw getException();
+    throw exception_;
   }
 
   @Override
   public String toString() {
-    throw getException();
-  }
-
-  private AccessControlException getException() {
-    return new AccessControlException("When using a DAO decorated by " +
-        "AuthenticatedDAO, you may only call the context-oriented methods: " +
-        "put_(), find_(), select_(), remove_(), removeAll_(), pipe_(), and " +
-        "listen_(). Alternatively, you can also use .inX() to set the " +
-        "context on the DAO.");
+    throw exception_;
   }
 }
