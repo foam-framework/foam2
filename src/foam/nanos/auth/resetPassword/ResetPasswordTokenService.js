8c5ecacb957dbfce2078bf738b840d57a31a7ca6
Kirk Eaton <kirk@nanopay.net>
2018-07-25 11:18:21 -0400

    Modified order of execution

--- src/foam/nanos/auth/resetPassword/ResetPasswordTokenService.js
+++ src/foam/nanos/auth/resetPassword/ResetPasswordTokenService.js
@@ -136,12 +136,8 @@ if ( data == null || data.size() == 0 ) {
   throw new RuntimeException("Token not found");
 }
 
-// set token processed to true
+// find user from token
 Token tokenResult = (Token) data.get(0);
-tokenResult = (Token) tokenResult.fclone();
-tokenResult.setProcessed(true);
-tokenDAO.put(tokenResult);
-
 User userResult = (User) userDAO.find(tokenResult.getUserId());
 if ( userResult == null ) {
   throw new RuntimeException("User not found");
@@ -158,6 +154,11 @@ userResult.setPreviousPassword(userResult.getPassword());
 userResult.setPassword(Password.hash(newPassword));
 userResult.setPasswordExpiry(null);
 userDAO.put(userResult);
+
+// set token processed to true
+tokenResult = (Token) tokenResult.fclone();
+tokenResult.setProcessed(true);
+tokenDAO.put(tokenResult);
 
 EmailService email = (EmailService) getEmail();
 EmailMessage message = new EmailMessage();
