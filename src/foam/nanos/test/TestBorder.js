910ce5e1ef173c7d8b66be02c19f3c66f3a9d866
shreymahendru <shreymahendru@gmail.com>
2018-07-26 15:42:07 -0400

    TestBorder fixed total run test calc

--- src/foam/nanos/test/TestBorder.js
+++ src/foam/nanos/test/TestBorder.js
@@ -9,9 +9,9 @@
    name: 'TestBorder',
    extends: 'foam.u2.view.ScrollTableView',
 
-   implements: [ 'foam.mlang.Expressions' ],
+   implements: ['foam.mlang.Expressions'],
 
-   requires: [ 'foam.nanos.test.Test' ],
+   requires: ['foam.nanos.test.Test'],
 
    properties: [
      'status',
@@ -21,18 +21,25 @@
 
    methods: [
      function initE() {
-       this.
-         startContext({data: this}).add(this.RUN_ALL, this.RUN_FAILED_TESTS).endContext().
-         start('span').style({'padding-left': '12px'}).add('Passed: ', this.passed$).end().
-         start('span').style({'padding-left': '12px'}).add('Failed: ', this.failed$).end().
-         start('span').style({'padding-left': '12px'}).add('Status: ', this.status$).end();
+      this
+      .startContext({ data: this })
+        .add(this.RUN_ALL, this.RUN_FAILED_TESTS)
+      .endContext()
+      .start('span').style({ 'padding-left': '12px' })
+        .add('Passed: ', this.passed$)
+      .end()
+      .start('span').style({ 'padding-left': '12px' })
+        .add('Failed: ', this.failed$)
+      .end()
+      .start('span').style({ 'padding-left': '12px' })
+        .add('Status: ', this.status$)
+      .end();
 
       this.SUPER();
     },
 
     function runTests(dao) {
-      var self  = this;
-      var count = 0;
+      var self = this;
       var startTime = Date.now();
 
       this.status = 'Testing...';
@@ -40,7 +47,6 @@
 
       dao.select({
         put: function(t) {
-          count++;
           self.status = 'Testing: ' + t.id;
           t.run();
           self.passed += t.passed;
@@ -48,7 +54,7 @@
         },
         eof: function() {
           var duration = (Date.now() - startTime) / 1000;
-          self.status = count + ' tests run in ' + duration.toFixed(2) + ' seconds';
+          self.status = `${self.passed + self.failed} tests run in ${duration.toFixed(2)} seconds`;
         }
       });
     }
