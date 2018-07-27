1743991b952cbbf417d28f6f7cf21c5287b9ae02
Kirk Eaton <kirk@nanopay.net>
2018-07-27 13:47:49 -0400

    Modified Date refinements to use SimpleDateFormat to parse dates

--- src/foam/core/AbstractDatePropertyInfo.java
+++ src/foam/core/AbstractDatePropertyInfo.java
@@ -6,12 +6,16 @@
 
 package foam.core;
 
+import freemarker.template.SimpleDate;
+
 import javax.xml.stream.XMLStreamReader;
 import java.nio.ByteBuffer;
 import java.security.MessageDigest;
 import java.security.Signature;
 import java.security.SignatureException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 public abstract class AbstractDatePropertyInfo
     extends AbstractPropertyInfo
@@ -27,6 +31,15 @@ public abstract class AbstractDatePropertyInfo
       ByteBuffer bb = super.get();
       bb.clear();
       return bb;
+    }
+  };
+
+  protected static final ThreadLocal<SimpleDateFormat> sdf = new ThreadLocal<SimpleDateFormat>() {
+    @Override
+    protected SimpleDateFormat initialValue() {
+      SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.S'Z'");
+      df.setTimeZone(TimeZone.getTimeZone("UTC"));
+      return df;
     }
   };
 
