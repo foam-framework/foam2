80fc7483f735bd9f5b4684e886b1f9b2f0e74a06
Minsun <minsun@nanopay.net>
2018-07-31 16:15:53 -0400

    fixed to inherit parents permissions

--- src/foam/nanos/auth/PermissionTableView.js
+++ src/foam/nanos/auth/PermissionTableView.js
@@ -92,9 +92,20 @@ foam.CLASS({
                           .select(self.groupDAO.orderBy(self.Group.ID), function(g) {
                               var cb = foam.u2.md.CheckBox.create({data: self.checkPermissionForGroup(p.id, g)});
                               cb.data$.sub(function() { self.updateGroup(p, g, cb.data); });
+                              //cb.data$.sub(function() { self.updateInheritedPermissions(p, g, self); });
                               this.start('td').show(groups[g.id].data$.map(function() {return groups[g.id].data;}))
                                     .style({'text-align': 'center', 'width': '100'}).tag(cb).call(function() {
-                                      if ( g.implies(p.id) ) { cb.style({'border-color': '#40C75B'}) };
+                                      self.groupDAO.find(g.parent).then(function(a){
+                                        if ( a != undefined && a.implies(p.id) ) {
+                                          cb.setAttribute('title', g.parent + ': ' + p.id);
+                                          cb.style({'border-color': '#40C75B'})
+                                        }
+                                      });
+
+                                      if ( g.implies(p.id) ) {
+                                        cb.setAttribute('title', g.id + ': ' + p.id);
+                                        cb.style({'border-color': '#40C75B'});
+                                      }
                                     }).end()
                           })
                       .end()
@@ -114,19 +125,49 @@ foam.CLASS({
       }
     },
 
-    function updateGroup(permission, group, data) {
+    function updateGroup(p_, g_, data, self) {
       var dao = this.groupDAO;
-      dao.find(group).then(function(group) {
+      var e = foam.mlang.Expressions.create();
+
+      dao.find(g_.id).then(function(group) {
         // Remove permission if found
         var permissions = group.permissions.filter(function(p) {
-          return p.id != permission.id;
+          return p.id != p_.id;
+        });
+
+        //parents' permissions
+        dao.find(group.parent).then(function(groupParent) {
+          if ( groupParent != undefined ) {
+              permissions += groupParent.permissions.filter(function(gp) {
+                return gp.id == p_.id;
+              });
+          }
         });
 
         // Add if requested
-        if ( data ) permissions.push(permission);
+        if ( data ) permissions.push(p_);
 
         group.permissions = permissions;
         dao.put(group);
+
+        self.updateChildrenPermission(g_.id, permissions);
+      });
+    },
+
+    function updateChildrenPermission(gp, permissions) {
+      var self = this;
+      var dao = this.groupDAO;
+      var e = foam.mlang.Expressions.create();
+
+      dao.where(e.EQ(this.Group.PARENT, gp)).select().then(function(sink) {
+        var array = sink.array;
+
+        for ( var i = 0; i < array.length; i++ ) {
+            self.updateChildrenPermission(array[i].id, permissions);
+
+            array[i].permissions = permissions;
+            dao.put(array[i]);
+        }
       });
     }
   ]
