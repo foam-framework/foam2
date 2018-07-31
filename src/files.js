c70ac94b3166efa67d01bbe4e4e1efa067c8b903
Troy Wolters <troy@nanopay.net>
2018-07-31 10:01:37 -0400

    Create CurrencySearchView

--- src/files.js
+++ src/files.js
@@ -427,6 +427,7 @@ FOAM_FILES([
   { name: "foam/u2/search/GroupBySearchView", flags: ['web'] },
   { name: "foam/u2/search/SearchManager", flags: ['web'] },
   { name: "foam/u2/search/TextSearchView", flags: ['web'] },
+  { name: 'foam/u2/search/CurrencySearchView', flags: ['web'] },
   { name: "foam/u2/stack/Stack", flags: ['web'] },
   { name: "foam/u2/stack/StackView", flags: ['web'] },
   { name: "foam/apploader/ModelRefines" },
--- src/foam/u2/search/FilterController.js
+++ src/foam/u2/search/FilterController.js
@@ -505,3 +505,18 @@ foam.CLASS({
     }
   ]
 });
+
+
+foam.CLASS({
+  package: 'foam.u2.search',
+  name: 'CurrencyRefinement',
+  refines: 'foam.core.Currency',
+
+  properties: [
+    {
+      class: 'foam.u2.ViewSpec',
+      name: 'searchView',
+      value: { class: 'foam.u2.search.CurrencySearchView' }
+    }
+  ]
+});
