be8919f853f67bdfd1d30cc95e6ff9200f451d81
Kirk Eaton <kirk@nanopay.net>
2018-08-01 16:34:31 -0400

    Modified GoogleTOTPAuthService to set twoFactorSuccess to true

--- src/foam/nanos/auth/twofactor/GoogleTOTPAuthService.js
+++ src/foam/nanos/auth/twofactor/GoogleTOTPAuthService.js
@@ -86,6 +86,7 @@ try {
 `long code = Long.parseLong(token, 10);
 User user = (User) x.get("user");
 DAO userDAO = (DAO) x.get("localUserDAO");
+DAO sessionDAO = (DAO) x.get("sessionDAO");
 
 // fetch from user dao to get secret key
 user = (User) userDAO.find(user.getId());
@@ -96,6 +97,12 @@ if ( checkCode(BaseEncoding.base32().decode(user.getTwoFactorSecret()), code, ST
     user.setTwoFactorEnabled(true);
     userDAO.put(user);
   }
+
+  // update session with two factor success set to true
+  Session session = x.get(Session.class);
+  session.setUserId(user.getId());
+  session.setContext(session.getContext().put("user", user).put("twoFactorSuccess", true));
+  sessionDAO.put(session);
   return true;
 }
 
