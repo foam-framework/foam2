5fc9e2f8f3b091cdf52a9da069af0b6b12c7e2dc
Troy Wolters <troy@nanopay.net>
2018-08-03 14:08:03 -0400

    Fix background style in SearchViewWrapper

--- src/foam/u2/view/SearchViewWrapper.js
+++ src/foam/u2/view/SearchViewWrapper.js
@@ -29,7 +29,8 @@ foam.CLASS({
     }
 
     ^ .form-element-container {
-      background-color: #e9e9e9;
+      background-color: #f6f9f9;
+      border-top: 1px solid #e9e9e9;
     }
 
     ^ .section {
