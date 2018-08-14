65784babc5fc05b47728d8b59ab550440959aa3e
Mike Carcasole <mikecarcasole@gmail.com>
2018-08-14 18:18:28 -0400

    Remove some more json2 code.

--- src/foam/apploader/NodeModelFileDAO.js
+++ src/foam/apploader/NodeModelFileDAO.js
@@ -9,18 +9,15 @@ foam.CLASS({
   name: 'NodeModelFileDAO',
   extends: 'foam.dao.ProxyDAO',
   requires: [
-    'foam.apploader.JSON2ModelFileDAO',
     'foam.apploader.ModelFileDAO',
     'foam.apploader.NodeModelFileFetcher',
   ],
   properties: [
     'root',
-    'json2',
     {
       name: 'delegate',
       factory: function() {
-        var cls = this.json2 ? this.JSON2ModelFileDAO : this.ModelFileDAO;
-        return cls.create({
+        return this.ModelFileDAO.create({
           fetcher: this.NodeModelFileFetcher.create({root: this.root}), });
       },
     },
