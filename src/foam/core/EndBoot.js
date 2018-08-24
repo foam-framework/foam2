b114992b3fbdf85901d8d7bdf60ea0f502893c5a
Troy Wolters <troy@nanopay.net>
2018-08-24 17:24:02 -0400

    Blacklist code-generation-related warnings

--- src/foam/core/EndBoot.js
+++ src/foam/core/EndBoot.js
@@ -149,7 +149,7 @@ foam.CLASS({
     */
     function initArgs(args, ctx) {
       if ( ctx  ) this.__context__ = ctx;
-      if ( args ) this.copyFrom(args);
+      if ( args ) this.copyFrom(args, true);
     },
 
     /**
