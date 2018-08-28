9a64b3968ef35c6f8fee8951a47272ebd4d0f80c
Mike Carcasole <mikecarcasole@gmail.com>
2018-08-28 11:03:21 -0400

    Add some classes and blacklists to classes.js. Make some java mlangs use Builder instead of constructor.

--- src/foam/mlang/MLang.java
+++ src/foam/mlang/MLang.java
@@ -29,42 +29,65 @@ public class MLang
   }
 
   public static Predicate LT(Object o1, Object o2) {
-    return new Lt(MLang.prepare(o1), MLang.prepare(o2));
+    return new Lt.Builder(null)
+      .setArg1(MLang.prepare(o1))
+      .setArg2(MLang.prepare(o2))
+      .build();
   }
 
   public static Predicate LTE(Object o1, Object o2) {
-    return new Lte(MLang.prepare(o1), MLang.prepare(o2));
+    return new Lte.Builder(null)
+      .setArg1(MLang.prepare(o1))
+      .setArg2(MLang.prepare(o2))
+      .build();
   }
 
   public static Predicate EQ(Object o1, Object o2) {
-    return new Eq(MLang.prepare(o1), MLang.prepare(o2));
+    return new Eq.Builder(null)
+      .setArg1(MLang.prepare(o1))
+      .setArg2(MLang.prepare(o2))
+      .build();
   }
 
   public static Predicate NEQ(Object o1, Object o2) {
-    return new Neq(MLang.prepare(o1), MLang.prepare(o2));
+    return new Neq.Builder(null)
+      .setArg1(MLang.prepare(o1))
+      .setArg2(MLang.prepare(o2))
+      .build();
   }
 
   public static Predicate GTE(Object o1, Object o2) {
-    return new Gte(MLang.prepare(o1), MLang.prepare(o2));
+    return new Gte.Builder(null)
+      .setArg1(MLang.prepare(o1))
+      .setArg2(MLang.prepare(o2))
+      .build();
   }
 
   public static Predicate GT(Object o1, Object o2) {
-    return new Gt(MLang.prepare(o1), MLang.prepare(o2));
+    return new Gt.Builder(null)
+      .setArg1(MLang.prepare(o1))
+      .setArg2(MLang.prepare(o2))
+      .build();
   }
 
   public static Predicate IN(Object o1, Object o2) {
-    return new In(MLang.prepare(o1), MLang.prepare(o2));
+    return new In.Builder(null)
+      .setArg1(MLang.prepare(o1))
+      .setArg2(MLang.prepare(o2))
+      .build();
   }
 
   public static Predicate AND(Predicate... args) {
-    return new And(args);
+    return new And.Builder(null)
+      .setArgs(args)
+      .build();
   }
 
   public static Sink GROUP_BY(Expr o1, Sink o2) {
-    GroupBy groupBy = new GroupBy();
-    groupBy.setArg1(o1);
-    groupBy.setArg2(o2);
-    return groupBy;
+    return new GroupBy.Builder(null)
+      .setArg1(o1)
+      .setArg2(o2)
+      .build();
   }
 
   public static Sink COUNT() {
@@ -72,27 +95,41 @@ public class MLang
   }
 
   public static Predicate OR(Predicate... args) {
-    return new Or(args);
+    return new Or.Builder(null)
+      .setArgs(args)
+      .build();
   }
 
   public static Predicate NOT(Predicate predicate) {
-    return new Not(predicate);
+    return new Not.Builder(null)
+      .setArg1(predicate)
+      .build();
   }
 
   public static Sink MAX(Object o1) {
-    return new Max(null, MLang.prepare(o1));
+    return new Max.Builder(null)
+      .setArg1(MLang.prepare(o1))
+      .build();
   }
 
   public static Sink MIN(Object o1) {
-    return new Min(null, MLang.prepare(o1));
+    return new Min.Builder(null)
+      .setArg1(MLang.prepare(o1))
+      .build();
   }
 
   public static Sink SUM(Object o1) {
-    return new Sum(MLang.prepare(o1), 0.0);
+    return new Sum.Builder(null)
+      .setArg1(MLang.prepare(o1))
+      .setValue(0.0)
+      .build();
   }
 
   public static Sink MAP(Object o1, foam.dao.Sink delegate) {
-    return new Map(MLang.prepare(o1), delegate);
+    return new Map.Builder(null)
+      .setArg1(MLang.prepare(o1))
+      .setDelegate(delegate)
+      .build();
   }
 
   public static Predicate INSTANCE_OF(ClassInfo info) {
