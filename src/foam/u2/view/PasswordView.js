2ac5d0ee7cec8e025611040fe6ebaaf21d22f867
Henry Han <henry.han@usask.ca>
2018-09-20 14:45:08 -0400

    Update PasswordView with customized password visibility icon

--- src/foam/u2/view/PasswordView.js
+++ src/foam/u2/view/PasswordView.js
@@ -18,7 +18,81 @@
 foam.CLASS({
   package: 'foam.u2.view',
   name: 'PasswordView',
-  extends: 'foam.u2.tag.Input',
+  extends: 'foam.u2.View',
 
-  properties: [ [ 'type', 'password' ] ]
+  css: `
+    .input-image {
+      position: absolute;
+      width: 24px;
+      height: 24px;
+      bottom: 8px;
+      right: 6px;
+    }
+    .input-field-container {
+      position: relative;
+    }
+    .input-field {
+      padding-right: 30px;
+      width: 100%;
+      width: 100%;
+      height: 40px;
+    }
+  `,
+
+  properties: [
+    ['nodeName', 'input'],
+    'data',
+    'type',
+    {
+      name: 'visibilityIcon',
+      value: '/foam2/src/foam/u2/images/visibility.svg'
+    },
+    {
+      class: 'Boolean',
+      name: 'passwordInvisible',
+      value: true
+    },
+    {
+      class: 'Boolean',
+      name: 'passwordIcon',
+      defaultValue: false
+    }
+  ],
+
+  methods: [
+    function initE() {
+      this.SUPER();
+
+      this.addClass(this.myClass());
+      this.addClass('input-field');
+      this.start('img').show(this.passwordIcon$).addClass('input-image').
+        attr('src', this.visibilityIcon$).on('click', this.visible).
+      end();
+
+      this.attrSlot(
+        'value',
+        this.onKey ? 'input' : 'change'
+      ).linkFrom(this.data$);
+    },
+
+    function visibleIcon(visibilityIcon, type) {
+      this.visibilityIcon = visibilityIcon;
+      this.type = type;
+      this.passwordInvisible = ! this.passwordInvisible;
+      this.enableClass('property-password', this.passwordInvisible);
+    }
+  ],
+
+  listeners: [
+    function visible() {
+      if ( this.passwordInvisible ) {
+        //  Password visible
+        this.visibleIcon('/foam2/src/foam/u2/images/visibility-off.svg',
+            'text');
+      } else {
+        this.visibleIcon('/foam2/src/foam/u2/images/visibility.svg',
+            'password');
+      }
+    }
+  ]
 });
