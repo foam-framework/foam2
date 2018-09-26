6225ec2f5aaabe33019f4a10d0404eb8cefea3fc
Kirk Eaton <kirk@nanopay.net>
2018-09-26 11:27:53 -0400

    Finished RoutingJournal replay method
    Formatting

--- src/foam/dao/RoutingJournal.js
+++ src/foam/dao/RoutingJournal.js
@@ -69,7 +69,56 @@ foam.CLASS({
     {
       name: 'replay',
       javaCode: `
-        return;
+        // count number of lines successfully read
+        int successReading = 0;
+        foam.lib.json.JSONParser parser = getParser();
+
+        try ( java.io.BufferedReader reader = getReader() ) {
+          for ( String line ; ( line = reader.readLine() ) != null ; ) {
+            if ( foam.util.SafetyUtil.isEmpty(line) ) continue;
+            if ( COMMENT.matcher(line).matches()    ) continue;
+
+            try {
+              String[] split = line.split("\\\\.", 2);
+              if ( split.length != 2 ) {
+                continue;
+              }
+
+              String service = split[0];
+              line = split[1];
+
+              char operation = line.charAt(0);
+              int length = line.trim().length();
+              line = line.trim().substring(2, length - 1);
+
+              dao = (foam.dao.DAO) x.get(service);
+              foam.core.FObject obj = parser.parseString(line);
+              if ( obj == null ) {
+                getLogger().error("Parse error", getParsingErrorMessage(line), "line:", line);
+                continue;
+              }
+
+              switch (operation) {
+                case 'p':
+                  foam.core.FObject old = dao.find(obj.getProperty("id"));
+                  dao.put(old != null ? mergeFObject(old, obj) : obj);
+                  break;
+
+                case 'r':
+                  dao.remove(obj);
+                  break;
+              }
+
+              successReading++;
+            } catch ( Throwable t ) {
+              getLogger().error("Error replaying journal line:", line, t);
+            }
+          }
+        } catch ( Throwable t ) {
+          getLogger().error("Failed to read from journal", t);
+        } finally {
+          getLogger().log("Successfully read " + successReading + " entries from file: " + getFilename());
+        }
       `
     }
   ]
