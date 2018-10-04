77007e935c6ae848c6f26b8d97b5c36ca7eac415
Kirk Eaton <kirk@nanopay.net>
2018-10-04 09:55:01 -0400

    Revert "Added ability to define abstract methods"
    This reverts commit 3ee87166fa0a834dc516d2af5109ed287cce29f6.

--- src/foam/java/refinements.js
+++ src/foam/java/refinements.js
@@ -481,11 +481,6 @@ foam.CLASS({
       name: 'final'
     },
     {
-      class: 'String',
-      name: 'visibility',
-      value: 'public'
-    },
-    {
       class: 'Boolean',
       name: 'abstract',
       value: true
@@ -513,8 +508,7 @@ foam.CLASS({
       cls.method({
         name: this.name,
         type: this.javaReturns || 'void',
-        visibility: this.visibility,
-        abstract: this.abstract,
+        visibility: 'public',
         static: this.isStatic(),
         final: this.final,
         synchronized: this.synchronized,
