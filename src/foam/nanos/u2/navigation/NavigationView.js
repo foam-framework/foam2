a17ea03887d197f31f86472240dd41c6df210c59
Kate Xia <katexia@Kates-MacBook.local>
2018-10-04 11:58:39 -0400

    fixed indentation and syntax

--- src/foam/nanos/u2/navigation/NavigationView.js
+++ src/foam/nanos/u2/navigation/NavigationView.js
@@ -1,29 +1,31 @@
 foam.CLASS({
-    package: 'foam.nanos.u2.navigation',
-    name: 'NavigationView',
-    extends: 'foam.u2.View',
-     documentation: 'Navigation bars',
-     css: `
-      ^ .top-nav {
-        width: calc(100% - 200px);
-        display: inline;
-      }
-      ^ .side-nav {
-        float:left;
-        display: inline-block;
-        width: 200px;
-        height: 100%
-      }
-    `,
-     methods: [
-      function initE() {
-        this.addClass(this.myClass()).addClass('nav-view')
-          .start().addClass('top-nav')
+  package: 'foam.nanos.u2.navigation',
+  name: 'NavigationView',
+  extends: 'foam.u2.View',
+   documentation: 'Navigation bars',
+   css: `
+    ^ .top-nav {
+      width: calc(100% - 200px);
+      display: inline;
+    }
+    ^ .side-nav {
+      float:left;
+      display: inline-block;
+      width: 200px;
+      height: 100%
+    }
+  `,
+   methods: [
+    function initE() {
+      this.addClass(this.myClass())
+        .start()
+          .addClass(this.myClass('top-nav'))
           .tag({ class: 'foam.nanos.u2.navigation.TopNavigation' })
-          .end()
-          .start().addClass('side-nav')
-            .tag({ class: 'foam.nanos.u2.navigation.SideNavigation' })
-          .end();
-      }
-    ]
-  });
+        .end()
+        .start()
+          .addClass(this.myClass('side-nav'))
+          .tag({ class: 'foam.nanos.u2.navigation.SideNavigation' })
+        .end();
+    }
+  ]
+});
