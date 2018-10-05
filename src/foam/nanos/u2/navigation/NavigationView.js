bd8c6aca3fa1ad16f1fb77bbe7cd02d94f7505de
Kate Xia <katexia@Kates-MacBook.local>
2018-10-05 13:47:28 -0400

    changed design of SideNavigation

--- src/foam/nanos/u2/navigation/NavigationView.js
+++ src/foam/nanos/u2/navigation/NavigationView.js
@@ -1,31 +1,31 @@
 foam.CLASS({
-  package: 'foam.nanos.u2.navigation',
-  name: 'NavigationView',
-  extends: 'foam.u2.View',
-   documentation: 'Navigation bars',
-   css: `
-    ^ .top-nav {
-      width: calc(100% - 200px);
-      display: inline;
-    }
-    ^ .side-nav {
-      float:left;
-      display: inline-block;
-      width: 200px;
-      height: 100%
-    }
-  `,
-   methods: [
-    function initE() {
-      this.addClass(this.myClass())
-        .start()
-          .addClass(this.myClass('top-nav'))
-          .tag({ class: 'foam.nanos.u2.navigation.TopNavigation' })
-        .end()
-        .start()
-          .addClass(this.myClass('side-nav'))
-          .tag({ class: 'foam.nanos.u2.navigation.SideNavigation' })
-        .end();
-    }
-  ]
-});
+    package: 'foam.nanos.u2.navigation',
+    name: 'NavigationView',
+    extends: 'foam.u2.View',
+     documentation: 'Navigation bars',
+     css: `
+      ^top-nav {
+        width: calc(100% - 200px);
+        display: inline;
+      }
+      ^side-nav {
+        float:left;
+        display: inline-block;
+        width: 200px;
+        height: 100%
+      }
+    `,
+     methods: [
+      function initE() {
+        this.addClass(this.myClass())
+          .start()
+           .addClass(this.myClass('top-nav'))
+            .tag({ class: 'foam.nanos.u2.navigation.TopNavigation' })
+          .end()
+          .start()
+            .addClass(this.myClass('side-nav'))
+            .tag({ class: 'foam.nanos.u2.navigation.SideNavigation' })
+          .end();
+      }
+    ]
+  });
--- src/foam/nanos/u2/navigation/SideNavigation.js
+++ src/foam/nanos/u2/navigation/SideNavigation.js
@@ -1,147 +1,113 @@
 foam.CLASS({
-    package: 'foam.nanos.u2.navigation',
-    name: 'SideNavigation',
-    extends: 'foam.u2.View',
+  package: 'foam.nanos.u2.navigation',
+  name: 'SideNavigation',
+  extends: 'foam.u2.View',
   
-    documentation: 'Side navigation bar',
+  documentation: 'Side navigation bar',
   
-    implements: [
-        'foam.mlang.Expressions'
-      ],
-       requires: [
-        'foam.nanos.menu.Menu'
-      ],
-       imports: [
-        'menuDAO'
-      ],
-       css: `
-        ^ .side-nav {
-          height: 100%;
-          width: 200px;
-          position
-          z-index: 1;
-          top: 0;
-          left: 0;
-          overflow-x: hidden;
-          background-color: white;
-          display: inline-block;
-        }
-        ^ .nav-row {
-          display: block;
-        }
-        ^ .side-nav a {
-          display: inline-block;
-          margin: 8px 8px 8px 8px;
-          text-decoration: none;
-          font-size: 20px;
-          transition: 0.3s;
-        }
-        ^ .side-nav a:hover {
-          color: #f1f1f1;
-        }
-        ^ .accordion-card a {
-          font-size: 16px;
-          margin: 8px 8px 8px 50px;
-        }
-        ^ .accordion-card-hide {
-          display: none;
-        }
-        ^ .accordion-card-show {
-          display: block !important;
-        }
-        ^ .accordion-button {
-          display: inline-block;
-          border: none;
-          padding: 0;
-          margin: 8px 8px;
-          font-size: 20px;
-          overflow: hidden;
-          text-decoration: none;
-          text-align: left;
-          cursor: pointer;
-          white-space: nowrap;
-        }
-        ^ .accordion-button:focus {
-          outline: 0;
-        }
-      `,
-       properties: [
-        {
-          name: 'accordionCardShowDict',
-          value: {}
-        },
-        {
-          class: 'Boolean',
-          name: 'accordionCardShow',
-          value: true
-        },
-        {
-          class: 'String',
-          name: 'menuName',
-        }
-      ],
-       methods: [
-        function initE() {
-          var Menu = this.Menu;
-          var self = this;
-          var dao = this.menuDAO.orderBy(Menu.ORDER)
-              .where(this.EQ(Menu.PARENT, this.menuName));
-              this.addClass(this.myClass())
-              .start()
-                .addClass('side-nav')
-                .select(dao, function(menu) {
-                  self.accordionCardShowDict[menu.id] = true;
-                  return this.E()
-                    .call(function() {
-                      var self2 = this;
-                      this
-                        .start('a')
-                          .addClass('menuItem')
-                          .add(menu.label)
-                          .on('click', function() {
-                            menu.children.select().then(function(temp) {
-                              // Only display submenu if array length is longer than 0
-                              temp.array.length === 0 ?
-                                menu.launch(self2.__context__, self2) :
-                                self.accordianToggle(menu.id);
-                            });
-                          })
-                        .end();
-                       // Genearete submenu
-                      var X = this.__subContext__;
-                      self.menuDAO.where(ctrl.EQ(Menu.PARENT, menu.id)).select(
-                        function(submenu) {
-                          var accordianSlot = self.accordionCardShowDict$.map(
-                            function( keypair ) {
-                              return keypair[submenu.parent];
-                            }
-                          );
-                          self2.start()
-                            .addClass('accordion-card')
-                            .addClass('accordion-card-hide')
-                            .enableClass('accordion-card-show',
-                            accordianSlot)
-                            .call(function() {
-                              this.start('a')
-                                .add(submenu.label)
-                                .on('click', function() {
-                                  submenu.launch(X, self2);
-                                })
-                              .end();
-                            })
-                          .end();
-                        }
-                      );
-                    });
-                })
-              .end();
-          },
-           function accordianToggle(menuId) {
-            var oldDict = this.accordionCardShowDict;
-            oldDict[menuId] = ! oldDict[menuId];
-            // accordianSlot won't be triggered if removed the next line
-            this.accordionCardShowDict = undefined;
-            this.accordionCardShowDict = oldDict;
-          }
-        ]
-      });
+  implements: [
+    'foam.mlang.Expressions'
+  ],
+
+  requires: [
+    'foam.nanos.menu.Menu',
+    'foam.nanos.u2.navigation.SideNavigationItemView'
+  ],
+
+  imports: [
+    'menuDAO'
+  ],
+
+  css:`
+  ^side-nav {
+    height: 100%;
+    width: 200px;
+    position
+    z-index: 1;
+    top: 0;
+    left: 0;
+    overflow-x: hidden;
+    background-color: white;
+    display: inline-block;
+  }
+  ^side-nav a {
+    display: inline-block;
+    margin: 8px 8px 8px 8px;
+    text-decoration: none;
+    font-size: 20px;
+    transition: 0.3s;
+  }
+  `,
+
+  properties: [
+    {
+      class: 'String',
+      name: 'menuName',
+    }
+  ],
+
+  methods: [
+    function initE() {
+      var self = this;
+      var dao = this.menuDAO
+        .orderBy(this.Menu.ORDER)
+        .where(this.EQ(this.Menu.PARENT, this.menuName));
+      this.addClass(this.myClass())
+        .start()
+          .addClass(this.myClass('side-nav'))
+          .select(dao, function(menu) {
+            // console.log('menu.label: ' + menu.label)
+            return foam.nanos.u2.navigation.SideNavigationItemView.create({ data: menu });
+          })
+        .end();
+    }
+  ]
+});
+
+foam.CLASS({
+  package: 'foam.nanos.u2.navigation',
+  name: 'SideNavigationItemView',
+  extends: 'foam.u2.View',
+
+  properties: [
+    {
+      class: 'Boolean',
+      name: 'expanded',
+      value: true
+    }
+  ],
+
+  methods: [
+    function initE() {
+      var view = this;
+      // console.log('label: ' + this.data.label + ' isSubMenu: ' + this.isSubMenu )
+
+      this
+        .start('a')
+          .add(this.data.label)
+          .on('click', function() {
+            var menu = view.data;
+            menu.children.select().then(function(c) {
+              if ( c.array.length === 0 ) {
+                // console.log('expanded: ' + view.expanded + ' length: 0' + ' submenu: ' + view.isSubMenu)
+                menu.launch(view.__context__, view);
+              } else {
+                // console.log('expanded: ' + view.expanded + ' array: ' + c.array[0].label + " " + c.array[1].label + ' submenu: ' + view.isSubMenu)
+                view.expanded = ! view.expanded;
+              }
+            });
+          })
+        .end()
+        .start('div')
+          .add(this.slot(function(expanded, data) {
+            return ! expanded ?
+              this.E() :
+              this.E()
+                .select(data.children, function(child) {
+                  return view.cls_.create({ data: child }, view).style({'margin-left': '30px'});
+                });
+          }))
+        .end();
+    }
+  ]
+})
