c5d0bf157bff8c14ab4c5da8eecc5a8cc3f7ac4e
penzital <penzital888@gmail.com>
2018-10-05 16:59:52 -0400

    parser changes

--- src/foam/java/refinements.js
+++ src/foam/java/refinements.js
@@ -1461,7 +1461,7 @@ foam.CLASS({
   properties: [
     ['javaType', 'foam.core.ClassInfo'],
     ['javaInfoType', 'foam.core.AbstractObjectPropertyInfo'],
-    ['javaJSONPaser', 'new foam.lib.parse.Fail()']
+    ['javaJSONPaser', 'new ClassReferenceParser()']
   ]
 });
 
