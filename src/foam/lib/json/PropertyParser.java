cf64e56eab3c3a4cc5d8d9c90846ba6033f1f0a2
Kevin Greer <kgrgreer@gmail.com>
2018-10-08 17:24:38 -0400

    Add toString method to PropertyParser.

--- src/foam/lib/json/PropertyParser.java
+++ src/foam/lib/json/PropertyParser.java
@@ -30,8 +30,12 @@ public class PropertyParser
 
   public PStream parse(PStream ps, ParserContext x) {
     ps = super.parse(ps, x);
-    if (ps == null) return null;
+    if ( ps == null ) return null;
     property.set(x.get("obj"), ps.value());
     return ps;
+  }
+
+  public String toString() {
+    return "PropertyParser(" + property.getName() + ")";
   }
 }
