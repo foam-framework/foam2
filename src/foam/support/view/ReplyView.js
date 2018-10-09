67449ac81aea1c7e6f7dce68417c796e90f47ad8
Kirk Eaton <kirk@nanopay.net>
2018-10-09 13:25:41 -0400

    Fixed CSS

--- src/foam/support/view/ReplyView.js
+++ src/foam/support/view/ReplyView.js
@@ -37,80 +37,81 @@ foam.CLASS({
   ],
   
   css: `
-  ^ .bg {
-    width: 1000px;
-    height: 250px;
-    border-radius: 2px;
-    background-color: #ffffff;
-  }
-  ^ .firstdiv {
-    width: 1200px;
-    height: 10px;
-    padding-top:20px;  
-  }
-  ^ .person {
-    width: 40px;
-    height: 40px;
-    object-fit: contain;
-    margin-left: 10px;
-    padding-left:10px;
-  }
-  ^ .Public-Reply {
-    font-family: Roboto;
-    font-size: 12px;
-    font-weight: normal;
-    font-style: normal;
-    font-stretch: normal;
-    line-height: 1.33;
-    letter-spacing: 0.2px;
-    text-align: left;
-    color: #093649;
-    border: none;
-    background: none;
-    position: relative;
-    top: -25;
-  }
-  ^ .Internal-Note {
-    font-family: Roboto;
-    font-size: 12px;
-    font-weight: normal;
-    font-style: normal;
-    font-stretch: normal;
-    line-height: 1.33;
-    letter-spacing: 0.2px;
-    text-align: left;
-    color: #093649;
-    border: none;
-    background: none;
-    position: relative;
-    top: -25; 
-  } 
-  ^ .Internal-Note:focus{
-    outline: none;
-  }
-  ^ .Public-Reply:focus{
-    outline: none;
-  }
-  ^ .Rectangle {
-    width: 820px;
-    height: 160px;
-    border-radius: 2px;
-    background-color: #ffffff;
-    border: solid 1px rgba(164, 179, 184, 0.5);
-    margin-left:75px;
-    margin-top:40px;
-  }
-  ^ .background-color{
-    background: #093649;
-    color:white 
-  }
-  ^ .border{ 
-    border-bottom: 3px solid #1cc2b7;
-    font-weight:bold
-  }
-  ^ .foam-u2-UnstyledActionView:focus{
-    outline: none;
-  }`,
+    ^ .bg {
+      width: 1000px;
+      height: 250px;
+      border-radius: 2px;
+      background-color: #ffffff;
+    }
+    ^ .firstdiv {
+      width: 1200px;
+      height: 10px;
+      padding-top:20px;
+    }
+    ^ .person {
+      width: 40px;
+      height: 40px;
+      object-fit: contain;
+      margin-left: 10px;
+      padding-left:10px;
+    }
+    ^ .Public-Reply {
+      font-family: Roboto;
+      font-size: 12px;
+      font-weight: normal;
+      font-style: normal;
+      font-stretch: normal;
+      line-height: 1.33;
+      letter-spacing: 0.2px;
+      text-align: left;
+      color: #093649;
+      border: none;
+      background: none;
+      position: relative;
+      top: -25;
+    }
+    ^ .Internal-Note {
+      font-family: Roboto;
+      font-size: 12px;
+      font-weight: normal;
+      font-style: normal;
+      font-stretch: normal;
+      line-height: 1.33;
+      letter-spacing: 0.2px;
+      text-align: left;
+      color: #093649;
+      border: none;
+      background: none;
+      position: relative;
+      top: -25;
+    }
+    ^ .Internal-Note:focus{
+      outline: none;
+    }
+    ^ .Public-Reply:focus{
+      outline: none;
+    }
+    ^ .Rectangle {
+      width: 820px;
+      height: 160px;
+      border-radius: 2px;
+      background-color: #ffffff;
+      border: solid 1px rgba(164, 179, 184, 0.5);
+      margin-left:75px;
+      margin-top:40px;
+    }
+    ^ .background-color{
+      background: #093649;
+      color:white
+    }
+    ^ .border{
+      border-bottom: 3px solid #1cc2b7;
+      font-weight:bold
+    }
+    ^ .foam-u2-UnstyledActionView:focus{
+      outline: none;
+    }
+  `,
 
   methods: [
     function initE(){
