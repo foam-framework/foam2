72c5ddacc26ff61d79fb205a75718870604fe51e
Devansh Jain <devansh@nanopay.net>
2018-10-16 13:01:51 -0400

    Address pr comments and refactor

--- src/foam/nanos/auth/AuthenticatedGroupDAO.java
+++ src/foam/nanos/auth/AuthenticatedGroupDAO.java
@@ -28,49 +28,43 @@ public class AuthenticatedGroupDAO extends ProxyDAO {
     super(x, delegate);
   }
 
-  /**
-   * Make sure that the user has all permissions in the given group.
-   */
-  public void checkUserHasAllPermissionsInGroup(X x, Group toCheck) {
-    AuthService auth = (AuthService) x.get("auth");
-    String groupId;
-    Group group = toCheck;
-
-    while ( true ) {
-      Permission[] permissions = group.getPermissions();
-
-      for ( Permission permission : permissions ) {
-        String id = permission.getId();
-        if ( ! auth.check(x, id) ) {
-          throw new AuthorizationException("Permission Denied. You do not have the '" + id + "' permission.");
-          }
-       }
-
-      groupId = group.getParent();
-
-      if ( SafetyUtil.isEmpty(groupId) ) {
-        break;
-      }
-
-      group = (Group) getDelegate().find_(x, groupId);
-
-      if ( group == null ) {
-        throw new RuntimeException("The '" + toCheck.getId() + "' group has a null ancestor named '" + groupId + "'.");
-      }
-    }
-  }
-
   @Override
   public FObject put_(X x, FObject obj) {
-    // If a new group is being created, check that the user has permission to create the group
-    if ( getDelegate().find_(x, obj) == null ) {
-      AuthService auth = (AuthService) x.get("auth");
-      User user = (User) x.get("user");
-      if ( ! auth.check(x, "group.create." + ((Group) obj).getId() ) ) {
-        throw new AuthorizationException("Permission Denied. You do not have requisite permission to create this group."); 
-      }
-    }
-    checkUserHasAllPermissionsInGroup(x, (Group) obj);
+    checkUserHasAllPermissionsInGroupAndAncestors(x, (Group) obj);
     return super.put_(x, obj);
+  }
+
+  private void checkUserHasAllPermissionsInGroupAndAncestors(X x, Group toCheck) {
+    Group group = toCheck;
+    checkUserHasAllPermissionsInGroup(x, group);
+    while ( getAncestor(x, group) != null ) {
+      group = getAncestor(x, group);
+      checkUserHasAllPermissionsInGroup(x, group);
+    }
+  }
+  
+  // Make sure that the user has all permissions in the given group.
+  private void checkUserHasAllPermissionsInGroup(X x, Group group) {
+    AuthService auth = (AuthService) x.get("auth");
+    Permission[] permissions = group.getPermissions();
+    for ( Permission permission : permissions ) {
+      String id = permission.getId();
+      if ( ! auth.check(x, id) ) {
+        throw new AuthorizationException("Permission Denied. You do not have the '" + id + "' permission.");
+        }
+     }
+  }
+
+  // returns ancestor of a group if it exists, null otherwise null
+  private Group getAncestor(X x, Group group) {    
+    String ancestorGroupId = group.getParent();
+    if ( SafetyUtil.isEmpty(ancestorGroupId) ) {
+      return null;
+    }
+    Group ancestor = (Group) getDelegate().find_(x, ancestorGroupId);
+    if ( ancestor == null ) {
+      throw new RuntimeException("The '" + group.getId() + "' group has a null ancestor named '" + ancestorGroupId + "'.");
+    }
+    return ancestor;
   }
 }
