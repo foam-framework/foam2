1f6aa698b77e4d2d83de709a488b1f25037137d8
Mike Carcasole <mikecarcasole@gmail.com>
2018-10-30 11:44:38 -0400

    Remove graphArgs in favor of individual args on the Pie mlang.

--- src/foam/u2/mlang/Pie.js
+++ src/foam/u2/mlang/Pie.js
@@ -7,26 +7,25 @@ foam.CLASS({
     'foam.graphics.PieGraph',
   ],
   properties: [
-    {
-      name: 'graphArgs',
-      value: {
-        radius: 50,
-        x: 50,
-        y: 50,
-        margin: 1.5,
-        height: 150,
-        width: 150,
-        graphColors: [ '#d81e05', '#093649', '#59a5d5', '#2cab70' ],
-      },
-    },
+    // TODO: When these defaults are no longer necessary, move these args into
+    // their own class and add them as a trait to this model so any new args
+    // used by PieGraph are automatically picked up by this model.
+    [ 'graphColors', [ '#d81e05', '#093649', '#59a5d5', '#2cab70' ] ],
+    [ 'height', 150 ],
+    [ 'margin', 1.5 ],
+    [ 'radius', 50 ],
+    [ 'width', 150 ],
+    [ 'x', 50 ],
+    [ 'y', 50 ],
+
     {
       name: 'graph_',
-      expression: function(graphArgs, groups) {
+      expression: function(groups) {
         var seriesValues = Object.values(groups).map(function(sink) {
           return sink.value;
         })
         if ( ! seriesValues.length ) seriesValues = [0];
-        var p = this.PieGraph.create(graphArgs);
+        var p = this.PieGraph.create(this);
         p.seriesValues = seriesValues;
         return p;
       },
