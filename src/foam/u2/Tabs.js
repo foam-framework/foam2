b9b35a66ef764c5c0210077da0c90e023aaf70e2
Scott Head <scott@nanopay.net>
2018-11-01 09:33:23 -0400

    Adding registration and splitting out the unstyled tab

--- src/foam/u2/Tabs.js
+++ src/foam/u2/Tabs.js
@@ -28,61 +28,6 @@ foam.CLASS({
   ]
 });
 
-
-foam.CLASS({
-  package: 'foam.u2',
-  name: 'UnstyledTabs',
-  extends: 'foam.u2.Element',
-  requires: [ 'foam.u2.Tab' ],
-  properties: [
-    /* not used
-    {
-      name: 'tabs',
-      factory: function() { return []; }
-    },
-    */
-    {
-      name: 'selected',
-      postSet: function(o, n) {
-        if ( o ) o.selected = false;
-        n.selected = true;
-      }
-    },
-    'tabRow'
-  ],
-
-  methods: [
-    function init() {
-      this.
-        addClass(this.myClass()).
-        start('div', null, this.tabRow$).
-          addClass(this.myClass('tabRow')).
-        end().
-        start('div', null, this.content$).
-          addClass(this.myClass('content')).
-        end();
-    },
-
-    function add(tab) {
-      if ( this.Tab.isInstance(tab) ) {
-
-        if ( ! this.selected ) this.selected = tab;
-
-        this.tabRow.start('span').
-          addClass(this.myClass('tab')).
-          enableClass('selected', tab.selected$).
-          on('click', function() { this.selected = tab; }.bind(this)).
-          add(tab.label).
-        end();
-
-        tab.shown$ = tab.selected$;
-      }
-
-      this.SUPER(tab);
-    }
-  ]
-});
-
 foam.CLASS({
   package: 'foam.u2',
   name: 'Tabs',
