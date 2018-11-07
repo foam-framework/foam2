1d0560493afa57fa2fbf97853c07f34d45d20887
shreymahendru <shreymahendru@gmail.com>
2018-11-07 10:11:42 -0500

    catching errors when running scripts/tests on the client

--- src/foam/nanos/test/Test.js
+++ src/foam/nanos/test/Test.js
@@ -9,7 +9,7 @@ foam.CLASS({
   name: 'Test',
   extends: 'foam.nanos.script.Script',
 
-  imports: [ 'testDAO as scriptDAO' ],
+  imports: ['testDAO as scriptDAO'],
 
   javaImports: [
     'bsh.Interpreter',
@@ -129,6 +129,7 @@ foam.CLASS({
         } catch (err) {
           this.failed += 1;
           this.output += err;
+          return Promise.reject(err);
         }
 
         ret.then(() => {
