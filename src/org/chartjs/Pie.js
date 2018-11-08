31f9d2f6975262f515cdf57367561f2263683778
Mike Carcasole <mikecarcasole@gmail.com>
2018-11-08 12:05:46 -0800

    Move a bunch of chart code into an AbstractChart for now and make Pie and Bar extend that. Introduce a ChartConfig that Pie and Bar respect. Update demos to use the config and show a detail view of it.

--- src/org/chartjs/Pie.js
+++ src/org/chartjs/Pie.js
@@ -1,43 +1,8 @@
 foam.CLASS({
   package: 'org.chartjs',
   name: 'Pie',
-  extends: 'foam.u2.View',
-  requires: [
-    'org.chartjs.Lib',
-    'org.chartjs.PieConfig',
-  ],
+  extends: 'org.chartjs.AbstractChart',
   properties: [
-    {
-      class: 'FObjectProperty',
-      of: 'foam.mlang.sink.GroupBy',
-      name: 'data',
-      hidden: true,
-      postSet: function(_, n) {
-        this.onDetach(n.groups$.sub(function(sub) {
-          if ( this.data !== n ) sub.detach();
-          else this.updateChart();
-        }.bind(this)));
-        this.updateChart()
-      },
-    },
-    {
-      name: 'chart',
-    },
-    {
-      class: 'FObjectProperty',
-      of: 'org.chartjs.PieConfig',
-      name: 'config',
-      factory: function() {
-        return this.PieConfig.create();
-      },
-      postSet: function(_, n) {
-        this.onDetach(n.sub(function(sub) {
-          if ( this.config !== n ) sub.detach();
-          else this.updateChart();
-        }.bind(this)));
-        this.updateChart()
-      },
-    },
     {
       name: 'chartConfig',
       factory: function(config) {
@@ -53,16 +18,6 @@ foam.CLASS({
   ],
   listeners: [
     {
-      name: 'loadChart',
-      isFramed: true,
-      code: function() {
-        var canvas = this.el().getElementsByTagName('canvas')[0];
-        var ctx = canvas.getContext('2d');
-        this.chart = new this.Lib.CHART(ctx, this.chartConfig);
-        this.updateChart();
-      },
-    },
-    {
       name: 'updateChart',
       isFramed: true,
       code: function() {
@@ -70,29 +25,13 @@ foam.CLASS({
         var groups = this.data.groups;
         var keys = Object.keys(groups);
         this.chartConfig.labels = keys;
-        this.chartConfig.datasets[0].backgroundColor = this.config.backgroundColor;
+        this.chartConfig.datasets[0].backgroundColor = this.config.colors;
         this.chartConfig.datasets[0].data = keys.map(function(k) {
           return groups[k].value
         });
         this.chart.data = this.chartConfig;
         this.chart.update();
       },
-    },
-  ],
-  methods: [
-    function init() {
-      this.onDetach(this.onload.sub(this.loadChart));
-      this.SUPER();
-    },
-    function initE() {
-      this.SUPER();
-      this.
-        style({
-          position: 'relative',
-          height: this.config$.dot('height'),
-          width: this.config$.dot('width'),
-        }).
-        start('canvas').end();
     },
   ],
 });
