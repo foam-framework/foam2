58fed5f0fa6407d393d8b34d51f653574e98ada7
Mike Carcasole <mikecarcasole@gmail.com>
2018-11-09 12:36:41 -0800

    Add Table visualization of a DAO.

---
+++ src/foam/dashboard/model/Table.js
@@ -0,0 +1,29 @@
+foam.CLASS({
+  package: 'foam.dashboard.model',
+  name: 'Table',
+  extends: 'foam.dashboard.model.Visualization',
+  requires: [
+    'foam.dao.ArraySink',
+    'foam.mlang.sink.Count',
+    'foam.mlang.sink.GroupBy',
+    'foam.dashboard.view.DAOTable',
+    'foam.u2.ContextSensitiveDetailView as DetailView',
+    'foam.mlang.sink.NullSink',
+  ],
+  properties: [
+    {
+      class: 'Int',
+      name: 'limit',
+      value: 5,
+    },
+    {
+      name: 'views',
+      factory: function() {
+        return [
+          [ this.DAOTable, 'Table' ],
+          [ this.DetailView, 'Configure' ],
+        ]
+      }
+    },
+  ]
+});
