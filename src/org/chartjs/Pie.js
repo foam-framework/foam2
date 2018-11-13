08b961e3e30946d3f9397e24dd4063ebd77fbfaf
Mike Carcasole <mikecarcasole@gmail.com>
2018-11-13 14:16:21 -0800

    Make the pie chart support nested groupings.

--- src/org/chartjs/Pie.js
+++ src/org/chartjs/Pie.js
@@ -4,16 +4,45 @@ foam.CLASS({
   extends: 'org.chartjs.AbstractChartCView',
   properties: [
     [ 'chartType', 'pie' ],
+    {
+      name: 'config',
+      factory: function() {
+        return {
+          type: this.chartType,
+          datasets: [{}],
+          options: {
+            responsive: false,
+            maintainAspectRatio: false,
+            tooltips: {
+              callbacks: {
+                title: function(tooltipItem, data) {
+                  tooltipItem = tooltipItem[0];
+                  return data.datasets[tooltipItem.datasetIndex].label;
+                },
+                label: function(tooltipItem, data) {
+                  return data.labels[tooltipItem.index] +
+                    ': ' + data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index]
+                },
+              }
+            }
+          }
+        }
+      }
+    },
   ],
   methods: [
     function updateChart_(data) {
       var chartData = this.toChartData(data);
       chartData.datasets.forEach(function(d, i) {
+        if ( d.data.length && foam.Object.isInstance(d.data[0]) ) {
+          d.data = d.data.map(function(d) { return d.y });
+        }
         d.backgroundColor = this.colors.map(function(c) {
           return this.Lib.CHART.helpers.color(c).alpha(0.5).rgbString();
         }.bind(this))
         d.borderColor = this.colors
       }.bind(this));
+
       this.chart.data = chartData;
       this.chart.update();
     }
