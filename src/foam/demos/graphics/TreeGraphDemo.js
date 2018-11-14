218b645b8c0038e8e9ff6e004c46d100fd5b581b
Kevin Greer <kgrgreer@gmail.com>
2018-11-13 23:05:52 -0500

    Simplified TreeGraphDemo.

--- src/foam/demos/graphics/TreeGraphDemo.js
+++ src/foam/demos/graphics/TreeGraphDemo.js
@@ -4,15 +4,15 @@ foam.CLASS({
 
   properties: [
     [ 'width', 2000 ],
-    [ 'height', 1000 ]
+    [ 'height', 1000 ],
+    [ 'x', 0 ],
+    [ 'y', 0 ]
   ],
 
   methods: [
     function init() {
-      var g = this.Node.create({x:910, y:50});
-      this.add(g.addChildNode());
-      this.add(g.addChildNode());
-      this.add(g.addChildNode());
+      var g = this.Node.create({x:1110, y:40}, this);
+      this.add(g.addChildNode().addChildNode().addChildNode().addChildNode());
       g.childNodes[0].addChildNode()/*.addChildNode()*/;
       g.childNodes[0].addChildNode();
       g.childNodes[0].childNodes[0].addChildNode().addChildNode()/*.addChildNode()*/;
@@ -26,8 +26,6 @@ foam.CLASS({
       g.childNodes[2].childNodes[0].addChildNode();
       g.childNodes[2].childNodes[0].childNodes[1].addChildNode();
       g.childNodes[2].childNodes[0].childNodes[2].addChildNode().addChildNode();
-      g.layout();
-      g.x+=100;
     }
   ]
 });
