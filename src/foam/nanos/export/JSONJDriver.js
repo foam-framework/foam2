b555f7df1e69a20a241b1855b5a989ef8f310eac
Kevin Greer <kgrgreer@gmail.com>
2018-11-14 19:43:44 -0500

    Add JSON/J export driver.

---
+++ src/foam/nanos/export/JSONJDriver.js
@@ -0,0 +1,35 @@
+/**
+ * @license
+ * Copyright 2017 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.CLASS({
+  package: 'foam.nanos.export',
+  name: 'JSONJDriver',
+  implements: [ 'foam.nanos.export.ExportDriver' ],
+
+  documentation: 'Class for exporting data from a DAO to JSON/J',
+
+  properties: [
+    {
+      class: 'FObjectProperty',
+      of: 'foam.json.Outputter',
+      name: 'outputter',
+      factory: function() { return foam.json.Storage; }
+    }
+  ],
+
+  methods: [
+    function exportFObject(X, obj) {
+      return 'p(' + this.outputter.stringify(obj) + ')\r\n';
+    },
+
+    function exportDAO(X, dao) {
+      var output = '';
+      return dao.select(function(o) {
+        output += this.exportFObject(X, o);
+      }.bind(this)).then(function() { return output; });
+    }
+  ]
+});
