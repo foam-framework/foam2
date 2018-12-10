24e7a3231f431c501deeb77fc6394fa9a6ac5c9a
Adam Van Ymeren <adam@vany.ca>
2018-12-10 18:33:22 -0500

    Add many to many relationships test to test.js

--- test.js
+++ test.js
@@ -21,6 +21,37 @@ foam.__context__.classloader.load('test.Foo').then(function(c) {
   c.create().refined_method();
 });
 
-foam.__context__.classloader.load('foam.nanos.auth.User').then(function(c) {
-  console.log(c.create().stringify());
-});
+//foam.__context__.classloader.load('foam.nanos.auth.User').then(function(c) {
+//  console.log(c.create().stringify());
+//});
+
+Promise.all(
+  [foam.__context__.classloader.load('foam.nanos.demo.relationship.Professor'),
+   foam.__context__.classloader.load('foam.nanos.demo.relationship.Course'),
+   foam.__context__.classloader.load('foam.nanos.demo.relationship.Student')]).then(function(c) {
+     var x = foam.__context__.createSubContext({
+       courseDAO: foam.dao.EasyDAO.create({ daoType: 'MDAO', of: foam.nanos.demo.relationship.Course }),
+       studentDAO: foam.dao.EasyDAO.create({ daoType: 'MDAO', of: foam.nanos.demo.relationship.Student }),
+       professorDAO: foam.dao.EasyDAO.create({ daoType: 'MDAO', of: foam.nanos.demo.relationship.Professor }),
+       studentCourseJunctionDAO: foam.dao.EasyDAO.create({ daoType: 'MDAO', of: foam.nanos.demo.relationship.StudentCourseJunction })
+     });
+
+     var cs101 = foam.nanos.demo.relationship.Course.create({
+       code: 'cs101'
+     }, x);
+
+     var adam =  foam.nanos.demo.relationship.Student.create({
+       studentId: 123,
+       name: 'adam'
+     }, x);
+
+     x.courseDAO.put(cs101);
+     x.studentDAO.put(adam);
+
+     x.courseDAO.find('cs101').then(function(c) {
+       c.students.add(adam).then(function() {
+         console.log("Students in cs101");
+         c.students.dao.select(console);
+       });
+     });
+   });
