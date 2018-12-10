b8c0d7fe7338293a5f6be8dac7f663fe75407c41
Adam Van Ymeren <adam@vany.ca>
2018-12-10 17:05:35 -0500

    Imporove classloader demo/testcase.

--- test.js
+++ test.js
@@ -1,3 +1,8 @@
+process.on('unhandledRejection', function(e) {
+  console.error("ERROR: Unhandled promise rejection ", e);
+  process.exit(1);
+});
+
 global.FOAM_FLAGS = {
   node: true,
   web: false,
@@ -10,6 +15,12 @@ global.FOAM_FLAGS = {
 require('./foam-bin.js');
 require('./modeldao.js');
 
+console.log("Requesting foo");
 foam.__context__.classloader.load('test.Foo').then(function(c) {
+  console.log("Trying to create one");
   c.create().refined_method();
+});
+
+foam.__context__.classloader.load('foam.nanos.auth.User').then(function(c) {
+  console.log(c.create().stringify());
 });
