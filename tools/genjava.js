4434482325393d08cc814d50da0383e1e75d576a
Mike Carcasole <mikecarcasole@gmail.com>
2018-12-22 11:44:26 -0500

    Copy some changes that were done to genjava2 to genjava.

--- tools/genjava.js
+++ tools/genjava.js
@@ -209,7 +209,6 @@ function writeFileIfUpdated(outfile, buildJavaSource, opt_result) {
 }
 
 var addDepsToClasses = function() {
-
   // Determine all of the paths that the modelDAO should search will use when
   // arequiring all of the classes.
   var paths = {};
@@ -231,29 +230,56 @@ var addDepsToClasses = function() {
     classloader.addClassPath(p);
   });
 
-  return Promise.all(classes.map(function(cls) {
-    return classloader.load(cls);
-  })).then(function() {
-    var classMap = {};
-    var classQueue = classes.slice(0);
-    while ( classQueue.length ) {
-      var cls = classQueue.pop();
-      if ( ! classMap[cls] && ! blacklist[cls] ) {
-        cls = foam.lookup(cls);
-        if ( ! flagFilter(cls.model_) ) continue;
-        classMap[cls.id] = true;
-        cls.getAxiomsByClass(foam.core.Requires).filter(flagFilter).forEach(function(r) {
-          r.javaPath && classQueue.push(r.javaPath);
-        });
-        cls.getAxiomsByClass(foam.core.Implements).filter(flagFilter).forEach(function(r) {
-          classQueue.push(r.path);
-        });
-        if ( cls.model_.extends ) classQueue.push(cls.model_.extends);
+  return (function() {
+    var loadClass = foam.cps.awrap(classloader.load.bind(classloader));
+
+    function collectDeps() {
+      var classMap = {};
+      var classQueue = classes.slice(0);
+      while ( classQueue.length ) {
+        var cls = classQueue.pop();
+        if ( ! classMap[cls] && ! blacklist[cls] ) {
+          cls = foam.lookup(cls);
+          if ( ! checkFlags(cls.model_) ) continue;
+          classMap[cls.id] = true;
+          cls.getAxiomsByClass(foam.core.Requires).filter(flagFilter).forEach(function(r) {
+            r.javaPath && classQueue.push(r.javaPath);
+          });
+          cls.getAxiomsByClass(foam.core.Implements).filter(flagFilter).forEach(function(r) {
+            classQueue.push(r.path);
+          });
+          if ( cls.model_.extends ) classQueue.push(cls.model_.extends);
+        }
       }
+      classes = Object.keys(classMap);
     }
-    classes = Object.keys(classMap);
-  });
+
+    with ( foam.cps ) {
+      return new Promise(
+        sequence(
+          compose(map(loadClass), value(classes)),
+          wrap(collectDeps)));
+
+    }
+  })();
 };
+
+function checkFlags(model) {
+  var parent = true;
+
+  if ( model.extends &&
+       ( model.extends != 'foam.core.FObject' && model.extends != 'FObject' ) ) {
+    parent = checkFlags(foam.lookup(model.extends).model_);
+  }
+
+  if ( ! parent ) return false;
+
+  if ( model.flags && model.flags.indexOf('java') == -1 ) {
+    return false;
+  }
+
+  return true;
+}
 
 addDepsToClasses().then(function() {
   classes.forEach(loadClass);
