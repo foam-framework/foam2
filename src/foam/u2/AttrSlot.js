8ec2ac4d5783cd77d02ed8802acd464a5481f784
Mike Carcasole <mikecarcasole@gmail.com>
2018-12-26 19:18:46 -0500

    Make AttrSlot extend Slot rather than implement because Slot is a class, not an interface.

--- src/foam/u2/AttrSlot.js
+++ src/foam/u2/AttrSlot.js
@@ -18,12 +18,7 @@
 foam.CLASS({
   package: 'foam.u2',
   name: 'AttrSlot',
-  implements: [
-    {
-      path: 'foam.core.Slot',
-      flags: ['js'],
-    },
-  ],
+  extends: 'foam.core.Slot',
 
   documentation: 'A Value bound to an Element attribute. Used to bind values to DOM.',
 
