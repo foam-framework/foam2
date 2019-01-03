c82d560542886559133e5d62a4182986b6cdb92c
Devansh Jain <devansh@nanopay.net>
2019-01-03 14:43:44 -0500

    Make styling changes to conform to style guide

--- src/foam/nanos/auth/resetPassword/EmailView.js
+++ src/foam/nanos/auth/resetPassword/EmailView.js
@@ -22,7 +22,7 @@ foam.CLASS({
     'foam.u2.dialog.NotificationMessage'
   ],
 
-  css:`
+  css: `
     ^{
       width: 490px;
       margin: auto;
@@ -144,7 +144,7 @@ foam.CLASS({
   ],
 
   methods: [
-    function initE(){
+    function initE() {
     this.SUPER();
     var self = this;
 
@@ -154,7 +154,7 @@ foam.CLASS({
         .start().addClass('Forgot-Password').add('Forgot Password').end()
         .start().addClass('Message-Container')
         .start().addClass('Instructions-Text').add(this.Instructions).end()
-        .start().addClass('Email-Text').add("Email Address").end()
+        .start().addClass('Email-Text').add('Email Address').end()
         .start(this.EMAIL).addClass('full-width-input').end()
         .start(this.NEXT).addClass('Next-Button').end()
         .start('p').add('Remember your password?').end()
@@ -171,16 +171,16 @@ foam.CLASS({
   actions: [
     {
       name: 'next',
-      code: function (X) {
+      code: function(X) {
         var self = this;
         var user = this.User.create({ email: this.email });
-        this.resetPasswordToken.generateToken(null, user).then(function (result) {
+        this.resetPasswordToken.generateToken(null, user).then(function(result) {
           if ( ! result ) {
             throw new Error('Error generating reset token');
           }
-          self.stack.push(self.ResendView.create({ email: self.email }));;
+          self.stack.push(self.ResendView.create({ email: self.email }));
         })
-        .catch(function (err) {
+        .catch(function(err) {
           self.add(self.NotificationMessage.create({ message: err.message, type: 'error' }));
         });
       }
