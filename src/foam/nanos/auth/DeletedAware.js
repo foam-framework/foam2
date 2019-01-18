42c99ef92514298076907ff8c7a62272ab765281
Chanmann Lim <chanmannlim@gmail.com>
2019-01-18 16:46:15 -0500

    Add DeletedAware interface and support

---
+++ src/foam/nanos/auth/DeletedAware.js
@@ -0,0 +1,29 @@
+/**
+ * @license
+ * Copyright 2019 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.INTERFACE({
+  package: 'foam.nanos.auth',
+  name: 'DeletedAware',
+
+  methods: [
+    {
+      name: 'getDeleted',
+      returns: 'Boolean',
+      javaReturns: 'boolean',
+      swiftReturns: 'Bool'
+    },
+    {
+      name: 'setDeleted',
+      args: [
+        {
+          name: 'value',
+          javaType: 'boolean',
+          swiftType: 'Bool'
+        }
+      ]
+    }
+  ]
+});
