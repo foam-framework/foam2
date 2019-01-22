f242bbdc0fe97d49ff457656322c8c7f7637b43f
Chanmann Lim <chanmannlim@gmail.com>
2019-01-22 14:49:48 -0500

    Cleanup and add java imports

--- src/foam/nanos/auth/DeletedAwareDAOTest.js
+++ src/foam/nanos/auth/DeletedAwareDAOTest.js
@@ -9,6 +9,13 @@ foam.CLASS({
   name: 'DeletedAwareDAOTest',
   extends: 'foam.nanos.test.Test',
 
+  javaImports: [
+    'foam.core.X',
+    'foam.core.FObject',
+    'foam.dao.DAO',
+    'foam.dao.MDAO',
+  ],
+
   methods: [
     {
       name: 'runTest',
@@ -20,22 +27,22 @@ foam.CLASS({
     {
       name: 'DeletedAwareDAOTest_remove_DeletedAware',
       args: [
-        { of: 'foam.core.X', name: 'x' }
+        { of: 'X', name: 'x' }
       ],
       javaCode: `
-        foam.dao.DAO delegate = new foam.dao.MDAO(foam.nanos.auth.DeletedAwareDummy.getOwnClassInfo());
-        foam.dao.DAO dao = (foam.dao.DAO) new DeletedAwareDAO.Builder(x)
+        DAO delegate = new MDAO(DeletedAwareDummy.getOwnClassInfo());
+        DAO dao = (DAO) new DeletedAwareDAO.Builder(x)
           .setDelegate(delegate)
           .build();
 
-        foam.core.FObject object = new foam.nanos.auth.DeletedAwareDummy.Builder(x)
+        FObject object = new DeletedAwareDummy.Builder(x)
           .setId(1)
           .setDeleted(false)
           .build();
-        object = (foam.core.FObject) dao.put(object);
+        object = (FObject) dao.put(object);
 
         dao.remove(object);
-        object = (foam.core.FObject) dao.find(object.getProperty("id"));
+        object = dao.find(object.getProperty("id"));
 
         test(object != null, "DeletedAwareDAO does not remove DeletedAware object from DAO.");
         test(
@@ -50,15 +57,15 @@ foam.CLASS({
         { of: 'foam.core.X', name: 'x' }
       ],
       javaCode: `
-        foam.dao.DAO delegate = new foam.dao.MDAO(foam.nanos.auth.User.getOwnClassInfo());
-        foam.dao.DAO dao = (foam.dao.DAO) new DeletedAwareDAO.Builder(x)
+        DAO delegate = new MDAO(User.getOwnClassInfo());
+        DAO dao = (DAO) new DeletedAwareDAO.Builder(x)
           .setDelegate(delegate)
           .build();
 
-        foam.core.FObject object = new foam.nanos.auth.User.Builder(x)
+        FObject object = new User.Builder(x)
           .setEmail("test@example.com")
           .build();
-        object = (foam.core.FObject) dao.put(object);
+        object = (FObject) dao.put(object);
 
         dao.remove(object);
         object = dao.find(object.getProperty("id"));
