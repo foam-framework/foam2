8874c63393cd8a0f03241f4c544d75c4f490123d
sirenChen <sirenchen.dev@gmail.com>
2019-01-23 13:47:43 -0500

    Add synchronized to put and remove

--- src/foam/dao/MDAO.java
+++ src/foam/dao/MDAO.java
@@ -70,7 +70,7 @@ public class MDAO
     state_ = state;
   }
 
-  public FObject put_(X x, FObject obj) {
+  public synchronized FObject put_(X x, FObject obj) {
     // Clone and freeze outside of lock to minimize time spent under lock
     obj = obj.fclone();
     obj.freeze();
@@ -90,7 +90,7 @@ public class MDAO
     return obj;
   }
 
-  public FObject remove_(X x, FObject obj) {
+  public synchronized FObject remove_(X x, FObject obj) {
     if ( obj == null ) return null;
 
     FObject found;
