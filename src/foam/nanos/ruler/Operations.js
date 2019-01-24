37169b34d86a715c26b359c5994c2b44a4dafe71
penzital <penzital888@gmail.com>
2019-01-24 16:34:11 -0500

    added rule class

---
+++ src/foam/nanos/ruler/Operations.js
@@ -0,0 +1,19 @@
+foam.ENUM({
+  package: 'foam.nanos.ruler',
+  name: 'Operations',
+
+  values: [
+    {
+      name: 'CREATE',
+      label: 'Create'
+    },
+    {
+      name: 'UPDATE',
+      label: 'Update'
+    },
+    {
+      name: 'REMOVE',
+      label: 'Remove'
+    }
+  ]
+});
---
+++ src/foam/nanos/ruler/Rule.js
@@ -0,0 +1,66 @@
+foam.CLASS({
+  package: 'foam.nanos.ruler',
+  name: 'Rule',
+
+  documentation: 'Rule',
+
+  javaImports: [
+    'foam.dao.ArraySink',
+    'foam.dao.DAO',
+    'foam.nanos.auth.User',
+    'java.util.List',
+    'net.nanopay.account.DigitalAccount'
+  ],
+
+  properties: [
+    {
+      class: 'Long',
+      name: 'id',
+      visibility: foam.u2.Visibility.RO
+    },
+    {
+      class: 'String',
+      name: 'name'
+    },
+    {
+      class: 'String',
+      name: 'ruleGroup'
+    },
+    {
+      class: 'String',
+      name: 'documentation'
+    },
+    {
+      class: 'FObjectProperty',
+      of: 'foam.mlang.predicate.Predicate',
+      name: 'predicate'
+    },
+    {
+      class: 'FObjectProperty',
+      of: 'foam.core.ContextAgent',
+      name: 'action'
+    },
+    {
+      class: 'Int',
+      name: 'priority'
+    },
+    {
+      class: 'Reference',
+      of: 'foam.nanos.boot.NSpec',
+      name: 'daoKey'
+    },
+    // {
+    //   class: 'Enum',
+    //   of: 'foam.nanos.ruler.Operations',
+    //   name: 'operation'
+    // },
+    {
+      class: 'Boolean',
+      name: 'after'
+    },
+    {
+      class: 'Boolean',
+      name: 'stops'
+    }
+  ]
+});
