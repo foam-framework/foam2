ca3d422be3e25e290e6f1c7c6f82d147c3c12388
Chanmann Lim <chanmannlim@gmail.com>
2019-01-24 07:15:26 -0500

    Add documentation

--- src/foam/nanos/auth/DeletedAwareDAO.js
+++ src/foam/nanos/auth/DeletedAwareDAO.js
@@ -9,7 +9,17 @@ foam.CLASS({
   name: 'DeletedAwareDAO',
   extends: 'foam.dao.ProxyDAO',
 
-  documentation: 'DAO that sets deleted property',
+  documentation: `DAO decorator that sets deleted property when an object
+    is removing and filters out deleted=true objects based on permission.
+
+    DeletedAwareDAO marks object as deleted instead of actually removing
+    the object from DAO then returns thus it should be placed at the end
+    of the decorator stack before MDAO so that it wouldn't cut-off other
+    decorators that also override "remove_" and "removeAll_".
+
+    For filtering, objects with deleted=true will be filtered out unless
+    the user group has {model}.read.deleted permission where {model} is
+    the lowercase name of the model of the objects.`,
 
   methods: [
     {
