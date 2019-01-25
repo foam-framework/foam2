68bcb7b720953d69501f8497270f0318fdb9058c
blakegreen <blake@nanopay.net>
2019-01-25 12:47:55 -0500

    Added blacklist properties

---
+++ src/foam/nanos/auth/Blacklist.js
@@ -0,0 +1,26 @@
+/**
+ * @license
+ * Copyright 2017 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+ foam.CLASS({
+   package: 'foam.nanos.auth',
+   name: 'Blacklist',
+   extends: 'foam.nanos.auth.Group',
+
+   documentation: 'Blacklist entity associated to a users group',
+
+   properties: [
+     {
+       class: 'String',
+       name: 'id',
+       documentation: 'Blacklist Id'
+     },
+     {
+       class: 'String',
+       name: 'name',
+       documentation: 'Name of blacklist'
+     }
+   ]
+ });
