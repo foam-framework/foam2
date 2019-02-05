fa43620b098b6864a8722ead33280026d94ffcb5
hchoura <32652535+hchoura@users.noreply.github.com>
2019-02-05 21:10:02 +0100

    add Hello example

---
+++ src/com/foamdev/demos/helloMVC/Controller.js
@@ -0,0 +1,32 @@
+/**
+ * @license
+ * Copyright 2019 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.CLASS({
+  package: 'com.foamdev.demos.helloMVC',
+  name: 'Controller',
+  extends: 'foam.u2.Element',
+
+  exports: [ 'data as dataImported' ],  //FOAM has DI (Dependency Injection) built-in at the object level.
+
+  requires: [
+    'com.foamdev.demos.helloMVC.Hello',
+    'com.foamdev.demos.helloMVC.HelloView',
+  ],
+
+
+  properties: [
+    {
+      name   : 'data',
+      factory: function() { return this.Hello.create().yourName; }
+    }
+  ],
+
+  methods: [
+    function initE() {
+      this.start(com.foamdev.demos.helloMVC.HelloView);
+    }
+  ]
+});
