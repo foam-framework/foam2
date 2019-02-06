b8dcf6ad15b4df17b248c8d937ab6e352c206889
hchoura <32652535+hchoura@users.noreply.github.com>
2019-02-06 12:37:31 +0100

    use dynamic import and export

--- src/com/foamdev/demos/helloMVC/HelloView.js
+++ src/com/foamdev/demos/helloMVC/HelloView.js
@@ -9,21 +9,16 @@ foam.CLASS({
   name: 'HelloView',
   extends: 'foam.u2.View',// it will imports 'data'
 
-  /*imports: [
-    'dataImported'
-  ],*/
  /* imports: [
-    'data'
+    'HelloUser'
   ],*/
 
   methods: [
     function initE() {
-      //this.start('div').add('Name:').end().start('div').add(this.dataImported.yourName).end(). 
-      this.start('div').add('Name:').end().start('div').add(this.data.yourName).end(). 
-        //TODO start(this.YOUR_NAME).attrs({onKey: true, placeholder:'Your name please'}).end().
-        //TODO .add(this.dataImported.YOUR_NAME)
-        //start('h1').add('Hello ').add(this.dataImported.yourName$).add(this.dataImported.YOUR_NAME).add('!').end();
-        start('h1').add('Hello ').add(this.data.yourName$).add(this.data.YOUR_NAME).add('!').end();
+      this.start('h1').add('Name:').end().start('div').add(this.data.yourName).end(). 
+        //start(this.data.YOUR_NAME).attrs({onKey: true, placeholder:'Your name please'}).end().
+        start('h1').add('Hello ').add(this.data.yourName$).add('!').end().
+        start('h1').add('Hello ').add(this.data.YOUR_NAME).end();
     }
   ]
 });
