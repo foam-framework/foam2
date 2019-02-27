bc7ecb256b8165912d939d919311875f6e20c9ae
Chanmann Lim <chanmannlim@gmail.com>
2019-02-27 06:53:00 -0500

    Indent

--- src/foam/nanos/ruler/RuleHistory.js
+++ src/foam/nanos/ruler/RuleHistory.js
@@ -5,47 +5,47 @@
  */
 
 foam.CLASS({
- package: 'foam.nanos.ruler',
- name: 'RuleHistory',
- documentation: 'Represents rule execution history.',
- properties: [
-   {
-     class: 'Long',
-     name: 'id',
-     visibility: 'RO'
-   },
-   {
-     class: 'DateTime',
-     name: 'created',
-     documentation: 'Creation date.'
-   },
-   {
-     class: 'DateTime',
-     name: 'lastModified',
-     documentation: 'Last modified date.'
-   },
-   {
-     class: 'Reference',
-     of: 'foam.nanos.ruler.Rule',
-     name: 'ruleId',
-     documentation: 'The applied rule.'
-   },
-   {
-     class: 'Object',
-     name: 'ObjectId',
-     visibility: 'RO',
-     documentation: 'Id of the object onwhich rule is applied.'
-   },
-   {
-     class: 'String',
-     name: 'objectDaoKey',
-     visibility: 'RO',
-     documentation: 'DAO name of the object'
-   },
-   {
-     class: 'Object',
-     name: 'result',
-     documentation: 'Result of rule execution.'
-   }
- ]
+  package: 'foam.nanos.ruler',
+  name: 'RuleHistory',
+  documentation: 'Represents rule execution history.',
+  properties: [
+    {
+      class: 'Long',
+      name: 'id',
+      visibility: 'RO'
+    },
+    {
+      class: 'DateTime',
+      name: 'created',
+      documentation: 'Creation date.'
+    },
+    {
+      class: 'DateTime',
+      name: 'lastModified',
+      documentation: 'Last modified date.'
+    },
+    {
+      class: 'Reference',
+      of: 'foam.nanos.ruler.Rule',
+      name: 'ruleId',
+      documentation: 'The applied rule.'
+    },
+    {
+      class: 'Object',
+      name: 'ObjectId',
+      visibility: 'RO',
+      documentation: 'Id of the object onwhich rule is applied.'
+    },
+    {
+      class: 'String',
+      name: 'objectDaoKey',
+      visibility: 'RO',
+      documentation: 'DAO name of the object'
+    },
+    {
+      class: 'Object',
+      name: 'result',
+      documentation: 'Result of rule execution.'
+    }
+  ]
 });
