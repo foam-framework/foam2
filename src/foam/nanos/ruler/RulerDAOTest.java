f8cab3c5e3d7ee07203dd390883d39d302fd7fd4
Chanmann Lim <chanmannlim@gmail.com>
2019-02-27 14:30:34 -0500

    Mock localUserDAO in tests

--- src/foam/nanos/ruler/RulerDAOTest.java
+++ src/foam/nanos/ruler/RulerDAOTest.java
@@ -5,6 +5,7 @@ import foam.dao.DAO;
 import foam.mlang.predicate.Predicate;
 import foam.nanos.auth.User;
 import foam.nanos.test.Test;
+import foam.test.TestUtils;
 
 import static foam.mlang.MLang.*;
 
@@ -15,10 +16,10 @@ public class RulerDAOTest extends Test {
   int asyncWait = 1000;
 
   public void runTest(X x) {
+    x = TestUtils.mockDAO(x, "localUserDAO");
     ruleDAO = ((DAO) x.get("ruleDAO"));
-    userDAO = new RulerDAO(x, (DAO)x.get("localUserDAO"), "localUserDAO");
+    userDAO = new RulerDAO(x, (DAO) x.get("localUserDAO"), "localUserDAO");
     createRule(x);
-    x = x.put(userDAO, "localUserDAO");
     testUsers(x);
     removeData(x);
   }
