13bdba5f3742be02c9b3cad0de9d669c69b1fc30
Joel Hughes <joel@nanopay.net>
2019-02-28 14:56:46 -0500

    Remove explicit reference to webroot.
    Also, images are now placed above webroot. See comments in code.

--- src/foam/nanos/servlet/ResourceImageServlet.java
+++ src/foam/nanos/servlet/ResourceImageServlet.java
@@ -9,6 +9,12 @@ package foam.nanos.servlet;
 import foam.util.SafetyUtil;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
+/**
+ * @license
+ * Copyright 2019 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 import org.apache.commons.text.StringEscapeUtils;
 
 import javax.servlet.ServletException;
@@ -21,30 +27,35 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ResourceImageServlet
+/**
+   Image lookup assumes images are places at the root of the jar file.
+   During build, generally output/copied to classes/
+   HTTPServlet setups the resourcebase using webroot, but so far that
+   setup requires explicit knowledge of the webroot folder name, which
+   needs to be prepended to the request.
+*/
+public class ResourceImageServlet 
   extends ImageServlet
-{
+{  
   @Override
   protected void service(HttpServletRequest req, HttpServletResponse resp)
     throws ServletException, IOException
   {
-    System.out.println("ResourceImageServlet.service uri: "+req.getRequestURI());
-    String request = req.getRequestURI().replaceFirst("/?images/?", "/");
-    System.out.println("ResourceImageServlet.service request: "+request);
-    // tried a few prefixes, /images, /webapp/images, images/, 
-    InputStream is = getClass().getResourceAsStream("images/"+request);
+    String request = req.getRequestURI();
+    //System.out.println("ResourceImageServlet.service uri: "+req.getRequestURI());
+
+    InputStream is = getClass().getResourceAsStream(request);
     if ( is != null ) {
       String ext = EXTS.get(request.substring(request.lastIndexOf('.') +1));
       try ( BufferedInputStream bis = new BufferedInputStream(is) ) {
         resp.setContentType(!SafetyUtil.isEmpty(ext) ? ext : DEFAULT_EXT);
         resp.setHeader("Content-Disposition", "filename=\"" + StringEscapeUtils.escapeHtml4(request) + "\"");
-        //resp.setContentLengthLong(src.length());
 
         IOUtils.copy(bis, resp.getOutputStream());
         return;
       }
-    } else {
-      System.out.println("ResourceImageServlet.service is: null");
+    // } else {
+    //  System.out.println("ResourceImageServlet.service '"+request+"' not found.");
     }
     resp.sendError(resp.SC_NOT_FOUND);
   }
