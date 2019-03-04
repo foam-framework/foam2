c37494d75065ddb47eedbf63bfacbe9d3608b6f4
Mike Carcasole <mikecarcasole@gmail.com>
2019-03-04 16:46:15 -0500

    Undo a couple of changes.

--- src/files.js
+++ src/files.js
@@ -100,7 +100,6 @@ FOAM_FILES([
   { name: "foam/swift/Argument", flags: ['swift'] },
   { name: "foam/swift/ProtocolArgument", flags: ['swift'] },
   { name: "foam/swift/refines/Action", flags: ['swift'] },
-  { name: "foam/swift/refines/Promised", flags: ['swift'] },
   { name: "foam/swift/refines/InnerClass", flags: ['swift'] },
   { name: "foam/swift/refines/AbstractInterface", flags: ['swift'] },
   { name: "foam/swift/refines/Argument", flags: ['swift'] },
@@ -359,6 +358,7 @@ FOAM_FILES([
   { name: "foam/box/AuthServiceClientBox" },
 
   { name: "foam/swift/refines/Stub", flags: ['swift'] },
+  { name: "foam/swift/refines/Promised", flags: ['swift'] },
   { name: "foam/swift/refines/Topic", flags: ['swift'] },
   { name: "foam/swift/refines/Remote", flags: ['swift'] },
 
