e7b64f3d1495ef87fe752b63565fd11ab53df180
Mike Carcasole <mikecarcasole@gmail.com>
2019-03-20 10:23:40 -0400

    Create a CSVSink template and demo

---
+++ src/foam/dao/CSVSink.js
@@ -0,0 +1,13 @@
+foam.CLASS({
+  package: 'foam.dao',
+  name: 'CSVSink',
+  extends: 'foam.dao.AbstractSink',
+  properties: [
+    {
+      class: 'String',
+      name: 'csv',
+      view: 'foam.u2.tag.TextArea',
+      value: 'TODO'
+    }
+  ]
+});
