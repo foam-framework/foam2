f43f4f9c9b352aeea8b818995f69867d898d2352
Kate Xia <xiamengjun@gmail.com>
2019-03-22 11:00:21 -0400

    updated test output

--- src/foam/nanos/hello/HelloTest.java
+++ src/foam/nanos/hello/HelloTest.java
@@ -9,6 +9,6 @@ public class HelloTest extends foam.nanos.test.Test {
   public void runTest(X x) {
     ClientHelloService helloService = new ClientHelloService.Builder(x).setDelegate(new HTTPBox.Builder(x).setUrl("http://localhost:8080/service/hello").build()).build();
     String result = helloService.hello("Java");
-    test( result.equals("helloJava"), "ClientHelloService succeed");
+    test( result.equals("helloJava"), "ClientHelloService Java");
   }
 }
