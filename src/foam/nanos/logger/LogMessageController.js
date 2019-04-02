58c67530352011ea7a1044dff0d6d4a33ae6b772
Troy Wolters <troy@nanopay.net>
2019-04-02 09:39:47 -0400

    Remove unnecessary code to limit table width
    With the recent changes to tables, this is no longer necessary.

--- src/foam/nanos/logger/LogMessageController.js
+++ src/foam/nanos/logger/LogMessageController.js
@@ -16,25 +16,6 @@ foam.CLASS({
       factory: function() {
         return this.logMessageDAO.orderBy(this.LogMessage.CREATED);
       }
-    },
-    {
-      name: 'summaryView',
-      factory: function() {
-        return this.FixedWidthScrollTableView;
-      }
-    }
-  ],
-
-  classes: [
-    {
-      name: 'FixedWidthScrollTableView',
-      extends: 'foam.u2.view.ScrollTableView',
-
-      css: `
-        ^ {
-          width: 968px;
-        }
-      `
     }
   ]
 });
