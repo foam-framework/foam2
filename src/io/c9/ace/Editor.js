3f6bc078f8d644984106886f66fd313b91fbd878
Mike Carcasole <mikecarcasole@gmail.com>
2019-04-04 09:16:02 -0400

    Make the ace editor Lib a foam.LIB and provide a fallback TextArea if the editor library fails to load.

--- src/io/c9/ace/Editor.js
+++ src/io/c9/ace/Editor.js
@@ -3,8 +3,12 @@ foam.CLASS({
   name: 'Editor',
   extends: 'foam.u2.View',
   requires: [
-    'io.c9.ace.Lib',
-    'foam.u2.DetailView'
+    'foam.u2.DetailView',
+    'foam.u2.tag.TextArea',
+    'io.c9.ace.Config'
+  ],
+  imports: [
+    'warn'
   ],
   reactions: [
     ['container', 'onload', 'initEditor'],
@@ -33,70 +37,52 @@ foam.CLASS({
       name: 'showConfig'
     }
   ],
-  classes: [
-    {
-      name: 'Config',
-      properties: [
-        {
-          class: 'Int',
-          name: 'height',
-          value: 500
-        },
-        {
-          class: 'Int',
-          name: 'width',
-          value: 700
-        },
-        {
-          class: 'Enum',
-          of: 'io.c9.ace.Theme',
-          name: 'theme'
-        },
-        {
-          class: 'Enum',
-          of: 'io.c9.ace.Mode',
-          name: 'mode',
-          value: 'JAVA'
-        },
-        {
-          class: 'Enum',
-          of: 'io.c9.ace.KeyBinding',
-          name: 'keyBinding',
-          value: 'VIM'
-        }
-      ]
-    }
-  ],
   methods: [
     function initE() {
       var self = this;
       this
-        .start('div', null, this.container$)
-          .style({
-            height: this.config$.dot('height').map(h => h + 'px'),
-            width: this.config$.dot('width').map(h => h + 'px')
-          })
-        .end()
-        .start('span')
-          .style({cursor: 'pointer'})
-          .add(self.showConfig$.map(b => b ? 'Hide Editor Config' : 'Show Editor Config'))
-          .on('click', function() { self.showConfig = ! self.showConfig; })
-        .end()
-        .add(this.slot(function(showConfig) {
-          return this.E().callIf(showConfig, function() {
-            this.tag(self.DetailView, { data$: self.config$ });
+        .add(this.slot(function(editor) {
+          // Initially render a TextArea while the library loads.
+          // If the library fails to load, this will be the fallback.
+          return editor ? null : self.TextArea.create({
+            data$: self.data$,
+            rows: 20, cols: 120,
+            css: { 'font-family': 'monospace' }
           });
-        }));
+        }))
+        .start()
+          .style({
+            display: this.editor$.map(e => e ? 'block' : 'none')
+          })
+          .start('div', null, this.container$)
+            .style({
+              height: this.config$.dot('height').map(h => h + 'px'),
+              width: this.config$.dot('width').map(h => h + 'px')
+            })
+          .end()
+          .start('span')
+            .style({cursor: 'pointer'})
+            .add(self.showConfig$.map(b => b ? 'Hide Editor Config' : 'Show Editor Config'))
+            .on('click', function() { self.showConfig = ! self.showConfig; })
+          .end()
+          .add(this.slot(function(showConfig) {
+            return this.E().callIf(showConfig, function() {
+              this.tag(self.DetailView, { data$: self.config$ });
+            });
+          }))
+        .end();
     }
   ],
   listeners: [
     function initEditor() {
       var self = this;
-      self.Lib.ACE.then(function(ace) {
+      io.c9.ace.Lib.ACE().then(function(ace) {
         self.editor = ace.edit(self.container.id);
         self.editor.session.on('change', self.editorToData);
         self.updateEditor();
         self.dataToEditor();
+      }).catch(function() {
+        self.warn('Unable to load ace editor.');
       });
     },
     {
