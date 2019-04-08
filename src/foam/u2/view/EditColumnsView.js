77f9d739770275dd6529e4bc2ddf634344618d10
Troy Wolters <troy@nanopay.net>
2019-04-08 11:35:47 -0400

    Remember the user's column preferences

--- src/foam/u2/view/EditColumnsView.js
+++ src/foam/u2/view/EditColumnsView.js
@@ -43,12 +43,23 @@ foam.CLASS({
         .addClass(this.myClass())
         .forEach(this.columns, function(columnName) {
           var axiom = this.table.getAxiomByName(columnName);
+          var localStorageKey = this.table.id + '.' + columnName;
+          var valueFromLocalStorage = localStorage.getItem(localStorageKey);
+
+          // This will be true only when seeing a new model for the first time.
+          if ( valueFromLocalStorage === null ) {
+            localStorage.setItem(localStorageKey, 'Y');
+            valueFromLocalStorage = true;
+          }
+
           var checkBox = this.CheckBox.create({
             label: axiom.label,
-            data: true // TODO: Load from localStorage.
+
+            // Double negate to cast String to Boolean.
+            data: !! valueFromLocalStorage
           });
 
-          checkBox.data$.sub(this.updateCols(axiom, index++));
+          checkBox.data$.sub(this.updateCols(axiom, index++, localStorageKey));
 
           this.start().tag(checkBox).end();
         });
@@ -56,15 +67,23 @@ foam.CLASS({
   ],
 
   listeners: [
-    function updateCols(axiom, index) {
+    function updateCols(axiom, index, localStorageKey) {
       return (_, __, ___, propSlot) => {
         var checked = propSlot.get();
         if ( checked ) {
+          // Any truthy string will suffice.
+          localStorage.setItem(localStorageKey, 'Y');
+
+          // Put the column back in the correct position.
           this.columns_.splice(index, 0, axiom);
 
           // Force the view to update.
           this.columns_ = this.columns_.slice();
         } else {
+          // Set to the only falsey string. We can't just set the items to true
+          // and false because localStorage only stores strings as values.
+          localStorage.setItem(localStorageKey, '');
+
           this.columns_ = this.columns_.filter((col) => col.name !== axiom.name);
         }
       };
