909c927a8fe550e4232a8efc4464d4bf47610a4d
Neelkanth Patel <neel@nanopay.net>
2019-04-12 14:11:37 -0400

    update date picker ui

--- src/foam/u2/view/date/Weekday.js
+++ src/foam/u2/view/date/Weekday.js
@@ -2,12 +2,12 @@ foam.ENUM({
   package: 'foam.u2.view.date',
   name: 'Weekday',
   values: [
-    { name: 'SUN', label: 'Sunday' },
-    { name: 'MON', label: 'Monday' },
-    { name: 'TUE', label: 'Tuesday' },
-    { name: 'WED', label: 'Wednesday' },
-    { name: 'THU', label: 'Thursday' },
-    { name: 'FRI', label: 'Friday' },
-    { name: 'SAT', label: 'Saturday' }
+    { name: 'SU', label: 'Sunday' },
+    { name: 'MO', label: 'Monday' },
+    { name: 'TU', label: 'Tuesday' },
+    { name: 'WE', label: 'Wednesday' },
+    { name: 'TH', label: 'Thursday' },
+    { name: 'FR', label: 'Friday' },
+    { name: 'SA', label: 'Saturday' }
   ]
 });
