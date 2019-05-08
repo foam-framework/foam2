edb27e6667cf58ab7f9131bf4873ed65cb9b783f
Danny Tharma <daniel@nanopay.net>
2019-05-08 13:16:14 -0400

    Included only the section and layout related items from CPF-1078/SectionedDetailView, plus made two bug fixes: 1 to CardBorder and 1 to User.js

---
+++ src/foam/u2/SectionedDetailPropertyView.js
@@ -0,0 +1,52 @@
+/**
+ * @license
+ * Copyright 2019 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.CLASS({
+  package: 'foam.u2',
+  name: 'SectionedDetailPropertyView',
+  extends: 'foam.u2.View',
+
+   documentation: `
+    View for one property of a SectionedDetailView.
+  `,
+
+   requires: [
+    'foam.u2.layout.Rows',
+    'foam.u2.layout.Cols',
+    'foam.u2.layout.Col'
+  ],
+
+   properties: [
+    'prop',
+  ],
+
+   methods: [
+    function initE() {
+      var self = this;
+      this.SUPER();
+
+       this
+        .addClass(this.myClass())
+        .add(this.slot(function(prop) {
+          var errorSlot = self.slot(function(data, prop$validateObj){
+            return data && prop$validateObj && data.slot(prop$validateObj);
+          });
+          return self.E()
+            .start(self.Rows)
+              .add(prop.label$)
+              .start(self.Cols)
+                .start(self.Col, { flex: 1 })
+                  .add(prop)
+                .end()
+                .add(prop.help$)
+              .end()
+              .add(errorSlot.map((s) => {
+                return self.E().add(s);
+              }));
+        }));
+    }
+  ]
+});
---
+++ src/foam/u2/layout/AlignmentType.js
@@ -0,0 +1,24 @@
+foam.ENUM({
+  package: 'foam.u2.layout',
+  name: 'AlignmentType',
+
+   documentation: `
+      An enum of alignment types for the Layout Package
+  `,
+
+   values: [
+    { name: "START", webFlexProp: 'flex-start' },
+    { name: "END", webFlexProp: 'flex-end' },
+    { name: "CENTER", webFlexProp: 'center' },
+    { name: "SPACE_AROUND", webFlexProp: 'space-around' },
+    { name: "SPACE_BETWEEN", webFlexProp: 'space-between' },
+    { name: "SPACE_EVENLY", webFlexProp: 'space-evenly' },
+  ],
+
+   properties:  [
+    {
+      class: 'String',
+      name: 'webFlexProp',
+    },
+  ]
+});
