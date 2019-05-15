aff9e154fb7dfa70ecd9babe786146801fa20843
Danny Tharma <daniel@nanopay.net>
2019-05-14 20:13:39 -0400

    Added in the checkbox styling

--- src/foam/u2/detail/SectionedDetailPropertyView.js
+++ src/foam/u2/detail/SectionedDetailPropertyView.js
@@ -52,12 +52,10 @@ foam.CLASS({
     }
 
     /*
+      !IMPORTANT!
       For the following inputs below, we are planning 
       encode these changes in the actual foam files
     */
-    ^ .foam-u2-CheckBox-label {
-      margin-left: 12px;
-    }
 
     ^ .foam-u2-view-RadioView label {
       margin-left: 12px;
@@ -67,9 +65,6 @@ foam.CLASS({
       width: 100%;
       padding: 10px 8px;
       font-size: 14px;
-    }
-
-    ^ .foam-u2-CheckBox {
     }
 
     ^ .foam-u2-view-date-DateTimePicker {
@@ -156,6 +151,40 @@ foam.CLASS({
     ^ .DefaultRowView-row:hover {
       background: #f4f4f9;
       cursor: pointer;
+    }
+
+    ^ .foam-u2-CheckBox {
+      -webkit-appearance: none;
+      border-radius: 2px;
+      border: solid 1px #8e9090;
+      box-sizing: border-box;
+      display: inline-block;
+      fill: rgba(0, 0, 0, 0);
+
+      vertical-align: middle;
+
+      height: 16px;
+      width: 16px;
+
+      opacity: 1;
+
+      transition: background-color 140ms, border-color 140ms;
+    }
+
+
+    ^ .foam-u2-CheckBox-label {
+      margin-left: 12px;
+      vertical-align: middle;
+    }
+
+    ^ .foam-u2-CheckBox:checked {
+      background-color: #604aff;
+      fill: white;
+      border: solid 1px #604aff;
+    }
+
+    ^ .foam-u2-CheckBox:checked:after {
+      content: url(data:image/svg+xml,%3Csvg%20viewBox%3D%220%200%2048%2048%22%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20width%3D%2215%22%20height%3D%2215%22%20version%3D%221.1%22%3E%0A%20%20%20%3Cpath%20fill%3D%22white%22%20stroke-width%3D%223%22%20d%3D%22M18%2032.34L9.66%2024l-2.83%202.83L18%2038l24-24-2.83-2.83z%22/%3E%0A%3C/svg%3E);
     }
   `,
 
