887059fec0d34bb2dcc9138a004288f18ca3e280
Danny Tharma <daniel@nanopay.net>
2019-05-17 16:13:58 -0400

    Rearranged the commented out code and added comments in

--- src/foam/u2/detail/SectionedDetailPropertyView.js
+++ src/foam/u2/detail/SectionedDetailPropertyView.js
@@ -220,8 +220,25 @@ foam.CLASS({
       white-space: pre-wrap;
     }
 
-    /*
+    ^ .foam-u2-view-RadioView .foam-u2-view-RadioView {
+      margin-bottom:16px;
+      display: flex;
+      align-items: center;
+      font-size: 16px;
+    }
 
+
+    ^ .foam-u2-view-RadioView label {
+      margin-left: 12px;
+    }
+
+    /*
+      !IMPORTANT!
+      For the following inputs below, we are planning 
+      encode these changes in the actual foam files
+    */
+
+    /* 
     ^ .foam-u2-CheckBox {
       -webkit-appearance: none;
       border-radius: 2px;
@@ -249,10 +266,7 @@ foam.CLASS({
     ^ .foam-u2-CheckBox:checked:after {
       content: url(data:image/svg+xml,%3Csvg%20viewBox%3D%220%200%2048%2048%22%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20width%3D%2215%22%20height%3D%2215%22%20version%3D%221.1%22%3E%0A%20%20%20%3Cpath%20fill%3D%22white%22%20stroke-width%3D%223%22%20d%3D%22M18%2032.34L9.66%2024l-2.83%202.83L18%2038l24-24-2.83-2.83z%22/%3E%0A%3C/svg%3E);
     }
-    */
 
-
-    /*
     ^ input[type="radio" i] {
       -webkit-appearance: none;
       border-radius: 8px;
@@ -279,20 +293,7 @@ foam.CLASS({
     ^ input[type="radio" i]:checked:after {
       content: url('images/active-radio.svg');
     }
-
     */
-
-    ^ .foam-u2-view-RadioView .foam-u2-view-RadioView {
-      margin-bottom:16px;
-      display: flex;
-      align-items: center;
-      font-size: 16px;
-    }
-
-
-    ^ .foam-u2-view-RadioView label {
-      margin-left: 12px;
-    }
   `,
 
   requires: [
