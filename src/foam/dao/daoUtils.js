422777a7d9b9f03ba7254bcdba66adabc8b43ebb
Troy Wolters <troy@nanopay.net>
2019-06-04 11:11:24 -0400

    Don't update when a listener is attached

--- src/foam/dao/daoUtils.js
+++ src/foam/dao/daoUtils.js
@@ -67,7 +67,6 @@ if let oldValue = oldValue as? foam_dao_AbstractDAO {
         }, context);
 
         listener.onDetach(this.sub('propertyChange', 'delegate', listener.update));
-        listener.update();
 
         return listener;
       },
