3060d6f97ed0ee6b914d5efbcc37528c589c01be
michaelconnor468 <michaelconnor468@gmail.com>
2019-06-05 16:14:54 -0400

    refactored duplicate code into method

--- src/foam/dao/PipelinePMDAO.java
+++ src/foam/dao/PipelinePMDAO.java
@@ -46,39 +46,34 @@ public class PipelinePMDAO
     }
   }
 
+  private void createPM(String name) {
+    PM pm = new PM();
+    pm.setClassType(PipelinePMDAO.getOwnClassInfo());
+    pm.setName(name);
+    X pipeX = getX().put("pipePmStart", pm);
+  }
+
   @Override
   public FObject put_(X x, FObject obj) {
-    PM pm = new PM();
-    pm.setClassType(PMDAO.getOwnClassInfo());
-    pm.setName(putName_);
-    X pipeX = getX().put("pipePmStart", pm);
+    createPM(putName_);
     return super.put_(pipeX, obj);
   }
 
   @Override
   public FObject find_(X x, Object id) {
-    PM pm = new PM();
-    pm.setClassType(PMDAO.getOwnClassInfo());
-    pm.setName(findName_);
-    X pipeX = getX().put("pipePmStart", pm);
+    createPM(findName_);
     return super.find_(pipeX, id);
   }
 
   @Override
   public FObject remove_(X x, FObject obj) {
-    PM pm = new PM();
-    pm.setClassType(PMDAO.getOwnClassInfo());
-    pm.setName(removeName_);
-    X pipeX = getX().put("pipePmStart", pm);
+    createPM(removeName_);
     return super.remove_(pipeX, obj);
   }
 
   @Override
   public void removeAll_(X x, long skip, long limit, Comparator order, Predicate predicate) {
-    PM pm = new PM();
-    pm.setClassType(PMDAO.getOwnClassInfo());
-    pm.setName(removeAllName_);
-    X pipeX = getX().put("pipePmStart", pm);
+    createPM(removeAllName_);
     super.removeAll_(pipeX, skip, limit, order, predicate);
   }
 
