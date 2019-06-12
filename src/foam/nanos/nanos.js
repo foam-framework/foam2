cfcce600eb81cf66b109eef7d2893ad0ec359ee1
Nick <nick@nanopay.net>
2019-06-12 10:33:52 -0400

    Added missing comma

--- src/foam/nanos/nanos.js
+++ src/foam/nanos/nanos.js
@@ -178,7 +178,7 @@ FOAM_FILES([
   { name: 'foam/nanos/dig/Argument'},
   { name: 'foam/nanos/dig/DIG' },
   { name: 'foam/nanos/dig/DigFileUploadView' },
-  { name: 'foam/nanos/dig/DigSnippetView' }
+  { name: 'foam/nanos/dig/DigSnippetView' },
   { name: 'foam/nanos/dig/DUG' },
   { name: 'foam/nanos/dig/LinkView' },
   { name: 'foam/nanos/dig/SUGAR' },
