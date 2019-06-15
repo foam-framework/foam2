2d9245ed6d19b12a5551ffdb03a9bedd4243c2c2
Joel Hughes <joel@nanopay.net>
2019-06-14 20:25:32 -0400

    If relationship called by system user then attempt to use unauthenticated local dao to avoid the unnessary overhead of authentication checks.

--- src/foam/dao/RelationshipDAO.js
+++ src/foam/dao/RelationshipDAO.js
@@ -71,6 +71,22 @@ foam.CLASS({
       },
       javaFactory:`
       try {
+        String daoKey = getTargetDAOKey();
+        foam.nanos.auth.User user = (foam.nanos.auth.User) getX().get("user");
+        if ( user != null &&
+             user.getId() == 1L &&
+             ! daoKey.startsWith("local") ) {
+          // By convention 'local' DAOs are unauthenticated.
+          // If invoked by system user, then avoid the unnessary
+          // overhead of authentication checks
+          String localDAOKey = "local"+daoKey.substring(0, 1).toUpperCase() + daoKey.substring(1);
+          foam.dao.DAO dao = (foam.dao.DAO) getX().get(localDAOKey);
+          if ( dao != null ) {
+            foam.nanos.logger.Logger logger = (foam.nanos.logger.Logger) getX().get("logger");
+            logger.debug("TargetDAOKey swapping", localDAOKey, "for", getTargetDAOKey());
+            return dao.inX(getX());
+          }
+        }
         return ((foam.dao.DAO) getX().get(getTargetDAOKey())).inX(getX());
       } catch ( NullPointerException e ) {
         foam.nanos.logger.Logger logger = (foam.nanos.logger.Logger) getX().get("logger");
