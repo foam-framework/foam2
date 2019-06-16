954b28d93ec1136938f0988e16fa7589fa7134d1
Mike Carcasole <mikecarcasole@gmail.com>
2019-06-16 10:12:22 -0400

    Create the IdentityExpr that simply returns what it's given.

--- src/foam/mlang/mlang.js
+++ src/foam/mlang/mlang.js
@@ -3445,6 +3445,22 @@ foam.CLASS({
 
 foam.CLASS({
   package: 'foam.mlang',
+  name: 'IdentityExpr',
+  extends: 'foam.mlang.AbstractExpr',
+  axioms: [
+    { class: 'foam.pattern.Singleton' }
+  ],
+  methods: [
+    {
+      name: 'f',
+      code: function(o) { return o; },
+      javaCode: 'return obj;'
+    }
+  ]
+});
+
+foam.CLASS({
+  package: 'foam.mlang',
   name: 'IsValid',
   extends: 'foam.mlang.AbstractExpr',
   properties: [
