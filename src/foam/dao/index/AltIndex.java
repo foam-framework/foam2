84bcdb028576e7198e095133a706004d991244cd
Kevin Greer <kgrgreer@gmail.com>
2019-07-02 20:20:06 -0400

    Fix Java AltIndex.

--- src/foam/dao/index/AltIndex.java
+++ src/foam/dao/index/AltIndex.java
@@ -35,7 +35,7 @@ public class AltIndex
     if ( state != null ) {
       Object[] s1 = (Object[]) state;
 
-      for ( int i = 0 ; i < s2.length ; i++ ) {
+      for ( int i = 0 ; i < s1.length ; i++ ) {
         s2[i] = s1[i];
       }
     }
