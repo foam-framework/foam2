e45417e66b14bf5d298742be55d2d8286fc23ded
Henry Han <henry.han@usask.ca>
2019-07-04 10:34:44 -0400

    Add the comment

--- src/foam/nanos/auth/twofactor/OTPKey.js
+++ src/foam/nanos/auth/twofactor/OTPKey.js
@@ -1,7 +1,7 @@
 foam.CLASS({
   package: 'foam.nanos.auth.twofactor',
   name: 'OTPKey',
-  documentation: ``,
+  documentation: `An model that contains key and QR code of two-factor authentication`,
 
   properties: [
     {
