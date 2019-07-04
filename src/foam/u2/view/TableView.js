6d60221a8df41c8808ef2d8efa294bdacde7f035
Troy Wolters <troy@nanopay.net>
2019-07-04 15:45:14 -0400

    Implement TableView using divs and update style to match designs

--- src/foam/u2/view/TableView.js
+++ src/foam/u2/view/TableView.js
@@ -11,69 +11,60 @@ foam.CLASS({
 
   css: `
     ^ {
+      width: 1450px;
       border-spacing: 14px 8px;
+      border-collapse: collapse;
     }
 
-    ^ th {
-      text-align: left;
-      white-space: nowrap;
-      font-family: Roboto, 'Helvetica Neue', helvetica, sans-serif;
-      padding-left: 15px;
-      font-size: 14px;
-      line-height: 1;
-      letter-spacing: 0.4px;
-      color: /*%BLACK%*/ #1e1f21;
+    ^tr {
+      background: white;
+      border-bottom: 1px solid /*%GREY4%*/ #e7eaec;
+      display: flex;
+      height: 48px;
+    }
+
+    ^tbody > ^tr:hover {
       background: /*%GREY5%*/ #f5f7fa;
-      box-sizing: border-box;
-    }
-
-    ^ th:not(:last-child) > img {
-      margin-left: 8px;
-    }
-
-    ^ td {
-      white-space: nowrap;
-      font-family: Roboto, 'Helvetica Neue', helvetica, sans-serif;
-      line-height: 1.33;
-      letter-spacing: 0.2px;
-      padding-left: 15px;
-      font-size: 14px;
-      color: /*%BLACK%*/ #1e1f21;
-    }
-
-    ^row:hover {
-      background: #eee;
       cursor: pointer;
     }
 
-    ^ tbody > tr {
-      height: 48px;
-      background: white;
+    ^thead {
+      border: 1px solid /*%GREY4%*/ #e7eaec;
+      border-radius: 5px;
+      box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.08);
+      overflow: hidden;
+      position: sticky;
+      top: 0;
     }
 
-    ^ tbody > tr > td {
-      border-top: solid 1px #e2e2e3;
-      border-bottom: solid 1px #e2e2e3;
+    ^td,
+    ^th {
+      align-items: center;
+      box-sizing: border-box;
+      color: /*%BLACK%*/ #1e1f21;
+      display: flex;
+      font-family: Roboto, 'Helvetica Neue', helvetica, sans-serif;
+      font-size: 14px;
+      line-height: 1.5;
+      overflow: hidden;
+      padding-left: 16px;
+      text-align: left;
+      white-space: nowrap;
     }
 
-    ^ tbody > tr > td:first-child {
-      border-top-left-radius: 5px;
-      border-left: solid 1px #e2e2e3;
+    ^th {
+      font-weight: 900;
     }
 
-    ^ tbody > tr > td:last-child {
-      border-top-right-radius: 5px;
-      border-right: solid 1px #e2e2e3;
+    ^th:not(:last-child) > img {
+      margin-left: 8px;
     }
 
+    /**
+     * OTHER
+     */
     ^selected {
-      background: #eee;
-    }
-
-    ^vertDots {
-      font-size: 20px;
-      font-weight: bold;
-      padding-right: 21px;
+      background: /*%PRIMARY5%*/ #e5f1fc;
     }
 
     ^noselect {
@@ -87,10 +78,6 @@ foam.CLASS({
 
     ^ .disabled {
       color: #aaa;
-    }
-
-    ^context-menu-cell {
-      margin-right: 12px;
     }
   `,
 });
