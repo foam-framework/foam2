d43fbe1bbeb16ecf654801ba72e0383c0ac3ba8d
noodlemoodle <qiyzhang@icloud.com>
2019-07-09 12:04:09 -0400

    #comment removed duplicated methods

--- src/foam/nanos/auth/CapabilityAuthService.js
+++ src/foam/nanos/auth/CapabilityAuthService.js
@@ -35,47 +35,10 @@ foam.CLASS({
       Check if the given input string is in the userCapabilityJunctions or implied by a capability in userCapabilityJunctions for the current context user
       `,
       javaCode: `
-      if ( x == null || permission == null ) return false;
-      if ( x.get(Session.class) == null ) return false;
       User user = (User) x.get("user");
-      if ( user == null || ! user.getEnabled() ) return false;
+      if ( user != null ) return checkUser(x, user, permission);
 
-      // temporary fix to get around authservice being called on build before some services (userCapabilityJunctionDAO) are available
-      if ( user.getId() == 1 ) return true;  
-
-      // check whether user has permission to check user permissions
-      if ( ! getDelegate().check(x, "service.auth.checkUser") ) throw new AuthorizationException();
-
-      try {
-        DAO userCapabilityJunctionDAO = (DAO) x.get("userCapabilityJunctionDAO");
-
-        if ( userCapabilityJunctionDAO.find(
-          AND(
-            EQ(UserCapabilityJunction.SOURCE_ID, user.getId()),
-            EQ(UserCapabilityJunction.TARGET_ID, permission),
-            EQ(UserCapabilityJunction.STATUS, CapabilityJunctionStatus.GRANTED)
-          )) != null ) return true;
-        
-        List<UserCapabilityJunction> userCapabilityJunctions = (List<UserCapabilityJunction>) ((ArraySink) userCapabilityJunctionDAO
-          .where(AND(
-            EQ(UserCapabilityJunction.SOURCE_ID, user.getId()),
-            EQ(UserCapabilityJunction.STATUS, CapabilityJunctionStatus.GRANTED)
-          )) 
-          .select(new ArraySink()))
-          .getArray();
-        
-        DAO capabilityDAO = (DAO) x.get("capabilityDAO");
-
-        for ( UserCapabilityJunction ucJunction : userCapabilityJunctions ) {
-          Capability capability = (Capability) capabilityDAO.find(ucJunction.getTargetId());
-          if ( capability.implies(x, permission) ) return true;
-        }
-      } catch (Exception e) {
-        Logger logger = (Logger) x.get("logger");
-        logger.error("check", permission, e);
-      } 
-
-      return getDelegate().check(x, permission);
+      return false;
       `
     },     
     {
