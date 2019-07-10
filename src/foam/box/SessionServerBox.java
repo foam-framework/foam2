6b83220a8acc445d47ab3cef24543b847325a646
Joel Hughes <jlhughes@users.noreply.github.com>
2019-07-10 14:51:40 -0400

    Revert "re-enable session/ip mapping"

--- src/foam/box/SessionServerBox.java
+++ src/foam/box/SessionServerBox.java
@@ -75,9 +75,9 @@ public class SessionServerBox
           // If an existing session is reused with a different remote host then
           // logout the session and force a re-login.
 //          logger.warning("Attempt to use session create for ", session.getRemoteHost(), " from ", req.getRemoteHost());
-         session.setContext(getX().put(Session.class, session));
-         session.setRemoteHost(req.getRemoteHost());
-         sessionDAO.put(session);
+//          session.setContext(getX().put(Session.class, session));
+//          session.setRemoteHost(req.getRemoteHost());
+//          sessionDAO.put(session);
         }
       }
 
