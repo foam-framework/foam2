64da5d33efcc2f96932191be6e5f691bd5a9ef5e
Nauna <anna.doulatshahi@gmail.com>
2019-07-15 09:26:53 -0400

    default adjustments to props and of on CSVSink

--- src/foam/dao/CSVSink.js
+++ src/foam/dao/CSVSink.js
@@ -8,7 +8,15 @@ foam.CLASS({
   package: 'foam.dao',
   name: 'CSVSink',
   extends: 'foam.dao.AbstractSink',
-  implements: [ 'foam.core.Serializable' ],
+  implements: [ 
+    'foam.core.Serializable'
+  ],
+  javaImports: [
+    'foam.core.PropertyInfo',
+    'java.util.ArrayList',
+    'java.util.List',
+    'java.util.stream.Collectors'
+  ],
 
   documentation: 'Sink runs the csv outputter, and contains the resulting string in this.csv',
 
@@ -24,15 +32,49 @@ foam.CLASS({
       visibility: 'HIDDEN'
     },
     {
+      class: 'StringArray',
+      name: 'props',
+      factory: function(of) {
+        if ( ! of ) return;
+        if ( of.getAxiomByName('tableColumns') ) return of.getAxiomByName('tableColumns').columns;
+        return of.getAxiomsByClass()
+          .map((p) => p.name )
+          .filter((p) => ! p.networkTransient)
+          .flat();
+      },
+      javaFactory: `
+        if ( ! isPropertySet("of") ) {
+          propsIsSet_ = false;
+          return null;
+        }
+
+        ArrayList<String> propInfoArrayList = ((List<PropertyInfo>)getOf().getAxioms()).stream()
+          .filter((propI) -> ! ((PropertyInfo)propI).getNetworkTransient())
+          .map((propI) -> ((PropertyInfo)propI).getName())
+          .collect(Collectors.toCollection(ArrayList::new));
+        return propInfoArrayList.toArray(new String[propInfoArrayList.size()]);
+      `
+    },
+    {
       name: 'outputter',
       class: 'FObjectProperty',
       of: 'foam.lib.csv.CSVOutputter',
       transient: true,
-      factory: function(of) {
-        return foam.lib.csv.CSVOutputter.create({ of: of });
+      factory: function(of, props) {
+        var csvOutputter = foam.lib.csv.CSVOutputter.create();
+        if ( of ) {
+          csvOutputter.of = of;
+          csvOutputter.props = props;
+        }
+        return csvOutputter;
       },
       javaFactory: `
-        return new foam.lib.csv.CSVOutputter.Builder(getX()).setOf(getOf()).build();
+      foam.lib.csv.CSVOutputter csvOutputter = new foam.lib.csv.CSVOutputter.Builder(getX()).build();
+      if ( isPropertySet("of") ) {
+        csvOutputter.setOf(getOf());
+        csvOutputter.setProps(getProps());
+      }
+      return csvOutputter;
       `
     }
   ],
