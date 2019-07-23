adb200f7a7db7d1c140a36329ebcac7a539a7d89
Chanmann Lim <chanmann@nanopay.net>
2019-07-23 17:19:03 -0400

    Convert StubThreadPool to AgentAgency

--- src/foam/nanos/pool/AgentAgency.java
+++ src/foam/nanos/pool/AgentAgency.java
@@ -1,5 +1,6 @@
 package foam.nanos.pool;
 
+import foam.core.Agency;
 import foam.core.ContextAgent;
 import foam.core.ContextAgentRunnable;
 import foam.core.X;
@@ -7,15 +8,15 @@ import foam.core.X;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class StubThreadPool implements ThreadPool {
+public class AgentAgency implements Agency {
   Queue<Runnable> agents = new LinkedList<>();
 
   @Override
-  public void submit(X x, ContextAgent agent) {
-    agents.add(new ContextAgentRunnable(x, agent, getClass().getSimpleName()));
+  public void submit(X x, ContextAgent agent, String description) {
+    agents.add(new ContextAgentRunnable(x, agent, description));
   }
 
-  public void invokeAll() {
+  public void execute() {
     while ( ! agents.isEmpty() ) {
       agents.poll().run();
     }
