57f073069ed68cde3837709ff1dc45f38f464aad
Kevin Greer <kgrgreer@gmail.com>
2019-08-18 10:44:36 -0400

    Update demo.

--- src/com/google/foam/demos/albums/TempleOfLove.js
+++ src/com/google/foam/demos/albums/TempleOfLove.js
@@ -7,18 +7,32 @@
 foam.CLASS({
   package: 'com.google.foam.demos.albums',
   name: 'TempleOfLove',
-  extends: 'foam.graphics.CView',
+  extends: 'foam.graphics.Box',
 
-  requires: [ 'foam.graphics.Arc' ],
+  requires: [
+    'foam.graphics.Circle',
+    'foam.graphics.Line'
+  ],
 
   properties: [
-    [ 'width',  500 ],
-    [ 'height', 500 ]
+    [ 'width',  600 ],
+    [ 'height', 500 ],
+    [ 'color', 'black' ]
   ],
 
   methods: [
     function initCView() {
       this.SUPER();
+
+      for ( var r = 0 ; r < Math.PI*2 ; r += Math.PI/80 ) {
+        this.add(this.Line.create({
+          startX: 180 + 50 * Math.cos(r),
+          startY: 150 + 50 * Math.sin(r),
+          endX: 180 + 700 * Math.cos(r),
+          endY: 150 + 700 * Math.sin(r),
+          color:'green'
+        }));
+      }
     }
   ]
 });
