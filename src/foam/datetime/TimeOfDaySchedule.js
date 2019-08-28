70f144c5b5d1fdbe9c08f7407aa23eabc9a64280
Eric <eric@nanopay.net>
2019-08-28 11:02:53 -0400

    Add broken TimeOfDaySchedule

---
+++ src/foam/datetime/TimeOfDaySchedule.js
@@ -0,0 +1,60 @@
+/**
+ * @license
+ * Copyright 2017 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.CLASS({
+  package: 'foam.datetime',
+  name: 'TimeOfDaySchedule',
+
+  implements: [
+    'foam.datetime.Schedule'
+  ],
+
+  javaImports: [
+    'java.util.Date',
+    'java.util.Calendar'
+  ],
+
+  properties: [
+    {
+      class: 'FObjectProperty',
+      of: 'foam.datetime.TimeHMS',
+      name: 'time'
+    }
+  ],
+
+  methods: [
+    {
+      name: 'getNextScheduledTime',
+      type: 'DateTime',
+      javaCode:
+`
+Calendar now = Calendar.getInstance();
+Calendar nextTOD = Calendar.getInstance();
+
+// Zero milliseconds
+nextTOD.set(Calendar.MILLISECOND, 0);
+
+// Set time for next run
+nextTOD.set(Calendar.HOUR_OF_DAY, getTime().getHour());
+nextTOD.set(Calendar.MINUTE,      getTime().getMinute());
+nextTOD.set(Calendar.SECOND,      getTime().getSecond());
+
+// Increment the date if time now is after scheduled time of day
+if ( nextTOD.getTimeInMillis() < now.getTimeInMillis() ) {
+  nextTOD.add(Calendar.DATE, 1);
+}
+
+System.out.println("TIME OF DAY");
+System.out.println(nextTOD.getTimeInMillis());
+System.out.println(now.getTimeInMillis());
+// System.exit(1);
+try { Thread.sleep(7000); } catch (Throwable t) {}
+
+return nextTOD.getTime();
+`
+    }
+  ]
+});
