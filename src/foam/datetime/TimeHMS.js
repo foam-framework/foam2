4618eda22d74e743643d27c618cbfb4c5c17a5fe
Eric <eric@nanopay.net>
2019-08-28 11:02:16 -0400

    Add TimeHMS class (forgot to commit earlier)

---
+++ src/foam/datetime/TimeHMS.js
@@ -0,0 +1,16 @@
+/**
+ * @license
+ * Copyright 2017 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.CLASS({
+  package: 'foam.datetime',
+  name: 'TimeHMS',
+
+  properties: [
+    { name: 'hour',   class: 'Int' },
+    { name: 'minute', class: 'Int' },
+    { name: 'second', class: 'Int' }
+  ]
+});
