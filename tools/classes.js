30abe580ff3a0faec1e9daa61b656b3f665543d2
Mike Carcasole <mikecarcasole@gmail.com>
2019-09-07 09:16:39 -0400

    Remove CsvUploadScript

--- tools/classes.js
+++ tools/classes.js
@@ -289,7 +289,6 @@ var classes = [
   'foam.nanos.notification.NotificationTemplateDAO',
   'foam.nanos.notification.notifications.ScriptRunNotification',
   'foam.nanos.script.Script',
-  'foam.nanos.script.CsvUploadScript',
 //  'foam.nanos.script.ScriptCompiler',
 //  'foam.nanos.script.JavaShellScriptCompiler',
   'foam.nanos.script.TestRunnerScript',
