55dfede07364fc129f03de4f34b608da13ed4d65
noodlemoodle <qiyzhang@icloud.com>
2019-09-13 16:20:41 -0400

    fill out interface methods calling rope method

--- src/foam/nanos/rope/ROPEAuthorizer.java
+++ src/foam/nanos/rope/ROPEAuthorizer.java
@@ -11,22 +11,38 @@ import foam.core.X;
 import foam.nanos.auth.AuthService;
 import foam.nanos.auth.AuthorizationException;
 import foam.nanos.auth.Authorizer;
+import foam.nanos.auth.User;
 
 public class ROPEAuthorizer implements Authorizer {
 
-  public ROPEAuthorizer() {
-  }
+  public ROPEAuthorizer() {}
 
   public void authorizeOnCreate(X x, FObject obj) throws AuthorizationException {
+    User user = (User) x.get("user");
+    String targetModel = obj.getClassInfo().getId();
+    if ( ! relationshipTreeSearch(targetModel, "C") ) throw new AuthorizationException("You don't have permission to create this object");
   }
 
   public void authorizeOnRead(X x, FObject obj) throws AuthorizationException {
+    User user = (User) x.get("user");
+    String targetModel = obj.getClassInfo().getId();
+    if ( ! relationshipTreeSearch(targetModel, "R") ) throw new AuthorizationException("You don't have permission to create this object");
   }
 
   public void authorizeOnUpdate(X x, FObject oldObj, FObject obj) throws AuthorizationException {
+    User user = (User) x.get("user");
+    String targetModel = obj.getClassInfo().getId();
+    if ( ! relationshipTreeSearch(targetModel, "U") ) throw new AuthorizationException("You don't have permission to create this object");
   }
 
   public void authorizeOnDelete(X x, FObject obj) throws AuthorizationException {
+    User user = (User) x.get("user");
+    String targetModel = obj.getClassInfo().getId();
+    if ( ! relationshipTreeSearch(targetModel, "D") ) throw new AuthorizationException("You don't have permission to create this object");
+  }
+
+  public boolean relationshipTreeSearch(String targetModel, String operation) {
+    return false;
   }
 
   public boolean checkGlobalRead(X x) {
