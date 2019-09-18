14780d1c538149a7f3ed8c46d03bb8d9e912d01b
Joel Hughes <joel@nanopay.net>
2019-09-17 22:56:25 -0400

    Add missing test class
    CPF-2915 #time 30m

--- src/foam/nanos/nanos.js
+++ src/foam/nanos/nanos.js
@@ -122,6 +122,7 @@ FOAM_FILES([
   { name: "foam/nanos/test/Test" },
   { name: "foam/nanos/test/TestBorder" },
   { name: "foam/nanos/cron/Cron" },
+  { name: "foam/nanos/cron/test/IntervalScheduleTest" },
   { name: "foam/nanos/export/ExportDriverRegistry"},
   { name: "foam/nanos/export/ExportDriver" },
   { name: "foam/nanos/export/JSONDriver"},
