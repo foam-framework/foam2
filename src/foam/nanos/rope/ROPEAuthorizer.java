5c669db203e65b189483f6914d5f46f0c84b99e3
noodlemoodle <qiyzhang@icloud.com>
2019-09-19 13:52:22 -0400

    fix casting part 1

--- src/foam/nanos/rope/ROPEAuthorizer.java
+++ src/foam/nanos/rope/ROPEAuthorizer.java
@@ -13,12 +13,12 @@ import foam.nanos.auth.AuthService;
 import foam.nanos.auth.AuthorizationException;
 import foam.nanos.auth.Authorizer;
 import foam.nanos.auth.User;
-import foam.nanos.rope.ROPECell;
 import foam.nanos.rope.ROPEActions;
 import foam.nanos.rope.ROPE;
 import java.lang.*;
 import foam.dao.ArraySink;
 import java.util.List;
+import static foam.mlang.MLang.*;
 
 public class ROPEAuthorizer implements Authorizer {
 
@@ -44,64 +44,68 @@ public class ROPEAuthorizer implements Authorizer {
 
   public void authorizeOnDelete(X x, FObject obj) throws AuthorizationException {
     String targetModel = obj.getClassInfo().getId();
-    if ( ! ropeSearch(targetModel, ROPEActions.D, obj, x) ) throw new AuthorizationException("You don't have permission to create this object");
+    if ( ! ropeSearch(ROPEActions.D, obj, x) ) throw new AuthorizationException("You don't have permission to create this object");
   }
 
-  public boolean ropeSearch(String targetModel, ROPEActions operation, FObject obj, X x) {
+  public boolean ropeSearch( ROPEActions operation, FObject obj, X x) {
 
     // TODO targetModel is no longer passed in and ROPE.targetModel is now of type CLASS
-    Class targetClass = Class.forName(targetModel);
+    String targetClassName = obj.getClassInfo().getId();
+    Class targetClass = Class.forName(targetClassName);
 
     // if the object is the context user itself return true for now TODO
-    if ( targetModel == "foam.nanos.auth.User" && ((User) obj).getId() == this.user_.getId() ) return true;
+    if ( targetClassName == "foam.nanos.auth.User" && ((User) obj).getId() == this.user_.getId() ) return true;
 
     List<ROPE> ropes = (List<ROPE>) ((ArraySink) this.ropeDAO_
-      .where(EQ(ROPE.TARGET_MODEL, targetModel)) // need a search for source_model as well
+      .where(EQ(ROPE.TARGET_MODEL, obj.getClassInfo())) // need a search for source_model as well
       .select(new ArraySink()))
       .getArray();
 
     for ( ROPE rope : ropes ) {
       DAO junctionDAO = (DAO) x.get(rope.getJunctionDAOKey());
-      Class junctionClass = Class.forName(rope.getJunctionModel());
       DAO sourceDAO = (DAO) x.get(rope.getSourceDAOKey());
-      Class sourceClass = Class.forName(rope.getSourceModel());
-      List<sourceClass> sourceObjs;
+      List<FObject> sourceObjs; 
 
       if ( rope.getCardinality().equals("*:*") ) {
-        List<junctionClass> junctionObjs = ((ArraySink) junctionDAO
+        List<FObject> junctionObjs = ((ArraySink) junctionDAO
           .where(
             OR(
-              AND(!rope.getIsInverse(), EQ(junctionClass.TARGET_ID, (targetClass) obj.getId())),
-              AND(rope.getIsInverse(), EQ(junctionClass.SOURCE_ID, (targetClass) obj.getId()))
+              AND(!rope.getIsInverse(), EQ(rope.getJunctionModel().getAxiomByName("targetId"), (targetClass.cast(obj)).getId())),
+              AND(rope.getIsInverse(), EQ(rope.getJunctionModel().getAxiomByName("sourceId"), (targetClass.cast(obj)).getId()))
             )
           )
           .select(new ArraySink()))
           .getArray();
 
-        for ( junctionClass junctionObj : junctionObjs ) {
-          sourceClass sourceObj = rope.getIsInverse() ? (sourceClass) sourceDAO.find(junctionObj.getTargetId()) : (sourceClass) sourceDAO.find(junctionObj.getSourceId());
+        for ( FObject junctionObj : junctionObjs ) {
+          FObject sourceObj = rope.getIsInverse() ? (FObject) sourceDAO.find(junctionObj.getTargetId()) : (FObject) sourceDAO.find(junctionObj.getSourceId());
           sourceObjs.add(sourceObj);
         }
       } else if ( rope.getCardinality().equals("*:1") ) {
         String methodName = "get" + rope.getInverseName().substring(0, 1).toUpperCase() + rope.getInverseName().substring(1);
-        Method searchFunction = strToFun(methodName, rope.getTargetModel(), foam.core.X.class);
-        sourceObjs = ((ArraySink) searchFunction.invoke((targetClass) obj, x).where(INSTANCE_OF(sourceClass.class)).select(new ArraySink())).getArray();
+        Method searchFunction = strToFun(methodName, rope.getTargetModel().getId(), foam.core.X.class);
+        DAO rDAO = (DAO) searchFunction.invoke(obj, x);
+        sourceObjs = ((ArraySink) rDAO.where(INSTANCE_OF(rope.getSourceModel().getObjClass())).select(new ArraySink())).getArray();
       } else if (rope.getCardinality().equals("1:*") ) {
         String methodName = "find" + rope.getInverseName().substring(0, 1).toUpperCase() + rope.getInverseName().substring(1);
-        Method searchFunction = strToFun(methodName, rope.getTargetModel(), foam.core.X.class);
-        sourceClass sourceObj = searchFunction.invoke((targetClass) obj, x);
+        Method searchFunction = strToFun(methodName, rope.getTargetModel().getId(), foam.core.X.class);
+        FObject sourceObj = searchFunction.invoke((FObject) obj, x);
         sourceObjs.add(sourceObj);
       } else return false;
         
       // if the relationship between the src and target is sufficient to permit the operation
-      if ( rope.getRelationshipImplies().contains(operation) && sourceObjs.size() > 0 ) return true;
+      if ( ((List<ROPEActions>) rope.getRelationshipImplies()).contains(operation) && sourceObjs.size() > 0 ) {
+       // NOT RETURN TRUE!!!!!!!!!!
+       // should recurse on srcObj or return true depending 
+       // 
+      }
 
       // if we need to check in the CRUD Matrix
       List<ROPEActions> actions = rope.getCRUD().get(operation);
       if ( actions != null && actions.size() > 0 ) {
-        for ( sourceClass sourceObj : sourceObjs ) {
+        for ( FObject sourceObj : sourceObjs ) {
           for ( ROPEActions action : actions ) {
-            if ( ropeSearch(rope.getSourceModel(), action, sourceObj, x) ) return true;
+            if ( ropeSearch(action, sourceObj, x) ) return true;
           }
         }
       }
