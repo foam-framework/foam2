2ccffb0ca14ed43f0ca1670a0d774eff9f68e275
nanoMichal <michall.pasternak@nanopay.net>
2019-09-23 13:19:25 -0400

    add file to nanos

--- src/foam/nanos/nanos.js
+++ src/foam/nanos/nanos.js
@@ -199,6 +199,7 @@ FOAM_FILES([
   { name: "foam/nanos/ruler/Operations" },
   { name: "foam/nanos/ruler/Rule" },
   { name: "foam/nanos/ruler/RuleAction" },
+  { name: "foam/nanos/ruler/CompositeRuleAction" },
   { name: "foam/nanos/ruler/RuleHistory" },
   { name: "foam/nanos/ruler/RuleHistoryStatus" },
   { name: "foam/nanos/ruler/RulerDAO" },
