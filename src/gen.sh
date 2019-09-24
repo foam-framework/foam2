17a93f4905a7f5ffc97bd1caddb8b287b574df63
hassene <hassene@192.168.20.139>
2019-09-24 12:03:41 -0400

    Delete old build files

--- src/gen.sh
+++ src/gen.sh
@@ -2,5 +2,9 @@
 #Concatenate files into one services file
 SRCDIR=$(dirname $0)
 cd "$SRCDIR"
+
+# Delete old build files
+rm -r ../build/
+
 node ../tools/genjava.js ../tools/classes.js ../build/
 node ../tools/build.js web
