a18bd249daf48604a186309c809c3945e39e0b86
Minsun <minsun@nanopay.net>
2019-09-26 11:30:41 -0400

    Menu - unification

--- src/foam/nanos/u2/navigation/NavigationView.js
+++ src/foam/nanos/u2/navigation/NavigationView.js
@@ -33,11 +33,11 @@ foam.CLASS({
       this.addClass(this.myClass())
         .start()
           .addClass(this.myClass('top-nav'))
-          .tag({ class: 'foam.nanos.u2.navigation.TopNavigation' })
+          .tag({ class: 'foam.nanos.ui.unification.TopNavigation' })
         .end()
         .start()
           .addClass(this.myClass('side-nav'))
-          .tag({ class: 'foam.nanos.u2.navigation.SideNavigation' })
+          .tag({ class: 'foam.nanos.ui.unification.SideNavigation' })
         .end();
     }
   ]
