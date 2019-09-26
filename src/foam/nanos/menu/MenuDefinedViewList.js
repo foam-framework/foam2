bd51493b66ef3553bb11559e8ffd7cf9ab2105c1
Eric <eric@nanopay.net>
2019-09-26 13:30:27 -0400

    Add MenuDefinedViewList

---
+++ src/foam/nanos/menu/MenuDefinedViewList.js
@@ -0,0 +1,36 @@
+foam.CLASS({
+  package: 'foam.nanos.menu',
+  name: 'MenuDefinedViewList',
+  extends: 'foam.u2.View',
+
+  properties: [
+    {
+      name: 'containerView',
+      class: 'foam.u2.ViewSpec'
+    },
+    {
+      name: 'childViews',
+      class: 'FObjectArray',
+      of: 'foam.u2.View'
+    }
+  ],
+
+  methods: [
+    function initE() {
+      var self = this;
+      var view = this;
+
+      view = view
+        .start(self.containerView);
+      
+      for ( var i = 0; i < self.childViews.length; i++ ) {
+        view = view
+          .start(self.childViews[i]).end();
+      }
+
+      view = view
+        .end();
+    }
+  ]
+});
+
