e33cb6213a74788cb24bb58a25a9675773a18830
Eric <eric@nanopay.net>
2019-09-27 10:06:00 -0400

    Update copyright headers

--- src/foam/dao/FileJournal.js
+++ src/foam/dao/FileJournal.js
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright 2018 The FOAM Authors. All Rights Reserved.
+ * Copyright 2019 The FOAM Authors. All Rights Reserved.
  * http://www.apache.org/licenses/LICENSE-2.0
  */
 
