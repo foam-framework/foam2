c8e8219eb711a084783f6f348af00eef8ec24d9b
Troy Wolters <troy@nanopay.net>
2019-10-04 15:45:40 -0400

    Remove commented out code

--- src/foam/u2/detail/SectionedDetailPropertyView.js
+++ src/foam/u2/detail/SectionedDetailPropertyView.js
@@ -188,73 +188,9 @@ foam.CLASS({
       margin-right: auto;
     }
 
-
     ^ .foam-u2-view-RadioView label {
       margin-left: 12px;
     }
-
-    /*
-      !IMPORTANT!
-      For the following inputs below, we are planning
-      encode these changes in the actual foam files
-    */
-
-    /*
-    ^ .foam-u2-CheckBox {
-      -webkit-appearance: none;
-      border-radius: 2px;
-      border: solid 1px #8e9090;
-      box-sizing: border-box;
-      display: inline-block;
-      fill: rgba(0, 0, 0, 0);
-
-      vertical-align: middle;
-
-      height: 16px;
-      width: 16px;
-
-      opacity: 1;
-
-      transition: background-color 140ms, border-color 140ms;
-    }
-
-    ^ .foam-u2-CheckBox:checked {
-      background-color: #604aff;
-      fill: white;
-      border: solid 1px #604aff;
-    }
-
-    ^ .foam-u2-CheckBox:checked:after {
-      content: url(data:image/svg+xml,%3Csvg%20viewBox%3D%220%200%2048%2048%22%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20width%3D%2215%22%20height%3D%2215%22%20version%3D%221.1%22%3E%0A%20%20%20%3Cpath%20fill%3D%22white%22%20stroke-width%3D%223%22%20d%3D%22M18%2032.34L9.66%2024l-2.83%202.83L18%2038l24-24-2.83-2.83z%22/%3E%0A%3C/svg%3E);
-    }
-
-    ^ input[type="radio" i] {
-      -webkit-appearance: none;
-      border-radius: 8px;
-      background-color: #ffffff;
-      border: solid 1px #8e9090;
-      box-sizing: border-box;
-      display: inline-block;
-      fill: rgba(0, 0, 0, 0);
-
-      vertical-align: middle;
-
-      height: 16px;
-      width: 16px;
-
-      opacity: 1;
-
-      transition: background-color 140ms, border-color 140ms;
-    }
-
-    ^ input[type="radio" i]:checked {
-      background-color: #604aff;
-    }
-
-    ^ input[type="radio" i]:checked:after {
-      content: url('images/active-radio.svg');
-    }
-    */
   `,
 
   requires: [
