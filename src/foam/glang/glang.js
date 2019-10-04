dcfb10819537cc59a6c90fbf4356c6db45d78271
nanoMichal <michall.pasternak@nanopay.net>
2019-10-04 17:18:46 -0400

    add hourly glang:

--- src/foam/glang/glang.js
+++ src/foam/glang/glang.js
@@ -75,6 +75,50 @@ foam.CLASS({
 
 foam.CLASS({
   package: 'foam.glang',
+  name: 'StartOfHour',
+  extends: 'foam.glang.AbstractDateGlang',
+  methods: [
+    {
+      name: 'f',
+      code: function(obj) {
+        var ts = new Date(this.delegate.f(obj));
+        ts.setMinutes(0, 0);
+        return ts;
+      }
+    }
+  ]
+});
+
+foam.CLASS({
+  package: 'foam.glang',
+  name: 'EndOfHour',
+  extends: 'foam.glang.AbstractDateGlang',
+  methods: [
+    {
+      name: 'f',
+      code: function(obj) {
+        var ts = new Date(this.delegate.f(obj));
+        ts.setMinutes(59, 59);
+        ts.setMilliseconds(999);
+        return ts;
+      },
+      javaCode: `
+      // Convert to LocalDate
+      java.util.Date date = (java.util.Date) getDelegate().f(obj);
+      java.time.LocalDate localDate = java.time.Instant.ofEpochMilli(date.getTime()).atZone(java.time.ZoneId.systemDefault()).toLocalDate();
+
+      // Convert to LocalDateTime set to End of Day
+      java.time.LocalDateTime localDateTime = localDate.atTime(java.time.LocalTime.MAX);
+
+      // Convert to Date using LocalDateTime
+      return java.util.Date.from(localDateTime.atZone(java.time.ZoneId.systemDefault()).toInstant());
+      `
+    }
+  ]
+});
+
+foam.CLASS({
+  package: 'foam.glang',
   name: 'StartOfDay',
   extends: 'foam.glang.AbstractDateGlang',
   methods: [
