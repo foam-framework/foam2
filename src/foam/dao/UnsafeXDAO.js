df496221730d4c6701405896db61b96716485b06
noodlemoodle <qiyzhang@icloud.com>
2019-10-18 16:37:15 -0400

    add unsafex to to createservice so that when a dao is created, it is wrapped in the unsafeX decorator

--- src/foam/dao/UnsafeXDAO.js
+++ src/foam/dao/UnsafeXDAO.js
@@ -33,56 +33,56 @@ foam.CLASS({
     {
       name: 'find',
       javaCode: `
-      getLogger().warning("Unsafe access to : " + getNspecName() + ". Please use context-oriented calls instead.");
+      getLogger().warning("Unsafe access : " + getNspecName() + ".find().  Please use context-oriented calls instead.");
       return getDelegate().find(id);
       `
     },
     {
       name: 'put',
       javaCode: `
-      getLogger().warning("Unsafe access to : " + getNspecName() + ". Please use context-oriented calls instead.");
+      getLogger().warning("Unsafe access : " + getNspecName() + ".put(). Please use context-oriented calls instead.");
       return getDelegate().put(obj);
       `
     },
     {
       name: 'select',
       javaCode: `
-      getLogger().warning("Unsafe access to : " + getNspecName() + ". Please use context-oriented calls instead.");
+      getLogger().warning("Unsafe access : " + getNspecName() + ".select(). Please use context-oriented calls instead.");
       return getDelegate().select(sink);
       `
     },
     {
       name: 'remove',
       javaCode: `
-      getLogger().warning("Unsafe access to : " + getNspecName() + ". Please use context-oriented calls instead.");
+      getLogger().warning("Unsafe access : " + getNspecName() + ".remove(). Please use context-oriented calls instead.");
       return getDelegate().remove(obj);
       `
     },
     {
       name: 'removeAll',
       javaCode: `
-      getLogger().warning("Unsafe access to : " + getNspecName() + ". Please use context-oriented calls instead.");
+      getLogger().warning("Unsafe access : " + getNspecName() + ".removeAll(). Please use context-oriented calls instead.");
       getDelegate().removeAll();
       `
     },
     {
       name: 'listen',
       javaCode: `
-      getLogger().warning("Unsafe access to : " + getNspecName() + ". Please use context-oriented calls instead.");
+      getLogger().warning("Unsafe access : " + getNspecName() + ".listen(). Please use context-oriented calls instead.");
       getDelegate().listen(sink, predicate);
       `
     },
     {
       name: 'pipe',
       javaCode: `
-      getLogger().warning("Unsafe access to : " + getNspecName() + ". Please use context-oriented calls instead.");
+      getLogger().warning("Unsafe access : " + getNspecName() + ".pipe(). Please use context-oriented calls instead.");
       getDelegate().pipe(sink);
       `
     },
     {
       name: 'cmd',
       javaCode: `
-      getLogger().warning("Unsafe access to : " + getNspecName() + ". Please use context-oriented calls instead.");
+      getLogger().warning("Unsafe access to : " + getNspecName() + ".cmd(). Please use context-oriented calls instead.");
       return getDelegate().cmd(obj);
       `
     }
