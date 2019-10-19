539655ad21702c23f18985722960d203023cd16e
noodlemoodle <qiyzhang@icloud.com>
2019-10-18 20:12:45 -0400

    override inx

--- src/foam/dao/UnsafeXDAO.js
+++ src/foam/dao/UnsafeXDAO.js
@@ -14,7 +14,6 @@ foam.CLASS({
       name: 'nspecName',
       class: 'String'
     },
-    
     {
       class: 'FObjectProperty',
       of: 'foam.nanos.logger.Logger',
@@ -27,62 +26,76 @@ foam.CLASS({
         return logger;
       `
     },
+    {
+      name: 'xIsSet',
+      class: 'Boolean',
+      javaFactory: ` 
+      return false; 
+      `
+    }
   ],
 
   methods: [
     {
+      name: 'inX',
+      javaCode: `
+      return new ProxyDAO.Builder(x).setDelegate(this.setXIsSet(true)).build();
+      this.setXIsSet(false);
+      `
+    },
+    {
       name: 'find',
       javaCode: `
-      getLogger().warning("Unsafe access : " + getNspecName() + ".find().  Please use context-oriented calls instead.");
+      if ( ! getXIsSet() ) getLogger().warning("Unsafe access : " + getNspecName() + ".find().  Please use context-oriented calls instead.");
       return getDelegate().find(id);
       `
     },
     {
       name: 'put',
       javaCode: `
-      getLogger().warning("Unsafe access : " + getNspecName() + ".put(). Please use context-oriented calls instead.");
+      if ( ! getXIsSet() ) getLogger().warning("Unsafe access : " + getNspecName() + ".put(). Please use context-oriented calls instead.");
       return getDelegate().put(obj);
       `
     },
     {
       name: 'select',
       javaCode: `
-      getLogger().warning("Unsafe access : " + getNspecName() + ".select(). Please use context-oriented calls instead.");
+      if ( ! getXIsSet() ) getLogger().warning("Unsafe access : " + getNspecName() + ".select(). Please use context-oriented calls instead.");
       return getDelegate().select(sink);
       `
     },
     {
       name: 'remove',
       javaCode: `
-      getLogger().warning("Unsafe access : " + getNspecName() + ".remove(). Please use context-oriented calls instead.");
+      if ( ! getXIsSet() ) getLogger().warning("Unsafe access : " + getNspecName() + ".remove(). Please use context-oriented calls instead.");
       return getDelegate().remove(obj);
       `
     },
     {
       name: 'removeAll',
       javaCode: `
-      getLogger().warning("Unsafe access : " + getNspecName() + ".removeAll(). Please use context-oriented calls instead.");
+      if ( ! getXIsSet() ) getLogger().warning("Unsafe access : " + getNspecName() + ".removeAll(). Please use context-oriented calls instead.");
       getDelegate().removeAll();
       `
     },
     {
       name: 'listen',
       javaCode: `
-      getLogger().warning("Unsafe access : " + getNspecName() + ".listen(). Please use context-oriented calls instead.");
+      if ( ! getXIsSet() ) getLogger().warning("Unsafe access : " + getNspecName() + ".listen(). Please use context-oriented calls instead.");
       getDelegate().listen(sink, predicate);
       `
     },
     {
       name: 'pipe',
       javaCode: `
-      getLogger().warning("Unsafe access : " + getNspecName() + ".pipe(). Please use context-oriented calls instead.");
+      if ( ! getXIsSet() ) getLogger().warning("Unsafe access : " + getNspecName() + ".pipe(). Please use context-oriented calls instead.");
       getDelegate().pipe(sink);
       `
     },
     {
       name: 'cmd',
       javaCode: `
-      getLogger().warning("Unsafe access to : " + getNspecName() + ".cmd(). Please use context-oriented calls instead.");
+      if ( ! getXIsSet() ) getLogger().warning("Unsafe access to : " + getNspecName() + ".cmd(). Please use context-oriented calls instead.");
       return getDelegate().cmd(obj);
       `
     }
