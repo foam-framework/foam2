dd7b0d1d7056fd71243cb0812a67e9c4639cb770
noodlemoodle <qiyzhang@icloud.com>
2019-10-28 17:34:54 -0400

    remove changes to testutil

--- src/foam/test/TestUtils.java
+++ src/foam/test/TestUtils.java
@@ -86,7 +86,6 @@ public class TestUtils {
       if ( ! wasCorrectExceptionType ) t.printStackTrace();
     }
     if ( ! returnedMessage.equals(expectedExceptionMessage) ) {
-      System.out.println("Threw = "+threw);
       System.out.println("Error message was not correct.");
       System.out.println("EXPECTED: \"" + expectedExceptionMessage + "\"");
       System.out.println("ACTUAL  : \"" + returnedMessage + "\"");
