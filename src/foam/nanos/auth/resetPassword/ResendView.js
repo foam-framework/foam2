df99c63bfa66f2f2c8e71c7bb51685a2654f6621
Nauna <anna.doulatshahi@gmail.com>
2019-10-28 17:10:06 -0400

    rewriting ResendView to manage all ResetPassword type views

--- src/foam/nanos/auth/resetPassword/ResendView.js
+++ src/foam/nanos/auth/resetPassword/ResendView.js
@@ -6,116 +6,89 @@
 
 foam.CLASS({
   package: 'foam.nanos.auth.resetPassword',
-  name: 'ResendView',
-  extends: 'foam.u2.Controller',
+  name: 'ResendView', // TODO change name to ForgotEmailView
+  extends: 'foam.u2.View',
 
   documentation: 'Forgot Password Resend View',
 
   imports: [
     'resetPasswordToken',
-    'stack'
+    'stack',
+    'theme'
   ],
 
+  css: `
+    ^ .center {
+      margin: 0 auto;
+    }
+  
+    ^ .top-bar {
+      width: 100%;
+      height: 8vh;
+      border-bottom: solid 1px #e2e2e3
+    }
 
-  requires: [
-    'foam.nanos.auth.User',
-    'foam.u2.dialog.NotificationMessage'
-  ],
-
-  css:`
-    ^{
-      width: 490px;
-      margin: auto;
-    }
-    ^ .Message-Container{
-      width: 490px;
-      height: 145px;
-      border-radius: 2px;
-      background-color: #ffffff;
-      padding-top: 5px;
-    }
-    ^ .Forgot-Password{
-      width: 236px;
-      height: 30px;
-      font-family: Roboto;
-      font-size: 30px;
-      font-weight: bold;
-      line-height: 1;
-      letter-spacing: 0.5px;
-      text-align: left;
-      color: /*%BLACK%*/ #1e1f21;
-      margin-top: 20px;
-      margin-bottom: 30px;
-    }
-    ^ p{
-      display: inline-block;
-    }
-    ^ .link{
-      margin-left: 2px;
-      color: #59a5d5;
-      cursor: pointer;
-    }
-    ^ .Instructions-Text{
-      width: 450px;
-      height: 40px;
-      font-family: Roboto;
-      font-size: 14px;
-      font-weight: 300;
-      letter-spacing: 0.2px;
-      text-align: left;
-      color: /*%BLACK%*/ #1e1f21;
-      margin-top: 15px;
-      margin-left: 20px;
-      margin-right: 20px;
-      margin-bottom: 20px;
-    }
-    ^ .Resend-Button{
-      width: 450px;
-      height: 40px;
-      border-radius: 2px;
-      border: solid 1px #59a5d5;
-      margin-left: 20px;
-      margin-right: 20px;
-      background: #ffffff;
-      text-align: center;
-      line-height: 40px;
-      cursor: pointer;
-      color: #59a5d5;
+    ^ .top-bar img {
+      height: 4vh;
+      padding-top: 2vh;
     }
   `,
 
+  requires: [
+    'foam.nanos.auth.User'
+  ],
+
+  sections: [
+    {
+      name: 'emailPasswordSection',
+      title: 'Forgot your password',
+      help: `Enter your account email and we will send you an email with a link to create a new one.`
+    }
+  ],
+
   properties: [
     {
-      class: 'String',
-      name: 'email'
+      class: 'EMail',
+      name: 'email',
+      section: 'emailPasswordSection',
+      required: true
     }
   ],
 
   messages: [
-    { name: 'Instructions', message: `We've sent the instructions to your email. Please check your inbox to continue.` }
+    { name: 'INSTRUC_ONE', message: 'Password reset instructions were sent to' },
+    { name: 'INSTRUC_TWO', message: 'Please check your inbox to continue.' },
+    { name: 'REDIRECTION', message: 'Remember your password?' },
+    { name: 'REDIRECTION_TO', message: 'Sign in.' },
+    { name: 'ACTION_PRESS', message: 'click' }
   ],
 
   methods: [
     function initE() {
       this.SUPER();
-      var self = this;
-
       this
         .addClass(this.myClass())
-        .start()
-          .start().addClass('Forgot-Password').add('Forgot Password').end()
-          .start().addClass('Message-Container')
-            .start().addClass('Instructions-Text').add(this.Instructions).end()
-            .start(this.RESEND_EMAIL).addClass('Resend-Button').end()
+        .startContext({ data: this })
+          .start().addClass('top-bar')
+            .start('img')
+              .attr('src', this.theme.logo)
+            .end()
           .end()
-          .start('p').add('Remember your password?').end()
+          .start().addClass('centerVerticle')
+            .start(this.SectionView, {
+              data: this,
+              sectionName: 'emailPasswordSection'
+            }).end()
+            .start(this.RESEND_EMAIL).end()
+          .end()
+          .start('p').add(this.REDIRECTION).end()
           .start('p').addClass('link')
-            .add('Sign in.')
-            .on('click', function() {self.stack.push({ class: 'foam.nanos.auth.SignInView' })})
-        .end()
-      .end();
-
-      this.add(self.NotificationMessage.create({ message: 'Password reset instructions sent to ' + self.email }));
+            .add(this.REDIRECTION_TO)
+            .on(this.ACTION_PRESS, () => {
+              this.stack.push({ class: 'foam.nanos.auth.SignInView' });
+            })
+          .end()
+        .endContext();
     }
   ],
 
@@ -124,14 +97,12 @@ foam.CLASS({
       name: 'resendEmail',
       label: 'Resend Email',
       code: function(X) {
-        var self = this;
-
         var user = this.User.create({ email: this.email });
-        this.resetPasswordToken.generateToken(null, user).then(function(result) {
-          self.add(self.NotificationMessage.create({ message: 'Password reset instructions sent to ' + self.email }));
+        this.resetPasswordToken.generateToken(null, user).then((_) => {
+          this.notify(`${this.INSTRUC_ONE} ${self.email}. ${this.INSTRUC_TWO}`);
         })
-        .catch(function(err) {
-          self.add(self.NotificationMessage.create({ message: err.message, type: 'error' }));
+        .catch((err) => {
+          this.notify(err.message, 'error');
         });
       }
     }
