577193538e850387b60b693057ee66400f4fa40d
Troy Wolters <troy@nanopay.net>
2019-11-01 16:45:30 -0400

    Add a default/null option to StrategizerChoiceView

--- src/foam/u2/view/StrategizerChoiceView.js
+++ src/foam/u2/view/StrategizerChoiceView.js
@@ -49,7 +49,7 @@ foam.CLASS({
               }
 
               return arr.concat([[sr.strategy.id, sr.strategy.name]]);
-            }, [])
+            }, [[null, 'Select...']])
             .filter(x => x);
         })
       }
