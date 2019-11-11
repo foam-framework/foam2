842e6e3c982ea988f10766c189bf66be80cc35b3
Troy Wolters <troy@nanopay.net>
2019-11-11 09:57:45 -0500

    Formatting
    * Invert condition and return early in simplifyPredicate instead of
    putting all of the logic inside an if block.
    * Clarify a few comments
    * Simplify code in planSelect

--- src/foam/dao/MDAO.java
+++ src/foam/dao/MDAO.java
@@ -22,7 +22,7 @@ import java.util.Set;
  The MDAO class for an ordering, fast lookup, single value,
  index multiplexer, or any other MDAO select() assistance class.
 
- The assitance class TreeiNdex implements the
+ The assitance class TreeIndex implements the
  data nodes that hold the indexed items and plan and execute
  queries. For any particular operational Index, there may be
  many IndexNode instances:
