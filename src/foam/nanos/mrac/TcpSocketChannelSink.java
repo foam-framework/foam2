231a9478428c54617d9e95bb32ba161ffbaa8b5a
xuerongNanopay <xuerong@nanopay.net>
2019-12-04 10:50:02 -0500

    format src/foam/nanos/mrac/TcpSocketChannelSink.java

--- src/foam/nanos/mrac/TcpSocketChannelSink.java
+++ src/foam/nanos/mrac/TcpSocketChannelSink.java
@@ -17,92 +17,85 @@ import foam.dao.AbstractSink;
 import foam.lib.NetworkPropertyPredicate;
 import foam.lib.json.Outputter;
 
-//TODO: model this class
 public class TcpSocketChannelSink extends AbstractSink {
+  
+  SelectionKey key;
+  SocketChannel socketChannel;
+  // This field use to determine if this sink is available to use.
+  // We currently do not support removing a sink from listen_.
+  // Once socket closed, this sink will still be picked while doing put and remove operation.
+  boolean isClose = false;
+  
+  protected int writeWithPrefix(String source, String prefix) {
+    return write(prefix + "(" + source + ")");
+  }
+  
+  protected int write(String source) {
+    byte[] bytes = source.getBytes(Charset.forName("UTF-8"));
+    ByteBuffer byteBuffer = ByteBuffer.allocate(4 + bytes.length);
+    byteBuffer.putInt(bytes.length);
+    byteBuffer.put(bytes);
+    byteBuffer.flip();
+    int len = write(byteBuffer);
+    byteBuffer.clear();
+    return len;
+  }
+  
+  
+  protected synchronized int write(ByteBuffer source) {
+    try {
+      return this.getSocketChannel().write(source);
+    } catch ( IOException e ) {
+      // Close socket when IOException occurs.
+      // Client side should sense this. And re-send sink cmd.
+      System.out.println(e);
+      handleFailure();
+      return -1;
+    }
+  }
+  
+  @Override
+  public void put(Object obj, foam.core.Detachable sub) {
+    if ( isClose ) return;
+    String out = new Outputter(getX()).setPropertyPredicate(new NetworkPropertyPredicate()).stringify((FObject) obj);
+    writeWithPrefix(out, "p");
     
-    SelectionKey key;
-    SocketChannel socketChannel;
-    // This field use to determine if this sink is available to use.
-    // We currently do not support removing a sink from listen_.
-    // Once socket closed, this sink will still be picked while doing put and remove operation.
-    boolean isClose = false;
-    
-    protected int writeWithPrefix(String source, String prefix) {
-        return write(prefix + "(" + source + ")");
+  }
+  
+  @Override
+  public void remove(Object obj, foam.core.Detachable sub) {
+    if ( isClose ) return;
+    String out = new Outputter(getX()).setPropertyPredicate(new NetworkPropertyPredicate()).stringify((FObject) obj);
+    writeWithPrefix(out, "r");
+  }
+  
+  public synchronized void handleFailure() {
+    if ( ! isClose ) return;
+    TCPNioServer.removeSelectionKey(key);
+    TCPNioServer.hardCloseSocketChannel(socketChannel);
+    isClose = true;
+  }
+  
+  public void setKey(SelectionKey key) {
+    this.key = key;
+  }
+  
+  public SelectionKey getSelectionKey() {
+    if ( key == null ) {
+      key = (SelectionKey) getX().get("selectionKey");
     }
-
-    protected int write(String source) {
-        // try {
-            byte[] bytes = source.getBytes(Charset.forName("UTF-8"));
-            ByteBuffer byteBuffer = ByteBuffer.allocate(4 + bytes.length);
-            byteBuffer.putInt(bytes.length);
-            byteBuffer.put(bytes);
-            byteBuffer.flip();
-            int len = write(byteBuffer);
-            byteBuffer.clear();
-            return len;
-        // } catch ( IOException e ) {
-        //     // This IOException is because of byteBuffer, so can be ignore.
-        //     // Log
-        //     return -1;
-        // }
+    //TODO: handle key == null exception.
+    return key;
+  }
+  
+  public void setSocketChannel(SocketChannel socketChannel) {
+    this.socketChannel = socketChannel;
+  }
+  
+  public SocketChannel getSocketChannel() {  
+    if ( socketChannel == null ) {
+      socketChannel = (SocketChannel) getX().get("socketChannel");
     }
-
-
-    protected synchronized int write(ByteBuffer source) {
-        try {
-            return this.getSocketChannel().write(source);
-        } catch ( IOException e ) {
-            // Close socket when IOException occurs.
-            // Client side should sense this. And re-send sink cmd.
-            System.out.println(e);
-            handleFailure();
-            return -1;
-        }
-    }
-    
-    @Override
-    public void put(Object obj, foam.core.Detachable sub) {
-        if ( isClose ) return;
-        String out = new Outputter(getX()).setPropertyPredicate(new NetworkPropertyPredicate()).stringify((FObject) obj);
-        writeWithPrefix(out, "p");
-
-    }
-
-    @Override
-    public void remove(Object obj, foam.core.Detachable sub) {
-        if ( isClose ) return;
-        String out = new Outputter(getX()).setPropertyPredicate(new NetworkPropertyPredicate()).stringify((FObject) obj);
-        writeWithPrefix(out, "r");
-    }
-
-    public synchronized void handleFailure() {
-        if ( ! isClose ) return;
-        TCPNioServer.removeSelectionKey(key);
-        TCPNioServer.hardCloseSocketChannel(socketChannel);
-        isClose = true;
-    }
-    
-    public void setKey(SelectionKey key) {
-        this.key = key;
-    }
-    
-    public SelectionKey getSelectionKey() {
-        if ( key == null ) {
-            key = (SelectionKey) getX().get("selectionKey");
-        }
-        //TODO: handle key == null exception.
-        return key;
-    }
-    
-    public void setSocketChannel(SocketChannel socketChannel) {
-        this.socketChannel = socketChannel;
-    }
-    
-    public SocketChannel getSocketChannel() {  
-        if ( socketChannel == null ) {
-            socketChannel = (SocketChannel) getX().get("socketChannel");
-        }
-        return socketChannel;
-    }
+    return socketChannel;
+  }
 }
