3f5867780e53507ff452f1e30a82f152bb0eecf0
Patrick Zanowski <patrick@nanopay.net>
2019-12-05 14:37:59 -0500

    Map email args to user - email settigs

--- src/foam/nanos/notification/EmailSetting.js
+++ src/foam/nanos/notification/EmailSetting.js
@@ -11,14 +11,47 @@ foam.CLASS({
   extends: 'foam.nanos.notification.NotificationSetting',
 
   javaImports: [
+    'foam.core.PropertyInfo',
+    'foam.dao.DAO',
+    'foam.nanos.auth.User',
     'foam.nanos.logger.Logger',
     'foam.nanos.notification.email.EmailMessage',
     'foam.util.Emails.EmailsUtility',
     'java.util.Arrays',
+    'java.util.Iterator',
     'java.util.List',
+    'java.util.Map',
+    'static foam.mlang.MLang.EQ'
   ],
 
   methods: [
+    {
+      name: 'mapNotificationArguments',
+      type: 'Map',
+      documentation: `Iterate through arguments to replace propertyInfo values with the notified user' values.`,
+      args: [
+        { name: 'x', type: 'Context' },
+        { name: 'arguments', type: 'Map' },
+        { name: 'user', type: 'User' }
+      ],
+      javaCode: `
+        Logger logger = (Logger) x.get("logger");
+
+        Iterator entries = arguments.entrySet().iterator();
+        while (entries.hasNext()) {
+          Map.Entry entry = (Map.Entry) entries.next();
+          if ( entry.getValue() instanceof PropertyInfo ) {
+            if ( ! ((PropertyInfo) entry.getValue()).getClassInfo().equals(User.getOwnClassInfo()) ) {
+              logger.error("Cannot set a none User PropertyInfo as an email argument value");
+              continue;
+            }
+            String newValue = (String) ((PropertyInfo) entry.getValue()).get(user);
+            arguments.replace(entry.getKey(), newValue);
+          }
+        }
+        return arguments;
+      `
+    },
     {
       name: 'sendNotification',
       javaCode: `
@@ -27,6 +60,11 @@ foam.CLASS({
           if ( ! disabledTopics.contains(notification.getNotificationType()) ) {
             EmailMessage message = new EmailMessage();
             message.setTo(new String[]{user.getEmail()});
+
+            if ( notification.getEmailArgs() != null ) {
+              Map<String, Object> emailArgs = mapNotificationArguments(x, notification.getEmailArgs(), user);
+              notification.setEmailArgs(emailArgs);
+            }
 
             try {
               if ( foam.util.SafetyUtil.isEmpty(notification.getEmailName()) ) {
@@ -46,4 +84,3 @@ foam.CLASS({
     }
   ]
 });
-  
