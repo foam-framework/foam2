ba9a35aae7c9aa7107ca770be39d69f86baafe73
Nauna <anna.doulatshahi@gmail.com>
2019-12-16 15:37:55 -0500

    adding a predicated dao -searchFilterDAO - to filter view - FilterSearch

--- src/foam/comics/v2/DAOBrowserView.js
+++ src/foam/comics/v2/DAOBrowserView.js
@@ -152,6 +152,13 @@ foam.CLASS({
       expression: function(config, cannedPredicate, searchPredicate) {
         return config.dao$proxy.where(this.AND(cannedPredicate, searchPredicate));
       }
+    },
+    {
+      class: 'foam.dao.DAOProperty',
+      name: 'searchFilterDAO',
+      expression: function(config, cannedPredicate) {
+        return config.dao$proxy.where(cannedPredicate);
+      }
     }
   ],
   actions: [
@@ -182,7 +189,7 @@ foam.CLASS({
       this.addClass(this.myClass());
       this.SUPER();
       this
-        .add(this.slot(function(data, config$cannedQueries, config$defaultColumns) {
+        .add(this.slot(function(config$cannedQueries, config$defaultColumns, searchFilterDAO) {
           return self.E()
             .start(self.Rows)
               .callIf(config$cannedQueries.length >= 1, function() {
@@ -193,7 +200,7 @@ foam.CLASS({
                       .callIf(config$cannedQueries.length > 1, function() {
                         this
                           .start(self.cannedQueriesView, {
-                            choices: config$cannedQueries.map(o => [o.predicate, o.label]),
+                            choices: config$cannedQueries.map((o) => [o.predicate, o.label]),
                             data$: self.cannedPredicate$
                           })
                             .addClass(self.myClass('canned-queries'))
@@ -204,7 +211,7 @@ foam.CLASS({
               })
               .start(self.Cols).addClass(self.myClass('query-bar'))
                 .startContext({
-                  dao: self.config.dao,
+                  dao: searchFilterDAO,
                   controllerMode: foam.u2.ControllerMode.EDIT
                 })
                   .callIf(self.config.searchMode === self.SearchMode.SIMPLE, function() {
@@ -219,7 +226,7 @@ foam.CLASS({
                     });
                   })
                 .endContext()
-                .startContext({data: self})
+                .startContext({ data: self })
                   .start(self.EXPORT, {
                     buttonStyle: foam.u2.ButtonStyle.SECONDARY
                   })
