d3d3f34cf4fba0ff4eb0b3d89f809438bd1ac915
Troy Wolters <troy@nanopay.net>
2019-12-19 15:08:03 -0500

    Formatting

--- src/foam/nanos/menu/DAOMenu2.js
+++ src/foam/nanos/menu/DAOMenu2.js
@@ -7,15 +7,17 @@
 foam.CLASS({
   package: 'foam.nanos.menu',
   name: 'DAOMenu2',
+  extends: 'foam.nanos.menu.AbstractMenu',
+
   documentation: `
     A DAOMenu which can accept a DAOControllerConfig and uses
     the v2 DAOController
   `,
 
-  extends: 'foam.nanos.menu.AbstractMenu',
   requires: [
     'foam.comics.v2.DAOControllerConfig'
   ],
+
   properties: [
     {
       class: 'FObjectProperty',
@@ -26,6 +28,7 @@ foam.CLASS({
       }
     }
   ],
+
   methods: [
     function createView(X) {
       return {
