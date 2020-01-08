0a050a6d99cdeac8b5c513fa943f88565b39beff
Joel Hughes <joel@nanopay.net>
2020-01-08 14:48:40 -0500

    Clone AppConfig so changes do not blead into other test cases

--- src/foam/nanos/auth/test/ServiceProviderAwareTest.js
+++ src/foam/nanos/auth/test/ServiceProviderAwareTest.js
@@ -45,7 +45,7 @@ foam.CLASS({
         ((DAO) x.get("localGroupDAO")).put(new Group.Builder(x).setId("test2").build());
         ((DAO) x.get("localGroupDAO")).put(new Group.Builder(x).setId("fail").build());
 
-        foam.nanos.app.AppConfig appConfig = (foam.nanos.app.AppConfig) x.get("appConfig");
+        foam.nanos.app.AppConfig appConfig = (foam.nanos.app.AppConfig) ((FObject) x.get("appConfig")).fclone();
         appConfig.setDefaultSpid("spid");
         X y = x.put("appConfig", appConfig);
 
