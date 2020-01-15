95adc7cbcd921c545e041057b17d07104d4f54d9
Chanmann Lim <chanmann@nanopay.net>
2020-01-15 10:49:53 -0500

    Log warning when retry async apply on rule

--- src/foam/nanos/ruler/RuleEngine.java
+++ src/foam/nanos/ruler/RuleEngine.java
@@ -174,6 +174,7 @@ public class RuleEngine extends ContextAwareSupport {
   private void asyncApplyRules(List<Rule> rules, FObject obj, FObject oldObj) {
     if (rules.isEmpty()) return;
     ((Agency) getX().get("threadPool")).submit(userX_, x -> {
+      Logger logger = (Logger) x.get("logger");
       for (Rule rule : rules) {
         if ( stops_.get() ) return;
 
@@ -191,6 +192,7 @@ public class RuleEngine extends ContextAwareSupport {
             rule.asyncApply(x, nu, oldObj, RuleEngine.this, rule);
             saveHistory(rule, nu);
           } catch (Exception ex) {
+            logger.warning("Retry asyncApply rule: " + rule.getId(), ex);
             retryAsyncApply(x, rule, nu, oldObj);
           }
         }
