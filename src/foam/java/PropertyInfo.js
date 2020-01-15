d1effe17c9043d5bc7e3ea996fb6147df36f0d3e
Kevin Greer <kgrgreer@gmail.com>
2020-01-15 14:15:09 -0500

    Fix empty uncommented validateObj methods and redundant name strings.

--- src/foam/java/PropertyInfo.js
+++ src/foam/java/PropertyInfo.js
@@ -272,7 +272,7 @@ foam.CLASS({
             name: 'createStatement',
             visibility: 'public',
             type: 'String',
-            body: 'return "' + this.propName.toLowerCase() + '";'
+            body: 'return getName();'
           },
           {
             name: 'isSet',
@@ -297,7 +297,7 @@ foam.CLASS({
               { name: 'x', type: 'foam.core.X' },
               { name: 'obj', type: 'foam.core.FObject' }
             ],
-            body: this.validateObj
+            body: this.validateObj || '/* Template Method: override in subclass if required. */'
           },
           {
             name: 'toCSV',
