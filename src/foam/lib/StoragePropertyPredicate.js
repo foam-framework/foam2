489bcdbcc040cf26c0ae66953d22050580e596d3
hchoura <choura.hassene@gmail.com>
2020-01-16 09:48:01 -0500

    Remove this unused import

--- src/foam/lib/StoragePropertyPredicate.js
+++ src/foam/lib/StoragePropertyPredicate.js
@@ -2,10 +2,7 @@ foam.CLASS({
   package: 'foam.lib',
   name: 'StoragePropertyPredicate',
   implements: [ 'foam.lib.PropertyPredicate'],
-  javaImports: [
-    'foam.nanos.auth.AuthService'
-  ],
-  
+
   methods: [
     {
       name: 'propertyPredicateCheck',
