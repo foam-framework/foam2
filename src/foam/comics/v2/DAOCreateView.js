d01661c6bb2fd6cce6940e028c1e7a6a60932a1a
Danny Tharma <daniel@nanopay.net>
2020-01-16 04:13:35 -0500

    Added back the pending thing

--- src/foam/comics/v2/DAOCreateView.js
+++ src/foam/comics/v2/DAOCreateView.js
@@ -87,6 +87,11 @@ foam.CLASS({
       },
       code: function() {
         var cData = this.data;
+
+        if ( foam.nanos.auth.LifecycleAware.isInstance(cData) ) {
+          cData = cData.clone();
+          cData.lifecycleState = foam.nanos.auth.LifecycleState.PENDING;
+        }
         
         this.config.dao.put(cData).then((o) => {
           this.data = o;
