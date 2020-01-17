e144da9fc78d16d2c74526f477142d60cf7a5ae1
xuerongNanopay <xuerong@nanopay.net>
2020-01-16 22:17:25 -0500

    add readable message

--- src/foam/nanos/mrac/TCPNioServer.java
+++ src/foam/nanos/mrac/TCPNioServer.java
@@ -73,10 +73,10 @@ public class TCPNioServer extends AbstractFObject implements NanoService {
                                   .where(EQ(ClusterNode.HOST_NAME, hostname))
                                   .select(new ArraySink());
     List list = sink.getArray();
-    if ( list.size() != 1 ) throw new RuntimeException("error on clusterNode journal");
+    if ( list.size() != 1 ) throw new RuntimeException("hostname: " +  hostname + " duplicate on journal");
     ClusterNode myself = (ClusterNode) list.get(0);
     //ClusterNode myself = (ClusterNode) clusterDAO.find(clusterId);
-    if ( myself == null ) throw new RuntimeException("ClusterNode no found: " + clusterId);
+    if ( myself == null ) throw new RuntimeException("can not find clusterNode with hostname: " + hostname);
 
 
     //TODO: do not hard coding following parameter.
