68354debb74746418f22822767b68735652dcb92
Kevin Greer <kgrgreer@gmail.com>
2020-02-19 09:57:47 -0500

    Placeholder for JSON String escaping.

---
+++ src/foam/lib/json/Util.java
@@ -0,0 +1,18 @@
+/**
+ * @license
+ * Copyright 2020 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package foam.lib.json;
+
+/** JSON Utility methods. **/
+public class Util {
+
+  /** Append src string to dst StringBuilder as an escaped JSON string. **/
+  public static void escape(String src, StringBuilder dst) {
+    // TODO: insert real code here
+    dst.append(src);
+  }
+
+}
