ac7e2e5c4088f5f400b8c4ad7764fc85e1e669bd
Kevin Greer <kgrgreer@gmail.com>
2020-02-19 14:28:37 -0500

    Cleanup.

--- src/foam/lib/json/Util.java
+++ src/foam/lib/json/Util.java
@@ -12,41 +12,39 @@ public class Util {
   /** Append src string to dst StringBuilder as an escaped JSON string. **/
   public static void escape(String src, StringBuilder dst) {
     char c;
-      for ( int i = 0; i < src.length() ; i++ ) {
-        c = src.charAt(i);
+    for ( int i = 0 ; i < src.length() ; i++ ) {
+      c = src.charAt(i);
 
-        switch ( c ) {
-          case '\t':
-            dst.append("\\t");
-            break;
-          case '\r':
-            dst.append("\\r");
-            break;
-          case '\n':
-            dst.append("\\n");
-            break;
-          case '\\':
-            dst.append("\\\\");
-            break;
-          case '\"':
-            dst.append("\\\"");
-            break;
-          default:
-            if ( c >= ' ' )
-              dst.append(c);
-            else {
-              char right = (char) (c & 0x0F);
-              char left = (char) ((c & 0xF0) >> 4);
-              right += '0';
-              if ( right > '9' ) right += 'A' - '9' - 1;
-              left += '0';
-              if ( left > '9' ) left += 'A' - '9' - 1;
-              dst.append("\\u00");
-              dst.append(left);
-              dst.append(right);
-            }
-        }
+      switch ( c ) {
+        case '\t':
+          dst.append("\\t");
+          break;
+        case '\r':
+          dst.append("\\r");
+          break;
+        case '\n':
+          dst.append("\\n");
+          break;
+        case '\\':
+          dst.append("\\\\");
+          break;
+        case '\"':
+          dst.append("\\\"");
+          break;
+        default:
+          if ( c >= ' ' ) {
+            dst.append(c);
+          } else {
+            dst.append("\\u00");
+            char right = (char) (c & 0x0F + '0');
+            char left  = (char) ((c & 0xF0) >> 4 + '0');
+            if ( right > '9' ) right += 'A' - '9' - 1;
+            if ( left > '9'  ) left  += 'A' - '9' - 1;
+            dst.append(left);
+            dst.append(right);
+          }
       }
+    }
   }
 
 }
