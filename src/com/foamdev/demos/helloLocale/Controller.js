3283b9bf038dff6382a15d3305d463efa130975b
hchoura <choura.hassene@gmail.com>
2020-02-24 11:32:21 -0500

    formatting

--- src/com/foamdev/demos/helloLocale/Controller.js
+++ src/com/foamdev/demos/helloLocale/Controller.js
@@ -44,7 +44,7 @@ foam.CLASS({
   methods: [
     function initE() {
       this.start('div').add(this.yourName$.label).end().start('div').add(this.YOUR_NAME).end();
-        this.start('h1').add(this.greeting$.label).add(this.yourName$).add('!').end();
+      this.start('h1').add(this.greeting$.label).add(this.yourName$).add('!').end();
     }
   ]
 });
