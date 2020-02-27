6564517cc0e930bdef2e55804be1fd6fac0d51cc
Patrick Zanowski <patrick@nanopay.net>
2020-02-27 14:32:53 -0500

    Detailed Action Boolean View

---
+++ src/foam/u2/DetailedActionBooleanView.js
@@ -0,0 +1,100 @@
+foam.CLASS({
+  package: 'foam.u2',
+  name: 'DetailedActionBooleanView',
+  extends: 'foam.u2.View',
+
+  documentation: `
+    Displays two labels and description that are visible based on the state of the boolean value.
+    Along with the dynamics labels, an action is provided to put the provided data object
+    into the targetDAO provided.
+  `,
+
+  imports: [
+    'notify'
+  ],
+
+  css: `
+    ^ {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+    ^ .foam-u2-DetailedActionBooleanView-text{
+      width: 50%;
+      white-space: pre-wrap;
+    }
+  `,
+
+  properties: [
+    'trueLabel',
+    'falseLabel',
+    'trueActionLabel',
+    'falseActionLabel',
+    'targetDAOKey',
+    'property',
+    {
+      class: 'String',
+      name: 'dynamicLabel',
+      expression: function(data) {
+        return data ? this.trueLabel : this.falseLabel;
+      }
+    },
+    {
+      class: 'String',
+      name: 'dynamicActionLabel',
+      expression: function(data) {
+        return data ? this.trueActionLabel : this.falseActionLabel;
+      }
+    }
+  ],
+
+  messages: [
+    { name: 'BOOLEAN_ERROR', message: 'There was an issue updating' },
+    { name: 'BOOLEAN_SUCCESS', message: 'was successfully updated.' }
+  ],
+
+  methods: [
+    function initE() {
+      this.start().addClass(this.myClass())
+        .start().addClass(this.myClass('text'))
+          .add(this.dynamicLabel$)
+        .end()
+        .startContext({ data: this })
+          .start().addClass(this.myClass('btn-box'))
+            .tag(this.UPDATE_OBJECT, {
+              label$: this.dynamicActionLabel$,
+              buttonStyle: 'SECONDARY'
+            })
+          .end()
+        .endContext()
+      .end();
+    },
+    function fromProperty(property) {
+      this.SUPER(property);
+      this.property = property;
+    }
+  ],
+
+  actions: [
+    {
+      name: 'updateObject',
+      isEnabled: function(mode) {
+        if ( mode ) {
+          return mode.name === 'RW';
+        }
+      },
+      code: async function(X) {
+        var dao = X[this.targetDAOKey];
+        this.data = ! this.data;
+        try {
+          dao.put(this.__context__.data);
+        } catch (e) {
+          console.error(`${ this.BOOLEAN_ERROR } ${ this.property.label } of ${ this.__context__.cls_.name }`);
+          this.notify(`${ this.BOOLEAN_ERROR } ${ this.property.label }`);
+          return;
+        }
+        this.notify(`${ this.property.label } ${ this.BOOLEAN_SUCCESS }`);
+      }
+    }
+  ]
+});
