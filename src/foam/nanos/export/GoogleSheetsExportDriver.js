1305fd0262233c4992b1ae85674687cfb683c391
Olha Bahatiuk <olha@nanopay.net>
2020-03-09 17:55:43 -0400

    Figuring out exportFObject

--- src/foam/nanos/export/GoogleSheetsExportDriver.js
+++ src/foam/nanos/export/GoogleSheetsExportDriver.js
@@ -17,25 +17,32 @@ foam.CLASS({
   ],
 
   methods: [
-    function exportFObject(X, obj) {
-      return '';
-    },
-    {
-      name: 'exportDAO',
-      code: async function(X, dao) {
-        self = this;
+    async function exportFObject(X, obj) {
+        var self = this;
         
-        var sink = await dao.select();
         var url  = '';
         var stringArray = [];
-        var columnNames = X.filteredTableColumns ? X.filteredTableColumns : self.outputter.getAllPropertyNames(dao.of);
+        var columnNames = X.filteredTableColumns ? X.filteredTableColumns : self.outputter.getAllPropertyNames(obj.cls_);
         stringArray.push(columnNames);
-        var values = self.outputter.outputArray(sink.array, columnNames);
+        var values = self.outputter.outputArray([ obj ], columnNames);
         stringArray = stringArray.concat(values);
 
         var url = await X.googleSheetsDataExport.createSheet(stringArray);
         return url;
-      }
+    },
+    async function exportDAO(X, dao) {
+      var self = this;
+      
+      var sink = await dao.select();
+      var url  = '';
+      var stringArray = [];
+      var columnNames = X.filteredTableColumns ? X.filteredTableColumns : self.outputter.getAllPropertyNames(dao.of);
+      stringArray.push(columnNames);
+      var values = self.outputter.outputArray(sink.array, columnNames);
+      stringArray = stringArray.concat(values);
+
+      var url = await X.googleSheetsDataExport.createSheet(stringArray);
+      return url;
     }
   ]
 });
