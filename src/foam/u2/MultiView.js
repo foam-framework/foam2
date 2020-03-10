2fd91eee8bca15a53961e62de7561a46a2ca7cdf
Kevin Greer <kgrgreer@gmail.com>
2020-03-10 10:38:24 -0400

    Add horizontal/vertical mode to MultiView. Adapt views arguments like ViewSpecs (which they are).

--- src/foam/u2/MultiView.js
+++ src/foam/u2/MultiView.js
@@ -18,13 +18,23 @@ foam.CLASS({
   exports: [ 'parentData as data' ],
 
   css: `
-    ^container { margin-right: 8px; margin-bottom: 4px; }
+    ^container {
+      margin: 2px 8px 2px 0;
+    }
   `,
 
   properties: [
     {
+      class: 'Boolean',
+      name: 'horizontal',
+      value: true
+    },
+    {
       class: 'Array',
-      name: 'views'
+      name: 'views',
+      adapt: function(_, a) {
+        return foam.Array.isInstance(a) ? a.map((o) => foam.String.isInstance(o) ? { class: o } : o) : a;
+      }
     },
     'prop'
   ],
@@ -40,8 +50,9 @@ foam.CLASS({
       this.add(this.slot(function(views) {
         return self.E().forEach(views, function(v) {
           return this.
-            start('span').
+            start().
               addClass(self.myClass('container')).
+              callIf(self.horizontal, function() { this.style({float: 'left'}); }).
               start(v, { data$: self.data$ }).
                 call(function() {
                   self.prop && this.fromProperty && this.fromProperty(self.prop);
