70ee33563e5b6e8b28fe1f70ed5ee038b9dc4eaa
Olha Bahatiuk <olha@nanopay.net>
2020-03-13 16:13:39 -0400

    Year fixed

--- src/foam/nanos/export/GoogleSheetsExportService.java
+++ src/foam/nanos/export/GoogleSheetsExportService.java
@@ -1,9 +1,9 @@
 /**
  * @license
- * Copyright 2017 The FOAM Authors. All Rights Reserved.
+ * Copyright 2020 The FOAM Authors. All Rights Reserved.
  * http://www.apache.org/licenses/LICENSE-2.0
  */
- 
+
 package foam.nanos.export;
 
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
