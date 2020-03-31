82a6182d047f2958f24969c4abfd471aef8ce5d1
Olha Bahatiuk <olha@nanopay.net>
2020-03-31 19:03:36 -0400

    Benchmarks

---
+++ src/foam/nanos/bench/benchmarks/MDAOPutFind.java
@@ -0,0 +1,35 @@
+package foam.nanos.bench.benchmarks;
+
+import foam.core.X;
+import foam.dao.DAO;
+import foam.dao.MDAO;
+import foam.nanos.auth.Country;
+import foam.nanos.bench.Benchmark;
+
+import java.util.Map;
+
+public class MDAOPutFind implements Benchmark {
+
+  protected DAO dao_;
+  protected Country a1_;
+
+  @Override
+  public void setup(X x) {
+    dao_ = new MDAO(Country.getOwnClassInfo());
+    ((MDAO) dao_).addIndex(new foam.core.PropertyInfo[] {Country.NAME});
+    ((MDAO) dao_).addIndex(new foam.core.PropertyInfo[] {Country.CODE});
+
+    a1_= new Country.Builder(x).setId("AA").setCode("AA").setName("AA").build();
+  }
+
+  @Override
+  public void teardown(X x, Map stats) {
+  }
+
+  @Override
+  public void execute(X x) {
+
+    dao_.put_(x, a1_);
+    Country c = (Country)dao_.find("AA");
+  }
+}
---
+++ src/foam/nanos/bench/benchmarks/MDAOUpdateSelect.java
@@ -0,0 +1,44 @@
+package foam.nanos.bench.benchmarks;
+
+import foam.core.X;
+import foam.dao.ArraySink;
+import foam.dao.DAO;
+import foam.dao.MDAO;
+import foam.nanos.auth.Country;
+import foam.nanos.bench.Benchmark;
+
+import java.util.Map;
+
+public class MDAOUpdateSelect implements Benchmark {
+
+  protected DAO dao_;
+  protected Country a1_;
+
+  @Override
+  public void setup(X x) {
+    dao_ = new MDAO(Country.getOwnClassInfo());
+    ((MDAO) dao_).addIndex(new foam.core.PropertyInfo[] {Country.NAME});
+    ((MDAO) dao_).addIndex(new foam.core.PropertyInfo[] {Country.CODE});
+
+    a1_= new Country.Builder(x).setId("AA").setCode("AA").setName("AA").build();
+
+    dao_.put_(x, a1_);
+  }
+
+  @Override
+  public void teardown(X x, Map stats) {
+  }
+
+  @Override
+  public void execute(X x) {
+    a1_.setCode("BB");
+    dao_.put_(x, a1_);
+    
+    ArraySink s = new ArraySink();
+    dao_.select(s);
+  }
+}
+
+//New
+
+//Old
