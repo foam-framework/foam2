f89f65a8aa36fa44f3ae3cd0e517887afd37dd02
Olha Bahatiuk <olha@nanopay.net>
2020-04-14 17:40:09 -0400

    Fixing errors

--- src/foam/nanos/extraconfig/Config.js
+++ src/foam/nanos/extraconfig/Config.js
@@ -22,22 +22,18 @@ foam.CLASS({
       name: 'configValue',
       factory: function() {     
           if ( this.exportMetadata.typeOfConfig === 'Boolean' )
-            this.configValue = false;
-          else if ( this.exportMetadata.typeOfConfig === 'Integer' )
-            this.configValue = 0;
-          else if ( this.exportMetadata.typeOfConfig === 'Float' )
-            this.configValue = 0;
+            return false;
+          else if ( this.exportMetadata.typeOfConfig === 'Integer' || this.exportMetadata.typeOfConfig === 'Float' )
+            return 0;
           else if ( this.exportMetadata.doesProvideOptions && this.exportMetadata.optionsChoice === 'Array' )
-            this.configValue = this.exportMetadata.options[0];
+            return this.exportMetadata.options[0];
           else if ( this.exportMetadata.doesProvideOptions && this.exportMetadata.optionsChoice === 'DAO' ) {
-            var dao = this.__context__[v.daoSource];
+            var dao = this.__context__[this.exportMetadata.daoSource];
             dao.select().then((o) => {
-              this.configValue = o.array[0].id;
+              return this.configValue = o.array[0].id;
             });
           }
-          else
-            return '';       
-        return;
+          return '';
       }
     }
   ]
