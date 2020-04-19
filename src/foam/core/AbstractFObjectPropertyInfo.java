7cc1e2930e28793174026480307653270431095c
Joel Hughes <joel@nanopay.net>
2020-04-19 16:51:05 -0400

    hash and updateDigest tests for Storage and Cluster Transient were inverted - cut and paste error

--- src/foam/core/AbstractFObjectPropertyInfo.java
+++ src/foam/core/AbstractFObjectPropertyInfo.java
@@ -67,9 +67,13 @@ public abstract class AbstractFObjectPropertyInfo
       PropertyInfo prop = (PropertyInfo) i.next();
       if ( ! prop.includeInDigest() ) continue;
       if ( ! prop.isSet(val) ) continue;
-      if ( ! prop.getStorageTransient() ) continue;
-      if ( ! prop.getClusterTransient() ) continue;
+      if ( prop.getStorageTransient() ) continue;
+      if ( prop.getClusterTransient() ) continue;
       if ( prop.isDefaultValue(val) ) continue;
+      if ( ! ( prop instanceof FObject ) ) {
+        System.out.println("updateDigest prop not FObject: "+prop.getClass().getName());
+        continue;
+      }
       md.update(prop.getNameAsByteArray());
       prop.updateDigest(val, md);
     }
@@ -87,8 +91,8 @@ public abstract class AbstractFObjectPropertyInfo
       PropertyInfo prop = (PropertyInfo) i.next();
       if ( ! prop.includeInSignature() ) continue;
       if ( ! prop.isSet(val) ) continue;
-      if ( ! prop.getStorageTransient() ) continue;
-      if ( ! prop.getClusterTransient() ) continue;
+      if ( prop.getStorageTransient() ) continue;
+      if ( prop.getClusterTransient() ) continue;
       if ( prop.isDefaultValue(val) ) continue;
       sig.update(prop.getNameAsByteArray());
       prop.updateSignature(val, sig);
