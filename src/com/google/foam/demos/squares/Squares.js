5438133d4a2cfd28fb0c9a3045381d0d3c2d35b4
Kevin Greer <kgrgreer@gmail.com>
2020-04-21 12:06:34 -0400

    Fix Squares demo.

--- src/com/google/foam/demos/squares/Squares.js
+++ src/com/google/foam/demos/squares/Squares.js
@@ -113,11 +113,3 @@ foam.CLASS({
     }
   ]
 });
-
-/*
-window.onload = () => {
-  var ss = com.google.foam.demos.squares.Squares.create();
-  ss.write();
-  foam.u2.DetailView.create({data: ss, showActions: true}).write();
-};
-*/
