d71732cfff23d5ddde939a65a99629842ba51583
Olha Bahatiuk <olha@nanopay.net>
2020-05-20 11:29:52 -0400

    fixing other methods

--- src/foam/nanos/column_config/ColumnConfigPropertyValue.js
+++ src/foam/nanos/column_config/ColumnConfigPropertyValue.js
@@ -72,7 +72,6 @@ foam.CLASS({
                 cls = m.getReturnType();
                 sb.setLength(4);//sb.delete(4, s.length);
               }
-            
               ci = (ClassInfo) cls.getMethod("getOwnClassInfo").invoke(null);
             } catch (Exception e) {
               Logger logger = (Logger) getX().get("logger");
@@ -111,18 +110,22 @@ foam.CLASS({
         var obj1 = obj;
         if ( foam.String.isInstance(propInfo) ) {
           var props = propInfo.split('.');
-          for ( var i = 0 ; i < props.length ; i++ ) {
+          for ( var i = 0; i < props.length; i++ ) {
             property = foam.String.isInstance(props[i])
             ? cls.getAxiomByName(props[i])
             :  foam.Array.isInstance(props[i]) ? 
             cls.getAxiomByName(props[i]) : props[i];
-            if ( !property ) {
+            if ( ! property )
               break;
-            }
             cls = property.of;
 
-            if ( i !== props.length - 1 && obj1 )
-              obj1 = property.f(obj1);
+            if ( i !== props.length - 1 && obj1 ) {
+              if ( foam.core.Reference.isInstance(property) ) {
+                obj1 = await obj1[property.name + '$find'].then(val => obj1 = val);
+              } else {
+                obj1 = property.f(obj1);
+              }
+            }
           }
         }
         return obj1;
@@ -138,9 +141,20 @@ foam.CLASS({
           p = (PropertyInfo) ci.getAxiomByName(props[i]);
 
           if ( i != props.length - 1 ) {
-            obj1 = (FObject) p.f(obj1);
-            Class cls = p.getValueClass();
             try {
+              Class cls;
+              if ( p instanceof foam.core.AbstractFObjectPropertyInfo ) {
+                obj1 = (FObject) p.f(obj1);
+                cls = p.getValueClass();
+              } else {
+                char[] arr = p.getName().toCharArray();
+                arr[0] = Character.toUpperCase(arr[0]);
+                sb.append(arr);
+                //String s = sb.toString();
+                obj1 = (FObject)obj1.getClass().getMethod(sb.toString(), foam.core.X.class).invoke(obj1, x);
+                sb.setLength(4);//delete(4, s.length);
+                cls = obj1.getClass();
+              }
               ci = (ClassInfo) cls.getMethod("getOwnClassInfo").invoke(null);
             } catch (Exception e) {
               Logger logger = (Logger) getX().get("logger");
@@ -260,10 +274,10 @@ foam.CLASS({
           type: 'FObject'
         }
       ],
-      code: function(of, propertyNameArrays, obj) {
+      code: async function(of, propertyNameArrays, obj) {
         var arr = [];
         for ( var i = 0 ; i < propertyNameArrays.length ; i++) {
-          arr.push(this.returnPropertyAndObject(of, propertyNameArrays, obj));
+          arr.push(await this.returnPropertyAndObject(of, propertyNameArrays, obj));
         }
         return arr;
       },
