ca6788c9582b074ed3f3194974481056022d2fb9
Eric Dubé <eric.alex.dube@gmail.com>
2020-05-21 00:05:35 -0400

    [NP-641] prepare for step wizard (#75)
    * Cleanup ScrollSectionWizardView messages
    
    * Prepare to add StepWizardletView
    
    * Add license headers
    
    Authored-by: Eric <eric@nanopay.net>

--- src/foam/u2/crunch/CrunchController.js
+++ src/foam/u2/crunch/CrunchController.js
@@ -22,7 +22,7 @@ foam.CLASS({
     'foam.nanos.crunch.CapabilityCapabilityJunction',
     'foam.nanos.crunch.CapabilityJunctionStatus',
     'foam.nanos.crunch.UserCapabilityJunction',
-    'foam.nanos.crunch.ui.CapabilityWizardSection'
+    'foam.nanos.crunch.ui.CapabilityWizardlet'
   ],
 
   methods: [
@@ -64,7 +64,7 @@ foam.CLASS({
       return this.getCapabilities(capabilityId).then(capabilities => {
         // Map capabilities to CapabilityWizardSection objects
         return Promise.all(capabilities.map(
-          cap => this.CapabilityWizardSection.create({
+          cap => this.CapabilityWizardlet.create({
             capability: cap
           }).updateUCJ()
         ));
@@ -84,8 +84,8 @@ foam.CLASS({
             )
           );
           self.stack.push({
-            class: "foam.nanos.crunch.ui.ScrollSectionWizardView",
-            sectionsList: sections
+            class: "foam.u2.wizard.ScrollWizardletView",
+            wizardlets: sections
           });
         });
       });
