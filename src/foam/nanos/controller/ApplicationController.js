bce24896189d9e19538d1bd11c7a3941fdb7a063
Kristina Smirnova <penzital888@gmail.com>
2020-05-27 14:44:17 -0400

    Add setter to context user (#3511)

--- src/foam/nanos/controller/ApplicationController.js
+++ src/foam/nanos/controller/ApplicationController.js
@@ -287,13 +287,17 @@ foam.CLASS({
         // add user and agent for backward compatibility
         Object.defineProperty(self, 'user', {
           get: function() {
-            console.info("Deprecated use of user. Use Subject to retrieve user")
+            console.info("Deprecated use of user. Use Subject to retrieve user");
             return this.subject.user;
+          },
+          set: function(newValue) {
+            console.warn("Deprecated use of user setter");
+            this.subject.user = newValue;
           }
         });
         Object.defineProperty(self, 'agent', {
           get: function() {
-            console.warn("Deprecated use of agent")
+            console.warn("Deprecated use of agent");
             return this.subject.realUser;
           }
         });
