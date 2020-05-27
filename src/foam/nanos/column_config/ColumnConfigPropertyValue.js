a879af46b9e9bcc1f3ac6ebe873862613aca3ef2
Olha Bahatiuk <olha@nanopay.net>
2020-05-27 18:47:20 -0400

    clean up

--- src/foam/nanos/column_config/ColumnConfigPropertyValue.js
+++ src/foam/nanos/column_config/ColumnConfigPropertyValue.js
@@ -100,91 +100,6 @@ foam.CLASS({
       `
     },
     {
-      name: 'returnValue',
-      documentation: `returns value that is one before last as there are number of uses cases when property's method is called on object
-       e.g for propName 'address.countryId.name' we return country object which can be used by name property as parameter for such function calls as f or toCSV`,
-      type: 'FObject',
-      args: [
-        {
-          name: 'x',
-          type: 'Context'
-        },
-        {
-          name: 'of',
-          type: 'ClassInfo'
-        },
-        {
-          name: 'propName',
-          class: 'String',
-        },
-        {
-          name: 'obj',
-          type: 'FObject'
-        }
-      ],
-      code: async function(of, propName, obj) {
-        var cls = of;
-        var property;
-        var obj1 = obj;
-        if ( foam.String.isInstance(propName) ) {
-          var props = propName.split('.');
-          for ( var i = 0; i < props.length; i++ ) {
-            property = foam.String.isInstance(props[i])
-            ? cls.getAxiomByName(props[i])
-            :  foam.Array.isInstance(props[i]) ? 
-            cls.getAxiomByName(props[i]) : props[i];
-            if ( ! property )
-              break;
-            cls = property.of;
-
-            if ( i !== props.length - 1 && obj1 ) {
-              if ( foam.core.Reference.isInstance(property) ) {
-                obj1 = await obj1[property.name + '$find'];
-              } else {
-                obj1 = property.f(obj1);
-              }
-            }
-          }
-        }
-        return obj1;
-      },
-      javaCode: `
-        ClassInfo ci = of;
-        FObject obj1 = obj;
-        PropertyInfo p = null;
-        String[] props = propName.split("\\\\.");
-        for ( int i = 0 ; i < props.length ; i++ ) {
-          if ( ( p == null && i != 0 ) || ci == null )
-            break;
-          p = (PropertyInfo) ci.getAxiomByName(props[i]);
-
-          if ( i != props.length - 1 ) {
-            try {
-              Class cls;
-              if ( p instanceof foam.core.AbstractFObjectPropertyInfo ) {
-                obj1 = (FObject) p.f(obj1);
-                cls = p.getValueClass();
-              } else {
-                char[] arr = p.getName().toCharArray();
-                arr[0] = Character.toUpperCase(arr[0]);
-                sb.append(arr);
-                //String s = sb.toString();
-                obj1 = (FObject)obj1.getClass().getMethod(sb.toString(), foam.core.X.class).invoke(obj1, x);
-                sb.setLength(4);//delete(4, s.length);
-                cls = obj1.getClass();
-              }
-              ci = (ClassInfo) cls.getMethod("getOwnClassInfo").invoke(null);
-            } catch (Exception e) {
-              Logger logger = (Logger) getX().get("logger");
-              logger.error(e);
-              return new ColumnPropertyValue.Builder(x).setPropertyValue(null).setObjValue(null).build();
-            }
-          }
-        }
-        return obj1;
-      `
-    },
-    {
       name: 'returnPropertyAndObject',
       type: 'foam.nanos.column.ColumnPropertyValue',
       documentation: `returns property and object on which such property's functions as f or toCSV can be called`,
@@ -267,46 +182,6 @@ foam.CLASS({
           }
         }
         return new ColumnPropertyValue.Builder(x).setPropertyValue(p).setObjValue(obj1).build();
-      `
-    },
-    {
-      name: 'returnPropertyAndObjectArray',
-      type: 'foam.nanos.column.ColumnPropertyValue[]',
-      static: true,
-      final: true,
-      args: [
-        {
-          name: 'x',
-          type: 'Context'
-        },
-        {
-          name: 'of',
-          type: 'ClassInfo'
-        },
-        {
-          name: 'propertyNameArrays',
-          class: 'Array',
-          javaType: 'String[]',
-        },
-        {
-          name: 'obj',
-          type: 'FObject'
-        }
-      ],
-      code: async function(of, propertyNameArrays, obj) {
-        var arr = [];
-        for ( var i = 0 ; i < propertyNameArrays.length ; i++) {
-          arr.push(await this.returnPropertyAndObject(of, propertyNameArrays, obj));
-        }
-        return arr;
-      },
-      javaCode: `
-        ClassInfo ci = of;
-        foam.nanos.column.ColumnPropertyValue[] arr = new foam.nanos.column.ColumnPropertyValue[propertyNameArrays.length];
-        for ( int i = 0 ; i < propertyNameArrays.length ; i++ ) {
-          arr[i] = returnPropertyAndObject(x, of, propertyNameArrays[i], obj);
-        }
-        return arr;
       `
     },
     async function returnValueForPropertyName(x, of, propName, obj) {
