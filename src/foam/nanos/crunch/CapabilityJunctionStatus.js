b4493ff263546eb9858a3532bd272858354fe0a4
Nauna <anna.doulatshahi@gmail.com>
2020-05-28 09:44:33 -0400

    review updates

--- src/foam/nanos/crunch/CapabilityJunctionStatus.js
+++ src/foam/nanos/crunch/CapabilityJunctionStatus.js
@@ -8,23 +8,33 @@ foam.ENUM({
   name: 'CapabilityJunctionStatus',
   values: [
     {
+      name: 'AVAILABLE',
+      label: 'available',
+      documentation: `This status is used when there is no UCJ - thus capability available.`,
+      background: '#604aff'
+    },
+    {
       name: 'PENDING',
       label: 'pending',
+      documentation: `This is for capabilities that are waiting approvals. UCJ has sent out an approval.`,
       background: '#bfae32'
     },
     {
       name: 'GRANTED',
       label: 'granted',
+      documentation: `This is for capabilities that have passed all checks.`,
       background: '#32bf5e'
     },
     {
       name: 'EXPIRED',
       label: 'expired',
+      documentation: `A cron runs to establish capabilities that are no longer valid and potentially expires a ucj.`,
       background: '#bf3232'
     },
     {
       name: 'ACTION_REQUIRED',
       label: 'action required',
+      documentation: `A capability started and requiring user action, should have ucj in this state.`,
       background: '#bf3232'
     },
   ]
