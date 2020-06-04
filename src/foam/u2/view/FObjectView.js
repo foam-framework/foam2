7f971cfb68270d220f02b274c0189aedc430f956
Kevin Greer <kgrgreer@gmail.com>
2020-06-04 15:05:56 -0400

    Revert "Switched DAO.js to use new shorter argument format."
    This reverts commit 1e4d05374f41d18c4f4eb2cff3bee1cc4196502a.

--- src/foam/u2/view/FObjectView.js
+++ src/foam/u2/view/FObjectView.js
@@ -7,13 +7,12 @@
 foam.CLASS({
   package: 'foam.u2.view',
   name: 'FObjectView',
-  extends: 'foam.u2.Controller',
+  extends: 'foam.u2.View',
 
   documentation: 'View for editing FObjects.',
 
   imports: [
-    'setTimeout',
-    'strategizer?'
+    'strategizer'
   ],
 
   properties: [
@@ -21,36 +20,46 @@ foam.CLASS({
       class: 'String',
       name: 'objectClass',
       label: '',
-      visibility: function(allowCustom, choices) {
-        return allowCustom || choices.length > 1 ?
+      visibility: function(choices) {
+        return choices.length > 1 ?
           foam.u2.DisplayMode.RW :
           foam.u2.DisplayMode.HIDDEN;
       },
       view: function(args, X) {
         return {
-          class: X.data.allowCustom ? 'foam.u2.TextField' : 'foam.u2.view.ChoiceView',
-          choices: X.data.choices/*,
+          class: 'foam.u2.view.ChoiceView',
+          choices$: X.data.choices$,
           defaultValue$: X.data.choices$.map((choices) => {
             return Array.isArray(choices) && choices.length > 0 ? choices[0][0] : '';
-          })*/
+          })
         };
+      },
+      postSet: function(oldValue, newValue) {
+        if ( newValue !== oldValue && oldValue !== '' ) {
+          if ( this.data && this.data.cls_.id === newValue ) return;
+          var m = this.__context__.lookup(newValue, true);
+          if ( m ) {
+            this.data = m.create(this.persistantData, this);
+          }
+        }
       }
     },
     {
       class: 'FObjectProperty',
       name: 'data',
       label: '',
+      postSet: function(_, data) {
+        if ( ! data ) {
+          this.objectClass = undefined;
+        } else if ( data.cls_.id != this.objectClass ) {
+          this.objectClass = data.cls_.id;
+        }
+      },
       // We need to override the default view, otherwise we end up with a
       // circular definition where FObjectView has an FObjectProperty which gets
       // rendered as an FObjectView, which leads to infinite recursion.
-      preSet: function(o, n) {
-        return n || o;
-      },
-      view: 'foam.u2.DetailView',
-      xview: 'foam.u2.detail.SectionedDetailView',
-      //view: 'foam.u2.DetailView',
-      xview: function(args, X) {
-        return /*X.data.dataView ||*/ { class: 'foam.u2.detail.SectionedDetailView' }
+      view: function(args, X) {
+        return X.data.dataView || { class: 'foam.u2.detail.SectionedDetailView' }
       }
     },
     {
@@ -63,13 +72,6 @@ foam.CLASS({
       name: 'of'
     },
     {
-      class: 'Boolean',
-      name: 'allowCustom',
-      factory: function() {
-        return this.choices.length == 0;
-      }
-    },
-    {
       class: 'Array',
       name: 'choices',
       documentation: `
@@ -77,12 +79,32 @@ foam.CLASS({
         model in the 'of' property. The user can choose to create an instance
         of one of the models in this list.
       `
+    },
+    {
+      class: 'Boolean',
+      name: 'enableStrategizer',
+      documentation: 'Boolean toggle for rendering the strategizer.',
+      value: true
+    },
+    {
+      class: 'Object',
+      name: 'persistantData',
+      documentation: 'Allows passed in data to persist to selected class instances.'
+    },
+    {
+      name: 'predicate',
+      documentation: 'Unique predicate to pass into strategizer query request.'
     }
   ],
 
   methods: [
+    function init() {
+      this.onDetach(this.of$.sub(this.updateChoices));
+      this.updateChoices();
+    },
+
     function updateChoices() {
-      if ( this.of == null ) {
+      if ( this.of == null || ! this.enableStrategizer ) {
         this.choices = [];
         return;
       }
@@ -92,9 +114,17 @@ foam.CLASS({
       // populate the list of choices using models related to 'of' via the
       // implements and extends relations.
       if ( this.strategizer != null ) {
-        this.strategizer.query(null, this.of.id).then((strategyReferences) => {
+        this.strategizer.query(null, this.of.id, null, this.predicate).then((strategyReferences) => {
+          // 'found' is set to true if the current data's objectClass is one
+          // of the valid choices. If it isn't, then the objectClass is set
+          // to the first choice to cause a new 'data' to be created.
+
+          var found = false;
+          var data = this.data;
+
           if ( ! Array.isArray(strategyReferences) || strategyReferences.length === 0 ) {
             this.choices = [[this.of.id, this.of.model_.label]];
+            found = data && data.cls_.id == this.of.id;
             return;
           }
 
@@ -105,12 +135,15 @@ foam.CLASS({
                 return arr;
               }
 
-              return arr.concat([[sr.strategy.id, sr.strategy.model_.label]]);
+              if ( data && data.cls_.id === sr.strategy.id ) found = true;
+              return arr.concat([[sr.strategy.id, sr.label || sr.strategy.model_.label]]);
             }, [])
             .filter(x => x);
 
           // Sort choices alphabetically by label.
           choices.sort((a, b) => a[1] > b[1] ? 1 : -1);
+
+          if ( ! found && choices.length ) this.objectClass = choices[0][0];
 
           this.choices = choices;
         }).catch(err => console.warn(err));
@@ -121,34 +154,13 @@ foam.CLASS({
 
     function initE() {
       this.SUPER();
-
-      if ( ! this.choices.length ) {
-        this.onDetach(this.of$.sub(this.updateChoices));
-        this.updateChoices();
-      }
-
-      function dataToClass(d) {
-        return d ? d.cls_.id : '';
-      }
-
-      var classToData = function(c) {
-        var m = c && this.__context__.lookup(c, true);
-        return m ? m.create(null, this) : null;
-      }.bind(this);
-
-      this.data$.relateTo(
-        this.objectClass$,
-        dataToClass,
-        classToData
-      );
-      if ( this.data ) { this.objectClass = dataToClass(this.data); }
-      if ( ! this.data && ! this.objectClass && this.choices.length ) this.objectClass = this.choices[0][0];
-
-      this.
-        tag(this.OBJECT_CLASS).
-//        tag(this.DATA).
-//        tag(foam.u2.detail.VerticalDetailView, {data$: this.data$})
-      tag(/*foam.u2.detail.TabbedDetailView*/ foam.u2.DetailView, {data$: this.data$});
+      this
+        .tag(foam.u2.detail.VerticalDetailView, {
+          data: this,
+          sections: [{
+            properties: [this.OBJECT_CLASS, this.DATA]
+          }]
+        });
     },
 
     function choicesFallback(of) {
