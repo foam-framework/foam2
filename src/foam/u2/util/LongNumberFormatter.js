a45ab66464e1908f5781c3d92884327ad1868e5c
Kenny Kan <kanqiyen@gmail.com>
2020-06-04 12:27:44 -0400

    LongNumberFormatter

--- src/foam/u2/util/LongNumberFormatter.js
+++ src/foam/u2/util/LongNumberFormatter.js
@@ -6,7 +6,7 @@
 
 foam.CLASS({
   package: 'foam.u2.util',
-  name: 'NumberUtil',
+  name: 'LongNumberFormatter',
   documentation: `
     A utility class to truncate numbers for displays. This is to allow really
     large values to be displayed nicely on table columns or elements with minimal
@@ -22,21 +22,32 @@ foam.CLASS({
     */
     function shortenNumber(number, precision, currency) {
       var brackets = ['K', 'M', 'B', 'T'];
-      var num = String(num);
+      var num = String(number);
       var shortened = '';
 
-      if ( ! requiresShortening_(num, currency) ) return num;
-
-      if ( currency ) {
-        var flatValue = currency.precision
+      if ( ! requiresShortening_(num, currency) ) {
+        return currency ? currency.format(number) : num;
       }
+
+      var flatValue = currency ? num.substring(0, num.length - currency.precision) : num;
+      brackets.forEach((bracket, index) => {
+        var numZeroes = (index + 1) * 3;
+        var lowerBound = Math.pow(10, numZeroes);
+        var upperBound = lowerBound * 1000;
+        if ( flatValue >= lowerBound && flatValue < upperBound ) {
+          var maxPrecision = precision <= numZeroes ? precision : numZeroes;
+          var value = precision ? (flatValue/lowerBound).toFixed(maxPrecision) : Math.floor(flatValue/lowerBound);
+          shortened = currency ? `${currency.id}${currency.symbol} ${value}${bracket}` : `${value}${bracket}`;
+        }
+      });
+      return shortened;
     },
 
     function requiresShortening_(number, currency) {
       var decimal = number;
       if ( currency ) {
         if ( decimal.length < currency.precision ) return false;
-        decimal = number.substring(0, number.length - ( 1 + currency.precision ));
+        decimal = number.substring(0, number.length - currency.precision );
       }
       return decimal / 1000 >= 1;
     }
