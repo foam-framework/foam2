6bd53be7f3921c355cb56195b6a44c22d754a8d7
Mykola Kolombet <mykola@nanopay.net>
2020-06-10 15:33:15 -0400

    Working on select

--- src/foam/dao/KeyValueDAO.js
+++ src/foam/dao/KeyValueDAO.js
@@ -9,7 +9,29 @@ foam.CLASS({
   name: 'KeyValueDAO',
   extends: 'foam.dao.ProxyDAO',
   documentation: 'DAO that only responds to put/find',
+  javaImports: [
+    'foam.dao.DAO',
+    'foam.mlang.sink.Projection',
+    'foam.nanos.fs.File',
+    'foam.core.FObject',
+    'foam.dao.Sink',
+    'foam.mlang.Expr',
+    'foam.mlang.sink.Count',
+    'java.util.*',
+  ],
   methods: [
+    {
+      name: 'put_',
+      javaCode:`
+        DAO fileDAO = (DAO) x.get("fileDAO");
+        String id = ((File) obj).getId();
+        if ( fileDAO.find(id) != null ) {
+          throw new RuntimeException("Duplicate entry");
+        } else {
+          return super.put_(x, obj);
+        }
+      `
+    },
     {
       name: 'remove_',
       code: function(obj) {
@@ -17,13 +39,27 @@ foam.CLASS({
       },
       javaCode: 'return obj;'
     },
-    {
-      name: 'select_',
-      code: function(sink) {
-        return Promise.resolve(sink);
-      },
-      javaCode: 'return sink;'
-    },
+//    {
+//      name: 'select_',
+//      javaCode: `
+//        Sink result = null;
+//        if ( ! (((ProxySink) sink).getDelegate() instanceof Projection) ){
+//          Expr[] expr = new Expr[]{File.ID, File.FILESIZE, File.MIME_TYPE, File.ADDRESS};
+//          Projection projection = new Projection.Builder(x)
+//            .setExprs(expr)
+//            .build();
+////         ((ProxySink) sink).setDelegate(projection);
+//         sink = this.getDelegate().select_(x, sink, skip, limit, order, predicate);
+//         List<File> files = ((foam.dao.ArraySink)((ProxySink) sink).getDelegate()).getArray();
+//         for (var file: files){
+//          projection.put(file, foam.dao.MDAO.DetachSelect.instance());
+//         }
+//          return projection;
+////          return sink;
+//        }
+//        return this.getDelegate().select_(x, sink, skip, limit, order, predicate);
+//      `
+//    },
     {
       name: 'removeAll_',
       code: function() {
