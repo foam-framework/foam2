474637141c722665e9cbf8806ab8fc41000ae647
Mykola Kolombet <mykola@nanopay.net>
2020-06-30 12:39:20 -0400

    working on select

---
+++ src/foam/nanos/fs/FileSelectDAO.js
@@ -0,0 +1,20 @@
+/**
+ * @license
+ * Copyright 2020 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.CLASS({
+  package: 'foam.nanos.fs',
+  name: 'FileSelectDAO',
+  extends: 'foam.dao.ProxyDAO',
+
+  methods:[
+    {
+      name: 'select',
+      code: function(x, sink, skip, limit, order, predicate){
+        debugger;
+      }
+    }
+  ]
+})
