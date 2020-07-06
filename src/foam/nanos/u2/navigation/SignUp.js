0bd009893c37788c7e396cf41812f1c9136c87d1
Arthur Pavlovs <arthur@nanopay.net>
2020-07-06 16:36:47 -0400

    refactor SignUp

--- src/foam/nanos/u2/navigation/SignUp.js
+++ src/foam/nanos/u2/navigation/SignUp.js
@@ -32,7 +32,8 @@ foam.CLASS({
     { name: 'ERROR_MSG', message: 'There was a problem creating your account.' },
     { name: 'SELECTION_TEXT', message: 'Select your country' },
     { name: 'SELECTION', message: 'Please select...' },
-    { name: 'VALIDATION_ERR_TEXT', message: 'Please enter job title'}
+    { name: 'VALIDATION_ERR_TEXT', message: 'Please enter job title'},
+    { name: 'DISCLAIMER', message: '*Ablii does not currently support businesses in Quebec. We are working hard to change this! If you are based in Quebec, check back for updates.' }
   ],
 
   properties: [
@@ -64,107 +65,6 @@ foam.CLASS({
       documentation: `Set this to true to disable the email input field.`,
       hidden: true
     },
-    // {
-    //   class: 'Boolean',
-    //   name: 'disableCompanyName_',
-    //   documentation: `Set this to true to disable the Company Name input field.`,
-    //   hidden: true
-    // },
-    // {
-    //   class: 'StringArray',
-    //   name: 'countryChoices_',
-    //   documentation: `Set this to the list of countries (Country.NAME) we want our signing up user to be able to select.`,
-    //   hidden: true
-    // },
-    // {
-    //   class: 'String',
-    //   name: 'firstName',
-    //   gridColumns: 6,
-    //   view: {
-    //     class: 'foam.u2.TextField',
-    //     placeholder: 'Jane'
-    //   },
-    //   required: true
-    // },
-    // {
-    //   class: 'String',
-    //   name: 'lastName',
-    //   gridColumns: 6,
-    //   view: {
-    //     class: 'foam.u2.TextField',
-    //     placeholder: 'Doe'
-    //   },
-    //   required: true
-    // },
-    // {
-    //   class: 'String',
-    //   name: 'jobTitle',
-    //   view: function(args, X) {
-    //     return {
-    //       class: 'foam.u2.view.ChoiceWithOtherView',
-    //       otherKey: 'Other',
-    //       choiceView: {
-    //         class: 'foam.u2.view.ChoiceView',
-    //         placeholder: X.data.SELECTION,
-    //         dao: X.jobTitleDAO,
-    //         objToChoice: function(a) {
-    //           return [a.name, a.label];
-    //         }
-    //       }
-    //     };
-    //   },
-    //   validationPredicates: [
-    //     {
-    //       args: ['jobTitle'],
-    //       predicateFactory: function(e) {
-    //         return e.NEQ(foam.nanos.u2.navigation.SignUp.JOB_TITLE, '');
-    //       },
-    //       errorMessage: 'VALIDATION_ERR_TEXT'
-    //     }
-    //   ],
-    //   required: true
-    // },
-    // {
-    //   class: 'PhoneNumber',
-    //   name: 'phone',
-    //   required: true
-    // },
-    // {
-    //   class: 'String',
-    //   name: 'organization',
-    //   label: 'Company Name',
-    //   view: {
-    //     class: 'foam.u2.TextField',
-    //     placeholder: 'ABC Company'
-    //   },
-    //   visibility: function(disableCompanyName_) {
-    //     return disableCompanyName_ ? foam.u2.DisplayMode.DISABLED : foam.u2.DisplayMode.RW;
-    //   },
-    //   required: true
-    // },
-    // {
-    //   class: 'Reference',
-    //   targetDAOKey: 'countryDAO',
-    //   name: 'countryId',
-    //   label: 'Country',
-    //   of: 'foam.nanos.auth.Country',
-    //   documentation: 'Country address.',
-    //   view: function(_, X) {
-    //     var E = foam.mlang.Expressions.create();
-    //     choices = X.data.slot(function(countryChoices_) {
-    //       if ( ! countryChoices_ || countryChoices_.length == 0 ) return X.countryDAO;
-    //       return X.countryDAO.where(E.IN(X.data.Country.ID, countryChoices_));
-    //     });
-    //     return foam.u2.view.ChoiceView.create({
-    //       placeholder: X.data.SELECTION_TEXT,
-    //       objToChoice: function(a) {
-    //         return [a.id, a.name];
-    //       },
-    //       dao$: choices
-    //     }, X);
-    //   },
-    //   required: true,
-    // },
     {
       class: 'EMail',
       name: 'email',
@@ -186,7 +86,6 @@ foam.CLASS({
         class: 'foam.u2.TextField',
         placeholder: 'example123'
       },
-      //TODO: uncomment when integrating
       validationPredicates: [
         {
           args: ['userName'],
@@ -198,9 +97,7 @@ foam.CLASS({
           errorString: 'Please enter username'
         }
       ],
-      //TODO: set to true when integrating
       required: true,
-      //TODO: set to false when integrating
       hidden: false
     },
     {
@@ -208,7 +105,7 @@ foam.CLASS({
       name: 'desiredPassword',
       label: 'Password',
       view: {
-        class: 'foam.u2.view.PasswordView',
+        class: 'net.nanopay.ui.NewPasswordView',
         passwordIcon: true
       },
       minLength: 6
@@ -255,25 +152,21 @@ foam.CLASS({
   actions: [
     {
       name: 'login',
-      label: 'Get Started',
+      label: 'Create account',
       isEnabled: function(errors_, isLoading_) {
         return ! errors_ && ! isLoading_;
       },
       code: function(x) {
+        debugger;
         this.isLoading_ = true;
         this.dao_
           .put(this.User.create({
-            // firstName: this.firstName,
-            // lastName: this.lastName,
             organization: this.userName,
             userName: this.userName,
             email: this.email,
             desiredPassword: this.desiredPassword,
             signUpToken: this.token_,
-            // address: this.Address.create({ countryId: this.countryId }),
             welcomeEmailSent: true,
-            // jobTitle: this.jobTitle,
-            // phoneNumber: this.phone,
             group: this.group_
           }))
           .then((user) => {
