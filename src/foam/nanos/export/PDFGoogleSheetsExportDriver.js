f955a39eeade335e38ee50d6722b8a182de6bfd7
Joel Hughes <jlhughes@users.noreply.github.com>
2020-07-07 17:35:24 -0400

    Revert "fixing cleanup (#3668)" (#3675)
    This reverts commit 55cd814db4847794be66fa34b1e6aeea2dd1c915.

--- src/foam/nanos/export/PDFGoogleSheetsExportDriver.js
+++ src/foam/nanos/export/PDFGoogleSheetsExportDriver.js
@@ -53,6 +53,7 @@ foam.CLASS({
         if ( ! sheetId || sheetId.length == 0)
           return '';
         var url = `https://docs.google.com/spreadsheets/d/${sheetId}/export?exportFormat=pdf&format=pdf&scale=3`;
+        X.googleSheetsDataExport.deleteSheet(X, sheetId);
         return url;
     },
     async function exportDAO(X, dao) {
@@ -75,14 +76,8 @@ foam.CLASS({
       if ( ! sheetId || sheetId.length == 0)
         return '';
       var url = `https://docs.google.com/spreadsheets/d/${sheetId}/export?exportFormat=pdf&format=pdf&scale=3`;
+      X.googleSheetsDataExport.deleteSheet(X, sheetId);
       return url;
-    },
-    async function tearDown(X, obj) {
-      var findMatch = obj.match(this.SPREADSHEET_ID_REGEX);
-      if ( findMatch ) X.googleSheetsDataExport.deleteSheet(X, findMatch[1]);
     }
-  ],
-  constants: {
-    SPREADSHEET_ID_REGEX: '/spreadsheets/d/([a-zA-Z0-9-_]+)',
-  }
+  ]
 });
