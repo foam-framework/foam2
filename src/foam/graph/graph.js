ceba88c87fda4f0a7d922c77186b90f04f2a8936
Eric <eric@nanopay.net>
2020-07-09 14:08:14 -0400

    Add Graph model and child node ordering

--- src/foam/graph/graph.js
+++ src/foam/graph/graph.js
@@ -20,9 +20,70 @@ foam.CLASS({
 
 foam.CLASS({
   package: 'foam.graph',
+  name: 'Graph',
+
+  properties: [
+    {
+      name: 'data',
+      class: 'Map'
+    },
+    {
+      name: 'roots',
+      class: 'Array'
+    }
+  ],
+
+  methods: [
+    function getDirectChildren(parentId, order) {
+      var parentNode = this.data[parentId];
+      var childNodes = parentNode.forwardLinks.map(id => this.data[id]);
+      
+      if ( order ) {
+        let i = 0;
+        let idsBehind = {};
+        while ( i < childNodes.length ) {
+          let node = childNodes[i];
+
+          // Determine if/what this node should be swapped with
+          let needToSwap = false;
+          for ( let j = 0 ; j < node.inverseLinks.length ; j ++ ) {
+            let link = node.inverseLinks[j];
+            if ( link == parentNode.data.id ) continue;
+            if ( ! idsBehind[link] ) {
+              needToSwap = link;
+              break;
+            }
+          }
+
+          // Find prerequisite to swap with
+          if ( needToSwap ) {
+            for ( let ii = i+1; ii < childNodes.length; ii++ ) {
+              if ( childNodes[ii].data.id == needToSwap ) {
+                let tmp = childNodes[ii];
+                childNodes[ii] = childNodes[i];
+                childNodes[i] = tmp;
+                break;
+              }
+            }
+            // Don't increment `i` if a swap was done
+            continue;
+          }
+          idsBehind[node.data.id] = true;
+          i++;
+        }
+      }
+
+      return childNodes;
+    }
+  ]
+});
+
+foam.CLASS({
+  package: 'foam.graph',
   name: 'GraphBuilder',
 
   requires: [
+    'foam.graph.Graph',
     'foam.graph.GraphNode'
   ],
 
@@ -54,6 +115,9 @@ foam.CLASS({
               [...this.data[o.id].inverseLinks, rootObject.id];
           });
         })));
+    },
+    function build() {
+      return this.Graph.create({ data: this.data });
     }
   ],
 });
