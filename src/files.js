aaa9311e4ea2601691341af5560a3d2f2a8e448f
Mykola97 <58699954+Mykola97@users.noreply.github.com>
2020-07-14 12:16:47 -0400

    Np 1600/refactory dao decorator and decorated dao (#3696)

--- src/files.js
+++ src/files.js
@@ -203,7 +203,7 @@ FOAM_FILES([
   { name: "foam/dao/DAO" },
   { name: "foam/dao/daoUtils" },
   { name: "foam/dao/ArraySink" },
-  { name: "foam/dao/DAODecorator" },
+  { name: "foam/dao/DAOInterceptor" },
   { name: "foam/dao/AbstractDAO" },
   { name: "foam/dao/FilteredDAO" },
   { name: "foam/dao/DAOProperty" },
