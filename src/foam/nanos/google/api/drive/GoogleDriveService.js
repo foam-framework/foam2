40cf9432e8db50bfd0d3fd8c813fcd2dd9024795
Olha Bahatiuk <olha@nanopay.net>
2020-07-18 11:50:59 -0400

    adding folder

--- src/foam/nanos/google/api/drive/GoogleDriveService.js
+++ src/foam/nanos/google/api/drive/GoogleDriveService.js
@@ -8,8 +8,9 @@ foam.CLASS({
   package: 'foam.nanos.google.api.drive',
   name: 'GoogleDriveService',
   javaImports: [
-    'foam.nanos.google.api.auth.GoogleApiAuthService',
-    
+    'com.google.api.client.http.FileContent',
+    'com.google.api.services.drive.model.File',
+    'com.google.api.services.drive.model.FileList',
     'com.google.api.client.googleapis.javanet.GoogleNetHttpTransport',
     'com.google.api.client.http.javanet.NetHttpTransport',
     'com.google.api.client.json.JsonFactory',
@@ -17,8 +18,12 @@ foam.CLASS({
     'com.google.api.services.drive.Drive',
     'com.google.api.services.drive.DriveScopes',
 
+    'foam.nanos.google.api.auth.GoogleApiAuthService',
+
     'java.io.IOException',
     'java.security.GeneralSecurityException',
+    'java.util.ArrayList',
+    'java.util.Arrays',
     'java.util.Collections',
     'java.util.List',
   ],
@@ -59,6 +64,88 @@ foam.CLASS({
           .build();
     
         service.files().delete(fileId).execute();
+      `
+    },
+    {
+      name: 'createFolderIfNotExists',
+      type: 'String',
+      args: [
+        {
+          name: 'x',
+          type: 'Context',
+        },
+        {
+          name: 'folderName',
+          javaType: 'String'
+        }
+      ],
+      javaThrows: [
+        'java.io.IOException',
+        'java.security.GeneralSecurityException'
+      ],
+      javaCode: `
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        GoogleApiAuthService googleApiAuthService = (GoogleApiAuthService)getX().get("googleApiAuthService");
+        Drive service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY,  googleApiAuthService.getCredentials(x, HTTP_TRANSPORT, SCOPES))
+          .setApplicationName("nanopay")
+          .build();
+        FileList result = service.files().list()
+          .setQ("mimeType = 'application/vnd.google-apps.folder' and name = '" + folderName + "'")// and title='nanopay_export'
+          .setSpaces("drive")
+          .execute();
+        File file;
+        if ( result.getFiles().size() == 0 ) {
+          File fileMetadata = new File();
+          fileMetadata.setName(folderName);
+          fileMetadata.setMimeType("application/vnd.google-apps.folder");
+    
+          file = service.files().create(fileMetadata)
+            .setFields("id")
+            .execute();
+        } else {
+          file = result.getFiles().get(0);
+        }
+        return file.getId();
+      `
+    },
+    {
+      name: 'createFile',
+      type: 'String',
+      args: [
+        {
+          name: 'x',
+          type: 'Context',
+        },
+        {
+          name: 'folderId',
+          javaType: 'String'
+        },
+        {
+          name: 'title',
+          javaType: 'String'
+        }
+      ],
+      javaThrows: [
+        'java.io.IOException',
+        'java.security.GeneralSecurityException'
+      ],
+      javaCode: `
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        GoogleApiAuthService googleApiAuthService = (GoogleApiAuthService)getX().get("googleApiAuthService");
+        Drive service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY,  googleApiAuthService.getCredentials(x, HTTP_TRANSPORT, SCOPES))
+          .setApplicationName("nanopay")
+          .build();
+        File fileMetadata = new File();
+        fileMetadata.setName(title);
+        if ( folderId != null ) {
+          fileMetadata.setParents(new ArrayList<String>() {{ add(folderId); }});
+        }
+        fileMetadata.setMimeType("application/vnd.google-apps.spreadsheet");
+
+        File file = service.files().create(fileMetadata)
+          .setFields("id")
+          .execute();
+        return file.getId();
       `
     }
   ]
