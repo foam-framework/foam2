a8f7b06d39f24890d7a6893cccd2d8705b67b523
Eric Dubé <eric.alex.dube@gmail.com>
2020-07-28 14:35:22 -0400

    [NP-613] Add CRUNCH Lab menu item and enhance CRUNCH Lab (#3706)

--- src/foam/u2/crunch/lab/CrunchLab.js
+++ src/foam/u2/crunch/lab/CrunchLab.js
@@ -9,16 +9,33 @@ foam.CLASS({
   name: 'CrunchLab',
   extends: 'foam.u2.Controller',
 
+  implements: [
+    'foam.mlang.Expressions'
+  ],
+
   css: `
     ^ svg {
       height: calc(100% - 40px); /* temp */
     }
   `,
 
+  imports: [
+    'userCapabilityJunctionDAO'
+  ],
+
   requires: [
     'foam.graph.GraphBuilder',
+    'foam.nanos.crunch.UserCapabilityJunction',
     'foam.u2.svg.graph.RelationshipGridPlacementStrategy',
+    'foam.u2.svg.graph.IdPropertyPlacementPlanDecorator',
     'foam.u2.svg.TreeGraph',
+    'foam.u2.Tab',
+    'foam.u2.Tabs'
+  ],
+
+  messages: [
+    { name: 'ALL_TAB', message: 'All Capabilities' },
+    { name: 'UCJ_TAB', message: 'User-Capability Junction' },
   ],
 
   properties: [
@@ -26,6 +43,11 @@ foam.CLASS({
       name: 'rootCapability',
       class: 'Reference',
       of: 'foam.nanos.crunch.Capability'
+    },
+    {
+      name: 'crunchUser',
+      class: 'Reference',
+      of: 'foam.nanos.auth.User'
     },
     {
       name: 'relation',
@@ -39,36 +61,80 @@ foam.CLASS({
       var self = this;
       this
         .addClass(this.myClass())
-        .add(this.ROOT_CAPABILITY)
-        .add(this.slot(function (rootCapability, relation) {
-          if ( ! rootCapability ) return this.E();
-          var graphBuilder = self.GraphBuilder.create();
-          var rootCapabilityObj = null;
-          return self.rootCapability$find
-            .then(o => {
-              rootCapabilityObj = o;
-              return graphBuilder.fromRelationship(o, self.relation)
-            })
-            .then(() => {
-              return self.RelationshipGridPlacementStrategy.create({
-                graph: graphBuilder.build()
-              }).getPlan();
-            })
-            .then(placementPlan => {
-              return this.E()
-                .tag(self.TreeGraph, {
-                  nodePlacementPlan: placementPlan,
-                  relationshipPropertyName: self.relation,
-                  rootObject: rootCapabilityObj,
-                })
-                ;
-            })
-          return this.E()
-            .add(this.TreeGraph, {
-              nodePlacementPlan
-            })
-        }))
+        .start(this.Tabs)
+          .start(this.Tab, {
+            label: this.ALL_TAB,
+            selected: true,
+          })
+            .add(this.ROOT_CAPABILITY)
+            .add(this.getGraphSlot())
+          .end()
+          .start(this.Tab, {
+            label: this.UCJ_TAB,
+          })
+            .add(this.ROOT_CAPABILITY)
+            .add(this.CRUNCH_USER)
+            .add(this.getGraphSlot(true))
+          .end()
+        .end()
         ;
+    },
+    function getGraphSlot(replaceWithUCJ) {
+      var self = this;
+      return this.slot(function (rootCapability, crunchUser, relation) {
+        if ( ! rootCapability ) return this.E();
+        var graphBuilder = self.GraphBuilder.create();
+
+        // Having these variables here makes promise returns cleaner
+        var rootCapabilityObj = null;
+        var placementPlan = null;
+        var graph = null;
+
+        return self.rootCapability$find
+          .then(o => {
+            rootCapabilityObj = o;
+            return graphBuilder.fromRelationship(o, self.relation)
+          })
+          .then(() => {
+            graph = graphBuilder.build();
+            return self.RelationshipGridPlacementStrategy.create({
+              graph: graph,
+            }).getPlan();
+          })
+          .then(placementPlan_ => {
+            placementPlan = placementPlan_;
+            if ( replaceWithUCJ ) {
+              capabilityIds = Object.keys(graph.data);
+              return self.userCapabilityJunctionDAO.where(self.AND(
+                self.IN(self.UserCapabilityJunction.TARGET_ID, capabilityIds),
+                self.EQ(self.UserCapabilityJunction.SOURCE_ID, crunchUser)
+              )).select().then(r => {
+                r.array.forEach(ucj => {
+                  console.log('replacing', ucj.targetId, ucj);
+                  let capability = graph.data[ucj.targetId].data;
+                  graph.data[ucj.targetId].data = [
+                    capability, ucj
+                  ];
+                })
+              })
+            }
+          })
+          .then(() => {
+            placementPlan = this.IdPropertyPlacementPlanDecorator.create({
+              delegate: placementPlan,
+              targetProperty: 'id'
+            });
+            return this.E()
+              .tag(self.TreeGraph, {
+                nodePlacementPlan: placementPlan,
+                graph: graph,
+                size: 200,
+                nodeView: 'foam.u2.crunch.lab.CapabilityGraphNodeView'
+                // nodeView: 'foam.u2.svg.graph.ZoomedOutFObjectGraphNodeView'
+              })
+              ;
+          })
+      });
     }
   ],
 });
