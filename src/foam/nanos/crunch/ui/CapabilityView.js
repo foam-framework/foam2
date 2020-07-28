2adb7cd70003588f6468161306bd35b8af1c6f55
Jin Jung <jin@nanopay.net>
2020-07-28 10:43:32 -0400

    fixed a bug where capabilites are overwritten by the preceding one and addressed comments on pr

--- src/foam/nanos/crunch/ui/CapabilityView.js
+++ src/foam/nanos/crunch/ui/CapabilityView.js
@@ -1,6 +1,6 @@
 foam.CLASS({
   package: 'foam.nanos.crunch.ui',
-  name: 'MyView',
+  name: 'CapabilityView',
   extends: 'foam.u2.View',
   documentation: 'All purpose capability view that takes in an array of capaiblity ids and display these capabilities',
 
@@ -35,43 +35,79 @@ foam.CLASS({
     async function initE() {
       const self = this;
 
+      this.wizardlets = [];
+      this.wizardletSectionsList = [];
+
+      // get the wizardlets and sections for all of the capabilties
+      for ( let cap of this.capabilities ) {
+        // wizardlets for current capability
+        const curWizardlets = (await this.crunchController.getCapsAndWizardlets(cap)).wizCaps;
+
+         // add listener on curWizardlets
+         for ( let wizardlet of curWizardlets ) {
+          this.addListeners(wizardlet.data);
+        }
+
+        // get all the sections associated with curWizardlets
+        const curWizardletSectionsList = this.crunchController.generateSections(curWizardlets);
+
+        this.wizardlets = this.wizardlets.concat(curWizardlets);
+        this.wizardletSectionsList = this.wizardletSectionsList.concat(curWizardletSectionsList);
+      }
+
       this.start().addClass(this.myClass())
-        .add(this.slot(function(capabilities) {
-          return this.E().forEach(capabilities, async function(cap) {
-            // get wizardlets for the capability and all of its dependent capabilities
-            self.wizardlets = (await self.crunchController.getCapsAndWizardlets(cap)).wizCaps;
-
-            // add listeners on wizardlets
-            for ( const wizardlet of self.wizardlets ) {
-              self.addListeners(wizardlet.data);
-            }
-
-            // get all the sections associated with the wizardlets
-            self.wizardletSectionsList = self.crunchController.generateSections(self.wizardlets);
-            
-            this.add(self.slot(function(wizardletSectionsList) {
-              return this.E().forEach(wizardletSectionsList, function(sections, index) {
-                sections.map(section => (
-                  this.tag(self.SectionView, {
-                    section,
-                    data: self.wizardlets[index].data,
-                    showTitle: false
-                  })
-                ));
-              });
-            }));
+        .add(self.slot(function(wizardletSectionsList) {
+          return this.E().forEach(wizardletSectionsList, function(sections, index) {
+            sections.map(section => (
+              this.tag(self.SectionView, {
+                section,
+                data: self.wizardlets[index].data,
+                showTitle: false
+              })
+            ));
           });
         }))
+
+
+
+        // .add(this.slot(function(capabilities) {
+        //   return this.E().forEach(capabilities, async function(cap) {
+        //     // get wizardlets for the capability and all of its dependent capabilities
+        //     self.wizardlets = (await self.crunchController.getCapsAndWizardlets(cap)).wizCaps;
+
+        //     // add listeners on wizardlets
+        //     for ( let wizardlet of self.wizardlets ) {
+        //       self.addListeners(wizardlet.data);
+        //     }
+
+        //     // get all the sections associated with the wizardlets
+        //     self.wizardletSectionsList = self.crunchController.generateSections(self.wizardlets);
+            
+        //     this.add(self.slot(function(wizardletSectionsList) {
+        //       return this.E().forEach(wizardletSectionsList, function(sections, index) {
+        //         sections.map(section => (
+        //           this.tag(self.SectionView, {
+        //             section,
+        //             data: self.wizardlets[index].data,
+        //             showTitle: false
+        //           })
+        //         ));
+        //       });
+        //     }));
+        //   });
+        // }))
       .end();
     },
 
+    // adds a listener to each wizardlet.data obj for the purpose of saving
+    // all user inputs releasing calling views from the responsibility
     function addListeners(obj) {
       if ( ! this.isFObject(obj) ) return;
 
       obj.sub(this.updateWizardlet);
 
       // add listeners on inner fobjects
-      for ( const value of Object.values(obj.instance_) ) {
+      for ( let value of Object.values(obj.instance_) ) {
         if ( this.isFObject(value) ) {
           this.addListeners(value);
         }
@@ -92,7 +128,7 @@ foam.CLASS({
       code: function() {
         // TODO: can we only update the wizardlet that has new data instead of updating all the wizardlets?
         // TODO: when/where do we call finalOnClose?
-        for ( const wizardlet of this.wizardlets ) {
+        for ( let wizardlet of this.wizardlets ) {
           try {
             wizardlet.save();
           } catch (err) {
