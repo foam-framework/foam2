9d9f5effc87b2ba243d25ed7932ace405828413d
Eric <eric@nanopay.net>
2020-08-07 11:47:10 -0400

    Add documentation comments

--- src/foam/nanos/crunch/CrunchService.js
+++ src/foam/nanos/crunch/CrunchService.js
@@ -1,10 +1,18 @@
 foam.INTERFACE({
   package: 'foam.nanos.crunch',
   name: 'CrunchService',
+  documentation: `
+    CrunchService provides common logic used by the client and other CRUNCH
+    services.
+  `,
 
   methods: [
     {
       name: 'getGrantPath',
+      documentation: `
+        getGrantPath provides a an array of capability objects representing
+        the list of capabilities required to grant the desired capability.
+      `,
       async: true,
       type: 'List',
       args: [
@@ -20,6 +28,10 @@ foam.INTERFACE({
     },
     {
       name: 'getJunction',
+      documentation: `
+        getJunction provides the correct UserCapabilityJunction based on the
+        context provided.
+      `,
       async: true,
       type: 'UserCapabilityJunction',
       args: [
