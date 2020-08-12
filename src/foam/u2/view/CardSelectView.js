29a48ffc7ddfc0b0da8661b7e1736af07d8267f6
Danny Tharma <daniel@nanopay.net>
2020-08-12 15:16:30 -0400

    Added card select view

---
+++ src/foam/u2/view/CardSelectView.js
@@ -0,0 +1,69 @@
+/**
+ * @license
+ * Copyright 2020 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.CLASS({
+  package: 'foam.u2.view',
+  name: 'CardSelectView',
+  extends: 'foam.u2.View',
+
+  requires: [
+    'foam.u2.borders.CardBorder'
+  ],
+
+  css: `
+    ^ {
+      display: flex;
+      padding-top: 5%;
+      padding-bottom: 5%;
+      margin: 1%;
+      justify-content: center;
+    }
+
+    ^selected {
+      border-color: /*%PRIMARY1%*/ #202341 !important;
+    }
+    ^disabled {
+      background-color: #f4f7fa !important;
+    }
+  `,
+
+  documentation: `
+    A selectable card which takes a boolean as data, has three stares: disabled, selected and unselected
+  `,
+
+  properties: [
+    {
+      class: 'String',
+      name: 'label',
+      factory: function(){
+        return String(this.value);
+      }
+    },
+  ],
+
+  methods: [
+    function initE() {
+      this
+        .start(this.CardBorder)
+          .addClass(this.myClass())
+          .enableClass(this.myClass('selected'), this.data$)
+          .enableClass(this.myClass('disabled'), this.slot((data, mode) => {
+            return ! data && mode === foam.u2.DisplayMode.DISABLED
+          }))
+          .on('click', this.onClick)
+          .add(this.label) 
+        .end()
+    }
+  ],
+
+  listeners: [
+    function onClick() {
+      if ( this.mode !== foam.u2.DisplayMode.DISABLED ) {
+        this.data = ! this.data
+      }
+    }
+  ]
+});
