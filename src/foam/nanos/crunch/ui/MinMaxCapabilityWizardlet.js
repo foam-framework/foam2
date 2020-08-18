7ab238e4136f89e614b15f63c6b3a6aba96edb33
Danny Tharma <daniel@nanopay.net>
2020-08-18 17:29:49 -0400

    Wired up createView method and the MinMaxCapabilityWizardlet

--- src/foam/nanos/crunch/ui/MinMaxCapabilityWizardlet.js
+++ src/foam/nanos/crunch/ui/MinMaxCapabilityWizardlet.js
@@ -1,5 +1,30 @@
 foam.CLASS({
   package: 'foam.nanos.crunch.ui',
   name: 'MinMaxCapabilityWizardlet',
-  extends: 'foam.nanos.crunch.ui.CapabilityWizardlet'
+  extends: 'foam.nanos.crunch.ui.CapabilityWizardlet',
+
+  properties: [
+    {
+      class: 'FObjectArray',
+      of: 'foam.u2.wizard.Wizardlet',
+      name: 'choiceWizardlets',
+      factory: function() {
+        return []
+      }
+    },
+    {
+      name: 'choices',
+      expression: function(choiceWizardlets){
+        return choiceWizardlets.map(wizardlet => [wizardlet.title, wizardlet.title, wizardlet.isAvailable$, foam.u2.DisplayMode.RW])
+      }
+    }
+  ],
+
+  methods: [
+    function createView() {
+      // TODO: Implement create view method based on the choices property and the MultiChoiceView with choiceView as CardSelectView
+      // TODO: Wire up wizard to see if createView returns null then render default SDV otherwise render the method output
+      return null;
+    }
+  ]
 });
