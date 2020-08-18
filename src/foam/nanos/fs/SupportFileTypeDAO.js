b7ff505ed6dfaf7f7d9b77c23d271bd499f2110a
yij793 <garfield@nanopay.net>
2020-08-18 19:47:23 -0400

    add missing sim colon

--- src/foam/nanos/fs/SupportFileTypeDAO.js
+++ src/foam/nanos/fs/SupportFileTypeDAO.js
@@ -50,8 +50,8 @@ foam.CLASS({
 
       File file = (File) obj;
       DAO fileTypeDAO = (DAO) x.get("fileTypeDAO");
-      String mimeType = file.getMimeType()
-      String subType = mimeType.substring(mimeType.indexOf("/") + 1)
+      String mimeType = file.getMimeType();
+      String subType = mimeType.substring(mimeType.indexOf("/") + 1);
       FileType fileType = (FileType) fileTypeDAO.find(
         EQ(FileType.SUBTYPE, subType)
       );
