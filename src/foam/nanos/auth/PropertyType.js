7de48f91806438a498bf148a2952819f412674a7
Patrick Zanowski <zahnno@gmail.com>
2020-08-17 20:27:05 -0400

    Formatting

--- src/foam/nanos/auth/PropertyType.js
+++ src/foam/nanos/auth/PropertyType.js
@@ -4,11 +4,11 @@ foam.ENUM({
   documentation: `Indicates property type of address`,
 
   values: [
-    { name: 'RESIDENTIAL',       label: 'Residential' },
-    { name: 'COMMERCIAL',      label: 'Commercial' },
-    { name: 'INDUSTRIAL',        label: 'Industrial' },
-    { name: 'AGRICULTURAL',        label: 'Agricultural' },
-    { name: 'MIXED_USE',        label: 'Mixed Use' },
-    { name: 'SPECIAL_USE',        label: 'Special Use' },
+    { name: 'RESIDENTIAL',  label: 'Residential' },
+    { name: 'COMMERCIAL',   label: 'Commercial' },
+    { name: 'INDUSTRIAL',   label: 'Industrial' },
+    { name: 'AGRICULTURAL', label: 'Agricultural' },
+    { name: 'MIXED_USE',    label: 'Mixed Use' },
+    { name: 'SPECIAL_USE',  label: 'Special Use' },
   ]
 });
