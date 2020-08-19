11aef19c7c30d95a80d569bfde08f48843a53d48
noodlemoodle <qiyzhang@icloud.com>
2020-08-18 21:58:08 -0400

    make capabilities renewable when in grace period, undo renewable when granted moved to another branch

--- src/foam/nanos/crunch/ruler/SaveUCJDataOnGranted.js
+++ src/foam/nanos/crunch/ruler/SaveUCJDataOnGranted.js
@@ -37,7 +37,8 @@ foam.CLASS({
               EQ(UserCapabilityJunction.TARGET_ID, ucj.getTargetId())
             ));
 
-            if ( ucj.getStatus() != CapabilityJunctionStatus.GRANTED ) 
+            if ( ucj.getStatus() != CapabilityJunctionStatus.GRANTED 
+              || ( old != null && old.getStatus() == CapabilityJunctionStatus.GRANTED ) ) 
               return;
 
             Capability capability = (Capability) ucj.findTargetId(x);
