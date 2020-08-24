5b2ae09128c09502804a65639c3752bf1aec749e
Mykola Kolombet <mykola@nanopay.net>
2020-08-24 12:49:23 -0400

    Add decorator for alarmNames

--- src/foam/nanos/alarming/AddAlarmNameDAO.js
+++ src/foam/nanos/alarming/AddAlarmNameDAO.js
@@ -10,16 +10,23 @@ foam.CLASS({
   extends: 'foam.dao.ProxyDAO',
   documentation: 'DAO that add name to alarmConfig on Alarm.put',
 
+  javaImports: [
+    "foam.dao.DAO",
+    "foam.nanos.alarming.Alarm",
+    "static foam.mlang.MLang.EQ"
+  ],
+
   methods: [
     {
       name: 'put_',
       javaCode: `
+        Alarm alarm = (Alarm) obj;
         DAO alarmConfigDAO = (DAO) x.get("alarmConfigDAO");
-        AlarmConfig config = (AlarmConfig) alarmConfigDAO.find(EQ(AlarmConfig.NAME, obj.getName()));
+        AlarmConfig config = (AlarmConfig) alarmConfigDAO.find(EQ(AlarmConfig.NAME, alarm.getName()));
         if ( config == null ) {
           AlarmConfig alarmConfig = new AlarmConfig.Builder(getX())
-           .setName(configName)
-           .build();
+            .setName(alarm.getName())
+            .build();
           alarmConfigDAO.put(alarmConfig);
         }
         return super.put_(x, obj);
