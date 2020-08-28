0e561abcc32384301f91c064e0f3281594a1c732
noodlemoodle <qiyzhang@icloud.com>
2020-08-28 10:53:26 -0400

    accidentally deleted this file

---
+++ src/foam/nanos/crunch/IsUserCapabilityJunctionStatusUpdate.js
@@ -0,0 +1,37 @@
+  
+/**
+ * @license
+ * Copyright 2019 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.CLASS({
+  package: 'foam.nanos.crunch',
+  name: 'IsUserCapabilityJunctionStatusUpdate',
+
+  extends: 'foam.mlang.predicate.AbstractPredicate',
+  implements: ['foam.core.Serializable'],
+
+  documentation: `Returns true if the status of the usercapabilityjunction has been updated`,
+
+  javaImports: [
+    'foam.core.X'
+  ],
+
+  methods: [
+    {
+      name: 'f', 
+      javaCode: `
+        X x = (X) obj;
+        UserCapabilityJunction old = (UserCapabilityJunction) x.get("OLD");
+        UserCapabilityJunction ucj = (UserCapabilityJunction) x.get("NEW");
+
+        if ( old == null ) return true;
+        if ( ucj.getRenewableStatusChanged(old) ) return true;
+        if ( old.getStatus() != ucj.getStatus() ) return true;
+
+        return false;
+      `
+    }
+  ]
+});
