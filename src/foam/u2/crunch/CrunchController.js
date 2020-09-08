11bb14ab69c7113a84aaf13135c8479cc2c840e4
Eric <eric@nanopay.net>
2020-09-08 15:06:06 -0400

    Add lite wizard sequence

--- src/foam/u2/crunch/CrunchController.js
+++ src/foam/u2/crunch/CrunchController.js
@@ -84,6 +84,21 @@ foam.CLASS({
         ;
     },
 
+    // Excludes UCJ-related logic
+    function createLiteWizardSequence(capabilityOrId) {
+      return this.Sequence.create(null, this.__subContext__.createSubContext({
+        rootCapability: capabilityOrId
+      }))
+        .add(this.ConfigureFlowAgent)
+        .add(this.CapabilityAdaptAgent)
+        .add(this.LoadCapabilitiesAgent)
+        .add(this.CreateWizardletsAgent)
+        .add(this.LoadTopConfig)
+        .add(this.StepWizardAgent)
+        // .add(this.TestAgent)
+        ;
+    },
+
     function maybeLaunchInterceptView(intercept) {
       // Clear stale intercepts (ones which have been closed already)
       this.activeIntercepts = this.activeIntercepts.filter((ic) => {
@@ -182,7 +197,7 @@ foam.CLASS({
             console.log('should be a cap id', capabilityId);
             if ( ! userWantsToContinue ) return false;
             return this
-              .createWizardSequence(capabilityId).execute();
+              .createLiteWizardSequence(capabilityId).execute();
           }),
           p
         );
