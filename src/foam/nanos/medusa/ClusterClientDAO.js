8c991fe1536a5ca49dfc6f6cf6eec9738c2bec71
Joel Hughes <joel@nanopay.net>
2020-10-05 00:30:36 -0400

    add PMs

--- src/foam/nanos/medusa/ClusterClientDAO.js
+++ src/foam/nanos/medusa/ClusterClientDAO.js
@@ -24,7 +24,8 @@ foam.CLASS({
     'foam.dao.DOP',
     'foam.dao.MDAO',
     'foam.nanos.logger.Logger',
-    'foam.nanos.logger.PrefixLogger'
+    'foam.nanos.logger.PrefixLogger',
+    'foam.nanos.pm.PM'
   ],
 
   properties: [
@@ -139,7 +140,8 @@ foam.CLASS({
       ],
       type: 'Object',
       javaCode: `
-       ClusterConfigSupport support = (ClusterConfigSupport) x.get("clusterConfigSupport");
+      ClusterConfigSupport support = (ClusterConfigSupport) x.get("clusterConfigSupport");
+      PM pm = PM.create(x, getClass().getSimpleName(), getServiceName(), dop);
 
      // REVIEW: set context to null after init so it's not marshalled across network. Periodically have contexts being marshalled
       ClusterCommand cmd = null;
@@ -155,63 +157,71 @@ foam.CLASS({
       cmd.setX(null);
 
       int retryDelay = 10;
-      while ( true ) {
-        try {
-          ClusterConfig serverConfig = support.getNextServer();
-          DAO dao = support.getClientDAO(x, getClusterServiceName(), getConfig(), serverConfig);
-          getLogger().debug("submit", "request", "to", serverConfig.getId(), "dao", dao.getClass().getSimpleName(), dop.getLabel(), obj.getClass().getSimpleName());
-
-          Object result = null;
-          if ( DOP.PUT == dop ) {
-            result = dao.put_(x, cmd);
-          } else if ( DOP.REMOVE == dop ) {
-            result = dao.remove_(x, cmd);
-          } else if ( DOP.CMD == dop ) {
-            result = dao.cmd_(x, cmd);
-            if ( result != null ) {
-              // getLogger().debug("submit", "response", "from", serverConfig.getId(), "dao", dao.getClass().getSimpleName(), dop.getLabel(), result.getClass().getSimpleName());
-              return result;
-            }
-          }
-          if ( obj instanceof ClusterCommand ) {
-            // getLogger().debug("submit", "response", "from", serverConfig.getId(), "dao", dao.getClass().getSimpleName(), dop.getLabel(), (result != null ? result.getClass().getSimpleName() : "null"));
-            cmd.setData((FObject) result);
-            cmd.addHop(x, dop, "reply");
-            return cmd;
-          }
-          return result;
-
-        } catch ( ClusterException e ) {
-          getLogger().debug("submit", e.getClass().getSimpleName(), e.getMessage());
-          throw e;
-        } catch ( RuntimeException e ) {
-          getLogger().debug("submit", e.getMessage());
-          throw e;
-        } catch ( Throwable t ) {
-          getLogger().debug("submit", t.getMessage());
-//          getLogger().debug("submit", t);
-
-          if ( getMaxRetryAttempts() > -1 &&
-               cmd.getRetry() >= getMaxRetryAttempts() ) {
-            getLogger().debug("retryAttempt >= maxRetryAttempts", cmd.getRetry(), getMaxRetryAttempts());
-            throw t;
-          }
-          cmd.setRetry(cmd.getRetry() + 1);
-
-          // delay
+      try {
+        while ( true ) {
           try {
-            retryDelay *= 2;
-            if ( retryDelay > getMaxRetryDelay() ) {
-              retryDelay = 10;
+            ClusterConfig serverConfig = support.getNextServer();
+            DAO dao = support.getClientDAO(x, getClusterServiceName(), getConfig(), serverConfig);
+            getLogger().debug("submit", "request", "to", serverConfig.getId(), "dao", dao.getClass().getSimpleName(), dop.getLabel(), obj.getClass().getSimpleName());
+
+            Object result = null;
+            if ( DOP.PUT == dop ) {
+              result = dao.put_(x, cmd);
+            } else if ( DOP.REMOVE == dop ) {
+              result = dao.remove_(x, cmd);
+            } else if ( DOP.CMD == dop ) {
+              result = dao.cmd_(x, cmd);
+              if ( result != null ) {
+                // getLogger().debug("submit", "response", "from", serverConfig.getId(), "dao", dao.getClass().getSimpleName(), dop.getLabel(), result.getClass().getSimpleName());
+                return result;
+              }
             }
-            getLogger().debug("retry attempt", cmd.getRetry(), "delay", retryDelay);
-            Thread.sleep(retryDelay);
-          } catch(InterruptedException e) {
-            Thread.currentThread().interrupt();
-            getLogger().debug("InterruptedException");
-            throw t;
+            if ( obj instanceof ClusterCommand ) {
+              // getLogger().debug("submit", "response", "from", serverConfig.getId(), "dao", dao.getClass().getSimpleName(), dop.getLabel(), (result != null ? result.getClass().getSimpleName() : "null"));
+              cmd.setData((FObject) result);
+              cmd.addHop(x, dop, "reply");
+              return cmd;
+            }
+            return result;
+
+          } catch ( ClusterException e ) {
+            getLogger().debug("submit", e.getClass().getSimpleName(), e.getMessage());
+            pm.error(x, e);
+            throw e;
+          } catch ( RuntimeException e ) {
+            getLogger().debug("submit", e.getMessage());
+            pm.error(x, e);
+            throw e;
+          } catch ( Throwable t ) {
+            getLogger().debug("submit", t.getMessage());
+            // getLogger().debug("submit", t);
+
+            if ( getMaxRetryAttempts() > -1 &&
+                 cmd.getRetry() >= getMaxRetryAttempts() ) {
+              getLogger().debug("retryAttempt >= maxRetryAttempts", cmd.getRetry(), getMaxRetryAttempts());
+              pm.error(x, "Retry limit reached.", t);
+              throw new RuntimeException("Rejected, retry limit reached.", t);
+            }
+            cmd.setRetry(cmd.getRetry() + 1);
+
+            // delay
+            try {
+              retryDelay *= 2;
+              if ( retryDelay > getMaxRetryDelay() ) {
+                retryDelay = 10;
+              }
+              getLogger().debug("retry attempt", cmd.getRetry(), "delay", retryDelay);
+              Thread.sleep(retryDelay);
+            } catch(InterruptedException e) {
+              Thread.currentThread().interrupt();
+              getLogger().debug("InterruptedException");
+              pm.error(x, t);
+              throw t;
+            }
           }
         }
+      } finally {
+        pm.log(x);
       }
       `
     },
