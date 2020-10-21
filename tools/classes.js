c91fd7535e0ffd7f731c4b8ab3ff8f56403fefeb
olhabn <55991618+olhabn@users.noreply.github.com>
2020-10-21 11:39:46 -0400

    Np 1347/reverse google sheets integration column header to prop mapper (#3670)
    * figuring out google sheets getValues
    
    * using cellsRange
    
    * temporary getValues method is added
    
    * figuring out transactions
    
    * fixing an error
    
    * renaming GoogleSheetsExportService to
    GoogleSheetsApiService
    
    * moving files
    
    * moving classes for google sheets export to
    google_api package
    
    * re-ordering
    
    * fixing conflicts
    
    * adding some code for future use
    
    * adding updateValues
    
    * fixing error UNFORMATTED_VALUE
    
    * removing try-catch
    
    * adding GoogleSheetsDataImportService
    
    * fixing errors
    
    * figuring out mapping view
    
    * renaming
    
    * checkout src/foam/mlang/mlang.js
    src/foam/nanos/column_config/NestedPropertiesExpression.js
    src/foam/nanos/column_config/NestedPropertiesExpression.js
    from NP-813/
    custom_table_columns_add_ability_to_display_property_of_property_
    reference
    
    * figuring out importData method
    
    * figuring out importData method
    
    * fixing mlang and reverting some changes
    
    * fixing views and adding showing error css
    
    * figuring out error message
    
    * figuring out values parsing
    
    * fixing currency parsing
    
    * refactoring
    
    * fixing error
    
    * fixing errors
    
    * fixing errors
    
    * updating columns
    
    * fixing error
    
    * figuring out unit property
    
    * figuring out getting rid of cells range
    
    * using sheetId instead of cellsRange
    
    * adding header
    
    * deleting code for ColumnHeader to property mapping
    which supported reference properties, which was overkill
    
    * small clean up
    
    * renaming ColumnHeaderToPropertyName to
    ColumnHeaderToPropertyMapping
    
    * clean up ColumnPropertyMapping
    
    * ColumnHeaderToPropertyMapping moved to
    GoogleSheetsImportConfig
    
    * setting dao from DAOBrowserView
    
    * comments added
    
    * adding
    GoogleSheetsTransactionsDataImportServiceImpl
    
    * comments added
    
    * fixing small things
    
    * adding ImportFromGoogleSheetsForm
    
    * removing ImportDataModal
    
    * some validation
    
    * some validation
    
    * changing visibility to disabled
    
    * fixing
    
    * comment added
    
    * fixing views
    
    * headers added
    
    * fixing labels
    
    * adding header
    
    * fixing data parsing
    
    * fixing date
    
    * ImportFromGoogleSheetsModal deleted
    
    * ImportDataMessage added
    
    * using ImportDataMessage
    
    * fixing import
    
    * clean up: ColumnToPropertyMapper deleted
    
    * fixing some logic and imports
    
    * small clean up
    
    * fixing formatting and namings
    
    * adding null prop support
    and making user id  sheetsOutput: true
    
    * fixing padding and error status
    
    * getStringValueByProperty added
    
    * fixing error messages
    
    * GoogleSheetsParsingHelper added
    
    * some comments deleted
    
    * fixing logic
    
    * fixing error
    
    * clean up and messages added
    
    * clean up
    
    * fixing errors
    
    * fixing message
    
    * clean up
    
    * clean up
    
    * clean up
    
    * clean up
    
    * checkout master mlang
    
    * fixing pr
    
    * clean up
    
    * findColumnIndex deleted. clean up
    
    * findAndValidateIndexOfColumnInArrayOfValues deleted
    clean up
    
    * fixing error
    
    * fixing pr
    
    * fixing models
    
    * fixing unitPropName being null

--- tools/classes.js
+++ tools/classes.js
@@ -447,7 +447,13 @@ var classes = [
   'foam.nanos.google.api.auth.GoogleApiAuthService',
   'foam.nanos.google.api.auth.GoogleApiCredentials',
   'foam.nanos.google.api.drive.GoogleDriveService',
+  'foam.nanos.google.api.sheets.ColumnHeaderToPropertyMapping',
   'foam.nanos.google.api.sheets.GoogleSheetsApiService',
+  'foam.nanos.google.api.sheets.GoogleSheetsDataImportService',
+  'foam.nanos.google.api.sheets.GoogleSheetsDataImportServiceImpl',
+  'foam.nanos.google.api.sheets.GoogleSheetsImportConfig',
+  'foam.nanos.google.api.sheets.GoogleSheetsTransactionsDataImportServiceImpl',
+  'foam.nanos.google.api.sheets.ImportDataMessage',
   'foam.dao.jdbc.ConnectionPool',
   'foam.lib.Outputter',
   'foam.lib.PropertyPredicate',
@@ -762,6 +768,7 @@ var skeletons = [
   'foam.strategy.StrategizerService',
   'foam.nanos.auth.UserQueryService',
   'foam.nanos.export.GoogleSheetsExport',
+  'foam.nanos.google.api.sheets.GoogleSheetsDataImportService',
   'foam.nanos.crunch.CrunchService',
   'foam.i18n.TranslationService'
 ];
