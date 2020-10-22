16638f83f8b058b8c52eb90329a6352965c0af16
Kenny Qi Yen Kan <kennyqiyenkan@users.noreply.github.com>
2020-10-22 16:15:16 -0400

    Added top padding to the content portion of the wizardlet view. (#4124)

--- src/foam/u2/wizard/StepWizardletView.js
+++ src/foam/u2/wizard/StepWizardletView.js
@@ -89,6 +89,7 @@ foam.CLASS({
       -webkit-mask-image: -webkit-gradient(linear, left 15, left top, from(rgba(0,0,0,1)), to(rgba(0,0,0,0)));
       overflow-y: auto;
       padding: 0 50px;
+      padding-top: 24px;
     }
     ^rightside ^top-buttons {
       text-align: right;
