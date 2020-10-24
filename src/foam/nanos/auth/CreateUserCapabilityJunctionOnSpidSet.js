4cefb7817a3af63432f033e323d43457b6127538
Chanmann Lim <chanmann@nanopay.net>
2020-10-23 22:29:32 -0400

    Fix ucjs for spid not getting setup

--- src/foam/nanos/auth/CreateUserCapabilityJunctionOnSpidSet.js
+++ src/foam/nanos/auth/CreateUserCapabilityJunctionOnSpidSet.js
@@ -48,13 +48,13 @@ foam.CLASS({
             Logger logger = (Logger) x.get("logger");
             User user = (User) obj;
             User old = (User) oldObj;
-    
+
             String spid = user.getSpid() == null ? null : user.getSpid().trim();
             String oldSpid = old == null || old.getSpid() == null ? null : old.getSpid().trim();
-    
+
             if ( spid == null || spid.isEmpty() || spid.equals(oldSpid) ) return;
 
-            ServiceProvider sp = (ServiceProvider) user.findSpid(x);
+            ServiceProvider sp = (ServiceProvider) user.findSpid(systemX);
             if ( sp == null ) {
               logger.error("Cannot find capability for service provider : ", spid);
               return;
