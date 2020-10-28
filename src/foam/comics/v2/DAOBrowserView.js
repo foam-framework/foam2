4018b94f04dd08e6d7a4e06d1b103bc715b71ce5
olhabn <55991618+olhabn@users.noreply.github.com>
2020-10-28 15:45:01 -0400

    Np 2512/add permissions for google sheets import (#4176)
    * adding permission
    
    * adding validation

--- src/foam/comics/v2/DAOBrowserView.js
+++ src/foam/comics/v2/DAOBrowserView.js
@@ -214,6 +214,7 @@ foam.CLASS({
     {
       name: 'import',
       label: '',
+      availablePermissions: [ "data.import.googleSheets" ],
       toolTip: 'Import From Google Sheet',
       // icon: 'images/export-arrow-icon.svg',//need find out where we're getting the icons
       code: function() {
