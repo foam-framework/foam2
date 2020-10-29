837f924b7b85dbe10ad42130ebb5532264feeaa7
Eric <eric@nanopay.net>
2020-10-29 12:19:49 -0400

    Add ucj listener to CachedAuthService

--- src/foam/nanos/auth/CachingAuthService.java
+++ src/foam/nanos/auth/CachingAuthService.java
@@ -12,6 +12,7 @@ import foam.dao.DAO;
 import foam.dao.Sink;
 import foam.mlang.predicate.Predicate;
 import foam.nanos.auth.User;
+import foam.nanos.crunch.CrunchService;
 import foam.nanos.session.Session;
 import java.security.Permission;
 import java.util.concurrent.ConcurrentHashMap;
@@ -96,6 +97,9 @@ public class CachingAuthService
       DAO       userDAO   = (DAO) x.get("localUserDAO");
       DAO       groupDAO  = (DAO) x.get("localGroupDAO");
       DAO       groupPermissionJunctionDAO = (DAO) x.get("groupPermissionJunctionDAO");
+      DAO       userCapabilityJunctionDAO = (DAO) x.get("userCapabilityJunctionDAO");
+      Predicate ucjPredicate = ((CrunchService) x.get("crunchService"))
+        .getAssociationPredicate(x);
       User      agent     = subject.getRealUser();
       Predicate predicate = EQ(User.ID, user.getId());
 
@@ -106,6 +110,7 @@ public class CachingAuthService
       groupDAO.listen(purgeSink, TRUE);
       userDAO.listen(purgeSink, predicate);
       groupPermissionJunctionDAO.listen(purgeSink, TRUE);
+      userCapabilityJunctionDAO.listen(purgeSink, ucjPredicate);
 
       String[] extraDAOsToListenTo = (String[]) x.get("extraDAOsToListenTo");
 
