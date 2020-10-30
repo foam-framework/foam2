0c403ca84efd77a37a084302c85bc199a9f1bce1
Scott Head <scott@nanopay.net>
2020-10-30 16:59:26 -0400

    Removing AdapterDAO from files

--- src/files.js
+++ src/files.js
@@ -253,7 +253,6 @@ FOAM_FILES([
   { name: "foam/dao/MDAO" },
   { name: "foam/dao/ArrayDAO" },
   { name: "foam/dao/TimestampDAO" },
-  { name: "foam/dao/AdapterDAO" },
   { name: "foam/dao/CopyFromDAO" },
   { name: "foam/dao/GUIDDAO" },
   { name: "foam/dao/KeyValueDAO" },
