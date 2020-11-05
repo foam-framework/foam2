b3521d76a5cc3adbaa5b7896c71ee0017900f14e
Olha Bahatiuk <olha@nanopay.net>
2020-11-05 06:35:08 -0500

    fixing errors

--- src/foam/nanos/controller/ApplicationController.js
+++ src/foam/nanos/controller/ApplicationController.js
@@ -513,10 +513,8 @@ foam.CLASS({
       }
       /** Use to load a specific menu. **/
       // Do it this way so as to not reset mementoTail if set
-      if ( ( this.memento.head && this.memento.head.length === 0 )  && this.memento.head !== menu || opt_forceReload ) {
-        this.memento.feedback_ = true;
+      if ( this.memento.head !== menu || opt_forceReload ) {
         this.memento.value = menu;
-        this.memento.feedback_ = false;
       }
     },
 
