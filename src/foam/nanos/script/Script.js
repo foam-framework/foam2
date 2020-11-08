77c40b36e34d264c379ba66fdc78f4d740d18dca
Chanmann Lim <chanmann@nanopay.net>
2020-11-08 16:52:07 -0500

    Update script output on success and error

--- src/foam/nanos/script/Script.js
+++ src/foam/nanos/script/Script.js
@@ -360,7 +360,9 @@ foam.CLASS({
         var self = this;
         var interval = setInterval(function() {
           self.__context__[self.daoKey].find(self.id).then(function(script) {
-            if ( script.status !== self.ScriptStatus.RUNNING ) {
+            if ( script.status === self.ScriptStatus.UNSCHEDULED
+              || script.status === self.ScriptStatus.ERROR
+            ) {
               self.copyFrom(script);
               clearInterval(interval);
 
