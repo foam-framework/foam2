1a3d417b3f6ae6dd85a8e7b37a22529c8042243b
Chanmann Lim <chanmann@nanopay.net>
2020-11-11 17:53:17 -0500

    Add documentation

--- src/foam/nanos/boot/AltFactory.java
+++ src/foam/nanos/boot/AltFactory.java
@@ -13,6 +13,24 @@ import foam.mlang.predicate.Predicate;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * AltFactory allows configuring multiple alternative services for an nspec
+ * depending on the associated predicates.
+ *
+ * Upon invoking the service wrapped with AltFactory, it checks the predicate
+ * of each alternative service then returns the service, otherwise, the original
+ * service is returned.
+ *
+ * Example usage: To configure a nspec serviceScript so that it returns
+ * "serviceA" when the current user is user(id:1) and "serviceB" for user(id:2)
+ * otherwise returns the "defaultService".
+ *
+ * <pre>
+ *   service = new foam.nanos.boot.AltFactory("defaultService");
+ *   service.addDelegate(new IsCurrentUser(1), "serviceA");
+ *   service.addDelegate(new isCurrentUser(2), "serviceB");
+ * </pre>
+ */
 public class AltFactory implements XFactory {
   private final String key_;
   private final List<AltDelegate> altDelegates_ = new ArrayList<>();
