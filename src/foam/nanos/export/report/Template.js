8a75e19baf17816a2a025be704820a5d93235605
Olha Bahatiuk <olha@nanopay.net>
2020-11-13 13:54:39 -0500

    using column labels

--- src/foam/nanos/export/report/Template.js
+++ src/foam/nanos/export/report/Template.js
@@ -7,6 +7,15 @@
 foam.CLASS({
   package: 'foam.nanos.export.report',
   name: 'Template',
+
+  imports: [
+    'columnConfigToPropertyConverter',
+  ],
+
+  requires: [
+    'foam.mlang.predicate.Eq'
+  ],
+
   properties: [
     {
       name: 'id',
@@ -23,7 +32,39 @@ foam.CLASS({
     },
     {
       class: 'StringArray',
+      name: 'columnLabels',
+      label: 'Table Columns',
+      documentation: 'List of property labels which required for template',
+      postSet: async function(o, n) {
+        var dao = await this.__subContext__.nSpecDAO.find(this.Eq.create({ arg1: foam.nanos.boot.NSpec.NAME, arg2: this.daoKey }));
+        var client = JSON.parse(dao.client);
+        var of =  foam.lookup(client.of);
+        var columnConfigToPropertyConverter = this.columnConfigToPropertyConverter;
+        if ( ! columnConfigToPropertyConverter )
+          columnConfigToPropertyConverter = foam.nanos.column.ColumnConfigToPropertyConverter.create();
+        if ( of )
+          this.columnNames = n.map( l => columnConfigToPropertyConverter.returnPropertyNameForLabel(of, l));
+      }
+    },
+    {
+      name: 'of',
+      hidden: true,
+      transient: true,
+      expression: function(daoKey) {
+        if ( ! daoKey )
+          return null;
+        var dao = this.__subContext__.nSpecDAO.find(this.Eq.create({ arg1: foam.nanos.boot.NSpec.NAME, arg2: daoKey }));
+        if ( ! dao || ! dao.client  )
+          return null;
+        var client = JSON.parse(dao.client);
+        return foam.lookup(client.of).create();
+      }
+    },
+    {
+      class: 'StringArray',
       name: 'columnNames',
+      transient: true,
+      hidden: true,
       documentation: 'List of property names which required for template'
     }
   ]
