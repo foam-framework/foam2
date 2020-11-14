308ad45617f4876bb14df6a283da17ef54a89343
Kevin Greer <kgrgreer@gmail.com>
2020-11-14 16:26:03 -0500

    Updated.

--- src/com/google/foam/demos/exgen/ExGen.js
+++ src/com/google/foam/demos/exgen/ExGen.js
@@ -23,6 +23,17 @@ foam.CLASS({
     }
   ],
 
+  css: `
+    ^ {
+      font-size: larger;
+    }
+    ^row:hover {
+      left-margin: 4px;
+      color: white;
+      background: cornflowerblue;
+    }
+  `,
+
   properties: [
     {
       class: 'FObjectArray',
@@ -31,8 +42,8 @@ foam.CLASS({
     },
     {
       class: 'Int',
-      name: 'length',
-      value: 40
+      name: 'duration',
+      value: 10
     },
     {
       class: 'FObjectArray',
@@ -44,34 +55,59 @@ foam.CLASS({
   methods: [
     function initE() {
       this.SUPER();
+
+      var self = this;
+
       this.dictionary = [
         { name: 'Chin Ups',      length: 1 },
-        { name: 'Clean & Press', length: 2 },
-        { name: 'Clean & Press', length: 2 },
+        { name: 'Clean & Press (L+R)', length: 2 },
+        { name: 'Clean & Press (L+R)', length: 2 },
         { name: 'Curls',         length: 1 },
         { name: 'Dips',          length: 1 },
         { name: 'Dips',          length: 1 },
-        { name: 'Floor Press',   length: 2 },
+        { name: 'Floor Press (L+R)',   length: 2 },
         { name: 'Plank',         length: 3 },
-        { name: 'Press',         length: 2 },
+        { name: 'Press (L+R)',         length: 2 },
         { name: 'Push Ups',      length: 1 },
-        { name: 'Rows ',         length: 2 },
-        { name: 'Snatch',        length: 2 },
-        { name: 'Squats',        length: 1 },
+        { name: 'Rows (L+R)',         length: 2 },
+        { name: 'Snatch (L+R)',        length: 2 },
+        { name: 'Squats (L+R)',        length: 1 },
         { name: 'Swings (10)',   length: 1 },
         { name: 'Swings (10)',   length: 1 },
         { name: 'Swings (20)',   length: 2 },
         { name: 'Swings (30)',   length: 3 },
-        { name: 'TGU',           length: 2 },
-        { name: 'TGU',           length: 2 }
+        { name: 'TGU (L+R)',           length: 2 },
+        { name: 'TGU (L+R)',           length: 2 }
       ];
 
       //this.add(this.PROGRAM);
-      this.add('Length: ', this.LENGTH, ' ', this.GENERATE).tag('hr');
-      this.forEach(this.program$, function(e) { this.tag({class: 'foam.u2.CheckBox'}).add(' ', e.name).br(); });
+      this
+        .addClass(this.myClass())
+        .add('Duration: ', this.DURATION, ' ', this.GENERATE)
+        .tag('hr')
+        .forEach(this.program$, function(e, i) {
+          this
+          .start()
+            .addClass(self.myClass('row'))
+            .tag({class: 'foam.u2.CheckBox'})
+            .add(' ')
+            .start('span')
+              .style({width: '400px', display: 'inline-block'})
+              .add(' ', e.name)
+            .end()
+            .start('span')
+              .add('x')
+              .on('click', () => {
+                var a = foam.Array.clone(self.program);
+                a.splice(i, 1);
+                self.program = a;
+              })
+            .end()
+          .end();
+        });
       /*
       this.add(this.program$.map(function(p) {
-        return self.E().forEach(p, );
+        return self.E().forEach(p, function(e) { thi.add(e.something); }
       }));
       */
     }
@@ -81,7 +117,7 @@ foam.CLASS({
     function generate() {
       var a = [];
       var l = 0;
-      while ( l <= this.length ) {
+      while ( l < this.duration ) {
         var e = this.dictionary[Math.floor(this.dictionary.length*Math.random())];
         a.push(e);
         l += e.length;
