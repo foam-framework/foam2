a6ad72a56dac2afaa7f94e9f2c088d3130f8b7a9
RachaelDing <rachel@nanopay.net>
2020-11-19 10:11:16 -0500

    update the text to be "The information you provided is pending review"

--- src/foam/nanos/crunch/CapabilityJunctionStatus.js
+++ src/foam/nanos/crunch/CapabilityJunctionStatus.js
@@ -48,7 +48,7 @@ foam.ENUM({
     {
       name: 'PENDING_REVIEW',
       label: { en: 'pending review', pt: 'revisão pendente' },
-      documentation: 'The information you provided is pending final review',
+      documentation: 'The information you provided is pending review',
       background: '#bfae32'
     },
     {
