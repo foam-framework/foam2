e263367e887d946f5d0bac915feb7e97b68c8816
Joel Hughes <jlhughes@users.noreply.github.com>
2020-11-23 08:44:00 -0500

    Add ability to disable generation of default java constructors.  Refa… (#4349)
    * Add ability to disable generation of default java constructors.  Refactor AuthenticationException to a modeled Exception
    
    * remove changes copied from medusa
    
    * Update src/foam/nanos/auth/AuthenticationException.js

--- src/files.js
+++ src/files.js
@@ -297,6 +297,7 @@ FOAM_FILES([
   { name: "foam/nanos/boot/NSpec" },
   { name: "foam/nanos/boot/NSpecAware" },
   { name: "foam/nanos/auth/Authorizer" },
+  { name: "foam/nanos/auth/AuthenticationException" },
   { name: "foam/nanos/auth/EnabledAware" },
   { name: "foam/nanos/auth/ServiceProviderAware" },
   { name: "foam/nanos/auth/ServiceProviderAwareSink" },
