72e0ef302731400daf7a441c1a9b283d320caac8
Olha Bahatiuk <olha@nanopay.net>
2020-11-26 09:41:30 -0500

    uncommenting value

--- src/foam/nanos/controller/Memento.js
+++ src/foam/nanos/controller/Memento.js
@@ -29,7 +29,7 @@ foam.CLASS({
     },
     {
       name: 'head',
-      // value: '',
+      value: '',
       postSet: function(o, n) {
         if ( this.feedback_ ) return;
         this.feedback_ = true;
