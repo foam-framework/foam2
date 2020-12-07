1af4fd2794f808211fde0a2432a4e13bb6b0789a
Eric <eric@nanopay.net>
2020-12-07 18:06:47 -0500

    Cleanup CapableCreateWizardletsAgent

--- src/foam/u2/crunch/wizardflow/lite/CapableCreateWizardletsAgent.js
+++ src/foam/u2/crunch/wizardflow/lite/CapableCreateWizardletsAgent.js
@@ -42,16 +42,6 @@ foam.CLASS({
     async function execute() {
       var wizardlets = await this.parseArrayToWizardlets(this.capabilities);
       this.wizardlets = wizardlets;
-      return;
-      var capable = this.capable;
-      var capablePayloads = await this.crunchService.getCapableObjectPayloads(null, this.capable.capabilityIds)
-      var wizardletsTuple = await this.createWizardletsFromPayloads(capablePayloads);
-
-      // TODO: not sure how this is going tto work if you have some of the payloads, might have to filter them out
-      // need to finalize with eric before proceeding
-      this.wizardlets = wizardletsTuple.allDescendantWizardlets;
-      console.log('CAPABLE', capable);
-      console.log('WIZARDLETS', this.wizardlets);
     },
     async function parseArrayToWizardlets(array, parent) {
       var capabilityDesired = array[array.length - 1];
@@ -107,88 +97,6 @@ foam.CLASS({
     },
     function isPrerequisiteAware(wizardlet) {
       return this.PrerequisiteAwareWizardlet.isInstance(wizardlet);
-    },
-    async function createWizardletsFromPayloads(payloads) {
-      var newWizardlets = [];
-      var childWizardlets = [];
-      for ( let i = 0 ; i < payloads.length ; i++ ) {
-        let capablePayload = payloads[i];
-        let wizardlet = await this.createWizardletFromPayload(capablePayload);
-        let handlePrereqsNormally = true;
-        let addWizardletAtEnd = true;
-
-        // If this is a MinMax capability, handle its prerequisites differently
-        var capability = await this.capabilityDAO.find(capablePayload.capability);
-
-        if ( this.MinMaxCapability.isInstance(capability) ) {
-          handlePrereqsNormally = false;
-          addWizardletAtEnd = false;
-
-          // MinMax wizardlets appear before their prerequisites
-          newWizardlets.push(wizardlet);
-          childWizardlets.push(wizardlet);
-
-          let minMaxPrereqWizardlets =
-            await this.createWizardletsFromPayloads(capablePayload.prerequisites);
-
-          capablePayload.prerequisites = null;
-
-          minMaxPrereqWizardlets.directChildrenWizardlets.forEach(prereqWizardlet => {
-            prereqWizardlet.isAvailable = false;
-            wizardlet.choiceWizardlets.push(prereqWizardlet);
-          })
-
-          newWizardlets = [
-            ...newWizardlets,
-            ...minMaxPrereqWizardlets.allDescendantWizardlets
-          ]
-        }
-
-        // If this is a prerequisite of a normal capability, bind isAvailable to the
-        // parent.
-        if ( handlePrereqsNormally && capablePayload.prerequisites.length > 0 ) {
-          let prereqWizardlets =
-            await this.createWizardletsFromPayloads(capablePayload.prerequisites);
-
-          capablePayload.prerequisites = null;
-
-          prereqWizardlets.directChildrenWizardlets.forEach(prereqWizardlet => {
-            prereqWizardlet.isAvailable$.follow(wizardlet.isAvailable$);
-          })
-
-          newWizardlets = [
-            ...newWizardlets,
-            ...prereqWizardlets.allDescendantWizardlets
-          ]
-        }
-
-        // Wizardlets appear after their prerequisites by default
-        if ( addWizardletAtEnd ) {
-          newWizardlets.push(wizardlet);
-          childWizardlets.push(wizardlet);
-        }
-      }
-
-      return {
-        allDescendantWizardlets: newWizardlets,
-        directChildrenWizardlets: childWizardlets
-      };
-    },
-    async function createWizardletFromPayload(capablePayload) {
-      var capability = await this.capabilityDAO.find(capablePayload.capability);
-
-      let wizardletClass = capability.wizardlet.cls_;
-
-      let wizardlet = wizardletClass.create({
-        capability: capability,
-        data$: capablePayload.data$,
-        dataController: this.CapableWAO.create(
-          {}, this.__context__)
-      }, this);
-
-      wizardlet.dataController.targetPayload = capablePayload;
-
-      return wizardlet;
     },
   ]
 });
