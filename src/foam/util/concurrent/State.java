a8600569397783dc14cc0d4c00896e1c854db00a
Kevin Greer <kgrgreer@gmail.com>
2021-01-05 17:19:03 -0500

    Initial check-in.

---
+++ src/foam/util/concurrent/State.java
@@ -0,0 +1,13 @@
+/**
+ * @license
+ * Copyright 2021 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package foam.util.concurrent;
+
+/**
+ * Marker interface for FoldReduce State.
+ **/
+public interface State {
+}
