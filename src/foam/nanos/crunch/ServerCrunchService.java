e7b479278a0f99d81ea4a536e61b647b0e25cf77
Ruby <32865869+noodlemoodle@users.noreply.github.com>
2021-01-19 15:54:21 -0500

    filter prereqs before adding to minmaxarray if there are capabilities that already exists in the grantpath outside of the minmaxarray path (#4645)

--- src/foam/nanos/crunch/ServerCrunchService.java
+++ src/foam/nanos/crunch/ServerCrunchService.java
@@ -122,6 +122,9 @@ public class ServerCrunchService extends ContextAwareSupport implements CrunchSe
         // Manually grab the direct  prereqs to the  MinMaxCapability
         for ( int i = prereqs.length - 1 ; i >= 0 ; i-- ) {
           var prereqGrantPath = this.getCapabilityPath(x, prereqs[i], filterGrantedUCJ);
+          
+          // remove prereqs that are already listed
+          prereqGrantPath.removeIf(c -> { return alreadyListed.contains(((Capability) c).getId()); });
 
           // Essentially we reserve arrays to denote  ANDs and ORs, must be at least 2  elements
           if ( prereqGrantPath.size() > 1 ) minMaxArray.add(prereqGrantPath);
