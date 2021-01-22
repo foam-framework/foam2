02333ad0252f59d27cc41eb514c5eda48a1655e0
Danny Tharma <daniel@nanopay.net>
2021-01-22 18:41:59 -0500

    MultiChoiceView Rework (#4615)
    * Rewired MultiChoiceView to uses data and deprecate reliance on isAvailable
    
    * Refactored CardSelectView based on MultiChoiceView changes
    
    * Added MinMaxCapabilityWizardlet section to slot isAvailable to selectedSlot
    
    * Reworked MinMaxCapabilityWizardlet to pass in selectedData and updated the isAvailable to with selectedData
    
    * had to add setting isAvailable to false
    
    * correct date

---
+++ src/foam/nanos/crunch/ui/MinMaxCapabilityWizardletSection.js
@@ -0,0 +1,46 @@
+/**
+ * @license
+ * Copyright 2021 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.CLASS({
+  package: 'foam.nanos.crunch.ui',
+  name: 'MinMaxCapabilityWizardletSection',
+  extends: 'foam.u2.wizard.WizardletSection',
+  flags: ['web'],
+  documentation: `
+    Describes a sub-section of a wizardlet.
+  `,
+
+  requires: [
+    'foam.u2.detail.SectionView',
+    'foam.u2.detail.VerticalDetailView',
+    'foam.u2.ViewSpec',
+  ],
+
+  properties: [
+    {
+      class: 'FObjectArray',
+      of: 'foam.u2.wizard.Wizardlet',
+      name: 'choiceWizardlets',
+      factory: function() {
+        return [];
+      }
+    }
+  ],
+
+  methods: [
+    function createView() {
+      var vs = this.ViewSpec.createView(
+        this.customView, this.customView, this, this.__subContext__
+      );
+
+      this.choiceWizardlets.forEach((choiceWizardlet) => {
+        choiceWizardlet.isAvailable$ = vs.getSelectedSlot(choiceWizardlet.capability);
+      })
+
+      return vs;
+    }
+  ],
+});
