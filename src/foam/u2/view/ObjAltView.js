605ca032b13e6a2bd9513003ccdd377b990e8eb2
Olha Bahatiuk <olha@nanopay.net>
2021-01-28 15:49:45 -0500

    clean up

--- src/foam/u2/view/ObjAltView.js
+++ src/foam/u2/view/ObjAltView.js
@@ -36,11 +36,10 @@ foam.CLASS({
         );
       },
       postSet: function() {
-        var view = this.views.find(v => v[0] === this.selectedView);
-
         if ( ! this.memento )
           return;
           
+        var view = this.views.find(v => v[0] === this.selectedView);
         if ( view ) {
           this.memento.paramsObj.sV = view[1];
         } else {
