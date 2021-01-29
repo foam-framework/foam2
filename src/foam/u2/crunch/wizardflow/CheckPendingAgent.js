26989a41fba38de1a27290c9680c2a15a0085903
Minsun <minsun@nanopay.net>
2021-01-29 12:57:10 -0500

    end sequence

--- src/foam/u2/crunch/wizardflow/CheckPendingAgent.js
+++ src/foam/u2/crunch/wizardflow/CheckPendingAgent.js
@@ -59,6 +59,7 @@ foam.CLASS({
       if ( ucj.status !== this.CapabilityJunctionStatus.AVAILABLE ) {
         var statusPending = ucj.status === this.CapabilityJunctionStatus.PENDING;
         var shouldReopen = await this.crunchService.maybeReopen(this.ctrl.__subContext__, ucj.targetId);
+
         if ( ! shouldReopen && this.showToast ) {
           var message = statusPending ? this.CANNOT_OPEN_PENDING : this.CANNOT_OPEN_GRANTED;
           this.ctrl.notify(message, '', this.LogLevel.INFO, true);
@@ -70,6 +71,10 @@ foam.CLASS({
       if ( shouldReopen && this.capabilities.length < 1 && this.showToast ) {
         // This is here because of a CertifyDataReviewed capability.
         this.ctrl.notify(this.CANNOT_OPEN_ACTION_PENDING);
+        this.cancelled = true;
+        this.sequence.endSequence();
+      }
+      if ( ! shouldReopen && statusPending ) {
         this.cancelled = true;
         this.sequence.endSequence();
       }
