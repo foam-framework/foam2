587c0b7d56c4bce413bbde1c5cf9a4d233e96af3
Minsun <minsun@nanopay.net>
2021-01-29 22:23:40 -0500

    clean up

--- src/foam/u2/crunch/wizardflow/CheckPendingAgent.js
+++ src/foam/u2/crunch/wizardflow/CheckPendingAgent.js
@@ -56,24 +56,30 @@ foam.CLASS({
       var ucj = await this.crunchService.getJunction(null, this.rootCapability.id);
 
       var shouldReopen = false;
-
-      if ( ucj.status == this.CapabilityJunctionStatus.PENDING ) {
+      if ( ucj.status !== this.CapabilityJunctionStatus.AVAILABLE ) {
+        var statusPending = ucj.status === this.CapabilityJunctionStatus.PENDING;
         var shouldReopen = await this.crunchService.maybeReopen(this.ctrl.__subContext__, ucj.targetId);
-
-        if ( this.showToast ) {
-          if ( ! shouldReopen ) {
+        if ( ! shouldReopen ) {
+          if ( this.showToast ) {
+            // checks for PENDING etc to display the correct message
             var message = statusPending ? this.CANNOT_OPEN_PENDING : this.CANNOT_OPEN_GRANTED;
             this.ctrl.notify(message, '', this.LogLevel.INFO, true);
-          } else if ( shouldReopen && this.capabilities.length < 1 ) {
+          }
+
+          this.cancelled = true;
+          this.sequence.endSequence();
+
+          return;
+        } else {
+          if ( this.showToast && this.capabilities.length < 1 ) {
             // This is here because of a CertifyDataReviewed capability.
             this.ctrl.notify(this.CANNOT_OPEN_ACTION_PENDING);
+            this.cancelled = true;
+            this.sequence.endSequence();
+
+            return;
           }
         }
-
-        this.cancelled = true;
-        this.sequence.endSequence();
-
-        return;
       }
     }
   ]
