85979ba467135a9b3c46df93709d0bdef3a1631f
kristina <penzital888@gmail.com>
2021-02-02 11:01:45 -0500

    add IsClassPredicate

---
+++ src/foam/nanos/ruler/predicate/IsClassPredicate.js
@@ -0,0 +1,36 @@
+/**
+ * @license
+ * Copyright 2019 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.CLASS({
+  package: 'foam.nanos.ruler.predicate',
+  name: 'IsClassPredicate',
+
+  documentation: 'Returns true if new object is class of',
+
+  extends: 'foam.mlang.predicate.AbstractPredicate',
+  implements: ['foam.core.Serializable'],
+
+  javaImports: [
+    'static foam.mlang.MLang.*'
+  ],
+  properties: [
+    {
+      class: 'Class',
+      name: 'of',
+      documentation: 'class that we want the object to be an class of'
+    }
+  ],
+  methods: [
+    {
+      name: 'f',
+      javaCode: `
+        return EQ(
+          DOT(NEW_OBJ, CLASS_OF(getOf().getObjClass())), true
+        ).f(obj);
+      `
+    }
+  ]
+});
