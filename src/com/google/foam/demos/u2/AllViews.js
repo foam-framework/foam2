57e2f50502cbff5df036e4e1e33dd6a2000697ea
Eric <eric@nanopay.net>
2021-02-03 16:27:30 -0500

    Fix problems with FObjectArray

--- src/com/google/foam/demos/u2/AllViews.js
+++ src/com/google/foam/demos/u2/AllViews.js
@@ -60,6 +60,20 @@ foam.CLASS({
 
 foam.CLASS({
   package: 'com.google.foam.demos.u2',
+  name: 'SampleData3',
+  properties: [
+    {
+      name: 'check',
+      class: 'Boolean',
+      validateObj: function (check) {
+        return check ? '' : 'Please check the box';
+      }
+    }
+  ],
+});
+
+foam.CLASS({
+  package: 'com.google.foam.demos.u2',
   name: 'AllViews',
 
   requires: [
@@ -412,6 +426,27 @@ foam.CLASS({
       view: { class: 'foam.u2.view.DAOtoFObjectArrayView', xxxdelegate: { class: 'foam.comics.InlineBrowserView' } },
       factory: function() {
         return this.sampleDataDAO.testData;
+      }
+    },
+    {
+      class: 'FObjectArray',
+      name: 'fobjectArray5',
+      of: 'com.google.foam.demos.u2.SampleData3',
+      view: {
+        class: 'foam.u2.view.FObjectArrayView',
+        mode: 'RW',
+        enableAdding: true,
+        enableRemoving: true,
+        defaultNewItem: ''
+      },
+      autoValidate: true,
+      validationTextVisible: true,
+      validateObj: function(fobjectArray5, fobjectArray5$errors) {
+        console.log('called');
+        if ( fobjectArray5.length < 1 )
+          return 'Please enter fobjectArray5 information'
+        console.log('...', fobjectArray5$errors)
+        return fobjectArray5$errors
       }
     },
     {
