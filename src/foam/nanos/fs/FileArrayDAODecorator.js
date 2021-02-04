f6f6d4b5b3e1d7ca2f815eb061dfdfc548b6ddea
Mykola Kolombet <mykola@nanopay.net>
2021-02-04 14:06:39 -0500

    fix indentation

--- src/foam/nanos/fs/FileArrayDAODecorator.js
+++ src/foam/nanos/fs/FileArrayDAODecorator.js
@@ -24,7 +24,7 @@ foam.CLASS({
     {
       class: 'Long',
       name: 'maxStringDataSize',
-      value: 1024 * 3
+      value: 1024 * 3 ** 10
     }
   ],
 
@@ -52,6 +52,10 @@ foam.CLASS({
       });
 
       return Promise.all(promises).then((values) => {
+        values.map(f => f.map(f2 => {
+          f2.dataString = undefined;
+          f2.data = undefined;
+        }));
         props.forEach((prop, i) => {
           prop.set(obj, values[i]);
         });
