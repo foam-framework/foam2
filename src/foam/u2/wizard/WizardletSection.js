26f4d454ea155f3d68eed6ede7aac474bb03eb02
Minsun <minsun@nanopay.net>
2021-02-05 14:54:56 -0500

    temp

--- src/foam/u2/wizard/WizardletSection.js
+++ src/foam/u2/wizard/WizardletSection.js
@@ -53,6 +53,7 @@ foam.CLASS({
       name: 'isValid',
       class: 'Boolean',
       expression: function (wizardlet$of, data, data$errors_) {
+        if ( ! wizardlet$of ) return true;
         if ( ! data ) return false;
 
         let sectionErrors = [];
