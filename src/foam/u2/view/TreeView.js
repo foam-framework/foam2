7d99b0bfd399af8079ce7d7b0f8443b787bc98cf
Olha Bahatiuk <olha@nanopay.net>
2021-02-09 16:12:14 -0500

    some padding config

--- src/foam/u2/view/TreeView.js
+++ src/foam/u2/view/TreeView.js
@@ -237,7 +237,7 @@ foam.CLASS({
                 'float':         'right',
                 'display':       'inline-block',
                 'height':        '6px',
-                'padding':       '3px 0px 3px 3px',
+                'padding':       '7px 0px 8px 3px',
                 'transform':     self.expanded$.map(function(c) { return c ? 'rotate(0deg)' : 'rotate(90deg)'; })
               }).
               on('click', this.toggleExpanded).
