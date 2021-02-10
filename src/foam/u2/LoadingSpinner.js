866bbaf128fdd42a1edb805082890c5ea0280c20
Kevin Greer <kgrgreer@gmail.com>
2021-02-10 14:21:35 -0500

    Change LoadingSpinner to use JS instead of CSS to animate.

--- src/foam/u2/LoadingSpinner.js
+++ src/foam/u2/LoadingSpinner.js
@@ -22,38 +22,6 @@ foam.CLASS({
           display: none;
         }
 
-        ^ img {
-          -webkit-animation-duration: 3s;
-          -webkit-animation-timing-function: linear;
-          -webkit-animation-delay: 0s;
-          -webkit-animation-iteration-count: infinite;
-          -webkit-animation-direction: normal;
-          -webkit-animation-fill-mode: none;
-          -webkit-animation-play-state: running;
-          -webkit-animation-name: spin;
-          
-          -moz-animation-duration: 3s;
-          -moz-animation-timing-function: linear;
-          -moz-animation-delay: 0s;
-          -moz-animation-iteration-count: infinite;
-          -moz-animation-direction: normal;
-          -moz-animation-fill-mode: none;
-          -moz-animation-play-state: running;
-          -moz-animation-name: spin;
-          
-          animation-duration: 3s;
-          animation-timing-function: linear;
-          animation-delay: 0s;
-          animation-iteration-count: infinite;
-          animation-direction: normal;
-          animation-fill-mode: none;
-          animation-play-state: running;
-          animation-name: spin;
-        }
-        @-moz-keyframes spin { 100% { -moz-transform: rotate(360deg); } }
-        @-webkit-keyframes spin { 100% { -webkit-transform: rotate(360deg); } }
-        @keyframes spin { 100% { -webkit-transform: rotate(360deg); transform:rotate(360deg); } }
-
         ^ .processing-notice {
           text-align: center;
         }
@@ -75,6 +43,10 @@ foam.CLASS({
     {
       class: 'String',
       name: 'text'
+    },
+    {
+      class: 'Int',
+      name: 'angle'
     }
   ],
 
@@ -84,20 +56,37 @@ foam.CLASS({
 
       this
         .addClass(this.myClass()).enableClass('hidden', this.isHidden$)
-        .start({ class: 'foam.u2.tag.Image', data: 'images/ic-loading.svg' }).end()
+        .start({ class: 'foam.u2.tag.Image', data: 'images/ic-loading.svg' })
+          .style({transform: this.angle$.map(a => 'rotate(' + a + 'deg)')})
+        .end()
         .start()
-            .show(this.showText$)
-            .addClass('processing-notice')
-            .add(this.text)
+        .addClass('processing-notice')
+          .show(this.showText$)
+          .add(this.text)
         .end();
+
+      this.tick();
     },
 
     function show() {
       this.isHidden = false;
+      this.tick();
     },
 
     function hide() {
       this.isHidden = true;
+    }
+  ],
+
+  listeners: [
+    {
+      name: 'tick',
+      isFramed: 'true',
+      code: function() {
+        if ( this.isHidden ) return;
+        this.angle = (this.angle + 6) % 360;
+        this.tick();
+      }
     }
   ]
 });
