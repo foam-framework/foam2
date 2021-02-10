b015e8d06d2f9800fe7e939d9f489a61bdcdb034
Ruby <32865869+noodlemoodle@users.noreply.github.com>
2021-02-10 06:12:26 -0500

    revert changes for dataIsSet (#4785)
    * revert changes for dataIsSet
    
    * use getpayload.cleardata instead of set data to null
    
    * reverting revert of the fix - this works

--- src/foam/nanos/crunch/UserCapabilityJunctionRefine.js
+++ src/foam/nanos/crunch/UserCapabilityJunctionRefine.js
@@ -141,12 +141,10 @@ foam.CLASS({
       storageTransient: true,
       getter: function () { return this.payload.data },
       javaGetter: `
-        if ( ! dataIsSet_ ) return null;
         return getPayload().getData();
       `,
       setter: function (nu) { this.payload.data = nu; },
       javaSetter: `
-        dataIsSet_ = true;
         getPayload().setData(val);
       `
     },
