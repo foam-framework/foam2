153fd33d55a56eab8a9a44ef85cbcf3dc9bd3253
Minsun <minsun@nanopay.net>
2021-02-11 16:32:48 -0500

    NOC template:

--- src/foam/u2/DeleteModal.js
+++ src/foam/u2/DeleteModal.js
@@ -46,7 +46,7 @@ foam.CLASS({
   messages: [
     { name: 'TITLE', message: 'Delete ' },
     { name: 'CONFIRM_DELETE_1', message: 'Are you sure you want to delete' },
-    { name: 'SUCCESS_MSG', message: ' deleted.' },
+    { name: 'SUCCESS_MSG', message: ' deleted' },
     { name: 'FAIL_MSG', message: 'Failed to delete' }
   ],
 
