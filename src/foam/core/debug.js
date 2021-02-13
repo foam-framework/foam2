5af3e3aadffd612aa968eb41386de3ff09cbbada
Kevin Greer <kgrgreer@gmail.com>
2021-02-13 10:44:56 -0500

    Remove duplicate import warning.

--- src/foam/core/debug.js
+++ src/foam/core/debug.js
@@ -446,17 +446,6 @@ foam.CLASS({
         }
       }
     }
-  ],
-
-  methods: [
-    function installInClass(c, superImport) {
-      // Produce warning for duplicate imports
-      if ( superImport && superImport.equals(this) ) {
-        this.__context__.warn(
-          'Import "' + this.name + '" already exists in ancestor class of ' +
-          c.id + '.');
-      }
-    }
   ]
 });
 
