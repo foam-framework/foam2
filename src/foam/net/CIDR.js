9539515deaa8458035775d8287aa369d2823d9cd
Joel Hughes <joel@nanopay.net>
2021-02-17 13:33:28 -0500

    Add java ip2long, long2ip and support calculating/generating low/high and start/end addresses from java as well. Rework notation expression
    NP-3587 #time 1h

--- src/foam/net/CIDR.js
+++ src/foam/net/CIDR.js
@@ -12,8 +12,17 @@ foam.CLASS({
 @see https://en.wikipedia.org/wiki/Classless_Inter-Domain_Routing
 List entries are of the form: 172.0.0.0/24 - this would restrict logins to 172.xxx.xxx adresses.`,
 
-  javaImports: [
-    'java.net.InetAddress'
+  constants: [
+    {
+      name: 'NOTATION_MIN_LENGTH',
+      type: 'Integer',
+      value: 10
+    },
+    {
+      name: 'NOTATION_MAX_LENGTH',
+      type: 'Integer',
+      value: 18
+    }
   ],
 
   properties: [
@@ -23,51 +32,44 @@ List entries are of the form: 172.0.0.0/24 - this would restrict logins to 172.x
       class: 'String',
       placeholder: '10.0.0.0/24',
       required: true,
-      maxLength: 18,
-      onKey: true,
+      onKey: false,
       validateObj: function(notation) {
-        if ( ! notation || notation.length > 18 ) {
+        if ( ! notation ||
+             notation.length < this.NOTATION_MIN_LENGTH ||
+             notation.length > this.NOTATION_MAX_LENGTH ) {
           return 'invalid notation';
         }
-        var m = notation && notation.match(/\d+/g); // [ '198', '162', '1', '1', '24' ]
+        var m = notation.match(/\d+/g); // [ '198', '162', '1', '1', '24' ]
         if ( ! m || m.length < 5 ) {
           return 'invalid notation';
         }
+      },
+      javaSetter: `
+      if ( val != null &&
+           val.length() >= NOTATION_MIN_LENGTH &&
+           val.length() <= NOTATION_MAX_LENGTH ) {
+        try {
+          calculateAddresses(val);
+        } catch ( java.net.UnknownHostException e ) {
+          throw new foam.core.FOAMException(e.getMessage(), e);
+        }
       }
+      notation_ = val;
+      notationIsSet_ = true;
+      `
     },
     {
       flags: ['js'],
-      name: 'calculateAddresses_',
+      name: 'calculate_',
       class: 'String',
-      hidden: true,
-//      visibility: 'HIDDEN',
+      visibility: 'HIDDEN',
       expression: function(notation) {
-        // https://stackoverflow.com/a/33003795
-        function u(n) {
-          return n >>> 0; // unsigned
-        };
-        function ip(n) {
-          return [
-            (n >>> 24) & 0xFF,
-            (n >>> 16) & 0xFF,
-            (n >>>  8) & 0xFF,
-            (n >>>  0) & 0xFF
-          ].join('.');
-        };
-
-        var m = notation.match(/\d+/g); // [ '198', '162', '1', '1', '24' ]
-        if ( ! m || m.length < 5 ) {
-          console.warn('invalid notation', notation);
-          return;
+        if ( notation &&
+                notation.length >= this.NOTATION_MIN_LENGTH &&
+                notation.length <= this.NOTATION_MAX_LENGTH ) {
+          this.calculateAddresses(notation);
         }
-        var addr32 = m.slice(0, 4).reduce(function (a, o) {
-          return u(+a << 8) + +o;
-        });                           // 0xc6a20101
-        let mask = u(~0 << (32 - +m[4])); // 0xffffff00
-        this.low = u(addr32 & mask);
-        this.high = u(addr32 | ~mask);
-        this.startAddress = ip(this.low);
-        this.endAddress = ip(this.high);
+        return notation;
       }
     },
     {
@@ -98,6 +100,56 @@ List entries are of the form: 172.0.0.0/24 - this would restrict logins to 172.x
 
   methods: [
     {
+      name: 'calculateAddresses',
+      args: [
+        {
+          name: 'notation',
+          type: 'String'
+        }
+      ],
+      code: function(notation) {
+        // https://stackoverflow.com/a/33003795
+        function u(n) {
+          return n >>> 0; // unsigned
+        };
+        function ip(n) {
+          return [
+            (n >>> 24) & 0xFF,
+            (n >>> 16) & 0xFF,
+            (n >>>  8) & 0xFF,
+            (n >>>  0) & 0xFF
+          ].join('.');
+        };
+
+        var m = notation.match(/\d+/g); // [ '198', '162', '1', '1', '24' ]
+        if ( ! m || m.length < 5 ) {
+          console.warn('invalid notation', notation);
+          return;
+        }
+        var addr32 = m.slice(0, 4).reduce(function (a, o) {
+          return u(+a << 8) + +o;
+        });                           // 0xc6a20101
+        let mask = u(~0 << (32 - +m[4])); // 0xffffff00
+        this.low = u(addr32 & mask);
+        this.high = u(addr32 | ~mask);
+        this.startAddress = ip(this.low);
+        this.endAddress = ip(this.high);
+      },
+      javaThrows: ['java.net.UnknownHostException'],
+      javaCode: `
+      String[] tokens = notation.split("/");
+      if ( tokens.length < 2 ) {
+        throw new foam.core.FOAMException("Invalid CIDR notation");
+      }
+      long addr32 = IPSupport.instance().ip2long(tokens[0]);
+      long mask = Long.parseLong(tokens[1]);
+      setLow(addr32 & mask);
+      setHigh(addr32 & mask);
+      setStartAddress(IPSupport.instance().long2ip(getLow()));
+      setEndAddress(IPSupport.instance().long2ip(getHigh()));
+      `
+    },
+    {
       name: 'inRange',
       args: [
         {
@@ -112,43 +164,10 @@ List entries are of the form: 172.0.0.0/24 - this would restrict logins to 172.x
       type: 'Boolean',
       javaThrows: ['java.net.UnknownHostException'],
       javaCode: `
-      long low = getLow();
-      if ( low == 0 ) {
-        low = ip2Long(getStartAddress());
-        setLow(low);
-      }
-      long high = getHigh();
-      if ( high == 0 ) {
-        high = ip2Long(getEndAddress());
-        setHigh(high);
-      }
-      long test = ip2Long(ip);
-
-      return test >= low &&
-             test <= high;
-      `
-    },
-    {
-      name: 'ip2Long',
-      args: [
-        {
-          name: 'ip',
-          type: 'String'
-        }
-      ],
-      type: 'Long',
-      javaThrows: ['java.net.UnknownHostException'],
-      javaCode: `
-      InetAddress address = InetAddress.getByName(ip);
-      byte[] octets = address.getAddress();
-      long result = 0;
-      for (byte octet : octets) {
-        result <<= 8;
-        result |= octet & 0xff;
-      }
-      return result;
+      long test = IPSupport.instance().ip2long(ip);
+      return test >= getLow() &&
+             test <= getHigh();
       `
     }
-
   ]
 });
