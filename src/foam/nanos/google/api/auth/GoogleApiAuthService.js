1548146aed7cb5dbd9df3bcaeb191f41a903db15
Olha Bahatiuk <olha@nanopay.net>
2021-02-18 18:02:20 -0500

    importing class

--- src/foam/nanos/google/api/auth/GoogleApiAuthService.js
+++ src/foam/nanos/google/api/auth/GoogleApiAuthService.js
@@ -22,6 +22,7 @@ foam.CLASS({
     'java.io.FileInputStream',
     'java.io.IOException',
     'java.io.InputStreamReader',
+    'javax.servlet.http.HttpServletRequest',
     'java.util.Arrays',
     'java.util.List'
   ],
@@ -54,7 +55,7 @@ foam.CLASS({
         'java.io.IOException'
       ],
       javaCode: `
-        javax.servlet.http.HttpServletRequest req = (javax.servlet.http.HttpServletRequest)x.get(javax.servlet.http.HttpServletRequest.class);
+        HttpServletRequest req = (HttpServletRequest)x.get(HttpServletRequest.class);
         GoogleApiCredentials credentialsConfig = (GoogleApiCredentials) ((DAO)getX().get("googleApiCredentialsDAO")).find(req.getServerName());
         
         if ( credentialsConfig == null )
