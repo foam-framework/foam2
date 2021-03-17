b7a5b0210e49235a5f10d0b10e2d50fb68afa544
Kevin Greer <kgrgreer@gmail.com>
2021-03-16 21:21:43 -0400

    Replace bind with arrow function.

--- src/foam/nanos/script/Script.js
+++ src/foam/nanos/script/Script.js
@@ -379,9 +379,9 @@ foam.CLASS({
     {
       name: 'runScript',
       code: function() {
-        var log = function() {
+        var log = () => {
           this.output += Array.from(arguments).join('') + '\n';
-        }.bind(this);
+        };
         try {
           with ({ log: log, print: log, x: this.__context__ })
           return Promise.resolve(eval(this.code));
