29ac135ef6d78b7230f0ca916b36dcdd267a162b
Joel Hughes <jlhughes@users.noreply.github.com>
2021-03-17 15:26:10 -0400

    Add explicitly desired logLevel support in AppConfig. (#4970)
    * Add explicitly desired logLevel support in AppConfig.
    Can be controlled at runtime via service logLevelFilterLogger.
    Only write to syslogd in PRODUCTION, only need one copy of log files.
    
    * Install StdoutLoggerDAO in context so it can be controlled at runtime.

--- src/foam/nanos/logger/StdoutLoggerDAO.js
+++ src/foam/nanos/logger/StdoutLoggerDAO.js
@@ -34,41 +34,27 @@ foam.CLASS({
     }
   ],
 
-  axioms: [
-    {
-      name: 'javaExtras',
-      buildJavaClass: function(cls) {
-        cls.extras.push(foam.java.Code.create({
-          data: `
-  protected static ThreadLocal<foam.util.FastTimestamper> timestamper_ = new ThreadLocal<foam.util.FastTimestamper>() {
-    @Override
-    protected foam.util.FastTimestamper initialValue() {
-      foam.util.FastTimestamper ft = new foam.util.FastTimestamper();
-      return ft;
-    }
-  };
-          `
-        }));
-      }
-    }
-  ],
-
   methods: [
     {
       name: 'put_',
       javaCode: `
-      LogMessage lm = (LogMessage) getDelegate().put_(x, obj);
+      LogMessage lm = (LogMessage) obj;
       if ( getEnabled() &&
            lm != null ) {
-        // Only write INFO, WARN, ERROR to SYSLOG in production to reduce
-        // burden on syslogd, journald. With our own journal logs we are
-        // effectively writing out logs twice. 
-        if ( getMode() != Mode.PRODUCTION ||
-             lm.getSeverity().getOrdinal() >= LogLevel.INFO.getOrdinal() ) {
-          System.err.println(timestamper_.get().createTimestamp(lm.getCreated().getTime())+","+lm.getThread()+","+lm.getSeverity()+","+lm.getMessage());
+        if ( ( getMode() != Mode.PRODUCTION &&
+               lm.getSeverity().getOrdinal() == LogLevel.DEBUG.getOrdinal() ) ||
+             lm.getSeverity().getOrdinal() == LogLevel.INFO.getOrdinal() ) {
+          System.out.println(lm.toString());
+        } else if ( lm.getSeverity().getOrdinal() > LogLevel.INFO.getOrdinal() ) {
+          System.err.println(lm.toString());
         }
+        // In PRODUCTION only write to syslogd for performance, no filesystem log
+        if ( getMode() != Mode.PRODUCTION ) {
+          return getDelegate().put_(x, lm);
+        }
+        return lm;
       }
-      return lm;
+      return getDelegate().put_(x, obj);
       `
     }
   ]
