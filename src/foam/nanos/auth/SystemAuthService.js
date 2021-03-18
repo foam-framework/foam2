604b59756aa0bda6752f377472ecc42ebcbbe0d9
Patrick Zanowski <zahnno@gmail.com>
2021-03-18 15:24:13 -0400

    isAdmin Deprecate (#4986)

--- src/foam/nanos/auth/SystemAuthService.js
+++ src/foam/nanos/auth/SystemAuthService.js
@@ -8,12 +8,19 @@ foam.CLASS({
   package: 'foam.nanos.auth',
   name: 'SystemAuthService',
   extends: 'foam.nanos.auth.ProxyAuthService',
+
+  javaImports: [
+    'foam.nanos.auth.Group',
+    'javax.security.auth.AuthPermission'
+  ],
+
   methods: [
     {
       name: 'check',
       javaCode: `
-        foam.nanos.auth.User user = ((foam.nanos.auth.Subject) x.get("subject")).getUser();
-        return user != null && user.isAdmin() || getDelegate().check(x, permission);
+        User user = ((Subject) x.get("subject")).getUser();
+        Group group = (Group) x.get("group");
+        return user != null && group != null && group.implies(x, new AuthPermission("*")) || getDelegate().check(x, permission);
       `
     }
   ]
