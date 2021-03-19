56976d2b84fab6e19930eb02bc745d4846e64db4
Joel Hughes <jlhughes@users.noreply.github.com>
2021-03-19 11:04:06 -0400

    LoggerJournal - support runtime control of writing to the file system (#4978)

--- src/foam/nanos/logger/StdoutLoggerDAO.js
+++ src/foam/nanos/logger/StdoutLoggerDAO.js
@@ -11,50 +11,23 @@ foam.CLASS({
 
   documentation: `Logger decorator which writes log message to System.out`,
 
-  implements: [
-    'foam.nanos.auth.EnabledAware'
-  ],
-  
   javaImports: [
-    'foam.log.LogLevel',
-    'foam.nanos.app.Mode'
-  ],
-
-  properties: [
-    {
-      name: 'enabled',
-      class: 'Boolean',
-      value: true
-    },
-    {
-      name: 'mode',
-      class: 'Enum',
-      of: 'foam.nanos.app.Mode',
-      value: 'DEVELOPMENT'
-    }
+    'foam.log.LogLevel'
   ],
 
   methods: [
     {
       name: 'put_',
       javaCode: `
-      LogMessage lm = (LogMessage) obj;
-      if ( getEnabled() &&
-           lm != null ) {
-        if ( ( getMode() != Mode.PRODUCTION &&
-               lm.getSeverity().getOrdinal() == LogLevel.DEBUG.getOrdinal() ) ||
-             lm.getSeverity().getOrdinal() == LogLevel.INFO.getOrdinal() ) {
+      LogMessage lm = (LogMessage) getDelegate().put_(x, obj);
+      if ( lm != null ) {
+        if ( lm.getSeverity().getOrdinal() <= LogLevel.INFO.getOrdinal() ) {
           System.out.println(lm.toString());
         } else if ( lm.getSeverity().getOrdinal() > LogLevel.INFO.getOrdinal() ) {
           System.err.println(lm.toString());
         }
-        // In PRODUCTION only write to syslogd for performance, no filesystem log
-        if ( getMode() != Mode.PRODUCTION ) {
-          return getDelegate().put_(x, lm);
-        }
-        return lm;
       }
-      return getDelegate().put_(x, obj);
+      return lm;
       `
     }
   ]
