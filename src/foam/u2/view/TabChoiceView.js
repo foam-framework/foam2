7d326121b52b54e1591150190b7faf77072a33a2
garfield jian <41084370+yij793@users.noreply.github.com>
2021-03-22 15:54:24 -0400

    add translation to notification page (#5001)
    * add translation to notification page
    
    * remove extra translations
    
    * fix lint
    
    * add english version
    
    * move notification translation to foam

--- src/foam/u2/view/TabChoiceView.js
+++ src/foam/u2/view/TabChoiceView.js
@@ -82,7 +82,7 @@ foam.CLASS({
           start('label').
             attrs({for: id}).
             start('span').
-              add(ctrl.__subContext__.translationService.getTranslation(foam.locale, c[1], c[1])).
+              translate(c[1]).
             end().
           end();
       }.bind(this)));
