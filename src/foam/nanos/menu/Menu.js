8f460dc24eb087f3859a299be73c23174c5b2bd6
Kevin Greer <kgrgreer@gmail.com>
2021-03-24 16:32:47 -0400

    Added 'border' property to Menu for setting top-level border for screen.

--- src/foam/nanos/menu/Menu.js
+++ src/foam/nanos/menu/Menu.js
@@ -93,6 +93,11 @@
       displayWidth: 80
     },
     {
+      class: 'foam.u2.ViewSpec',
+      name: 'border',
+      value: { class: 'foam.u2.borders.NullBorder' }
+    },
+    {
       class: 'FObjectArray',
       of: 'foam.nanos.menu.XRegistration',
       name: 'registrations'
