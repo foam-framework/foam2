e2fd020abda55f7b1abac9ce0c609a6248be4641
Kevin Greer <kgrgreer@gmail.com>
2021-03-28 23:45:52 -0400

    Prevent from running in node.js.

--- src/com/google/foam/demos/examples/bench.js
+++ src/com/google/foam/demos/examples/bench.js
@@ -1,28 +1,31 @@
+
+// Prevent from running in node.js
+if ( globalThis.window ) {
+
 var N = 1000;
 
-function bench() {
-console.log('finished');
+console.log('started');
 
 function test1() {
-  var startTime = Date.now();
+  var startTime = performance.now();
   var node = foam.u2.Element.create({nodeName: 'UL'});     // Create a <ul> node
   for ( var i = 0 ; i < N ; i++ )
-    node.start('li').add("1text" + i).end();                // Append an <li>
+    node.start('li').setID(null).add("1text" + i).end();                // Append an <li>
   node.write(document);
-  console.log('U2 (pre)', Date.now() - startTime);
+  console.log('U2 (pre)', performance.now() - startTime);
 }
 
 function test2() {
-  var startTime = Date.now();
+  var startTime = performance.now();
   var node = foam.u2.Element.create({nodeName: 'UL'});     // Create a <ul> node
   node.write(document);
   for ( var i = 0 ; i < N ; i++ )
     node.start('li').add("2text" + i).end();                // Append an <li>
-  console.log('U2 (post)', Date.now() - startTime);
+  console.log('U2 (post)', performance.now() - startTime);
 }
 
 function test3() {
-  var startTime = Date.now();
+  var startTime = performance.now();
   var node = document.createElement("UL");               // Create a <ul> node
   for ( var i = 0 ; i < N ; i++ ) {
     var li = document.createElement("LI")
@@ -30,11 +33,11 @@ function test3() {
     node.appendChild(li);
   }
   document.body.appendChild(node);
-  console.log('DOM (pre)', Date.now() - startTime);
+  console.log('DOM (pre)', performance.now() - startTime);
 }
 
 function test4() {
-  var startTime = Date.now();
+  var startTime = performance.now();
   var node = document.createElement("UL");               // Create a <ul> node
   document.body.appendChild(node);
   for ( var i = 0 ; i < N ; i++ ) {
@@ -42,28 +45,28 @@ function test4() {
     li.appendChild(document.createTextNode("4text" + i)); // Append an <li>
     node.appendChild(li);
   }
-  console.log('DOM (post)', Date.now() - startTime);
+  console.log('DOM (post)', performance.now() - startTime);
 }
 
 function test5() {
-  var startTime = Date.now();
+  var startTime = performance.now();
   var node = '<ul>';
   for ( var i = 0 ; i < N ; i++ ) {
     node = node + '<li>5text' + i + '</li>'
   }
   node = node + '</ul>';
   document.body.outerHTML = node;
-  console.log('outerHTML', Date.now() - startTime);
+  console.log('outerHTML', performance.now() - startTime);
 }
 
 function test6() {
   var v = foam.u2.DefaultValidator.create();
 
-  var startTime = Date.now();
+  var startTime = performance.now();
   for ( var i = 0 ; i < N ; i++ ) {
     v.sanitizeText('6text' + i);
   }
-  console.log('sanitize', Date.now() - startTime);
+  console.log('sanitize', performance.now() - startTime);
 }
 
 function allTests() {
@@ -84,4 +87,5 @@ allTests();
 console.profile('p1');
 test1();
 console.profileEnd();
+
 }
