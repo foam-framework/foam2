815f2f4929c8f4b90455e2a2c28020dda852e1d7
Eric <eric@nanopay.net>
2021-03-29 15:50:11 -0400

    Add Block border

---
+++ src/foam/u2/borders/Block.js
@@ -0,0 +1,18 @@
+foam.CLASS({
+  package: 'foam.u2.borders',
+  name: 'Block',
+  extends: 'foam.u2.Element',
+
+  css: `
+    ^ {
+      border-left: 4px solid /*%GREY4%*/ #e7eaec;
+      padding-left: 16px;
+    }
+  `,
+
+  methods: [
+    function initE() {
+      this.addClass(this.myClass());
+    }
+  ]
+});
