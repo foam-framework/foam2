ca1ce1488f63cbf3b736e9f05663730fbee57534
Kevin Greer <kgrgreer@gmail.com>
2021-04-01 13:57:50 -0400

    Add Date MultiView example to AllViews.

--- src/com/google/foam/demos/u2/AllViews.js
+++ src/com/google/foam/demos/u2/AllViews.js
@@ -365,9 +365,12 @@ foam.CLASS({
       class: 'Date',
       name: 'dateRWAndRO',
       view: {
-        class: 'foam.u2.view.DualView',
-        viewa: 'foam.u2.DateView',
-        viewb: {class: 'foam.u2.DateView', mode: foam.u2.DisplayMode.RO}
+        class: 'foam.u2.MultiView',
+        views: [
+          'foam.u2.view.DateView',
+          'foam.u2.view.DateView',
+          {class: 'foam.u2.view.DateView', mode: foam.u2.DisplayMode.RO}
+        ]
       }
     },
     {
