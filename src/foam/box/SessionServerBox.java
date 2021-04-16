ade3ac3e512e52c3cb9a38b7d98fe02bc9ef5405
Chanmann Lim <chanmannlim@gmail.com>
2021-04-16 16:07:41 -0400

    Fix sub context service override

--- src/foam/box/SessionServerBox.java
+++ src/foam/box/SessionServerBox.java
@@ -6,6 +6,7 @@
 
 package foam.box;
 
+import foam.core.SubX;
 import foam.core.X;
 import foam.core.XLocator;
 import foam.dao.DAO;
@@ -175,6 +176,12 @@ public class SessionServerBox
         logger.warning("Missing permission", group != null ? group.getId() : "NO GROUP");
         msg.replyWithException(e);
         return;
+      }
+
+      // Sub context might have service override for the delegate
+      if ( effectiveContext instanceof SubX ) {
+        ((Skeleton) getDelegate()).setDelegateObject(
+          effectiveContext.get(getX().get(NSpec.class).getId()));
       }
 
       msg.getLocalAttributes().put("x", effectiveContext);
