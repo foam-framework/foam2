db68f1d0c56469321d6029e14ac7706d1dc09f74
Chanmann Lim <chanmannlim@gmail.com>
2021-04-18 02:16:45 -0400

    Keep track of services added to subX and prevent propagating to parent context (for null override)

--- src/foam/core/SubX.java
+++ src/foam/core/SubX.java
@@ -6,6 +6,8 @@
 
 package foam.core;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.function.Supplier;
 
 public class SubX extends ProxyX {
@@ -19,6 +21,19 @@ public class SubX extends ProxyX {
    */
   protected String parent_;
 
+  /**
+   * Keys of all services added to the subX.
+   *
+   * Storing {@code serviceKeys} allows preventing {@code get(key)} call from
+   * propagating to the parent context when overriding a service as null.
+   */
+  protected Set serviceKeys_;
+
+  /**
+   * Key of {@code serviceKeys} to put in the subX
+   */
+  public static final String SERVICE_KEYS = "_SubX_";
+
   public SubX(Supplier<X> root, String parent) {
     this(root, parent, new ProxyX());
   }
@@ -26,7 +41,11 @@ public class SubX extends ProxyX {
   public SubX(Supplier<X> root, String parent, X delegate) {
     root_ = root;
     parent_ = parent;
-    setX(delegate);
+
+    serviceKeys_ = (Set) delegate.get(SERVICE_KEYS);
+    if ( serviceKeys_ == null ) serviceKeys_ = new HashSet();
+
+    setX(delegate.put(SERVICE_KEYS, serviceKeys_));
   }
 
   private X getParent() {
@@ -37,31 +56,38 @@ public class SubX extends ProxyX {
   @Override
   public Object get(X x, Object key) {
     var ret = getX().get(x, key);
-    return ret != null ? ret : getParent().get(x, key);
+    return ret != null || serviceKeys_.contains(key) ? ret
+      : getParent().get(x, key);
   }
 
   @Override
   public X put(Object key, Object value) {
+    serviceKeys_.add(key);
     if ( getX() instanceof ProxyX ) {
       getX().put(key, value);
       return this;
     }
-    return new SubX(root_, parent_, new ProxyX(getX())).put(key, value);
+
+    var delegate = new ProxyX(getX()).put(SERVICE_KEYS, new HashSet(serviceKeys_));
+    return new SubX(root_, parent_, delegate).put(key, value);
   }
 
   @Override
   public X putFactory(Object key, XFactory factory) {
+    serviceKeys_.add(key);
     if ( getX() instanceof ProxyX ) {
       getX().putFactory(key, factory);
       return this;
     }
-    return new SubX(root_, parent_, new ProxyX(getX())).putFactory(key, factory);
+
+    var delegate = new ProxyX(getX()).put(SERVICE_KEYS, new HashSet(serviceKeys_));
+    return new SubX(root_, parent_, delegate).putFactory(key, factory);
   }
 
   @Override
   public int getInt(Object key, int defaultValue) {
     Number i = (Number) getX().get(key);
-    if ( i == null ) {
+    if ( i == null && ! serviceKeys_.contains(key) ) {
       i = (Number) getParent().get(key);
     }
     return i == null ? defaultValue : i.intValue();
@@ -70,7 +96,7 @@ public class SubX extends ProxyX {
   @Override
   public boolean getBoolean(Object key, boolean defaultValue) {
     Boolean b = (Boolean) getX().get(key);
-    if ( b == null ) {
+    if ( b == null && ! serviceKeys_.contains(key) ) {
       b = (Boolean) getParent().get(key);
     }
     return b == null ? defaultValue : b;
