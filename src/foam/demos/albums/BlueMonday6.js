7fdf6c0f7f829789bd342a72dfd5597311cff258
Kevin Greer <kgrgreer@gmail.com>
2021-04-19 12:37:11 -0400

    Update demo.

--- src/foam/demos/albums/BlueMonday6.js
+++ src/foam/demos/albums/BlueMonday6.js
@@ -19,14 +19,15 @@ foam.CLASS({
   properties: [
     [ 'width',  500 ],
     [ 'height', 500 ],
-    [ 'color', 'black' ]
+    [ 'color', 'black' ],
+    [ 'sides', 6 ],
   ],
 
   methods: [
     function initCView() {
       this.SUPER();
 
-      var s = 6, r = 0;
+      var s = this.sides, r = -3*Math.PI/2;
       this.add(this.RegularPolygon.create({x:250, y:250, rotation: r, sides: s, radius:167, arcWidth: 22, border: 'dodgerblue', shadowColor: 'dodgerblue', shadowBlur: 15, alpha: 0.6}));
       this.add(this.RegularPolygon.create({x:250, y:250, rotation: r, sides: s, radius:140, arcWidth: 5, border: 'deepskyblue'}));
       this.add(this.RegularPolygon.create({x:250, y:250, rotation: r, sides: s, radius:120-2.5, arcWidth: 15, border: 'darkgreen', shadowColor: 'green', shadowBlur: 15, alpha: 0.5}));
