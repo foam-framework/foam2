370b7c5749f4774ec8a907547a21b29544aefdf3
Arthur Pavlovs <arthur@nanopay.net>
2021-04-20 13:22:45 -0400

    add support for single cache entry purging

---
+++ src/foam/dao/PurgeRecordCmd.js
@@ -0,0 +1,12 @@
+foam.CLASS({
+  package: 'foam.dao',
+  name: 'PurgeRecordCmd',
+  documentation: `
+    This model is passed to the dao cmd_ method to specify an individual
+    item to be purged from the cache.
+  `,
+
+  properties: [
+    'id'
+  ]
+});
