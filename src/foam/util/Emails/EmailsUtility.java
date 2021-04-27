aa239a71feddaf5355c1c2748db8497deb608198
Mykola Kolombet <mykola@nanopay.net>
2021-04-27 04:24:16 -0400

    add property validation

--- src/foam/util/Emails/EmailsUtility.java
+++ src/foam/util/Emails/EmailsUtility.java
@@ -116,19 +116,19 @@ public class EmailsUtility {
       templateArgs.put("appName", (theme.getAppName()));
 
       templateArgs.put("locale", user.getLanguage().getCode().toString());
-  
+
       foam.nanos.auth.Address address = supportConfig.getSupportAddress();
       templateArgs.put("supportAddress", address == null ? "" : address.toSummary());
       templateArgs.put("supportPhone", (supportConfig.getSupportPhone()));
       templateArgs.put("supportEmail", (supportConfig.getSupportEmail()));
-  
+
       // personal support user
       User psUser = supportConfig.findPersonalSupportUser(x);
       templateArgs.put("personalSupportPhone", psUser == null ? "" : psUser.getPhoneNumber());
       templateArgs.put("personalSupportEmail", psUser == null ? "" : psUser.getEmail());
       templateArgs.put("personalSupportFirstName", psUser == null ? "" : psUser.getFirstName());
       templateArgs.put("personalSupportFullName", psUser == null ? "" : psUser.getLegalName());
-      
+
       emailMessage.setTemplateArguments(templateArgs);
     }
 
