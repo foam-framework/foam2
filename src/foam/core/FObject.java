a78c952f8cd85758e3c91393b7d23b50805c82f5
Joel Hughes <joel@nanopay.net>
2021-04-29 00:42:26 -0400

    Remove unused Iterator import

--- src/foam/core/FObject.java
+++ src/foam/core/FObject.java
@@ -12,7 +12,6 @@ import foam.lib.json.Outputter;
 import foam.util.SecurityUtil;
 import java.security.*;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.ArrayList;
